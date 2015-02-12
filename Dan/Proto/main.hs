fe63b15615fb9c273e6cdfbe73c49bcea0a83e47
szymczdm <szymczdm@mcmaster.ca>
2015-02-11 22:15:04 -0500

    Modified the code slightly, added Recipe as [DocType] and moved each print to its own function. Currently having issues creating the docs based on the recipe.

--- Dan/Proto/main.hs
+++ Dan/Proto/main.hs
@@ -8,15 +8,19 @@ data DocType = SRS
              | LPM
              | Code
 
-             
-gen = do
-  outh <- openFile "SRS.tex" WriteMode
-  hPutStrLn outh $ render $ createSRS
-  hClose outh
-  outh <- openFile "LPM.tex" WriteMode
-  hPutStrLn outh $ render $ createLPM
-  hClose outh
+data Recipe = Recipe [DocType]
+
+gen r = do prnt SRS
+           prnt LPM
   
+prnt SRS = do outh <- openFile "SRS.tex" WriteMode
+              hPutStrLn outh $ render $ createSRS
+              hClose outh
+prnt LPM = do outh <- openFile "LPM.tex" WriteMode
+              hPutStrLn outh $ render $ createLPM
+              hClose outh
+prnt _ = error "Invalid recipe"
+
 auth = "Spencer Smith"
 
 
@@ -32,4 +36,4 @@ createLPM = lpre $$ title "Literate Programmer's Manual for $h_g$ and $h_c$" $$
             author auth $$ begin
   
 main = do
-  gen
+  gen (Recipe [SRS, LPM])

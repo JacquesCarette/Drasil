7cee1b02d039139aa34f37d0935060152a62815b
szymczdm <szymczdm@mcmaster.ca>
2015-03-09 04:10:10 -0400

    Minor additions

--- Dan/Proto/h_c.hs
+++ Dan/Proto/h_c.hs
@@ -6,6 +6,7 @@ h_c :: Chunk FName FDesc
 h_c = newChunk $
   [("Symbol",text "$h_c$"),
    ("Equation", text "\\frac{ 2k_{c}h_{b}}{2k_{c}+\\tau_ch_{b}}"),
-   ("Description", text "convective heat transfer coefficient between clad and coolant"),
+   ("Description", 
+    text "convective heat transfer coefficient between clad and coolant"),
    ("SIU", text "($\\mathrm{\\frac{kW}{m^2C}}$)")
   ]

fdb1fec242979a0ded924b515d446fac66c54f47
szymczdm <szymczdm@mcmaster.ca>
2015-05-06 14:04:28 -0400

    implemented Expr lang for equations. Working on TeX printing now.

--- Dan/Proto/h_g.hs
+++ Dan/Proto/h_g.hs
@@ -1,5 +1,6 @@
 module H_g where
 import Chunk
+import ASTInternal
 import Text.PrettyPrint
 import Tau_c
 import H_p
@@ -8,7 +9,9 @@ import K_c
 h_g :: Chunk FName FDesc
 h_g = newChunk $
   [("Symbol",text "$h_g$"),
-   ("Equation", text "\\frac{2k_{c}h_{p}}{2k_{c}+\\tau_c h_{p}}"),
+   ("Equation", text "\\frac{2" <> get "Equation" k_c <> get "Equation" h_p <>
+                  text "}{2" <> get "Equation" k_c <> text "+" <> 
+                  get "Equation" tau_c <> get "Equation" h_p <> text "}"),
    ("SIU", text "($\\mathrm{\\frac{kW}{m^2C}}$)"),
    ("Description", 
     text "effective heat transfer coefficient between clad and fuel surface")
@@ -18,3 +21,5 @@ h_g_dep :: Dependency
 h_g_dep = [tau_c,
            h_p,
            k_c]
+           
+h_g_eq = Frac ((Dbl 2):*(c k_c):*(c h_p)) ((Dbl 2):*(c k_c):+(c tau_c):*(c h_p))

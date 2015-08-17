636892c69f62f3695eacba9883f5d16c10071799
Dan Szymczak <dan.szymczak@gmail.com>
2015-08-17 17:00:58 -0400

    Changed "print" to "build" to clarify what the function does. Will use print for other function(s).

--- Dan/Proto/Mark II/PrintTeX_MK2.hs
+++ Dan/Proto/Mark II/PrintTeX_MK2.hs
@@ -7,8 +7,10 @@ import qualified ASTInternal_MK2 as A
 import Prelude hiding (print)
 
 genTeX :: A.DocType -> A.Document -> Doc
-genTeX typ doc = print typ $ makeDocument doc
+genTeX typ doc = build typ $ makeDocument doc
 
-print :: A.DocType -> Document -> Doc
-print typ (Document title author contents) = text ""
-  
+build :: A.DocType -> Document -> Doc
+build A.SRS (Document title author contents) = text ""
+build A.LPM (Document title author contents) = text ""
+build A.Code (Document title author contents) = error "Unimplemented"
+

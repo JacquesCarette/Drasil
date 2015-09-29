5bd919a44d54c4a2055d4880f108aeb0fc734721
Jacques Carette <carette@mcmaster.ca>
2015-09-29 15:18:00 -0400

    simplify gen and prnt

--- Dan/Proto/Mark II/Gen.hs
+++ Dan/Proto/Mark II/Gen.hs
@@ -9,21 +9,21 @@ data Recipe = Recipe DocType String Doc
         --DocType, Filename, 'Body'
 
 gen :: [Recipe] -> IO ()
-gen ((Recipe x y z):[]) = do prnt x y z
-gen ((Recipe x y z):xs) = do prnt x y z
-                             gen xs
-gen ([])                  = return ()
+gen rl = mapM_ prnt rl
 
-prnt :: DocType -> String -> Doc -> IO ()  
-prnt SRS filename body = do outh <- openFile filename WriteMode
-                            hPutStrLn outh $ render $ body
-                            hClose outh
-prnt LPM filename body = do outh <- openFile filename WriteMode
-                            hPutStrLn outh $ render $ body
-                            hClose outh
+prnt :: Recipe -> IO ()
+prnt (Recipe SRS filename body) = 
+  do outh <- openFile filename WriteMode
+     hPutStrLn outh $ render $ body
+     hClose outh
+prnt (Recipe LPM filename body) = 
+  do outh <- openFile filename WriteMode
+     hPutStrLn outh $ render $ body
+     hClose outh
+
   -- No difference b/w SRS and LPM as yet, but there may be in the future so
   -- keeping them separate for the time being is a good idea
-prnt Code _ _ = error "Code DocType is not implemented yet"
+prnt (Recipe Code _ _) = error "Code DocType is not implemented yet"
 
 writeDoc :: OutFormat -> DocType -> Document -> Doc
 writeDoc TeX    = genTeX

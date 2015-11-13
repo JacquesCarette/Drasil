61b09918d46e3523b4c6cd029a4c98b87a7c4ec7
Dan Szymczak <dan.szymczak@gmail.com>
2015-11-13 15:00:59 -0500

    Created Chunk typeclass.

--- Dan/Proto/Chunk.hs
+++ Dan/Proto/Chunk.hs
@@ -1,25 +1,26 @@
 {-# OPTIONS -Wall #-} 
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Chunk where
 
 import qualified Data.Map.Strict as Map
 import qualified ASTInternal as AST
 import Data.Maybe (fromMaybe)
-import Format
+import Control.Lens
 
 --How to design the chunks? --
 
 --varname is redundant, hopefully can be removed.
 --Going to need a chunk type to make these instances of.
 --Not sure what to do with it right now.
-data Stub a = Stub { stubname :: AST.Spec a
-                 , stubvarname :: AST.Spec a
-                 , stubdescription :: AST.Spec a}
+-- data Stub a = Stub { stubname :: AST.Spec a
+                 -- , stubvarname :: AST.Spec a
+                 -- , stubdescription :: AST.Spec a}
 
-data Calc a = Calc { calcname :: AST.Spec a
-                 , calcdescription :: AST.Spec a
-                 , calcequation :: AST.Spec a
-                 , calcdependencies :: AST.Spec a
-                 , calcsiu :: AST.Spec a}
+-- data Calc a = Calc { calcname :: AST.Spec a
+                 -- , calcdescription :: AST.Spec a
+                 -- , calcequation :: AST.Spec a
+                 -- , calcdependencies :: AST.Spec a
+                 -- , calcsiu :: AST.Spec a}
 
                  
 -- class (Format b) => Chunk (T b) where --with T as a type constructor for Stub or Calc
@@ -35,6 +36,24 @@ data Calc a = Calc { calcname :: AST.Spec a
   -- description = calcdescription
   
 --Keep this stuff for now until the Chunk type is solidified.
+
+class Chunk c mode where
+   name :: Simple Lens c (AST.Spec mode)
+   descr :: Simple Lens c (AST.Spec mode)
+   
+-- class Chunk c where
+   -- type SRep c :: *
+   -- name :: Simple Lens c (SRep c)
+   -- descr :: Simple Lens c (SRep c)
+
+class Chunk c mode => EqChunk c mode where
+  equat :: Simple Lens c (AST.Spec mode)
+  symbol :: Simple Lens c (AST.Spec mode)
+
+-- You can allow more generality by not hard-coding String, and do something like
+
+
+
 
 newChunk :: String -> [(AST.FName, AST.FDesc a)] -> AST.Chunk a
 newChunk nm l = AST.Chunk nm (Map.fromList l)

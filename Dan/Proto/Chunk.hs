43f9a84f990ce77eb56186e8c5a40a6adfea03b9
Dan Szymczak <dan.szymczak@gmail.com>
2015-11-16 23:17:51 -0500

    First attempt at new style of chunk creation. Lenses are not yet implemented for the data types. Not sure if this makes sense right now.

--- Dan/Proto/Chunk.hs
+++ Dan/Proto/Chunk.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS -Wall #-} 
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 module Chunk where
 
 import qualified Data.Map.Strict as Map
@@ -12,18 +12,28 @@ import Control.Lens
 class Chunk c mode where
    name :: Simple Lens c (AST.Spec mode)
    descr :: Simple Lens c (AST.Spec mode)
-   
--- class Chunk c where
-   -- type SRep c :: *
-   -- name :: Simple Lens c (SRep c)
-   -- descr :: Simple Lens c (SRep c)
+   symbol :: Simple Lens c (AST.Spec mode)
 
 class Chunk c mode => EqChunk c mode where
   equat :: Simple Lens c (AST.Spec mode)
-  symbol :: Simple Lens c (AST.Spec mode)
+  siu :: Simple Lens c (AST.Spec mode)
+  dependencies :: Simple Lens c (AST.Spec mode)
 
+data VarChunk mode = VC { vname :: AST.Spec mode
+                        , vdesc :: AST.Spec mode
+                        , vsymb :: AST.Spec mode}
 
+data FullChunk mode = FC { cname :: AST.Spec mode
+                         , cdesc :: AST.Spec mode
+                         , csymb :: AST.Spec mode 
+                         , cequat :: AST.Spec mode
+                         , csiu :: AST.Spec mode
+                         , cdep :: AST.Spec mode }
 
+instance Chunk (VarChunk mode) mode where
+instance Chunk (FullChunk mode) mode where
+
+instance EqChunk (FullChunk mode) mode where
 
 newChunk :: String -> [(AST.FName, AST.FDesc a)] -> AST.Chunk a
 newChunk nm l = AST.Chunk nm (Map.fromList l)

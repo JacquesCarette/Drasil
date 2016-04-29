60f356dc19d887ea25141e768462aa4184542aeb
Jacques Carette <carette@mcmaster.ca>
2016-04-29 14:19:54 -0400

    superscript in Spec do not make sense (anymore)

--- Dan/Proto/Spec.hs
+++ Dan/Proto/Spec.hs
@@ -12,7 +12,6 @@ data Spec where
   N     :: Symbol -> Spec
   Sy    :: USymb -> Spec
   S     :: String -> Spec           -- Strings, used for Descriptions in Chunks
-  (:^:) :: Spec -> Spec -> Spec     -- Superscript (Spec :^: Spec -> Spec^{Spec} in TeX)
   (:+:) :: Spec -> Spec -> Spec     -- Concatenation of two Specs (e.g. delta :+: T -> deltaT)
   (:/:) :: Spec -> Spec -> Spec     -- Fractions (Spec :/: Spec -> frac{Spec}{Spec} in TeX)
   Empty :: Spec                     -- Blank
@@ -45,7 +44,6 @@ instance Show RefType where
 -- (if we generalize mapping somehow)
 sMap :: (String->String) -> Spec -> Spec
 sMap f (S a) = S (f a)
-sMap f (a :^: b) = sMap f a :^: sMap f b
 sMap f (a :+: b) = sMap f a :+: sMap f b
 sMap f (a :/: b) = sMap f a :/: sMap f b
 sMap _ a = a

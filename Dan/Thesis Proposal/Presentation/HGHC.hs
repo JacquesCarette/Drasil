ea8b5cda403dae5ed2af7d2aff0e1f9fa98d962a
Dan Szymczak <dan.szymczak@gmail.com>
2016-06-23 00:50:10 -0400

    Minor tweaks to presentation.

--- Dan/Thesis Proposal/Presentation/HGHC.hs
+++ Dan/Thesis Proposal/Presentation/HGHC.hs
@@ -25,21 +25,5 @@ srs ls author body =
     (foldr1 (:+:) (intersperse (S " and ") (map (\x -> U $ x ^. symbol) ls))))
     (S author) body  
   
-srsBody,lpmBody :: Document
+srsBody :: Document
 srsBody = srs vars "Spencer Smith" [s1, s2, s3, s4]
-  
-lpmBody = Document ((S "Literate Programmer's Manual for ") :+: 
-  (foldr1 (:+:) (intersperse (S " and ") (map (\x -> U $ x ^. symbol) vars))))
-  (S "Spencer Smith") [l1]
-
-l1 :: LayoutObj
-l1 = Paragraph (
-  S "@ First we define the overall structure of the library of functions." :+:
-  S "\n\n@c\n@<Header files@>@/\n@<Functions@>@/\n\n" :+:
-  S "Although not necessary for this simple example, we will include the " :+:
-  S "math library, since future expansion will likely require it.\n\n" :+:
-  S "@<Header files@>=\n#include <math.h>\n\n" :+:
-  S "@ This library will consist of a set of functions.\n\n" :+:
-  S "@<Functions@>=\n@<Function to Calculate hg@>@/\n" :+: 
-  S "@<Function to Calculate hc@>@/\n\n"  
-  )

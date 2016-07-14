9d6151c039b348f9e42fb8992c1b73ba39f01abe
Jacques Carette <carette@mcmaster.ca>
2016-07-14 16:04:43 -0400

    instance of Unit' for MUChunk

--- code/Language/Drasil/Chunk/MUChunk.hs
+++ code/Language/Drasil/Chunk/MUChunk.hs
@@ -4,7 +4,7 @@ module Language.Drasil.Chunk.MUChunk (MUChunk(..)) where
 import Control.Lens (Simple, Lens, (^.), set)
 
 import Language.Drasil.Chunk (Chunk(..), Concept(..), Quantity(..))
-import Language.Drasil.Unit (Unit)
+import Language.Drasil.Unit (Unit(..),Unit'(..))
 
 data MUChunk where --May have Unit chunk
   Has :: (Quantity h, Unit h) => h -> MUChunk
@@ -19,8 +19,11 @@ instance Concept MUChunk where
 instance Quantity MUChunk where
   symbol = mulens symbol
   
--- instance Unit MUChunk where
+instance Unit' MUChunk where
+  unit' f (Has    h) = fmap (Has . maybe h (\t -> set unit t h)) (f $ Just $ h^.unit)
+  unit' f (HasNot h) = fmap (HasNot . maybe h (\_ -> h)) (f $ Nothing)
 
+-- utilities which should not be exported
 mulens :: (forall c. Quantity c => Simple Lens c a) -> Simple Lens MUChunk a
 mulens l f (Has a) = fmap (\x -> Has (set l x a)) (f (a ^. l))
 mulens l f (HasNot a) = fmap (\x -> HasNot (set l x a)) (f (a ^. l))

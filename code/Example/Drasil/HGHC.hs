c56ecd4546a93957fd71e5ccddd0ea7dd96b4976
Jacques Carette <carette@mcmaster.ca>
2016-09-22 21:26:34 -0400

    lpm is so bit rotted that it is best to delete it entirely.

--- code/Example/Drasil/HGHC.hs
+++ code/Example/Drasil/HGHC.hs
@@ -45,20 +45,3 @@ mgBody = doc "MG" vars "Spencer Smith" mgSecs
 
 misBody :: Document
 misBody = doc "MIS" vars "Spencer Smith" misSecs
-  
--- lpmBody :: Document  
--- lpmBody = Document ((S "Literate Programmer's Manual for ") :+: 
-  -- (foldr1 (:+:) (intersperse (S " and ") (map (\x -> U $ x ^. symbol) vars))))
-  -- (S "Spencer Smith") [l1]
-
--- l1 :: LayoutObj
--- l1 = Paragraph (
-  -- S "@ First we define the overall structure of the library of functions." :+:
-  -- S "\n\n@c\n@<Header files@>@/\n@<Functions@>@/\n\n" :+:
-  -- S "Although not necessary for this simple example, we will include the " :+:
-  -- S "math library, since future expansion will likely require it.\n\n" :+:
-  -- S "@<Header files@>=\n#include <math.h>\n\n" :+:
-  -- S "@ This library will consist of a set of functions.\n\n" :+:
-  -- S "@<Functions@>=\n@<Function to Calculate hg@>@/\n" :+: 
-  -- S "@<Function to Calculate hc@>@/\n\n"  
-  -- )

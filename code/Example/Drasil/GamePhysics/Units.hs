65f1875f8c82dcfb470905276265193b1523bbe6
Jacques Carette <carette@mcmaster.ca>
2016-10-30 22:31:58 -0400

    and indeed do remove Units.hs

--- code/Example/Drasil/GamePhysics/Units.hs
+++
@@ -1,9 +0,0 @@
-module Drasil.GamePhysics.Units where
-
-import Language.Drasil
-import Data.Drasil.SI_Units
-
------ Table of Units -----
-
-cpSIUnits :: [UnitDefn]
-cpSIUnits = map UU [metre, kilogram, second] ++ map UU [newton, radians]

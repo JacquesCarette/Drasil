c13abaa58f2af45a7fa089df376072947c2655cf
Jacques Carette <carette@mcmaster.ca>
2016-12-24 18:33:00 -0500

    warnings

--- code/Language/Drasil/Code/Imperative/LanguageRenderer/ObjectiveCRenderer.hs
+++ code/Language/Drasil/Code/Imperative/LanguageRenderer/ObjectiveCRenderer.hs
@@ -5,14 +5,15 @@ module Language.Drasil.Code.Imperative.LanguageRenderer.ObjectiveCRenderer (
 ) where
 
 import Language.Drasil.Code.Code (Code(..))
-import Language.Drasil.Code.Imperative.AST hiding (comment,bool,int,float,char)
+import Language.Drasil.Code.Imperative.AST 
+  hiding (comment,bool,int,float,char,tryBody,catchBody,initState,guard,
+          update,forBody)
 import Language.Drasil.Code.Imperative.LanguageRenderer
 import Language.Drasil.Code.Imperative.Helpers (blank,oneTab,oneTabbed,
                                             doubleQuotedText,himap,vmap,vibmap)
 
-import Data.List (intersperse)
-import Prelude hiding (break,print,return)
-import Text.PrettyPrint.HughesPJ hiding (Str)
+import Prelude hiding (break,print,return,init)
+import Text.PrettyPrint.HughesPJ hiding (Str,integer)
 
 validListTypes :: [Label]
 validListTypes = ["NSArray", "NSMutableArray"]
@@ -60,7 +61,9 @@ objcConfig options c =
         clsDecDoc = clsDecDocD c, clsDecListDoc = clsDecListDocD c, classDoc = classDoc' c, objAccessDoc = objAccessDoc' c,
         objVarDoc = objVarDocD c, paramDoc = paramDoc' c, paramListDoc = paramListDoc' c, patternDoc = patternDocD c, printDoc = printDoc' c, retDoc = retDocD c, scopeDoc = scopeDocD,
         stateDoc = stateDocD c, stateListDoc = stateListDocD c, statementDoc = statementDocD c, methodDoc = methodDoc' c,
-        methodListDoc = methodListDoc' c, methodTypeDoc = methodTypeDoc' c, unOpDoc = unOpDoc', valueDoc = valueDoc' c
+        methodListDoc = methodListDoc' c, methodTypeDoc = methodTypeDoc' c, unOpDoc = unOpDoc', valueDoc = valueDoc' c,
+
+        getEnv = \_ -> error "getEnv not implemented (yet) in ObjC"
     }
 
 -- for convenience
@@ -96,7 +99,7 @@ objcstateType _ (Type name) Dec    = text name <> ptr
 objcstateType c s d                = stateTypeD c s d
 
 objctop :: Config -> FileType -> Label -> Doc
-objctop c Header p = vcat [
+objctop c Header _ = vcat [
     include c "<Foundation/NSObject.h>",
     include c "<Foundation/NSString.h>",
     include c "<Foundation/NSArray.h>"]
@@ -144,7 +147,7 @@ exceptionDoc' c (TryCatch tryBody catchBody) = vcat [
     rbrace]
 
 exprDoc' :: Config -> Expression -> Doc
-exprDoc' c (BinaryExpr v1 r v2@(Lit (LitStr _))) = objAccessDoc c v1 $ Func "isEqual" [v2]
+exprDoc' c (BinaryExpr v1 _ v2@(Lit (LitStr _))) = objAccessDoc c v1 $ Func "isEqual" [v2]
 exprDoc' c (Exists (Arg i)) = argsListAccess c i
 exprDoc' c e = exprDocD' c e
 
@@ -160,13 +163,13 @@ funcDoc' c ListSize = innerFuncAppDoc c "count" []
 funcDoc' c (ListAccess i) = innerFuncAppDoc c "objectAtIndex" [i]
 funcDoc' c (ListAdd _ v) = innerFuncAppDoc c "addObject" [v]
 funcDoc' c (ListSet i v) = space <> text "replaceObjectAtIndex:" <+> valueDoc c i <+> text "withObject:" <+> valueDoc c v
-funcDoc' c (ListPopulate s t) = error "ListPopulate must be rendered at the ObjAccess level in Objective-C"
+funcDoc' _ (ListPopulate _ _) = error "ListPopulate must be rendered at the ObjAccess level in Objective-C"
 funcDoc' c f = funcDocD c f
 
 iterationDoc' :: Config -> Iteration -> Doc
 --the index is declared inside the for statement (e.g. 'for (int i = 0;...)');
 --must compile the resulting files with gcc -std=c99 when doing this
-iterationDoc' c (ForEach i listVar@(ListVar _ t) b) = iterationDoc c $ For initState guard update $ bodyReplace (Var i) (listVar $. at i) b
+iterationDoc' c (ForEach i listVar@(ListVar _ _) b) = iterationDoc c $ For initState guard update $ bodyReplace (Var i) (listVar $. at i) b
     where initState = varDecDef i (Base Integer) (litInt 0)
           guard     = Var i ?< (listVar $. ListSize)
           update    = (&++)i
@@ -220,7 +223,7 @@ classDoc' c ft@(Source) _ (Class n _ _ vs fs) =
         text "@end"
     ]
 classDoc' _ (Header) _ (MainClass _ _ _) = empty
-classDoc' c ft m (MainClass n vs fs) = vcat [
+classDoc' c ft _ (MainClass n vs fs) = vcat [
     stateListDoc c vs,
     stateBlank,
     methodListDoc c ft n fs]
@@ -230,11 +233,11 @@ objAccessDoc' :: Config -> Value -> Function -> Doc
 objAccessDoc' c v f@(Cast _) = objAccessDocD c v f
 objAccessDoc' c v (ListPopulate size t) = iterationDoc c $ For (varDecDef i (Base Integer) (litInt 0)) (Var i ?< size) ((&++)i) forBody
     where i = "i"
-          dftVal = case t of Base bt   -> defaultValue bt
-                             otherwise -> error $ "ListPopulate does not yet support list type " ++ render (doubleQuotes $ stateType c t Def)
+          dftVal = case t of Base bt -> defaultValue bt
+                             _       -> error $ "ListPopulate does not yet support list type " ++ render (doubleQuotes $ stateType c t Def)
           forBody = oneLiner $ ValState $ v $. ListAdd (Var i) dftVal
-objAccessDoc' c v f@(Floor) = text "floor" <> parens(valueDoc c v)
-objAccessDoc' c v f@(Ceiling) = text "ceil" <> parens(valueDoc c v)
+objAccessDoc' c v (Floor) = text "floor" <> parens(valueDoc c v)
+objAccessDoc' c v (Ceiling) = text "ceil" <> parens(valueDoc c v)
 objAccessDoc' c v f = brackets (valueDoc c v <> funcDoc c f)
 
 paramDoc' :: Config -> Parameter -> Doc
@@ -247,10 +250,12 @@ paramListDoc' c ps = colonMapListDoc (text " : ") (paramDoc c) ps
 printDoc' :: Config -> Bool -> StateType -> Value -> Doc    --this function assumes that the StateType and Value match up as appropriate (e.g. if the StateType is a List, then the Value should be a ListVar)
 printDoc' c newLn t v = printFunc c <> parens (text ("\"%" ++ frmt ++ nl ++ "\",") <+> value)
     where nl = if newLn then "\\n" else ""
-          value =  case t of Base String -> case v of Lit (LitStr s) -> doubleQuotedText s
-                                                      otherwise -> objAccessDoc c v (Func "UTF8String" [])
-                             otherwise -> case v of ListVar _ _ -> objAccessDoc c (v $. Func "description" []) (Func "UTF8String" [])
-                                                    otherwise -> valueDoc c v
+          value =  case t of 
+                     Base String -> 
+                       case v of Lit (LitStr s) -> doubleQuotedText s
+                                 _              -> objAccessDoc c v (Func "UTF8String" [])
+                     _ -> case v of ListVar _ _ -> objAccessDoc c (v $. Func "description" []) (Func "UTF8String" [])
+                                    _ -> valueDoc c v
           frmt = case t of Base Boolean -> "B"      --TODO does B work for Booleans? Bools might not be directly printable
                            Base Integer -> "d"
                            Base Float -> "f"
@@ -262,7 +267,7 @@ printDoc' c newLn t v = printFunc c <> parens (text ("\"%" ++ frmt ++ nl ++ "\",
 
 methodDoc' :: Config -> FileType -> Label -> Method -> Doc
 methodDoc' _ Header _ (MainMethod _) = empty
-methodDoc' c Header _ f@(Method n _ _ _ _) | isDtor n = empty
+methodDoc' c Header _ f@(Method n _ _ _ _) | isDtor n  = empty
                                            | otherwise = transDecLine c f <> endStatement c 
 methodDoc' c Source _ f@(Method _ _ (Construct _) _ b) = vcat [
     transDecLine c f <+> lbrace,
@@ -281,7 +286,7 @@ methodDoc' c Source _ f@(Method _ _ _ _ b) = vcat [
     transDecLine c f <+> lbrace,
     oneTab $ bodyDoc c b,
     rbrace]
-methodDoc' c ft@(Source) m f@(MainMethod b) = methodDocD' c ft m $ MainMethod poolB
+methodDoc' c ft@(Source) m (MainMethod b) = methodDocD' c ft m $ MainMethod poolB
     where poolB = poolDec ++ b ++ poolDrain
           poolDec = if null b then [] else oneLiner $ varDecDef "pool" (Type "NSAutoreleasePool") $ Var "[[NSAutoreleasePool alloc] init]"
           poolDrain = if null b then [] else oneLiner $ ValState $ ObjAccess (Var "pool") (Func "drain" [])
@@ -301,7 +306,7 @@ unOpDoc' Abs = text "ABS"
 unOpDoc' op = unOpDocD op
 
 valueDoc' :: Config -> Value -> Doc
-valueDoc' c (EnumElement _ e) = text e
+valueDoc' _ (EnumElement _ e) = text e
 --NSArrays of base types like bools are not allowed, so we store them as an array of NSNumbers instead and convert to base-type values when accessing the list
 valueDoc' c (ObjAccess v@(ListVar _ t) f@(ListAccess _)) = getValueDoc t $ objAccessDoc c v f
 valueDoc' c (ObjAccess v@(ObjVar _ (ListVar _ t)) f@(ListAccess _)) = getValueDoc t $ objAccessDoc c v f
@@ -332,11 +337,11 @@ isDtor ('~':_) = True
 isDtor _ = False
 
 destructor :: Config -> [StateVar] -> Method
-destructor c vs =
+destructor _ vs =
     let checkDelPriority s@(StateVar _ _ _ _ del) | del < 2   = []
-                                                | otherwise = [s]
+                                                  | otherwise = [s]
         releaseVars = concatMap checkDelPriority vs
-        releaseStatements = concatMap (\v@(StateVar lbl _ _ _ _) -> [ValState $ (Var lbl $. Func release [])]) releaseVars
+        releaseStatements = concatMap (\(StateVar lbl _ _ _ _) -> [ValState $ (Var lbl $. Func release [])]) releaseVars
         releaseBlock = if null releaseVars then [] else [Block(releaseStatements)]
         deallocBody = releaseBlock ++ (oneLiner $ ValState (super $. Func dealloc []))
     in Method dealloc Public Void [] deallocBody
@@ -355,7 +360,7 @@ getValueDoc t d = let integer = "integer" in
               Base Integer   -> valFrom integer
               Base Float     -> valFrom "float"
               Base Character -> valFrom "char"
-              otherwise      -> d
+              _              -> d
     where valFrom typeName = brackets (d <+> text (typeName ++ "Value"))
 
 nsFromCString :: Config -> Doc -> Doc
@@ -369,7 +374,7 @@ makeNumber c v = case v of EnumElement _ _  -> numFrom c (Base Integer) v
                            Lit (LitBool _)  -> numFrom c (Base Boolean) v
                            Lit (LitFloat _) -> numFrom c (Base Float) v
                            Lit (LitChar _)  -> numFrom c (Base Character) v
-                           otherwise        -> v
+                           _                -> v
 
 numFrom :: Config -> StateType -> Value -> Value
 numFrom c t v = let integer = "Integer" in
@@ -378,7 +383,7 @@ numFrom c t v = let integer = "Integer" in
               Base Integer   -> numWith integer
               Base Float     -> numWith "Float"
               Base Character -> numWith "Char"
-              otherwise      -> v
+              _              -> v
     where numWith n = Var $ "[NSNumber numberWith" ++ n ++ ": " ++ render (valueDoc c v) ++ "]"
 
 -- | formats a Doc string for the inner part of an Objective-C function call (message)

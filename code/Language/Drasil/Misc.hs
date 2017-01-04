2dfcc9d788635940db3564adc4a88181b53f5ad1
Dan Szymczak <dan.szymczak@gmail.com>
2017-01-04 15:48:33 -0500

    Reimplemented unit'2Contents in Misc.hs, will rename it to something more relevant later.

--- code/Language/Drasil/Misc.hs
+++ code/Language/Drasil/Misc.hs
@@ -2,6 +2,7 @@ module Language.Drasil.Misc where
 
 import Language.Drasil.Spec
 import Language.Drasil.Chunk.Quantity
+import Language.Drasil.Unit
 
 import Control.Lens ((^.))
 
@@ -11,17 +12,7 @@ mkTable _     []  = []
 mkTable []     _  = error "Attempting to make table without data"
 mkTable fl (c:cl) = map ($ c) fl : mkTable fl cl
 
-{-
+
 -- where should this go?
-unit'2Contents :: Unit' u => u -> Sentence
-unit'2Contents x = maybe (S "") Sy (x ^. unit')
--}
-
-{-
 unit'2Contents :: Quantity u => u -> Sentence
-unit'2Contents x = maybe (S "") Sy (x ^. getUnit)-}
-
---FIXME: This is a placeholder until getUnit is working.
---       It should be replaced with something like the above.
-unit'2Contents :: Quantity u => u -> Sentence
-unit'2Contents x = S ""
+unit'2Contents x = maybe (S "") (\y -> Sy (y ^. unit)) (getUnit x)

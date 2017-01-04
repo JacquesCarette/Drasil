d61adf1ec8d0470fe4456269aa55caeb74721df1
Dan Szymczak <dan.szymczak@gmail.com>
2017-01-04 15:01:40 -0500

    Just realized all of the commits previously missed "Quantity.hs" in Language/Drasil/Chunk/. Adding it now.

---
+++ code/Language/Drasil/Chunk/Quantity.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS -Wall #-}
+{-# LANGUAGE GADTs #-}
+module Language.Drasil.Chunk.Quantity where
+
+import Control.Lens
+
+import Language.Drasil.Space
+import Language.Drasil.Chunk
+import Language.Drasil.Unit
+import Prelude hiding (id)
+
+class NamedIdea c => Quantity c where
+  typ      :: Simple Lens c Space
+  getSymb  :: c -> Maybe SF
+  getUnit  :: c -> Maybe UnitDefn
+
+instance Quantity VarChunk where
+  getSymb vc = Just $ SF vc 
+  getUnit _  = Nothing
+  typ f (VC n d s t) = fmap (\x -> VC n d s x) (f t)
+  
+instance Quantity ConVar where
+  typ    f (CV c s t) = fmap (\x -> CV c s x) (f t)
+  getSymb   = Just . SF 
+  getUnit _ = Nothing

0989ac94fcce8e827faf7aea1aafd43359e9004c
Dan Szymczak <dan.szymczak@gmail.com>
2017-01-12 16:26:01 -0500

    Added definitions for mass and length.

--- code/Data/Drasil/Concepts/PhysicalProperties.hs
+++ code/Data/Drasil/Concepts/PhysicalProperties.hs
@@ -11,5 +11,7 @@ solid   = dcc "solid" "Solid" "solid state"
 ctrOfMass = dcc "ctrOfMass" "centre of mass" 
   "The mean location of the distribution of mass of the object."
 
-mass = dcc "mass" "mass" "FIXME: MISSING DEFINITION"
-length = dcc "length" "length" "FIXME: MISSING DEFINITION"
+mass = dcc "mass" "mass" "the quantity of matter in a body"
+length = dcc "length" "length" 
+  ("the straight-line distance between two points along an object. " ++
+  "Typically used to represent the size of an object from one end to the other.")

9a3d5094ad9b6ebe733212b4a4df5a23d210587a
Dan Szymczak <dan.szymczak@gmail.com>
2017-01-16 13:29:15 -0500

    Cleaned up sloppy conventions in Changes.hs

--- code/Example/Drasil/SWHS/Changes.hs
+++ code/Example/Drasil/SWHS/Changes.hs
@@ -14,58 +14,58 @@ ucs = [uc1, uc2, uc3, uc4, uc5, uc6]
 
 lc1, lc2, lc3, lc4, lc5, lc6, lc7, lc8, lc9, lc10, lc11, lc12 :: LCChunk
 
-lc1 = LCChunk (CC "hardware" (S "The specific hardware on which the " :+:
-      S "software is running.")) [mod_hw]
+lc1 = LCChunk (makeCC "hardware" ("The specific hardware on which the " ++
+  "software is running.")) [mod_hw]
 
-lc2 = LCChunk (CC "inputformat" (S "The format of the initial input data.")) 
-      [mod_inputf]
+lc2 = LCChunk (makeCC "inputformat" "The format of the initial input data.") 
+  [mod_inputf]
 
-lc3 = LCChunk (CC "parameters" (S "The format of the input parameters.")) 
-      [mod_inputp]
+lc3 = LCChunk (makeCC "parameters" "The format of the input parameters.") 
+  [mod_inputp]
 
-lc4 = LCChunk (CC "inputverification" (S "The constraints on the input " :+:
-      S "parameters.")) [mod_inputv]
+lc4 = LCChunk (makeCC "inputverification" "The constraints on the input parameters.")
+  [mod_inputv]
 
-lc5 = LCChunk (CC "outputformat" (S "The format of the final output data."))
-      [mod_outputf]
+lc5 = LCChunk (makeCC "outputformat" "The format of the final output data.")
+  [mod_outputf]
 
-lc6 = LCChunk (CC "outputverification" (S "The constraints on the output " :+:
-      S "results.")) [mod_outputv]
+lc6 = LCChunk (makeCC "outputverification" ("The constraints on the output results."))
+  [mod_outputv]
 
-lc7 = LCChunk (CC "temp" (S "How the governing " :+: (ordDiffEq ^. term) :+:
-      S "s are defined using the input parameters.")) [mod_temp]
+lc7 = LCChunk (ncWDS "temp" (S "How the governing " :+: (ordDiffEq ^. term) :+:
+  S "s are defined using the input parameters.")) [mod_temp]
 
-lc8 = LCChunk (CC "energy" (S "How the energy equations are defined using " :+:
-      S "the input parameters.")) [mod_ener]
+lc8 = LCChunk (makeCC "energy" ("How the energy equations are defined using " ++
+  "the input parameters.")) [mod_ener]
 
-lc9 = LCChunk (CC "control" (S "How the overall control of the calculations" :+:
-      S " is orchestrated.")) [mod_ctrl]
+lc9 = LCChunk (makeCC "control" ("How the overall control of the calculations" ++
+  " is orchestrated.")) [mod_ctrl]
 
-lc10 = LCChunk (CC "sequence" (S "The implementation for the sequence " :+:
-       S "(array) data structure.")) [mod_seq]
+lc10 = LCChunk (makeCC "sequence" ("The implementation for the sequence " ++
+  "(array) data structure.")) [mod_seq]
 
-lc11 = LCChunk (CC "ode" (S "The algorithm used for the " :+: 
-       (ordDiffEq ^. term) :+: S " solver.")) [mod_ode]
+lc11 = LCChunk (ncWDS "ode" (S "The algorithm used for the " :+: 
+  (ordDiffEq ^. term) :+: S " solver.")) [mod_ode]
 
-lc12 = LCChunk (CC "plot" (S "The implementation of plotting data.")) [mod_plot]
+lc12 = LCChunk (makeCC "plot" "The implementation of plotting data.") [mod_plot]
 
 
 uc1, uc2, uc3, uc4, uc5, uc6 :: UCChunk
 
-uc1 = CC "IO" (S "Input/Output devices (Input: File and/or Keyboard, Output" :+:
-      S ": File, Memory, and/or Screen).")
+uc1 = makeCC "IO" ("Input/Output devices (Input: File and/or Keyboard, Output" ++
+  ": File, Memory, and/or Screen).")
 
-uc2 = CC "inputsource" (S "There will always be a source of input data " :+:
-      S "external to the software.")
+uc2 = makeCC "inputsource" ("There will always be a source of input data " ++
+  "external to the software.")
 
-uc3 = CC "output" (S "Output data are displayed to the output device.")
+uc3 = makeCC "output" "Output data are displayed to the output device."
 
-uc4 = CC "goal" (S "The goal of the system is to calculate temperatures and" :+:
-      S " energies.")
+uc4 = makeCC "goal" ("The goal of the system is to calculate temperatures and" ++
+  " energies.")
 
-uc5 = CC "odes" (S "The " :+: (ordDiffEq ^. term) :+: S "s for " :+:
-      S "temperature can be defined using parameters defined in the input " :+:
-      S "parameters module.")
+uc5 = ncWDS "odes" (S "The " :+: (ordDiffEq ^. term) :+: S "s for " :+:
+  S "temperature can be defined using parameters defined in the input " :+:
+  S "parameters module.")
 
-uc6 = CC "energy" (S "The energy equations can be defined using the " :+:
-      S "parameters defined in the input parameters module.")
+uc6 = makeCC "energy" ("The energy equations can be defined using the " ++
+  "parameters defined in the input parameters module.")

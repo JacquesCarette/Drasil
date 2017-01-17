5ac74b8302c000c86e83f354a65b79da1941a6f6
Dan Szymczak <dan.szymczak@gmail.com>
2017-01-17 13:36:01 -0500

    Fixed up some sloppy conventions in GlassBR Changes.hs, leaving the Haskell variable names the same for the time being (they make it a bit easier to find where differences happen compared with the original document(s)).

--- code/Example/Drasil/GlassBR/Changes.hs
+++ code/Example/Drasil/GlassBR/Changes.hs
@@ -11,56 +11,51 @@ ucs = [uc1, uc2, uc3, uc4, uc5]
 
 lc1, lc2, lc3, lc4, lc5, lc6, lc7, lc8 :: LCChunk
 
-lc1 = LCChunk ( CC "hardware"
-    ( S "The specific hardware on which the software is running." )
+lc1 = LCChunk (
+  makeCC "hardware" "The specific hardware on which the software is running."
   ) [mod_hw]
 
-lc2 = LCChunk ( CC "input"
-    ( S "The format of the initial input data." )
+lc2 = LCChunk ( makeCC "input" "The format of the initial input data." 
   ) [mod_inputf]
 
-lc3 = LCChunk ( CC "parameters"
-    ( S "The format of the input parameters.")
+lc3 = LCChunk ( makeCC "parameters" "The format of the input parameters."
   ) [mod_inputp]
 
-lc4 = LCChunk ( CC "output"
-    ( S "The format of the final output data." )
+lc4 = LCChunk ( makeCC "output" "The format of the final output data."
   ) [mod_outputf]
 
-lc5 = LCChunk ( CC "equations"
-    ( S "How the equations are defined using the input parameters.")
+lc5 = LCChunk ( 
+  makeCC "equations" "How the equations are defined using the input parameters."
   ) [mod_calc]
 
-lc6 = LCChunk (CC "control"
-    (S "How the overall control of the calculations is orchestrated.")
+lc6 = LCChunk (makeCC "control"
+  "How the overall control of the calculations is orchestrated."
   ) [mod_ctrl]
 
-lc7 = LCChunk ( CC "interpd"
-    ( S "The format of the data used for interpolation." )
+lc7 = LCChunk ( makeCC "interpd" "The format of the data used for interpolation."
   ) [mod_interpd]
 
-lc8 = LCChunk ( CC "interp"
-    ( S "The algorithm used for interpolation.")
+lc8 = LCChunk ( makeCC "interp" "The algorithm used for interpolation."
   ) [mod_interp]
 
 
 uc1, uc2, uc3, uc4, uc5 :: UCChunk
 
-uc1 = CC "IO"
-  ( S "Input/Output devices (Input: File and/or Keyboard, Output: File, " :+:
-    S "Memory, and/or Screen)." )
+uc1 = makeCC "IO" (
+  "Input/Output devices (Input: File and/or Keyboard, Output: File, " ++
+  "Memory, and/or Screen)." )
 
-uc2 = CC "inputsource"
-  ( S "There will always be a source of input data external to the software." )
+uc2 = makeCC "inputsource" (
+  "There will always be a source of input data external to the software." )
 
-uc3 = CC "output"
-  ( S "Output data are displayed to the output device." )
+uc3 = makeCC "output" (
+  "Output data are displayed to the output device." )
 
-uc4 = CC "goal"
-  ( S "The goal of the system is to predict whether the glass slab under " :+:
-    S "consideration can withstand an explosion of a certain degree.")
+uc4 = makeCC "goal" (
+  "The goal of the system is to predict whether the glass slab under " ++
+  "consideration can withstand an explosion of a certain degree.")
 
-uc5 = CC "equations"
-  ( S "The equations for Safety can be defined using parameters defined " :+:
-    S "in the input parameters module.")
+uc5 = makeCC "equations" (
+  "The equations for Safety can be defined using parameters defined " ++
+  "in the input parameters module.")
 

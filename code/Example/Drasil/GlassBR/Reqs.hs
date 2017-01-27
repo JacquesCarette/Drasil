1ce3bb3d912653d203389d0c60abdd0e3c0f1a72
Jacques Carette <carette@mcmaster.ca>
2017-01-26 22:54:18 -0500

    add FIXME

--- code/Example/Drasil/GlassBR/Reqs.hs
+++ code/Example/Drasil/GlassBR/Reqs.hs
@@ -8,6 +8,8 @@ reqs = [r1,r2,r3,r4,r5,r6]
 
 r1,r2,r3,r4,r5,r6 :: ReqChunk
 
+-- FIXME: having id "" and term "" is completely bogus, and should not
+-- be allowed.  This implicitly says that something here does not make sense.
 r1 = ReqChunk (CC "" $ S "") [mod_hw, mod_inputf, mod_inputp, mod_ctrl]
 r2 = ReqChunk (CC "" $ S "") [mod_inputf, mod_inputp]
 r3 = ReqChunk (CC "" $ S "") [mod_inputc]

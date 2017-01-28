26e7566cafce55eebd3c092204b5710c5c6f24b7
Jacques Carette <carette@mcmaster.ca>
2017-01-28 13:58:21 -0500

    generalize how this section is built.

--- code/Example/Drasil/SpecificSystemDescription.hs
+++ code/Example/Drasil/SpecificSystemDescription.hs
@@ -1,12 +1,13 @@
 module Drasil.SpecificSystemDescription where
 
 import Language.Drasil
+import qualified Data.Drasil.Concepts.Documentation as D
 import Control.Lens ((^.))
 
 specSysDescr :: NamedIdea c => c -> [Section] -> Section
-specSysDescr sys subs = Section (S "Specific System Description") 
-  ([Con $ intro sys] ++ map Sub subs)
-  
+specSysDescr sys subs = section (D.specificsystemdescription^.term) (intro sys) subs
+
+-- FIXME: this all should be broken down and mostly generated.
 intro :: NamedIdea c => c -> Contents
 intro sys = Paragraph $ S "This section first presents the problem" +:+
   S "description, which gives a high-level view of the problem to be" +:+

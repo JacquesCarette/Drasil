d348b647ac828ed9adce8e9d61a2b792ee299d3c
Jacques Carette <carette@mcmaster.ca>
2017-01-28 17:03:44 -0500

    continue pulling out words

--- code/Example/Drasil/TableOfUnits.hs
+++ code/Example/Drasil/TableOfUnits.hs
@@ -6,6 +6,7 @@ import Control.Lens ((^.))
 import Data.Char (toLower)
 import Prelude hiding (id)
 import Language.Drasil
+import Data.Drasil.Concepts.Documentation
 
 table_of_units :: Unit s => [s] -> Section
 table_of_units u = Section (S "Table of Units") [Con s1_intro, Con (unit_table u)]
@@ -21,7 +22,8 @@ s1_intro = Paragraph
    S " name in parentheses.")
 
 unit_table :: Unit s => [s] -> Contents
-unit_table u = Table [S "Symbol", S "Description"] (mkTable
+unit_table u = Table
+  (map (^.term) [symbol_, description])  (mkTable
   [(\x -> Sy (x ^. unit)),
    (\x -> (x ^. defn) :+: S " (" :+: sMap (map toLower) (x ^. term) :+: S ")")
   ] u)

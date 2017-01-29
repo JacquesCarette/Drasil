3f1039550c0a04f7738a06ac674d15c5a3b31175
Jacques Carette <carette@mcmaster.ca>
2017-01-29 09:32:53 -0500

    Add Alex and Luthfi

--- code/Data/Drasil/Authors.hs
+++ code/Data/Drasil/Authors.hs
@@ -4,3 +4,8 @@ import Language.Drasil (Person, person)
 
 spencerSmith :: Person
 spencerSmith = person "Spencer" "Smith"
+
+-- short versions for now
+luthfi, alex :: Person
+luthfi = person "Luthfi" "Mawarid"
+alex = person "Alex" "Halliwushka"

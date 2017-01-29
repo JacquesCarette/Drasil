e3f92b5424c023bdb7af3c09fbb0ea921624cd1a
Jacques Carette <carette@mcmaster.ca>
2017-01-29 10:17:37 -0500

    further cleaning

--- code/Example/Drasil/SSP/Body.hs
+++ code/Example/Drasil/SSP/Body.hs
@@ -42,10 +42,8 @@ s1_1, s1_2, s1_3, s1_1_intro, s1_1_table, s1_2_intro, s1_2_table,
 
 s4_2_2_tmods :: [SecCons]
 
-
 -- SECTION 1 --
 s1 = Section (S "Reference Material") [s1_1, s1_2, s1_3]
-
 
 -- SECTION 1.1 --
 s1_1 = Sub $ Section (S "Table of Units") [s1_1_intro, s1_1_table]
@@ -59,7 +57,6 @@ s1_1_table = Con $ Table [S "Symbol", S "Description", S "Name"] (mkTable
   ] this_si)
   (S "Table of Units") True
 
-  
 -- SECTION 1.2 --
 s1_2 = Sub $ Section (S "Table of Symbols") [s1_2_intro, s1_2_table]
 
@@ -74,7 +71,6 @@ s1_2_table = Con $ Table [S "Symbol", S "Units", S "Description"] (mkTable
   sspSymbols)
   (S "Table of Symbols") False
 
-  
 -- SECTION 1.3 --
 s1_3 = Sub $ table_of_abb_and_acronyms acronyms
   
@@ -85,7 +81,6 @@ s2_p1 = Con $ Paragraph $ S "A slope of geological mass, composed of soil and ro
 
 s2_p2 = Con $ Paragraph $ S "The following section provides an overview of the Software Requirements Specification (SRS) for a slope stability analysis problem. The developed program will be referred to as the Slope Stability Analysis (SSA) program. This section explains the purpose of this document, the scope of the system, the organization of the document and the characteristics of the intended readers."
 
-
 -- SECTION 2.1 --
 s2_1 = Sub $ Section (S "Purpose") [s2_1_p1, s2_1_p2]
 
@@ -93,52 +88,40 @@ s2_1_p1 = Con $ Paragraph $ S "The SSA program determines the critical slip surf
 
 s2_1_p2 = Con $ Paragraph $ S "This document will be used as a starting point for subsequent development phases, including writing the design specification and the software verification and validation plan.  The design document will show how the requirements are to be realized, including decisions on the numerical algorithms and programming environment.  The verification and validation plan will show the steps that will be used to increase confidence in the software documentation and the implementation.  Although the SRS fits in a series of documents that follow the so-called waterfall model, the actual development process is not  constrained in any way.  Even when the waterfall model is not followed, as Parnas and Clements point out, the most logical way to present the documentation is still to fake a rational design process."
 
-
 -- SECTION 2.2 --
 s2_2 = Sub $ Section (S "Scope of Requirements") [s2_2_p1]
 
 s2_2_p1 = Con $ Paragraph $ S "The scope of the requirements is limited to stability analysis of a 2 dimensional slope, composed of homogeneous soil layers. Given appropriate inputs, the code for SSA will identify the most likely failure surface within the possible input range, and find the factor of safety for the slope as well as displacement of soil that will occur on the slope."
-
 
 -- SECTION 2.3 --
 s2_3 = Sub $ Section (S "Organization of Document") [s2_3_p1]
 
 s2_3_p1 = Con $ Paragraph $ S "The organization of this document follows the template for an SRS for scientific computing software proposed by Koothoor as well as Smith and Lai.  The presentation follows the standard pattern of presenting goals, theories, definitions, and assumptions.  For readers that would like a more bottom up approach, they can start reading the instance models in " :+: makeRef sec_IMs :+: S " and trace back to find any additional information they require.  The instance models provide the set of algebraic equations that must be solved iteratively to perform a Morgenstern Price Analysis. The goal statements are refined to the theoretical models (" :+: makeRef sec_TMs :+: S ") and instance models (" :+: makeRef sec_IMs :+: S ")."
 
-
-
-
 -- SECTION 3 --
 s3 = Section (S "General System Description") [s3_p1, s3_1, s3_2]
 
 s3_p1 = Con $ Paragraph $ S "This section provides general information about the system, identifies the interfaces between the system and its environment, and describes the user characteristics and the system constraints."
-
 
 -- SECTION 3.1 --
 s3_1 = Sub $ Section (S "User Characteristics") [s3_1_p1]
 
 s3_1_p1 = Con $ Paragraph $ S "The end user of SSA should have an understanding of undergraduate Level 1 Calculus and Physics, and be familiar with soil and material properties."
 
-
 -- SECTION 3.2 --
 s3_2 = Sub $ Section (S "System Constraints") [s3_2_p1]
 
 s3_2_p1 = Con $ Paragraph $ S "There are no system constraints."
-
-
-
 
 -- SECTION 4 --
 s4 = Section (S "Specific System Description") [s4_p1, s4_1, s4_2]
 
 s4_p1 = Con $ Paragraph $ S "This section first presents the problem description, which gives a high-level view of the problem to be solved.  This is followed by the solution characteristics specification, which presents the assumptions, theories, definitions and finally the instance models that model the slope."
 
-
 -- SECTION 4.1 --
 s4_1 = Sub $ Section (S "Problem Description") [s4_1_p1, s4_1_1, s4_1_2, s4_1_3]
 
 s4_1_p1 = Con $ Paragraph $ S "SSA is a computer program developed to evaluate the factor of safety of a slopes slip surface and to calculate the displacement that the slope will experience."
-
 
 -- SECTION 4.1.1 --
 s4_1_1 = Sub $ Section (S "Terminology") [s4_1_1_list]
@@ -155,7 +138,6 @@ s4_1_1_list = Con $ Enumeration $ Simple $ map (\(a,b) -> (a, Flat b)) [
   (S "Plane Strain", S "The resultant stresses in one of the directions of a 3 dimensional material can be approximated as 0. Results when the length of one dimension of the body dominates the others. Stresses in the dominate dimensions direction are the ones that can be approximated as 0.")
   ]
 
-  
 -- SECTION 4.1.2 --
 s4_1_2 = Sub $ Section (S "Physical System Description")
   [s4_1_2_p1, s4_1_2_bullets, s4_1_2_p2, s4_1_2_fig1, s4_1_2_fig2]
@@ -179,7 +161,6 @@ s4_1_2_fig2 = Con fig_forceacting
 fig_forceacting :: Contents
 fig_forceacting = Figure (S "Forces acting on a slice") "ForceDiagram.png"
 
-
 -- SECTION 4.1.3 --
 s4_1_3 = Sub $ Section (S "Goal Statements") [s4_1_3_p1, s4_1_3_list]
 
@@ -191,13 +172,11 @@ s4_1_3_list = Con $ Enumeration $ Simple $ [
   (S "GS3", Flat $ S "Determine the displacement of the slope.")
   ]
 
-  
 -- SECTION 4.2 --
 s4_2 = Sub $ Section (S "Solution Characteristics Specification")
   [s4_2_p1, s4_2_1, s4_2_2, s4_2_3, s4_2_4, s4_2_5, s4_2_6]
 
 s4_2_p1 = Con $ Paragraph $ S "The instance models that govern SSA are presented in " :+: makeRef sec_IMs :+: S ".  The information to understand the meaning of the instance models and their derivation is also presented, so that the instance models can be verified."
-
 
 -- SECTION 4.2.1 --
 s4_2_1 = Sub $ Section (S "Assumptions") [s4_2_1_p1, s4_2_1_list]
@@ -217,7 +196,6 @@ s4_2_1_list = Con $ Enumeration $ Simple $ [
   (S "A10", Flat $ S "The surface and base of a slice between interslice nodes are approximated as straight lines.")
   ]
 
-  
 -- SECTION 4.2.2 --
 s4_2_2 = Sub sec_TMs
 
@@ -228,14 +206,11 @@ s4_2_2_p1 = Con $ Paragraph $ S "This section focuses on the general equations a
 
 s4_2_2_tmods = map (Con . Definition) (map Theory [fs_rc])
 
-
 -- SECTION 4.2.3 --
 s4_2_3 = Sub $ Section (S "General Definitions") []
 
-
 -- SECTION 4.2.4 --
 s4_2_4 = Sub $ Section (S "Data Definitions") []
-
 
 -- SECTION 4.2.5 --
 s4_2_5 = Sub sec_IMs

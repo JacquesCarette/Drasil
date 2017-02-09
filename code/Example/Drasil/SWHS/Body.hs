704804169b106801ca05d0c24da47fb3b9fef626
Dan Szymczak <dan.szymczak@gmail.com>
2017-02-09 15:38:19 -0500

    Updated SWHS example SystemInformation with extra concept field. Note the redundancy currently present (this is likely to change, technically it could be replaced with '[]' right now, but I was testing tsymb').

--- code/Example/Drasil/SWHS/Body.hs
+++ code/Example/Drasil/SWHS/Body.hs
@@ -60,7 +60,10 @@ authors :: Sentence
 authors = manyNames [thulasi, brooks, spencerSmith]
 
 swhs_si :: SystemInformation
-swhs_si = SIC swhs_pcm srs [thulasi, brooks, spencerSmith] this_si swhsSymbols
+swhs_si = SI swhs_pcm srs [thulasi, brooks, spencerSmith] 
+  this_si swhsSymbols swhsSymbols --Note: The second swhsSymbols here is 
+    -- Redundant b/c the unitals are not really concepts (yet). There
+    -- Will still likely be a better way to do this.
 
 {-
 This table is ALMOST correct (just the normal vector is wrong because it should

733058e9beb2172d6f28b8c1cc6ea601aaefff51
Jacques Carette <carette@mcmaster.ca>
2017-02-27 10:37:24 -0500

    remove bad imports, delete obsolete code.

--- code/Language/Drasil/Chunk/Constrained.hs
+++ code/Language/Drasil/Chunk/Constrained.hs
@@ -1,9 +1,7 @@
 module Language.Drasil.Chunk.Constrained where
 
 import Control.Lens (Simple, Lens)
-import Prelude hiding (id)
 import Language.Drasil.Expr (Expr, Relation)
-import Language.Drasil.Chunk
 {-
 The lists of Constraints below can be implemented in multiple ways.
 For ease of writing I think having a list of functions that take
@@ -21,35 +19,3 @@ class Constrained c where
   physConstraints :: Simple Lens c [Expr -> Relation]
   sfwrConstraints :: Simple Lens c [Expr -> Relation]
   reasVal :: Simple Lens c Expr
-
- 
-{- DEPRECATED CODE BELOW
-
---Currently testing with Constrained MUC only, could probably change
---this to "ConstrainedChunk" and have constructors for CMUC, CEq, etc.
-data ConstrainedMUC = CMUC 
-  { muc :: MUChunk, 
-    pc :: [Expr -> Relation], -- physical constraints 
-    sc :: [Expr -> Relation], --software constraints
-    rv :: Expr  -- reasonable value (number?)
-  }
-  
-instance Chunk ConstrainedMUC where
-  id = ul . id
-
-instance NamedIdea ConstrainedMUC where
-  term = ul . term
-  
-instance SymbolForm ConstrainedMUC where
-  symbol = ul . symbol
-  
-
-  
-instance Constrained ConstrainedMUC where
-  physConstraints f (CMUC a b c d) = fmap (\x -> CMUC a x c d) (f b)
-  sfwrConstraints f (CMUC a b c d) = fmap (\x -> CMUC a b x d) (f c)
-  reasVal         f (CMUC a b c d) = fmap (\x -> CMUC a b c x) (f d)
-
-ul :: Simple Lens ConstrainedMUC MUChunk
-ul f (CMUC a b c d) = fmap (\x -> CMUC x b c d) (f a)
--}

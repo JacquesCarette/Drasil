821f4586144fa26978fef0fe0038f24e45d7bb39
Dan Szymczak <dan.szymczak@gmail.com>
2017-03-02 12:57:27 -0500

    Updated Data.Drasil.Concepts.Software to use ConceptChunks in place of the previous NamedChunks because they were hacks and "id" was used where it shouldn't have been.

--- code/Data/Drasil/Concepts/Software.hs
+++ code/Data/Drasil/Concepts/Software.hs
@@ -20,63 +20,63 @@ program = dcc "program" "program"
 -- MODULES Concepts (Maybe move to D.D.C.Software.Modules ?)
 
 hwHiding :: ConceptChunk
-hwHiding = dcc "hardware hiding" "hardware hiding"
-  ("Hides the exact details of the hardware, and provides a uniform interface" ++
-   "for the rest of the system to use.")
-   
-modBehavHiding :: NamedChunk
-modBehavHiding = ncWDS "behaviour hiding" (S "Includes programs that provide " :+:
-                 S "externally visible behaviour of the system as specified" :+:
-                 S " in the " :+: (sLower (srs ^. term)) :+:
-                 S " (" :+: (getAcc srs) :+: S ") documents. This module" :+:
-                 S " serves as a communication layer between the hardware-" :+:
-                 S "hiding module and the software decision module. The " :+:
-                 S "programs in this module will need to change if there " :+:
-                 S "are changes in the " :+: (getAcc srs) :+: S ".")
-    
-modControl :: NamedChunk
-modControl = ncWDS "control" (S "Provides the main program.")
+hwHiding = dcc "hwHiding" "hardware hiding" (
+  "Hides the exact details of the hardware, and provides a uniform interface" ++
+  "for the rest of the system to use.")
 
-modSfwrDecision :: NamedChunk
-modSfwrDecision = ncWDS "software decision"
-    (S "Includes data structures and algorithms used in the system that " :+:
-     S "do not provide direct interaction with the user.")
-     
-modInputFormat :: NamedChunk
-modInputFormat = ncWDS "input format"
-  (S "Converts the input data into the data structure used by the input " :+:
-   S "parameters module.")
-   
-modInputParams :: NamedChunk
-modInputParams = ncWDS "input parameters" (S "Stores the parameters needed " :+:
-                  S "for the program, including material properties, " :+:
-                  S "processing conditions, and numerical parameters. The " :+:
-                  S "values can be read as needed. This module knows how " :+:
-                  S "many parameters it stores.")
-   
-modInputConstraints :: NamedChunk
-modInputConstraints = ncWDS "input constraints"
-  (S "Defines the constraints on the input data and gives an error if " :+:
-   S "a constraint is violated.")
-   
-modInputVerif :: NamedChunk
-modInputVerif = ncWDS "input verification" (S "Verifies that the input " :+:
-                  S "parameters comply with physical and software " :+: 
-                  S "constraints. Throws an error if a parameter violates a" :+:
-                  S " physical constraint. Throws a warning if a parameter " :+:
-                  S "violates a software constraint.")
-   
-modDerivedVals :: NamedChunk
-modDerivedVals = ncWDS "derived values"
-  (S "Defines the equations transforming the initial inputs into derived " :+:
-   S "quantities.")
-   
-modInterpolation :: NamedChunk
-modInterpolation = ncWDS "interpolation"
-  (S "Provides the equations that take the input parameters and " :+:
-   S "interpolation data and return an interpolated value.")
-   
-modInterpData :: NamedChunk
-modInterpData = ncWDS "interpolation data"
-  (S "Converts the input interpolation data into the data structure used " :+:
-   S "by the interpolation module.")
+modBehavHiding :: ConceptChunk
+modBehavHiding = dccWDS "modBehavHiding" "behaviour hiding" (
+  S "Includes programs that provide externally visible behaviour of the" +:+ 
+  S "system as specified in the" +:+ (sLower (srs ^. term)) +:+
+  S "(" :+: (short srs) :+: S ") documents. This module" +:+
+  S "serves as a communication layer between the hardware-hiding module" +:+
+  S "and the software decision module. The programs in this module will need" +:+
+  S "to change if there are changes in the" +:+. (short srs))
+
+modControl :: ConceptChunk
+modControl = dcc "modControl" "control" "Provides the main program."
+
+modSfwrDecision :: ConceptChunk
+modSfwrDecision = dcc "modSfwrDecision" "software decision" 
+  ("Includes data structures and algorithms used in the system that do not " ++
+  "provide direct interaction with the user.")
+
+modInputFormat :: ConceptChunk
+modInputFormat = dcc "modInputFormat" "input format" ("Converts the input " ++
+  "data into the data structure used by the input parameters module.")
+  
+modInputParams :: ConceptChunk
+modInputParams = dccWDS "modInputParams" "input parameters" (
+  S "Stores the parameters needed " :+:
+  S "for the program, including material properties, " :+:
+  S "processing conditions, and numerical parameters. The " :+:
+  S "values can be read as needed. This module knows how " :+:
+  S "many parameters it stores.")
+
+modInputConstraints :: ConceptChunk
+modInputConstraints = dccWDS "modInputConstraints" "input constraints" (
+  S "Defines the constraints on the input data and gives an error if " :+:
+  S "a constraint is violated.")
+
+modInputVerif :: ConceptChunk
+modInputVerif = dccWDS "modInputVerif" "input verification" (
+  S "Verifies that the input " :+:
+  S "parameters comply with physical and software " :+: 
+  S "constraints. Throws an error if a parameter violates a" :+:
+  S " physical constraint. Throws a warning if a parameter " :+:
+  S "violates a software constraint.")
+
+modDerivedVals :: ConceptChunk
+modDerivedVals = dccWDS "modDerivedVals" "derived values" (
+  S "Defines the equations transforming the initial inputs into derived " :+:
+  S "quantities.")
+
+modInterpolation :: ConceptChunk
+modInterpolation = dccWDS "modInterpolation" "interpolation" (
+  S "Provides the equations that take the input parameters and " :+:
+  S "interpolation data and return an interpolated value.")
+
+modInterpData :: ConceptChunk
+modInterpData = dccWDS "modInterpData" "interpolation data" (
+  S "Converts the input interpolation data into the data structure used " :+:
+  S "by the interpolation module.")

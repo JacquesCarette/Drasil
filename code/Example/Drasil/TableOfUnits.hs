2d71c43b646ed1d03403a8627c6470a83c5019cc
Jacques Carette <carette@mcmaster.ca>
2017-03-01 22:07:59 -0500

    dramatric rephrasing of intro paragraph.

--- code/Example/Drasil/TableOfUnits.hs
+++ code/Example/Drasil/TableOfUnits.hs
@@ -13,13 +13,11 @@ table_of_units u = Section (S "Table of Units") [Con s1_intro, Con (unit_table u
 
 s1_intro :: Contents
 s1_intro = Paragraph 
-  (S "Throughout this document SI (Syst" :+: (F Grave 'e') :+: 
+  (S "The unit system used throughout is SI (Syst" :+: (F Grave 'e') :+: 
    S "me International d'Unit" :+: (F Acute 'e') :+: 
-   S "s) is employed as the unit system." :+:
-   S " In addition to the basic units, several derived units are" :+: 
-   S " employed as described below. For each unit, the symbol is" :+: 
-   S " given followed by a description of the unit with the SI" :+: 
-   S " name in parentheses.")
+   S "s). In addition to the basic units, several derived units are" :+: 
+   S " also used. For each unit, the table lists the symbol," :+: 
+   S " a description and the SI name.")
 
 unit_table :: Unit s => [s] -> Contents
 unit_table u = Table

681a2dc68245ad102334d81d36f72ff17e905a53
Dan Szymczak <dan.szymczak@gmail.com>
2017-03-08 12:22:47 -0500

    ConceptChunks should be built off NamedIdeas. Question is whether they should be built off NamedChunks only (like so), NWrappers (so we can use any NamedIdea), or have multiple data types which cover all cases (my preference). I plan to add data types as needed.

--- code/Language/Drasil/Chunk/Concept.hs
+++ code/Language/Drasil/Chunk/Concept.hs
@@ -1,9 +1,10 @@
+{-# Language Rank2Types #-}
 module Language.Drasil.Chunk.Concept where
 
 import Language.Drasil.Chunk
 import Language.Drasil.Chunk.NamedIdea
 
-import Control.Lens (Simple, Lens, (^.))
+import Control.Lens (Simple, Lens, (^.), set)
 
 import Language.Drasil.Spec
 
@@ -15,27 +16,30 @@ class NamedIdea c => Concept c where
 -- === DATA TYPES === --
 
 --- ConceptChunk ---  
-data ConceptChunk = DCC String Sentence Sentence (Maybe Sentence)
+data ConceptChunk = CC NamedChunk Sentence
 instance Eq ConceptChunk where
   c1 == c2 = (c1 ^. id) == (c2 ^. id)
 instance Chunk ConceptChunk where
-  id f (DCC n t d a) = fmap (\x -> DCC x t d a) (f n)
+  id = nl id
 instance NamedIdea ConceptChunk where
-  term f (DCC n t d a) = fmap (\x -> DCC n x d a) (f t)
-  getA (DCC _ _ _ a) = a
+  term = nl term
+  getA (CC n _) = getA n
 instance Concept ConceptChunk where
-  defn f (DCC n t d a) = fmap (\x -> DCC n t x a) (f d)
+  defn f (CC n d) = fmap (\x -> CC n x) (f d)
+  
+nl :: (forall c. (NamedIdea c) => Simple Lens c a) -> Simple Lens ConceptChunk a
+nl l f (CC n d) = fmap (\x -> CC (set l x n) d) (f (n ^. l))
   
 makeDCC, dcc :: String -> String -> String -> ConceptChunk
-makeDCC i ter des = DCC i (S ter) (S des) Nothing
+makeDCC i ter des = CC (nc i ter) (S des)
 
 dcc = makeDCC
 
 dccWDS :: String -> String -> Sentence -> ConceptChunk
-dccWDS i t d = DCC i (S t) d Nothing
+dccWDS i t d = CC (nc i t) d
 
 ccStSS :: String -> Sentence -> Sentence -> ConceptChunk
-ccStSS i t d = DCC i t d Nothing
+ccStSS i t d = CC (ncs i t) d
 
 dcc' :: String -> String -> String -> String -> ConceptChunk
-dcc' i t d a = DCC i (S t) (S d) (Just (S a))
+dcc' i t d a = CC (nc' i t a) (S d)

5660e0431bfbcbc40c3285f8a9f28c1f02f28bbe
Dan Szymczak <dan.szymczak@gmail.com>
2017-03-13 12:07:34 -0400

    Introduce ConceptDomain tagging on Concepts.

--- code/Language/Drasil/Chunk/Concept.hs
+++ code/Language/Drasil/Chunk/Concept.hs
@@ -1,4 +1,4 @@
-{-# Language Rank2Types #-}
+{-# Language GADTs, Rank2Types #-}
 module Language.Drasil.Chunk.Concept where
 
 import Language.Drasil.Chunk
@@ -14,36 +14,38 @@ class NamedIdea c => Concept c where
   defn :: Simple Lens c Sentence
 
 -- === DATA TYPES === --
-
 --- ConceptChunk ---  
-data ConceptChunk = CC NamedChunk Sentence
+data ConceptChunk where
+  CC :: Concept c => NamedChunk -> Sentence -> [c] -> ConceptChunk 
+  --[c] is the ConceptDomain(s)
 instance Eq ConceptChunk where
   c1 == c2 = (c1 ^. id) == (c2 ^. id)
 instance Chunk ConceptChunk where
   id = nl id
 instance NamedIdea ConceptChunk where
   term = nl term
-  getA (CC n _) = getA n
+  getA (CC n _ _) = getA n
 instance Concept ConceptChunk where
-  defn f (CC n d) = fmap (\x -> CC n x) (f d)
+  defn f (CC n d cd) = fmap (\x -> CC n x cd) (f d)
   
 nl :: (forall c. (NamedIdea c) => Simple Lens c a) -> Simple Lens ConceptChunk a
-nl l f (CC n d) = fmap (\x -> CC (set l x n) d) (f (n ^. l))
+nl l f (CC n d cd) = fmap (\x -> CC (set l x n) d cd) (f (n ^. l))
   
+--FIXME: Temporary ConceptDomain tag hacking to not break everything.  
 dcc :: String -> String -> String -> ConceptChunk 
-dcc i ter des = CC (nc i ter) (S des)
+dcc i ter des = CC (nc i ter) (S des) ([] :: [ConceptChunk])
 
 dccWDS :: String -> String -> Sentence -> ConceptChunk
-dccWDS i t d = CC (nc i t) d
+dccWDS i t d = CC (nc i t) d ([] :: [ConceptChunk])
 
 ccStSS :: String -> Sentence -> Sentence -> ConceptChunk
-ccStSS i t d = CC (ncs i t) d
+ccStSS i t d = CC (ncs i t) d ([] :: [ConceptChunk])
 
 dcc' :: String -> String -> String -> String -> ConceptChunk
-dcc' i t d a = CC (nc' i t a) (S d)
+dcc' i t d a = CC (nc' i t a) (S d) ([] :: [ConceptChunk])
 
-ccs :: NamedChunk -> Sentence -> ConceptChunk
+ccs :: Concept c => NamedChunk -> Sentence -> [c] -> ConceptChunk --Explicit tagging
 ccs = CC
 
 cc :: NamedChunk -> String -> ConceptChunk
-cc n d = CC n (S d)
+cc n d = CC n (S d) ([] :: [ConceptChunk])

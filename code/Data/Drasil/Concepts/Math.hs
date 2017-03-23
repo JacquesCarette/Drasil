df5a588c79f3f2d7f74d3a2bd1e5a831c6694157
Dan Szymczak <dan.szymczak@gmail.com>
2017-03-23 18:31:01 -0400

    Change commonidea (CI) to commonINP (CINP) for common ideas in Math.

--- code/Data/Drasil/Concepts/Math.hs
+++ code/Data/Drasil/Concepts/Math.hs
@@ -3,10 +3,10 @@ module Data.Drasil.Concepts.Math where
 import Language.Drasil
 
 gradient, norm_vect :: ConceptChunk 
-ode :: CI
+ode :: CINP
 
 --FIXME: Make some of these definitions better.
 gradient  = dcc "gradient" "gradient" "gradient operator"
 norm_vect = dcc "norm_vect" "normal" "unit outward normal vector for a surface"
 
-ode       = commonidea "ode" "Ordinary Differential Equation" "ODE"
+ode       = commonINP "ode" (cn' "Ordinary Differential Equation") "ODE"

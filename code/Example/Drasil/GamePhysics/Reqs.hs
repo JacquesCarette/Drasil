cfe02951c297da593446482e0494eea83a11177a
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-12 13:23:05 -0400

    minor style

--- code/Example/Drasil/GamePhysics/Reqs.hs
+++ code/Example/Drasil/GamePhysics/Reqs.hs
@@ -8,17 +8,17 @@ reqs = [r1,r2,r3,r4,r5,r6,r7,r8]
 
 r1,r2,r3,r4,r5,r6,r7,r8 :: ReqChunk
 --FIXME: Why are all of these empty? Makes no sense.
-r1 = ReqChunk (nw $ emptyN) [mod_space, mod_control, mod_seq]
-r2 = ReqChunk (nw $ emptyN) [mod_body, mod_control, mod_vector, mod_trans]
-r3 = ReqChunk (nw $ emptyN) [mod_shape, mod_circle, mod_segment, mod_poly,
+r1 = ReqChunk (nw emptyN) [mod_space, mod_control, mod_seq]
+r2 = ReqChunk (nw emptyN) [mod_body, mod_control, mod_vector, mod_trans]
+r3 = ReqChunk (nw emptyN) [mod_shape, mod_circle, mod_segment, mod_poly,
   mod_control, mod_vector]
-r4 = ReqChunk (nw $ emptyN) [mod_body, mod_shape, mod_circle, mod_segment,
+r4 = ReqChunk (nw emptyN) [mod_body, mod_shape, mod_circle, mod_segment,
   mod_poly, mod_space, mod_control]
-r5 = ReqChunk (nw $ emptyN) [mod_body, mod_space, mod_vector, mod_trans]
-r6 = ReqChunk (nw $ emptyN) [mod_body, mod_space, mod_vector, mod_trans]
-r7 = ReqChunk (nw $ emptyN) [mod_body, mod_space, mod_bb, mod_spatial,
+r5 = ReqChunk (nw emptyN) [mod_body, mod_space, mod_vector, mod_trans]
+r6 = ReqChunk (nw emptyN) [mod_body, mod_space, mod_vector, mod_trans]
+r7 = ReqChunk (nw emptyN) [mod_body, mod_space, mod_bb, mod_spatial,
   mod_coll, mod_linked, mod_assoc]
-r8 = ReqChunk (nw $ emptyN) [mod_body, mod_space, mod_arbiter, mod_vector,
+r8 = ReqChunk (nw emptyN) [mod_body, mod_space, mod_arbiter, mod_vector,
   mod_trans]
 
 emptyN :: NPNC

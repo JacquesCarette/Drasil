9e0c872279f6a55d5d58d7b5575c1e8ec50ceb31
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-12 12:45:51 -0400

    Remove NamedChunk from LC

--- code/Language/Drasil/Chunk/LC.hs
+++ code/Language/Drasil/Chunk/LC.hs
@@ -3,12 +3,13 @@ module Language.Drasil.Chunk.LC(LCChunk(..)) where
 import Control.Lens (Simple, Lens)
 import Prelude hiding (id)
 import Language.Drasil.Chunk
-import Language.Drasil.Chunk.NamedIdea (NamedIdea(term,getA), NamedChunk)
+import Language.Drasil.Chunk.NamedIdea (NamedIdea(term,getA))
 import Language.Drasil.Chunk.Module
+import Language.Drasil.Chunk.Wrapper (NWrapper)
 
 -- BEGIN LCCHUNK (likely change chunk) --
 data LCChunk = LCChunk
-  { lcCC :: NamedChunk
+  { lcCC :: NWrapper
   , lcRelatedModules :: [ModuleChunk]
   }
 
@@ -21,5 +22,5 @@ instance NamedIdea LCChunk where
 -- END LCCHUNK --
 
 -- don't export this
-cl :: Simple Lens LCChunk NamedChunk
+cl :: Simple Lens LCChunk NWrapper
 cl f (LCChunk a b) = fmap (\x -> LCChunk x b) (f a)

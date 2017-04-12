36a119b233cf91b9e2699466ba244c21dad4299d
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-12 12:54:21 -0400

    update Chipmunk reqs and add fixme.

--- code/Example/Drasil/GamePhysics/Reqs.hs
+++ code/Example/Drasil/GamePhysics/Reqs.hs
@@ -7,16 +7,16 @@ reqs :: [ReqChunk]
 reqs = [r1,r2,r3,r4,r5,r6,r7,r8]
 
 r1,r2,r3,r4,r5,r6,r7,r8 :: ReqChunk
-
-r1 = ReqChunk (ncWDS "" $ S "") [mod_space, mod_control, mod_seq]
-r2 = ReqChunk (ncWDS "" $ S "") [mod_body, mod_control, mod_vector, mod_trans]
-r3 = ReqChunk (ncWDS "" $ S "") [mod_shape, mod_circle, mod_segment, mod_poly,
+--FIXME: Why are all of these empty? Makes no sense.
+r1 = ReqChunk (nw $ ncWDS "" $ S "") [mod_space, mod_control, mod_seq]
+r2 = ReqChunk (nw $ ncWDS "" $ S "") [mod_body, mod_control, mod_vector, mod_trans]
+r3 = ReqChunk (nw $ ncWDS "" $ S "") [mod_shape, mod_circle, mod_segment, mod_poly,
   mod_control, mod_vector]
-r4 = ReqChunk (ncWDS "" $ S "") [mod_body, mod_shape, mod_circle, mod_segment,
+r4 = ReqChunk (nw $ ncWDS "" $ S "") [mod_body, mod_shape, mod_circle, mod_segment,
   mod_poly, mod_space, mod_control]
-r5 = ReqChunk (ncWDS "" $ S "") [mod_body, mod_space, mod_vector, mod_trans]
-r6 = ReqChunk (ncWDS "" $ S "") [mod_body, mod_space, mod_vector, mod_trans]
-r7 = ReqChunk (ncWDS "" $ S "") [mod_body, mod_space, mod_bb, mod_spatial,
+r5 = ReqChunk (nw $ ncWDS "" $ S "") [mod_body, mod_space, mod_vector, mod_trans]
+r6 = ReqChunk (nw $ ncWDS "" $ S "") [mod_body, mod_space, mod_vector, mod_trans]
+r7 = ReqChunk (nw $ ncWDS "" $ S "") [mod_body, mod_space, mod_bb, mod_spatial,
   mod_coll, mod_linked, mod_assoc]
-r8 = ReqChunk (ncWDS "" $ S "") [mod_body, mod_space, mod_arbiter, mod_vector,
+r8 = ReqChunk (nw $ ncWDS "" $ S "") [mod_body, mod_space, mod_arbiter, mod_vector,
   mod_trans]

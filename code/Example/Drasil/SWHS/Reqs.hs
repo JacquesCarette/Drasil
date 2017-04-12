483535b6075f18baa1577f9cb78b51da09b99e9f
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-12 12:46:15 -0400

    Update SWHS Reqs and add FIXME.

--- code/Example/Drasil/SWHS/Reqs.hs
+++ code/Example/Drasil/SWHS/Reqs.hs
@@ -7,17 +7,17 @@ reqs :: [ReqChunk]
 reqs = [r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11]
 
 r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11 :: ReqChunk
-
-r1 = ReqChunk (ncWDS "" $ S "") [mod_hw, mod_inputf, mod_inputp, mod_ctrl]
-r2 = ReqChunk (ncWDS "" $ S "") [mod_inputf, mod_inputp]
-r3 = ReqChunk (ncWDS "" $ S "") [mod_inputv]
-r4 = ReqChunk (ncWDS "" $ S "") [mod_outputf, mod_ctrl]
-r5 = ReqChunk (ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
+--FIXME: Why are these all empty?
+r1 = ReqChunk (nw $ ncWDS "" $ S "") [mod_hw, mod_inputf, mod_inputp, mod_ctrl]
+r2 = ReqChunk (nw $ ncWDS "" $ S "") [mod_inputf, mod_inputp]
+r3 = ReqChunk (nw $ ncWDS "" $ S "") [mod_inputv]
+r4 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_ctrl]
+r5 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
   mod_plot]
-r6 = ReqChunk (ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
+r6 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
   mod_plot]
-r7 = ReqChunk (ncWDS "" $ S "") [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
-r8 = ReqChunk (ncWDS "" $ S "") [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
-r9 = ReqChunk (ncWDS "" $ S "") [mod_outputv]
-r10 = ReqChunk (ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl]
-r11 = ReqChunk (ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ener, mod_ctrl]
+r7 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
+r8 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
+r9 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputv]
+r10 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl]
+r11 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ener, mod_ctrl]

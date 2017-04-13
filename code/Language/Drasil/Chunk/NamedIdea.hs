40b01d8d84f011d3e414ea25045ad03fd7406a7a
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-12 21:43:40 -0400

    First pass changing over term from Sentence to NP.

--- code/Language/Drasil/Chunk/NamedIdea.hs
+++ code/Language/Drasil/Chunk/NamedIdea.hs
@@ -10,17 +10,17 @@ import Language.Drasil.NounPhrase
 import Prelude hiding (id)
 
 class Chunk c => NamedIdea c where
-  term :: Simple Lens c Sentence
+  term :: Simple Lens c NP
   getA :: c -> Maybe Sentence
   --Get Abbreviation/Acronym? These might need to be separated 
   --depending on contexts, but for now I don't see a problem with it.
 
 -- Get short form (if exists), else get term.
 short :: NamedIdea c => c -> Sentence
-short c = maybe (c^.term) (\x -> x) (getA c)
+short c = maybe (phrase (c^.term)) (\x -> x) (getA c)
 
 -- === DATA TYPES/INSTANCES === --
-data NamedChunk = NC String Sentence (Maybe Sentence)
+data NamedChunk = NC String NP (Maybe Sentence)
 instance Eq NamedChunk where
   c1 == c2 = (c1 ^. id) == (c2 ^. id)
 instance Chunk NamedChunk where
@@ -29,21 +29,11 @@ instance NamedIdea NamedChunk where
   term f (NC a b c) = fmap (\x -> NC a x c) (f b)
   getA (NC _ _ c) = c
   
-nc :: String -> String -> NamedChunk
-nc i des = NC i (S des) Nothing
+nc :: String -> NP -> NamedChunk
+nc i des = NC i des Nothing
 
-ncs :: String -> Sentence -> NamedChunk
-ncs i des = NC i des Nothing
-
-nc' :: String -> String -> String -> NamedChunk
-nc' i t acc = NC i (S t) (Just (S acc))
-
---Currently only used by RelationChunk and EqChunk
-ncWDS :: String -> Sentence -> NamedChunk
-ncWDS n d = NC n d Nothing
-
-ncWDS' :: String -> Sentence -> String -> NamedChunk
-ncWDS' i t a = NC i t (Just (S a))
+nc' :: String -> NP -> String -> NamedChunk
+nc' i t acc = NC i t (Just (S acc))
 
 data NPNC where
   NPNC :: String -> Sentence -> (Maybe Sentence) -> NP -> NPNC
@@ -52,7 +42,7 @@ instance Eq NPNC where
 instance Chunk NPNC where
   id f (NPNC a b c d) = fmap (\x -> NPNC x b c d) (f a)
 instance NamedIdea NPNC where
-  term f (NPNC a b c d) = fmap (\x -> NPNC a x c d) (f b)
+  term f (NPNC a b c d) = fmap (\x -> NPNC a b c x) (f d)
   getA (NPNC _ _ c _) = c
 instance NounPhrase NPNC where
   phrase (NPNC _ _ _ d) = phrase d
@@ -68,12 +58,13 @@ npnc' i n a = NPNC i (phrase n) (Just $ S a) n
 
 ----------------------
 -- various combinators
-compoundterm :: (NamedIdea c, NamedIdea d) => c -> d -> NamedChunk
-compoundterm t1 t2 = NC (t1^.id ++ t2^.id) ((t1^.term) +:+ (t2^.term)) Nothing
+compoundterm :: (NamedIdea c, NounPhrase c, NamedIdea d, NounPhrase d) => 
+  c -> d -> NamedChunk
+compoundterm t1 t2 = NC (t1^.id ++ t2^.id) (compoundPhrase t1 t2) Nothing
 
 compoundNPNC :: NPNC -> NPNC -> NPNC
 compoundNPNC t1@(NPNC _ _ _ n1) t2@(NPNC _ _ _ n2) = 
-  NPNC (t1^.id ++ t2^.id) ((t1^.term) +:+ (t2^.term)) Nothing 
+  NPNC (t1^.id ++ t2^.id) (phrase $ compoundPhrase n1 n2) Nothing 
   (compoundPhrase n1 n2) 
   
 -- we might want to eventually restrict the use of these via
@@ -82,10 +73,10 @@ compoundNPNC t1@(NPNC _ _ _ n1) t2@(NPNC _ _ _ n2) =
 -- 2. t1 `of_` t2 means that t1 is a view of part of the structure of t2
 --FIXME: This should be NamedIdea c & d, but temporarily swapped to NounPhrase
 for :: (NounPhrase c, NamedIdea d) => c -> d -> Sentence
-for t1 t2 = (titleize t1) +:+ S "for" +:+ (t2^.term)
+for t1 t2 = (titleize t1) +:+ S "for" +:+ (phrase $ t2^.term)
 
 of_ :: (NamedIdea c, NamedIdea d) => c -> d -> Sentence
-of_ t1 t2 = (t1^.term) +:+ S "of" +:+ (t2^.term)
+of_ t1 t2 = (phrase $ t1^.term) +:+ S "of" +:+ (phrase $ t2^.term)
 
 of' :: (NounPhrase c, NounPhrase d) => c -> d -> NP
 of' t1 t2 = nounPhrase'' 

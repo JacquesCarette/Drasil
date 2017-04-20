af64f134809dada8271d6a439d092160c3f13323
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-19 22:39:35 -0400

    Math concepts use NP.

--- code/Data/Drasil/Concepts/Math.hs
+++ code/Data/Drasil/Concepts/Math.hs
@@ -6,8 +6,8 @@ gradient, norm_vect :: ConceptChunk
 ode :: CINP
 
 --FIXME: Make some of these definitions better.
-gradient  = dcc "gradient" "gradient" "gradient operator"
-norm_vect = dcc "norm_vect" "normal" "unit outward normal vector for a surface"
+gradient  = dcc "gradient" (cn' "gradient") "gradient operator"
+norm_vect = dcc "norm_vect" (cn' "normal") "unit outward normal vector for a surface"
 
 --FIXME: use nounphrase instead of cn'
 ode       = commonINP "ode" (cn' "Ordinary Differential Equation") "ODE"

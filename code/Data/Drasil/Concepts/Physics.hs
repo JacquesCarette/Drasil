4579acb5ba4834562bccd9e97ee923ef3bd402ef
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-19 23:08:15 -0400

    Physics concepts use NPs.

--- code/Data/Drasil/Concepts/Physics.hs
+++ code/Data/Drasil/Concepts/Physics.hs
@@ -8,61 +8,65 @@ rigidBody, velocity, angularV, friction, elasticity, collision, space,
   cartesian, rightHand, surface, restitutionCoef, acceleration,
   angularAccel, momentOfInertia, force, impulseS, impulseV, displacement,
   gravitationalAccel, gravitationalConst, position, distance, angularDisplacement,time, torque :: ConceptChunk
-    
-rigidBody  = dcc "rigidBody" "rigid body" 
-  "A solid body in which deformation is neglected."
-velocity   = dccWDS "velocity" "velocity" 
-  (S "The rate of change of a body's " :+: (position ^. term))
-angularV   = dcc "angularV" "angular velocity" 
+
+rigidBody  = dcc "rigidBody" 
+  (cnIES "rigid body") "A solid body in which deformation is neglected."
+velocity   = dccWDS "velocity" (cnIES "velocity")
+  (S "The rate of change of a body's " :+: (phrase (position ^. term)))
+angularV   = dcc "angularV" (cnIES "angular velocity")
   "The rate of change of a body's orientation."
-friction   = dcc "friction" "friction" 
+friction   = dcc "friction" (cn' "friction")
   "The force resisting the relative motion of two surfaces."
-elasticity = dcc "elasticity" "elasticity" ("Ratio of the relative velocities " ++
-    "of two colliding objects after and before a collision.")
-collision  = dcc "collision" "collision" ("An encounter between particles resulting " ++
-    "in an exchange or transformation of energy.")
-space      = dcc "space" "space" ("A two-dimensional extent where objects and " ++
-    "events have relative positions and directions.")
-cartesian  = dcc "cartesian" "Cartesian coordinate" ("A coordinate system that " ++
-    "specifies each point uniquely in a plane by a pair of numerical " ++
-    "coordinates.")
-rightHand  = dcc "rightHand" "right-handed coordinate system" 
+elasticity = dcc "elasticity" (cnIES "elasticity") 
+  ("Ratio of the relative velocities " ++
+  "of two colliding objects after and before a collision.")
+collision  = dcc "collision" (cn' "collision")
+  ("An encounter between particles resulting " ++
+  "in an exchange or transformation of energy.")
+space      = dcc "space" (cn' "space") 
+  ("A two-dimensional extent where objects and " ++
+  "events have relative positions and directions.")
+cartesian  = dcc "cartesian" (pn' "Cartesian coordinate") 
+  ("A coordinate system that specifies each point uniquely in a plane by a " ++
+  "pair of numerical coordinates.")
+rightHand  = dcc "rightHand" (cn' "right-handed coordinate system")
   "A coordinate system where the positive z-axis comes out of the screen."
   
   
-position   = dcc "position" "position" 
+position   = dcc "position" (cn' "position")
   "an object's location relative to a reference point"
-acceleration = dccWDS "acceleration" "acceleration" 
-  (S "the rate of change of a body's " :+: (velocity ^. term))
-angularAccel = dccWDS "angularAccel" "angular acceleration" 
-  (S "the rate of change of a body's " :+: (angularV ^. term))  
-displacement = dccWDS "displacement" "displacement" 
+acceleration = dccWDS "acceleration" (cn' "acceleration")
+  (S "the rate of change of a body's " :+: (phrase (velocity ^. term)))
+angularAccel = dccWDS "angularAccel" (cn' "angular acceleration")
+  (S "the rate of change of a body's " :+: (phrase (angularV ^. term)))
+displacement = dccWDS "displacement" (cn' "displacement")
   (S "the change in " :+: (position ^. defn))
-force      = dcc "force" "force" 
+force      = dcc "force" (cn' "force")
   "an interaction that tends to produce change in the motion of an object"
-surface    = dcc "surface" "surface"
+surface    = dcc "surface" (cn' "surface")
   "the outer or topmost boundary of an object"
-distance   = dcc "distance" "distance" 
+distance   = dcc "distance" (cn' "distance")
   "the interval measured along a path connecting two locations"
 
 -- The following feel like they're missing something/need to be more
 -- descriptive. See issue tracker for details.  
+-- FIXME: plurals below?
 --TODO: The 'FIXME's below.
-restitutionCoef = dcc "restitutionCoef" "coefficient of restitution" 
-  "FIXME: MISSING DEFINITION"
-momentOfInertia = dcc "momentOfInertia" "moment of inertia" 
-  "FIXME: MISSING DEFINITION"
-impulseV   = dcc "impulseV" "impulse (vector)" 
-  "FIXME: MISSING DEFINITION"
-impulseS   = dcc "impulseS" "impulse (scalar)" 
-  "FIXME: MISSING DEFINITION"
-angularDisplacement = dcc "angularDisplacement" "angular displacement"
-  "FIXME: MISSING DEFINITION"
-time = dcc "time" "time" "FIXME: MISSING DEFINITION"
-torque = dcc "torque" "torque" "FIXME: MISSING DEFINITION"
+fixme :: String
+fixme = "FIXME: MISSING DEFINITION"
 
+restitutionCoef = dcc "restitutionCoef" (cn "coefficient of restitution") fixme
+momentOfInertia = dcc "momentOfInertia" (cn "moment of inertia") fixme
 
-gravitationalAccel = dcc "gravitationalAccel" "gravitational acceleration" 
-  "FIXME: MISSING DEFINITION"
-gravitationalConst = dcc "gravitationalConst" "gravitational constant" 
+--FIXME: These two should be built off "impulse"
+impulseV   = dcc "impulseV" (cn "impulse (vector)") fixme 
+impulseS   = dcc "impulseS" (cn "impulse (scalar)") fixme 
+
+angularDisplacement = dcc "angularDisplacement" (cn' "angular displacement") fixme
+time = dcc "time" (cn' "time") fixme
+torque = dcc "torque" (cn' "torque") fixme
+
+gravitationalAccel = dcc "gravitationalAccel" 
+  (cn "gravitational acceleration") fixme
+gravitationalConst = dcc "gravitationalConst" (cn "gravitational constant" )
   "gravitational constant (6.673 * 10E-11)"

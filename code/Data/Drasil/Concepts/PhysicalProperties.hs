3ef228a897e171dff7ce4da39757bd0851e10cc3
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-19 22:59:35 -0400

    Physical Properties concepts use NPs.

--- code/Data/Drasil/Concepts/PhysicalProperties.hs
+++ code/Data/Drasil/Concepts/PhysicalProperties.hs
@@ -4,15 +4,15 @@ import Language.Drasil
 
 gaseous, liquid, solid, ctrOfMass, mass, length :: ConceptChunk
 
-gaseous = dcc "gaseous" "Gas" "gaseous state"
-liquid  = dcc "liquid" "Liquid" "liquid state"
-solid   = dcc "solid" "Solid" "solid state"
+gaseous = dcc "gaseous" (cn''' "Gas") "gaseous state"
+liquid  = dcc "liquid" (cn' "Liquid") "liquid state"
+solid   = dcc "solid" (cn' "Solid") "solid state"
 
-ctrOfMass = dcc "ctrOfMass" "centre of mass" 
+ctrOfMass = dcc "ctrOfMass" (cn "centre of mass") --FIXME: Plural?
   "The mean location of the distribution of mass of the object."
 
-mass = dcc "mass" "mass" "the quantity of matter in a body"
-length = dcc "length" "length" 
+mass = dcc "mass" (cn''' "mass") "the quantity of matter in a body"
+length = dcc "length" (cn' "length")
   ("the straight-line distance between two points along an object. " ++
   "Typically used to represent the size of an object from one end to the other.")
   

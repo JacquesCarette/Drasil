cb9082d4ba54bdd31ae56054481192f3dd65e7e7
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-20 15:09:59 -0400

    Finish fixing tiny example

--- code/Example/Drasil/HGHC/HGHC.hs
+++ code/Example/Drasil/HGHC/HGHC.hs
@@ -22,7 +22,7 @@ modules = [mod_calc, mod_hw, mod_inputp, mod_inputf, mod_behav, mod_outputf,
 
 s1, s2, s3 :: Section --, s4 
 s1 = table_of_units si_units -- probably want to not do all of them
-s2 = table_of_symbols ((map qs vars) ++ (map qs htVars)) (^. term)
+s2 = table_of_symbols ((map qs vars) ++ (map qs htVars)) (\x -> phrase $ x ^. term)
 s3 = Section (S "Data Definitions") $ map (Con . Definition . Data) vars
 --s4 = Section 0 (S "Code -- Test") $ map (CodeBlock . toCode CLang Calc) [htTransCladCool]
 

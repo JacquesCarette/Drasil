42b81fb0b9c682771bc61097c9f0f3c4e9b4cf6e
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-19 22:59:05 -0400

    Thermodynamics concepts use NP.

--- code/Data/Drasil/Concepts/Thermodynamics.hs
+++ code/Data/Drasil/Concepts/Thermodynamics.hs
@@ -6,28 +6,34 @@ boiling,law_cons_energy, law_conv_cooling, latent_heat, melting, phase_change,
   sens_heat, thermal_analysis, thermal_conduction, thermal_energy,
   thermal_conductor, heat_cap_spec, heat_trans :: ConceptChunk
 
-boiling = dcc "boiling" "Boiling" 
+--To save space.
+fixme :: String
+fixme = "FIXME: MISSING DEFINITION"
+  
+-- FIXME: "Boiling" is not a noun. How should we deal with it?
+--    Same for "Melting"
+boiling = dcc "boiling" (cn "Boiling")
   "Phase change from liquid to vapour"
-heat_trans = dcc "heat_trans" "Heat transfer" 
-  "FIXME: MISSING DEFINITION"
-heat_cap_spec = dcc "heat_cap_spec" "specific heat capacity" 
-  "FIXME: MISSING DEFINITION"
-latent_heat = dcc "latent_heat" "Latent heat" 
-  "FIXME: MISSING DEFINITION"
-law_cons_energy = dcc "law_cons_energy" "Law of conservation of energy" 
+heat_trans = dcc "heat_trans" (cn' "Heat transfer") fixme
+heat_cap_spec = dcc "heat_cap_spec" (cnIES "specific heat capacity") fixme
+latent_heat = dcc "latent_heat" (cn' "Latent heat") fixme
+law_cons_energy = dcc "law_cons_energy" 
+  (nounPhraseSP "Law of conservation of energy" )
   "Energy is conserved"
-law_conv_cooling = dcc "law_conv_cooling" "Newton's law of cooling" 
+law_conv_cooling = dcc "law_conv_cooling" 
+  (nounPhraseSP "Newton's law of cooling")
   "Newton's law of convective cooling"
-melting = dcc "melting" "Melting" 
+melting = dcc "melting" (cn "Melting")
   "Phase change from solid to liquid"
-phase_change = dcc "phase_change" "Phase change" "Change of state"
+phase_change = dcc "phase_change" (cn' "Phase change") "Change of state"
 --FIXME: sens_heat's definition is useless.
-sens_heat = dcc "sens_heat" "Sensible heat" "FIXME: MISSING DEFINITION"
-thermal_analysis = dcc "thermal_analysis" "Thermal analysis" 
+sens_heat = dcc "sens_heat" (cn' "Sensible heat") fixme
+thermal_analysis = dcc "thermal_analysis" 
+  (cnIP "Thermal analysis" (IrregPlur (\x -> init (init x) ++ "es")))
   "The study of material properties as they change with temperature"
-thermal_conduction = dcc "thermal_conduction" "Thermal conduction" 
+thermal_conduction = dcc "thermal_conduction" (nounPhraseSP "Thermal conduction")
   "The transfer of heat energy through a substance"
-thermal_conductor = dcc "thermal_conductor" "Thermal conductor" 
+thermal_conductor = dcc "thermal_conductor" (cn' "Thermal conductor")
   ("An object through which thermal energy can be transferred")
-thermal_energy = dcc "thermal_energy" "Thermal energy"
-  "The energy that comes from heat"
+thermal_energy = dcc "thermal_energy" 
+  (cnIES "Thermal energy") "The energy that comes from heat"

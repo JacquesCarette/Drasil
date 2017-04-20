968238feb1ddd13f5d20512224fa2b0bd009b830
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-20 14:57:57 -0400

    Thermodynamics Units use NPs.

--- code/Data/Drasil/Units/Thermodynamics.hs
+++ code/Data/Drasil/Units/Thermodynamics.hs
@@ -5,17 +5,17 @@ import Data.Drasil.SI_Units
 import Control.Lens ((^.))
 
 heat_capacity :: DerUChunk
-heat_capacity = makeDerU (dccWDS "heat_capacity" "heat capacity" 
+heat_capacity = makeDerU (dccWDS "heat_capacity" (cnIES "heat capacity")
   (S "heat capacity (constant pressure)")) $ 
   USynonym (joule /: centigrade)
 
 heat_cap_spec :: DerUChunk --Specific heat capacity
-heat_cap_spec = makeDerU (dccWDS "heat_cap_spec" "specific heat" 
+heat_cap_spec = makeDerU (dccWDS "heat_cap_spec" (cn' "specific heat")
   (S "heat capacity per unit mass")) $
   USynonym (UDiv (joule ^. usymb) (UProd [kilogram ^. usymb, centigrade ^. usymb]))
 
 thermal_flux :: DerUChunk
-thermal_flux = makeDerU (dccWDS "thermal_flux" "heat flux"
+thermal_flux = makeDerU (dccWDS "thermal_flux" (cn'' "heat flux")
   (S "the rate of heat energy transfer per unit area")) $ USynonym (watt /: m_2)
 
 heat_transfer_coef :: DerUChunk
@@ -23,5 +23,5 @@ heat_transfer_coef = new_unit "heat transfer coefficient" $ (UDiv
   (watt ^. usymb) (UProd [m_2 ^. usymb, centigrade ^. usymb]))
 
 volHtGenU :: DerUChunk
-volHtGenU = makeDerU (dcc "volHtGenU" "volumetric heat generation" 
+volHtGenU = makeDerU (dcc "volHtGenU" (cn "volumetric heat generation")
   "the rate of heat energy generation per unit volume") $ USynonym (watt /: m_3)

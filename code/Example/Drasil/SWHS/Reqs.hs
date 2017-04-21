2d6c41a3bf08188a83d783c3efda1a08db9bf890
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-21 15:27:00 -0400

    Hack cleanup (still hacked as before, but looks nicer).

--- code/Example/Drasil/SWHS/Reqs.hs
+++ code/Example/Drasil/SWHS/Reqs.hs
@@ -8,19 +8,19 @@ reqs = [r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11]
 
 r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11 :: ReqChunk
 --FIXME: Why are these all empty?
-r1 = ReqChunk (nw $ ncWDS "" $ S "") [mod_hw, mod_inputf, mod_inputp, mod_ctrl]
-r2 = ReqChunk (nw $ ncWDS "" $ S "") [mod_inputf, mod_inputp]
-r3 = ReqChunk (nw $ ncWDS "" $ S "") [mod_inputv]
-r4 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_ctrl]
-r5 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
+r1 = ReqChunk (nw $ emptyN) [mod_hw, mod_inputf, mod_inputp, mod_ctrl]
+r2 = ReqChunk (nw $ emptyN) [mod_inputf, mod_inputp]
+r3 = ReqChunk (nw $ emptyN) [mod_inputv]
+r4 = ReqChunk (nw $ emptyN) [mod_outputf, mod_ctrl]
+r5 = ReqChunk (nw $ emptyN) [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
   mod_plot]
-r6 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
+r6 = ReqChunk (nw $ emptyN) [mod_outputf, mod_temp, mod_ctrl, mod_seq, mod_ode,
   mod_plot]
-r7 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
-r8 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
-r9 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputv]
-r10 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ctrl]
-r11 = ReqChunk (nw $ ncWDS "" $ S "") [mod_outputf, mod_temp, mod_ener, mod_ctrl]
+r7 = ReqChunk (nw $ emptyN) [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
+r8 = ReqChunk (nw $ emptyN) [mod_outputf, mod_ener, mod_ctrl, mod_seq, mod_plot]
+r9 = ReqChunk (nw $ emptyN) [mod_outputv]
+r10 = ReqChunk (nw $ emptyN) [mod_outputf, mod_temp, mod_ctrl]
+r11 = ReqChunk (nw $ emptyN) [mod_outputf, mod_temp, mod_ener, mod_ctrl]
 
 emptyN :: NPNC
 emptyN = npnc "" (cn "")

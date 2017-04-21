016c4b03e924fced9bf2aefd0eab226b52755462
Dan Szymczak <dan.szymczak@gmail.com>
2017-04-20 20:28:41 -0400

    Update to use NPs

--- code/Example/Drasil/SpecificSystemDescription.hs
+++ code/Example/Drasil/SpecificSystemDescription.hs
@@ -13,4 +13,4 @@ intro sys = Paragraph $ S "This section first presents the problem" +:+
   S "description, which gives a high-level view of the problem to be" +:+
   S "solved. This is followed by the solution characteristics" +:+
   S "specification, which presents the assumptions" `sC`
-  S "theories, and definitions that are used for the" +:+. (sys ^. term)
+  S "theories, and definitions that are used for the" +:+. (phrase $ sys ^. term)

cb5c6fe83e67a29bcfb938fef291745e7ab67c49
Maryyam Niazi <niazim3@mcmaster.ca>
2017-05-03 15:33:04 -0400

    removed original s6_2 definition and replaced characteristicsSpecification in s6_intro

--- code/Example/Drasil/GlassBR/Body.hs
+++ code/Example/Drasil/GlassBR/Body.hs
@@ -254,7 +254,7 @@ s6 = Section(S "Specific System Description") [Con s6_intro, Sub s6_1,
 s6_intro = Paragraph $ 
   S "This section first presents the problem description, which gives a" +:+
   S "high-level view of the problem to be solved. This is followed by" +:+
-  S "the solution characteristics specification, which presents the" +:+
+  S "the solution" +:+ characteristicsSpecification :+: S ", which presents the" +:+
   (plural assumption) `sC` S "theories, definitions."
 
 s6_1 = Section (S "Problem Description") [Con s6_1_intro, Sub s6_1_1, 
@@ -339,7 +339,6 @@ s6_1_3_list = Enumeration $ Simple $ map (\(a,b) -> (a, Flat b)) [
   S "will be able to withstand the explosion of a certain degree which" +:+.
   S "is calculated based on user input")]
 
---s6_2 = Section (S "Solution Characteristics Specification") 
 s6_2 = Section (S "Solution" +:+ titleize characteristicsSpecification) 
   [Con s6_2_intro, Sub s6_2_1, Sub s6_2_2, Sub s6_2_3, Sub s6_2_4, Sub s6_2_5]
 

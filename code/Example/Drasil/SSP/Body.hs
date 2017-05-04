22cb81b42647e7f62d0c938d365e78e43f30cf9b
njericha <njericha@edu.uwaterloo.ca>
2017-05-04 12:02:01 -0400

    used the new indroduceAbb combinator

--- code/Example/Drasil/SSP/Body.hs
+++ code/Example/Drasil/SSP/Body.hs
@@ -98,7 +98,7 @@ s2_p1 = Con $ Paragraph $ S "A slope of geological mass, composed" +:+
   S "relative stability known as the factor of safety."
 
 s2_p2 = Con $ Paragraph $ S "The following section provides an overview" +:+
-  S "of the" +:+ (titleize srs) +:+ S "(" :+: (short srs) :+: S ")" +:+
+  S "of the" +:+ (introduceAbb srs) +:+
   S "for a slope stability analysis problem. The developed program" +:+
   S "will be referred to as the Slope Stability Analysis (SSA)" +:+
   S "program. This section explains the purpose of this document," +:+

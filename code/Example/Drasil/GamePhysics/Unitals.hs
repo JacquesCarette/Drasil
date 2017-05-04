1ddec2ed34f6fc5a274836cb552d43375835f4b6
Henry <hmadej@gmail.com>
2017-05-04 15:59:03 -0400

    sup -> sub typo

--- code/Example/Drasil/GamePhysics/Unitals.hs
+++ code/Example/Drasil/GamePhysics/Unitals.hs
@@ -122,12 +122,12 @@ vel_B, vel_O, r_OB :: UnitalChunk
 
 -- FIXME: parametrized hack
 vel_B   = ucFromVC velo velU
-  where velo = cvR (dccWDS "velocity" (compoundPhrase' (QP.velocity ^. term) (cn "at point B")) (phrase $ QP.velocity ^. term)) (sup (QP.velocity ^. symbol) cB)
+  where velo = cvR (dccWDS "velocity" (compoundPhrase' (QP.velocity ^. term) (cn "at point B")) (phrase $ QP.velocity ^. term)) (sub (QP.velocity ^. symbol) cB)
 --vel_B   = uc' "v_B" (nounPhraseSP "velocity at point B")
 --  "FIXME: Define this or remove the need for definitions" (sub (vel ^. symbol) cB) velU
 -- FIXME: parametrized hack
 vel_O   = ucFromVC velo velU
-  where velo = cvR (dccWDS "velocity" (compoundPhrase' (QP.velocity ^. term) (cn "at the origin")) (phrase $ QP.velocity ^. term)) (sup (QP.velocity ^. symbol) cO)
+  where velo = cvR (dccWDS "velocity" (compoundPhrase' (QP.velocity ^. term) (cn "at the origin")) (phrase $ QP.velocity ^. term)) (sub (QP.velocity ^. symbol) cO)
 --vel_O   = uc' "v_O" (nounPhraseSP "velocity at the origin")
 --  "FIXME: Define this or remove the need for definitions" (sub (vel ^. symbol) cO) velU
 r_OB    = uc' "r_OB" 

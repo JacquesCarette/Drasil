cdbf3145deced7ccf76b15dbd809eb982b730795
Henry <hmadej@gmail.com>
2017-05-04 15:20:35 -0400

    changed normal to normal vector, changed collision norm vector

--- code/Data/Drasil/Concepts/Math.hs
+++ code/Data/Drasil/Concepts/Math.hs
@@ -7,11 +7,11 @@ ode :: CINP
 
 --FIXME: Make some of these definitions better.
 gradient  = dcc "gradient" (cn' "gradient") "gradient operator"
-norm_vect = dcc "norm_vect" (cn' "normal") "unit outward normal vector for a surface"
+norm_vect = dcc "norm_vect" (cn' "normal vector") "unit outward normal vector for a surface"
 -- could use better name for this? namespace collision when using orientation
 orient = dcc "orientation" (cn' "orientation") "orientation"
 unit_vect = dcc "unit_vect" (cn' "unit vector") "unit vector"
-euclid_norm = dcc "euclid_norm" (cn' "Euclidean Norm") "Euclidean Norm"
+euclid_norm = dcc "euclid_norm" (cn' "euclidean norm") "euclidean norm"
 
 --FIXME: use nounphrase instead of cn'
 ode       = commonINP "ode" (cn' "Ordinary Differential Equation") "ODE"

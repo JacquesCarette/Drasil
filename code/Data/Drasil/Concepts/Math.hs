6265cda08d4b29afe9a1302fb1f983371b9c01d9
Maryyam Niazi <niazim3@mcmaster.ca>
2017-05-04 13:59:47 -0400

    added matrix definition

--- code/Data/Drasil/Concepts/Math.hs
+++ code/Data/Drasil/Concepts/Math.hs
@@ -2,7 +2,7 @@ module Data.Drasil.Concepts.Math where
 
 import Language.Drasil
 
-gradient, norm_vect, orient :: ConceptChunk 
+gradient, norm_vect, orient, matrix :: ConceptChunk 
 ode :: CINP
 
 --FIXME: Make some of these definitions better.
@@ -12,5 +12,8 @@ norm_vect = dcc "norm_vect" (cn' "normal") "unit outward normal vector for a sur
 orient = dcc "orientation" (cn' "orientation") "orientation"
 unit_vect = dcc "unit_vect" (cn' "unit vector") "unit vector"
 
+matrix = dcc "matrix" (cn' "matrix") "matrix"
+
 --FIXME: use nounphrase instead of cn'
 ode       = commonINP "ode" (cn' "Ordinary Differential Equation") "ODE"
+

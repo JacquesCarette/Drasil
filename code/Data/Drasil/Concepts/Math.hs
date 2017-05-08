4c5b8be3bde3ab537de1a7a1c9555216bc995740
Henry <hmadej@gmail.com>
2017-05-08 11:49:22 -0400

    added definition for perpendicular vector

--- code/Data/Drasil/Concepts/Math.hs
+++ code/Data/Drasil/Concepts/Math.hs
@@ -10,6 +10,7 @@ ode :: CINP
 --FIXME: Make some of these definitions better.
 gradient  = dcc "gradient" (cn' "gradient") "gradient operator"
 norm_vect = dcc "norm_vect" (cn' "normal vector") "unit outward normal vector for a surface"
+perp_vect = dcc "perp_vect" (cn' "perpendicular vector") "vector perpendicular or 90 degrees to another vector"
 -- could use better name for this? namespace collision when using orientation
 orient = dcc "orientation" (cn' "orientation") "orientation"
 unit_vect = dcc "unit_vect" (cn' "unit vector") "unit vector"
--- code/Data/Drasil/Quantities/Math.hs
+++ code/Data/Drasil/Quantities/Math.hs
@@ -7,6 +7,7 @@ gradient, norm_vect, orientation, unit_vect, euclid_norm :: ConVar
 
 gradient  = cvR CM.gradient (Greek Nabla)
 norm_vect = cvR CM.norm_vect (vec $ hat lN)
+perp_vect = cvR CM.perp_vect (vec $ lN)
 orientation = cvR CM.orient (Greek Phi_L)
 unit_vect = cvR CM.unit_vect (vec $ hat lI)
 euclid_norm = cvR CM.euclid_norm (Concat [Atomic "||", (vec lR), Atomic "||"])

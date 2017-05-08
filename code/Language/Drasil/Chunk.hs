04131925da5d51ef074630de2bc52d83df900ddd
Dan Szymczak <szymczdm@mcmaster.ca>
2017-05-08 11:04:39 -0400

    Updated Chunk.hs for haddock output

--- code/Language/Drasil/Chunk.hs
+++ code/Language/Drasil/Chunk.hs
@@ -4,5 +4,8 @@ import Control.Lens (Simple,Lens)
 
 import Prelude hiding (id)
 
+-- | The base Chunk class
 class Chunk c where
+  -- | Chunk implements the id function which provides a 
+  -- preferably /unique/ id for internal Drasil use
   id :: Simple Lens c String

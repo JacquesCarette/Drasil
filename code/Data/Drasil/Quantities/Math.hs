c3ad29d4ce8799bd72f70de421d465799c03747d
Henry <hmadej@gmail.com>
2017-05-09 12:16:18 -0400

    changed quantities, added new ones

--- code/Data/Drasil/Quantities/Math.hs
+++ code/Data/Drasil/Quantities/Math.hs
@@ -2,12 +2,20 @@ module Data.Drasil.Quantities.Math where
 
 import Language.Drasil
 import Data.Drasil.Concepts.Math as CM
+import Control.Lens((^.)) --need for parametrization hack
 
-gradient, norm_vect, orientation, unit_vect, euclid_norm, perp_vect :: ConVar
+gradient, normalVect, orientation, unitVect, euclidNorm, perpVect :: ConVar
 
 gradient  = cvR CM.gradient (Greek Nabla)
-norm_vect = cvR CM.norm_vect (vec $ lN)
-perp_vect = cvR CM.perp_vect (vec $ lN)
+normalVect = cvR normalV (vec $ lN)
+  where normalV = dcc "normal vector" (compoundPhrase' (CM.normal ^. term) (CM.vector ^. term)) "unit outward normal vector for a surface"
+--FIXME: COMBINATION HACK
+unitVect = cvR unitV (vec $ hat lI)
+  where unitV = dcc "unit_vect" (compoundPhrase' (CM.unit ^. term) (CM.vector ^. term)) "unit vector"
+perpVect = cvR perpV (vec $ lN)
+  where perpV = dcc "perp_vect" (compoundPhrase' (CM.perp ^. term) (CM.vector ^. term)) "vector perpendicular or 90 degrees to another vector"
+--perp_vect = cvR CM.perp_vect (vec $ lN)
 orientation = cvR CM.orient (Greek Phi_L)
-unit_vect = cvR CM.unit_vect (vec $ hat lI)
-euclid_norm = cvR CM.euclid_norm (Concat [Atomic "||", (vec lR), Atomic "||"])
+--unit_vect = cvR CM.unit_vect (vec $ hat lI)
+euclidNorm = cvR euclidN (Concat [Atomic "||", (vec lR), Atomic "||"])
+  where euclidN = dcc "euclidNorm" (compoundPhrase' (CM.euclidSpace ^. term) (CM.norm ^. term))"Euclidean norm"

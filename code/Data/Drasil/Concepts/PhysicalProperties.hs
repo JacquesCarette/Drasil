00bf364bd677d6945419ea1a829da01b92a2f2c8
AKM <moria@mcmaster.ca>
2017-05-09 14:17:05 -0400

    New terms added to the files of the concept section.

--- code/Data/Drasil/Concepts/PhysicalProperties.hs
+++ code/Data/Drasil/Concepts/PhysicalProperties.hs
@@ -2,7 +2,7 @@ module Data.Drasil.Concepts.PhysicalProperties where
 
 import Language.Drasil
 
-gaseous, liquid, solid, ctrOfMass, mass, length :: ConceptChunk
+gaseous, liquid, solid, ctrOfMass, density, mass, length :: ConceptChunk
 
 gaseous = dcc "gaseous" (cn''' "Gas") "gaseous state"
 liquid  = dcc "liquid" (cn' "Liquid") "liquid state"
@@ -11,9 +11,11 @@ solid   = dcc "solid" (cn' "Solid") "solid state"
 ctrOfMass = dcc "ctrOfMass" (cn "centre of mass") --FIXME: Plural?
   "The mean location of the distribution of mass of the object."
 
-mass = dcc "mass" (cn''' "mass") "the quantity of matter in a body"
+density = dcc "density" (cnIES "density") ""
 length = dcc "length" (cn' "length")
   ("the straight-line distance between two points along an object. " ++
   "Typically used to represent the size of an object from one end to the other.")
+mass = dcc "mass" (cn''' "mass") "the quantity of matter in a body"
+
   
 --TODO: Add density
--- code/Data/Drasil/Concepts/Thermodynamics.hs
+++ code/Data/Drasil/Concepts/Thermodynamics.hs
@@ -3,8 +3,8 @@ module Data.Drasil.Concepts.Thermodynamics where
 import Language.Drasil
 
 boiling,law_cons_energy, law_conv_cooling, latent_heat, melting, phase_change,
-  sens_heat, thermal_analysis, thermal_conduction, thermal_energy,
-  thermal_conductor, heat_cap_spec, heat_trans :: ConceptChunk
+  sens_heat, temperature, thermal_analysis, thermal_conduction, thermal_energy,
+  thermal_conductor, heat, heat_cap_spec, heat_trans :: ConceptChunk
 
 --To save space.
 fixme :: String
@@ -12,28 +12,32 @@ fixme = "FIXME: MISSING DEFINITION"
   
 -- FIXME: "Boiling" is not a noun. How should we deal with it?
 --    Same for "Melting"
-boiling = dcc "boiling" (cn "Boiling")
-  "Phase change from liquid to vapour"
-heat_trans = dcc "heat_trans" (cn' "Heat transfer") fixme
-heat_cap_spec = dcc "heat_cap_spec" (cnIES "specific heat capacity") fixme
-latent_heat = dcc "latent_heat" (cn' "Latent heat") fixme
-law_cons_energy = dcc "law_cons_energy" 
-  (nounPhraseSP "Law of conservation of energy" )
-  "Energy is conserved"
-law_conv_cooling = dcc "law_conv_cooling" 
-  (nounPhraseSP "Newton's law of cooling")
-  "Newton's law of convective cooling"
-melting = dcc "melting" (cn "Melting")
-  "Phase change from solid to liquid"
-phase_change = dcc "phase_change" (cn' "Phase change") "Change of state"
+boiling             = dcc "boiling"         (cn "boiling")
+                      "Phase change from liquid to vapour"
+heat                = dcc "heat"            (cn "heat") fixme
+heat_trans          = dcc "heat_trans"      (cn' "heat transfer") fixme
+heat_cap_spec       = dcc "heat_cap_spec"   (cnIES "specific heat capacity") fixme
+latent_heat         = dcc "latent_heat"     (cn' "latent heat") fixme
+law_cons_energy     = dcc "law_cons_energy" 
+                      (nounPhraseSP "law of conservation of energy" )
+                      "Energy is conserved"
+law_conv_cooling    = dcc "law_conv_cooling" 
+                      (nounPhraseSP "Newton's law of cooling")
+                      "Newton's law of convective cooling"
+melting             = dcc "melting"         (cn "melting")
+                      "Phase change from solid to liquid"
+phase_change        = dcc "phase_change"    (cn' "phase change") "Change of state"
 --FIXME: sens_heat's definition is useless.
-sens_heat = dcc "sens_heat" (cn' "Sensible heat") fixme
-thermal_analysis = dcc "thermal_analysis" 
-  (cnIP "Thermal analysis" (IrregPlur (\x -> init (init x) ++ "es")))
-  "The study of material properties as they change with temperature"
-thermal_conduction = dcc "thermal_conduction" (nounPhraseSP "Thermal conduction")
-  "The transfer of heat energy through a substance"
-thermal_conductor = dcc "thermal_conductor" (cn' "Thermal conductor")
-  ("An object through which thermal energy can be transferred")
-thermal_energy = dcc "thermal_energy" 
-  (cnIES "Thermal energy") "The energy that comes from heat"
+sens_heat           = dcc "sens_heat"       (cn' "sensible heat") fixme
+temperature         = dcc "temperature"     (cn' "temperature") fixme
+thermal_analysis    = dcc "thermal_analysis" 
+                      (cnIP "thermal analysis" (IrregPlur (\x -> init (init x) ++ "es")))
+                      "The study of material properties as they change with temperature"
+thermal_conduction  = dcc "thermal_conduction"
+                      (nounPhraseSP "thermal conduction")
+                      "The transfer of heat energy through a substance"
+thermal_conductor   = dcc "thermal_conductor"
+                      (cn' "thermal conductor")
+                      ("An object through which thermal energy can be transferred")
+thermal_energy      = dcc "thermal_energy" 
+                      (cnIES "thermal energy") "The energy that comes from heat"

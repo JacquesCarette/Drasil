1b6e72799ed18d647a786535d3a604ca1d2c3321
AKM <moria@mcmaster.ca>
2017-05-09 14:29:00 -0400

    Minor bug fix.

--- code/Data/Drasil/Concepts/PhysicalProperties.hs
+++ code/Data/Drasil/Concepts/PhysicalProperties.hs
@@ -2,7 +2,7 @@ module Data.Drasil.Concepts.PhysicalProperties where
 
 import Language.Drasil
 
-gaseous, liquid, solid, ctrOfMass, density, mass, length :: ConceptChunk
+gaseous, liquid, solid, ctrOfMass, density_, mass, length :: ConceptChunk
 
 gaseous = dcc "gaseous" (cn''' "Gas") "gaseous state"
 liquid  = dcc "liquid" (cn' "Liquid") "liquid state"
@@ -11,7 +11,7 @@ solid   = dcc "solid" (cn' "Solid") "solid state"
 ctrOfMass = dcc "ctrOfMass" (cn "centre of mass") --FIXME: Plural?
   "The mean location of the distribution of mass of the object."
 
-density = dcc "density" (cnIES "density") ""
+density_ = dcc "density" (cnIES "density") ""
 length = dcc "length" (cn' "length")
   ("the straight-line distance between two points along an object. " ++
   "Typically used to represent the size of an object from one end to the other.")

8c7c33102c72cd2b9d50c08f4d35aa28161dfde0
Daniel Scime <danscime@gmail.com>
2017-05-09 10:34:31 -0400

    removed duplicate files, re added proper files

--- code/Example/Drasil/SWHS/Generate.hs
+++ code/Example/Drasil/SWHS/Generate.hs
@@ -2,7 +2,7 @@ module Drasil.SWHS.Generate where
 
 import Language.Drasil (DocType(SRS,MG,Website), Recipe(..), gen)
 
-import Drasil.SWHS.Body (swhs_srs', swhs_mg)
+import Drasil.SWHS.Body (swhs_srs', swhs_srs, swhs_mg)
 
 docs :: [Recipe]
 docs = [Recipe (SRS "SWHS_SRS") swhs_srs',

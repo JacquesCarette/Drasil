9ea2307d485750ee2e7d5c9498a63377cd959b83
Henry <hmadej@gmail.com>
2017-05-10 11:25:58 -0400

    removed unused variables

--- code/Example/Drasil/GamePhysics/Body.hs
+++ code/Example/Drasil/GamePhysics/Body.hs
@@ -69,8 +69,8 @@ mgBod :: [Section]
 --fold helper functions applies f to all but the last element applies g to 
 --last element and the accumulator
 foldle :: (a -> a -> a) -> (a -> a -> a) -> a -> [a] -> a
-foldle f g z []     = z
-foldle f g z [x]    = g z x
+foldle _ _ z []     = z
+foldle _ g z [x]    = g z x
 foldle f g z [x,y]  = g (f z x) y
 foldle f g z (x:xs) = foldle f g (f z x) xs
 

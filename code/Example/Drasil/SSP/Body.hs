70b9b056134fb833148f38d8c6ad6d1d1850d32b
njericha <njericha@edu.uwaterloo.ca>
2017-05-11 15:25:56 -0400

    fixed type error

--- code/Example/Drasil/SSP/Body.hs
+++ code/Example/Drasil/SSP/Body.hs
@@ -408,7 +408,7 @@ s5_1_list = Enumeration $ Simple $ (map (\(a,b) -> (a, Flat b)) [
 s5_1_table = table_inputdata
 
 table_inputdata :: Contents --FIXME: use table function?
-table_inputdata =  Table $ map titleize [symbol_, unit_, description]
+table_inputdata =  Table [titleize symbol_, titleize' unit_, titleize description]
   (mkTable
     [(\ch -> P (ch ^. symbol)),
      (\ch -> Sy $ unit_symb ch),

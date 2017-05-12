d23e0aa6043eea08acbea6e632e132aa6cdffbf4
Spencer Smith <smiths@mcmaster.ca>
2017-05-12 16:58:48 -0400

    Removal of the SWHS-PCM example documentation and code, since the same information is reproduced at https://github.com/smiths/swhs.  It isn't a good idea to maintain the documentation in two separate locations, and the other repo is cited in several papers.

--- CaseStudies/swpcm/Program/Testing/errorCalc.m
+++
@@ -1,9 +0,0 @@
-function del_rel = errorCalc(trueTime, calcTime, trueVect, calcVect)
-
-%errorCalc calculates the percentage error between two curves
-
-trueVect_new = interp1(trueTime, trueVect, calcTime, 'linear', 'extrap');
-
-delta = trueVect_new - calcVect;
-
-del_rel = norm(delta)/norm(trueVect_new);
--- CaseStudies/swpcm/Program/Testing/testMain.m
+++
@@ -1,3 +0,0 @@
-%Note to self: this was originally in the Program directory
-results = runtests(pwd);
-  

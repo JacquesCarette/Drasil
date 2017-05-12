28b6663dfd514323843aad753a3d4134eb9f3db6
Maryyam Niazi <niazim3@mcmaster.ca>
2017-05-12 14:21:04 -0400

    removed 'a_''

--- code/Language/Drasil.hs
+++ code/Language/Drasil.hs
@@ -25,11 +25,7 @@ module Language.Drasil (
   -- Chunk.NamedIdea
   , NamedIdea(..), NamedChunk, NPNC, short, nc, nc', npnc
   , compoundterm, for, for', for'', of_, of', of'', compoundNPNC, compoundNPNC'
-<<<<<<< HEAD
-  , npnc', with, with', and_, and_', aNP, a_, a_'
-=======
-  , npnc', with, with', and_, and_',aNP, the
->>>>>>> exported 'the' combinator
+  , npnc', with, with', and_, and_',aNP, the, a_
   -- Chunk.Constrained
   , Constrained(..)
   -- Chunk.Eq

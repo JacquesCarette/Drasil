9ef18b534bf976e9cca39ac58110131fd6c78c83
Daniel Scime <danscime@gmail.com>
2017-05-16 10:22:31 -0400

    Added 'System Context' section to stable versions. Fixed some errors.

--- code/Example/Drasil/SWHS/Body.hs
+++ code/Example/Drasil/SWHS/Body.hs
@@ -48,7 +48,7 @@ s2, s2_1, s2_2, s2_3, s2_4, s3, s3_2, s3_3, s4, s4_1,
   s4_2_6, s4_2_7, s5, s5_1, s5_2, s6, s7 :: Section
 
 s2_2_contents, s2_3_contents, s3_intro, s3_1_contents, sys_context_fig,
-    s3_1_2_intro, s3_1_2_bullets, s3_2_contents, s4_intro, 
+  s3_1_2_intro, s3_1_2_bullets, s3_2_contents, s4_intro, 
   s4_1_intro, s4_1_1_intro, s4_1_1_bullets, s4_1_2_intro, s4_1_2_list,
   fig_tank, s4_1_3_intro, s4_1_3_list, s4_2_intro, s4_2_1_intro, 
   s4_2_1_list, s4_2_2_intro, s4_2_3_intro, s4_2_4_intro, s4_2_6_intro, 
@@ -263,7 +263,8 @@ s3_intro = Paragraph (S "This" +:+ phrase section_ +:+ S "provides" +:+
 
 
 
-s3_1 = section (titleize physSyst) [s3_1_intro, sys_context_fig, s3_1_2_intro, s3_1_2_bullets] []
+s3_1 = section (S "System Context") [s3_1_contents, sys_context_fig, s3_1_2_intro,
+  s3_1_2_bullets] []
 
 s3_1_contents = Paragraph ((makeRef sys_context_fig) +:+ S "shows the system" +:+
   S "context. A circle represents an external entity outside the software," +:+
@@ -272,7 +273,7 @@ s3_1_contents = Paragraph ((makeRef sys_context_fig) +:+ S "shows the system" +:
   S "system andits environment.")
 
 sys_context_fig = Figure ((makeRef sys_context_fig) :+: S ": System Context")
-  "SystemContextFigure.pdf"
+  "SystemContextFigure.png"
 
 s3_1_2_intro = Paragraph (S "SWHS is mostly self-contained. The only" +:+
   S "external interaction is through the user inter- face. The" +:+
@@ -284,13 +285,13 @@ s3_1_2_bullets = Enumeration (Bullet $
   S "Take care that consistent units are used for input variables"
   ]),
 
-  [Nested (S "SWHS Responsibilities:") (Bullet $ map (\c -> Flat c)
+  Nested (S "SWHS Responsibilities:") (Bullet $ map (\c -> Flat c)
   [S "Detect data type mismatch, such as a string of characters instead" +:+
   S "of a floating point number",
   S "Determine if the inputs satisfy the required physical and software constraints",
   S "Calculate the required outputs"
-  ])
-  ]
+  ])])
+  
 
 
 

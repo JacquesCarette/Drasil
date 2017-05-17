0f710a3779d98b40a59c4dfb83e71d5d3a4b0457
palmerst <palmes4@mcmaster.ca>
2017-05-17 14:52:03 -0400

    Input Constraints module gool code done (for now)

--- GOOLCodeGen/GlassBR/Modules/InputConstraints.hs
+++ GOOLCodeGen/GlassBR/Modules/InputConstraints.hs
@@ -2,6 +2,7 @@ module Modules.InputConstraints (inputConstraints) where
 
 import Language.Drasil.Code
 
+-- TODO:  add gool support for non-object functions;  don't need a class for this
 inputConstraints :: Class
 inputConstraints = pubClass
   "InputConstraints"
@@ -10,7 +11,19 @@ inputConstraints = pubClass
   [ pubMethod methodTypeVoid "check_constraints" (params [("params", obj "InputParameters")]) 
     [ 
       block [
-        ifelse [ (var "params" $-> var "a" ?<= (litFloat 0.0), oneLiner (excThrow "InputError: a must be greater than 0")) ] 
+        -- TODO:  add gool support for "else-less" if statements
+        ifelse [ 
+          (var "params" $-> var "a" ?<= (litFloat 0.0), oneLiner (excThrow "InputError: a must be greater than 0")),
+          (var "params" $-> var "b" ?<= (litFloat 0.0), oneLiner (excThrow "InputError: b must be greater than 0")),
+          (var "params" $-> var "asprat" ?< (litFloat 1.0), oneLiner (excThrow "InputError: a/b cannot be less than 1.0")),
+          (var "params" $-> var "asprat" ?> (litFloat 5.0), oneLiner (excThrow "InputError: a/b cannot be greater than 5.0")),
+          -- TODO: missing a condition, gool needs logical operators
+          (var "params" $-> var "tnt" ?<= (litFloat 0.0), oneLiner (excThrow "InputError: tnt must be greater than 0")),
+          (var "params" $-> var "wtnt" ?< (litFloat 4.5), oneLiner (excThrow "InputError: wtnt cannot be less than 4.5")),
+          (var "params" $-> var "wtnt" ?> (litFloat 910.0), oneLiner (excThrow "InputError: wtnt cannot be greater than 910.0")),
+          (var "params" $-> var "sd" ?< (litFloat 6.0), oneLiner (excThrow "InputError: sd cannot be less than 6.0")),
+          (var "params" $-> var "sd" ?> (litFloat 130.0), oneLiner (excThrow "InputError: sd cannot be greater than 130.0"))
+        ] 
         [ block [] ]
       ]
     ]

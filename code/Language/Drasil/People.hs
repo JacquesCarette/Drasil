5e9d19ad7f1b88572f4108e18e92b4cdcc57370b
Dan Szymczak <szymczdm@mcmaster.ca>
2017-05-17 13:56:17 -0400

    Update haddock for People.hs

--- code/Language/Drasil/People.hs
+++ code/Language/Drasil/People.hs
@@ -7,26 +7,48 @@ module Language.Drasil.People
 import Language.Drasil.Spec (Sentence(S, EmptyS),(+:+), sC)
 import Data.List
 
+-- | A person can have a given name, middle name(s), and surname, as well
+-- as the naming convention they use.
 data Person = Person { _given :: String, _surname :: String, 
                        _middle :: [String], _convention :: Conv}
+-- ^ Western style conventions are given name followed
+-- by middle names, followed by surname.
+-- Eastern style conventions are surname followed by middle names, 
+-- followed by given name.
+-- Mononyms are for those people who have only one name (ex. Madonna)
+
 type People = [Person]
 
+-- | Naming conventions.
 data Conv = Western
           | Eastern
           | Mono
 
+-- | Constructor for a person using Western naming conventions. 
+-- Used for a person with only a given name and surname.
+-- Arguments are in the order: given name, surname.
 person :: String -> String -> Person
 person f l = Person f l [] Western
 
+-- | Constructor for a person using Eastern naming conventions. 
+-- Used for a person with only a given name and surname.
+-- Arguments are in the order: surname, given name
 person' :: String -> String -> Person
-person' g s = Person g s [] Eastern
+person' s g = Person g s [] Eastern
 
+-- | Constructor for a person using Western naming conventions.
+-- Similar to the 'person' constructor,
+-- except the middle argument is a list of middle names.
 personWM :: String -> [String] -> String -> Person
 personWM f ms l = Person f l ms Western
 
+-- | Constructor for a person using Eastern naming conventions.
+-- Similar to the 'person\'' constructor,
+-- except the middle argument is a list of middle names.
 personWM' :: String -> [String] -> String -> Person
 personWM' g ms s = Person g s ms Eastern
 
+-- | Constructor for a person with a mononym (only one name).
 mononym :: String -> Person
 mononym n = Person "NFN" n [] Mono
 
@@ -40,6 +62,7 @@ instance HasName Person where
 
 -- this is a weirder recursion, so it's ok to do it explicitly
 -- make it work for short lists too, but it shouldn't be used that way!
+-- | Used for rendering lists of names (one or more) to Sentences.
 manyNames :: (HasName p) => [p] -> Sentence
 manyNames [x,y] = (name x) +:+ (S "and") +:+ (name y)
 manyNames names = nameList names

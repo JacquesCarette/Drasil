61424d91197c52f5c7ba328fa62f160a235d453e
Dan Szymczak <szymczdm@mcmaster.ca>
2017-05-17 12:18:33 -0400

    Updated People to allow for more culturally appropriate uses. Fixes #81

--- code/Language/Drasil/People.hs
+++ code/Language/Drasil/People.hs
@@ -1,19 +1,42 @@
-module Language.Drasil.People (People, Person, person, HasName, name, manyNames) where
+module Language.Drasil.People 
+  ( People, Person
+  , person, person', personWM, personWM', mononym
+  , HasName
+  , name, manyNames) where
 
 import Language.Drasil.Spec (Sentence(S, EmptyS),(+:+), sC)
+import Data.List
 
-data Person = Person { _first :: String, _last :: String, _name :: Sentence}
+data Person = Person { _given :: String, _surname :: String, 
+                       _middle :: [String], _convention :: Conv}
 type People = [Person]
 
+data Conv = Western
+          | Eastern
+          | Mono
 
 person :: String -> String -> Person
-person f l = Person f l (S $ f ++ " " ++ l)
+person f l = Person f l [] Western
+
+person' :: String -> String -> Person
+person' g s = Person g s [] Eastern
+
+personWM :: String -> [String] -> String -> Person
+personWM f ms l = Person f l ms Western
+
+personWM' :: String -> [String] -> String -> Person
+personWM' g ms s = Person g s ms Eastern
+
+mononym :: String -> Person
+mononym n = Person "NFN" n [] Mono
 
 class HasName p where
   name :: p -> Sentence
 
 instance HasName Person where
-  name (Person _ _ n) = n
+  name (Person _ n _ Mono) = S n
+  name (Person f l ms Western) = S $ concat (intersperse " " $ [f] ++ ms ++ [l])
+  name (Person g s ms Eastern) = S $ concat (intersperse " " $ [s] ++ ms ++ [g])
 
 -- this is a weirder recursion, so it's ok to do it explicitly
 -- make it work for short lists too, but it shouldn't be used that way!

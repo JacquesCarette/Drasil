5e23bf0462dca885444139abb1e2b49bae113507
njericha <njericha@edu.uwaterloo.ca>
2017-05-18 11:41:34 -0400

    Removed unnecessary TableOfSymbols duplicate function

--- code/Example/Drasil/TableOfSymbols.hs
+++ code/Example/Drasil/TableOfSymbols.hs
@@ -1,33 +1,17 @@
 -- Standard code to make a table of symbols.
 module Drasil.TableOfSymbols 
-  ( table_of_symbols
-  , table
+  ( table
   , defnExcept
   , termExcept) where
 
 import Control.Lens ((^.))
 
 import Data.Maybe (isJust)
-import Data.List (sort)
 
 import Language.Drasil
 import qualified Data.Drasil.Concepts.Math as CM
 import Data.Drasil.Concepts.Documentation
-
-table_of_symbols :: (Ord s,Quantity s) => 
-  [s] -> (s -> Sentence) -> Section
-table_of_symbols ls f = Section (titleize tOfSymb)
-  [Con intro, Con (table (sort ls) f)]
-
-intro :: Contents
-intro = Paragraph $ 
-  S "The table that follows summarizes the symbols used in this" +:+
-  S "document along with their units.  The choice of symbols was" +:+
-  S "made with the goal of being consistent with the nuclear" +:+
-  S "physics literature and that used in the FP manual.  The SI" +:+
-  S "units are listed in brackets following the definition of" +:+
-  S "the symbol."
-  
+ 
 --Removed SymbolForm Constraint and filtered non-symbol'd chunks 
 table :: (Quantity s) => [s] -> (s -> Sentence) -> Contents
 table ls f = Table [at_start symbol_, at_start description, at_start' (CM.unit_ ^. term)] (mkTable

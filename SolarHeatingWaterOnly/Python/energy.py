79a19f13ac9be7af133559550d13f0ef9ddf5d72
AKM11 <moria@mcmaster.ca>
2017-05-24 10:49:44 -0400

    Removal of all(?) references to PCM.

--- SolarHeatingWaterOnly/Python/energy.py
+++ SolarHeatingWaterOnly/Python/energy.py
@@ -6,14 +6,6 @@ def energy1Wat(tWat, params):
     return eWat
 
 
-def energy1PCM(tPCM, params):
-    ePCM = []
-    for temp in tPCM:
-        ePCM = ePCM + [params.C_ps * params.Mp * (temp - params.Tinit)]
-
-    return ePCM
-
-
 def energy2Wat(tWat, params):
     eWat = []
     for temp in tWat:
@@ -22,25 +14,9 @@ def energy2Wat(tWat, params):
     return eWat
 
 
-def energy2PCM(Qp, params):
-    ePCM = []
-    for latHeat in Qp:
-        ePCM = ePCM + [params.Epmelt_init + latHeat]
-
-    return ePCM
-
-
 def energy3Wat(tWat, params):
     eWat = []
     for temp in tWat:
         eWat = eWat + [params.C_w * params.Mw * (temp - params.Tinit)]
 
     return eWat
-
-
-def energy3PCM(tPCM, params):
-    ePCM = []
-    for temp in tPCM:
-        ePCM = ePCM + [params.Epmelt_init + params.Ep_melt3 + params.C_pl * params.Mp * (temp - params.Tmelt)]
-
-    return ePCM
--- SolarHeatingWaterOnly/Python/load_params.py
+++ SolarHeatingWaterOnly/Python/load_params.py
@@ -1,6 +1,7 @@
 import parameters
 import math
 
+#Commented lines are ones that probably need removal but are awaiting verification
 
 def load_params(filename):
     f = open(filename, 'r')
@@ -17,37 +18,37 @@ def load_params(filename):
 
     params.L = param[0]
     params.diam = param[1]
-    params.Vp = param[2]
-    params.Ap = param[3]
-    params.rho_p = param[4]
-    params.Tmelt = param[5]
-    params.C_ps = param[6]
-    params.C_pl = param[7]
-    params.Hf = param[8]
-    params.Ac = param[9]
-    params.Tc = param[10]
-    params.rho_w = param[11]
-    params.C_w = param[12]
-    params.hc = param[13]
-    params.hp = param[14]
-    params.Tinit = param[15]
-    params.tstep = param[16]
-    params.tfinal = param[17]
-    params.AbsTol = param[18]
-    params.RelTol = param[19]
-    params.ConsTol = param[20]
+#    params.Vp = param[2]
+#    params.Ap = param[3]
+#    params.rho_p = param[4]
+#    params.Tmelt = param[5]
+#    params.C_ps = param[6]
+#    params.C_pl = param[7]
+#    params.Hf = param[2]
+    params.Ac = param[3]
+    params.Tc = param[4]
+    params.rho_w = param[5]
+    params.C_w = param[6]
+    params.hc = param[7]
+#    params.hp = param[14]
+    params.Tinit = param[8]
+    params.tstep = param[9]
+    params.tfinal = param[10]
+    params.AbsTol = param[11]
+    params.RelTol = param[12]
+    params.ConsTol = param[13]
 
     # calculated parameters
 
     params.Vt = math.pi * (params.diam / 2) ** 2 * params.L
     params.Mw = params.rho_w * (params.Vt - params.Vp)
     params.tau_w = (params.Mw * params.C_w) / (params.hc * params.Ac)
-    params.eta = (params.hp * params.Ap) / (params.hc * params.Ac)
-    params.Mp = params.rho_p * params.Vp
-    params.tau_ps = (params.Mp * params.C_ps) / (params.hp * params.Ap)
-    params.tau_pl = (params.Mp * params.C_pl) / (params.hp * params.Ap)
-    params.Epmelt_init = params.C_ps * params.Mp * (params.Tmelt - params.Tinit)
-    params.Ep_melt3 = params.Hf * params.Mp
+#    params.eta = (params.hp * params.Ap) / (params.hc * params.Ac)
+#    params.Mp = params.rho_p * params.Vp
+#    params.tau_ps = (params.Mp * params.C_ps) / (params.hp * params.Ap)
+#    params.tau_pl = (params.Mp * params.C_pl) / (params.hp * params.Ap)
+#    params.Epmelt_init = params.C_ps * params.Mp * (params.Tmelt - params.Tinit)
+#    params.Ep_melt3 = params.Hf * params.Mp
     params.Mw_noPCM = params.rho_w * params.Vt
     params.tau_w_noPCM = (params.Mw_noPCM * params.C_w) / (params.hc * params.Ac)
 

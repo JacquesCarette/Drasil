cef3ac5ec9d2dc176d6cb523a25ac0dd657deb3c
Henry <hmadej@gmail.com>
2017-05-25 12:09:33 -0400

    qualified imports

--- code/Example/Drasil/GamePhysics/Body.hs
+++ code/Example/Drasil/GamePhysics/Body.hs
@@ -11,16 +11,20 @@ import Data.Drasil.SI_Units
 import Data.Drasil.Authors
 import Data.Drasil.Concepts.Documentation
 import Data.Drasil.Concepts.Software
+import qualified Data.Drasil.Quantities.Math as QM (orientation)
+import qualified Data.Drasil.Quantities.Physics as QP (restitutionCoef, time, 
+  position, torque, acceleration, angularAccel, force, gravitationalConst, 
+  gravitationalAccel, velocity, momentOfInertia, displacement, angularVelocity)
+import qualified Data.Drasil.Quantities.PhysicalProperties as QPP (mass, len)
 import qualified Data.Drasil.Concepts.Physics as CP (rigidBody, elasticity, 
   cartesian, friction, rightHand, collision, space, joint)
-import Data.Drasil.Concepts.PhysicalProperties (ctrOfMass, dimension)
+import qualified Data.Drasil.Concepts.PhysicalProperties as CPP (ctrOfMass, 
+  dimension)
 import qualified Data.Drasil.Concepts.Math as CM (equation, surface, ode, 
   constraint)
 import Data.Drasil.Utils (foldle, foldlSent, mkEnumAbbrevList, mkConstraintList, 
   makeTMatrix)
 import Data.Drasil.Software.Products
-import Data.Drasil.Quantities.Physics (restitutionCoef, time)
-import Data.Drasil.Quantities.PhysicalProperties (mass, len)
 
 import Drasil.SpecificSystemDescription
 import Drasil.OrganizationOfSRS
@@ -275,7 +279,7 @@ s4_1_1_intro = Paragraph $ foldle (+:+) (+:) (EmptyS)
 
 s4_1_1_bullets = Enumeration (Bullet $ 
   (map (\x -> Flat $ (at_start $ x ^. term) :+: S ":" +:+ (x ^. defn)))
-  [CP.rigidBody, CP.elasticity, ctrOfMass, CP.cartesian, CP.rightHand])
+  [CP.rigidBody, CP.elasticity, CPP.ctrOfMass, CP.cartesian, CP.rightHand])
 
 
 -----------------------------
@@ -290,33 +294,33 @@ s4_1_2 = SRS.goalStmt [s4_1_2_list] []
 s4_1_2_stmt1, s4_1_2_stmt2, s4_1_2_stmt3, s4_1_2_stmt4 :: Sentence
 s4_1_2_stmt1 = foldlSent 
   [S "Given the", (plural physicalProperty) `sC` S "initial", 
-  (plural $ position ^. term), S "and",
-  (plural $ vel ^. term) `sC` S "and", (plural $ force ^. term),
+  (plural $ QP.position ^. term), S "and",
+  (plural $ QP.velocity^. term) `sC` S "and", (plural $ QP.force ^. term),
   S "applied on a set of", (plural $ CP.rigidBody ^. term) `sC`
-  S "determine their new", (plural $ position ^. term), S "and",
-  (plural $ vel ^. term), S "over a period of", (phrase $ time ^. term)]
+  S "determine their new", (plural $ QP.position ^. term), S "and",
+  (plural $ QP.velocity ^. term), S "over a period of", (phrase $ QP.time ^. term)]
 
 s4_1_2_stmt2 = foldlSent 
   [S "Given the", (plural physicalProperty) `sC` S "initial", 
-  (plural $ orientation ^. term), S "and", (plural $ angVel ^. term) `sC`
-  S "and", (plural $ force ^. term), S "applied on a set of", 
+  (plural $ QM.orientation ^. term), S "and", (plural $ QP.angularVelocity  ^. term) `sC`
+  S "and", (plural $ QP.force ^. term), S "applied on a set of", 
   (plural $ CP.rigidBody ^. term) `sC` S "determine their new",
-  (plural $ orientation ^. term), S "and", (plural $ angVel ^. term), 
-  S "over a period of", (phrase $ time ^. term)]
+  (plural $ QM.orientation ^. term), S "and", (plural $ QP.angularVelocity  ^. term), 
+  S "over a period of", (phrase $ QP.time ^. term)]
 
 s4_1_2_stmt3 = foldlSent 
-  [S "Given the initial", (plural $ position ^. term), S "and", 
-  (plural $ vel ^. term), S "of a", S "set of", 
+  [S "Given the initial", (plural $ QP.position ^. term), S "and", 
+  (plural $ QP.velocity ^. term), S "of a", S "set of", 
   (plural $ CP.rigidBody ^. term) `sC` S "determine if any of",
   S "them will collide with one another over a period of", 
-  (phrase $ time ^. term)]
+  (phrase $ QP.time ^. term)]
 
 s4_1_2_stmt4 = foldlSent 
   [S "Given the", (plural physicalProperty) :+: S ",", S "initial linear and angular", 
-  (plural $ position ^. term), 
-  S "and", (plural $ vel ^. term) `sC` S "determine the new",
-  (plural $ position ^. term), S "and", (plural $ vel ^. term),
-  S "over a period of", (phrase $ time ^. term), S "of",
+  (plural $ QP.position ^. term), 
+  S "and", (plural $ QP.velocity ^. term) `sC` S "determine the new",
+  (plural $ QP.position ^. term), S "and", (plural $ QP.velocity ^. term),
+  S "over a period of", (phrase $ QP.time ^. term), S "of",
   (plural $ CP.rigidBody ^. term), S "that have undergone a", 
   (phrase $ CP.collision ^. term)]
 
@@ -435,7 +439,7 @@ s4_2_4 = SRS.dataDefn ([s4_2_4_intro] ++
 
 s4_2_4_intro = Paragraph $ foldlSent [S "This", (phrase section_), 
   S "collects and defines all the", (plural datum), S "needed to build the" +:+. 
-  titleize' inModel, S "The", (phrase $ dimension ^. term), S "of each", 
+  titleize' inModel, S "The", (phrase $ CPP.dimension ^. term), S "of each", 
   (phrase quantity), S "is also given"]
 
 s4_2_4_DDefs = map Definition (map Data cpDDefs)
@@ -505,17 +509,17 @@ s4_2_6_intro = Paragraph $ foldlSent
 
 s4_2_6_symbolList :: [(Sentence, Sentence, Sentence, Sentence, Sentence)]
 s4_2_6_symbolList = 
-  [((P $ len ^. symbol), S "is G/E to 0", EmptyS, S "44.2", (Sy $ unit_symb len)), 
-  ((P $ mass ^. symbol), S "is greater than 0", EmptyS, S "56.2", (Sy $ unit_symb mass)), 
-  ((P $ momtInert ^. symbol), S "is G/E to 0", EmptyS, S "74.5", (Sy $ unit_symb momtInert)),
-  ((P $ gravAccel ^. symbol), EmptyS, EmptyS, S "9.8", (Sy $ unit_symb gravAccel)), 
-  ((P $ position ^. symbol), EmptyS, EmptyS, S "(0.412, 0.502)", (Sy $ unit_symb position)), 
-  ((P $ vel ^. symbol), EmptyS, EmptyS, S "2.51", (Sy $ unit_symb vel)), 
-  ((P $ restitutionCoef ^. symbol), S "G/E to 0", S "less than 1", S "0.8", EmptyS),
-  ((P $ orientation ^. symbol), S "G/E to 0", S "less than 2pi", S "pi/2", (Sy $ unit_symb orientation)),  
-  ((P $ angVel ^. symbol), EmptyS, EmptyS, S "2.1", (Sy $ unit_symb angVel)),
-  ((P $ force ^. symbol), EmptyS, EmptyS, S "98.1", (Sy $ unit_symb force)), 
-  ((P $ torque ^. symbol), EmptyS, EmptyS, S "200", (Sy $ unit_symb torque))]
+  [((P $ QPP.len ^. symbol), S "is G/E to 0", EmptyS, S "44.2", (Sy $ unit_symb QPP.len)), 
+  ((P $ QPP.mass ^. symbol), S "is greater than 0", EmptyS, S "56.2", (Sy $ unit_symb QPP.mass)), 
+  ((P $ QP.momentOfInertia ^. symbol), S "is G/E to 0", EmptyS, S "74.5", (Sy $ unit_symb QP.momentOfInertia)),
+  ((P $ QP.gravitationalAccel ^. symbol), EmptyS, EmptyS, S "9.8", (Sy $ unit_symb QP.gravitationalAccel)), 
+  ((P $ QP.position ^. symbol), EmptyS, EmptyS, S "(0.412, 0.502)", (Sy $ unit_symb QP.position)), 
+  ((P $ QP.velocity ^. symbol), EmptyS, EmptyS, S "2.51", (Sy $ unit_symb QP.velocity)), 
+  ((P $ QP.restitutionCoef ^. symbol), S "G/E to 0", S "less than 1", S "0.8", EmptyS),
+  ((P $ QM.orientation ^. symbol), S "G/E to 0", S "less than 2pi", S "pi/2", (Sy $ unit_symb QM.orientation)),  
+  ((P $ QP.angularVelocity  ^. symbol), EmptyS, EmptyS, S "2.1", (Sy $ unit_symb QP.angularVelocity )),
+  ((P $ QP.force ^. symbol), EmptyS, EmptyS, S "98.1", (Sy $ unit_symb QP.force)), 
+  ((P $ QP.torque ^. symbol), EmptyS, EmptyS, S "200", (Sy $ unit_symb QP.torque))]
 
 s4_2_6_table1 = Table [S "Var", titleize' physicalConstraint, S "Typical Value"]
   (mkTable [(\x -> x!!0), (\x -> x!!1), (\x -> x!!2)] $ map (mkConstraintList) s4_2_6_symbolList) 
@@ -523,11 +527,11 @@ s4_2_6_table1 = Table [S "Var", titleize' physicalConstraint, S "Typical Value"]
 
 s4_2_6_table2 = Table [S "Var", titleize' physicalConstraint]
   (mkTable [(\x -> x!!0), (\x -> x!!1)] [
-  [(P $ position ^. symbol), S "None"],
-  [(P $ vel ^. symbol), S "None"],
-  [(P $ orientation ^. symbol), (P $ orientation ^. symbol) +:+ S "G/E to 0" +:+
-  S "and" +:+ (P $ orientation ^. symbol) +:+ S "less than 2pi"],
-  [(P $ angVel ^. symbol), S "None"]
+  [(P $ QP.position ^. symbol), S "None"],
+  [(P $ QP.velocity ^. symbol), S "None"],
+  [(P $ QM.orientation ^. symbol), (P $ QM.orientation ^. symbol) +:+ S "G/E to 0" +:+
+  S "and" +:+ (P $ QM.orientation ^. symbol) +:+ S "less than 2pi"],
+  [(P $ QP.angularVelocity  ^. symbol), S "None"]
   ]) (S "Table 2: Output Variables") True
 
 ------------------------------
@@ -564,9 +568,9 @@ s5_1_req1 = foldlSent [S "Create a", (phrase $ CP.space ^. term), S "for all of 
   S "to interact in"]
 
 s5_1_req2 = foldlSent [S "Input the initial", 
-  (plural $ mass ^. term) `sC` (plural $ vel ^. term) `sC` 
-  (plural $ orientation ^. term) `sC` (plural $ angVel ^. term), 
-  S "of" `sC` S "and", (plural $ force ^. term), S "applied on", 
+  (plural $ QPP.mass ^. term) `sC` (plural $ QP.velocity ^. term) `sC` 
+  (plural $ QM.orientation ^. term) `sC` (plural $ QP.angularVelocity  ^. term), 
+  S "of" `sC` S "and", (plural $ QP.force ^. term), S "applied on", 
   (plural $ CP.rigidBody ^. term)]
 
 s5_1_req3 = foldlSent [S "Input the", (phrase $ CM.surface ^. term), 
@@ -577,21 +581,21 @@ s5_1_req4 = foldlSent [S "Verify that the inputs",
   S "satisfy the required", plural physicalConstraint, S "from", (makeRef s4_2_6_table1)]
 
 s5_1_req5 = foldlSent 
-  [S "Determine the", (plural $ position ^. term), S "and", (plural $ vel ^. term), 
-  S "over a", S "period of", (phrase $ time ^. term), S "of the", (getAcc twoD),
-  (plural $ CP.rigidBody ^. term), S "acted upon by a", (phrase $ force ^. term)]
+  [S "Determine the", (plural $ QP.position ^. term), S "and", (plural $ QP.velocity ^. term), 
+  S "over a", S "period of", (phrase $ QP.time ^. term), S "of the", (getAcc twoD),
+  (plural $ CP.rigidBody ^. term), S "acted upon by a", (phrase $ QP.force ^. term)]
 
 s5_1_req6 = foldlSent
-  [S "Determine the", (plural $ orientation ^. term), S "and", 
-  (plural $ angVel ^. term), S "over a period of", (phrase $ time ^. term),
+  [S "Determine the", (plural $ QM.orientation ^. term), S "and", 
+  (plural $ QP.angularVelocity  ^. term), S "over a period of", (phrase $ QP.time ^. term),
    S "of the", (getAcc twoD), (plural $ CP.rigidBody ^. term)]
 
 s5_1_req7 = foldlSent [S "Determine if any of the", (plural $ CP.rigidBody ^. term), 
   S "in the", (phrase $ CP.space ^. term), S "have collided"]
 
 s5_1_req8 = foldlSent
-  [S "Determine the", (plural $ position ^. term), S "and", (plural $ vel ^. term), 
-  S "over a", S "period of", (phrase $ time ^. term), S "of the", (getAcc twoD), 
+  [S "Determine the", (plural $ QP.position ^. term), S "and", (plural $ QP.velocity ^. term), 
+  S "over a", S "period of", (phrase $ QP.time ^. term), S "of the", (getAcc twoD), 
   (plural $ CP.rigidBody ^. term), S "that have undergone a", 
   (phrase $ CP.collision ^. term)]
 

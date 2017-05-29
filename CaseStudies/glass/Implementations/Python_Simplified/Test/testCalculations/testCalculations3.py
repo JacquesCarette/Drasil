9c33d64c36e77cb333067af4027f7ee7aa4ee058
palmerst <palmes4@mcmaster.ca>
2017-05-29 08:41:05 -0400

    Copy of glassbr python implementation to be simplified for gool

---
+++ CaseStudies/glass/Implementations/Python_Simplified/Test/testCalculations/testCalculations3.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+from Implementation import param
+from Implementation import inputFormat
+from Implementation import derivedValues
+from numpy import float64
+from Implementation import calculations
+ 
+class TestCalculations(unittest.TestCase):
+ 
+    def setUp(self):
+        self.params = param.Param()
+        self.inputFormat = inputFormat.get_input(os.path.join("Test/Inputfiles","testInput2.txt"),self.params)
+        self.derivedValues = derivedValues.derived_params(self.params)
+ 
+    def test_calc_pb(self):
+        """
+        Test to make sure returns expected value of pb.  Test should
+        actually use some epsilon error, instead of equality of floats
+        """
+        pb = calculations.calc_pb(float64(1.225261500913697e+01), self.params)
+        self.assertEqual(pb, 3.459068155453604e-04)
+    
+    def test_calc_lr(self):
+        (lr,nfl) = calculations.calc_lr(float64(5.895281852290154e+01), self.params)
+        self.assertTupleEqual((lr,nfl),(1.0929742089945215e+01,2.7324355224863037e+00))
+        
+    def test_is_safe(self):
+        (is_safe1, is_safe2, safe) = calculations.is_safe(float64( 3.459068155453604e-04),\
+        float64(1.092974208994522e+01),float64(5.777809021268771e+00),self.params)
+        self.assertTupleEqual((is_safe1,is_safe2,safe),(True,True,\
+        'For the given input parameters, the glass is considered safe'))
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ CaseStudies/glass/Implementations/Python_Simplified/Test/testDerivedValues/testDerivedValues3.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+from Implementation import param
+from Implementation import derivedValues
+from Implementation import inputFormat
+
+class TestDerivedValues(unittest.TestCase):
+    
+    def setup(self):
+        pass
+    
+    def test_derived_params(self):
+        params=param.Param()
+        inputFormat.get_input(os.path.join("Test/Inputfiles","testInput2.txt"),params)
+        derivedValues.derived_params(params)
+        self.assertEqual(1.25,params.asprat)
+        self.assertEqual(9.093404203047394,params.sd)
+        self.assertEqual(0.2696493494752911,params.ldf)
+        self.assertEqual(15.0,params.wtnt)
+        self.assertEqual(9.02,params.h)
+        self.assertEqual(4,params.gtf)
+        
+if __name__ == '__main__':
+    unittest.main()

c9508906c6dba33ce593e0f29bd754eeb4f6b877
palmerst <palmes4@mcmaster.ca>
2017-05-29 08:44:16 -0400

    Removed multiple output functions from readTable.py

--- CaseStudies/glass/Implementations/Python_Simplified/Implementation/readTable.py
+++ CaseStudies/glass/Implementations/Python_Simplified/Implementation/readTable.py
@@ -6,14 +6,19 @@ case the input is the tabular data necessary for the different interpolations.
 import numpy as np
 
 
-def read_table(filename):
+def read_num_col(filename):
     with open(filename, 'rb') as f:
         num_col = [f.readline()]
 
     num_col = np.genfromtxt(num_col, delimiter=',', dtype=str)
     num_col = num_col[1::2].astype(float)
-
-    array1 = np.loadtxt(filename, delimiter=',', usecols=range(0, 2*len(num_col), 2), skiprows=1)
-    array2 = np.loadtxt(filename, delimiter=',', usecols=range(1, 2*len(num_col), 2), skiprows=1)
-
-    return num_col, array1, array2
+    
+    return num_col
+    
+def read_array1(filename, length):
+    array1 = np.loadtxt(filename, delimiter=',', usecols=range(0, 2*length, 2), skiprows=1)
+    return array1
+    
+def read_array2(filename, length):
+    array2 = np.loadtxt(filename, delimiter=',', usecols=range(1, 2*length, 2), skiprows=1)
+    return array2

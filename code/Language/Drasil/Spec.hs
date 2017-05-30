c7a680c929d65d3eb2d1cc9d142bf5188e10aa3b
Dan Szymczak <szymczdm@mcmaster.ca>
2017-05-30 14:06:33 -0400

    Embed Expr in Sentence

--- code/Language/Drasil/Spec.hs
+++ code/Language/Drasil/Spec.hs
@@ -4,6 +4,7 @@ module Language.Drasil.Spec where
 
 import Language.Drasil.Unicode (Greek,Special)
 import Language.Drasil.Symbol
+import Language.Drasil.Expr
 
 -- | For writing accented characters
 data Accent = Grave | Acute deriving Eq
@@ -31,6 +32,7 @@ data Sentence where
   -- Direct concatenation of two Specs (no implicit spaces!)
   (:+:) :: Sentence -> Sentence -> Sentence   
   EmptyS :: Sentence
+  E :: Expr -> Sentence
 
 --Moving this here to avoid cyclic imports
 -- | Language of unit equations, to define a unit relative

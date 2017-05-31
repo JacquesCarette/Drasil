edc4b957f09af22b7676820da9fe8858afca8dd9
AKM11 <moria@mcmaster.ca>
2017-05-31 10:34:25 -0400

    Implemented more auto-generated paragraphs.

--- code/Example/Drasil/NoPCM/Body.hs
+++ code/Example/Drasil/NoPCM/Body.hs
@@ -24,11 +24,10 @@ import Drasil.OrganizationOfSRS
 this_si :: [UnitDefn]
 this_si = map UU [metre, kilogram, second] ++ map UU [centigrade, joule, watt]
 
-s2, s2_3, s3, s3_1, s4, s4_1, s4_1_1, s4_1_2, s4_1_3, s4_2, {-s4_2_1, s4_2_2, s4_2_3, s4_2_4, s4_2_5, s4_2_6, -}s6 :: Section
+s2, s2_3, s3, s3_1, s4, s4_1, s4_1_1, s4_1_2, s4_1_3, s4_2, s6 :: Section
 
-s2_3_intro, s3_1_intro, sys_context_fig,
-  s4_1_intro, s4_1_1_intro, s4_1_1_bullets, s4_1_2_intro, s4_1_2_list, s4_1_3_intro,
-  s4_1_3_list, s4_2_intro, {-s4_2_1_intro, -}fig_tank, s4_2_3_intro, s4_2_4_intro, s4_2_5_intro, s4_2_6_table1, s4_2_6_table2:: Contents
+s3_1_intro, sys_context_fig, s4_1_intro, s4_1_1_bullets, s4_1_2_list, s4_1_3_intro,
+  s4_1_3_list, fig_tank, s4_2_3_intro, s4_2_4_intro, s4_2_5_intro, s4_2_6_table1, s4_2_6_table2:: Contents
 
 mkSRS :: DocDesc
 mkSRS = RefSec (RefProg intro [TUnits, tsymb [TSPurpose, SymbConvention [Lit (nw ht_trans), Doc' (nw sWHS)], SymbOrder], TAandA]) : 
@@ -46,17 +45,11 @@ nopcmSymbMap = symbolMap pcmSymbols
 
 s2 = SRS.intro [] [s2_3]
 
-s2_3 = SRS.charOfIR [s2_3_intro] []
-
-s2_3_intro = Paragraph $
-            (at_start' $ reviewer ^. term) +:+ S "of this" +:+ (phrase $ documentation ^. term) +:+
-            S "should have a strong knowledge in" +:+ (phrase $ heat ^. term) +:+ S "transfer" +:+. (phrase $ theory ^. term) +:+
-           S "A third or fourth year Mechanical Engineering course on the topic is recommended. The" +:+
-            (phrase $ reviewer ^. term) +:+ S "should also have an understanding of differential" +:+ (plural $ equation ^. term) `sC` S "as typically" +:+
-           S "covered in first and second year Calculus courses. The" +:+ (plural $ user ^. term) +:+ S "of" +:+ (getAcc sWHS) +:+
-           S "can have a lower level expertise, as explained in" +:+ (titleize $ section_ ^. term)
-           -- FIXME: Section 3.2 does not exist yet, when it does, add reference
-
+s2_3 = charIntRdrF knowledge understanding (getAcc sWHS) fig_tank --FIXME: referencing this for now until we figure out how to reference auto-generated section (section 3.2)
+  where knowledge = ((phrase $ heat ^. term) +:+ S "transfer" +:+. (phrase $ theory ^. term) +:+
+                    S "A third or fourth year Mechanical Engineering course on the topic is recommended")
+        understanding = (S "differential" +:+ (plural $ equation ^. term) `sC` S "as typically" +:+
+                        S "covered in first and second year Calculus courses")
            
 s3 = genSysF [s3_1] (Paragraph $ EmptyS) Nothing []
 
@@ -84,23 +77,13 @@ s4_1_intro = Paragraph $
             (getAcc sWHS) +:+ S "is a computer" +:+ (phrase $ program ^. term) +:+ S "developed to investigate" +:+
            S "the heating of" +:+ (phrase $ water ^. term) +:+ S "in a" +:+. (phrase $ sWHT ^. term)
 
-s4_1_1 = SRS.termAndDefn [s4_1_1_intro, s4_1_1_bullets] []
-  
-s4_1_1_intro = Paragraph $
-           S "This subsection provides a list of terms that" +:+
-           S "are used in subsequent" +:+ (plural $ section_ ^. term) +:+ S "and their meaning, with the" +:+
-            (phrase $ purpose ^. term) +:+ S "of reducing ambiguity and making it easier to correctly" +:+
-           S "understand the" +: (plural $ requirement ^. term)
+s4_1_1 = termDefnF Nothing [s4_1_1_bullets]
   
 s4_1_1_bullets = Enumeration $ (Bullet $ map (\c -> Flat $ 
-  ((at_start $ c ^. term)) :+: S ":" +:+ (c ^. defn)) 
-  [thermal_flux, heat_cap_spec])
+          ((at_start $ c ^. term)) :+: S ":" +:+ (c ^. defn)) 
+          [thermal_flux, heat_cap_spec])
   
-s4_1_2 = SRS.physSyst [s4_1_2_intro, s4_1_2_list, fig_tank] []
-
-s4_1_2_intro = Paragraph $
-           S "The" +:+ (phrase $ physicalSystem ^. term) +:+ S "of" +:+ (getAcc sWHS) `sC`
-           S "as shown in" +:+ (makeRef fig_tank) `sC` S "includes the following" +: (plural $ element ^. term)
+s4_1_2 = physSystDesc (getAcc sWHS) fig_tank [s4_1_2_list, fig_tank]
 
 fig_tank = Figure ((at_start $ sWHT ^. term) `sC` S "with" +:+ (phrase $ ht_flux ^. term) +:+ S "from" +:+ (phrase $ coil ^. term) +:+ S "of" +:+
             P (ht_flux_C ^. symbol)) "TankWaterOnly.png"
@@ -115,7 +98,7 @@ s4_1_3 = SRS.goalStmt [s4_1_3_intro, s4_1_3_list] []
 
 s4_1_3_intro = Paragraph $
            S "Given the" +:+ (phrase $ temp ^. term) +:+ S "of the" +:+ (phrase $ coil ^. term) `sC` S "initial" +:+
-            (phrase $ temp ^. term) +:+ S "of the" +:+ (phrase $ water ^. term) :+: S "," +:+
+            (phrase $ temp ^. term) +:+ S "of the" +:+ (phrase $ water ^. term) `sC`
            S "and material" +:+ (plural $ property ^. term) `sC` S "the goal statement is"
 
 s4_1_3_list = Enumeration $ Simple $ map (\(a,b) -> (a, Flat b)) [
@@ -123,48 +106,15 @@ s4_1_3_list = Enumeration $ Simple $ map (\(a,b) -> (a, Flat b)) [
 
 s4_2 = solChSpecF sWHS (s4_1, s6) True EmptyS (((makeRef s4_2_6_table1) +:+ S "and" +:+ (makeRef s4_2_6_table2) +:+ S "show"), EmptyS, False, EmptyS)
           ([], s4_2_2_TMods, [s4_2_3_intro], [s4_2_4_intro], [s4_2_5_intro], [s4_2_6_table1, s4_2_6_table2]) []
---s4_2 = SRS.solCharSpec [s4_2_intro] [s4_2_1, s4_2_2, s4_2_3, s4_2_4, s4_2_5, s4_2_6]
-
-s4_2_intro = Paragraph $
-           S "The" +:+ (phrase $ inModel ^. term) +:+ sParen (getAcc ode) +:+
-           S "that governs" +:+ (getAcc sWHS) +:+. S "is presented in " +:+ --TODO: Subsec reference
-           S "The" +:+ (phrase $ information ^. term) +:+
-           S "to understand the meaning of the" +:+ (phrase $ inModel ^. term) +:+ 
-           S "and its derivation is also" +:+ S "presented, so that the" +:+ 
-            (phrase $ inModel ^. term) +:+. S "can be verified"
-  
---s4_2_1 = SRS.assump [s4_2_1_intro] []
-
-{-s4_2_1_intro = Paragraph $
-           S "This" +:+ (phrase $ section_ ^. term) +:+
-           S "simplifies the original" +:+ (phrase $ problem ^. term) +:+
-           S "and helps in developing the" +:+ (phrase $ thModel ^. term) +:+
-           S "by filling in the missing" +:+ (phrase $ information ^. term) +:+
-           S "for the" +:+. (phrase $ physicalSystem ^. term) +:+ S "The" +:+ (plural $ number ^. term)+:+
-           S "given in the square brackets refer to the" +:+ foldr1 (:+:) (intersperse (S ", ") 
-            (map (\ch -> (phrase $ ch ^. term) +:+ sSqBr (getAcc ch)) [thModel, genDefn, dataDefn, inModel]))
-            `sC` S "or" +:+ phrase likelyChg +:+ sSqBr (getAcc likelyChg) `sC`
-           S "in which the respective" +:+ (phrase assumption) +:+. S "is used"-}
---TODO: Simple List
-
---s4_2_2 = thModF sWHS s4_2_2_TMods
   
 s4_2_2_TMods :: [Contents]
 s4_2_2_TMods = map (Definition nopcmSymbMap . Theory) [t1consThermE]
 
---s4_2_3 = genDefnF []
-
 s4_2_3_intro = Paragraph $ EmptyS
-
---s4_2_4 = dataDefnF EmptyS []
 
 s4_2_4_intro = Paragraph $ EmptyS
 
---s4_2_5 = inModelF s4_1 s4_2_4 s4_2_2 s4_2_3 []
-
 s4_2_5_intro = Paragraph $ EmptyS
-
---s4_2_6 = datConF ((makeRef s4_2_6_table1) +:+ S "and" +:+ (makeRef s4_2_6_table2) +:+ S "show") EmptyS False EmptyS [s4_2_6_table1, s4_2_6_table2]
 
 s4_2_6_table1 = Table [S "Var", titleize' physicalConstraint, S "Typical Value"]
   (mkTable [(\x -> x!!0), (\x -> x!!1), (\x -> x!!2)] $ map (listConstS) []) 

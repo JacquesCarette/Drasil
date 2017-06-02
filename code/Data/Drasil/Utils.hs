78cca69414b7a99c1419b1eaad89fde557df164e
AKM11 <moria@mcmaster.ca>
2017-06-02 16:58:04 -0400

    Last thing I hope.

--- code/Data/Drasil/Utils.hs
+++ code/Data/Drasil/Utils.hs
@@ -1,9 +1,6 @@
 module Data.Drasil.Utils
   ( foldle
   , foldle1
-  , foldlSent
-  , foldlList
-  , foldlsC
   , mkEnumAbbrevList
   , listConstS
   , listConstExpr
@@ -17,22 +14,20 @@ module Data.Drasil.Utils
   , enumBullet
   , mkRefsList
   , mkInputDatTb
-  , ofThe, ofThe'
   , getS
   , weave
   , fmtU
   , unwrap
-  , isThe
   , fmtBF
   , symbolMapFun
   ) where
 
 import Data.List
 import Control.Lens ((^.))
-import Language.Drasil (Sentence(Sy, P, EmptyS, S, (:+:), E), (+:+), (+:+.), 
-  ItemType(Flat), sC, sParen, sSqBr, Contents(Definition, Enumeration), 
+import Language.Drasil (Sentence(Sy, P, EmptyS, S, (:+:), E), (+:+),
+  ItemType(Flat), sParen, sSqBr, Contents(Definition, Enumeration), 
   makeRef, DType, Section, ListType(Simple, Bullet), getUnit, Quantity,
-  symbol, SymbolForm, SymbolMap, symbolMap, UnitDefn, usymb, QDefinition, Chunk, Expr(..),
+  symbol, SymbolForm, SymbolMap, symbolMap, UnitDefn, usymb, Chunk, Expr(..),
   phrase, titleize, titleize', mkTable, term, Contents(Table))
 import Data.Drasil.Concepts.Documentation (description, input_, datum, symbol_)
 import Data.Drasil.Concepts.Math (unit_)
@@ -53,23 +48,6 @@ foldle1 _ _ []       = error "foldle1 cannot be used with empty list"
 foldle1 _ _ [x]      = x
 foldle1 _ g [x,y]    = g x y
 foldle1 f g (x:y:xs) = foldle f g (f x y) xs
-
--- | partial function application of foldle for sentences specifically
-foldlSent :: [Sentence] -> Sentence
-foldlSent = foldle (+:+) (+:+.) EmptyS
-
--- | creates a list of elements seperated by commas, ending in a "_, and _"
-foldlList :: [Sentence] -> Sentence
-foldlList []    = EmptyS
-foldlList [a,b] = a +:+ S "and" +:+ b
-foldlList lst   = foldle1 sC (\a b -> a `sC` S "and" +:+ b) lst
-
--- | creates a list of elements seperated by commas, including the last element
-foldlsC :: [Sentence] -> Sentence
-foldlsC []       = EmptyS
-foldlsC [x]      = x
-foldlsC [x,y]    = x `sC` y
-foldlsC (x:y:xs) = foldle sC sC (x `sC` y) xs
 
 -- | concantenates number to abbreviation
 -- should not be exported
@@ -181,41 +159,6 @@ enumSimple s t l = Enumeration $ Simple $ mkEnumAbbrevList s t l
 -- | interweaves two lists together [[a,b,c],[d,e,f]] -> [a,d,b,e,c,f]
 weave :: [[a]] -> [a]
 weave = (concat . transpose)
-
---combinator functions that are used by introF in OrganizationOfSRS
-sAnd :: Sentence -> Sentence -> Sentence
-sAnd p1 p2 = p1 +:+ S "and" +:+ p2
-
-andIts :: Sentence -> Sentence -> Sentence
-andIts p1 p2 = p1 +:+ S "and its" +:+ p2
-
-andThe :: Sentence -> Sentence -> Sentence
-andThe p1 p2 = p1 +:+ S "and the" +:+ p2
-
-sAre :: Sentence -> Sentence -> Sentence
-sAre p1 p2 = p1 +:+ S "are" +:+ p2
-
-sIn :: Sentence -> Sentence -> Sentence
-sIn p1 p2 = p1 +:+ S "in" +:+ p2
-
-sIs :: Sentence -> Sentence -> Sentence
-sIs p1 p2 = p1 +:+ S "is" +:+ p2
-
-isThe :: Sentence -> Sentence -> Sentence
-isThe p1 p2 = p1 +:+ S "is the" +:+ p2
-
-sOf :: Sentence -> Sentence -> Sentence
-sOf p1 p2 = p1 +:+ S "of" +:+ p2
-
-sOr :: Sentence -> Sentence -> Sentence
-sOr p1 p2 = p1 +:+ S "or" +:+ p2
-
-ofThe, ofThe' :: Sentence -> Sentence -> Sentence
-ofThe  p1 p2 = S "the" +:+ p1 +:+ S "of the" +:+ p2
-ofThe' p1 p2 = S "The" +:+ p1 +:+ S "of the" +:+ p2
-
-toThe :: Sentence -> Sentence -> Sentence
-toThe p1 p2 = p1 +:+ S "to the" +:+ p2
 
 unwrap :: (Maybe UnitDefn) -> Sentence
 unwrap (Just a) = Sy (a ^. usymb)

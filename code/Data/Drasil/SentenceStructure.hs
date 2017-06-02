fcf06db8213157b4106f9d0568fa103b93eeb677
njericha <njericha@edu.uwaterloo.ca>
2017-06-02 15:17:42 -0400

    Created Data.Drasil.SentenceStructure and moved appropriate functions from Utils.hs to SentenceStructure.hs

---
+++ code/Data/Drasil/SentenceStructure.hs
@@ -0,0 +1,62 @@
+module Data.Drasil.SentenceStructure 
+  ( foldlSent, foldlsC, foldlList
+  , sAnd, andIts, andThe, sAre, sIn
+  , sIs, isThe, sOf, sOr, ofThe, ofThe'
+  , toThe
+  ) where
+
+import Language.Drasil
+import Data.Drasil.Utils (foldle, foldle1)
+
+{--** Sentence Folding **--}
+-- | partial function application of foldle for sentences specifically
+foldlSent :: [Sentence] -> Sentence
+foldlSent = foldle (+:+) (+:+.) EmptyS
+
+-- | creates a list of elements seperated by commas, including the last element
+foldlsC :: [Sentence] -> Sentence
+foldlsC []       = EmptyS
+foldlsC [x]      = x
+foldlsC [x,y]    = x `sC` y
+foldlsC (x:y:xs) = foldle sC sC (x `sC` y) xs
+
+-- | creates a list of elements seperated by commas, ending in a "_, and _"
+foldlList :: [Sentence] -> Sentence
+foldlList []    = EmptyS
+foldlList [a,b] = a +:+ S "and" +:+ b
+foldlList lst   = foldle1 sC (\a b -> a `sC` S "and" +:+ b) lst
+
+
+{--** Combinators **--}
+sAnd, andIts :: Sentence -> Sentence -> Sentence
+sAnd p1 p2 = p1 +:+ S "and" +:+ p2
+
+andIts p1 p2 = p1 +:+ S "and its" +:+ p2
+
+andThe :: Sentence -> Sentence -> Sentence
+andThe p1 p2 = p1 +:+ S "and the" +:+ p2
+
+sAre :: Sentence -> Sentence -> Sentence
+sAre p1 p2 = p1 +:+ S "are" +:+ p2
+
+sIn :: Sentence -> Sentence -> Sentence
+sIn p1 p2 = p1 +:+ S "in" +:+ p2
+
+sIs :: Sentence -> Sentence -> Sentence
+sIs p1 p2 = p1 +:+ S "is" +:+ p2
+
+isThe :: Sentence -> Sentence -> Sentence
+isThe p1 p2 = p1 +:+ S "is the" +:+ p2
+
+sOf :: Sentence -> Sentence -> Sentence
+sOf p1 p2 = p1 +:+ S "of" +:+ p2
+
+sOr :: Sentence -> Sentence -> Sentence
+sOr p1 p2 = p1 +:+ S "or" +:+ p2
+
+ofThe, ofThe' :: Sentence -> Sentence -> Sentence
+ofThe  p1 p2 = S "the" +:+ p1 +:+ S "of the" +:+ p2
+ofThe' p1 p2 = S "The" +:+ p1 +:+ S "of the" +:+ p2
+
+toThe :: Sentence -> Sentence -> Sentence
+toThe p1 p2 = p1 +:+ S "to the" +:+ p2

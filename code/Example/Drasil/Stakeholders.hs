0e74d26c0da4099ddf2f9dbc2c6f8292a7468b80
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-05 11:33:23 -0400

    reverted Stakeholders.hs to original file; fixed errors stemming from this file

--- code/Example/Drasil/Stakeholders.hs
+++ code/Example/Drasil/Stakeholders.hs
@@ -5,7 +5,6 @@ import Language.Drasil
 import qualified Drasil.SRS as SRS
 import Data.Drasil.SentenceStructures (foldlSent)
 import Data.Drasil.Concepts.Documentation
-import Control.Lens ((^.))
 
 stakehldrGeneral :: CI -> Sentence -> Section
 stakehldrGeneral kWord clientDetails = (SRS.stakeholder) [stakeholderIntro] subs
@@ -13,22 +12,22 @@ stakehldrGeneral kWord clientDetails = (SRS.stakeholder) [stakeholderIntro] subs
 
 -- general stakeholders introduction
 stakeholderIntro :: Contents
-stakeholderIntro = Paragraph $ foldlSent [S "This", (phrase $ section_ ^. term),
-            S "describes the" +: (titleize' $ stakeholder ^. term), S "the people who have an",
-            (phrase $ interest ^. term), S "in the product"{-, (phrase $ the product_)-}]
+stakeholderIntro = Paragraph $ foldlSent [S "This", (phrase section_),
+            S "describes the" +: (titleize' stakeholder), S "the people who have an",
+            (phrase interest), S "in", (phrase $ the product_)]
 
 tClientF :: CI -> Sentence ->  Section
 tClientF kWord details = SRS.theClient [clientIntro kWord details] []
 
 clientIntro :: CI -> Sentence -> Contents
-clientIntro kWord  details = Paragraph $ foldlSent [--(at_start $ the client),
-  S "The client for", (short kWord), S "is a", (phrase $ company ^. term), S "named" +:+. details,
-  {-(at_start $ the client),-} S "The client has the final say on acceptance of the", 
-  (phrase $ product_ ^. term)]
+clientIntro kWord  details = Paragraph $ foldlSent [(at_start $ the client),
+  S "for", (short kWord), S "is a", (phrase company), S "named" +:+. details,
+  (at_start $ the client), S "has the final say on acceptance of the", 
+  (phrase product_)]
 
 tCustomerF :: CI -> Section
 tCustomerF kWord = SRS.theCustomer [customerIntro kWord] []
 
 customerIntro :: CI -> Contents
-customerIntro kWord = Paragraph $ foldlSent [--(at_start' $ the customer), 
-  S "The customers are the", (phrase $ endUser ^. term), S "of", (short kWord)]
+customerIntro kWord = Paragraph $ foldlSent [(at_start' $ the customer), 
+  S "are the", (phrase endUser), S "of", (short kWord)]

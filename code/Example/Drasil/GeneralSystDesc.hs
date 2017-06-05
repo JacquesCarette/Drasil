5c13f8359688361d2b58f04889dfa85fd8713915
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-05 11:40:59 -0400

    removed more instances of 'fterm' and '^. term' causing errors

--- code/Example/Drasil/GeneralSystDesc.hs
+++ code/Example/Drasil/GeneralSystDesc.hs
@@ -14,14 +14,14 @@ genSysF sCntxt userIntro constraints systSubSec = SRS.genSysDes [genSysIntro]
 --generalized general system description introduction
 genSysIntro :: Contents
 genSysIntro = Paragraph $ foldlSent
-              [S "This", fterm phrase section_, S "provides general",
-              fterm phrase information, S "about the", fterm phrase system `sC` S "identifies",
-              S "the interfaces between the", fterm phrase system, S "and its", fterm phrase environment `sC`
-              S "and describes the", fterm plural userCharacteristic, S "and the", fterm plural systemConstraint]
+              [S "This", phrase section_, S "provides general",
+              phrase information, S "about the", phrase system `sC` S "identifies",
+              S "the interfaces between the", phrase system, S "and its", phrase environment `sC`
+              S "and describes the", plural userCharacteristic, S "and the", plural systemConstraint]
 
 -- System Constraints
 -- generalized if no constraints, but if there are, they can be passed through
 systCon :: [Contents] -> [Section] -> Section
 systCon [] subSec  = SRS.sysCon [systCon_none] subSec
-            where systCon_none = Paragraph (S "There are no" +:+. fterm plural systemConstraint)
+            where systCon_none = Paragraph (S "There are no" +:+. plural systemConstraint)
 systCon a subSec = SRS.sysCon a subSec  

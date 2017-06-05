066ba3d0c0670004a9cfe0d080300e2c4bf08bb0
AKM11 <moria@mcmaster.ca>
2017-06-05 11:35:59 -0400

    Removed redundant commands.

--- code/Example/Drasil/SpecificSystemDescription.hs
+++ code/Example/Drasil/SpecificSystemDescription.hs
@@ -6,7 +6,7 @@ import Control.Lens ((^.))
 
 -- | Specific System description section builder. Takes the system and subsections.
 specSysDescr :: NP -> [Section] -> Section
-specSysDescr sys subs = section (D.fterm titleize D.specificsystemdescription) [intro sys] subs
+specSysDescr sys subs = section (titleize D.specificsystemdescription) [intro sys] subs
 
 -- FIXME: this all should be broken down and mostly generated.
 -- Generates an introduction based on the system.
@@ -15,4 +15,4 @@ intro sys = Paragraph $ S "This section first presents the problem" +:+
   S "description, which gives a high-level view of the problem to be" +:+
   S "solved. This is followed by the solution characteristics" +:+
   S "specification, which presents the assumptions" `sC`
-  S "theories, and definitions that are used for the" +:+. (phrase $ sys ^. term)
+  S "theories, and definitions that are used for the" +:+. (phrase sys)

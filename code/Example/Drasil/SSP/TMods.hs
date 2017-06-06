443b713c7b7c87c25a9ffd7b941fe9075566b477
njericha <njericha@edu.uwaterloo.ca>
2017-06-06 16:24:21 -0400

    Added more place holder Theoretical Models

--- code/Example/Drasil/SSP/TMods.hs
+++ code/Example/Drasil/SSP/TMods.hs
@@ -13,7 +13,7 @@ import Data.Drasil.Quantities.SolidMechanics
 --------------------------
 
 sspTMods :: [RelationConcept]
-sspTMods = [fs_rc, equilibrium, mcShrStrgth, effStress]
+sspTMods = [fs_rc, equilibrium, mcShrStrgth, effStress, normForcEq, bsShrFEq]
 
 fixmeS :: Sentence
 fixmeS = S "FIXME: add description"
@@ -69,11 +69,29 @@ effS_desc :: Sentence
 effS_desc = fixmeS
 
 --
+normForcEq :: RelationConcept
+normForcEq = makeRC "normForcEq" (nounPhraseSP "normal force equilibrium") nmFEq_desc nmFEq_rel
+
+nmFEq_rel :: Relation
+nmFEq_rel = (C ni) := (Int 0) --FIXME: add the long equation
+
+nmFEq_desc :: Sentence
+nmFEq_desc = fixmeS
+
+--
+bsShrFEq :: RelationConcept
+bsShrFEq = makeRC "bsShrFEq" (nounPhraseSP "base shear force equilibrium") bShFEq_desc bShFEq_rel
+
+bShFEq_rel :: Relation
+bShFEq_rel = (C si) := (Int 0) --FIXME: add the long equation
+
+bShFEq_desc :: Sentence
+bShFEq_desc = fixmeS
 
 
 {-
 name :: RelationConcept
-name + makeRC "" (nounPhraseSP "") mcSS_desc mcSS_rel
+name = makeRC "" (nounPhraseSP "") mcSS_desc mcSS_rel
 
 mcSS_rel :: Relation
 mcSS_rel = 

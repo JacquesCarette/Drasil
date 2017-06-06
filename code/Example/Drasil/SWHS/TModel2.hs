9ee33f9355256a929d488fd2cdd0c2ffc08d4836
Daniel Scime <danscime@gmail.com>
2017-06-06 16:25:51 -0400

    Removed some redundancies in theoretical models.

--- code/Example/Drasil/SWHS/TModel2.hs
+++ code/Example/Drasil/SWHS/TModel2.hs
@@ -9,6 +9,7 @@ import Data.Drasil.SI_Units
 import Data.Drasil.Concepts.Thermodynamics
 import qualified Data.Drasil.Quantities.Thermodynamics as QT
 import Data.Drasil.Quantities.PhysicalProperties
+import Data.Drasil.Quantities.Physics (energy)
 import Drasil.SWHS.DataDefs
 
 import Control.Lens ((^.))
@@ -33,30 +34,26 @@ sensHtEEqn = (C QT.sens_heat) := Case [((C htCap_S) * (C mass) * (C deltaT),
 -- were implemented incorrectly.
 t2descr :: Sentence
 t2descr = (P (QT.sens_heat ^. symbol) +:+ S "is the change in" +:+
-  phrase sens_heat +:+ S "energy (" :+:
-  Sy (joule ^. usymb) :+: S ")." +:+ P (htCap_S ^. symbol) :+: 
-  S "," +:+ P (htCap_L ^. symbol) :+: S "," +:+ 
-  P (htCap_V ^. symbol) +:+ S "are the" +:+ phrase htCap_S :+: 
-  S "," +:+ phrase htCap_L :+: S ", and" +:+ 
-  phrase htCap_V :+: S ", respectively (" :+: Sy (unit_symb QT.heat_cap_spec) :+:
-  S ")." +:+ P (mass ^. symbol) +:+ S "is the" +:+
-  phrase mass +:+ S "(" :+: Sy (unit_symb mass) :+: S ")." +:+
-  P (QT.temp ^. symbol) +:+ S "is the" +:+ phrase temp +:+ S "(" :+:
-  Sy (unit_symb QT.temp) :+: S "), and" +:+ P (deltaT ^. symbol) +:+
-  S "is the" +:+ phrase deltaT +:+ S "(" :+:
-  Sy (unit_symb deltaT) :+: S ")." +:+ P (QT.melt_pt ^. symbol) +:+ 
-  S "and" +:+ P (QT.boil_pt ^. symbol) +:+ S "are the" +:+ 
-  phrase QT.melt_pt +:+ S "and" +:+ phrase QT.boil_pt :+:
-  S ", respectively (" :+: Sy (unit_symb QT.temp) :+: S ")." +:+ 
+  phrase sens_heat +:+ phrase energy +:+. sParen (Sy (joule ^. usymb)) +:+
+  P (htCap_S ^. symbol) `sC` P (htCap_L ^. symbol) `sC`
+  P (htCap_V ^. symbol) +:+ S "are the" +:+ phrase htCap_S `sC`
+  phrase htCap_L `sC` S "and" +:+ phrase htCap_V `sC` S "respectively" +:+.
+  sParen (Sy (unit_symb QT.heat_cap_spec)) +:+ P (mass ^. symbol) +:+
+  S "is the" +:+ phrase mass +:+. sParen (Sy (unit_symb mass)) +:+
+  P (QT.temp ^. symbol) +:+ S "is the" +:+ phrase temp +:+
+  sParen (Sy (unit_symb QT.temp)) `sC` S "and" +:+ P (deltaT ^. symbol) +:+
+  S "is the" +:+ phrase deltaT +:+. sParen (Sy (unit_symb deltaT)) +:+
+  P (QT.melt_pt ^. symbol) +:+ S "and" +:+ P (QT.boil_pt ^. symbol) +:+
+  S "are the" +:+ phrase QT.melt_pt +:+ S "and" +:+ phrase QT.boil_pt `sC`
+  S "respectively" +:+. sParen (Sy (unit_symb QT.temp)) +:+ 
   at_start sens_heat :+: S "ing occurs as long as the material does" +:+ 
   S "not reach a" +:+ phrase temp +:+ S "where a" +:+ 
   phrase phase_change +:+ S "occurs. A" +:+
   phrase phase_change +:+ S "occurs if" +:+
   P (QT.temp ^. symbol) :+: S "=" :+: P (QT.boil_pt ^. symbol) +:+
-  S "or" +:+ P (QT.temp ^. symbol) :+: S "=" :+: 
-  P (QT.melt_pt ^. symbol) :+: S ". If this is the case, refer to" +:+ 
-  swhsSymbMapTRef t3LatHtE :+: S "," +:+ at_start latent_heat +:+. 
-  S "energy")
+  S "or" +:+ P (QT.temp ^. symbol) :+: S "=" +:+. 
+  P (QT.melt_pt ^. symbol) +:+ S "If this is the case, refer to" +:+ 
+  swhsSymbMapTRef t3LatHtE `sC` at_start latent_heat +:+. phrase energy)
   
 
 --How to have new lines in the description? 
--- code/Example/Drasil/SWHS/TModel3.hs
+++ code/Example/Drasil/SWHS/TModel3.hs
@@ -8,7 +8,7 @@ import Language.Drasil
 import Data.Drasil.SI_Units
 import Data.Drasil.Concepts.Thermodynamics
 import qualified Data.Drasil.Quantities.Thermodynamics as QT
-import Data.Drasil.Quantities.Physics (time)
+import Data.Drasil.Quantities.Physics (time, energy)
 import Data.Drasil.Concepts.Math (rOfChng)
 
 import Control.Lens ((^.))
@@ -29,15 +29,15 @@ latHtEEqn = FCall (C QT.latent_heat) [C time] := UnaryOp (Integral (Just (Low 0)
 
 t3descr :: Sentence
 t3descr = (P (QT.latent_heat ^. symbol) +:+ S "is the change in" +:+
-  phrase thermal_energy +:+ S "(" :+:
-  Sy (joule ^. usymb) :+: S ")," +:+
-  phrase latent_heat +:+ S "energy." +:+
+  phrase thermal_energy +:+ sParen (Sy (joule ^. usymb)) `sC`
+  phrase latent_heat +:+. phrase energy +:+
   S "FIXME: THE INTEGRAL FROM THE ABOVE EQUATION SHOULD GO HERE" +:+ 
-  S "is the" +:+ phrase rOfChng +:+ S "of" +:+ P (QT.latent_heat ^. symbol) +:+ S "with respect" +:+
-  S "to" +:+ phrase time :+: S "" +:+ P (tau ^. symbol) +:+ 
-  S "(" :+: Sy (unit_symb tau) :+: S ")." +:+ P (time ^. symbol) +:+
-  S "is the" +:+ phrase time +:+ S "(" :+: Sy (unit_symb time) :+:
-  S ") elapsed, as long as the" +:+
+  S "is the" +:+ phrase rOfChng +:+ S "of" +:+
+  P (QT.latent_heat ^. symbol) +:+ S "with respect" +:+
+  S "to" +:+ phrase time :+: EmptyS +:+ P (tau ^. symbol) +:+.
+  sParen (Sy (unit_symb tau))+:+ P (time ^. symbol) +:+
+  S "is the" +:+ phrase time +:+ sParen(Sy (unit_symb time)) +:+ 
+  S "elapsed, as long as the" +:+
   phrase phase_change +:+ S "is not complete. The status of" +:+
   S "the" +:+ phrase phase_change +:+
   S "depends on the" +:+ phrase melt_frac `sC`
@@ -45,8 +45,8 @@ t3descr = (P (QT.latent_heat ^. symbol) +:+ S "is the change in" +:+
   P (QT.melt_pt ^. symbol) +:+
   S "and" +:+ P (QT.boil_pt ^. symbol) +:+ S "are the" +:+
   phrase QT.melt_pt +:+ S "and" +:+ phrase QT.boil_pt `sC`
-  S "respectively (" :+: Sy (unit_symb QT.temp) :+: S ")." +:+
-  at_start latent_heat :+: S "ing stops when all material has" +:+
-  S "changed to the new phase.")
+  S "respectively" +:+. sParen (Sy (unit_symb QT.temp)) +:+
+  at_start latent_heat :+: S "ing stops when all material has" +:+.
+  S "changed to the new phase")
   
 -- Wrong DD reference above, change when DD4 is available

4dd70fc6a6dfe249aad4dd75c9e0551081192822
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-06 11:52:42 -0400

    deleted file 'OrganizationOfSRS.hs'; removed instances from cabal file

--- code/Example/Drasil/OrganizationOfSRS.hs
+++
@@ -1,9 +0,0 @@
-module Drasil.OrganizationOfSRS where
-
-import Language.Drasil
-import Data.Drasil.Concepts.Documentation
-import Data.Drasil.Concepts.Math (equation)
-import Data.Drasil.Concepts.Software (program)
-import Data.Drasil.Utils (foldle)
-import Data.Drasil.SentenceStructures
-import qualified Drasil.SRS as SRS

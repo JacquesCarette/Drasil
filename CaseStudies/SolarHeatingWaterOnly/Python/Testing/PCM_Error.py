5b83b806fb8ea7b73e1074c3baa5f7f8d855e5e4
AKM11 <moria@mcmaster.ca>
2017-06-07 14:19:13 -0400

    Diagnosing failures in comparison tests.

--- CaseStudies/SolarHeatingWaterOnly/Python/Testing/PCM_Error.py
+++ CaseStudies/SolarHeatingWaterOnly/Python/Testing/PCM_Error.py
@@ -1,3 +1,4 @@
+#Commented sections pending removal
 import sys
 sys.path.insert(0, '.')
 
@@ -9,18 +10,18 @@ def PCM_ErrorF(Ffile, Pfile, comparator):
     linesFor = f.readlines()
     timeFor = []
     tempWFor = []
-    tempPFor = []
+    #tempPFor = []
     eWFor = []
-    ePFor = []
+    #ePFor = []
     tNoPCM = []
     eNoPCM = []
     for line in linesFor[23:]:
         values = line.split()
         timeFor += [float(values[0])]
         tempWFor += [float(values[2])]
-        tempPFor += [float(values[3])]
+        #tempPFor += [float(values[3])]
         eWFor += [float(values[5])]
-        ePFor += [float(values[4])]
+        #ePFor += [float(values[4])]
         tNoPCM += [float(values[8])]
         eNoPCM += [float(values[9])]
     f.close()
@@ -29,33 +30,33 @@ def PCM_ErrorF(Ffile, Pfile, comparator):
     linesPy = f.readlines()
     timePy = []
     tempWPy = []
-    tempPPy = []
+    #tempPPy = []
     eWPy = []
-    ePPy = []
+    #ePPy = []
     for line in linesPy[34:]:
         values = line.split()
         timePy += [float(values[0])]
         tempWPy += [float(values[1])]
-        tempPPy += [float(values[2])]
+        #tempPPy += [float(values[2])]
         eWPy += [float(values[3])]
-        ePPy += [float(values[4])]
+        #ePPy += [float(values[4])]
     f.close()
 
     if comparator is "TWat":
         error = errorCalcInterp(timeFor, timePy, tempWFor, tempWPy)
-    elif comparator is "TPCM":
-        error = errorCalcInterp(timeFor, timePy, tempPFor, tempPPy)
+    #elif comparator is "TPCM":
+        #error = errorCalcInterp(timeFor, timePy, tempPFor, tempPPy)
     elif comparator is "EWat":
         error = errorCalcInterp(timeFor, timePy, eWFor, eWPy)
-    elif comparator is "EPCM":
-        error = errorCalcInterp(timeFor, timePy, ePFor, ePPy)
+    #elif comparator is "EPCM":
+        #error = errorCalcInterp(timeFor, timePy, ePFor, ePPy)
     elif comparator is "TWatNoP":
         error = errorCalcInterp(timeFor, timePy, tNoPCM, tempWPy)
     elif comparator is "EWatNoP":
         error = errorCalcInterp(timeFor, timePy, eNoPCM, eWPy)
     else:
         error = 1
-
+    print error
     return error
 
 
@@ -64,42 +65,42 @@ def PCM_ErrorM(Mfile, Pfile, comparator):
     linesM = f.readlines()
     timeM = []
     tempWM = []
-    tempPM = []
+    #tempPM = []
     eWM = []
-    ePM = []
+    #ePM = []
     for line in linesM[36:]:
         values = line.split()
         timeM += [float(values[0])]
         tempWM += [float(values[1])]
-        tempPM += [float(values[2])]
+        #tempPM += [float(values[2])]
         eWM += [float(values[4])]
-        ePM += [float(values[3])]
+        #ePM += [float(values[3])]
     f.close()
 
     f = open(Pfile, 'r')
     linesPy = f.readlines()
     timePy = []
     tempWPy = []
-    tempPPy = []
+    #tempPPy = []
     eWPy = []
-    ePPy = []
+    #ePPy = []
     for line in linesPy[34:]:
         values = line.split()
         timePy += [float(values[0])]
         tempWPy += [float(values[1])]
-        tempPPy += [float(values[2])]
+        #tempPPy += [float(values[2])]
         eWPy += [float(values[3])]
-        ePPy += [float(values[4])]
+        #ePPy += [float(values[4])]
     f.close()
 
     if comparator is "TWat":
         error = errorCalcInterp(timeM, timePy, tempWM, tempWPy)
-    elif comparator is "TPCM":
-        error = errorCalcInterp(timeM, timePy, tempPM, tempPPy)
+    #elif comparator is "TPCM":
+        #error = errorCalcInterp(timeM, timePy, tempPM, tempPPy)
     elif comparator is "EWat":
         error = errorCalcInterp(timeM, timePy, eWM, eWPy)
-    elif comparator is "EPCM":
-        error = errorCalcInterp(timeM, timePy, ePM, ePPy)
+    #elif comparator is "EPCM":
+        #error = errorCalcInterp(timeM, timePy, ePM, ePPy)
     else:
         error = 1
 
@@ -111,42 +112,42 @@ def PCM_ErrorC(Cfile, Pfile, comparator):
     linesC = f.readlines()
     timeC = []
     tempWC = []
-    tempPC = []
+    #tempPC = []
     eWC = []
-    ePC = []
+    #ePC = []
     for line in linesC[34:]:
         values = line.split()
         timeC += [float(values[0])]
         tempWC += [float(values[1])]
-        tempPC += [float(values[2])]
+        #tempPC += [float(values[2])]
         eWC += [float(values[3])]
-        ePC += [float(values[4])]
+        #ePC += [float(values[4])]
     f.close()
 
     f = open(Pfile, 'r')
     linesPy = f.readlines()
     timePy = []
     tempWPy = []
-    tempPPy = []
+    #tempPPy = []
     eWPy = []
-    ePPy = []
+    #ePPy = []
     for line in linesPy[34:]:
         values = line.split()
         timePy += [float(values[0])]
         tempWPy += [float(values[1])]
-        tempPPy += [float(values[2])]
+        #tempPPy += [float(values[2])]
         eWPy += [float(values[3])]
-        ePPy += [float(values[4])]
+        #ePPy += [float(values[4])]
     f.close()
 
     if comparator is "TWat":
         error = errorCalcInterp(timeC, timePy, tempWC, tempWPy)
-    elif comparator is "TPCM":
-        error = errorCalcInterp(timeC, timePy, tempPC, tempPPy)
+    #elif comparator is "TPCM":
+        #error = errorCalcInterp(timeC, timePy, tempPC, tempPPy)
     elif comparator is "EWat":
         error = errorCalcInterp(timeC, timePy, eWC, eWPy)
-    elif comparator is "EPCM":
-        error = errorCalcInterp(timeC, timePy, ePC, ePPy)
+    #elif comparator is "EPCM":
+        #error = errorCalcInterp(timeC, timePy, ePC, ePPy)
     else:
         error = 1
 
--- CaseStudies/SolarHeatingWaterOnly/Python/Testing/compareFortranTest.py
+++ CaseStudies/SolarHeatingWaterOnly/Python/Testing/compareFortranTest.py
@@ -1,3 +1,4 @@
+#Commented sections pending removal
 import sys
 sys.path.insert(0, '.')
 
@@ -12,43 +13,43 @@ class TestCompareFortran(unittest.TestCase):
 
     def test_CF1(self):
         errTw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F01.out', 'P01.out', 'TWat')
-        errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F01.out', 'P01.out', 'TPCM')
+        #errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F01.out', 'P01.out', 'TPCM')
         errEw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F01.out', 'P01.out', 'EWat')
-        errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F01.out', 'P01.out', 'EPCM')
+        #errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F01.out', 'P01.out', 'EPCM')
         self.assertAlmostEqual(errTw, 0, places=None, msg='Water temperature', delta=self.delta)
-        self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
+        #self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
         self.assertAlmostEqual(errEw, 0, places=None, msg='Water energy', delta=self.delta)
-        self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
+        #self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
 
     def test_CF2(self):
         errTw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F02.out', 'P02.out', 'TWat')
-        errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F02.out', 'P02.out', 'TPCM')
+        #errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F02.out', 'P02.out', 'TPCM')
         errEw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F02.out', 'P02.out', 'EWat')
-        errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F02.out', 'P02.out', 'EPCM')
+        #errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F02.out', 'P02.out', 'EPCM')
         self.assertAlmostEqual(errTw, 0, places=None, msg='Water temperature', delta=self.delta)
-        self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
+        #self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
         self.assertAlmostEqual(errEw, 0, places=None, msg='Water energy', delta=self.delta)
-        self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
+        #self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
 
     def test_CF3(self):
         errTw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F03.out', 'P03.out', 'TWat')
-        errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F03.out', 'P03.out', 'TPCM')
+        #errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F03.out', 'P03.out', 'TPCM')
         errEw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F03.out', 'P03.out', 'EWat')
-        errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F03.out', 'P03.out', 'EPCM')
+        #errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F03.out', 'P03.out', 'EPCM')
         self.assertAlmostEqual(errTw, 0, places=None, msg='Water temperature', delta=self.delta)
-        self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
+        #self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
         self.assertAlmostEqual(errEw, 0, places=None, msg='Water energy', delta=self.delta)
-        self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
+        #self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
 
     def test_CF4(self):
         errTw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F04.out', 'P04.out', 'TWat')
-        errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F04.out', 'P04.out', 'TPCM')
+        #errTp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F04.out', 'P04.out', 'TPCM')
         errEw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F04.out', 'P04.out', 'EWat')
-        errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F04.out', 'P04.out', 'EPCM')
+        #errEp = PCM_Error.PCM_ErrorF('Testing/compareFortran/F04.out', 'P04.out', 'EPCM')
         self.assertAlmostEqual(errTw, 0, places=None, msg='Water temperature', delta=self.delta)
-        self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
+        #self.assertAlmostEqual(errTp, 0, places=None, msg='PCM temperature', delta=self.delta)
         self.assertAlmostEqual(errEw, 0, places=None, msg='Water energy', delta=self.delta)
-        self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
+        #self.assertAlmostEqual(errEp, 0, places=None, msg='PCM energy', delta=self.delta)
 
     def test_CF5(self):
         errTw = PCM_Error.PCM_ErrorF('Testing/compareFortran/F01.out', 'P05.out', 'TWatNoP')

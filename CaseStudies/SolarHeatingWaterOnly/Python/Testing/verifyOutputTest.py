5d19fb33f282b8c2b65c23f86b416f3acc45db1b
AKM11 <moria@mcmaster.ca>
2017-06-07 10:16:13 -0400

    Cleared errors from functionality tests.

--- CaseStudies/SolarHeatingWaterOnly/Python/Testing/verifyOutputTest.py
+++ CaseStudies/SolarHeatingWaterOnly/Python/Testing/verifyOutputTest.py
@@ -23,14 +23,15 @@ class TestVerifyOutput(unittest.TestCase):
             verify_output.verify_output(self.time, self.tempW, eW, self.params)#self.tempP, eW, eP, self.params)
             assert len(w) is 0
 
-    def test_VO2(self):
-        eW = [0, 1000, 2000, 25200]
-        #eP = [0, 1000, 2000, 3000]
-        with warnings.catch_warnings(record=True) as w:
-            verify_output.verify_output(self.time, self.tempW, eW, self.params)#self.tempP, eW, eP, self.params)
-            assert issubclass(w[0].category, UserWarning)
-            assert ('There is > ' + str(self.params.ConsTol) + '% relative error between the energy in the PCM output' +
-                    ' and the expected output based on the law of conservation of energy.\n') in str(w[0].message)
+    # def test_VO2(self):
+        # eW = [0, 1000, 2000, 25200]
+        # #eP = [0, 1000, 2000, 3000]
+        # with warnings.catch_warnings(record=True) as w:
+            # verify_output.verify_output(self.time, self.tempW, eW, self.params)#self.tempP, eW, eP, self.params)
+            # print w
+            # assert issubclass(w[0].category, UserWarning)
+            # assert ('There is > ' + str(self.params.ConsTol) + '% relative error between the energy in the PCM output' +
+                    # ' and the expected output based on the law of conservation of energy.\n') in str(w[0].message)
 
     def test_VO3(self):
         eW = [0, 1000, 2000, 3000]
@@ -41,17 +42,17 @@ class TestVerifyOutput(unittest.TestCase):
             assert ('There is > ' + str(self.params.ConsTol) + '% relative error between the energy in the water ' +
                     'output and the expected output based on the law of conservation of energy.\n') in str(w[0].message)
 
-    def test_VO4(self):
-        eW = [0, 1000, 2000, 3000]
-        #eP = [0, 1000, 2000, 3000]
-        with warnings.catch_warnings(record=True) as w:
-            verify_output.verify_output(self.time, self.tempW, eW, self.params)#self.tempP, eW, eP, self.params)
-            assert issubclass(w[0].category, UserWarning)
-            assert issubclass(w[1].category, UserWarning)
-            assert ('There is > ' + str(self.params.ConsTol) + '% relative error between the energy in the water ' +
-                    'output and the expected output based on the law of conservation of energy.\n') in str(w[0].message)
-            assert ('There is > ' + str(self.params.ConsTol) + '% relative error between the energy in the PCM output' +
-                    ' and the expected output based on the law of conservation of energy.\n') in str(w[1].message)
+    # def test_VO4(self):
+        # eW = [0, 1000, 2000, 3000]
+        # #eP = [0, 1000, 2000, 3000]
+        # with warnings.catch_warnings(record=True) as w:
+            # verify_output.verify_output(self.time, self.tempW, eW, self.params)#self.tempP, eW, eP, self.params)
+            # assert issubclass(w[0].category, UserWarning)
+            # assert issubclass(w[1].category, UserWarning)
+            # assert ('There is > ' + str(self.params.ConsTol) + '% relative error between the energy in the water ' +
+                    # 'output and the expected output based on the law of conservation of energy.\n') in str(w[0].message)
+            # assert ('There is > ' + str(self.params.ConsTol) + '% relative error between the energy in the PCM output' +
+                    # ' and the expected output based on the law of conservation of energy.\n') in str(w[1].message)
 
 
 class VerifyOutputSuite:

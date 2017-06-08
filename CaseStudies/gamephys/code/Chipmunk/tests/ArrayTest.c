d0c72205a02dced1b410e26a5e5a729a089a038f
Henry <hmadej@gmail.com>
2017-06-08 12:35:11 -0400

    added more unit tests

--- CaseStudies/gamephys/code/Chipmunk/tests/ArrayTest.c
+++ CaseStudies/gamephys/code/Chipmunk/tests/ArrayTest.c
@@ -89,6 +89,21 @@ void test_push_n_diff() {
   TEST_ASSERT_TRUE(&test_val2==test_array->arr[1]);
   TEST_ASSERT_TRUE(&test_val2==test_array->arr[2]);
   TEST_ASSERT_TRUE(&test_val1==test_array->arr[3]);
+  TEST_ASSERT_TRUE(4==test_array->num); // size check
+}
+
+/**
+ * test size of array is 4 after pushing 4 object ptr
+ */
+void test_push_size_check() {
+  Array * test_array = arrayNew(4);
+  int test_val1 = 196613;
+  char test_val2 = 'a';
+  arrayPush(test_array, &test_val1);
+  arrayPush(test_array, &test_val2);
+  arrayPush(test_array, &test_val2);
+  arrayPush(test_array, &test_val1);
+  TEST_ASSERT_TRUE(4==test_array->num); // size check
 }
 
 /**
@@ -101,6 +116,9 @@ void test_pop() {
   arrayPop(test_array);
 }
 
+/**
+ * push pop should result in push ptr not being in first location for array
+ */
 void test_push_pop() {
   Array * test_array = arrayNew(0);
   int test_val = 196613;
@@ -108,6 +126,95 @@ void test_push_pop() {
   arrayPop(test_array);
   TEST_ASSERT_TRUE(&test_val!=test_array->arr[0]);
 }
+
+/**
+ * push pop multiple times result should be the same as push pop a single time
+ */
+void test_push_pop_n() {
+  Array * test_array = arrayNew(0);
+  int test_val = 196613;
+  arrayPush(test_array, &test_val);
+  arrayPush(test_array, &test_val);
+  arrayPush(test_array, &test_val);
+  arrayPush(test_array, &test_val);
+  arrayPop(test_array);
+  arrayPop(test_array);
+  arrayPop(test_array);
+  arrayPop(test_array);
+  TEST_ASSERT_TRUE(&test_val!=test_array->arr[0]);
+  TEST_ASSERT_TRUE(&test_val!=test_array->arr[1]);
+  TEST_ASSERT_TRUE(&test_val!=test_array->arr[2]);
+  TEST_ASSERT_TRUE(&test_val!=test_array->arr[3]);
+}
+
+
+/**
+ * test contains with single ptr in array
+ */
+void test_containsObj() {
+  Array * test_array = arrayNew(0);
+  int test_val = 196613;
+  arrayPush(test_array, &test_val);
+  TEST_ASSERT_TRUE(arrayContains(test_array, &test_val));
+}
+
+/**
+ * test contains with multiple different ptr
+ */
+void test_containsObj_n() {
+  Array * test_array = arrayNew(0);
+  int test_val = 196613;
+  int test_val1 = 196614;
+  int test_val2 = 196615;
+  int test_val3 = 196616;
+  arrayPush(test_array, &test_val);
+  arrayPush(test_array, &test_val1);
+  arrayPush(test_array, &test_val2);
+  arrayPush(test_array, &test_val3);
+  TEST_ASSERT_TRUE(arrayContains(test_array, &test_val2));
+}
+
+/**
+ * test deleteObject with only object in array
+ */
+void test_deleteObject() {
+  Array * test_array = arrayNew(0);
+  int test_val = 196613;
+  arrayPush(test_array, &test_val);
+  arrayDeleteObj(test_array, &test_val);
+  TEST_ASSERT_TRUE(&test_val!=test_array->arr[0]);
+}
+
+/**
+ * test deleteObject with ptr not in array
+ */
+void test_not_deleteObject() {
+  Array * test_array = arrayNew(0);
+  int test_val = 196613;
+  int test_val1 = 196614;
+  arrayPush(test_array, &test_val);
+  arrayDeleteObj(test_array, &test_val1);
+  TEST_ASSERT_TRUE(arrayContains(test_array, &test_val));
+}
+
+
+/**
+ * test deleteObject with array of multiple ptr
+ */
+void test_deleteObject_n() {
+  Array * test_array = arrayNew(0);
+  int test_val0 = 196613;
+  int test_val1 = 196614;
+  int test_val2 = 196615;
+  int test_val3 = 196616;
+  arrayPush(test_array, &test_val0);
+  arrayPush(test_array, &test_val1);
+  arrayPush(test_array, &test_val2);
+  arrayPush(test_array, &test_val3);
+  arrayDeleteObj(test_array, &test_val0);
+  TEST_ASSERT_TRUE((!arrayContains(test_array, &test_val0)));
+}
+
 
 int main() {
   UNITY_BEGIN();
@@ -117,7 +224,14 @@ int main() {
   RUN_TEST(test_push);
   RUN_TEST(test_push_n);
   RUN_TEST(test_push_n_diff);
+  RUN_TEST(test_push_size_check);
   RUN_TEST(test_pop);
   RUN_TEST(test_push_pop);
+  RUN_TEST(test_push_pop_n);
+  RUN_TEST(test_containsObj);
+  RUN_TEST(test_containsObj_n);
+  RUN_TEST(test_deleteObject);
+  RUN_TEST(test_not_deleteObject);
+  RUN_TEST(test_deleteObject_n);
   return UNITY_END();
 }

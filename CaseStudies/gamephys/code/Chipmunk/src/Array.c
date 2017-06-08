93c8aae9e7da5c6539dd1160c5e2053bfbedb434
Henry <hmadej@gmail.com>
2017-06-08 09:42:03 -0400

    refactored Array.c, corrected small bug with regards to index

--- CaseStudies/gamephys/code/Chipmunk/src/Array.c
+++ CaseStudies/gamephys/code/Chipmunk/src/Array.c
@@ -21,73 +21,118 @@
 
  // ARRAY_C - ARRAY MODULE //
 
-#include <string.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include "../include/Chipmunk.h"
 
-Array *arrayNew(int size) {
-    Array *arr = (Array *) calloc(1, sizeof(Array));
+//       CONSTANTS       //
+const int START_INDEX  = 0;
+const int DEFAULT_SIZE = 4;
 
-    arr->num = size;
-    arr->max = (size ? size : 4);
-    arr->arr = (void **) calloc(arr->max, sizeof(void*));
 
-    return arr;
+/**
+ * Creates a new array of size: size
+ * size    - size of the new array to be created
+ * returns - array pointer to new array
+ */
+Array *arrayNew(int size) { // Out -> Array*
+  Array * array = calloc(1, sizeof(Array));
+  array->num = START_INDEX;
+
+  if (size) { // <=> size ? size : 4
+    array->max = size;
+  } else {
+    array->max = DEFAULT_SIZE;
+  }
+  array->arr = calloc(array->max, sizeof(void*));
+  return array;
 }
 
-void arrayFree(Array *arr) {
-    if (arr) {
-        free(arr->arr);
-        arr->arr = NULL;
-
-        free(arr);
-    }
+/**
+ * frees memory previously allocated for the array pointer passed
+ * array - takes a pointer to an array
+ */
+void arrayFree(Array *array) {
+  free(array->arr);
+  array->arr = NULL;
+  free(array);
 }
 
+/**
+ * doubles the maximum size of the array
+ * arr - takes a pointer to an array
+ */
+void arrayResize(Array *arr) {
+  arr->max *= 2;
+  arr->arr = realloc(arr->arr, arr->max * sizeof(void*));
+}
+
+/**
+ * push a new pointer to the array
+ * arr    - pointer of the array to be pushed
+ * object - pointer of the object to be pushed on the array
+ */
 void arrayPush(Array *arr, void *object) {
-    if (arr->num == arr->max) {
-        arr->max *= 2;
-        arr->arr = (void **) realloc(arr->arr, arr->max * sizeof(void*));
-    }
-
-    arr->arr[arr->num] = object;
-    arr->num++;
+  if (arr->num == arr->max) {
+    arrayResize(arr);
+  }
+  arr->arr[arr->num] = object;
+  arr->num++;
 }
 
+
+/**
+ * removes the last item pushed onto the array
+ * arr - pointer of the array to be popped
+ */
 void *arrayPop(Array *arr) {
-    assertHard(arr->num > 0, VOID_ERR, "Unable to pop items from an empty array!");
-    arr->num--;
-
-    void *retval = arr->arr[arr->num];
-    arr->arr[arr->num] = NULL;
-
-    return retval;
+  assertHard(arr->num > START_INDEX, VOID_ERR, "Unable to pop items from an empty array!");
+  arr->num--;
+  void *object = arr->arr[arr->num];
+  arr->arr[arr->num] = NULL;
+  return object;
 }
 
+/**
+ * if object pointer exists in the array, remove it
+ * arr    - pointer to the array which the item will be deleted from
+ * object - pointer to the object that will be removed (if it exists in array)
+ */
 void arrayDeleteObj(Array *arr, void *object) {
-    for (int i = 0; i < arr->num; ++i) {
-        if (arr->arr[i] == object) {
-            --arr->num;
-
-            arr->arr[i] = arr->arr[arr->num];
-            arr->arr[arr->num] = NULL;
-
-            return;
-        }
+  for (int i = 0; i<=arr->num; i++) {
+    if (arr->arr[i] == object) {
+      arr->num--;
+      arr->arr[i] = arr->arr[arr->num]; // move last item to ith position
+      arr->arr[arr->num] = NULL; // delete last item
+      return; // break out of loop
     }
+  }
 }
 
-bool arrayContains(Array *arr, void *ptr) {
-    for (int i = 0; i < arr->num; ++i) {
-        if (arr->arr[i] == ptr) return true;
+/**
+ * Check if object in contained in the array
+ * arr    - pointer to the array which the item may be found
+ * object - pointer to object to be checked for
+ */
+bool arrayContains(Array *arr, void *object) {
+  for (int i = 0; i<=arr->num; i++) {
+    if (arr->arr[i] == object) {
+      return true;
     }
-
-    return false;
+  }
+  return false;
 }
 
-void arrayFreeEach(Array *arr, void (freeFunc)(void*)) {
-    for (int i = 0; i < arr->num; ++i) {
-        freeFunc(arr->arr[i]);
-    }
+
+/**
+ * apply function to each element of array
+ * arr   - pointer to array to apply function to elements of
+ * freef - pointer to function to be applied
+ */
+void arrayFreeEach(Array *arr, void (freef)(void*)) {
+  for (int i = 0; i<=arr->num; i++) {
+    freef(arr->arr[i]);
+  }
 }

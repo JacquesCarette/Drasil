79b94f0a4443d5d9166149d75df2a82b059b5dea
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-09 11:48:41 -0400

    minor formatting

--- CaseStudies/glass/Implementations/Python/Test/testMainfun/testMainfun6.py
+++ CaseStudies/glass/Implementations/Python/Test/testMainfun/testMainfun6.py
@@ -8,14 +8,14 @@ from Implementation import mainfun
 class TestMainfun(unittest.TestCase):
     
     def setUp(self):
-        self.output = mainfun.main(os.path.join("Test/Inputfiles","testInput5.txt"))
+        self.output = mainfun.main(os.path.join("Test/Inputfiles", "testInput5.txt"))
         
     def test_main(self):
         f1 = open("outputfile.txt", 'r')
-        f2 = open(os.path.join("Test/Inputfiles","output5.txt"), 'r')
+        f2 = open(os.path.join("Test/Inputfiles", "output5.txt"), 'r')
         text1 = f1.readlines()
         text2 = f2.readlines()
-        self.assertEqual(text1,text2)
+        self.assertEqual(text1, text2)
         f1.close()
         f2.close()
 

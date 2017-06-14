68797666cb69186b4504a9fa880f5215571bbb62
Daniel Scime <danscime@gmail.com>
2017-06-14 10:21:48 -0400

    Updated SSP GenDefs descriptions.

--- code/Example/Drasil/SSP/GenDefs.hs
+++ code/Example/Drasil/SSP/GenDefs.hs
@@ -37,7 +37,7 @@ nmFEq_desc :: Sentence
 nmFEq_desc = S "For a" +:+ phrase slice +:+ S "of" +:+ phrase mass +:+
   S "in the" +:+ phrase slope +:+ S "the" +:+ phrase force +:+
   S "equilibrium to satisfy" +:+ acroT "2" +:+ S "in the direction" +:+
-  S "perpendicular to" +:+. (S "base" +:+ phrase surface `ofThe`
+  phrase perp +:+ S "to" +:+. (S "base" +:+ phrase surface `ofThe`
   phrase slice) +:+ S "Rearranged to solve for" +:+ (phrase normForce `ofThe`
   phrase surface) +:+. getS totNrmForce +:+ at_start force +:+ S "equilibrium is" +:+
   S "derived from the free body diagram of" +:+
@@ -80,13 +80,13 @@ resShr_rel = C shrResI := C nrmFSubWat :* tan (C fricAngle) :+ C cohesion :* C b
 resShr_desc :: Sentence
 resShr_desc = S "The Mohr-Coulomb resistive shear strength of a" +:+
   phrase slice +:+ getS shrResI +:+ S "is adjusted to account for the" +:+
-  S "effective normal" +:+
+  S "effective" +:+ phrase normal +:+
   getS nrmStrss :+: S "'" +:+ -- FIXME: Need to add prime without hardcoding
-  S "=" +:+ S "N' = N -Ub of a soil from" +:+. acroT "4" +:+
-  S "Also and the cohesion is" +:+
-  S "adjusted to account for the length l of the plane where the normal occurs" `sC`
+  S "=" +:+ S "N' = N - Ub of a soil from" +:+. acroT "4" +:+
+  S "Also and the cohesion is adjusted to account for the" +:+ phrase len +:+
+  S "l of the plane where the" +:+ phrase normal +:+ S "occurs" `sC`
   S "where lb,i = bi * sec (alpha), and bi is the x width of the base." +:+
-  S "Therefore c=c' *bi *sec(alpha*i)."
+  S "Therefore c = c' * bi * sec(alpha * i)."
   -- FIXME: Still needs to be more automated
 
 --
@@ -98,10 +98,12 @@ mobShr_rel = C mobShrI := C shrResI :/ C fs :=
   (C nrmFSubWat :* tan (C fricAngle) :+ C cohesion :* C baseWthX :* sec (C baseAngle)) :/ C fs
 
 mobShr_desc :: Sentence
-mobShr_desc = foldlSent [S "From the definition of the Factor of Safety in T1" `sC`
-  S "and the new definition of", getS shrResI `sC` S "a new relation for the net mobile", 
-  S "shear force of the slice", getS ti, S "is found as the resistive shear" , getS shrResI,
-  S "(GD3) divided by the factor of safety", getS fs]
+mobShr_desc = foldlSent [
+  S "From", (phrase definition `ofThe` S "Factor of Safety in"), acroT "1" `sC`
+  S "and the new" +:+ phrase definition +:+ S "of", getS shrResI `sC` S "a new",
+  S "relation for" +:+ (S "net mobile" +:+ phrase shearForce `ofThe` phrase slice),
+  getS ti, S "is found as the resistive shear" , getS shrResI,
+  sParen (acroGD "3"), S "divided by the factor of safety", getS fs]
 
 --
 normShrR :: RelationConcept
@@ -111,12 +113,15 @@ nmShrR_rel :: Relation
 nmShrR_rel = C intShrForce := C normToShear :* C scalFunc :* C intNormForce
 
 nmShrR_desc :: Sentence
-nmShrR_desc = foldlSent [S "The assumption for the Morgenstern Price method (A5)",
-  S "that the interslice shear force", getS xi, S "is proportional to the interslice", 
-  S "normal force", getS intNormForce, S "by a proportionality constant",
-  (P $ (Greek Lambda_L)), S "and a predetermined scaling function", P (lF) `sC` S "that", 
-  S "changes the proportionality as a function of the x-ordinate position of the interslice.",
-  P (lF), S "is typically either a half-sine along the slip surface, or a constant"]
+nmShrR_desc = foldlSent [S "The", phrase assumption, S "for the Morgenstern Price",
+  phrase method_, sParen (acroA "5"), S "that the", phrase intrslce,
+  phrase shearForce, getS xi, S "is proportional to the", phrase intrslce, 
+  phrase normForce, getS intNormForce, S "by a proportionality constant",
+  (P $ (Greek Lambda_L)), S "and a predetermined scaling function",
+  P (lF) `sC` S "that changes", (S "proportionality as a function" `ofThe`
+  S "x-ordinate position of the") +:+. phrase intrslce,
+  P (lF), S "is typically either a half-sine along the slip", phrase surface `sC`
+  S "or a constant"]
 
 --
 momentEql :: RelationConcept
@@ -136,17 +141,24 @@ fNet_rel :: Relation
 fNet_rel = C genForce := (Int 0) --FIXME: requires two lines of equal signs
 
 fNet_desc :: Sentence
-fNet_desc = foldlSent [S "The net sum of forces acting on a slice for",
-  S "the RFEM model. The forces that create an applied load on the slice.", 
-  S "Fx,i refers to the load in the direction perpendicular to the", 
-  S "direction of the force of gravity for slice i, while Fy,i refers", 
-  S "to the load in the direction parallel to the force of gravity for", 
-  S "slice i. Forces are found in the free body diagram of Fig2 in section", 
-  S "4.1.2. In this model the elements are not exerting forces on each other" `sC`
-  S "so the interslice forces E and X are not a part of the model. Index i", 
-  S "refers to the values of the properties for slice/interslices following", 
-  S "convention in Fig1 in section 4.1.2. Force variable definitions can",
-  S "be found in DD1 to DD8"]
+fNet_desc = foldlSent [S "The net sum of", plural force, S "acting on a",
+  phrase slice, S "for the RFEM" +:+. phrase model, S "The", plural force,
+  S "that create an applied load on the" +:+. phrase slice,
+  -- FIXME: Sentence does not sound like a sentence
+  S "Fx,i refers to the load in the direction", phrase perp, S "to the", 
+  S "direction of the", phrase force, S "of gravity for", phrase slice,
+  S "i, while Fy,i refers to the load in the direction parallel to the",
+  phrase force, S "of gravity for", phrase slice,
+  S "i.", at_start' force, S "are found in the free body diagram of" +:+.
+  makeRef (SRS.physSyst SRS.missingP []), S "In this", phrase model,
+  S "the", plural element, S "are not exerting", plural force,
+  S "on each other" `sC` S "so the", phrase intrslce, plural force,
+  S "E and X are not a part of the" +:+. phrase model, S "Index i", 
+  S "refers to", (plural value `ofThe` plural property), S "for",
+  phrase slice :+: S "/" :+: plural intrslce, S "following", 
+  S "convention in" +:+. makeRef (SRS.physSyst SRS.missingP []), 
+  at_start force, phrase variable, plural definition, S "can be found in",
+  acroDD "1", S "to" +:+. acroDD "8"]
   --FIXME:Finish pulling out symbols
 
 --

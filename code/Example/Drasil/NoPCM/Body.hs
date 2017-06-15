1c84990a950f78ceeea322549bbcf06929e5a982
AKM11 <moria@mcmaster.ca>
2017-06-15 11:18:35 -0400

    Updated syntax of Body and added more place-holder values.

--- code/Example/Drasil/NoPCM/Body.hs
+++ code/Example/Drasil/NoPCM/Body.hs
@@ -10,7 +10,7 @@ import Data.Drasil.SI_Units
 import Data.Drasil.Authors
 import Data.Drasil.Utils(listConstS)
 import Data.Drasil.Concepts.Documentation
-import Data.Drasil.Concepts.Math (ode, equation)
+import Data.Drasil.Concepts.Math (ode)
 import Data.Drasil.Concepts.Software
 import Data.Drasil.Concepts.Thermodynamics (heat)
 import Data.Drasil.Units.Thermodynamics
@@ -20,9 +20,10 @@ import Drasil.ReferenceMaterial (intro)
 import qualified Drasil.SRS as SRS
 import Drasil.DocumentLanguage
 import Drasil.SpecificSystemDescription
-import Drasil.Introduction
 import Drasil.Requirements
 import Drasil.GeneralSystDesc
+
+import Data.Drasil.SentenceStructures
 
 this_si :: [UnitDefn]
 this_si = map UU [metre, kilogram, second] ++ map UU [centigrade, joule, watt]
@@ -30,8 +31,8 @@ this_si = map UU [metre, kilogram, second] ++ map UU [centigrade, joule, watt]
 s3, s3_1, s4, s4_1, s4_1_1, s4_1_2, s4_1_3, s4_2,
   s5, s5_1, s5_2, s6, s7 :: Section
 
-s3_1_intro, sys_context_fig, s4_1_intro, s4_1_1_bullets, s4_1_2_list,
-  s4_1_3_intro, s4_1_3_list, fig_tank, s4_2_3_intro, s4_2_4_intro,
+s3_1_intro, sys_context_fig, s3_2_intro, s3_3_intro, s4_1_intro, s4_1_1_bullets, 
+  s4_1_2_list, s4_1_3_intro, s4_1_3_list, fig_tank, s4_2_3_intro, s4_2_4_intro, 
   s4_2_5_intro, s4_2_6_table1, s4_2_6_table2:: Contents
 
 -------------------------------
@@ -58,6 +59,7 @@ nopcmSymbMap :: SymbolMap
 nopcmSymbMap = symbolMap pcmSymbols
 
 
+
 --------------------------
 --Section 2 : INTRODUCTION
 --------------------------
@@ -65,15 +67,29 @@ nopcmSymbMap = symbolMap pcmSymbols
 s2s, s2e, s2_1, s2_2s, s2_2e  :: Sentence
 
 s2s = EmptyS
+
 s2e = EmptyS
 --TODO: Placeholder values until content can be added
+
+-----------------------------------
+--Section 2.1 : PURPOSE OF DOCUMENT
+-----------------------------------
 
 s2_1 = EmptyS 
 --TODO: Placeholder values until content can be added
 
+-------------------------------------
+--Section 2.2 : SCOPE OF REQUIREMENTS
+-------------------------------------
+
 s2_2s = EmptyS
+
 s2_2e = EmptyS
 --TODO: Placeholder values until content can be added
+
+----------------------------------------
+--Section 2.3 : ORGANIZATION OF DOCUMENT
+----------------------------------------
 
 {-s2_3 = charIntRdrF knowledge understanding sWHS EmptyS
   (SRS.userChar SRS.missingP [])
@@ -93,21 +109,39 @@ s2_2e = EmptyS
 --Section 3 : GENERAL SYSTEM DESCRIPTION
 ----------------------------------------
 
-s3 = genSysF [s3_1] (Paragraph EmptyS) [] []
---TODO: fill in the empty (last three) parameters
+s3 = genSysF [s3_1] s3_2_intro [s3_3_intro] []
+
+------------------------------
+--Section 3.1 : SYSTEM CONTEXT
+------------------------------
 
 s3_1 = SRS.sysCont [s3_1_intro, sys_context_fig] []
 
-s3_1_intro = Paragraph $ makeRef sys_context_fig +:+ S "shows the" +:+.
-  phrase sysCont +:+ S "A circle represents an external entity outside the" +:+
-  phrase software `sC` S "the" +:+ phrase user +:+
-  S "in this case. A rectangle represents the" +:+ phrase softwareSys +:+
-  S "itself" +:+. sParen (getAcc sWHS) +:+ S "Arrows are used to show the" +:+
-  plural datum +:+ S "flow between the" +:+ phrase section_ +:+
-  S "and its" +:+. phrase environment
+s3_1_intro = foldlSP [makeRef sys_context_fig, S "shows the" +:+.
+  phrase sysCont, S "A circle represents an external entity outside the",
+  phrase software `sC` S "the", phrase user,
+  S "in this case. A rectangle represents the", phrase softwareSys,
+  S "itself" +:+. sParen (getAcc sWHS), S "Arrows are used to show the",
+  plural datum, S "flow between the", phrase section_,
+  S "and its", phrase environment]
             
 sys_context_fig = Figure (makeRef sys_context_fig :+: S ":" +:+
   titleize sysCont) "SystemContextFigure.png"
+  
+------------------------------------
+--Section 3.2 : USER CHARACTERISTICS
+------------------------------------
+
+s3_2_intro = Paragraph $ EmptyS
+--TODO: Placeholder values until content can be added
+
+----------------------------------
+--Section 3.3 : SYSTEM CONSTRAINTS
+----------------------------------
+
+s3_3_intro = Paragraph $ EmptyS
+--TODO: Placeholder values until content can be added
+
 
 
 -----------------------------------------
@@ -117,20 +151,24 @@ sys_context_fig = Figure (makeRef sys_context_fig :+: S ":" +:+
 --TODO: finish filling in the subsections
 s4 = specSysDesF words_ [s4_1, s4_2]
   where words_ = (plural definition +:+ S "and finally the" +:+
-          phrase inModel +:+ sParen (getAcc ode) +:+
-          S "that" +:+ plural model +:+ S "the" +:+ phrase sWHT)
+                  phrase inModel +:+ sParen (getAcc ode) +:+
+                  S "that" +:+ plural model +:+ S "the" +:+ phrase sWHT)
 
+-----------------------------------
+--Section 4.1 : PROBLEM DESCRIPTION
+-----------------------------------
+          
 s4_1 = SRS.probDesc [s4_1_intro] [s4_1_1, s4_1_2, s4_1_3]
 
-s4_1_intro = Paragraph $ getAcc sWHS +:+ S "is a computer" +:+
-  phrase program +:+ S "developed to investigate" +:+
-  S "the heating of" +:+ phrase water +:+ S "in a" +:+. phrase sWHT
+s4_1_intro = foldlSP [getAcc sWHS, S "is a computer",
+  phrase program, S "developed to investigate",
+  S "the heating of", phrase water, S "in a", phrase sWHT]
 
 s4_1_1 = termDefnF Nothing [s4_1_1_bullets]
   
 s4_1_1_bullets = Enumeration $ (Bullet $ map (\x -> Flat $ 
-          (at_start x) :+: S ":" +:+ (x ^. defn)) 
-          [thermal_flux, heat_cap_spec])
+  (at_start x) :+: S ":" +:+ (x ^. defn)) 
+  [thermal_flux, heat_cap_spec])
   
 s4_1_2 = physSystDesc (getAcc sWHS) fig_tank [s4_1_2_list, fig_tank]
 
@@ -140,9 +178,9 @@ fig_tank = Figure (at_start sWHT `sC` S "with" +:+ phrase ht_flux +:+
   
 s4_1_2_list = Enumeration $ Simple $ map (\(a, b) -> (a, Flat b)) [
   (acroPS "1", at_start tank +:+ S "containing" +:+ phrase water), 
-  (acroPS "2", S "Heating" +:+ phrase coil +:+ S "at bottom of" +:+. phrase tank +:+
-  sParen (P (ht_flux_C ^. symbol) +:+ S "represents the" +:+ phrase ht_flux_C +:+
-  S "into the" +:+. phrase water))]
+  (acroPS "2", foldlSent [S "Heating", phrase coil, S "at bottom of" +:+. phrase tank,
+  sParen (foldlSent [P (ht_flux_C ^. symbol), S "represents the", phrase ht_flux_C,
+  S "into the", phrase water])])]
 
 s4_1_3 = SRS.goalStmt [s4_1_3_intro, s4_1_3_list] []
 
@@ -154,6 +192,10 @@ s4_1_3_intro = Paragraph $ S "Given the" +:+ phrase temp +:+ S "of the" +:+
 s4_1_3_list = Enumeration $ Simple $ map (\(a, b) -> (a, Flat b)) [
   (acroGS "1", S "predict the" +:+ phrase temp_water +:+ S "over time")]
 
+------------------------------------------------------
+--Section 4.2 : SOLUTION CHARACTERISTICS SPECIFICATION
+------------------------------------------------------
+  
 s4_2 = solChSpecF sWHS (s4_1, s6) True EmptyS ((makeRef s4_2_6_table1 +:+
   S "and" +:+ makeRef s4_2_6_table2 +:+ S "show"), EmptyS, False, EmptyS)
   ([], s4_2_2_TMods, [s4_2_3_intro], [s4_2_4_intro], [s4_2_5_intro],
@@ -179,6 +221,7 @@ s4_2_6_table2 = Table [S "Var", titleize' physicalConstraint, S "Typical" +:+
   titleize' variable) True
     
 
+    
 --------------------------
 --Section 5 : REQUIREMENTS
 --------------------------
@@ -188,15 +231,18 @@ s5 = reqF [s5_1, s5_2]
 s5_1 = SRS.funcReq [] [] --TODO: Placeholder values until content can be added
 
 s5_2 = nonFuncReqF [performance] [correctness, verifiability,
-        understandability, reusability, maintainability]
-        (S "This problem is small in size and relatively simple")
-        (S "Any reasonable implementation will be very quick and use minimal storage.")
+  understandability, reusability, maintainability]
+  (S "This problem is small in size and relatively simple")
+  (S "Any reasonable implementation will be very quick and use minimal storage.")
 
+        
+        
 ----------------------------
 --Section 6 : LIKELY CHANGES
 ----------------------------
 
 s6 = SRS.likeChg [] [] --TODO: Add the rest of the section
+
 
 
 ------------

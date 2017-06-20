dd7b4578e9c020c0f20b514d58cc5f446b51f700
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-20 15:31:38 -0400

    updated Python files in 'Python' folder to read in 't' as a String

--- CaseStudies/glass/Implementations/Python/Implementation/derivedValues.py
+++ CaseStudies/glass/Implementations/Python/Implementation/derivedValues.py
@@ -18,32 +18,32 @@ def derived_params(params):
     # cal weight of tnt
     params.wtnt = params.w * params.tnt
     # cal actual thickness
-    if params.t == 2.50:
+    if params.t == "2.5":
         params.h = 2.16
-    elif params.t == 2.70:
+    elif params.t == "2.7":
         params.h = 2.59
-    elif params.t == 3.0:
+    elif params.t == "3.0":
         params.h = 2.92
-    elif params.t == 4.0:
+    elif params.t == "4.0":
         params.h = 3.78
-    elif params.t == 5.0:
+    elif params.t == "5.0":
         params.h = 4.57
-    elif params.t == 6.0:
+    elif params.t == "6.0":
         params.h = 5.56
-    elif params.t == 8.0:
+    elif params.t == "8.0":
         params.h = 7.42
-    elif params.t == 10.0:
+    elif params.t == "10.0":
         params.h = 9.02
-    elif params.t == 12.00:
+    elif params.t == "12.0":
         params.h = 11.91
-    elif params.t == 16.00:
+    elif params.t == "16.0":
         params.h = 15.09
-    elif params.t == 19.00:
+    elif params.t == "19.0":
         params.h = 18.26
-    elif params.t == 22.00:
+    elif params.t == "22.0":
         params.h = 21.44
     else:
-        print("Wrong thickness entered -Not an industrial standard ")
+        print("Wrong thickness entered -Not a industrial standard ")
 
     # cal glass type factor
     if params.gt == 'AN' or params.gt == 'an':
@@ -55,3 +55,4 @@ def derived_params(params):
     else:
         print("Wrong glass type entered")
     return params
+

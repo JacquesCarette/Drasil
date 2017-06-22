9a781ff1654e2bb1c3d41bd8ff05283b4d4f5354
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-22 12:28:53 -0400

    added description for SWHS' IM4

--- code/Example/Drasil/SWHS/IMods.hs
+++ code/Example/Drasil/SWHS/IMods.hs
@@ -1,7 +1,11 @@
 module Drasil.SWHS.IMods where
 
 import Language.Drasil
+import Data.Drasil.Concepts.Documentation
 import Prelude hiding (id)
+import Drasil.SWHS.Unitals
+import Data.Drasil.Utils (getS, unwrap)
+import Data.Drasil.SentenceStructures (foldlSent, isThe, sAnd, ofThe)
 
 iModels :: [RelationConcept]
 iModels = [eBalanceOnWtr, eBalanceOnPCM, heatEInWtr, heatEInPCM]
@@ -52,7 +56,24 @@ htPCM_Rel :: Relation
 htPCM_Rel = Int 0
 
 htPCMDesc :: Sentence
-htPCMDesc = fixmeS
+htPCMDesc = foldlSent [S "The above equation is derived using" +:+. (acroT "2" `sAnd` acroT "3"),
+  (getS pcm_E) `isThe` S "change in thermal energy of the PCM relative to the energy",
+  S "at the", (phrase temp_init), sParen (getS temp_init) +:+. (unwrap $ getUnit pcm_initMltE), (getS pcm_E), 
+  S "for the solid PCM is found using", acroT "2", S "for sensible heating, with", S "specific heat capacity" `ofThe`
+  S "solid PCM" `sC` (getS htCap_S_P), sParen (unwrap $ getUnit htCap_S_P), S "and the change in the PCM temperature", 
+  S "from the", (phrase temp_init) +:+. sParen (unwrap $ getUnit temp_init), (getS pcm_E), S "for the melted PCM",
+  sParen (E ((C temp_PCM) :> (C pcm_initMltE))), S "is found using", acroT "2", S "for sensible heating of the" +:+. 
+  S "liquid PCM, plus the energy when melting starts, plus the energy required to melt all of the PCM",
+  S "The energy when melting starts is", (getS pcm_initMltE) +:+. sParen (unwrap $ getUnit pcm_initMltE),
+  S "The energy required to melt all of the PCM is", E ((C htFusion) :* (C pcm_mass)), 
+  sParen (unwrap $ getUnit pcm_initMltE) +:+. sParen (acroDD "3"),
+  S "The specific heat capacity of the liquid PCM is", (getS htCap_L_P),
+  sParen (unwrap $ getUnit htCap_L_P) `sAnd` S "the change in temperature is", E ((C temp_PCM) :- (C temp_melt_P)) +:+.
+  sParen (unwrap $ getUnit temp_melt_P), (getS pcm_E), S "during melting of the PCM is found using the", 
+  S "energy required at the instant melting of the PCM begins" `sC` (getS pcm_initMltE), 
+  S "plus the latent heat energy added to the PCM, QP (J) since the time when melting began", 
+  (getS t_init_melt) +:+. sParen (unwrap $ getUnit t_init_melt), S "The heat energy for boiling of the PCM is not detailed" `sC`
+  S "since the PCM is assumed to either be in a solid or liquid state", sParen (acroA "18")]
 
 {--}
 

92e447fe27498e4551f31db5fd3d6539035abcd7
AKM11 <moria@mcmaster.ca>
2017-06-26 10:29:21 -0400

    Updated equation format.

--- code/Example/Drasil/SWHS/GenDefs.hs
+++ code/Example/Drasil/SWHS/GenDefs.hs
@@ -5,6 +5,8 @@ import Prelude hiding (sin, cos, tan)
 import Language.Drasil
 import Data.Drasil.SentenceStructures (foldlSent)
 import Data.Drasil.Quantities.PhysicalProperties as QPP
+import Data.Drasil.Quantities.Thermodynamics as QT
+import Data.Drasil.Quantities.Physics as QP
 import Drasil.SWHS.Unitals
 
 ---------------------------
@@ -20,7 +22,8 @@ nwtnCooling = makeRC "nwtnCooling" (nounPhraseSP "Newton's law of cooling")
   nwtnCooling_desc nwtnCooling_rel
 
 nwtnCooling_rel :: Relation
-nwtnCooling_rel = (C thFluxVect) := (C htTransCoeff):*(C temp_diff)
+nwtnCooling_rel = ((FCall (C thFluxVect) [C QP.time]) := (C htTransCoeff) :* 
+  (FCall (C temp_diff) [C QP.time]))
 --FIMXE:^ function of t?
 
 nwtnCooling_desc :: Sentence
@@ -35,8 +38,8 @@ rocTempSimp = makeRC "rocTempSimp" (nounPhraseSP "Simplified rate of change of t
   rocTempSimp_desc rocTempSimp_rel
 
 rocTempSimp_rel :: Relation
-rocTempSimp_rel = (C QPP.mass):*(Int 0) := (C ht_flux_in):*(C in_SA) :- (C ht_flux_out):*(C out_SA) :+ (C vol_ht_gen):*(C QPP.vol)
---FIXME:Derivative Implementation?
+rocTempSimp_rel = ((C QPP.mass):*(C QT.heat_cap_spec):*(Deriv Total (C QT.temp) (C QP.time)) := 
+  (C ht_flux_in):*(C in_SA) :- (C ht_flux_out):*(C out_SA) :+ (C vol_ht_gen):*(C QPP.vol))
 
 rocTempSimp_desc :: Sentence
 rocTempSimp_desc = fixmeS

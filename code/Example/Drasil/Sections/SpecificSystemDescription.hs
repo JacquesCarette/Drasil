f3a9babdec953f973457d2cca88ab12fe9dd83ce
hmadej <hmadej@gmail.com>
2017-06-27 12:45:58 -0400

    issue #244 #316

--- code/Example/Drasil/Sections/SpecificSystemDescription.hs
+++ code/Example/Drasil/Sections/SpecificSystemDescription.hs
@@ -76,44 +76,40 @@ termDefnF end otherContents = SRS.termAndDefn ((intro):otherContents) []
 
 --general introduction for Physical System Description
 physSystDesc :: Sentence -> Contents -> [Contents] -> Section
-physSystDesc kWord fig otherContents = SRS.physSyst ((intro):otherContents) []
+physSystDesc progName fig otherContents = SRS.physSyst ((intro):otherContents) []
   where intro = Paragraph $ foldle (+:+) (+:) (EmptyS)
-                [S "The", (phrase physicalSystem), S "of", kWord `sC`
+                [S "The", (phrase physicalSystem), S "of", progName `sC`
                 S "as shown in", (makeRef fig) `sC` S "includes the following", 
                 plural element]
 
 --List all the given inputs. Might be possible to use ofThe combinator from utils.hs
 goalStmtF :: [Sentence] -> [Contents] -> Section
-goalStmtF givenInputs otherContents = SRS.goalStmt ((Paragraph intro):otherContents) []
-  where intro = S "Given" +:+ foldlList givenInputs `sC` S "the" +:+ 
+goalStmtF givenInputs otherContents = SRS.goalStmt (intro:otherContents) []
+  where intro = Paragraph $ S "Given" +:+ foldlList givenInputs `sC` S "the" +:+ 
                 plural goalStmt +: S "are"
 
--- kWord (ex ssp, progName), the two sections, gendef is True if you want general definitions sections, 
+-- progName (ex ssp, progName), the two sections, gendef is True if you want general definitions sections, 
 --  ddEndSent is the ending sentence for Data Definitions, this is a 4-tuple of inputs for Data Constraints, 
 --  the last input is a tupple of lists of Sections for each Subsection in order.
-solChSpecF :: (NamedIdea a) => a -> (Section, Section) -> Bool -> Sentence -> 
-  (Sentence, Sentence, Bool, Sentence) -> 
+solChSpecF :: (NamedIdea a) => a -> (Section, Section) -> Sentence -> 
+  (Sentence, Bool, Sentence) -> 
   ([Contents], [Contents], [Contents], [Contents], [Contents], [Contents]) -> 
   [Section] -> Section
-solChSpecF kWord (probDes, likeChg) gendef ddEndSent (tbRef, mid, end, trail) (a, t, g, dd, i, dc) adSubSec = 
-  SRS.solCharSpec [solutionCharSpecIntro kWord {--(instModels gendef)--}] (subSec gendef)
-  where subSec True  = [assumption_ True, theModels, generDefn, 
-                        dataDefin, instModels True{--, dataConstr--}] ++ adSubSec
-        subSec False = [assumption_ False, theModels, 
-                        dataDefin, instModels False{--, dataConstr--}] ++ adSubSec
-        assumption_ True  = assumpF  theModels generDefn dataDefin (instModels True ) likeChg a
-        assumption_ False = assumpF' theModels           dataDefin (instModels False) likeChg a
-        theModels  = thModF kWord t
-        generDefn  = genDefnF g
-        dataDefin  = dataDefnF ddEndSent dd
-        instModels True  = inModelF  probDes dataDefin theModels generDefn i
-        instModels False = inModelF' probDes dataDefin theModels           i
-        dataConstr = datConF mid end trail dc
+solChSpecF progName (probDes, likeChg) ddEndSent (mid, end, trail) (a, t, g, dd, i, dc) adSubSec = 
+  SRS.solCharSpec [solutionCharSpecIntro progName instModels] (subSec)
+  where subSec = [assumption_, theModels, generDefn, 
+                        dataDefin, instModels, dataConstr] ++ adSubSec
+        assumption_  = assumpF  theModels generDefn dataDefin instModels likeChg a
+        theModels    = thModF progName t
+        generDefn    = genDefnF g
+        dataDefin    = dataDefnF ddEndSent dd
+        instModels   = inModelF  probDes dataDefin theModels generDefn i
+        dataConstr   = datConF mid end trail dc
 
-solutionCharSpecIntro :: (NamedIdea a) => a {---> Section--} -> Contents
-solutionCharSpecIntro progName {--instModelSection--} = foldlSP [S "The", plural inModel, 
+solutionCharSpecIntro :: (NamedIdea a) => a -> Section -> Contents
+solutionCharSpecIntro progName instModelSection = foldlSP [S "The", plural inModel, 
   S "that govern", short progName, S "are presented in" +:+. 
-  {--makeRef (instModelSection),--} S "FIXME",  S "The", phrase information, S "to understand", 
+  makeRef (instModelSection), S "FIXME",  S "The", phrase information, S "to understand", 
   (S "meaning" `ofThe` plural inModel), 
   S "and their derivation is also presented, so that the", plural inModel, 
   S "can be verified"]
@@ -124,7 +120,7 @@ solutionCharSpecIntro progName {--instModelSection--} = foldlSP [S "The", plural
 {--
 assumption_ True  = assumpF  theModels generDefn dataDefin (instModels True ) likeChg a
 --assumption_ False = assumpF' theModels           dataDefin (instModels False) likeChg a
-theModels  = thModF kWord t
+theModels  = thModF progName t
 generDefn  = genDefnF g
 dataDefin  = dataDefnF ddEndSent dd
 instModels True  = inModelF  probDes dataDefin theModels generDefn i
@@ -158,31 +154,38 @@ assumpIntro r1 r2 r3 r4 r5 = Paragraph $ foldlSent
 
 --wrapper for thModelIntro
 thModF :: (NamedIdea a) => a -> [Contents] -> Section
-thModF kword otherContents = SRS.thModel ((thModIntro kword):otherContents) []
+thModF progName otherContents = SRS.thModel ((thModIntro progName):otherContents) []
 
 -- generalized theoretical model introduction: identifies key word pertaining to topic
 thModIntro :: (NamedIdea a) => a -> Contents
-thModIntro k_word = foldlSP
+thModIntro progName = foldlSP
           [S "This", phrase section_, S "focuses on", 
           S "the", phrase general, (plural equation), S "and", 
-          S "laws that", short k_word, S "is based on"]
+          S "laws that", short progName, S "is based on"]
 
 -- just supply the other contents for General Definition. Use empty list if none needed
 genDefnF :: [Contents] -> Section
-genDefnF otherContents = SRS.genDefn (genDefnIntro:otherContents) []
-  where genDefnIntro = foldlSP [S "This", phrase section_, 
-                       S "collects the", S "laws and", (plural equation), 
-                       S "that will be used in", S "deriving the", 
-                       plural dataDefn `sC` S "which in turn are used to", 
-                       S "build the", plural inModel]
+genDefnF otherContents = SRS.genDefn (generalDefinitionIntro otherContents:otherContents) []
+
+generalDefinitionIntro :: (LayoutObj t) => [t] -> Contents
+generalDefinitionIntro [] = Paragraph $ S "There are no general definitions."
+generalDefinitionIntro _ = foldlSP [S "This", phrase section_, 
+  S "collects the", S "laws and", (plural equation), 
+  S "that will be used in", S "deriving the", 
+  plural dataDefn `sC` S "which in turn are used to", S "build the", 
+  plural inModel]
+
                        
 -- uses EmptyS if ending sentence is not needed
 dataDefnF :: Sentence -> [Contents] -> Section                      
-dataDefnF endingSent otherContents = SRS.dataDefn ((dataDefnIntro endingSent):otherContents) []
-  where dataDefnIntro ending = Paragraph $ foldlSent
-            [S "This", phrase section_, 
-            S "collects and defines all the", plural datum, 
-            S "needed to build the", plural inModel] +:+ ending
+dataDefnF endingSent otherContents = SRS.dataDefn 
+  ((dataDefinitionIntro endingSent):otherContents) []
+
+
+dataDefinitionIntro :: Sentence -> Contents
+dataDefinitionIntro closingSent = Paragraph $ (foldlSent [S "This", phrase section_, 
+    S "collects and defines all the", plural datum, 
+    S "needed to build the", plural inModel] +:+ closingSent)
 
 -- wrappers for inModelIntro. Use inModelF' if genDef are not needed
 inModelF :: Section -> Section -> Section -> Section -> [Contents] -> Section
@@ -194,34 +197,37 @@ inModelF' probDes datDef theMod otherContents = SRS.inModel ((inModelIntro probD
 -- just need to provide the four references in order to this function. Nothing can be input into r4 if only three tables are present
 inModelIntro :: Section -> Section -> Section -> Maybe Section -> Contents
 inModelIntro r1 r2 r3 r4 = foldlSP [S "This", phrase section_, 
-          S "transforms the", phrase problem, S "defined in", (makeRef r1), 
-          S "into one which is expressed in mathematical terms. It uses concrete", 
-          plural symbol_, S "defined in", (makeRef r2), 
-          S "to replace the abstract", plural symbol_, S "in the", 
-          plural model, S "identified in", (makeRef r3) :+: end r4]
-          where end (Just genDef) = S " and" +:+ (makeRef genDef)
-                end Nothing       = EmptyS
+  S "transforms the", phrase problem, S "defined in", (makeRef r1), 
+  S "into one which is expressed in mathematical terms. It uses concrete", 
+  plural symbol_, S "defined in", (makeRef r2), 
+  S "to replace the abstract", plural symbol_, S "in the", 
+  plural model, S "identified in", (makeRef r3) :+: end r4]
+    where end (Just genDef) = S " and" +:+ (makeRef genDef)
+          end Nothing       = EmptyS
 
-
-
-        
 -- wrapper for datConPar
 datConF :: Sentence -> Bool -> Sentence -> [Contents] -> Section
-datConF mid end trailing tables = SRS.datCon ((datConPar (tbRef tables) mid end trailing):tables) []
+datConF mid end trailing tables = SRS.datCon 
+  ((dataConstraintParagraph end (listofTablesToRefs tables) mid trailing):tables) []
   
 -- reference to the input/ ouput tables -> optional middle sentence(s) (use EmptyS if not wanted) -> 
 -- True if standard ending sentence wanted -> optional trailing sentence(s) -> Contents
-datConPar :: Sentence -> Sentence -> Bool -> Sentence -> Contents
-datConPar tableRef middleSent endingSent trailingSent = foldlSP
-  [(dataConstraintIntroSent tableRef), middleSent, 
-  dataConstraintClosingSent (endS endingSent) trailingSent]
-    where endS False = EmptyS
-          endS True  = dataConstraintUncertainty
+dataConstraintParagraph :: Bool -> Sentence -> Sentence -> Sentence -> Contents
+dataConstraintParagraph uncertainty tableRef middleSent trailingSent = Paragraph $
+  (dataConstraintIntroSent tableRef) +:+ middleSent +:+ 
+  (dataConstraintClosingSent (hasUncertainty uncertainty) trailingSent)
+    where hasUncertainty False = EmptyS
+          hasUncertainty True  = dataConstraintUncertainty
 
-tbRef []     = EmptyS
-tbRef [x]    = (makeRef x) +:+ S "shows"
-tbRef [x,y]  = (makeRef x) `sC` S "and" +:+ tbRef [y]
-tbRef (x:xs) = (makeRef x) `sC` tbRef (xs)
+
+-- makes a list of references to tables takes
+-- l  list of layout objects that can be referenced
+-- outputs a sentence containing references to the layout objects 
+listofTablesToRefs :: LayoutObj l => [l] -> Sentence
+listofTablesToRefs  []     = EmptyS
+listofTablesToRefs  [x]    = (makeRef x) +:+ S "shows"
+listofTablesToRefs  [x,y]  = (makeRef x) `sC` S "and" +:+ listofTablesToRefs [y]
+listofTablesToRefs  (x:xs) = (makeRef x) `sC` listofTablesToRefs (xs)
  
 dataConstraintIntroSent :: Sentence -> Sentence
 dataConstraintIntroSent tableRef = foldlSent [tableRef, S "the", plural datumConstraint, S "on the", phrase input_, 

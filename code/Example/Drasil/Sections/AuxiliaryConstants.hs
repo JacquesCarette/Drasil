80bc3bda5cfaaa3c3f47b7e742f59c5877c79cb2
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-28 09:55:58 -0400

    cleaned up 'AuxiliaryConstants' file

--- code/Example/Drasil/Sections/AuxiliaryConstants.hs
+++ code/Example/Drasil/Sections/AuxiliaryConstants.hs
@@ -1,20 +1,12 @@
 module Drasil.Sections.AuxiliaryConstants 
   (valsOfAuxConstantsF) where
 
-import Control.Lens ((^.))
 import Language.Drasil
 import qualified Drasil.SRS as SRS
 import Data.Drasil.SentenceStructures (foldlSP)
 import Data.Drasil.Concepts.Documentation
-import qualified Data.Drasil.Concepts.Math as CM
 import Data.Drasil.Utils(getS)
-
-import Data.Maybe (isJust)
-import Data.Drasil.Utils(getS)
-
-import Language.Drasil
 import qualified Data.Drasil.Concepts.Math as CM
-import Data.Drasil.Concepts.Documentation
 
 valsOfAuxConstantsF :: (NamedIdea a) => a ->[QDefinition] -> Integer -> Section
 valsOfAuxConstantsF kWord listOfConstants tblNum = (SRS.valsOfAuxCons) [intro (kWord), tableOfConstants (listOfConstants) (tblNum)] []
@@ -25,21 +17,7 @@ intro kWord = foldlSP [S "This section contains the standard values that are use
 
 tableOfConstants :: [QDefinition] -> Integer -> Contents
 tableOfConstants f num = Table
-  [S "Symbol", S "Description", S "Value", S "Unit"]
+  [titleize symbol_, titleize description, titleize value, titleize CM.unit_]
   (mkTable [getS, phrase, (\c -> E $ equat c), unit'2Contents] f)
   (titleize table_ +: S (show num) +:+ S "Auxiliary Constants")
   True
-
-{-
-tableOfConstantsNEW :: [QDefinition] -> Integer -> Contents
-tableOfConstantsNEW constants tblNum = Table
-  [at_start symbol_, at_start description, at_start value, at_start' CM.unit_]
-  (mkTable
-  [(\ch -> (\(Just t) -> (getS t)) (getSymb ch)),
-  phrase constants,
-  unit'2Contents]
-  sls)
-  (titleize table_ +: S (show tblNum) +:+ S "Auxiliary ConstantsNEW")
-  False
-  where sls = filter (isJust . getSymb) constants
-  -}

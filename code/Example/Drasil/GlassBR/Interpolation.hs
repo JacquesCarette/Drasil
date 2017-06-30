af4103695c50bb9d015a3dad79a769ed90a910d5
Maryyam Niazi <niazim3@mcmaster.ca>
2017-06-30 15:51:12 -0400

    contributes to issue #328

--- code/Example/Drasil/GlassBR/Interpolation.hs
+++ code/Example/Drasil/GlassBR/Interpolation.hs
@@ -3,12 +3,22 @@ module Drasil.GlassBR.Interpolation where
 import Language.Drasil
 
 interpData :: [QDefinition]
-interpData = []
+interpData = [y_interp]
+
+lin_interp :: Expr
+lin_interp = (((C y_2) - (C y_1)) :/ ((C x_2) - (C x_1))) * ((C x) - (C x_1)) + (C y_1)
+
+y_interp :: QDefinition
+y_interp = fromEqn' "y_interp" (nounPhraseSP "interpolated y value") lY lin_interp
+
+y_2, y_1, x_2, x_1, x :: VarChunk
+y_1 = makeVC "y_1"  (nounPhraseSP "y1") (sub (lY) (Atomic "1"))
+y_2 = makeVC "y_2"  (nounPhraseSP "y2") (sub (lY) (Atomic "2"))
+x_1 = makeVC "x_1"  (nounPhraseSP "x1") (sub (lX) (Atomic "1"))
+x_2 = makeVC "x_2"  (nounPhraseSP "x2") (sub (lX) (Atomic "2"))
+x   = makeVC "x"    (nounPhraseSP "x")    lX -- = params.wtnt from mainFun.py
 
 {-
-
-lin_interp ::
-lin_interp x1 y1 x2 y2 x = ((y2 :- y1) :/ (x2 - x1)) :* (x :- x1) :+ y1
 
 wj, wjplus1
 q1 = demand on wj, q2 = demand on wjplus1 : (@ sd)

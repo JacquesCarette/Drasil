ce2392fa3c69bee9870cc41f58df5174aebeb032
Daniel Scime <danscime@gmail.com>
2017-06-30 16:25:05 -0400

    Implementing more SWHS IMods.

--- code/Example/Drasil/SWHS/IMods.hs
+++ code/Example/Drasil/SWHS/IMods.hs
@@ -6,12 +6,13 @@ import Prelude hiding (id)
 import Drasil.SWHS.Unitals
 import Data.Drasil.Utils (getS, unwrap)
 import Data.Drasil.SentenceStructures (foldlSent, isThe, sAnd, ofThe)
-import Data.Drasil.Quantities.Physics (time)
-import Data.Drasil.Concepts.Math (equation)
-import Drasil.SWHS.Concepts (water)
+import Data.Drasil.Quantities.Physics (time, energy)
+import Data.Drasil.Concepts.Math (equation, change)
+import Drasil.SWHS.Concepts
 import Data.Drasil.SentenceStructures (foldlSent)
 import Data.Drasil.Concepts.PhysicalProperties
-import Data.Drasil.Concepts.Thermodynamics (melting, boil_pt, temp)
+import Data.Drasil.Concepts.Thermodynamics (melting, boil_pt, temp, thermal_energy,
+  heat_cap_spec)
 import Control.Lens ((^.))
 
 swhsInModels :: [RelationConcept]
@@ -34,13 +35,17 @@ balWtrDesc :: Sentence
 balWtrDesc = foldlSent [(E $ C temp_W) `isThe` phrase temp_W +:+. sParen (unwrap $ getUnit temp_W),
   (E $ C temp_PCM) `isThe` phrase temp_PCM +:+. sParen (unwrap $ getUnit temp_PCM),
   (E $ C temp_C) `isThe` phrase temp_C +:+. sParen (unwrap $ getUnit temp_C),
-  (E $ C tau_W := (C w_mass * C htCap_W) / (C coil_HTC * C coil_SA)), S "is a constant" +:+. sParen (unwrap $ getUnit tau_W),
-  (E $ C eta := (C pcm_HTC * C pcm_SA) / (C coil_HTC * C coil_SA)), S "is a constant" +:+. sParen (S "dimensionless"),
-  S "The above", phrase equation, S "applies as long as the", phrase water, S "is in", phrase liquid,
-  S "form" `sC` (E $ Int 0 :< C temp_W :< (Int 100)), sParen (unwrap $ getUnit temp_W), S "where",
-  S $ show (0 :: Integer), sParen (unwrap $ getUnit temp_W) `sAnd` (S $ show (100 :: Integer)),
-  sParen (unwrap $ getUnit temp_W), S "are the", phrase melting `sAnd` plural boil_pt, S "of",
-  phrase water `sC` S "respectively", sParen (acroA 14 `sC` acroA 19)]
+  (E $ C tau_W := (C w_mass * C htCap_W) / (C coil_HTC * C coil_SA)),
+  S "is a constant" +:+. sParen (unwrap $ getUnit tau_W),
+  (E $ C eta := (C pcm_HTC * C pcm_SA) / (C coil_HTC * C coil_SA)),
+  S "is a constant" +:+. sParen (S "dimensionless"),
+  S "The above", phrase equation, S "applies as long as the", phrase water,
+  S "is in", phrase liquid, S "form" `sC` (E $ Int 0 :< C temp_W :< (Int 100)),
+  sParen (unwrap $ getUnit temp_W), S "where", S $ show (0 :: Integer),
+  sParen (unwrap $ getUnit temp_W) `sAnd` (S $ show (100 :: Integer)),
+  sParen (unwrap $ getUnit temp_W), S "are the", phrase melting `sAnd`
+  plural boil_pt, S "of", phrase water `sC` S "respectively",
+  sParen (acroA 14 `sC` acroA 19)]
 
 
 ---------
@@ -59,7 +64,12 @@ balPCM_Rel = (Deriv Total (C temp_PCM) (C time)) := Case [case1, case2, case3, c
 
 
 balPCMDesc :: Sentence
-balPCMDesc = foldlSent [(E $ C temp_W) `isThe` S "water temperature" +:+. sParen (unwrap $ getUnit temp_W), fixmeS]
+balPCMDesc = foldlSent [(E $ C temp_W) `isThe` phrase temp_W +:+. sParen (unwrap $ getUnit temp_W),
+  (E $ C temp_PCM) `isThe` phrase temp_PCM +:+. sParen (unwrap $ getUnit temp_PCM),
+  (E $ (C tau_S_P) := ((C pcm_mass) * (C htCap_S_P)) / ((C pcm_HTC) * (C pcm_SA))),
+  S "is a constant" +:+. sParen (unwrap $ getUnit tau_S_P),
+  (E $ (C tau_L_P) := ((C pcm_mass) * (C htCap_L_P)) / ((C pcm_HTC) * (C pcm_SA))),
+  S "is a constant" +:+. sParen (unwrap $ getUnit tau_S_P)]
 
 
 ---------
@@ -70,20 +80,24 @@ heatEInWtr = makeRC "heatEInWtr" (nounPhraseSP "Heat energy in the water")
   htWtrDesc htWtr_Rel
 
 htWtr_Rel :: Relation
-htWtr_Rel = Int 0 := Int 0
+htWtr_Rel = (FCall (C w_E) [C time]) := (C htCap_W) * (C w_mass) *
+  ((FCall (C temp_W) [C time]) - C temp_init)
 
 htWtrDesc :: Sentence
-htWtrDesc = foldlSent [S "The above equation is derived using" +:+. acroT 2, 
-  (getS w_E) `isThe` S "change in thermal energy of the liquid water relative to",
-  S "the energy at the initial temperature", sParen (getS temp_init) +:+.
-  sParen (unwrap $ getUnit pcm_initMltE), (getS htCap_W) `isThe` S "specific heat",
-  S "capacity of liquid water", (((sParen (unwrap $ getUnit htCap_S_P)) `sAnd` (getS w_mass))
-  `isThe` S "mass of the water") +:+. sParen (unwrap $ getUnit w_mass), 
-  S "The change in temperature is the difference between the temperature at time", 
-  P (time ^. symbol), sParen (unwrap $ getUnit t_init_melt) `sC` (getS temp_W) `sAnd`
-  S "the", phrase temp_init `sC` getS temp_init +:+. sParen (unwrap $ getUnit temp_init),
-  S "This equation applies as long as", E ((Int 0) :< (C temp_W) :< (Int 0)) :+: 
-  (unwrap $ getUnit temp_W), sParen (acroA 14 `sC` acroA 19)]
+htWtrDesc = foldlSent [S "The above", phrase equation, S "is derived using" +:+. acroT 2, 
+  (getS w_E) `isThe` S "change in", phrase thermal_energy, S "of the", phrase liquid,
+  phrase water, S "relative to the", phrase energy, S "at the initial", phrase temp,
+  sParen (getS temp_init) +:+. sParen (unwrap $ getUnit pcm_initMltE), (getS htCap_W) `isThe`
+  phrase heat_cap_spec, S "of", phrase liquid, phrase water,
+  sParen (unwrap $ getUnit htCap_S_P) `sAnd` (getS w_mass)
+  `isThe` phrase mass, S "of the", phrase water +:+. sParen (unwrap $ getUnit w_mass), 
+  S "The", phrase change, S "in", phrase temp, S "is the difference between the",
+  phrase temp, S "at", phrase time, getS time,
+  sParen (unwrap $ getUnit t_init_melt) `sC` (getS temp_W) `sAnd` S "the",
+  phrase temp_init `sC` getS temp_init +:+. sParen (unwrap $ getUnit temp_init),
+  S "This", phrase equation, S "applies as long as",
+  (E $ (Int 0) :< (C temp_W) :< (Int 0)) :+: (unwrap $ getUnit temp_W),
+  sParen (acroA 14 `sC` acroA 19)]
 
 ---------
 -- IM4 --
@@ -96,10 +110,11 @@ htPCM_Rel :: Relation
 htPCM_Rel = Int 0 := Int 0
 
 htPCMDesc :: Sentence
-htPCMDesc = foldlSent [S "The above equation is derived using" +:+. (acroT 2 `sAnd` acroT 3),
-  (getS pcm_E) `isThe` S "change in thermal energy of the PCM relative to the energy",
+htPCMDesc = foldlSent [S "The above", phrase equation, S "is derived using" +:+.
+  (acroT 2 `sAnd` acroT 3), (getS pcm_E) `isThe` phrase change, S "in",
+  phrase thermal_energy, S "of the", short phsChgMtrl, S "relative to the", phrase energy,
   S "at the", (phrase temp_init), sParen (getS temp_init) +:+. (unwrap $ getUnit pcm_initMltE), (getS pcm_E), 
-  S "for the solid PCM is found using", acroT 2, S "for sensible heating, with", S "specific heat capacity" `ofThe`
+  S "for the", phrase solid, short phsChgMtrl, S "is found using", acroT 2, S "for sensible heating, with", S "specific heat capacity" `ofThe`
   S "solid PCM" `sC` (getS htCap_S_P), sParen (unwrap $ getUnit htCap_S_P), S "and the change in the PCM temperature", 
   S "from the", (phrase temp_init) +:+. sParen (unwrap $ getUnit temp_init), (getS pcm_E), S "for the melted PCM",
   sParen (E ((C temp_PCM) :> (C pcm_initMltE))), S "is found using", acroT 2, S "for sensible heating of the" +:+. 
@@ -115,7 +130,7 @@ htPCMDesc = foldlSent [S "The above equation is derived using" +:+. (acroT 2 `sA
   (getS t_init_melt) +:+. sParen (unwrap $ getUnit t_init_melt), S "The heat energy for boiling of the PCM is not detailed" `sC`
   S "since the PCM is assumed to either be in a solid or liquid state", sParen (acroA 18)]
 
-{--}
+
 
 {--varWithDesc :: N c => c -> Sentence
 varWithDesc conceptVar = (E $ C conceptVar) `isThe` phrase conceptVar +:+.

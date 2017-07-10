f4a2c22a4cc5ec1af1b0ca67169d28c43148478f
njericha <njericha@edu.uwaterloo.ca>
2017-07-10 12:36:47 -0400

    Questionable file SSP/Reqs.hs but cleaned it up

--- code/Example/Drasil/SSP/Reqs.hs
+++ code/Example/Drasil/SSP/Reqs.hs
@@ -8,17 +8,17 @@ reqs = [r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11]
 
 r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11 :: ReqChunk
 --FIXME: Why are these empty?
-r1  = ReqChunk (nw emptyN) [mod_ctrl, mod_inputf, mod_sds]
-r2  = ReqChunk (nw emptyN) [mod_genalg, mod_sds, mod_rng]
-r3  = ReqChunk (nw emptyN) [mod_kinadm, mod_sds]
-r4  = ReqChunk (nw emptyN) [mod_genalg, mod_slipslicer, mod_sds]
-r5  = ReqChunk (nw emptyN) [mod_mp, mod_sds]
-r6  = ReqChunk (nw emptyN) [mod_slipweight, mod_sds]
-r7  = ReqChunk (nw emptyN) [mod_genalg, mod_sds, mod_rng]
-r8  = ReqChunk (nw emptyN) [mod_genalg, mod_sds]
-r9  = ReqChunk (nw emptyN) [mod_ctrl, mod_outputf, mod_slipslicer, mod_sds]
-r10 = ReqChunk (nw emptyN) [mod_outputf, mod_mp, mod_rfem, mod_sds]
-r11 = ReqChunk (nw emptyN) [mod_outputf, mod_plot]
+r1  = ReqChunk emptyN [mod_ctrl, mod_inputf, mod_sds]
+r2  = ReqChunk emptyN [mod_genalg, mod_sds, mod_rng]
+r3  = ReqChunk emptyN [mod_kinadm, mod_sds]
+r4  = ReqChunk emptyN [mod_genalg, mod_slipslicer, mod_sds]
+r5  = ReqChunk emptyN [mod_mp, mod_sds]
+r6  = ReqChunk emptyN [mod_slipweight, mod_sds]
+r7  = ReqChunk emptyN [mod_genalg, mod_sds, mod_rng]
+r8  = ReqChunk emptyN [mod_genalg, mod_sds]
+r9  = ReqChunk emptyN [mod_ctrl, mod_outputf, mod_slipslicer, mod_sds]
+r10 = ReqChunk emptyN [mod_outputf, mod_mp, mod_rfem, mod_sds]
+r11 = ReqChunk emptyN [mod_outputf, mod_plot]
 
-emptyN :: NamedChunk
-emptyN = npnc "" (cn "")
+emptyN :: NWrapper
+emptyN = nw $ npnc "" (cn "")

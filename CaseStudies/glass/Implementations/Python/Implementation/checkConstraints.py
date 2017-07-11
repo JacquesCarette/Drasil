9aed9dc8bac64bc334db7b2b28d02e6c5cd64775
Maryyam Niazi <niazim3@mcmaster.ca>
2017-07-11 16:56:35 -0400

    less tests fail; still not fully fixed; #347

--- CaseStudies/glass/Implementations/Python/Implementation/checkConstraints.py
+++ CaseStudies/glass/Implementations/Python/Implementation/checkConstraints.py
@@ -10,11 +10,11 @@ def check_constraints(params):
         raise SystemExit("InputError: a and b must be greater than 0")
     if params.asprat < 1 or params.asprat > 5:
         raise SystemExit("InputError: a/b must be between 1 and 5")
-    if not (params.t in ["2.5","2.7","3.0","4.0","5.0","6.0","8.0","10.0","12.0","16.0","19.0","22.0"]):
-        raise SystemExit("InputError: t must be in [2.5,2.7,3.0,4.0,5.0,6.0,8.0,10.0,12.0,16.0,19.0,22.0]")
     if params.tnt <= 0:
         raise SystemExit("InputError: TNT must be greater than 0")
     if params.wtnt < 4.5 or params.wtnt > 910:
         raise SystemExit("InputError: wtnt must be between 4.5 and 910")
     if params.sd<6 or params.sd>130:
         raise SystemExit("InputError: SD must be between 6 and 130")
+    if not (params.t in ["2.5","2.7","3.0","4.0","5.0","6.0","8.0","10.0","12.0","16.0","19.0","22.0"]):
+        raise SystemExit("InputError: t must be in [2.5,2.7,3.0,4.0,5.0,6.0,8.0,10.0,12.0,16.0,19.0,22.0]")

bb86866e2d7331df095ee136aaae58b8f9252ca8
Maryyam Niazi <niazim3@mcmaster.ca>
2017-07-11 16:31:20 -0400

    added VarChunks for issue #328

--- code/Example/Drasil/GlassBR/Interpolation.hs
+++ code/Example/Drasil/GlassBR/Interpolation.hs
@@ -20,26 +20,49 @@ w_TNTEqn :: Expr
 w_TNTEqn = (C char_weight) * (C tNT)
 
 y_2, y_1, x_2, x_1, x :: VarChunk
-y_1 = makeVC "y_1"  (nounPhraseSP "y1") (sub (lY) (Atomic "1"))
-y_2 = makeVC "y_2"  (nounPhraseSP "y2") (sub (lY) (Atomic "2"))
-x_1 = makeVC "x_1"  (nounPhraseSP "x1") (sub (lX) (Atomic "1"))
-x_2 = makeVC "x_2"  (nounPhraseSP "x2") (sub (lX) (Atomic "2"))
-x   = makeVC "x"    (nounPhraseSP "x")    lX -- = params.wtnt from mainFun.py
+y_1  = makeVC "y_1"    (nounPhraseSP "y1")   (sub (lY) (Atomic "1"))
+y_2  = makeVC "y_2"    (nounPhraseSP "y2")   (sub (lY) (Atomic "2"))
+x_1  = makeVC "x_1"    (nounPhraseSP "x1")   (sub (lX) (Atomic "1"))
+x_2  = makeVC "x_2"    (nounPhraseSP "x2")   (sub (lX) (Atomic "2"))
+x    = makeVC "x"    	 (nounPhraseSP "x")    lX -- = params.wtnt from mainFun.py
 
-w_j, s_j, q_j :: VarChunk
-w_j      = makeVC "w_j"      (nounPhraseSP "wj") (sub (lW) (lJ))
-s_j      = makeVC "s_j"      (nounPhraseSP "sj") (sub (lS) (lJ))
-q_j      = makeVC "q_j"      (nounPhraseSP "qj") (sub (lQ) (lJ))
+w_j, s_j, q_j, q_1, q_2, i, k :: VarChunk
+w_j  = makeVC "w_j"    (nounPhraseSP "wj")   (sub (lW) (lJ))
+s_j  = makeVC "s_j"    (nounPhraseSP "sj")   (sub (lS) (lJ))
+q_j  = makeVC "q_j"    (nounPhraseSP "qj")   (sub (lQ) (lJ))
+q_1  = makeVC "q_1"    (nounPhraseSP "q_1")  (sub (lQ) (Atomic "1"))
+q_2  = makeVC "q_2"    (nounPhraseSP "q_2")  (sub (lQ) (Atomic "2"))
+i    = makeVC "i"      (nounPhraseSP "i")    lI
+k    = makeVC "k"      (nounPhraseSP "k")    lK
 
-q_1, q_2 :: VarChunk
-q_1 = makeVC "q_1" (nounPhraseSP "q_1") (sub (lQ) (Atomic "1"))
-q_2 = makeVC "q_2" (nounPhraseSP "q_2") (sub (lQ) (Atomic "2"))
+---
 
-i, k :: VarChunk
-i   = makeVC "i" (nounPhraseSP "i") lI
-k   = makeVC "k" (nounPhraseSP "k") lK
+proper_index :: Expr
+proper_index = 0
 
-{-
-q = wtnt and sd -> both are user inputs
-  = charge weights, num of w curves, [list of sds]
--}
+index1, index2, data_, value :: VarChunk
+index1 = makeVC "index1"    (nounPhraseSP "index1")   (Atomic "index1")
+index2 = makeVC "index2"    (nounPhraseSP "index2")   (Atomic "index2")
+data_  = makeVC "data_"     (nounPhraseSP "data")     (Atomic "data")
+value  = makeVC "value"     (nounPhraseSP "value")    (Atomic "value")
+
+---
+
+find_bounds :: Expr
+find_bounds = 0
+
+data1, data2, value1, value2 :: VarChunk
+data1  = makeVC "data1"     (nounPhraseSP "data1")    (Atomic "data1")
+data1  = makeVC "data2"     (nounPhraseSP "data2")    (Atomic "data2")
+value1 = makeVC "value1"    (nounPhraseSP "value1")   (Atomic "value1")
+value2 = makeVC "value2"    (nounPhraseSP "value2")   (Atomic "value2")
+
+---
+
+interp :: Expr
+interp = 0
+
+idx, jdx, kdx, num_interp1, num_interp2, data3 :: VarChunk
+idx    = makeVC "idx"     (nounPhraseSP "idx")   (sub lI (Atomic "dx"))
+jdx    = makeVC "jdx"     (nounPhraseSP "jdx")   (sub lJ (Atomic "dx"))
+kdx    = makeVC "kdx"     (nounPhraseSP "kdx")   (sub lK (Atomic "dx"))

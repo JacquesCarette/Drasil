9a81dd4d7ea980659eb6c30fe994784a230f0e49
njericha <njericha@edu.uwaterloo.ca>
2017-07-12 13:58:57 -0400

    #319 Messing with CSS

--- code/Language/Drasil/HTML/Helpers.hs
+++ code/Language/Drasil/HTML/Helpers.hs
@@ -131,12 +131,12 @@ makeCSS _ = vcat [
     ],
   text ".fdn {border-top: thin solid black;}",
   text ".table {text-align:left;padding-left:1%;width:90%;margin-bottom:2%;margin-top:2%}",
-  text ("table, th, td {border: 1px solid black; border-collapse: collapse;" ++ 
+  text ("table, th, td {border-collapse: collapse;" ++ 
     "margin-left:auto;margin-right:auto;}"),
-  text "th, td {padding:1%;}",
-  text ".tdefn {width:75%;margin-top:1%;margin-bottom:1%;}",
-  text ".tdefn th {width:15%; padding:1%;}",
-  text ".ddefn {width:75%;margin-top:1%;margin-bottom:1%;}",
+  text "th, td {border: 1px solid black; padding:1%;}",
+  text ".tdefn, .ddefn {width:75%;margin-top:1%;margin-bottom:1%;}",
+  text ".tdefn th {width:15%;}",
+  text ".ddefn th {width:15%;}",
   text ".section {width:80%; margin:0 auto;text-align:left;}",
   vcat [
     text ".code {",
@@ -162,7 +162,7 @@ makeCSS _ = vcat [
     text "  position: absolute;",
     text "  top: 0;",
     text "  border: 1px solid #000;",
-    text "  width: 10px;",
+    text "  width: 6px;",
     text "  height: 100%;}",
     text ".matrix:before {",
     text "  left: -6px;",
@@ -173,10 +173,7 @@ makeCSS _ = vcat [
     text ".matrix td {",
     text "  padding: 6px;",
     text "  text-align: center;",
-    text "  border: 0px;",
-    text "  border-collapse: separate;",
-    text "  margin-left: initial;",
-    text "  margin-right: initial;}"
+    text "  border: 0px;}"
     ]
   ]
 

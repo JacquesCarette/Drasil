a1278826d5ee336268812d701128ded74a4fb8c1
Maryyam Niazi <niazim3@mcmaster.ca>
2017-07-17 15:46:30 -0400

    cleaned up Interpolation for GlassBR

--- code/Example/Drasil/GlassBR/Interpolation.hs
+++ code/Example/Drasil/GlassBR/Interpolation.hs
@@ -3,9 +3,6 @@ module Drasil.GlassBR.Interpolation where
 import Language.Drasil
 import Drasil.GlassBR.Unitals(char_weight)
 
-interpData :: [QDefinition]
-interpData = [y_interp]
-
 y_interp :: QDefinition
 y_interp = fromEqn' "y_interp" (nounPhraseSP "interpolated y value") lY lin_interp
 
@@ -19,15 +16,8 @@ x_1  = makeVC "x_1"    (nounPhraseSP "x1")   (sub (lX) (Atomic "1"))
 x_2  = makeVC "x_2"    (nounPhraseSP "x2")   (sub (lX) (Atomic "2"))
 x    = makeVC "x"      (nounPhraseSP "x")    lX -- = params.wtnt from mainFun.py
 
-w_j, s_j, q_j, q_1, q_2, i, v, z, z_array, y_array, x_array, y :: VarChunk
-w_j  = makeVC "w_j"    (nounPhraseSP "wj")   (sub (lW) (lJ))
-s_j  = makeVC "s_j"    (nounPhraseSP "sj")   (sub (lS) (lJ))
-q_j  = makeVC "q_j"    (nounPhraseSP "qj")   (sub (lQ) (lJ))
-q_1  = makeVC "q_1"    (nounPhraseSP "q_1")  (sub (lQ) (Atomic "1"))
-q_2  = makeVC "q_2"    (nounPhraseSP "q_2")  (sub (lQ) (Atomic "2"))
+i, z, z_array, y_array, x_array, y :: VarChunk
 i    = makeVC "i"      (nounPhraseSP "i")    lI
---k    = makeVC "k"      (nounPhraseSP "k")    lK
-v    = makeVC "v"      (nounPhraseSP "v")    lV
 z    = makeVC "z"      (nounPhraseSP "z")    lZ
 z_array = makeVC "z_array" (nounPhraseSP "z_array") (sub (lZ) (Atomic "array"))
 y_array = makeVC "y_array" (nounPhraseSP "y_array") (sub (lY) (Atomic "array"))
@@ -46,29 +36,26 @@ matrixCol = 0
 
 ---
 
-iVal, x_z_1, y_z_1, x_z_2, y_z_2, y_2Expr, y_1Expr :: Expr
-interpY :: Expr
+{-interpY-}
 
-iVal = (FCall (indInSeq) [C z_array, C z])
-x_z_1 = (FCall (matrixCol)[C x_array, C i]){-(C x_array, C i) -> leads to (Expr, Expr) -> error since FCall expects an Expr; is it a typo from original file-}
-y_z_1 = (FCall (matrixCol)[C y_array, C i])
-x_z_2 = (FCall (matrixCol)[C x_array, (C i) + 1])
-y_z_2 = (FCall (matrixCol)[C y_array, (C i) + 1])
-j = FCall (indInSeq) [x_z_1, C x]
-k = FCall (indInSeq) [x_z_2, C x]
+iVal, x_z_1, y_z_1, x_z_2, y_z_2, y_2Expr, y_1Expr, interpY :: Expr
+
+iVal    = FCall (indInSeq)   [C z_array, C z]
+x_z_1   = FCall (matrixCol)  [C x_array, C i]
+y_z_1   = FCall (matrixCol)  [C y_array, C i]
+x_z_2   = FCall (matrixCol)  [C x_array, (C i) + 1]
+y_z_2   = FCall (matrixCol)  [C y_array, (C i) + 1]
+j       = FCall (indInSeq)   [x_z_1, C x]
+k       = FCall (indInSeq)   [x_z_2, C x]
 y_1Expr = FCall (lin_interp) [x_z_1{-[C j]-},      y_z_1{-[C j]-}, x_z_1{-[C j + 1] -},       y_z_1{-[C j + 1] -}, C x]
 y_2Expr = FCall (lin_interp) [x_z_2{-[k]-},        y_z_2{-[C k]-}, x_z_2{-[k + 1]-},          y_z_2{-[k + 1]-},    C x]
 interpY = FCall (lin_interp) [C z_array {-!!(i)-}, y_1Expr,        C z_array{-!!(iVal + 1)-}, y_2Expr,             C z]
 
 ---
 
+{-interpZ-}
+
+interpZ :: Expr
+
 --for i in range(len(z_array) - 1):
---x_z_1 = (FCall (matrixCol)[C x_array, C i])
---y_z_1 = (FCall (matrixCol)[C y_array, C i])
---x_z_2 = (FCall (matrixCol)[C x_array, (C i) + 1])
---y_z_2 = (FCall (matrixCol)[C y_array, (C i) + 1])
---j = FCall (indInSeq) [x_z_1, C x]
---k = FCall (indInSeq) [x_z_2, C x]
-y_lower = FCall (lin_interp) [x_z_1{-[C j]-},      y_z_1{-[C j]-}, x_z_1{-[C j + 1] -},       y_z_1{-[C j + 1] -}, C x] --same as y_1Expr
-y_upper = FCall (lin_interp) [x_z_2{-[k]-},        y_z_2{-[C k]-}, x_z_2{-[k + 1]-},          y_z_2{-[k + 1]-},    C x] --same as y_2Expr
-interpZ = FCall (lin_interp) [y_lower, C z_array {-!!(i)-}, y_upper, C z_array{-!!(iVal + 1)-}, C y]
+interpZ = FCall (lin_interp) [y_1Expr, C z_array {-!!(i)-}, y_2Expr, C z_array{-!!(iVal + 1)-}, C y]

5751d0c8640e407f1117e7d074dce76ecd2a5d38
Maryyam Niazi <niazim3@mcmaster.ca>
2017-07-18 10:23:32 -0400

    pulled out data for Python cleanup of derived values for glassbr

--- CaseStudies/glass/Implementations/Python_Simplified/OldTests/testDerivedValues/derivedValTests.py
+++ CaseStudies/glass/Implementations/Python_Simplified/OldTests/testDerivedValues/derivedValTests.py
@@ -22,3 +22,27 @@ class DerivedValTests(unittest.TestSuite):
       
     if derivedVal_failures > 0:
         print(testResult.failures)
+
+
+"""
+    def setup(self):
+        pass
+    
+    def test_derived_params(self):
+        params = param.Param()
+        inputFormat.get_input(os.path.join("Test/Inputfiles", inputFileName), params)
+        derivedValues.derived_params(params)
+        self.assertEqual(arExpected, params.asprat)
+        self.assertEqual(sdExpected, params.sd)
+        self.assertEqual(ldfExpected, params.ldf)
+        self.assertEqual(wtntExpected, params.wtnt)
+        self.assertEqual(hExpected, params.h)
+        self.assertEqual(gtfExpected, params.gtf)
+
+"""
+"""
+testFileName      , inputFileName     , arExpected        , sdExpected        , ldfExpected       , wtntExpected, hExpected, gtfExpected
+testDerivedValues , "defaultInput.txt", 1.0666666666666667, 11.10180165558726 , 0.2696493494752911, 10.0        , 9.02     , 2
+testDerivedValues2, "testInput1.txt"  , 1.2               , 10.198039027185569, 0.2696493494752911, 10.0        , 7.42     , 1
+testDerivedValues3, "testInput2.txt"  , 1.25              , 9.093404203047394 , 0.2696493494752911, 15.0        , 9.02     , 4
+"""

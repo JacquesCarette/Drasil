3ef7e27547a65babe40dab83aa11c1aedab445e3
Maryyam Niazi <niazim3@mcmaster.ca>
2017-07-19 10:54:54 -0400

    added files for InputFormat testing; working on getting rid of errors; #347

---
+++ CaseStudies/glass/Implementations/Python_Simplified/Test/testInputFormat.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+from Implementation import param
+from Implementation import inputFormat
+
+class TestInputFormat(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+
+    def test_get_input(self):
+        with open(os.path.join("Test/Inputfiles", "inputFormat.txt"), 'r') as f:
+            input = f.readlines()
+        input = list(map(lambda x: x.split(","), input))
+        self.numTests = len(input)
+        self.inputFileName    = [row[0]        for row in input]
+        self.aExpctd          = [float(row[1]) for row in input]
+        self.bExpctd          = [float(row[2]) for row in input]
+        self.tExpctd          = [float(row[3]) for row in input]
+        self.gtExpctd         = [row[4]        for row in input]
+        self.wExpctd          = [float(row[5]) for row in input]
+        self.tntExpctd        = [float(row[6]) for row in input]
+        self.sdxExpctd        = [float(row[7]) for row in input]
+        self.sdyExpctd        = [float(row[8]) for row in input]
+        self.sdzExpctd        = [float(row[9]) for row in input]
+        self.pbTolExpctd      = [float(row[10].rstrip()) for row in input]
+        self.params           = [param.Param() for i in range(self.numTests)]
+
+        for i in range(self.numTests):
+            inputFormat.get_input(os.path.join("Test/Inputfiles", self.inputFileName[i]), self.params[i])
+
+        self.assertEqual(self.aExpctd[i], self.params[i])
+        self.assertEqual(self.bExpctd[i], params.b)
+        self.assertEqual(self.tExpctd[i], params.t)
+        self.assertEqual(self.gtExpctd[i], params.gt)
+        self.assertEqual(self.wExpctd[i], params.w)
+        self.assertEqual(self.tntExpctd[i], params.tnt)
+        self.assertEqual((self.sdxExpctd[i], self.sdyExpctd, self.sdzExpctd), params.sdvect)
+        self.assertEqual(self.pbTolExpctd[i], params.pbtol)
+        
+if __name__ == '__main__':
+    unittest.main()

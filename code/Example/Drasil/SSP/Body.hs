3d6dad7e23b41335ffb285d49b8ef5aa4fb5cbcd
njericha <njericha@edu.uwaterloo.ca>
2017-07-19 09:44:13 -0400

    Created SSP/References.hs to de-embed body

--- code/Example/Drasil/SSP/Body.hs
+++ code/Example/Drasil/SSP/Body.hs
@@ -14,6 +14,7 @@ import Drasil.SSP.Defs
 import Drasil.SSP.GenDefs
 import Drasil.SSP.IMods
 import Drasil.SSP.Modules
+import Drasil.SSP.References
 import Drasil.SSP.Reqs
 import Drasil.SSP.Requirements
 import Drasil.SSP.TMods
@@ -52,8 +53,7 @@ s4_1, s4_1_1, s4_1_2,
 
 s4_1_1_list, s4_1_2_p1, s4_1_2_bullets,
   s4_1_2_p2, s4_1_3_list, s4_2_1_list,
-  s4_2_5_p2, s4_2_5_p3, s5_1_list,
-  s8_list :: Contents
+  s4_2_5_p2, s4_2_5_p3, s5_1_list :: Contents
 
 s4_2_2_tmods, s4_2_3_genDefs, s4_2_4_dataDefs, s4_2_5_IMods :: [Contents]
 
@@ -402,32 +402,4 @@ s6 = SRS.likeChg [] []
 s7 = valsOfAuxConstantsF ssa []
 
 -- References --
-s8 = SRS.reference [s8_list] []
-
-s8_list = mkRefsList 1 [ --FIXME: names should be in italics
-  S "Q.H. Qian D.Y. Zhu, C.F. Lee and G.R. Chen. A concise algorithm for computing" +:+
-            S "the factor of safety using the morgensternprice method. Can. Geotech. J.," +:+.
-            S "(42):272-278, 19 February 2005",
-  S "D.G. Fredlund and J.Krahn. Comparison of slope stability methods of" +:+.
-            phrase analysis +:+. S "Can. Geotech. J., (14):429-439, 4 April 1977",
-  S "Nirmitha Koothoor. A document drive approach to certifying" +:+.
-            phrase sciCompS +:+ S "Master's thesis, McMaster University," +:+.
-            S "Hamilton, Ontario, Canada, 2013",
-  S "David L. Parnas and P.C. Clements. A rational design process: How" +:+
-            S "and why to fake it. IEEE Transactions on Software Engineering," +:+.
-            S "12(2):251-257, February 1986",
-  S "W. Spencer Smith and Lei Lai. A new requirements template for" +:+
-            S "scientific computing. In J. Ralyt" :+: (F Acute 'e') `sC` S "P. Agerfalk, and N. Kraiem" `sC`
-            S "editors, Proceedings of the First International Workshopon" +:+
-            S "Situational Requirements Engineering Processes - Methods," +:+
-            S "Techniques and Tools to Support Situation-Specific Requirements" +:+
-            S "Engineering Processes, SREP'05, pages 107-121, Paris, France" `sC`
-            S "2005. In conjunction with 13th IEEE International Requirements" +:+.
-            S "Engineering Conference",
-  S "Dieter Stolle and Peijun Guo. Limit equilibrum" +:+ phrase ssa +:+.
-            S "using rigid finite elements. Can. Geotech. J., (45):653-662, 20 May 2008",
-  S "Tony L.T Zhan Dao-Sheng Ling Yu-Chao Li, Yun-Min Chen and" +:+
-            S "Peter John Cleall. An efficient approach for locating the" +:+
-            phrase crtSlpSrf +:+ S "in" +:+ plural ssa +:+ S "using a" +:+
-            S "real-coded genetic algorithm. Can. Geotech. J., (47):806-820," +:+.
-            S "25 June 2010"]
+s8 = SRS.reference [sspReferences] []

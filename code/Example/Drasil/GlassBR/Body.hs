c869089ad89e74f86db2e436e7c5a82675dd70c3
Maryyam Niazi <niazim3@mcmaster.ca>
2017-07-19 10:12:08 -0400

    moved 'References' to separate GlassBR\References file

--- code/Example/Drasil/GlassBR/Body.hs
+++ code/Example/Drasil/GlassBR/Body.hs
@@ -13,7 +13,6 @@ import Data.Drasil.Concepts.Thermodynamics (degree_')
 import Data.Drasil.Concepts.Software
 import Data.Drasil.Concepts.Math (graph, calculation, probability,
   parameter, surface, equation, shape)
-import Data.Drasil.Concepts.Thermodynamics (heat)
 import Prelude hiding (id)
 import Data.Drasil.Utils
 import Data.Drasil.SentenceStructures
@@ -33,6 +32,7 @@ import Drasil.GlassBR.Reqs
 import Drasil.GlassBR.TMods
 import Drasil.GlassBR.IMods
 import Drasil.GlassBR.DataDefs
+import Drasil.GlassBR.References
 
 import Drasil.DocumentLanguage
 import Drasil.Sections.TraceabilityMandGs
@@ -53,7 +53,7 @@ s3, s4, s5,
 s5_1_table, s5_2_bullets, 
   s6_1_2_list, s6_2_intro, s6_2_5_table1, 
   s6_2_5_table2, s7_2_intro, s9_table1,
-  s9_table2, s9_table3, s11_list, s12_intro,
+  s9_table2, s9_table3, s12_intro, 
   fig_glassbr, fig_2, fig_3, fig_4, fig_5,
   fig_6 :: Contents
 
@@ -843,47 +843,6 @@ s10 = valsOfAuxConstantsF gLassBR auxiliaryConstants
 {--REFERENCES--}
 
 s11 = SRS.reference [s11_list] []
-
-s11_list = mkRefsList 1 (map (foldlsC) references)
-
-s11_ref1, s11_ref2, s11_ref3, s11_ref4, s11_ref5, s11_ref6 :: [Sentence]
-
-references :: [[Sentence]]
-references = [s11_ref1, s11_ref2, s11_ref3, s11_ref4, s11_ref5, s11_ref6]
-
-s11_ref1 = [S "N. Koothoor",
-  Quote (S "A" +:+ phrase document +:+ S "drive approach to certifying" 
-  +:+ phrase sciCompS :+: S ",") +:+ S "Master's thesis", 
-  S "McMaster University, Hamilton, Ontario, Canada", S "2013."]
-
-s11_ref2 = [S "W. S. Smith and L. Lai", 
-  Quote (S "A new requirements template for scientific computing,")
-  +:+ S "in Proceedings of the First International Workshop on Situational Requirements" +:+ 
-  S "Engineering Processes - Methods, Techniques and Tools to Support Situation-Specific Requirements" +:+
-  S "Engineering Processes, SREP'05"
-  +:+ sParen (S "J.Ralyt" :+: (F Acute 'e') `sC` S "P.Agerfalk, and N.Kraiem, eds."),
-  sParen (S "Paris, France"), S "pp. 107-121", 
-  S "In conjunction with 13th IEEE International Requirements Engineering Conference",
-  S "2005."]
-  --FIXME:Make a compoundNC "requirement template"?
-
-s11_ref3 = [S "J. Robertson and S. Robertson", 
-  Quote (S "Volere requirements specification template edition 16.") +:+
-  Quote (S "www.cs.uic.edu/ i442/VolereMaterials/templateArchive16/c Volere template16.pdf"),
-  S "2012."]
-  --FIXME:Make a compoundNC "requirement specification template"?
-
-s11_ref4 = [S "ASTM Standards Committee",
-  Quote (S "Standard practice for determining load resistance of glass in buildings,")
-  +:+ S "Standard E1300-09a", S "American Society for Testing and Material (ASTM)",
-  S "2009."]
-
-s11_ref5 = [S "ASTM", S "developed by subcommittee C1408", S "Book of standards 15.02",
-  Quote (S "Standard" +:+ phrase specification +:+. S "for flat glass, C1036")]
-
-s11_ref6 = [S "ASTM", S "developed by subcommittee C14.08", S "Book of standards 15.02",
-  Quote (at_start specification +:+ S "for" +:+ plural heat +:+.
-  S "treated flat glass-Kind HS, kind FT coated and uncoated glass, C1048")]
 
 {--APPENDIX--}
 

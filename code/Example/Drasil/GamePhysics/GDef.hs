b0e8df871a9fa1f9f6bed01d854d446b0bd4a56f
hmadej <hmadej@gmail.com>
2017-07-26 14:41:36 -0400

    more gendefs

--- code/Example/Drasil/GamePhysics/GDef.hs
+++ code/Example/Drasil/GamePhysics/GDef.hs
@@ -75,3 +75,35 @@ accelerationDueToGravityGDef = makeRC "accelDueToGrav"
   (nounPhraseSP "Acceleration due to gravity") 
   accelerationDueToGravityDesc accelerationDueToGravityRel
 
+accelerationDueToGravityRel :: Relation
+accelerationDueToGravityRel = FCall (C thFluxVect) [C QP.time] := C htTransCoeff :*
+  FCall (C temp_diff) [C QP.time] -- replace with proper Expr
+
+accelerationDueToGravityDesc :: Sentence
+accelerationDueToGravityDesc = foldlSent [S ""]
+
+-- [forceDueToGravity, mass, gravityConst]
+
+accelerationDueToGravityDeriv :: Sentence
+accelerationDueToGravityDeriv = foldlSent [S "From Newton's law of universal",
+  S "gravitation (T3 ref), we have:",
+  S "(expr1)",
+  S "Equation 3 **ref to 3** governs the gravitational attraction between two",
+  S "bodies. Suppose that one of the bodies is significantly more massive than",
+  S "other, so that we concern ourselves with the force the massive body exerts",
+  S "on the lighter body" +:+. S "Further suppose that the coordinate system is",
+  S "chosen such that this force acts on a line which lies along one of the",
+  S "principle axes (A2 ref)" +:+. S "Then our unit vector", S "(expr2)", S "for",
+  S "the x or y axes (A3 ref), respectively"
+  S "Given the above assumptions, let M and m be the mass of the massive and",
+  S "light body, respectively" +:+. S "Using 3 **ref to 3** and equating this",
+  S "with Newton's second law (T1 ref) for the force experienced by the light",
+  S "body, we get:",
+  S "(expr3)",
+  S "where (gravity symbol) is (phrase gravity accel). Dividing 4 **ref to 4**",
+  S "by m, and resolving this into separate x and y components:",
+  S "(expr4)",
+  S "(expr5)",
+  S "Thus:",
+  S "(expr6)"
+  ]

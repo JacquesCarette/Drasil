b71a0c038e8898fb6763b986df9b0cc816e9bc01
Maryyam Niazi <niazim3@mcmaster.ca>
2017-07-31 15:56:17 -0400

    updated output of sets to use 'Epsilon_L' instead of word 'in'

--- code/Data/Drasil/SentenceStructures.hs
+++ code/Data/Drasil/SentenceStructures.hs
@@ -175,7 +175,7 @@ eqN n = phrase equation +:+ sParen (S $ show n)
 
 --Produces a sentence that displays the constraints in a {}.
 displayConstrntsAsSet :: SymbolForm a => a -> [String] -> Sentence
-displayConstrntsAsSet ch listOfVals = getS ch `sIn` (sCurlyBr (foldlsC (map S listOfVals)))
+displayConstrntsAsSet ch listOfVals = getS ch +:+ (G Epsilon_L) +:+ (sCurlyBr (foldlsC (map S listOfVals)))
 
 extrctStrng :: Sentence -> String
 extrctStrng (S strng) = strng

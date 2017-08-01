285b402cdd8276cc6532297677ec395dc5a34446
hmadej <hmadej@gmail.com>
2017-08-01 16:02:52 -0400

    more function definitions

--- code/Example/Drasil/GamePhysics/Vector.hs
+++ code/Example/Drasil/GamePhysics/Vector.hs
@@ -3,7 +3,7 @@ module Drasil.Gamephys.Vector where
 import Language.Drasil
 
 v_v1, v_v2, v_x, v_y, v_v, v_s, v_length, v_dist, v_DBL_MIN, v_v1_x,
-  v_v1_y, v_v2_x, v_v2_y :: VarChunk
+  v_v1_y, v_v2_x, v_v2_y, v_rad :: VarChunk
 
 v_v1      = makeVC "v1"      (nounPhrase "v1")      (sub (lV) (Atomic "1"))
 v_v2      = makeVC "v2"      (nounPhrase "v2")      (sub (lV) (Atomic "2"))
@@ -18,7 +18,7 @@ v_v1_x    = makeVC "v1_x"    (nounPhrase "v1_x")    (Atomic "v1_x")
 v_v1_y    = makeVC "v1_y"    (nounPhrase "v1_y")    (Atomic "v1_y")
 v_v2_x    = makeVC "v2_x"    (nounPhrase "v2_x")    (Atomic "v2_x")
 v_v2_y    = makeVC "v2_y"    (nounPhrase "v2_y")    (Atomic "v2_y")
-
+v_rad     = makeVC "v_rad"   (nounPhrase "rad" )    (Atomic "rad") 
 
 vectEqual, vectAdd, vectSub, vectMult, vectNeg, vectDot, vectCross, vectPerp,
   vectRPerp, vectProject, vectForAngle, vectToAngle, vectRotate, vectUnrotate,
@@ -115,5 +115,31 @@ vectProject = funcDef "vectProject" [v_v1, v_v2] vector
       ]
   ]
 
+vectForAngle = funcDef "vectForAngle" [rad] vector
+  [
+    FRet (FCall (asExpr vect)
+      [
+        (FCall (asExpr cos) [rad]),
+        (FCall (asExpr sin) [rad])
+      ]
+  ]
 
+vectToAngle = funcDef "vectToAngle" [v_v] Rational
+  [
+    FRet (FCall (asExpr atan2) 
+      [
+        (FCall (asExpr getY) [v_v]),
+        (FCall (asExpr getX) [v_v]
+      ]
+  ]
 
+vectRotate = funcDec "vectRotate" [v_v1, v_v2] vector
+  [
+    FRet (FCall (asExpr vect)
+      [
+        ((FCall (asExpr getX) [v_v1]) * (FCall (asExpr getX) [v_v2])) -
+        ((FCall (asExpr getY) [v_v1]) * (FCall (asExpr getY) [v_v2])),
+        ((FCall (asExpr getX) [v_v1]) * (FCall (asExpr getY) [v_v2]) +
+        ((FCall (asExpr getY) [v_v1]) * (FCall (asExpr getX) [v_v2]))
+      ]
+  ]        

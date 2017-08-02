2e08236eff466975fb2c678fee7c53eaab479a4e
Maryyam Niazi <niazim3@mcmaster.ca>
2017-08-02 10:58:56 -0400

    unsuccessful attempt at fixing output from TAandA for glassbr

--- code/Language/Drasil/Chunk/Wrapper.hs
+++ code/Language/Drasil/Chunk/Wrapper.hs
@@ -15,10 +15,16 @@ data NWrapper where
   
 instance Chunk NWrapper where
   id = nlens id
-  
+instance Ord NWrapper where
+  compare a b = compare (a ^. id) (b ^. id) --Not ideal?
+  --compare a b = compare (abrv a) (abrv b)
 instance NamedIdea NWrapper where
   term = nlens term
-  getA (NW a) = getA a
+  getA (NW a) = getA a  
+instance Eq NWrapper where
+  a == b = (a ^. id) == (b ^. id)
+
+--abrv (NC _ _ c) = c --> cannot acces due to being wrapped by NWrapper
 
 -- | NamedIdea Wrapper constructor.
 -- Used to wrap different NamedIdeas, so they will have the same type.
@@ -30,8 +36,3 @@ nw = NW
 nlens :: (forall c. (NamedIdea c) => 
   Simple Lens c a) -> Simple Lens NWrapper a
 nlens l f (NW a) = fmap (\x -> NW (set l x a)) (f (a ^. l))
-
-instance Eq NWrapper where
-  a == b = (a ^. id) == (b ^. id)
-
-

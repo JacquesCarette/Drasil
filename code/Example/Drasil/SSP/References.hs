2316cd2d2ff4aacec2870aea1fe8512c982bb742
njericha <njericha@edu.uwaterloo.ca>
2017-08-04 15:57:16 -0400

    Cleaned up References.hs, updated stable

--- code/Example/Drasil/SSP/References.hs
+++ code/Example/Drasil/SSP/References.hs
@@ -5,7 +5,6 @@ import Drasil.SSP.Defs (ssa, crtSlpSrf, fs_concept)
 
 import Data.Drasil.Concepts.Documentation (analysis)
 import Data.Drasil.Software.Products (sciCompS)
-import Data.Drasil.Utils (mkRefsList)
 
 sspCitations :: BibRef
 sspCitations = [chen2005, parnas1986, koothoor2013, fredlund1977, smith2005, stolle2008, li2010]
@@ -17,8 +16,9 @@ chen2005 = Article [
           personWM' "D" ["Y"] "Zhu",
           personWM' "C" ["F"] "Lee",
           personWM' "G" ["R"] "Chen"],
-  Title (S "A concise algorithm for computing the" +:+
-         phrase fs_concept +:+ S "using the morgenstern price method"),
+  Title $ S "A concise algorithm for computing the" +:+
+          phrase fs_concept +:+ S "using the" +:+
+          S "morgenstern price method",
   journalCGJ,
   Volume 42,
   Issue 1,
@@ -40,7 +40,7 @@ parnas1986 = Article [
 koothoor2013 = MThesis [
   Author [person "Nirmitha" "Koothoor"],
   Title $ S "A document drive approach to certifying"
-        +:+ phrase sciCompS,
+          +:+ phrase sciCompS,
   School $ S "McMaster University",
   Place (S "Hamilton", S "Canada"),
   Year 2013]
@@ -48,8 +48,8 @@ koothoor2013 = MThesis [
 fredlund1977 = Article [
   Author [personWM "D" ["G"] "Fredlund",
           person "J" "Krahn"],
-  Title $ S "Comparison of slope stability methods of" +:+
-         phrase analysis,
+  Title $ S "Comparison of slope stability methods of"
+          +:+ phrase analysis,
   journalCGJ,
   Volume 14,
   Issue 3,
@@ -71,9 +71,8 @@ smith2005 = Article [
         S "Techniques and Tools to Support Situation-Specific Requirements" +:+
         S "Engineering Processes." +:+
         --FIXME: The above section is part of the citation but not the note
-        S "In conjunction with 13th IEEE International Requirements" +:+.
-        S "Engineering Conference")
-  ]
+        S "In conjunction with 13th IEEE International" +:+.
+        S "Requirements Engineering Conference")]
 
 stolle2008 = Article [
   Author [person "Dieter" "Stolle",
@@ -93,8 +92,8 @@ li2010 = Article [
           person' "Sao-Sheng" "Ling",
           personWM "Peter" ["John"] "Cleall"],
   Title $ S "An efficient approach for locating the" +:+
-          phrase crtSlpSrf +:+ S "in" +:+ plural ssa +:+ S "using a" +:+
-          S "real-coded genetic algorithm",
+          phrase crtSlpSrf +:+ S "in" +:+ plural ssa +:+
+          S "using a real-coded genetic algorithm",
   journalCGJ,
   Volume 47,
   Issue 7,
@@ -104,32 +103,3 @@ li2010 = Article [
 
 journalCGJ :: CiteField
 journalCGJ = Journal $ S $ "Canadian Geotechnical Journal"
-
-sspReferences :: Contents
-sspReferences = mkRefsList 1 [
-  S "Q.H. Qian D.Y. Zhu, C.F. Lee and G.R. Chen. A concise algorithm for computing" +:+
-            S "the factor of safety using the morgensternprice method. Can. Geotech. J.," +:+.
-            S "(42):272-278, 19 February 2005",
-  S "D.G. Fredlund and C.Krahn. Comparison of slope stability methods of" +:+.
-            phrase analysis +:+. S "Can. Geotech. J., (14):429-439, 4 April 1977",
-  S "Nirmitha Koothoor. A document drive approach to certifying" +:+.
-            phrase sciCompS +:+ S "Master's thesis, McMaster University," +:+.
-            S "Hamilton, Ontario, Canada, 2013",
-  S "David L. Parnas and P.C. Clements. A rational design process: How" +:+
-            S "and why to fake it. IEEE Transactions on Software Engineering," +:+.
-            S "12(2):251-257, February 1986",
-  S "W. Spencer Smith and Lei Lai. A new requirements template for" +:+
-            S "scientific computing. In J. Ralyt" :+: (F Acute 'e') `sC` S "P. Agerfalk, and N. Kraiem" `sC`
-            S "editors, Proceedings of the First International Workshopon" +:+
-            S "Situational Requirements Engineering Processes - Methods," +:+
-            S "Techniques and Tools to Support Situation-Specific Requirements" +:+
-            S "Engineering Processes, SREP'05, pages 107-121, Paris, France" `sC`
-            S "2005. In conjunction with 13th IEEE International Requirements" +:+.
-            S "Engineering Conference",
-  S "Dieter Stolle and Peijun Guo. Limit equilibrum" +:+ phrase ssa +:+.
-            S "using rigid finite elements. Can. Geotech. J., (45):653-662, 20 May 2008",
-  S "Tony L.T Zhan Dao-Sheng Ling Yu-Chao Li, Yun-Min Chen and" +:+
-            S "Peter John Cleall. An efficient approach for locating the" +:+
-            phrase crtSlpSrf +:+ S "in" +:+ plural ssa +:+ S "using a" +:+
-            S "real-coded genetic algorithm. Can. Geotech. J., (47):806-820," +:+.
-            S "25 June 2010"]

29999fb5ac1970e8fd1ab49d23597fc75a27212a
njericha <njericha@edu.uwaterloo.ca>
2017-08-04 15:26:51 -0400

    Added all citations to SSP

--- code/Example/Drasil/SSP/References.hs
+++ code/Example/Drasil/SSP/References.hs
@@ -8,9 +8,9 @@ import Data.Drasil.Software.Products (sciCompS)
 import Data.Drasil.Utils (mkRefsList)
 
 sspCitations :: BibRef
-sspCitations = [chen2005, parnas1986, koothoor2013]
+sspCitations = [chen2005, parnas1986, koothoor2013, fredlund1977, smith2005, stolle2008, li2010]
 
-chen2005, parnas1986, koothoor2013 :: Citation
+chen2005, parnas1986, koothoor2013, fredlund1977, smith2005, stolle2008, li2010 :: Citation
 --See Language.Drasil.People for all person constructors
 chen2005 = Article [
   Author [personWM' "Q" ["H"] "Qian",
@@ -19,8 +19,9 @@ chen2005 = Article [
           personWM' "G" ["R"] "Chen"],
   Title (S "A concise algorithm for computing the" +:+
          phrase fs_concept +:+ S "using the morgenstern price method"),
-  Journal (S "Can. Geotech. J."),
-  Issue 42,
+  journalCGJ,
+  Volume 42,
+  Issue 1,
   Date 19 Feb 2005,
   Pages (272,278)]
   
@@ -33,9 +34,8 @@ parnas1986 = Article [
   Volume 12,
   Issue 2,
   Pages (251,257),
-  Year 1986,
-  Place (S "Washington", S "USA")
-  ]
+  Year 1986, --February, but day unknown
+  Place (S "Washington", S "USA")]
 
 koothoor2013 = MThesis [
   Author [person "Nirmitha" "Koothoor"],
@@ -43,15 +43,74 @@ koothoor2013 = MThesis [
         +:+ phrase sciCompS,
   School $ S "McMaster University",
   Place (S "Hamilton", S "Canada"),
-  Year 2013  
+  Year 2013]
+
+fredlund1977 = Article [
+  Author [personWM "D" ["G"] "Fredlund",
+          person "J" "Krahn"],
+  Title $ S "Comparison of slope stability methods of" +:+
+         phrase analysis,
+  journalCGJ,
+  Volume 14,
+  Issue 3,
+  Date 4 Apr 1977,
+  Pages (429,439)]
+
+smith2005 = Article [
+  Author [personWM "W" ["Spencer"] "Smith",
+          person "Lei" "Lai"],
+  Title $ S "A new requirements template for" +:+
+          S "scientific computing",
+  Journal $ S "SREP",
+  Pages (107,121),
+  Year 2005,
+  Place (S "Paris", S "France"),
+  Note (S "In J. Ralyt" :+: (F Acute 'e') `sC` S "P. Agerfalk, and N. Kraiem" `sC`
+        S "editors, Proceedings of the First International Workshopon" +:+
+        S "Situational Requirements Engineering Processes - Methods," +:+
+        S "Techniques and Tools to Support Situation-Specific Requirements" +:+
+        S "Engineering Processes." +:+
+        --FIXME: The above section is part of the citation but not the note
+        S "In conjunction with 13th IEEE International Requirements" +:+.
+        S "Engineering Conference")
   ]
-  
+
+stolle2008 = Article [
+  Author [person "Dieter" "Stolle",
+          person "Peijun" "Guo"],
+  Title $ S "Limit equilibrum" +:+ phrase ssa +:+
+          S "using rigid finite elements",
+  journalCGJ,
+  Volume 45,
+  Issue 5,
+  Date 20 May 2008,
+  Pages (653,662)]
+
+li2010 = Article [
+  Author [person' "Yu-Chao" "Li",
+          person' "Yun-Min" "Chen",
+          personWM "Tony" ["L","T"] "Zhan",
+          person' "Sao-Sheng" "Ling",
+          personWM "Peter" ["John"] "Cleall"],
+  Title $ S "An efficient approach for locating the" +:+
+          phrase crtSlpSrf +:+ S "in" +:+ plural ssa +:+ S "using a" +:+
+          S "real-coded genetic algorithm",
+  journalCGJ,
+  Volume 47,
+  Issue 7,
+  Date 25 Jun 2010,
+  Pages (806,820)]
+
+
+journalCGJ :: CiteField
+journalCGJ = Journal $ S $ "Canadian Geotechnical Journal"
+
 sspReferences :: Contents
-sspReferences = mkRefsList 1 [ --FIXME: names should be in italics
+sspReferences = mkRefsList 1 [
   S "Q.H. Qian D.Y. Zhu, C.F. Lee and G.R. Chen. A concise algorithm for computing" +:+
             S "the factor of safety using the morgensternprice method. Can. Geotech. J.," +:+.
             S "(42):272-278, 19 February 2005",
-  S "D.G. Fredlund and J.Krahn. Comparison of slope stability methods of" +:+.
+  S "D.G. Fredlund and C.Krahn. Comparison of slope stability methods of" +:+.
             phrase analysis +:+. S "Can. Geotech. J., (14):429-439, 4 April 1977",
   S "Nirmitha Koothoor. A document drive approach to certifying" +:+.
             phrase sciCompS +:+ S "Master's thesis, McMaster University," +:+.

8929750e65f96c6766d45a3905ea49578fb52b7d
Daniel Scime <danscime@gmail.com>
2017-08-07 14:29:50 -0400

    Making SWHS imports more precise.

--- code/Example/Drasil/SWHS/Modules.hs
+++ code/Example/Drasil/SWHS/Modules.hs
@@ -6,12 +6,16 @@ import Language.Drasil (ModuleChunk, ConceptChunk,
   (+:+), phrase, plural, cn', (+:+.), sC, dccWDS, short, makeImpModule,
   Sentence (S, (:+:)))
 import Drasil.SWHS.Concepts (swhsProg)
-import Data.Drasil.Concepts.Software
-import Data.Drasil.Concepts.Math hiding (constraint)
-import Data.Drasil.Concepts.Computation
-import Data.Drasil.Concepts.Documentation
-import Data.Drasil.Modules
-import Data.Drasil.Software.Products
+import Data.Drasil.Concepts.Software (modInputParam, modInputVerif,
+  modInputFormat)
+import Data.Drasil.Concepts.Math (parameter, equation, ode)
+import Data.Drasil.Concepts.Computation (algorithm)
+import Data.Drasil.Concepts.Documentation (input_, module_, datum,
+  output_, constraint, physical, software)
+import Data.Drasil.Modules (mod_plot_fun, mod_ode_fun, mod_hw,
+  mod_seq_fun, mod_ctrl_fun, mod_param_fun, mod_behav, mod_io_fun,
+  mod_outputf_desc_fun, mod_sw)
+import Data.Drasil.Software.Products (matlab)
 import Data.Drasil.Quantities.Thermodynamics (temp)
 import Data.Drasil.Quantities.Physics (energy, time)
 import Data.Drasil.Concepts.Thermodynamics (law_cons_energy, melting)
@@ -20,16 +24,6 @@ modules :: [ModuleChunk]
 modules = [mod_hw, mod_behav, mod_inputf, mod_inputp, mod_inputv, mod_outputf,
           mod_outputv, mod_temp, mod_ener, mod_ctrl, mod_sw, mod_seq,
           mod_ode, mod_plot]
-
--- HW Hiding Module
---mod_hw :: ModuleChunk
---mod_hw = makeImpModule hwHiding (S "The data structure and algorithm " :+:
---         S "used to implement the virtual hardware.") os [] [] [] Nothing
-
--- Behaviour Hiding Module
---mod_behav :: ModuleChunk
---mod_behav = makeUnimpModule modBehavHiding (S "The contents of the required" :+:
---            S " behaviours.") Nothing
 
 -- Input Format Module
 mod_inputf :: ModuleChunk
@@ -93,56 +87,19 @@ mod_ener = mod_param_fun swhsProg [mod_inputp, mod_seq]
   S "using the" +:+ phrase input_ +:+ plural parameter) mod_ener_desc
 
 -- Control Module
---mod_ctrl :: ModuleChunk
---mod_ctrl = makeImpModule modControl (S "The algorithm for coordinating " :+:
---           S "the running of the prograM.") swhsProg [] [] [M.mod_hw, mod_inputp,
---           mod_inputf, mod_inputv, mod_temp, mod_ener, mod_ode, mod_plot,
---           mod_outputv, mod_outputf, mod_seq] (Just M.mod_behav)
 mod_ctrl :: ModuleChunk
 mod_ctrl = mod_ctrl_fun (S "The" +:+ phrase algorithm)
   swhsProg [] [mod_hw, mod_inputp, mod_inputf, mod_inputv, mod_temp, mod_ener,
   mod_ode, mod_plot, mod_outputv, mod_outputf, mod_seq]
 
--- Software Decision Module
---mod_sw :: ModuleChunk
---mod_sw = makeUnimpModule modSfwrDecision (S "The design decision based on " :+:
---         S "mathematical theorems, physical facts, or programming " :+:
---         S "considerations. The secrets of this module are not described " :+:
---         S "in the " :+: (short srs) :+: S ".") Nothing
-
 -- Sequence Data Structure Module
---mod_seq_desc :: ConceptChunk
---mod_seq_desc = dccWDS "mod_seq_desc" (nounPhraseSP "sequence data structure")
---  (S "Provides array manipulation" :+:
---  S ", including building an array, accessing a specific entry" :+:
---  S ", slicing an array, etc.")
-
 mod_seq :: ModuleChunk
 mod_seq = mod_seq_fun matlab []
 
 -- ODE Solver Module
---mod_ode_desc :: ConceptChunk
---mod_ode_desc = dccWDS "mod_ode_desc" (nounPhraseSP "ODE solver") (
---  S "Provides solvers that take the governing equation, initial conditions," +:+
---  S "and numerical parameters, and solve them.")
-
---mod_ode :: ModuleChunk
---mod_ode = makeImpModule mod_ode_desc (S "The algorithm to solve a system of" :+:
---          S " first order " :+: (short ode) :+: S "s.") matlab [] []
---          [mod_seq] (Just mod_sw)
-
 mod_ode :: ModuleChunk
 mod_ode = mod_ode_fun matlab [mod_seq]
 
 -- Plotting Module
---mod_plot_desc :: ConceptChunk
---mod_plot_desc = dcc "mod_plot_desc" (nounPhraseSP "plotting")
---  "Provides a plot function."
-
---mod_plot :: ModuleChunk
---mod_plot = makeImpModule mod_plot_desc (S "The data structures and " :+:
---           S "algorithms for plotting data graphically.") matlab [] [] [mod_seq]
---           (Just mod_sw)
-
 mod_plot :: ModuleChunk
 mod_plot = mod_plot_fun matlab [mod_seq]

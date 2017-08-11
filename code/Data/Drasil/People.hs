d871a210bbb46bf0f865bc0a8d3aeacdabd80464
hmadej <hmadej@gmail.com>
2017-08-11 09:49:08 -0400

    added People, right now duplicate of authors

---
+++ code/Data/Drasil/People.hs
@@ -0,0 +1,17 @@
+module Data.Drasil.Authors where
+
+import Language.Drasil (Person, person, personWM)
+  
+spencerSmith, henryFrankis, nKoothoor, dParnas :: Person
+spencerSmith = personWM "W" ["Spencer"] "Smith"
+henryFrankis = person "Henry"   "Frankis"
+nKoothoor    = person "Nirmitha" "Koothoor"
+dParnas      = personWM "David" ["L"] "Parnas"
+  
+-- short versions for now
+luthfi, alex, nikitha, thulasi, brooks :: Person
+luthfi   = person "Luthfi"  "Mawarid"
+alex     = person "Alex"    "Halliwushka"
+nikitha  = person "Nikitha"  "Krithnan"
+thulasi  = person "Thulasi" "Jegatheesan"
+brooks   = person "Brooks"  "MacLachlan"

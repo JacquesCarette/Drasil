74fb7aa920c1d749f458d503282e9074a7d1f107
njericha <njericha@edu.uwaterloo.ca>
2017-08-14 11:03:19 -0400

    #373 Started SSP/DataDesc.hs framework

--- code/Example/Drasil/SSP/DataDesc.hs
+++ code/Example/Drasil/SSP/DataDesc.hs
@@ -1,12 +1,21 @@
 module Drasil.SSP.DataDesc where
 
 import Language.Drasil
-import Drasil.SSP.Unitals
+import Drasil.SSP.Unitals (elasticMod, cohesion, poissnsRatio, fricAngle, dryWeight, satWeight,
+  waterWeight)
 
 sspInputMod :: Mod
 sspInputMod = ModData "InputFormat" [sspInputData]
 
 sspInputData :: DataDesc
-sspInputData = [
-  
+sspInputData = [ {- --FIXME: unfinished. Needs more inputs? Needs way to think of (x,y) as two seperate things
+  --number of layers, layer direction
+  multiLine (straight $ map (listEntry [WithPattern]) [
+  fricAngle, cohesion, dryWeight, satWeight, elasticMod, poissnsRatio
+  ]) ' ',
+  --(x,y) co-ordinates, repeated and part of the multiline
+  {- the bellow three fields are optional-}
+  --number of geometry points
+  singleton waterWeight
+  --(x,y) water table geometry points -}
   ]

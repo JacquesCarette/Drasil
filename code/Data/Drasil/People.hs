3e8b0a048fb44b232683a237a0c4414ac5c9dc38
njericha <njericha@edu.uwaterloo.ca>
2017-08-14 09:38:02 -0400

    Sorted People in People.hs

--- code/Data/Drasil/People.hs
+++ code/Data/Drasil/People.hs
@@ -6,29 +6,30 @@ spencerSmith, henryFrankis, nKoothoor, dParnas, daAruliah, gWilson,
   cTitus, kdHuff, cHong, mDavis, rGuy, shdHaddock, imMitchell, mdPlumblet,
   bWaugh, epWhite, pWilson, pcClements, dDewitt, tBergman, aLavine,
   jBueche, fIncropera :: Person
-spencerSmith = personWM "W"         ["Spencer"]     "Smith"
-dParnas      = personWM "David"     ["L"]           "Parnas"
-daAruliah    = personWM "D"         ["A"]           "Aruliah"
-henryFrankis = person   "Henry"                     "Frankis"
-nKoothoor    = person   "Nirmitha"                  "Koothoor"
-gWilson      = person   "Greg"                      "Wilson"
-cTitus       = person   "C"                         "Titus"
-kdHuff       = personWM "Kathryn"   ["D"]           "Huff"
+bWaugh       = person   "Ben"                       "Waugh" -- Best Practices for Scientific Computing 2013
 cHong        = person   "Chue"                      "Hong"
+cTitus       = person   "C"                         "Titus"
+daAruliah    = personWM "D"         ["A"]           "Aruliah"
+dParnas      = personWM "David"     ["L"]           "Parnas"
+epWhite      = personWM "Ethan"     ["P"]           "White" -- Best Practices for Scientific Computing 2013
+gWilson      = person   "Greg"                      "Wilson"
+henryFrankis = person   "Henry"                     "Frankis"
+imMitchell   = personWM "Ian"       ["M"]           "Mitchell"
+kdHuff       = personWM "Kathryn"   ["D"]           "Huff"
 mDavis       = person   "Matt"                      "Davis"
+mdPlumblet   = personWM "Mark"      ["D"]           "Plumblet"
+nKoothoor    = person   "Nirmitha"                  "Koothoor"
+pcClements   = personWM "P"         ["C"]           "Clements" -- The Modular Structure of Complex Systems ICSE '84
+pWilson      = person   "Paul"                      "Wilson" -- Best Practices for Scientific Computing 2013
 rGuy         = personWM "Richard"   ["T"]           "Guy"
 shdHaddock   = personWM "Steven"    ["H","D"]       "Haddock"
-imMitchell   = personWM "Ian"       ["M"]           "Mitchell"
-mdPlumblet   = personWM "Mark"      ["D"]           "Plumblet"
-bWaugh       = person   "Ben"                       "Waugh" -- Best Practices for Scientific Computing 2013
-epWhite      = personWM "Ethan"     ["P"]           "White" -- Best Practices for Scientific Computing 2013
-pWilson      = person   "Paul"                      "Wilson" -- Best Practices for Scientific Computing 2013
-pcClements   = personWM "P"         ["C"]           "Clements" -- The Modular Structure of Complex Systems ICSE '84
+spencerSmith = personWM "W"         ["Spencer"]     "Smith"
 aLavine      = personWM "A"         ["S"]           "Lavine"
 tBergman     = personWM "T"         ["L"]           "Bergman"
 dDewitt      = personWM "D"         ["P"]           "Dewitt"
 fIncropera   = personWM "F"         ["P"]           "Incropera"
 jBueche      = personWM "J"         ["Frederick"]   "Bueche"
+
 
 -- short versions for now
 luthfi, alex, nikitha, thulasi, brooks, mLightstone, lLai,

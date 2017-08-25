ad7e040da1e60e2ca6a7ed7c6591584b03dd01da
Dan Szymczak <szymczdm@mcmaster.ca>
2017-08-25 17:21:47 -0400

    Fix the last few errors in Language.Drasil

--- code/Language/Drasil.hs
+++ code/Language/Drasil.hs
@@ -18,7 +18,7 @@ module Language.Drasil (
   , (^:), (/:), (*:), new_unit
   -- Chunk
   , Chunk(..), VarChunk(..), ConceptChunk
-  , makeVC, vc, makeVCObj, SymbolForm(..)
+  , makeVC, vc, makeVCObj, SymbolForm
   , dcc, dccWDS, dccWDS', cv, dcc', vc', vc'', ccs, cc, cc'
   , Quantity(..), QWrapper, qw, ConVar(..), cvR, cvRs
   , Concept(..), cw, CWrapper

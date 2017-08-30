3c8eea676d104e9b2cc27449cf6c2d63bca05ab9
Jacques Carette <carette@mcmaster.ca>
2017-08-30 13:17:57 -0400

    db in sysinfo, no longer needed for codegen'

--- code/Example/Drasil/GamePhysics/Body.hs
+++ code/Example/Drasil/GamePhysics/Body.hs
@@ -120,7 +120,7 @@ chipChoices = Choices {
 }
 
 chipCode :: CodeSpec
-chipCode = codeSpec' chipmunkSysInfo chipChoices [] everything
+chipCode = codeSpec' chipmunkSysInfo chipChoices []
 
 
 --FIXME: The SRS has been partly switched over to the new docLang, so some of

3b9588e0ba6216f718571fb0ab8511c2ed92576e
Steven Palmer <palmes4@mcmaster.ca>
2017-09-20 12:34:39 -0400

    Clean up and simplify nopcm python code

--- CaseStudies/SolarHeatingWaterOnly/Python/output.py
+++ CaseStudies/SolarHeatingWaterOnly/Python/output.py
@@ -1,22 +1,13 @@
-#Commented lines pending removal
-def output(params, time, tempW, eW, eTot, filename):#tempP, eW, eP, eTot, filename):
+def output(params, time, tempW, eW, eTot, filename):
     outputFilename = filename + 'out'
     f = open(outputFilename, 'w')
     f.write('\tL\t\t\t\t' + str(params.L) + '\n')
     f.write('\tdiam\t\t\t' + str(params.diam) + '\n')
-#    f.write('\tVp\t\t\t\t' + str(params.Vp) + '\n')
-#    f.write('\tAp\t\t\t\t' + str(params.Ap) + '\n')
-#    f.write('\trho_p\t\t\t' + str(params.rho_p) + '\n')
-#    f.write('\tTmelt\t\t\t' + str(params.Tmelt) + '\n')
-#    f.write('\tC_ps\t\t\t' + str(params.C_ps) + '\n')
-#    f.write('\tC_pl\t\t\t' + str(params.C_pl) + '\n')
-#    f.write('\tHf\t\t\t\t' + str(params.Hf) + '\n')
     f.write('\tAc\t\t\t\t' + str(params.Ac) + '\n')
     f.write('\tTc\t\t\t\t' + str(params.Tc) + '\n')
     f.write('\trho_w\t\t\t' + str(params.rho_w) + '\n')
     f.write('\tC_w\t\t\t\t' + str(params.C_w) + '\n')
     f.write('\thc\t\t\t\t' + str(params.hc) + '\n')
-#    f.write('\thp\t\t\t\t' + str(params.hp) + '\n')
     f.write('\tTinit\t\t\t' + str(params.Tinit) + '\n')
     f.write('\ttstep\t\t\t' + str(params.tstep) + '\n')
     f.write('\ttfinal\t\t\t' + str(params.tfinal) + '\n')
@@ -26,19 +17,13 @@ def output(params, time, tempW, eW, eTot, filename):#tempP, eW, eP, eTot, filena
     f.write('\tVt\t\t\t\t' + str(params.Vt) + '\n')
     f.write('\tMw\t\t\t\t' + str(params.Mw) + '\n')
     f.write('\ttau_w\t\t\t' + str(params.tau_w) + '\n')
-#    f.write('\teta\t\t\t\t' + str(params.eta) + '\n')
-#    f.write('\tMp\t\t\t\t' + str(params.Mp) + '\n')
-#    f.write('\ttau_ps\t\t\t' + str(params.tau_ps) + '\n')
-#    f.write('\ttau_pl\t\t\t' + str(params.tau_pl) + '\n')
-#    f.write('\tEpmelt_init\t\t' + str(params.Epmelt_init) + '\n')
-#   f.write('\tEp_melt3\t\t' + str(params.Ep_melt3) + '\n')
     f.write('\tMw_noPCM\t\t' + str(params.Mw_noPCM) + '\n')
     f.write('\ttau_w_noPCM\t\t' + str(params.tau_w_noPCM) + '\n')
-    results = zip(time, tempW, eW, eTot)#tempP, eW, eP, eTot)
+    results = zip(time, tempW, eW, eTot)
     max_width = max(len(str(result)) for category in results for result in category)
-    f.write('\n' + 'Time'.ljust(max_width+1) + 'Twater'.ljust(max_width+1) +# 'TPCM'.ljust(max_width+1) +
-            'Ewater'.ljust(max_width+1) + 'Etotal'.ljust(max_width+1) + '\n')#'EPCM'.ljust(max_width+1) + 'Etotal'.ljust(max_width+1) + '\n')
-    for t, Tw, Ew, Et in zip(time, tempW, eW, eTot):#Tp, Ew, Ep, Et in zip(time, tempW, tempP, eW, eP, eTot):
-        f.write(str(t).ljust(max_width+1) + str(Tw).ljust(max_width+1) +# str(Tp).ljust(max_width+1) +
-                str(Ew).ljust(max_width+1) + str(Et).ljust(max_width+1) + '\n')#str(Ep).ljust(max_width+1) + str(Et).ljust(max_width+1) + '\n')
+    f.write('\n' + 'Time'.ljust(max_width+1) + 'Twater'.ljust(max_width+1) +
+            'Ewater'.ljust(max_width+1) + 'Etotal'.ljust(max_width+1) + '\n')
+    for t, Tw, Ew, Et in zip(time, tempW, eW, eTot):
+        f.write(str(t).ljust(max_width+1) + str(Tw).ljust(max_width+1) +
+                str(Ew).ljust(max_width+1) + str(Et).ljust(max_width+1) + '\n')
     f.close()
--- CaseStudies/SolarHeatingWaterOnly/Python/verify_output.py
+++ CaseStudies/SolarHeatingWaterOnly/Python/verify_output.py
@@ -1,40 +1,28 @@
-#Commented lines pending removal
 import warnings
 
 warnings.simplefilter('always', UserWarning)
 
-def verify_output(time, tempW, eW, params):#tempP, eW, eP, params):
-    previous = time[0]
-    deltaTime = []
-    for element in time[1:]:
-        deltaTime = deltaTime + [element - previous]
-        previous = element
-    eCoil = []
-#    ePCM = []
-    eWater = []
-    prevWaterTemp = tempW[0]
-#    prevPCMTemp = tempP[0]
-    for timeStep, waterTemp in zip(deltaTime, tempW[1:]): #pcmTemp in zip(deltaTime, tempW[1:], tempP[1:]):
-        eCoilStep = params.hc * params.Ac * timeStep * (params.Tc - waterTemp + params.Tc - prevWaterTemp) / 2
-#        ePCMStep = params.hp * params.Ap * timeStep * (waterTemp - pcmTemp + prevWaterTemp - prevPCMTemp) / 2
-        eCoil = eCoil + [eCoilStep]
-#        ePCM = ePCM + [ePCMStep]
-        eWater = eWater + [eCoilStep]# - ePCMStep]
-        prevWaterTemp = waterTemp
-#        prevPCMTemp = pcmTemp
+def verify_output(t, Tw, Ew, params):
+  previous = t[0]
+  deltaTime = []
+  for element in t[1:]:
+      deltaTime = deltaTime + [element - previous]
+      previous = element
 
-    eWaterTotal = sum(eWater)
-#    ePCMTotal = sum(ePCM)
+  deltaEw = []
+  prevWaterTemp = Tw[0]
 
-    errorWater = abs(eWaterTotal - eW[-1]) / eW[-1] * 100
-#    errorPCM = abs(ePCMTotal - eP[-1]) / eP[-1] * 100
-    
-    if errorWater > params.ConsTol:
-        warnings.warn('There is > ' + str(params.ConsTol) + '% relative error between the energy in the water output' +
-                      ' and the expected output based on the law of conservation of energy.\n', UserWarning,
-                      stacklevel=2)
+  for timeStep, waterTemp in zip(deltaTime, Tw[1:]):
+    deltaE = params.hc * params.Ac * timeStep * ((params.Tc - waterTemp) + (params.Tc - prevWaterTemp)) / 2
+    deltaEw = deltaEw + [deltaE]
+    prevWaterTemp = waterTemp
 
-#    if errorPCM > params.ConsTol:
-#        warnings.warn('There is > ' + str(params.ConsTol) + '% relative error between the energy in the PCM output ' +
-#                      'and the expected output based on the law of conservation of energy.\n', UserWarning,
-#                      stacklevel=2)
+  Ew_tot = sum(deltaEw)
+  error = abs(Ew_tot - Ew[-1]) / Ew[-1] * 100
+  
+  print(error)
+  
+  if error > params.ConsTol:
+      warnings.warn('There is ' + str(error) + '% relative error ( > ' + str(params.ConsTol) + '% ) between the energy in the water output' +
+                    ' and the expected output based on the law of conservation of energy.\n', UserWarning,
+                    stacklevel=2)

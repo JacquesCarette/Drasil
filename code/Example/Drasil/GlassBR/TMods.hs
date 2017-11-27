cc5abe37ec97523d32c21f5a5e86faf5201e86a5
Dan Szymczak <dan.szymczak@gmail.com>
2017-11-27 14:17:08 -0500

    Testing something

--- code/Example/Drasil/GlassBR/TMods.hs
+++ code/Example/Drasil/GlassBR/TMods.hs
@@ -21,7 +21,7 @@ tModels = [t1SafetyReq, t2SafetyReq]
 t1IsSafe :: TheoryModel
 t1IsSafe = tm (cw t1SafetyReq) 
   (tc' "isSafe" [qw is_safe1, qw prob_br, qw pb_tol] ([] :: [CWrapper])
-  [] [Phys (\a -> a := (C prob_br) :< (C pb_tol))] [])
+  [] [Phys (\_ -> is_safe1 := (C prob_br) :< (C pb_tol))] [])
 
 t1SafetyReq :: RelationConcept
 t1SafetyReq = makeRC "t1SafetyReq" (nounPhraseSP "Safety Requirement-1")

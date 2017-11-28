7931038369bfe5bafd1dcd6979a19a2e34d3475b
Dan Szymczak <dan.szymczak@gmail.com>
2017-11-27 23:17:16 -0500

    Begin work on #386. This breaks things because I need to update the printers as well. [ci skip]

--- code/Language/Drasil/People.hs
+++ code/Language/Drasil/People.hs
@@ -8,13 +8,13 @@ module Language.Drasil.People
   , rendPersLFM, rendPersLFM', rendPersLFM''
   ) where
 
-import Language.Drasil.Spec (Sentence(S, EmptyS),(+:+), sC)
+import Language.Drasil.Spec (Sentence(S, EmptyS, (:+:)),(+:+), sC)
 import Data.List
 
 -- | A person can have a given name, middle name(s), and surname, as well
 -- as the naming convention they use.
-data Person = Person { _given :: String, _surname :: String, 
-                       _middle :: [String], _convention :: Conv}
+data Person = Person { _given :: Sentence, _surname :: Sentence, 
+                       _middle :: [Sentence], _convention :: Conv}
 -- ^ Western style conventions are given name followed
 -- by middle names, followed by surname.
 -- Eastern style conventions are surname followed by middle names, 
@@ -32,42 +32,42 @@ data Conv = Western
 -- Used for a person with only a given name and surname.
 -- Arguments are in the order: given name, surname.
 person :: String -> String -> Person
-person f l = Person f l [] Western
+person f l = Person (S f) (S l) [] Western
 
 -- | Constructor for a person using Eastern naming conventions. 
 -- Used for a person with only a given name and surname.
 -- Arguments are in the order: surname, given name
 person' :: String -> String -> Person
-person' s g = Person g s [] Eastern
+person' s g = Person (S g) (S s) [] Eastern
 
 -- | Constructor for a person using Western naming conventions.
 -- Similar to the 'person' constructor,
 -- except the middle argument is a list of middle names.
 personWM :: String -> [String] -> String -> Person
-personWM f ms l = Person f l ms Western
+personWM f ms l = Person (S f) (S l) (map S ms) Western
 
 -- | Constructor for a person using Eastern naming conventions.
 -- Similar to the 'person\'' constructor,
 -- except the middle argument is a list of middle names.
 personWM' :: String -> [String] -> String -> Person
-personWM' g ms s = Person g s ms Eastern
+personWM' g ms s = Person (S g) (S s) (map S ms) Eastern
 
 -- | Constructor for a person with a mononym (only one name).
 mononym :: String -> Person
-mononym n = Person "NFN" n [] Mono
+mononym n = Person (S "NFN") (S n) [] Mono
 
 class HasName p where
-  nameStr :: p -> String
+  nameStr :: p -> Sentence
 
 instance HasName Person where
-  nameStr (Person _ n _ Mono) = isInitial n
-  nameStr (Person f l ms Western) = concat (intersperse " " $
+  nameStr (Person _ n _ Mono) =  isInitial n
+  nameStr (Person f l ms Western) = foldr1 (+:+) (
     [isInitial f] ++ map isInitial ms ++ [isInitial l])
-  nameStr (Person g s ms Eastern) = concat (intersperse " " $
+  nameStr (Person g s ms Eastern) = foldr1 (+:+) (
     [isInitial s] ++ map isInitial ms ++ [isInitial g])
 
 name :: (HasName n) => n -> Sentence
-name = S . nameStr
+name = nameStr
 -- this is a weirder recursion, so it's ok to do it explicitly
 -- make it work for short lists too, but it shouldn't be used that way!
 -- | Used for rendering lists of names (one or more) to Sentences.
@@ -79,30 +79,33 @@ manyNames names = nameList names
         nameList [x,y] = (name x) `sC` (S "and") +:+ (name y)
         nameList (x : y : rest) = (name x) `sC` (nameList (y : rest))
 
-lstName :: Person -> String
+lstName :: Person -> Sentence
 lstName (Person {_surname = l}) = l
 
 -- LFM is Last, First Middle
-rendPersLFM :: Person -> String
+rendPersLFM :: Person -> Sentence
 rendPersLFM (Person {_surname = n, _convention = Mono}) = n
 rendPersLFM (Person {_given = f, _surname = l, _middle = ms}) =
-  isInitial l ++ ", " ++ unwords (isInitial f: map isInitial ms)
+  (isInitial l) `sC` (isInitial f) +:+ foldr1 (+:+) ms
 
 -- LFM' is Last, F. M.
-rendPersLFM' :: Person -> String
+rendPersLFM' :: Person -> Sentence
 rendPersLFM' (Person {_surname = n, _convention = Mono}) = n
 rendPersLFM' (Person {_given = f, _surname = l, _middle = ms}) =
-  isInitial l ++ ", " ++ (unwords . map initial) (f:ms)
+  (isInitial l) `sC` S ((unwords . map initial) (f:ms))
 
 -- LFM'' is Last, First M.
-rendPersLFM'' :: Person -> String
+rendPersLFM'' :: Person -> Sentence
 rendPersLFM'' (Person {_surname = n, _convention = Mono}) = n
 rendPersLFM'' (Person {_given = f, _surname = l, _middle = ms}) =
-  isInitial l ++ ", " ++ unwords (isInitial f:(map initial ms))
+  (isInitial l) `sC` (isInitial f) +:+ S (unwords (map initial ms))
 
-initial :: String -> String
-initial = (\xs -> head xs : ".")
+initial :: Sentence -> String
+initial (a :+: _) = initial a
+initial (S s) = (head s : ".")
+initial _ = error "Cannot get initials for this name"
 
-isInitial :: String -> String
-isInitial [x]  = [x,'.']
+
+isInitial :: Sentence -> Sentence
+isInitial (S [x])  = S $ [x,'.']
 isInitial nm = nm

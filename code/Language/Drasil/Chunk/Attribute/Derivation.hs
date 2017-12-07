30c62831a3c2ce17732a66eef9e7ee54b7372e70
Dan Szymczak <szymczdm@mcmaster.ca>
2017-12-06 22:23:10 -0500

    Add Derivations type (more to come in following commit) [ci skip]

---
+++ code/Language/Drasil/Chunk/Attribute/Derivation.hs
@@ -0,0 +1,13 @@
+module Language.Drasil.Chunk.Attribute.Derivation where
+
+import Language.Drasil.Spec (Sentence)
+import Language.Drasil.Expr (Expr)
+
+-- | Necessary for removing the dependency on the "Contents" type. 
+data DerWrapper = DE Expr
+                | DS Sentence
+
+-- Derivations are an ordered list of sentences and expressions.
+-- They are rendered in order as paragraphs and equation blocks to display
+-- the derivation.
+type Derivation = [DerWrapper]

a1ad5be27b8f182b923cfcd118102850a49db0c6
Dan Szymczak <szymczdm@mcmaster.ca>
2017-12-14 14:04:22 -0500

    Half-fixed #437, still need to fix the description, but that's likely going to require the use of a defn database (like our Symbol database) since we want to be more descriptive

--- code/Example/Drasil/SWHS/DataDefs.hs
+++ code/Example/Drasil/SWHS/DataDefs.hs
@@ -58,7 +58,7 @@ htFusionEqn = (C latent_heat) / (C mass)
 
 dd4MeltFrac :: QDefinition
 dd4MeltFrac = fromEqn' (melt_frac ^. id) -- FIXME Should (^. id) be used
-  (nounPhraseSP "fraction of the PCM that is liquid") EmptyS
+  (melt_frac ^. term) (S "fraction of the PCM that is liquid")
   (eqSymb melt_frac) melt_frac_eqn
 --FIXME: "Phi is the melt fraction" is produced; 
   --"Phi is the fraction of the PCM that is liquid" is what is supposed to be

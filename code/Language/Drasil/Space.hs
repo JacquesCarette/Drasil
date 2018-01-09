b7b99914ea1fd6022eb0617565d307dc733bcfd2
Jacques Carette <carette@mcmaster.ca>
2018-01-08 22:16:56 -0500

    remove unecessary use of GADT syntax.

--- code/Language/Drasil/Space.hs
+++ code/Language/Drasil/Space.hs
@@ -1,22 +1,21 @@
-{-# LANGUAGE GADTs #-}
 module Language.Drasil.Space where
 
 -- FIXME: These need to be spaces and not just types, but until Steven
 --  has a chance to integrate his work I think this should be left alone
 -- | Spaces
-data Space where
-  Integer :: Space
-  Rational :: Space
-  Real :: Space
-  Natural :: Space
-  Boolean :: Space
-  Char :: Space
-  String :: Space
-  Radians :: Space
-  Vect :: Space -> Space
-  Obj :: String -> Space
-  DiscreteI :: [Int] -> Space --ex. let A = {1, 2, 4, 7}
-  DiscreteD :: [Double] -> Space
-  DiscreteS :: [String] -> Space --ex. let Meal = {"breakfast", "lunch", "dinner"}
+data Space =
+    Integer
+  | Rational
+  | Real
+  | Natural
+  | Boolean
+  | Char
+  | String
+  | Radians
+  | Vect Space
+  | Obj String
+  | DiscreteI [Int]  --ex. let A = {1, 2, 4, 7}
+  | DiscreteD [Double]
+  | DiscreteS [String] --ex. let Meal = {"breakfast", "lunch", "dinner"}
   deriving Eq
   

d55ac798f84c9ececee5a4c7339cf084ccd98a47
Steven Palmer <palmes4@mcmaster.ca>
2018-02-07 14:35:36 -0500

    ValueSym class

--- code/Language/Drasil/Code/Imperative/New.hs
+++ code/Language/Drasil/Code/Imperative/New.hs
@@ -18,11 +18,12 @@ data Block
 data Statement
 data Value
 data StateType
+data Function
 
 type Label = String
 
 class Symantics repr where
-    block   :: repr [Statement] -> repr Block
+    block   :: [repr Statement] -> repr Block
     assign  :: repr Value -> repr Value -> repr Statement
     varDec  :: Label -> repr StateType -> repr Statement
     
@@ -32,8 +33,52 @@ class Symantics repr where
     char   :: repr StateType
     string :: repr StateType
     
+
+    
+class ValueSym repr where    
     litBool   :: Bool -> repr Value
     litChar   :: Char -> repr Value
     litFloat  :: Double -> repr Value
     litInt    :: Integer -> repr Value
     litString :: String -> repr Value
+    
+    defaultValue :: repr StateType -> repr Value
+    
+    (?!)  :: repr Value -> repr Value
+    (?<)  :: repr Value -> repr Value -> repr Value
+    (?<=) :: repr Value -> repr Value -> repr Value
+    (?>)  :: repr Value -> repr Value -> repr Value
+    (?>=) :: repr Value -> repr Value -> repr Value
+    (?==) :: repr Value -> repr Value -> repr Value
+    (?!=) :: repr Value -> repr Value -> repr Value
+    (?&&) :: repr Value -> repr Value -> repr Value
+    (?||) :: repr Value -> repr Value -> repr Value
+
+    --arithmetic operators (#)
+    (#~)  :: repr Value -> repr Value
+    (#/^) :: repr Value -> repr Value
+    (#|)  :: repr Value -> repr Value 
+    (#+)  :: repr Value -> repr Value -> repr Value
+    (#-)  :: repr Value -> repr Value -> repr Value
+    (#*)  :: repr Value -> repr Value -> repr Value
+    (#/)  :: repr Value -> repr Value -> repr Value
+    (#%)  :: repr Value -> repr Value -> repr Value
+    (#^)  :: repr Value -> repr Value -> repr Value 
+
+     --other operators ($)
+    ($->) :: repr Value -> repr Value -> repr Value
+    ($.)  :: repr Value -> repr Function -> repr Value
+    ($:)  :: Label -> Label -> repr Value
+    
+    
+    log :: repr Value -> repr Value
+    exp :: repr Value -> repr Value
+    sin :: repr Value -> repr Value
+    cos :: repr Value -> repr Value
+    tan :: repr Value -> repr Value
+    csc :: repr Value -> repr Value
+    sec :: repr Value -> repr Value
+    cot :: repr Value -> repr Value
+    
+    
+class FunctionSym repr where

ab3d37b23f2aa55a10071840b3a0f63cfbb54840
deviprasad135 <deviprasad135@gmail.com>
2018-02-14 11:21:26 -0500

    updated

--- code/Example/Drasil/GlassBR/TMods.hs
+++ code/Example/Drasil/GlassBR/TMods.hs
@@ -37,7 +37,7 @@ t1descr = tDescr (is_safe1) s ending
 
 
 t2IsSafe :: TheoryModel
-t2IsSafe = tm(cw t1SafetyReq)
+t2IsSafe = tm(cw t2SafetyReq)
    (tc' "isSafe" [qw is_safe2, qw lRe, qw demand] ([] :: [CWrapper])
    [] [TCon Invariant $ (C is_safe2) $= (C lRe) $> (C demand)] [])
 

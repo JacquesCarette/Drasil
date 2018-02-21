4c3e37d59389b9b7d619afa2e4092794f65b376a
Jacques Carette <carette@mcmaster.ca>
2018-02-20 23:13:22 -0500

    and simplify the code in Chunk.Relation. The constructors are a little silly, but that can be dealt with later.

--- code/Language/Drasil/Chunk/Relation.hs
+++ code/Language/Drasil/Chunk/Relation.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE GADTs, Rank2Types #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Language.Drasil.Chunk.Relation
   ( RelationConcept(..)
   , makeRC, makeRC'
   ) where
 
-import Control.Lens (Simple, Lens, (^.), set)
+import Control.Lens (makeLenses, (^.))
 import Prelude hiding (id)
 import Language.Drasil.Expr (Relation)
 import Language.Drasil.Chunk
@@ -15,20 +15,17 @@ import Language.Drasil.Chunk.ExprRelat
 
 import Language.Drasil.NounPhrase (NP)
 
-data RelationConcept where 
-  RC :: Concept c => c -> Relation -> RelationConcept
+data RelationConcept = RC {_conc :: ConceptChunk, _rel :: Relation }
+makeLenses ''RelationConcept
 
-instance Chunk RelationConcept where id = rcl id
-instance NamedIdea RelationConcept where term = rcl term
+instance Chunk RelationConcept where id = conc . id
+instance NamedIdea RelationConcept where term = conc . term
 instance Idea RelationConcept where getA (RC c _) = getA c
-instance Definition RelationConcept where defn = rcl defn
-instance ConceptDomain RelationConcept where cdom = rcl cdom
+instance Definition RelationConcept where defn = conc . defn
+instance ConceptDomain RelationConcept where cdom = conc . cdom
 instance Concept RelationConcept where
 instance ExprRelat RelationConcept where relat f (RC c r) = fmap (\x -> RC c x) (f r)
-
--- don't export this
-rcl :: (forall c. (Concept c) => Simple Lens c a) -> Simple Lens RelationConcept a
-rcl l f (RC a b) = fmap (\x -> RC (set l x a) b) (f (a ^. l))
+instance Eq RelationConcept where a == b = (a ^. id) == (b ^. id)
 
 -- | Create a RelationConcept from a given id, term, defn, and relation.
 makeRC :: String -> NP -> Sentence -> Relation -> RelationConcept
@@ -38,5 +35,3 @@ makeRC rID rTerm rDefn rel = RC (dccWDS rID rTerm rDefn) rel
 makeRC' :: String -> NP -> Sentence -> String -> Relation -> RelationConcept
 makeRC' rID rTerm rDefn rAbb rel = RC (dccWDS' rID rTerm rDefn rAbb) rel
 
-instance Eq RelationConcept where
-  a == b = (a ^. id) == (b ^. id)

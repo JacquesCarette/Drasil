8b07eff7d92477e4fe51d301a68592e825bf10dc
Jacques Carette <carette@mcmaster.ca>
2018-02-24 20:14:49 -0500

    no real need for GADT here, so use 'normal' syntax.

--- code/Language/Drasil/Symbol.hs
+++ code/Language/Drasil/Symbol.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE GADTs #-}
-
 -- | A 'Symbol' is actually going to be a graphical description of what
 -- gets rendered as a (unique) symbol.  This is actually NOT based on
 -- semantics at all, but just a description of how things look.
@@ -20,20 +18,20 @@ data Decoration = Hat | Vector | Prime deriving (Eq, Ord)
 -- - Decorated symbols
 -- - Concatenations of symbols, including subscripts and superscripts
 -- - empty! (this is to give this a monoid-like flavour)
-data Symbol where
-  Atomic   :: String -> Symbol
-  Special  :: Special -> Symbol
-  Greek    :: Greek -> Symbol
-  Atop  :: Decoration -> Symbol -> Symbol
-  Corners  :: [Symbol] -> [Symbol] -> [Symbol] -> [Symbol] -> Symbol -> Symbol
-            --upleft  -> lowleft  -> upright  -> lowright -> base   -> out
-            -- [1] -> [2] -> [3] -> [4] -> [5] -> out
-            --  Visually:  [1]   [3]
-            --    (out)       [5]
-            --             [2]   [4]
-  Concat :: [ Symbol ]  -> Symbol
+data Symbol =
+    Atomic  String
+  | Special Special
+  | Greek   Greek
+  | Atop    Decoration Symbol
+  | Corners [Symbol] [Symbol] [Symbol] [Symbol] Symbol
+          -- upleft   lowleft  upright  lowright base
+          -- [1]      [2]      [3]      [4]      [5]
+          --  Visually:  [1]   [3]
+          --    (out)       [5]
+          --             [2]   [4]
+  | Concat [Symbol]
             -- [s1, s2] -> s1s2
-  Empty  :: Symbol
+  | Empty
   deriving Eq
 
 instance Monoid Symbol where

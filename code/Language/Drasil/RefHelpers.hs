de31a6e61d4fd72a9ba71f64f893ac64dbf89134
Daniel Szymczak <szymczdm@mcmaster.ca>
2018-03-01 18:46:13 -0500

    Remove firstFourLetters hack

--- code/Language/Drasil/RefHelpers.hs
+++ code/Language/Drasil/RefHelpers.hs
@@ -2,12 +2,6 @@ module Language.Drasil.RefHelpers where
 
 import Data.Char (isAlphaNum)
 
--- | helper to get first 4 letters of each word. Used by inferName
-firstFourLetters :: String -> String
-firstFourLetters s1 = (filter (\x -> not (x `elem` ",.?!")) stringId)
-  where stringId = concat (map (take 4) (words s1))
-
-
 -- | replace underscore in strings with "."
 repUnd :: String -> String
 repUnd s = map (\c -> if c == '_' then '.' else c) s

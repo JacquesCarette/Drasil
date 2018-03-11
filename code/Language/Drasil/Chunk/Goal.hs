620c1c3112f2185ee336a2851941e339e39d86a4
Daniel Szymczak <szymczdm@mcmaster.ca>
2018-03-11 16:13:24 -0400

    Add Goal statement hack -- to be removed along with assumptions, requirements, etc.

---
+++ code/Language/Drasil/Chunk/Goal.hs
@@ -0,0 +1,38 @@
+{-# Language TemplateHaskell #-}
+-- | Hack in goal statements for the time being. We need to remove these doc
+-- concepts from the back-end and put them at the Example level (ie. in 
+-- Drasil.DocumentationLanguage.X
+module Language.Drasil.Chunk.Goal
+  ( Goal
+  , goal
+  , gs, gs'
+  ) where
+
+import Language.Drasil.Chunk
+import Language.Drasil.Chunk.Attribute
+import Language.Drasil.Spec (Sentence, RefAdd)
+
+import Control.Lens (makeLenses, (^.))
+
+data Goal = GS
+          { _gid :: String
+          , goal :: Sentence
+          , _refAddr :: RefAdd
+          , _attribs :: Attributes -- FIXME: I doubt this is necessary for these
+                                   -- but included for consistency.
+          }
+
+makeLenses ''Goal
+
+instance Chunk Goal where uid = gid
+instance HasAttributes Goal where attributes = attribs
+instance Eq Goal where 
+  a == b = a ^. uid == b ^. uid
+  
+-- | Goal smart constructor (has no explicit 'Attributes')
+gs :: String -> Sentence -> RefAdd -> Goal
+gs i g r = GS i g r []
+
+-- | Goal smart constructor (with explicit 'Attributes')
+gs' :: String -> Sentence -> RefAdd -> Attributes -> Goal
+gs' = GS

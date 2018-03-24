8e5abda9ac241062590747b97a32fcbbc3497c20
Jacques Carette <carette@mcmaster.ca>
2018-03-24 10:49:30 -0400

    140 LOC were in common between HTML/Import.hs and TeX/Import.hs. Now they are in in Printing/Import.hs.

--- code/Language/Drasil/Printing/Import.hs
+++ code/Language/Drasil/Printing/Import.hs
@@ -1,8 +1,17 @@
-module Language.Drasil.Printing.Import where
+module Language.Drasil.Printing.Import(space,expr,symbol) where
 
-import Language.Drasil.Space
+import Language.Drasil.Expr (Expr(..), BinOp(..), UFunc(..), Oper(..),
+    DerivType(..), EOperator(..), DomainDesc(..), RealRange(..))
+import Language.Drasil.Expr.Precedence (prec, eprec)
 import qualified Language.Drasil.Printing.AST as P
 
+import Language.Drasil.Chunk.SymbolForm (eqSymb)
+import Language.Drasil.ChunkDB (HasSymbolTable(..), symbLookup)
+import Language.Drasil.Symbol
+import Language.Drasil.Unicode (Special(Partial))
+
+import Control.Lens ((^.))
+import Language.Drasil.Space
 import Data.List (intersperse)
 
 space :: Space -> P.Expr
@@ -27,4 +36,153 @@ p_space (DiscreteI a)  = "{" ++ (concat $ intersperse ", " (map show a)) ++ "}"
 p_space (DiscreteD a)  = "{" ++ (concat $ intersperse ", " (map show a)) ++ "}"
 p_space (DiscreteS a)  = "{" ++ (concat $ intersperse ", " a) ++ "}"
 -}
+
+-- | expr translation function from Drasil to layout AST
+expr :: HasSymbolTable s => Expr -> s -> P.Expr
+expr (Dbl d)          _ = P.Dbl   d
+expr (Int i)          _ = P.Int   i
+expr (Str s)          _ = P.Str   s
+expr (Assoc And l)    sm = P.Row $ intersperse (P.MO P.And) $ map (expr' sm (prec And)) l
+expr (Assoc Or l)     sm = P.Row $ intersperse (P.MO P.Or) $ map (expr' sm (prec Or)) l
+expr (Assoc Add l)     sm = P.Row $ intersperse (P.MO P.Add) $ map (expr' sm (prec Add)) l
+expr (Assoc Mul l)     sm = P.Row $ intersperse (P.MO P.Mul) $ map (expr' sm (prec Mul)) l
+expr (Deriv Part a b) sm =
+  P.Div (P.Row [P.Spec Partial, P.Spc P.Thin, expr a sm])
+        (P.Row [P.Spec Partial, P.Spc P.Thin,
+                symbol $ eqSymb $ symbLookup b $ sm^.symbolTable])
+expr (Deriv Total a b)sm =
+  P.Div (P.Row [P.Ident "d", P.Spc P.Thin, expr a sm])
+        (P.Row [P.Ident "d", P.Spc P.Thin, symbol $ eqSymb $ symbLookup b $ sm^.symbolTable])
+expr (C c)            sm = symbol $ eqSymb $ symbLookup c $ sm^.symbolTable
+expr (FCall f [x])    sm = P.Row [expr f sm, P.Fenced P.Paren P.Paren $ expr x sm]
+expr (FCall f l)      sm = P.Row [expr f sm,
+  P.Fenced P.Paren P.Paren $ P.Row $ intersperse (P.MO P.Comma) $ map (flip expr sm) l]
+expr (Case ps)        sm = if length ps < 2 then
+                    error "Attempting to use multi-case expr incorrectly"
+                    else P.Case (zip (map (flip expr sm . fst) ps) (map (flip expr sm . snd) ps))
+expr (Matrix a)         sm = P.Mtx $ map (map (flip expr sm)) a
+expr (UnaryOp Log u)    sm = mkCall sm P.Log u
+expr (UnaryOp Sin u)    sm = mkCall sm P.Sin u
+expr (UnaryOp Cos u)    sm = mkCall sm P.Cos u
+expr (UnaryOp Tan u)    sm = mkCall sm P.Tan u
+expr (UnaryOp Sec u)    sm = mkCall sm P.Sec u
+expr (UnaryOp Csc u)    sm = mkCall sm P.Csc u
+expr (UnaryOp Cot u)    sm = mkCall sm P.Cot u
+expr (UnaryOp Dim u)    sm = mkCall sm P.Dim u
+expr (UnaryOp Not u)    sm = P.Row [P.MO P.Not, expr u sm]
+expr (UnaryOp Exp u)    sm = P.Row [P.MO P.Exp, P.Sup $ expr u sm]
+expr (UnaryOp Abs u)    sm = P.Fenced P.Abs P.Abs $ expr u sm
+expr (UnaryOp Norm u)   sm = P.Fenced P.Norm P.Norm $ expr u sm
+expr (UnaryOp Sqrt u)   sm = P.Sqrt $ expr u sm
+expr (UnaryOp Neg u)    sm = neg sm u
+expr (BinaryOp Frac a b) sm = P.Div (expr a sm) (expr b sm)
+expr (BinaryOp Cross a b) sm = mkBOp sm P.Cross a b
+expr (BinaryOp Dot a b) sm = mkBOp sm P.Dot a b
+expr (BinaryOp Eq a b) sm = mkBOp sm P.Eq a b
+expr (BinaryOp NEq a b) sm = mkBOp sm P.NEq a b
+expr (BinaryOp Lt a b) sm = mkBOp sm P.Lt a b
+expr (BinaryOp Gt a b) sm = mkBOp sm P.Gt a b
+expr (BinaryOp LEq a b) sm = mkBOp sm P.LEq a b
+expr (BinaryOp GEq a b) sm = mkBOp sm P.GEq a b
+expr (BinaryOp Impl a b) sm = mkBOp sm P.Impl a b
+expr (BinaryOp Iff a b) sm = mkBOp sm P.Iff a b
+expr (BinaryOp Index a b) sm = indx sm a b
+expr (BinaryOp Pow a b) sm = pow sm a b
+expr (BinaryOp Subt a b)   sm = P.Row [expr a sm, P.MO P.Subt, expr b sm]
+expr (EOp o)            sm = eop o sm
+expr (IsIn  a b)        sm = P.Row  [expr a sm, P.MO P.IsIn, space b]
+
+mkCall :: HasSymbolTable ctx => ctx -> P.Ops -> Expr -> P.Expr
+mkCall s o e = P.Row [P.MO o, P.Fenced P.Paren P.Paren $ expr e s]
+
+mkBOp :: HasSymbolTable ctx => ctx -> P.Ops -> Expr -> Expr -> P.Expr
+mkBOp sm o a b = P.Row [expr a sm, P.MO o, expr b sm]
+
+expr' :: HasSymbolTable ctx => ctx -> Int -> Expr -> P.Expr
+expr' s p e = fence e'
+  where
+  e' = expr e s
+  fence = if eprec e < p then P.Fenced P.Paren P.Paren else id
+
+-- | Helper for properly rendering negation of expressions
+neg' :: Expr -> Bool
+neg' (Dbl     _)     = True
+neg' (Int     _)     = True
+neg' (EOp _)         = True
+neg' (Assoc Mul _)   = True
+neg' (BinaryOp Index _ _) = True
+neg' (UnaryOp _ _)   = True
+neg' (C _)           = True
+neg' _               = False
+
+neg :: HasSymbolTable s => s -> Expr -> P.Expr
+neg sm a = if (neg' a) then
+             P.Row [P.MO P.Neg, expr a sm]
+           else
+             P.Row [P.MO P.Neg, P.Fenced P.Paren P.Paren $ expr a sm]
+
+-- | For printing indexes
+indx :: HasSymbolTable ctx => ctx -> Expr -> Expr -> P.Expr
+indx sm (C c) i = f s
+  where
+    i' = expr i sm
+    s = eqSymb $ symbLookup c $ sm^.symbolTable
+    f (Corners [] [] [] [b] e) =
+      let e' = symbol e
+          b' = symbol b in
+      P.Row [P.Row [e', P.Sub (P.Row [b', P.MO P.Comma, i'])]] -- FIXME, extra Row
+    f a@(Atomic _) = P.Row [symbol a, P.Sub i']
+    f a@(Greek _)  = P.Row [symbol a, P.Sub i']
+    f   e          = let e' = symbol e in P.Row [P.Row [e'], P.Sub i']
+indx sm a i = P.Row [P.Row [expr a sm], P.Sub $ expr i sm]
+
+-- | Helper function for translating 'EOperator's
+eop :: HasSymbolTable s => EOperator -> s -> P.Expr
+eop (Summation (IntegerDD v (BoundedR l h)) e) sm =
+  P.Row [P.MO P.Summ, P.Sub (P.Row [symbol v, P.MO P.Eq, expr l sm]), P.Sup (expr h sm),
+         P.Row [expr e sm]]
+eop (Summation (All _) e) sm = P.Row [P.MO P.Summ, P.Row [expr e sm]]
+eop (Summation(RealDD _ _) _) _ = error "HTML/Import.hs Summation cannot be over Real"
+eop (Product (IntegerDD v (BoundedR l h)) e) sm =
+  P.Row [P.MO P.Prod, P.Sub (P.Row [symbol v, P.MO P.Eq, expr l sm]), P.Sup (expr h sm),
+         P.Row [expr e sm]]
+eop (Product (All _) e) sm = P.Row [P.MO P.Prod, P.Row[expr e sm]]
+eop (Product (RealDD _ _) _) _ = error "HTML/Import.hs Product cannot be over Real"
+eop (Integral (RealDD v (BoundedR l h)) e) sm =
+  P.Row [P.MO P.Inte, P.Sub (expr l sm), P.Sup (expr h sm),
+         P.Row [expr e sm], P.Spc P.Thin, P.Ident "d", symbol v]
+eop (Integral (All v) e) sm =
+  P.Row [P.MO P.Inte, P.Sub (symbol v), P.Row [expr e sm], P.Spc P.Thin, 
+         P.Ident "d", symbol v]
+eop (Integral (IntegerDD _ _) _) _ =
+  error "HTML/Import.hs Integral cannot be over Integers"
+
+symbol :: Symbol -> P.Expr
+symbol (Atomic s)  = P.Ident s
+symbol (Special s) = P.Spec s
+symbol (Greek g)   = P.Gr g
+symbol (Concat sl) = P.Row $ map symbol sl
+--
+-- handle the special cases first, then general case
+symbol (Corners [] [] [x] [] s) = P.Row [P.Row [symbol s, P.Sup $ symbol x]]
+symbol (Corners [] [] [] [x] s) = P.Row [P.Row [symbol s, P.Sub $ symbol x]]
+symbol (Corners [_] [] [] [] _) = error "rendering of ul prescript"
+symbol (Corners [] [_] [] [] _) = error "rendering of ll prescript"
+symbol (Corners _ _ _ _ _)      = error "rendering of Corners (general)"
+symbol (Atop f s) = sFormat f s
+symbol (Empty)    = P.Row []
+
+sFormat :: Decoration -> Symbol -> P.Expr
+sFormat Hat    s = P.Over P.Hat $ symbol s
+sFormat Vector s = P.Font P.Bold $ symbol s
+sFormat Prime  s = P.Row [symbol s, P.MO P.Prime]
+
+-- | Helper for properly rendering exponents
+pow :: HasSymbolTable ctx => ctx -> Expr -> Expr -> P.Expr
+pow sm a@(Assoc Add _)  b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
+pow sm a@(BinaryOp Subt _ _) b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
+pow sm a@(BinaryOp Frac _ _) b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
+pow sm a@(Assoc Mul _)  b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
+pow sm a@(BinaryOp Pow _ _)  b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
+pow sm a                b = P.Row [expr a sm, P.Sup (expr b sm)]
 
--- code/Language/Drasil/TeX/Import.hs
+++ code/Language/Drasil/TeX/Import.hs
@@ -1,11 +1,8 @@
 module Language.Drasil.TeX.Import(makeDocument,spec) where
 
 import Control.Lens ((^.))
-import Data.List (intersperse)
 
-import Language.Drasil.Expr (Expr(..), BinOp(..), sy, UFunc(..), Oper(..),
-    DerivType(..), EOperator(..), ($=), RealRange(..), DomainDesc(..))
-import Language.Drasil.Expr.Precedence (prec, eprec)
+import Language.Drasil.Expr (sy, ($=))
 import Language.Drasil.Chunk.AssumpChunk
 import Language.Drasil.Expr.Extract
 import Language.Drasil.Chunk.Change (chng, chngType, ChngType(..))
@@ -13,13 +10,12 @@ import Language.Drasil.Chunk.Concept (defn)
 import Language.Drasil.Spec
 import qualified Language.Drasil.TeX.AST as T
 import qualified Language.Drasil.Printing.AST as P
-import Language.Drasil.Unicode (Special(Partial))
 import Language.Drasil.Chunk.Eq
 import Language.Drasil.Chunk.ExprRelat (relat)
 import Language.Drasil.Chunk.NamedIdea (term)
 import Language.Drasil.Chunk.Quantity (Quantity(..))
 import Language.Drasil.Chunk.SymbolForm (eqSymb)
-import Language.Drasil.ChunkDB (getUnitLup, HasSymbolTable(..),symbLookup)
+import Language.Drasil.ChunkDB (getUnitLup, HasSymbolTable(..))
 import Language.Drasil.Chunk.ReqChunk (requires)
 import Language.Drasil.Chunk.Citation ( Citation, CiteField(..), HP(..)
                                       , citeID, externRefT, fields)
@@ -28,150 +24,8 @@ import Language.Drasil.Document
 import Language.Drasil.Misc (unit'2Contents)
 import Language.Drasil.NounPhrase (phrase, titleize)
 import Language.Drasil.Reference
-import Language.Drasil.Symbol
 import Language.Drasil.Unit (usymb)
-import Language.Drasil.Printing.Import (space)
-
-expr :: HasSymbolTable ctx => Expr -> ctx -> P.Expr
-expr (Dbl d)            _ = P.Dbl  d
-expr (Int i)            _ = P.Int  i
-expr (Str s)            _ = P.Str  s
-expr (Assoc And l)    sm = P.Row $ intersperse (P.MO P.And) $ map (expr' sm (prec And)) l
-expr (Assoc Or l)     sm = P.Row $ intersperse (P.MO P.Or) $ map (expr' sm (prec Or)) l
-expr (Assoc Add l)     sm = P.Row $ intersperse (P.MO P.Add) $ map (expr' sm (prec Add)) l
-expr (Assoc Mul l)     sm = P.Row $ intersperse (P.MO P.Mul) $ map (expr' sm (prec Mul)) l
-expr (Deriv Part a b)  sm = 
-  P.Div (P.Row [P.Font P.Emph $ P.Spec Partial, P.Spc P.Thin, expr a sm])
-        (P.Row [P.Font P.Emph $ P.Spec Partial, P.Spc P.Thin, 
-                P.Font P.Emph $ symbol $ eqSymb $ symbLookup b $ sm^.symbolTable])
-expr (Deriv Total a b)sm = 
-  P.Div (P.Row [P.Font P.Emph $ P.Ident "d", P.Spc P.Thin, expr a sm])
-        (P.Row [P.Font P.Emph $ P.Ident "d", P.Spc P.Thin, P.Font P.Emph $ symbol $ eqSymb $ symbLookup b $ sm^.symbolTable])
-expr (C c)            sm = symbol $ eqSymb $ symbLookup c $ sm^.symbolTable -- FIXME Stage?
-expr (FCall f [x])     sm = P.Row [expr f sm, P.Fenced P.Paren P.Paren $ expr x sm]
-expr (FCall f x)       sm = P.Row [expr f sm, 
-  P.Fenced P.Paren P.Paren $ P.Row $ intersperse (P.MO P.Comma) $ map (flip expr sm) x]
-expr (Case ps)         sm = if length ps < 2 then
-        error "Attempting to use multi-case expr incorrectly"
-        else P.Case (zip (map (flip expr sm . fst) ps) (map (flip expr sm . snd) ps))
-expr (Matrix a)        sm = P.Mtx $ map (map (flip expr sm)) a
-expr (UnaryOp Log u)   sm = mkCall sm P.Log u
-expr (UnaryOp Sin u)    sm = mkCall sm P.Sin u
-expr (UnaryOp Cos u)    sm = mkCall sm P.Cos u
-expr (UnaryOp Tan u)    sm = mkCall sm P.Tan u
-expr (UnaryOp Sec u)    sm = mkCall sm P.Sec u
-expr (UnaryOp Csc u)    sm = mkCall sm P.Csc u
-expr (UnaryOp Cot u)    sm = mkCall sm P.Cot u
-expr (UnaryOp Dim u)    sm = mkCall sm P.Dim u
-expr (UnaryOp Not u)    sm = P.Row [P.MO P.Not, expr u sm]
-expr (UnaryOp Exp u)    sm = P.Row [P.MO P.Exp, P.Sup $ expr u sm]
-expr (UnaryOp Abs u)    sm = P.Fenced P.Abs P.Abs $ expr u sm
-expr (UnaryOp Norm u)   sm = P.Fenced P.Norm P.Norm $ expr u sm
-expr (UnaryOp Sqrt u)   sm = P.Sqrt $ expr u sm
-expr (UnaryOp Neg u)    sm = neg sm u
-expr (EOp o)           sm = eop o sm
-expr (BinaryOp Frac a b) sm = P.Div (expr a sm) (expr b sm)
-expr (BinaryOp Cross a b) sm = mkBOp sm P.Cross a b
-expr (BinaryOp Dot a b) sm = mkBOp sm P.Dot a b
-expr (BinaryOp Eq a b) sm = mkBOp sm P.Eq a b
-expr (BinaryOp NEq a b) sm = mkBOp sm P.NEq a b
-expr (BinaryOp Lt a b) sm = mkBOp sm P.Lt a b
-expr (BinaryOp Gt a b) sm = mkBOp sm P.Gt a b
-expr (BinaryOp LEq a b) sm = mkBOp sm P.LEq a b
-expr (BinaryOp GEq a b) sm = mkBOp sm P.GEq a b
-expr (BinaryOp Impl a b) sm = mkBOp sm P.Impl a b
-expr (BinaryOp Iff a b) sm = mkBOp sm P.Iff a b
-expr (BinaryOp Index a b) sm = indx sm a b
-expr (BinaryOp Pow a b) sm = pow sm a b
-expr (BinaryOp Subt a b)  sm = P.Row [expr a sm, P.MO P.Subt, expr b sm]
-expr (IsIn  a b)       sm = P.Row [expr a sm, P.MO P.IsIn, space b]
-
-expr' :: HasSymbolTable ctx => ctx -> Int -> Expr -> P.Expr
-expr' s p e = fence pe
-  where
-  pe = expr e s
-  fence = if eprec e < p then P.Fenced P.Paren P.Paren else id
-
-mkCall :: HasSymbolTable ctx => ctx -> P.Ops -> Expr -> P.Expr
-mkCall s o e = P.Row [P.MO o, P.Fenced P.Paren P.Paren $ expr e s]
-
-mkBOp :: HasSymbolTable ctx => ctx -> P.Ops -> Expr -> Expr -> P.Expr
-mkBOp sm o a b = P.Row [expr a sm, P.MO o, expr b sm]
-
-neg :: HasSymbolTable ctx => ctx -> Expr -> P.Expr
-neg s x@(Dbl _) = P.Row [P.MO P.Neg, expr x s]
-neg s x@(Int _) = P.Row [P.MO P.Neg, expr x s]
-neg s a@(C   _) = P.Row [P.MO P.Neg, expr a s]
-neg s a@(UnaryOp _ _)   = P.Row [P.MO P.Neg, expr a s]
-neg s a@(Assoc Mul _)   = P.Row [P.MO P.Neg, expr a s]
-neg s x                 = P.Fenced P.Paren P.Paren $ P.Row $ [P.MO P.Neg, expr x s]
-
-
--- | Helper function for translating 'EOperator's
-eop :: HasSymbolTable s => EOperator -> s -> P.Expr
-eop (Summation (IntegerDD v (BoundedR l h)) e) sm =
-  P.Row [P.MO P.Summ, P.Sub (P.Row [symbol v, P.MO P.Eq, expr l sm]), P.Sup (expr h sm),
-         P.Row [expr e sm]]
-eop (Summation (All _) e) sm = P.Row [P.MO P.Summ, P.Row [expr e sm]]
-eop (Summation(RealDD _ _) _) _ = error "HTML/Import.hs Summation cannot be over Real"
-eop (Product (IntegerDD v (BoundedR l h)) e) sm =
-  P.Row [P.MO P.Prod, P.Sub (P.Row [symbol v, P.MO P.Eq, expr l sm]), P.Sup (expr h sm),
-         P.Row [expr e sm]]
-eop (Product (All _) e) sm = P.Row [P.MO P.Prod, P.Row[expr e sm]]
-eop (Product (RealDD _ _) _) _ = error "HTML/Import.hs Product cannot be over Real"
-eop (Integral (RealDD v (BoundedR l h)) e) sm =
-  P.Row [P.MO P.Inte, P.Sub (expr l sm), P.Sup (expr h sm),
-         P.Row [expr e sm], P.Spc P.Thin, P.Ident "d", symbol v]
-eop (Integral (All v) e) sm =
-  P.Row [P.MO P.Inte, P.Sub (symbol v), P.Row [expr e sm], P.Spc P.Thin, 
-         P.Ident "d", symbol v]
-eop (Integral (IntegerDD _ _) _) _ =
-  error "HTML/Import.hs Integral cannot be over Integers"
-
--- | For printing indexes
-indx :: HasSymbolTable ctx => ctx -> Expr -> Expr -> P.Expr
-indx sm (C c) i = f s
-  where
-    i' = expr i sm
-    s = eqSymb $ symbLookup c $ sm^.symbolTable
-    f (Corners [] [] [] [b] e) = 
-      let e' = symbol e
-          b' = symbol b in
-      P.Row [P.Row [e', P.Sub (P.Row [b', P.MO P.Comma, i'])]] -- FIXME, extra Row
-    f a@(Atomic _) = P.Row [symbol a, P.Sub i']
-    f a@(Greek _)  = P.Row [symbol a, P.Sub i']
-    f   e          = let e' = symbol e in P.Row [P.Row [e'], P.Sub i']
-indx sm a i = P.Row [P.Row [expr a sm], P.Sub $ expr i sm]
-
-symbol :: Symbol -> P.Expr
-symbol (Atomic s)  = P.Ident s
-symbol (Special s) = P.Spec s
-symbol (Greek g)   = P.Gr g
-symbol (Concat sl) = P.Row $ map symbol sl
---
--- handle the special cases first, then general case
--- double Row is to reproduce what was there before.  FIXME ?
-symbol (Corners [] [] [x] [] s) = P.Row [P.Row [symbol s, P.Sup $ symbol x]]
-symbol (Corners [] [] [] [x] s) = P.Row [P.Row [symbol s, P.Sub $ symbol x]]
-symbol (Corners [_] [] [] [] _) = error "rendering of ul prescript"
-symbol (Corners [] [_] [] [] _) = error "rendering of ll prescript"
-symbol (Corners _ _ _ _ _)      = error "rendering of Corners (general)"
-symbol (Atop f s) = sFormat f s
-symbol (Empty)    = P.Row []
-
-sFormat :: Decoration -> Symbol -> P.Expr
-sFormat Hat    s = P.Over P.Hat $ symbol s
-sFormat Vector s = P.Font P.Bold $ symbol s
-sFormat Prime  s = P.Row [symbol s, P.MO P.Prime]
-
--- | Helper for properly rendering exponents
-pow :: HasSymbolTable ctx => ctx -> Expr -> Expr -> P.Expr
-pow sm a@(Assoc Add _)  b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
-pow sm a@(BinaryOp Subt _ _) b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
-pow sm a@(BinaryOp Frac _ _) b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
-pow sm a@(Assoc Mul _)  b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
-pow sm a@(BinaryOp Pow _ _)  b = P.Row [P.Fenced P.Paren P.Paren (expr a sm), P.Sup (expr b sm)]
-pow sm a                b = P.Row [expr a sm, P.Sup (expr b sm)]
+import Language.Drasil.Printing.Import (symbol,expr)
 
 spec :: HasSymbolTable ctx => ctx -> Sentence -> P.Spec
 spec _  (S s)          = P.S s

61f4cf6f94403dc31b37fb9084af8a250e3e8075
deviprasad135 <deviprasad135@gmail.com>
2018-05-08 01:37:03 -0400

    Removed Greek Character Iota_L

--- code/Language/Drasil/TeX/Monad.hs
+++ code/Language/Drasil/TeX/Monad.hs
@@ -112,7 +112,6 @@ instance RenderGreek Latex where
   greek Chi_L     = L "\\chi{}"
   greek Chi       = L "X"
   greek Ell       = L "\\ell{}"
-  greek Iota_L    = L "\\iota{}"
   greek Iota      = L "I"
   greek Kappa_L   = L "\\kappa{}"
   greek Kappa     = L "K"

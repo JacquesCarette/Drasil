c957abb111d70ba9a24827a6839a8201375a2c96
Daniel Szymczak <szymczdm@mcmaster.ca>
2018-05-14 00:10:47 -0400

    Update referencing error messages

--- code/Language/Drasil/Reference.hs
+++ code/Language/Drasil/Reference.hs
@@ -198,19 +198,19 @@ instance Referable TheoryModel where
   rType   _ = Def
 
 instance Referable GenDefn where
-  refName _ = error "No explicit name given for theory model -- build a custom Ref"
+  refName _ = error "No explicit name given for gen defn -- build a custom Ref"
   refAdd  g = "GD:" ++ repUScore (g ^. uid)
   rType   _ = Def
 
 instance Referable QDefinition where -- FIXME: This could lead to trouble; need
                                      -- to ensure sanity checking when building
                                      -- Refs. Double-check QDef is a DD before allowing
-  refName _ = error "No explicit name given for theory model -- build a custom Ref"
+  refName _ = error "No explicit name given for data defn -- build a custom Ref"
   refAdd  d = "DD:" ++ repUScore (d ^. uid)
   rType   _ = Def
 
 instance Referable InstanceModel where
-  refName _ = error "No explicit name given for theory model -- build a custom Ref"
+  refName _ = error "No explicit name given for instance model -- build a custom Ref"
   refAdd  i = "IM:" ++ repUScore (i ^. uid)
   rType   _ = Def
 

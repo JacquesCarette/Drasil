724d8e8471bcfcf7544850dcfb9daddd713858fd
Maryyam Niazi <niazim3@mcmaster.ca>
2018-05-15 10:25:10 -0400

    Plurals.hs is no longer used

--- code/Data/Drasil/Plurals.hs
+++
@@ -1,9 +0,0 @@
-module Data.Drasil.Plurals where
-
-import qualified Data.Map as Map
-
-irregularPlurals :: Map.Map String String
-irregularPlurals = Map.fromList [
-  ("body"    , "bodies"),
-  ("velocity", "velocities")
-  ]

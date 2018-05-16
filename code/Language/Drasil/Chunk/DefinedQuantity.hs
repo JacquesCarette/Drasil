c9df4cdd06c2b1ce9bb258a0dfa8254a0009ae6b
Sam Crawford <sam.crawford82@gmail.com>
2018-05-16 14:11:21 -0400

    Started merge of ConVar and DQD

--- code/Language/Drasil/Chunk/DefinedQuantity.hs
+++ code/Language/Drasil/Chunk/DefinedQuantity.hs
@@ -10,30 +10,38 @@ import Language.Drasil.Classes (HasUID(uid), NamedIdea(term), Idea(getA),
 import Language.Drasil.Chunk.Concept (ConceptChunk,cw)
 import qualified Language.Drasil.Chunk.Quantity as Q
 
-import Control.Lens ((^.), makeLenses)
+import Language.Drasil.Symbol (Symbol,Stage)
+import Language.Drasil.Space (Space)
+import Language.Drasil.Chunk.Attribute.Core (Attributes)
+import Language.Drasil.Unit(UnitDefn)
+
+import Control.Lens ((^.), makeLenses, view)
 
 -- | DefinedQuantity = Concept + Quantity
--- not really the best as this duplicates |id|.  At least, it should...
-data DefinedQuantityDict = DQD { _quant :: Q.QuantityDict
-                               , _con :: ConceptChunk
+-- not really the best as this duplicates |id|.  At least, it should... -> resolved
+data DefinedQuantityDict = DQD { _con :: ConceptChunk
+                               , _symb :: Stage -> Symbol
+                               , _spa :: Space
+                               , _unit :: Maybe UnitDefn
+                               , _attribs :: Attributes
                                }
   
 makeLenses ''DefinedQuantityDict
 
 -- but we pick it from the Quantity.
-instance HasUID        DefinedQuantityDict where uid = quant . uid
+instance HasUID        DefinedQuantityDict where uid = con . uid
 instance Eq            DefinedQuantityDict where a == b = (a ^. uid) == (b ^. uid)
 instance NamedIdea     DefinedQuantityDict where term = con . term
-instance Idea          DefinedQuantityDict where getA (DQD a _) = getA a
+instance Idea          DefinedQuantityDict where getA (DQD a _ _ _ _) = getA a
 instance Definition    DefinedQuantityDict where defn = con . defn
 instance ConceptDomain DefinedQuantityDict where
   type DOM DefinedQuantityDict = ConceptChunk
   cdom = con . cdom
 instance Concept       DefinedQuantityDict where
-instance Q.HasSpace    DefinedQuantityDict where typ = quant . Q.typ
-instance HasSymbol     DefinedQuantityDict where symbol q st = symbol (q^.quant) st
-instance Q.Quantity    DefinedQuantityDict where getUnit (DQD a _) = Q.getUnit a
-instance HasAttributes DefinedQuantityDict where attributes = quant . attributes
+instance Q.HasSpace    DefinedQuantityDict where typ = spa
+instance HasSymbol     DefinedQuantityDict where symbol = view symb
+instance Q.Quantity    DefinedQuantityDict where getUnit = view unit
+instance HasAttributes DefinedQuantityDict where attributes = attribs
 
-cqs :: (HasAttributes c, Q.Quantity c, Concept c, DOM c ~ ConceptChunk) => c -> DefinedQuantityDict
-cqs c = DQD (Q.qw c) (cw c)
+cqs :: ConceptChunk -> Symbol -> Space -> Maybe UnitDefn -> Attributes-> DefinedQuantityDict
+cqs c s sp u atts = DQD c (\_ -> s) sp u atts

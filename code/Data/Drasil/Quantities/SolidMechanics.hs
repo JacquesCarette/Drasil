427f5f5cb6a3670e031d45b4a3eea010820a060e
Maryyam Niazi <niazim3@mcmaster.ca>
2018-05-17 16:45:41 -0400

    SolidMechanics no longer breaks from use of obsolete constructor

--- code/Data/Drasil/Quantities/SolidMechanics.hs
+++ code/Data/Drasil/Quantities/SolidMechanics.hs
@@ -7,11 +7,11 @@ import Data.Drasil.SI_Units
 
 elastMod, mobShear, nrmStrss, shearRes, stffness :: UnitalChunk
 
-elastMod = ucEL CSM.elastMod cE pascal
-mobShear = ucEL CSM.mobShear cS newton
-shearRes = ucEL CSM.shearRes cP newton
-stffness = ucEL CSM.stffness cK stiffnessU
-nrmStrss = ucEL CSM.nrmStrss (Greek Sigma_L) pascal
+elastMod = uc CSM.elastMod cE pascal []
+mobShear = uc CSM.mobShear cS newton []
+shearRes = uc CSM.shearRes cP newton []
+stffness = uc CSM.stffness cK stiffnessU []
+nrmStrss = uc CSM.nrmStrss (Greek Sigma_L) pascal []
 
 poissnsR :: DefinedQuantityDict
 poissnsR = cqs CSM.poissnsR (Greek Nu_L) Real

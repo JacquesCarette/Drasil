12a5beb4530a742338d95c43994b6ae30664ba9d
Sam Crawford <sam.crawford82@gmail.com>
2018-05-18 16:24:42 -0400

    Failed attempt at #536

--- code/Language/Drasil/Chunk/DefinedQuantity.hs
+++ code/Language/Drasil/Chunk/DefinedQuantity.hs
@@ -9,7 +9,7 @@ import Language.Drasil.Classes (HasUID(uid), NamedIdea(term), Idea(getA),
   HasAttributes(attributes), HasSpace(typ))
 import Language.Drasil.Chunk.Concept (ConceptChunk, cw)
 import qualified Language.Drasil.Chunk.Quantity as Q
-
+import Language.Drasil.Unit (UnitDefn)
 import Language.Drasil.Symbol (Symbol, Stage)
 import Language.Drasil.Space (Space)
 import Language.Drasil.Chunk.Attribute.Core (Attributes)
@@ -20,6 +20,7 @@ import Control.Lens ((^.), makeLenses, view)
 data DefinedQuantityDict = DQD { _con :: ConceptChunk
                                , _symb :: Stage -> Symbol
                                , _spa :: Space
+                               , _unit :: Maybe UnitDefn
                                , _attribs :: Attributes
                                }
   
@@ -28,7 +29,7 @@ makeLenses ''DefinedQuantityDict
 instance HasUID        DefinedQuantityDict where uid = con . uid
 instance Eq            DefinedQuantityDict where a == b = (a ^. uid) == (b ^. uid)
 instance NamedIdea     DefinedQuantityDict where term = con . term
-instance Idea          DefinedQuantityDict where getA (DQD a _ _ _) = getA a
+instance Idea          DefinedQuantityDict where getA (DQD a _ _ _ _) = getA a
 instance Definition    DefinedQuantityDict where defn = con . defn
 instance ConceptDomain DefinedQuantityDict where
   type DOM DefinedQuantityDict = ConceptChunk
@@ -37,20 +38,20 @@ instance Concept       DefinedQuantityDict where
 instance Q.HasSpace    DefinedQuantityDict where typ = spa
 instance HasSymbol     DefinedQuantityDict where symbol = view symb
 instance HasAttributes DefinedQuantityDict where attributes = attribs
-instance Q.Quantity    DefinedQuantityDict where getUnit con = Nothing -- needed for use with qw wrapper
+instance Q.Quantity    DefinedQuantityDict where getUnit = view unit -- needed for use with qw wrapper
 
 
 -- For when the symbol is constant through stages
 dqd :: ConceptChunk -> Symbol -> Space -> Attributes -> DefinedQuantityDict
-dqd c s sp atts = DQD c (\_ -> s) sp atts
+dqd c s sp atts = DQD c (\_ -> s) sp Nothing atts
 
 -- For when the symbol changes depending on the stage
 dqd' :: ConceptChunk -> (Stage -> Symbol) -> Space -> Attributes -> DefinedQuantityDict
-dqd' c symbs sp atts = DQD c symbs sp atts
+dqd' c symbs sp atts = DQD c symbs sp Nothing atts
 
 -- Same as dqd, but passes an empty list as the Attibutes
 dqdEL :: ConceptChunk -> Symbol -> Space -> DefinedQuantityDict
-dqdEL c s sp = DQD c (\_ -> s) sp []
+dqdEL c s sp = DQD c (\_ -> s) sp Nothing []
 
 dqdWr :: (Concept c, HasAttributes c, Q.HasSpace c, HasSymbol c, DOM c ~ ConceptChunk) => c -> DefinedQuantityDict
-dqdWr c = DQD (cw c) (symbol c) (c ^. typ) (c ^. attributes)
+dqdWr c = DQD (cw c) (symbol c) (c ^. typ) Nothing (c ^. attributes)

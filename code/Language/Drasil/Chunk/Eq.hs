d5b414e184f683fb6d47c4af0c46db53f85ca56f
Sam Crawford <sam.crawford82@gmail.com>
2018-05-22 12:52:00 -0400

    Added references to QDefinition

--- code/Language/Drasil/Chunk/Eq.hs
+++ code/Language/Drasil/Chunk/Eq.hs
@@ -6,7 +6,7 @@ module Language.Drasil.Chunk.Eq
 import Control.Lens ((^.), makeLenses)
 import Language.Drasil.Expr (Expr)
 import Language.Drasil.Classes (HasUID(uid),NamedIdea(term), Idea(getA), DOM,
-  HasSymbol(symbol), IsUnit, HasAttributes(attributes), ExprRelat(relat), HasReference)
+  HasSymbol(symbol), IsUnit, HasAttributes(attributes), ExprRelat(relat), HasReference(getReferences))
 import Language.Drasil.Chunk.Attribute.Core (Attributes)
 import Language.Drasil.Chunk.Attribute.References (References)
 import Language.Drasil.Chunk.Concept (ConceptChunk)
@@ -24,6 +24,7 @@ import Language.Drasil.Spec (Sentence)
 data QDefinition = EC
           { _qua :: QuantityDict
           , _equat :: Expr
+          , _ref :: References
           }
 makeLenses ''QDefinition
 
@@ -33,10 +34,10 @@ instance NamedIdea     QDefinition where term = qua . term
 instance Idea          QDefinition where getA c = getA $ c ^. qua
 instance HasSpace      QDefinition where typ = qua . typ
 instance HasSymbol     QDefinition where symbol e st = symbol (e^.qua) st
-instance Quantity      QDefinition where getUnit (EC a _)   = getUnit a
+instance Quantity      QDefinition where getUnit (EC a _ _)   = getUnit a
 instance ExprRelat     QDefinition where relat = equat
 instance HasAttributes QDefinition where attributes = qua . attributes
-instance HasReference  QDefinition where
+instance HasReference  QDefinition where getReferences = ref
 instance Eq            QDefinition where a == b = (a ^. uid) == (b ^. uid)
   
 -- | Create a 'QDefinition' with an uid, noun phrase (term), definition, symbol,
@@ -45,24 +46,24 @@ instance Eq            QDefinition where a == b = (a ^. uid) == (b ^. uid)
 fromEqn :: (IsUnit u, DOM u ~ ConceptChunk) => 
   String -> NP -> Sentence -> Symbol -> u -> Expr -> Attributes -> QDefinition
 fromEqn nm desc _ symb un eqn atts = 
-  EC (mkQuant nm desc symb Real (Just $ unitWrapper un) Nothing atts) eqn
+  EC (mkQuant nm desc symb Real (Just $ unitWrapper un) Nothing atts) eqn []
 
 -- | Same as fromEqn, but has no units.
 --FIXME: Space hack
 fromEqn' :: String -> NP -> Sentence -> Symbol -> Expr -> Attributes -> QDefinition
-fromEqn' nm desc _ symb eqn atts = EC (mkQuant nm desc symb Real Nothing Nothing atts) eqn
+fromEqn' nm desc _ symb eqn atts = EC (mkQuant nm desc symb Real Nothing Nothing atts) eqn []
 
 -- | Create a 'QDefinition' with an uid, noun phrase (term), symbol,
 -- abbreviation, unit, and defining equation.
 fromEqn'' :: (IsUnit u, DOM u ~ ConceptChunk) => String -> NP -> Sentence ->
  Symbol -> String -> Maybe u -> Expr -> Attributes -> QDefinition
 fromEqn'' nm desc _ symb abbr u eqn atts = 
-  EC (mkQuant nm desc symb Real (fmap unitWrapper u) (Just abbr) atts) eqn
+  EC (mkQuant nm desc symb Real (fmap unitWrapper u) (Just abbr) atts) eqn []
 
 -- | Smart constructor for QDefinitions. Requires a quantity, its defining 
 -- equation, and a list of attributes
 ec :: (HasAttributes c, Quantity c) => c -> Expr -> QDefinition
-ec c eqn = EC (qw c) eqn
+ec c eqn = EC (qw c) eqn []
 
 -- | Returns a 'VarChunk' from a 'QDefinition'.
 -- Currently only used in example /Modules/ which are being reworked.

8b796cabda87aaf2922c3158251c55f68139c81c
Sam Crawford <sam.crawford82@gmail.com>
2018-05-23 11:21:52 -0400

    Fixed 'conflicts' from Derivation merge

--- code/Language/Drasil/Chunk/Eq.hs
+++ code/Language/Drasil/Chunk/Eq.hs
@@ -26,7 +26,7 @@ data QDefinition = EC
           { _qua :: QuantityDict
           , _equat :: Expr
           , _ref :: References
-      	  , _deri :: Derivation
+          , _deri :: Derivation
           }
 makeLenses ''QDefinition
 
@@ -36,7 +36,7 @@ instance NamedIdea     QDefinition where term = qua . term
 instance Idea          QDefinition where getA c = getA $ c ^. qua
 instance HasSpace      QDefinition where typ = qua . typ
 instance HasSymbol     QDefinition where symbol e st = symbol (e^.qua) st
-instance Quantity      QDefinition where getUnit (EC a _ _)   = getUnit a
+instance Quantity      QDefinition where getUnit (EC a _ _ _)   = getUnit a
 instance ExprRelat     QDefinition where relat = equat
 instance HasAttributes QDefinition where attributes = qua . attributes
 instance HasReference  QDefinition where getReferences = ref
@@ -49,24 +49,24 @@ instance HasDerivation QDefinition where derivation = deri
 fromEqn :: (IsUnit u, DOM u ~ ConceptChunk) => 
   String -> NP -> Sentence -> Symbol -> u -> Expr -> Attributes -> QDefinition
 fromEqn nm desc _ symb un eqn atts = 
-  EC (mkQuant nm desc symb Real (Just $ unitWrapper un) Nothing atts) eqn []
+  EC (mkQuant nm desc symb Real (Just $ unitWrapper un) Nothing atts) eqn [] []
 
 -- | Same as fromEqn, but has no units.
 --FIXME: Space hack
 fromEqn' :: String -> NP -> Sentence -> Symbol -> Expr -> Attributes -> QDefinition
-fromEqn' nm desc _ symb eqn atts = EC (mkQuant nm desc symb Real Nothing Nothing atts) eqn []
+fromEqn' nm desc _ symb eqn atts = EC (mkQuant nm desc symb Real Nothing Nothing atts) eqn [] []
 
 -- | Create a 'QDefinition' with an uid, noun phrase (term), symbol,
 -- abbreviation, unit, and defining equation.
 fromEqn'' :: (IsUnit u, DOM u ~ ConceptChunk) => String -> NP -> Sentence ->
  Symbol -> String -> Maybe u -> Expr -> Attributes -> QDefinition
 fromEqn'' nm desc _ symb abbr u eqn atts = 
-  EC (mkQuant nm desc symb Real (fmap unitWrapper u) (Just abbr) atts) eqn []
+  EC (mkQuant nm desc symb Real (fmap unitWrapper u) (Just abbr) atts) eqn [] []
 
 -- | Smart constructor for QDefinitions. Requires a quantity, its defining 
 -- equation, and a list of attributes
 ec :: (HasAttributes c, Quantity c) => c -> Expr -> QDefinition
-ec c eqn = EC (qw c) eqn []
+ec c eqn = EC (qw c) eqn [] []
 
 -- | Returns a 'VarChunk' from a 'QDefinition'.
 -- Currently only used in example /Modules/ which are being reworked.

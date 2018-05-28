f9f36d1900b23cbae02edaa82dd8fde9df67b022
Maryyam Niazi <niazim3@mcmaster.ca>
2018-05-28 12:01:34 -0400

    removed error implementation from EQ (#549)

--- code/Language/Drasil/Chunk/Eq.hs
+++ code/Language/Drasil/Chunk/Eq.hs
@@ -11,6 +11,7 @@ import Language.Drasil.Classes (HasUID(uid),NamedIdea(term), Idea(getA), DOM,
 
 import Language.Drasil.Chunk.Attribute.Core (Attributes)
 import Language.Drasil.Chunk.Attribute.References (References)
+import Language.Drasil.Chunk.Attribute.ShortName
 import Language.Drasil.Chunk.Concept (ConceptChunk)
 import Language.Drasil.Chunk.Quantity (Quantity(getUnit), HasSpace(typ), QuantityDict,
   mkQuant, qw)
@@ -29,6 +30,7 @@ data QDefinition = EC
           , _equat :: Expr
           , _ref :: References
           , _deri :: Derivation
+          , _refName :: ShortNm
           }
 makeLenses ''QDefinition
 
@@ -38,13 +40,13 @@ instance NamedIdea     QDefinition where term = qua . term
 instance Idea          QDefinition where getA c = getA $ c ^. qua
 instance HasSpace      QDefinition where typ = qua . typ
 instance HasSymbol     QDefinition where symbol e st = symbol (e^.qua) st
-instance Quantity      QDefinition where getUnit (EC a _ _ _)   = getUnit a
+instance Quantity      QDefinition where getUnit (EC a _ _ _ _)   = getUnit a
 instance ExprRelat     QDefinition where relat = equat
 instance HasAttributes QDefinition where attributes = qua . attributes
 instance HasShortName  QDefinition where -- FIXME: This could lead to trouble; need
                                      -- to ensure sanity checking when building
                                      -- Refs. Double-check QDef is a DD before allowing
-  shortname _ = error "No explicit name given for data definition -- build a custom Ref"
+  shortname = refName
 instance HasReference  QDefinition where getReferences = ref
 instance Eq            QDefinition where a == b = (a ^. uid) == (b ^. uid)
 instance HasDerivation QDefinition where derivations = deri
@@ -55,24 +57,24 @@ instance HasDerivation QDefinition where derivations = deri
 fromEqn :: (IsUnit u, DOM u ~ ConceptChunk) => 
   String -> NP -> Sentence -> Symbol -> u -> Expr -> Attributes -> References -> QDefinition
 fromEqn nm desc _ symb un eqn atts refs = 
-  EC (mkQuant nm desc symb Real (Just $ unitWrapper un) Nothing atts) eqn refs []
+  EC (mkQuant nm desc symb Real (Just $ unitWrapper un) Nothing atts) eqn refs [] (shortname' "")
 
 -- | Same as fromEqn, but has no units.
 --FIXME: Space hack
 fromEqn' :: String -> NP -> Sentence -> Symbol -> Expr -> Attributes -> References -> QDefinition
-fromEqn' nm desc _ symb eqn atts refs = EC (mkQuant nm desc symb Real Nothing Nothing atts) eqn refs []
+fromEqn' nm desc _ symb eqn atts refs = EC (mkQuant nm desc symb Real Nothing Nothing atts) eqn refs [] (shortname' "")
 
 -- | Create a 'QDefinition' with an uid, noun phrase (term), symbol,
 -- abbreviation, unit, and defining equation.
 fromEqn'' :: (IsUnit u, DOM u ~ ConceptChunk) => String -> NP -> Sentence ->
  Symbol -> String -> Maybe u -> Expr -> Attributes -> References -> QDefinition
 fromEqn'' nm desc _ symb abbr u eqn atts refs = 
-  EC (mkQuant nm desc symb Real (fmap unitWrapper u) (Just abbr) atts) eqn refs []
+  EC (mkQuant nm desc symb Real (fmap unitWrapper u) (Just abbr) atts) eqn refs [] (shortname' "")
 
 -- | Smart constructor for QDefinitions. Requires a quantity, its defining 
 -- equation, and a list of attributes
 ec :: (HasAttributes c, Quantity c) => c -> Expr -> QDefinition
-ec c eqn = EC (qw c) eqn [] []
+ec c eqn = EC (qw c) eqn [] [] (shortname' "")
 
 -- | Returns a 'VarChunk' from a 'QDefinition'.
 -- Currently only used in example /Modules/ which are being reworked.

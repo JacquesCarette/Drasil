85823a2acb6147b55c94f6c3a1596bea4d367634
Maryyam Niazi <niazim3@mcmaster.ca>
2018-05-28 12:09:52 -0400

    removed error implementation from InstanceModel (#549)

--- code/Language/Drasil/Chunk/InstanceModel.hs
+++ code/Language/Drasil/Chunk/InstanceModel.hs
@@ -11,6 +11,7 @@ import Language.Drasil.Classes (HasUID(uid), NamedIdea(term), Idea(getA),
 
 import Language.Drasil.Chunk.Attribute.Core (Attributes)
 import Language.Drasil.Chunk.Attribute.References (References)
+import Language.Drasil.Chunk.Attribute.ShortName
 import Language.Drasil.Chunk.Concept
 import Language.Drasil.Chunk.Constrained.Core (TheoryConstraint)
 import Language.Drasil.Chunk.Eq
@@ -40,13 +41,14 @@ data InstanceModel = IM { _rc :: RelationConcept
                         , _outCons :: OutputConstraints
                         , _ref :: References
                         , _attribs :: Attributes 
- 			, _deri :: Derivation
+                        , _deri :: Derivation
+                        , _refName :: ShortNm
                         }
 makeLenses ''InstanceModel
   
 instance HasUID        InstanceModel where uid = rc . uid
 instance NamedIdea     InstanceModel where term = rc . term
-instance Idea          InstanceModel where getA (IM a _ _ _ _ _ _ _) = getA a
+instance Idea          InstanceModel where getA (IM a _ _ _ _ _ _ _ _) = getA a
 instance Concept       InstanceModel where
 instance Definition    InstanceModel where defn = rc . defn
 instance ConceptDomain InstanceModel where
@@ -54,18 +56,18 @@ instance ConceptDomain InstanceModel where
   cdom = rc . cdom
 instance ExprRelat     InstanceModel where relat = rc . relat
 instance HasAttributes InstanceModel where attributes = attribs
-instance HasShortName  InstanceModel where
-  shortname _ = error "No explicit name given for instance model -- build a custom Ref"
+instance HasShortName  InstanceModel where shortname = refName
 instance HasDerivation InstanceModel where derivations = deri
 instance HasReference  InstanceModel where getReferences = ref
 
 -- | Smart constructor for instance models
 im :: RelationConcept -> Inputs -> InputConstraints -> Output -> 
   OutputConstraints -> Attributes -> InstanceModel
-im rc i ic o oc atts = IM rc i ic o oc [] atts []
+im rc i ic o oc atts = IM rc i ic o oc [] atts [] (shortname' "")
 
 -- | Smart constructor for instance model from qdefinition 
 -- (Sentence is the "concept" definition for the relation concept)
 imQD :: HasSymbolTable ctx => ctx -> QDefinition -> Sentence -> InputConstraints -> OutputConstraints -> Attributes -> InstanceModel
 imQD ctx qd dfn incon ocon atts = IM (makeRC (qd ^. uid) (qd ^. term) dfn 
-  (sy qd $= qd ^. equat) atts) (vars (qd^.equat) ctx) incon (qw qd) ocon [] atts []--FIXME: atts used twice?
+  (sy qd $= qd ^. equat) atts) (vars (qd^.equat) ctx) incon (qw qd) ocon [] atts [] --FIXME: atts used twice?
+  (shortname' "")

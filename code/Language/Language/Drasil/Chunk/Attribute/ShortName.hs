731757a1f8b0ae649f3d4f7453463329b9750e4d
Sam Crawford <sam.crawford82@gmail.com>
2018-06-05 16:21:55 -0400

    Fixed erroneous merge changes

--- code/Language/Language/Drasil/Chunk/Attribute/ShortName.hs
+++ code/Language/Language/Drasil/Chunk/Attribute/ShortName.hs
@@ -1,6 +1,14 @@
 {-# Language TemplateHaskell #-}
 module Language.Drasil.Chunk.Attribute.ShortName where
 
-import Language.Drasil.Spec (Sentence(..))
+--It is a hack to think of ShortName as a String
+data ShortName = ShortNm String
 
-data ShortNm = ShortName Sentence
+class HasShortName s where
+    shortname :: s -> ShortName -- String; The text to be displayed for the link.
+                             -- A short name used for referencing within a document that can 
+                             -- include symbols and whatnot if required.
+                             -- Visible in the typeset documents (pdf)
+
+shortname' :: String -> ShortName
+shortname' = ShortNm 

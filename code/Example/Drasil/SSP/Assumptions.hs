3dccaa2c4143cc89e9a1fc05401e32422617940a
Sam Crawford <35857611+samm82@users.noreply.github.com>
2018-06-07 20:39:31 -0400

    Improved imports in SSP/Assumptions.hs, BasicExprs.hs, and Body.hs (#594)
    * Alphabetize imports for Assumptions.hs
    
    * Alphabetize imports for BasicExprs.hs
    
    * Specify and alphabetize imports for Body.hs

--- code/Example/Drasil/SSP/Assumptions.hs
+++ code/Example/Drasil/SSP/Assumptions.hs
@@ -2,18 +2,17 @@ module Drasil.SSP.Assumptions where
 
 import Language.Drasil
 
-import Drasil.SSP.Defs (slpSrf, slopeSrf, slope,
-  mtrlPrpty, soil, soilLyr, soilPrpty, intrslce, slice)
-import Drasil.SSP.Unitals (coords, normToShear, scalFunc, fs)
-
-import Data.Drasil.Utils (getES)
-import Data.Drasil.SentenceStructures (ofThe, ofThe', getTandS, foldlSent)
+import Drasil.SSP.Defs (intrslce, mtrlPrpty, slice, slope, slopeSrf, slpSrf, 
+    soil, soilLyr, soilPrpty)
+import Drasil.SSP.References (sspCitations)
+import Drasil.SSP.Unitals (coords, fs, normToShear, scalFunc)
 
 import Data.Drasil.Concepts.Documentation (condition)
-import Data.Drasil.Concepts.Physics (force, stress, strain)
 import Data.Drasil.Concepts.Math (surface, unit_)
+import Data.Drasil.Concepts.Physics (force, strain, stress)
 import Data.Drasil.Concepts.SolidMechanics (shearForce)
-import Drasil.SSP.References (sspCitations)
+import Data.Drasil.SentenceStructures (foldlSent, getTandS, ofThe, ofThe')
+import Data.Drasil.Utils (getES)
 
 sspRefDB :: ReferenceDB
 sspRefDB = rdb [] [] newAssumptions [] [] sspCitations -- FIXME: Convert the rest to new chunk types

494140f59358f4f14b35443754a3e51e95de42d4
Sam Crawford <sam.crawford82@gmail.com>
2018-06-12 13:14:24 -0400

    Deleted Quantities/Chemistry.hs and Units/Chemistry.hs; closes #631

--- code/Data/Drasil/Units/Chemistry.hs
+++
@@ -1,9 +0,0 @@
-module Data.Drasil.Units.Chemistry where
-
-import Data.Drasil.SI_Units (mole, kilogram)
-
-
-densityU :: DerUChunk
-
-molarityU     = new_unit "molarity"              $ mole /: litre
-molalityU     = new_unit "molality"              $ mole /: kilogram

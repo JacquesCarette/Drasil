19465c20005a615979717c2015dd6e20d9e9aeff
Maryyam Niazi <niazim3@mcmaster.ca>
2018-06-13 12:23:25 -0400

    made mkLabel constructors

--- code/Language/Drasil/Label.hs
+++ code/Language/Drasil/Label.hs
@@ -6,12 +6,24 @@ import Language.Drasil.UID (UID)
 import Language.Drasil.Classes (HasUID(uid))
 
 -- import reference address from Language.Drasil.References?
-data LblType = RefAdd | MetaLink | URI
+data LblType = RefAdd String | MetaLink String | URI String
 
-data Label = Lbl 
-  { _uniqueID  :: UID
+-- Used for referencing; has to be pure ASCII
+data Label = Lbl
+  { _uniqueID  :: UID --internal, unique
   , lblType    :: LblType 
   }
 makeLenses ''Label
   
 instance HasUID Label where uid = uniqueID
+
+-- multiple mkLabel constructors for label creation
+
+mkLabelRA :: String -> String -> Label
+mkLabelRA id ref = Lbl id (RefAdd ref)
+
+mkLabelML :: String -> String -> Label
+mkLabelML id ref = Lbl id (MetaLink ref)
+
+mkLabelURI :: String -> String -> Label
+mkLabelURI id ref = Lbl id (URI ref)

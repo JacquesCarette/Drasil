be8ed14d609eae96444706bf9a52d2a0f1050cff
Maryyam <niazim3@mcmaster.ca>
2018-06-14 16:57:30 -0400

    Created isLabel and HasLabel classes (#643)
    * created isLabel and HasLabel classes
    
    * moved newly created classes to Classes file; created new file to avoid cyclic imports
    
    * Renamed some files as suggested

--- code/Language/Drasil/Label.hs
+++ code/Language/Drasil/Label.hs
@@ -1,21 +1,9 @@
-{-# Language TemplateHaskell #-}
 module Language.Drasil.Label (Label) where
 
-import Control.Lens (makeLenses)
-import Language.Drasil.UID (UID)
+import Language.Drasil.Label.Core
 import Language.Drasil.Classes (HasUID(uid))
 import Data.Char (isAscii)
 
--- import reference address from Language.Drasil.References?
-data LblType = RefAdd String | MetaLink String | URI String
-
--- Used for referencing; has to be pure ASCII
-data Label = Lbl
-  { _uniqueID  :: UID --internal, unique
-  , lblType    :: LblType 
-  }
-makeLenses ''Label
-  
 instance HasUID Label where uid = uniqueID
 
 -- multiple mkLabel constructors for label creation
@@ -29,6 +17,5 @@ mkLabelML id ref = Lbl id (MetaLink $ ensureASCII ref)
 mkLabelURI :: String -> String -> Label
 mkLabelURI id ref = Lbl id (URI $ ensureASCII ref)
 
--- helpers
 ensureASCII :: String -> String
 ensureASCII s = map (\y -> if isAscii y then y else error "Label needs to be pure ASCII.") s

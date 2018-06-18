1db82a3957eaa43dc1f23714ca0d4758cc7331dc
Sam Crawford <sam.crawford82@gmail.com>
2018-06-18 13:40:41 -0400

    Updated GlassBR Body as per #675 (a)

--- code/Example/Drasil/GlassBR/Body.hs
+++ code/Example/Drasil/GlassBR/Body.hs
@@ -519,7 +519,9 @@ s7_1_req1Table = Table
 req2Desc = foldlSent [S "The", phrase system,
   S "shall set the known", plural value +: S "as follows",
   foldlList [(foldlsC (map getES (take 4 assumptionConstants)) `followA` 4),
-  ((getES constant_LoadDF) `followA` 8), (short lShareFac `followA` 5)]]
+  ((getES constant_LoadDF) `followA` 8), (short lShareFac `followA` 5),
+  (getES hFromt) +:+ S "from" +:+ (makeRef hFromt), 
+  (getES glaTyFac) +:+ S "from" +:+ (makeRef glaTyFac)]]
 
 --ItemType
 {-s7_1_req2 = (Nested (S "The" +:+ phrase system +:+

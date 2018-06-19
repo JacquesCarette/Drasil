95c920d33c2b7dc8ba7627124f1ba65c6c2b7d73
Maryyam Niazi <niazim3@mcmaster.ca>
2018-06-19 10:23:22 -0400

    added shortname field to Label

--- code/Language/Drasil/Label/Core.hs
+++ code/Language/Drasil/Label/Core.hs
@@ -3,6 +3,7 @@ module Language.Drasil.Label.Core where
 
 import Control.Lens (makeLenses)
 import Language.Drasil.UID (UID)
+import Language.Drasil.Chunk.ShortName (ShortName)
 
 -- import reference address from Language.Drasil.References?
 data LblType = RefAdd String | MetaLink String | URI String
@@ -10,6 +11,7 @@ data LblType = RefAdd String | MetaLink String | URI String
 -- Used for referencing; has to be pure ASCII
 data Label = Lbl
   { _uniqueID  :: UID --internal, unique
-  , lblType    :: LblType 
+  , lblType    :: LblType
+  , sn         :: ShortName
   }
 makeLenses ''Label

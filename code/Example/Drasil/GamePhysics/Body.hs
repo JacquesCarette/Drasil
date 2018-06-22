d7fdbde02a74f82835aa4e38ff2de8dd35a0f706
elwazana <elwazana@mcmaster.ca>
2018-06-22 12:42:43 -0400

    Added back DDs into CHipmunk example as discussed in issue #682

--- code/Example/Drasil/GamePhysics/Body.hs
+++ code/Example/Drasil/GamePhysics/Body.hs
@@ -94,6 +94,7 @@ mkSRS = RefSec (RefProg RM.intro [TUnits, tsymb tableOfSymbols, TAandA]) :
           , TMs ([Label] ++ stdFields) [t1NewtonSL_new, t2NewtonTL_new, t3NewtonLUG_new, t4ChaslesThm_new, t5NewtonSLR_new]
           , GDs [] [] HideDerivation -- No Gen Defs for Gamephysics
           , IMs ([Label, Input, Output, InConstraints, OutConstraints] ++ stdFields) [im1_new, im2_new, im3_new] ShowDerivation
+          , DDs ([Label, Symbol, Units] ++ stdFields) cpDDefs ShowDerivation
           , Constraints EmptyS dataConstraintUncertainty (S "FIXME") [inDataConstTbl cpInputConstraints, outDataConstTbl cpOutputConstraints]
           ]
         )

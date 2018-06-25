20da131b1afdbb0bb17704d3b540ba3edc1dbcc9
Maryyam Niazi <niazim3@mcmaster.ca>
2018-06-25 14:54:27 -0400

    added filler data to modelTraceTable function to avoid type mismatch errors in TraceabilityMatrix.hs

--- code/drasil-example/Drasil/DocumentLanguage/TraceabilityMatrix.hs
+++ code/drasil-example/Drasil/DocumentLanguage/TraceabilityMatrix.hs
@@ -2,11 +2,11 @@ module Drasil.DocumentLanguage.TraceabilityMatrix where
 
 import Language.Drasil
 import Drasil.DocumentLanguage.RefHelpers (ModelDB, ddRefDB, gdRefDB, imRefDB, 
-    mdb, modelsFromDB, refDD, refGD, refIM, refTM, tmRefDB)
+    modelsFromDB, refDD, refGD, refIM, refTM, tmRefDB)
 
 modelTraceTable :: ModelDB -> Contents
 modelTraceTable mdb = Table
-  [ EmptyS : crossListItems ]
+  ( EmptyS : crossListItems ) [[{-data (rows)-}]] (S "Title") True {-<-- showLabel?-} "RefAdd"
   where crossListItems = getRefs refTM tmDB ++ getRefs refGD gdDB ++
                          getRefs refDD ddDB ++ getRefs refIM imDB
         getRefs f db = map (f db) (modelsFromDB db)

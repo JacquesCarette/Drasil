c672dc92bd68d9351f67615d42fc8552d805b784
Sam Crawford <sam.crawford82@gmail.com>
2018-06-26 15:09:39 -0400

    Fixed error in SRS.hs

--- code/drasil-example/Drasil/SRS.hs
+++ code/drasil-example/Drasil/SRS.hs
@@ -100,6 +100,11 @@ tOfSymb cs ss = section' (titleize Doc.tOfSymb) cs ss "ToS"
 --function that sets the shortname of each section to be the reference address
 section' :: Sentence -> [Contents] -> [Section] -> RefAdd -> Section
 section' a b c d = section a b c d (shortname' $ getStr a) --FIXME: getStr hack 
+  where
+    getStr :: Sentence -> String
+    getStr (S s) = s
+    getStr ((:+:) s1 s2) = getStr s1 ++ getStr s2
+    getStr _ = error "Term is not a string" 
 
 --
 missingP :: [Contents]

fb9ed9c8a604b8d855b8ee6ad27401469db10fae
Maryyam Niazi <niazim3@mcmaster.ca>
2018-07-04 17:26:05 -0400

    additions/upgrades added to DataDefinition type

--- code/Language/Drasil/Chunk/DataDefinition.hs
+++ code/Language/Drasil/Chunk/DataDefinition.hs
@@ -3,12 +3,18 @@ module Language.Drasil.Chunk.DataDefinition where
 
 import Language.Drasil.Chunk.Eq (QDefinition)
 import Language.Drasil.Spec (Sentence)
+import Language.Drasil.Label.Core (Label)
+import Language.Drasil.Chunk.References (References)
+import Language.Drasil.Chunk.Derivation (Derivation)
 
 import Control.Lens(makeLenses)
 
 -- A data definition is a QDefinition that may have additional notes. 
 -- It also has attributes like derivation, source, etc.
 data DataDefinition = DD { _qd :: QDefinition
+                         , _ref :: References 
+                         , _deri :: Derivation 
+                         , _lbl :: Label
                          , _notes :: Maybe [Sentence]
                          }
 makeLenses ''DataDefinition

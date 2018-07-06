5b51f8708856962e26cdd5ddcbb2db01ae2a290a
Sam Crawford <sam.crawford82@gmail.com>
2018-07-06 16:12:39 -0400

    Moved Drasil.Sections to drasil-docLang

--- code/drasil-docLang/Drasil/DocLang.hs
+++ code/drasil-docLang/Drasil/DocLang.hs
@@ -4,4 +4,17 @@ import Drasil.DocumentLanguage
 import Drasil.DocumentLanguage.Definitions
 import Drasil.DocumentLanguage.RefHelpers
 import Drasil.DocumentLanguage.TraceabilityMatrix
+import Drasil.Sections.AuxiliaryConstants
+import Drasil.Sections.GeneralSystDesc
+import Drasil.Sections.Introduction
+import Drasil.Sections.ReferenceMaterial
+import Drasil.Sections.Requirements
+import Drasil.Sections.ScopeOfTheProject
+import Drasil.Sections.SolutionCharacterSpec
+import Drasil.Sections.SpecificSystemDescription
+import Drasil.Sections.Stakeholders
+import Drasil.Sections.TableOfAbbAndAcronyms
+import Drasil.Sections.TableOfSymbols
+import Drasil.Sections.TableOfUnits
+import Drasil.Sections.TraceabilityMandGs
 import Drasil.SRS

a48aedd46246fd94dcba86a37461d30e90cadaa7
Maryyam Niazi <niazim3@mcmaster.ca>
2018-07-06 17:00:10 -0400

    added labels to SSP references

--- code/drasil-example/Drasil/SSP/References.hs
+++ code/drasil-example/Drasil/SSP/References.hs
@@ -19,16 +19,20 @@ chen2005 = cArticle "chen2005" [qhQian, dyZhu, cfLee, grChen]
   (S "A concise algorithm for computing the" +:+
   phrase fs_concept +:+ S "using the" +:+ S "morgenstern price method")
   jnlCGJ 2005 [month Feb, volume 42, number 1, pages [272,278]]
+  (makeRA'' "chen2005")
 
 fredlund1977 = cArticle "fredlund1977" [dgFredlund, jKrahn]
   (S "Comparison of slope stability methods of" +:+ phrase analysis)
   jnlCGJ 1977 [month Apr, pages [429, 439], volume 14, number 3]
-  
+  (makeRA'' "fredlund1977")
+
 stolle2008 = cArticle "stolle2008" [dStolle, pGuo]
   (S "Limit equilibrum" +:+ phrase ssa +:+ S "using rigid finite elements")
   jnlCGJ 2008 [month May, pages [653,662], volume 45, number 5]
+  (makeRA'' "stolle2008")
 
 li2010 = cArticle "li2010" [yCLi, ymChen, tltZhan, ssLing, pjCleall]
   (S "An efficient approach for locating the" +:+ phrase crtSlpSrf +:+ 
   S "in" +:+ plural ssa +:+ S "using a real-coded genetic algorithm")
   jnlCGJ 2010 [month Jun, pages [806,820], volume 47, number 7]
+  (makeRA'' "li2010")

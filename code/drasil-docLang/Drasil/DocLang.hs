093814c3210a176f7e6c467bbebd2321629d6604
Sam Crawford <sam.crawford82@gmail.com>
2018-07-06 15:11:35 -0400

    Added drasil-docLang with .cabal, stack.yaml, and files needed

---
+++ code/drasil-docLang/Drasil/DocLang.hs
@@ -0,0 +1,7 @@
+module Drasil.DocLang where
+
+import Drasil.DocumentLanguage
+import Drasil.DocumentLanguage.Definitions
+import Drasil.DocumentLanguage.RefHelpers
+import Drasil.DocumentLanguage.TraceabilityMatrix
+import Drasil.SRS

a47028f8fffb82bf424a88b8b320cee249abd031
elwazana <elwazana@mcmaster.ca>
2018-07-09 16:01:03 -0400

    Improved imports in CShareRenderer.hs

--- code/drasil-code/Language/Drasil/Code/Imperative/LanguageRenderer/CSharpRenderer.hs
+++ code/drasil-code/Language/Drasil/Code/Imperative/LanguageRenderer/CSharpRenderer.hs
@@ -6,11 +6,29 @@ module Language.Drasil.Code.Imperative.LanguageRenderer.CSharpRenderer (
 
 import Language.Drasil.Code.Code (Code(..))
 import Language.Drasil.Code.Imperative.AST hiding (body,comment,bool,int,float,char)
-import Language.Drasil.Code.Imperative.LanguageRenderer
+import Language.Drasil.Code.Imperative.LanguageRenderer (Config(Config), FileType(Source, Header),
+  DecDef(Dec, Def), getEnv, complexDoc, inputDoc, ioDoc, functionListDoc, functionDoc, unOpDoc,
+  valueDoc, methodTypeDoc, methodDoc, methodListDoc, statementDoc, stateDoc, stateListDoc,
+  scopeDoc, retDoc, printDoc, patternDoc, paramDoc, paramListDoc, classDoc, objAccessDoc,
+  objVarDoc, clsDecListDoc, clsDecDoc, litDoc, iterationDoc, funcDoc, funcAppDoc, exprDoc,
+  exceptionDoc, declarationDoc, enumElementsDoc, conditionalDoc, callFuncParamList,
+  blockDoc, bodyDoc,binOpDoc, body, bottom, top, assignDoc, elseIf, ifBodyStart, blockEnd,
+  printFunc, printFileFunc, printFileLnFunc, printLnFunc, stateType, blockStart, clsDec,
+  listObj, package, list, iterInLabel, iterForEachLabel, inherit, inputFunc, include,
+  includeScope, fileName, ext, dir, enumsEqualInts, commentStart, endStatement, bitArray,
+  renderCode, argsList, Options, ioDocD, StatementLocation(NoLoop, Loop), dot, inputDocD,
+  valueDocD, valueDocD', methodDocD, methodDocD', methodListDocD, paramDocD, paramListDocD,
+  objAccessDocD, iterationDocD, funcDocD, declarationDocD', assignDocD, stateTypeD, fileCode,
+  functionListDocD, methodTypeDocD, unOpDocD, statementDocD, scopeDocD, stateDocD, stateListDocD,
+  doubleSlash, retDocD, patternDocD, clsDecListDocD, clsDecDocD, funcAppDocD, enumElementsDocD,
+  exprDocD', litDocD, conditionalDocD'', callFuncParamListD, bodyDocD, blockDocD, binOpDocD,
+  classDec, namespaceD, includeD, fileNameD, cpplist, functionDocD, new, printDocD, objVarDocD,
+  classDocD, exceptionDocD, exprDocD'', declarationDocD, conditionalDocD)
 import Language.Drasil.Code.Imperative.Helpers (oneTab, vibmap)
 
 import Prelude hiding (print)
-import Text.PrettyPrint.HughesPJ
+import Text.PrettyPrint.HughesPJ (Doc, text, (<>), (<+>), semi, parens, equals, vcat, doubleQuotes,
+  render, rbrace, lbrace, empty, colon)
 
 csharpConfig :: Options -> Config -> Config
 csharpConfig _ c = 

27378cb8773f87c51a92521246c7502d9dd9bd45
Dan Szymczak <szymczdm@mcmaster.ca>
2018-07-11 14:15:25 -0400

    Expr style updates

--- code/drasil-lang/Language/Drasil/Expr.hs
+++ code/drasil-lang/Language/Drasil/Expr.hs
@@ -42,7 +42,7 @@ data Expr where
   Str      :: String -> Expr
   AssocA   :: ArithOper -> [Expr] -> Expr
   AssocB   :: BoolOper  -> [Expr] -> Expr
-  Deriv    :: DerivType -> Expr -> UID -> Expr 
+  Deriv    :: DerivType -> Expr -> UID -> Expr
   -- Derivative, syntax is:
   -- Type (Partial or total) -> principal part of change -> with respect to
   -- For example: Deriv Part y x1 would be (dy/dx1)
@@ -63,22 +63,22 @@ data Expr where
   IsIn     :: Expr -> Space -> Expr --	element of
   RealI    :: UID -> RealInterval Expr Expr -> Expr -- a different kind of 'element of'
 
-($=), ($!=), ($<), ($>), ($<=), ($>=), ($=>), ($<=>), ($.), ($-), 
-  ($/) :: Expr -> Expr -> Expr
-($=)  a b = BinaryOp Eq a b
-($!=) a b = BinaryOp NEq a b
-($<)  a b = BinaryOp Lt a b
-($>)  a b = BinaryOp Gt a b
-($<=) a b = BinaryOp LEq a b
-($>=) a b = BinaryOp GEq a b
-a $=> b = BinaryOp Impl a b
-a $<=> b = BinaryOp Iff a b
-a $. b = BinaryOp Dot a b
-a $- b = BinaryOp Subt a b
-a $/ b = BinaryOp Frac a b
+($=), ($!=), ($<), ($>), ($<=), ($>=), ($=>), ($<=>), ($.), ($-),
+  ($/), ($^) :: Expr -> Expr -> Expr
+($=)   = BinaryOp Eq
+($!=)  = BinaryOp NEq
+($<)   = BinaryOp Lt
+($>)   = BinaryOp Gt
+($<=)  = BinaryOp LEq
+($>=)  = BinaryOp GEq
+($=>)  = BinaryOp Impl
+($<=>) = BinaryOp Iff
+($.)   = BinaryOp Dot
+($-)   = BinaryOp Subt
+($/)   = BinaryOp Frac
+($^)   = BinaryOp Pow
 
-($^), ($&&), ($||) :: Expr -> Expr -> Expr
-($^) = BinaryOp Pow
+($&&), ($||) :: Expr -> Expr -> Expr
 a $&& b = AssocB And [a,b]
 a $|| b = AssocB Or  [a,b]
 
@@ -97,7 +97,7 @@ instance Num Expr where
 
   a * b = AssocA Mul [a, b]
   a - b = BinaryOp Subt a b
-  fromInteger a = Int a
+  fromInteger = Int
   abs = UnaryOp Abs
   negate = UnaryOp Neg
 
@@ -105,23 +105,23 @@ instance Num Expr where
   signum _ = error "should not use signum in expressions"
 
 instance Eq Expr where
-  Dbl a == Dbl b               =  a == b
-  Int a == Int b               =  a == b
-  Str a == Str b               =  a == b
-  AssocA o1 l1 == AssocA o2 l2   =  o1 == o2 && l1 == l2
-  AssocB o1 l1 == AssocB o2 l2   =  o1 == o2 && l1 == l2
-  Deriv t1 a b == Deriv t2 c d =  t1 == t2 && a == c && b == d
-  C a == C b                   =  a == b
-  FCall a b == FCall c d       =  a == c && b == d
-  Case a == Case b             =  a == b
-  IsIn  a b  == IsIn  c d      =  a == c && b == d
-  BinaryOp o a b == BinaryOp p c d =  o == p && a == c && b == d
-  _ == _                       =  False
+  Dbl a          == Dbl b          =   a == b
+  Int a          == Int b          =   a == b
+  Str a          == Str b          =   a == b
+  AssocA o1 l1   == AssocA o2 l2   =  o1 == o2 && l1 == l2
+  AssocB o1 l1   == AssocB o2 l2   =  o1 == o2 && l1 == l2
+  Deriv t1 a b   == Deriv t2 c d   =  t1 == t2 && a == c && b == d
+  C a            == C b            =   a == b
+  FCall a b      == FCall c d      =   a == c && b == d
+  Case a         == Case b         =   a == b
+  IsIn  a b      == IsIn  c d      =   a == c && b == d
+  BinaryOp o a b == BinaryOp p c d =   o == p && a == c && b == d
+  _              == _              =   False
 
 instance Fractional Expr where
   a / b = BinaryOp Frac a b
   fromRational r = BinaryOp Frac (fromInteger $ numerator   r)
-                                (fromInteger $ denominator r)
+                                 (fromInteger $ denominator r)
 
 -- | Topology of a subset of reals.
 data RTopology = Continuous | Discrete

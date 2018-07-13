efd971dfa8e50a74a88e8b10b7c5e78d5885a1e8
Maryyam Niazi <niazim3@mcmaster.ca>
2018-07-13 13:04:43 -0400

    GamePhysics Tmods and References updated for labels

--- code/drasil-example/Drasil/GamePhysics/TMods.hs
+++ code/drasil-example/Drasil/GamePhysics/TMods.hs
@@ -25,12 +25,12 @@ cpTMods = [newtonSL, newtonTL, newtonLUG, chaslesThm, newtonSLR]
 t1NewtonSL_new :: TheoryModel
 t1NewtonSL_new = tm' (cw newtonSL)
   (tc' "NewtonSL" [qw QP.force, qw QPP.mass, qw QP.acceleration] ([] :: [ConceptChunk])
-  [] [TCon Invariant $ (sy QP.force) $= (sy QPP.mass) * (sy QP.acceleration)] []) "NewtonSecLawMot" 
-  [newtonSLDesc]
+  [] [TCon Invariant $ (sy QP.force) $= (sy QPP.mass) * (sy QP.acceleration)] []) 
+  (mkLabelRA'' "NewtonSecLawMot") [newtonSLDesc]
 
 newtonSL :: RelationConcept
 newtonSL = makeRC "newtonSL" (nounPhraseSP "Newton's second law of motion")
-  newtonSLDesc newtonSLRel
+  newtonSLDesc newtonSLRel Nothing--label
 
 newtonSLRel :: Relation
 newtonSLRel = (sy QP.force) $= (sy QPP.mass) * (sy QP.acceleration)
@@ -51,12 +51,12 @@ newtonSLDesc = foldlSent [S "The net", (phrase QP.force), (ch QP.force),
 t2NewtonTL_new :: TheoryModel
 t2NewtonTL_new = tm' (cw newtonTL)
   (tc' "NewtonTL" [qw force_1, qw force_2] ([] :: [ConceptChunk])
-  [] [TCon Invariant $ (sy force_1) $= (negate (sy force_2))] [] ) "NewtonThirdLawMot"
-  [newtonTLDesc]
+  [] [TCon Invariant $ (sy force_1) $= (negate (sy force_2))] [] ) 
+  (mkLabelRA'' "NewtonThirdLawMot") [newtonTLDesc]
 
 newtonTL :: RelationConcept
 newtonTL = makeRC "newtonTL" (nounPhraseSP "Newton's third law of motion")
-  newtonTLDesc newtonTLRel
+  newtonTLDesc newtonTLRel Nothing--label
 
 newtonTLRel :: Relation
 newtonTLRel = (sy force_1) $= (negate (sy force_2))
@@ -79,12 +79,13 @@ t3NewtonLUG_new = tm' (cw newtonLUG)
   [] [TCon Invariant $ (sy QP.force) $= (sy QP.gravitationalConst) * ((sy mass_1) * 
   (sy mass_2) / ((sy dispNorm) $^ (fromInteger 2))) * (sy dispUnit) $= 
   (sy QP.gravitationalConst) * ((sy mass_1) * (sy mass_2) / ((sy dispNorm) 
-  $^ (fromInteger 2))) * ((sy QP.displacement) / (sy dispNorm))] [] ) "UniversalGravLaw"
-  [newtonLUGDesc]
+  $^ (fromInteger 2))) * ((sy QP.displacement) / (sy dispNorm))] [] ) 
+  (mkLabelRA'' "UniversalGravLaw") [newtonLUGDesc]
 
 newtonLUG :: RelationConcept
 newtonLUG = makeRC "newtonLUG" 
   (nounPhraseSP "Newton's law of universal gravitation") newtonLUGDesc newtonLUGRel
+  Nothing--label
 
 newtonLUGRel :: Relation
 newtonLUGRel = (sy QP.force) $=
@@ -128,12 +129,12 @@ t4ChaslesThm_new :: TheoryModel
 t4ChaslesThm_new = tm' (cw chaslesThm)
   (tc' "ChaslesThm" [qw vel_B, qw vel_O, qw QP.angularVelocity, qw r_OB] 
   ([] :: [ConceptChunk]) [] [TCon Invariant $ (sy vel_B) $= (sy vel_O) + (cross 
-  (sy  QP.angularVelocity) (sy r_OB))] []) "ChaslesTheorem"
+  (sy  QP.angularVelocity) (sy r_OB))] []) (mkLabelRA'' "ChaslesTheorem")
   [chaslesThmDesc]
 
 chaslesThm :: RelationConcept
 chaslesThm = makeRC "chaslesThm" (nounPhraseSP "Chasles' theorem")
-  chaslesThmDesc chaslesThmRel
+  chaslesThmDesc chaslesThmRel Nothing--label
 
 -- Need the cross product symbol - third term should be a cross product.
 chaslesThmRel :: Relation
@@ -160,12 +161,13 @@ t5NewtonSLR_new :: TheoryModel
 t5NewtonSLR_new = tm' (cw newtonSLR)
   (tc' "NewtonSLR" [qw QP.torque, qw QP.momentOfInertia, qw QP.angularAccel] 
   ([] :: [ConceptChunk]) [] [TCon Invariant $ (sy  QP.torque) $= (sy QP.momentOfInertia) 
-  * (sy QP.angularAccel)] []) "NewtonSecLawRotMot"
+  * (sy QP.angularAccel)] []) (mkLabelRA'' "NewtonSecLawRotMot")
   [newtonSLRDesc]
 
 newtonSLR :: RelationConcept
 newtonSLR = makeRC "newtonSLR" 
   (nounPhraseSP "Newton's second law for rotational motion") newtonSLRDesc newtonSLRRel
+  Nothing--label
 
 newtonSLRRel :: Relation
 newtonSLRRel = (sy  QP.torque) $= (sy QP.momentOfInertia) * (sy QP.angularAccel)

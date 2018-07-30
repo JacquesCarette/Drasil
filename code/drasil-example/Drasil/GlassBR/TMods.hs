9f52521b0111ac42a0a9213da34ba51ffd25c821
Sam Crawford <35857611+samm82@users.noreply.github.com>
2018-07-30 16:49:25 -0400

    [Small PR] Improved GlassBR Code (#965)
    * Moved functional_requirements_req6_pulledList as per #902
    
    * Changed underscores in function names to camelCase in Body
    
    * Changed is_safe1 and is_safe2 to is_safePb and is_safeLR

--- code/drasil-example/Drasil/GlassBR/TMods.hs
+++ code/drasil-example/Drasil/GlassBR/TMods.hs
@@ -1,6 +1,6 @@
 module Drasil.GlassBR.TMods (gbrTMods, tModels, pbSafetyReq, lrSafetyReq, pbIsSafe, lrIsSafe) where
 
-import Drasil.GlassBR.Unitals (demand, demandq, is_safe1, is_safe2, lRe,
+import Drasil.GlassBR.Unitals (demand, demandq, is_safePb, is_safeLR, lRe,
   pb_tol, prob_br)
 import Drasil.GlassBR.IMods (calofCapacity, calofDemand, probOfBreak)
 import Drasil.GlassBR.Concepts (lResistance)
@@ -21,48 +21,48 @@ gbrTMods = [pbIsSafe, lrIsSafe]
 
 -- FIXME: This is a hack to see if TheoryModel printing will work. This chunk
 -- needs to be updated properly.
--- this is the new function but it still uses the pbSafetyReq,
+-- this is the new function but it still uses the lrSafetyReq,
 -- so basically we have to combine the old function with the new function
-
-pbIsSafe :: TheoryModel
-pbIsSafe = tm' (cw pbSafetyReq) 
-  (tc' "isSafe" [qw is_safe1, qw prob_br, qw pb_tol] ([] :: [ConceptChunk])
-  [] [TCon Invariant $ (sy is_safe1) $= (sy prob_br) $< (sy pb_tol)] [])
-  (mkLabelRA'' "isSafe") [pbSafeDescr]
-
-pbSafetyReq :: RelationConcept
-pbSafetyReq = makeRC "safetyReqPb" (nounPhraseSP "Safety Req-Pb")
-  pbSafeDescr ((sy is_safe1) $= (sy prob_br) $< (sy pb_tol))
-  Nothing--label
-
-pbSafeDescr :: Sentence
-pbSafeDescr = tDescr (is_safe1) s ending
-  where 
-    s = (ch is_safe1) `sAnd` (ch is_safe2) +:+ sParen (S "from" +:+
-      (ref lrSafetyReq))
-    ending = ((ch prob_br) `isThe` (phrase prob_br)) `sC` S "as calculated in" +:+.
-      (makeRef probOfBreak) +:+ (ch pb_tol) `isThe` (phrase pb_tol) +:+ S "entered by the user"
 
 lrIsSafe :: TheoryModel
 lrIsSafe = tm' (cw lrSafetyReq)
-   (tc' "isSafe2" [qw is_safe2, qw lRe, qw demand] ([] :: [ConceptChunk])
-   [] [TCon Invariant $ (sy is_safe2) $= (sy lRe) $> (sy demand)] []) 
-   (mkLabelRA'' "isSafe2") [lrSafeDescr]
+   (tc' "isSafeLR" [qw is_safeLR, qw lRe, qw demand] ([] :: [ConceptChunk])
+   [] [TCon Invariant $ (sy is_safeLR) $= (sy lRe) $> (sy demand)] []) 
+   (mkLabelRA'' "isSafeLR") [lrSafeDescr]
 
 lrSafetyReq :: RelationConcept
 lrSafetyReq = makeRC "safetyReqLR" (nounPhraseSP "Safety Req-LR")
-  lrSafeDescr ( (sy is_safe2) $= (sy lRe) $> (sy demand))
+  lrSafeDescr ( (sy is_safeLR) $= (sy lRe) $> (sy demand))
   Nothing--label
 
 lrSafeDescr :: Sentence
-lrSafeDescr = tDescr (is_safe2) s ending
+lrSafeDescr = tDescr (is_safeLR) s ending
   where 
-    s = ((ch is_safe1) +:+ sParen (S "from" +:+ (ref pbSafetyReq)) `sAnd` (ch is_safe2))
+    s = ((ch is_safePb) +:+ sParen (S "from" +:+ (ref pbSafetyReq)) `sAnd` (ch is_safeLR))
     ending = (short lResistance) `isThe` (phrase lResistance) +:+ 
       sParen (S "also called capacity") `sC` S "as defined in" +:+. 
       (makeRef calofCapacity) +:+ (ch demand) +:+ sParen (S "also referred as the" +:+ 
       (titleize demandq)) `isThe` (demandq ^. defn) `sC` S "as defined in" +:+ 
       makeRef calofDemand
+
+pbIsSafe :: TheoryModel
+pbIsSafe = tm' (cw pbSafetyReq) 
+  (tc' "isSafe" [qw is_safePb, qw prob_br, qw pb_tol] ([] :: [ConceptChunk])
+  [] [TCon Invariant $ (sy is_safePb) $= (sy prob_br) $< (sy pb_tol)] [])
+  (mkLabelRA'' "isSafe") [pbSafeDescr]
+
+pbSafetyReq :: RelationConcept
+pbSafetyReq = makeRC "safetyReqPb" (nounPhraseSP "Safety Req-Pb")
+  pbSafeDescr ((sy is_safePb) $= (sy prob_br) $< (sy pb_tol))
+  Nothing--label
+
+pbSafeDescr :: Sentence
+pbSafeDescr = tDescr (is_safePb) s ending
+  where 
+    s = (ch is_safePb) `sAnd` (ch is_safeLR) +:+ sParen (S "from" +:+
+      (ref lrSafetyReq))
+    ending = ((ch prob_br) `isThe` (phrase prob_br)) `sC` S "as calculated in" +:+.
+      (makeRef probOfBreak) +:+ (ch pb_tol) `isThe` (phrase pb_tol) +:+ S "entered by the user"
 
 tDescr :: VarChunk -> Sentence -> Sentence -> Sentence
 tDescr main s ending = foldlSent [S "If", ch main `sC` S "the glass is" +:+.

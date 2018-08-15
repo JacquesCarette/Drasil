cfd313c71c449a84697441c181a1ed3740f88e1e
Sam Crawford <sam.crawford82@gmail.com>
2018-08-15 15:34:50 -0400

    Revert "no longer export relToQD, which was a real hack."
    This reverts commit 28e22a8a710d0b25457b5b62858ed740aa680835.

--- code/drasil-code/Language/Drasil/Code.hs
+++ code/drasil-code/Language/Drasil/Code.hs
@@ -29,7 +29,7 @@ module Language.Drasil.Code (
   generator, generateCode,
   ($:=), Choices(..), CodeSpec, Comments(CommentNone), ConstraintBehaviour(..), Func, 
   FuncStmt(..), ImplementationType(..), Lang(..), Logging(LogNone), Mod(Mod), Structure(..),
-  asExpr, asVC, codeSpec, fdec, ffor, funcData, funcDef, packmod,
+  asExpr, asVC, codeSpec, fdec, ffor, funcData, funcDef, packmod, relToQD,
   Ind(..), junk, junkLine, listEntry, multiLine, repeated, singleLine, singleton
 ) where
 
@@ -73,4 +73,6 @@ import Language.Drasil.Code.DataDesc (Ind(..), junk, junkLine, listEntry, multiL
 
 import Language.Drasil.CodeSpec (($:=), Choices(..), CodeSpec, Comments(..), ConstraintBehaviour(..), 
   Func, FuncStmt(..), ImplementationType(..), Lang(..), Logging(..), Mod(Mod), Structure(..), 
-  asExpr, asVC, codeSpec, fdec, ffor, funcData, funcDef, packmod)
+  asExpr, asVC, codeSpec, fdec, ffor, funcData, funcDef, packmod, relToQD,
+  )
+

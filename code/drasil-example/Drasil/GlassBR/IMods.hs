c3b26c67585166f112a44468616b5e86cbd26eec
Sam Crawford <sam.crawford82@gmail.com>
2018-08-16 15:02:56 -0400

    Fixed not in SymbolMap error (finally)

--- code/drasil-example/Drasil/GlassBR/IMods.hs
+++ code/drasil-example/Drasil/GlassBR/IMods.hs
@@ -1,4 +1,4 @@
-module Drasil.GlassBR.IMods (glassBRsymb, gbrIMods, probOfBreak,
+module Drasil.GlassBR.IMods (glassBRsymb, gbrIMods, probOfBreak, probOfBr,
   calofCapacity, calofDemand) where
 
 import Prelude hiding (exp)
@@ -10,8 +10,9 @@ import Drasil.GlassBR.Concepts (glassTypeFac, lResistance, lShareFac)
 import Drasil.GlassBR.DataDefs (glaTyFac, nonFL, risk, standOffDis)
 import Drasil.GlassBR.Labels (probOfBrL, calOfCapL, calOfDemandL)
 import Drasil.GlassBR.References (astm2009, beasonEtAl1998)
-import Drasil.GlassBR.Unitals (char_weight, demand, demandq, eqTNTWeight, lRe, 
-  loadSF, plate_len, plate_width, prob_br, risk_fun, standOffDist, wtntWithEqn)
+import Drasil.GlassBR.Unitals (capacity, char_weight, demand, 
+  demandq, eqTNTWeight, lRe, loadSF, plate_len, plate_width, 
+  probBreak, prob_br, risk_fun, standOffDist, wtntWithEqn)
 
 import Data.Drasil.Concepts.Math (parameter)
 import Data.Drasil.SentenceStructures (foldlSent, isThe, sAnd, sOr)
@@ -20,7 +21,7 @@ gbrIMods :: [InstanceModel]
 gbrIMods = [probOfBreak, calofCapacity, calofDemand]
 
 glassBRsymb :: [DefinedQuantityDict]
-glassBRsymb = map dqdWr [plate_len, plate_width, char_weight, standOffDist] ++ map dqdWr [probOfBreak]
+glassBRsymb = map dqdWr [plate_len, plate_width, char_weight, standOffDist] ++ [dqdQd (qw probOfBreak) probBreak, dqdQd (qw calofCapacity) capacity, dqdQd (qw calofDemand) demandq]
 
 {--}
 
@@ -48,7 +49,7 @@ calofCapacity = im' calOfCap [qw nonFL, qw glaTyFac, qw loadSF]
   TCon AssumedCon $ sy loadSF $> 0] (qw lRe) [] calOfCapL [capdescr]
 
 calOfCap :: RelationConcept
-calOfCap = makeRC "calOfCap" (nounPhraseSP "Calculation of Capacity(LR)") 
+calOfCap = makeRC "calOfCap" (nounPhraseSP "Calculation of Capacity (LR)") 
   capdescr ( (sy lRe) $= ((sy nonFL) * (sy glaTyFac) * (sy loadSF))) calOfCapL
 
 capdescr :: Sentence
@@ -73,7 +74,7 @@ calofDemand = im' calOfDe [qw demand, qw eqTNTWeight, qw standOffDist]
   [dedescr]
 
 calOfDe :: RelationConcept
-calOfDe = makeRC "calOfDe" (nounPhraseSP "Calculation of Demand(q)") 
+calOfDe = makeRC "calOfDe" (nounPhraseSP "Calculation of Demand (q)") 
   dedescr ( (sy demand) $= apply2 demand eqTNTWeight standOffDist) calOfDemandL
   --dedescr $ (C demand) $= FCall (asExpr interpY) [V "TSD.txt", sy standOffDist, sy eqTNTWeight] 
   
--- code/drasil-example/Drasil/GlassBR/Symbols.hs
+++ code/drasil-example/Drasil/GlassBR/Symbols.hs
@@ -3,13 +3,13 @@ module Drasil.GlassBR.Symbols where
 import Language.Drasil
 import Language.Drasil.Code (Mod(Mod), asVC)
 
+import Drasil.GlassBR.IMods (gbrIMods)
 import Drasil.GlassBR.ModuleDefs (allMods, implVars)
 import Drasil.GlassBR.Unitals (gbInputDataConstraints, gbInputs, 
-    gBRSpecParamVals, glassBRSymbols, glassBRSymbolsWithDefns, glassBRUnitless, 
-    prob_br)
+    gBRSpecParamVals, glassBRSymbols, glassBRSymbolsWithDefns, glassBRUnitless)
 
 this_symbols :: [QuantityDict]
-this_symbols = qw prob_br : gbInputs ++ (map qw gBRSpecParamVals) ++ 
+this_symbols = (map qw gbrIMods) ++ gbInputs ++ (map qw gBRSpecParamVals) ++ 
   (map qw glassBRSymbolsWithDefns) ++ (map qw glassBRSymbols) ++
   (map qw glassBRUnitless) ++ (map qw gbInputDataConstraints)
   -- include all module functions as symbols

d96c2d01ca42ff027b77e6ce62e43229f49ee207
Sam Crawford <35857611+samm82@users.noreply.github.com>
2018-08-17 23:16:50 -0400

    Minor GlassBR Improvements (#1006)
    * Minor formatting updates
    
    * Updated Changes names

--- code/drasil-example/Drasil/GlassBR/References.hs
+++ code/drasil-example/Drasil/GlassBR/References.hs
@@ -15,19 +15,14 @@ rbrtsn2012 = cMisc "rbrtsn2012" [author [jRobertson, sRobertson], title
   (S "Volere requirements specification template edition 16"),
   howPublishedU (S "https://pdfs.semanticscholar.org/cf57/27a59801086cbd3d14e5" :+: 
     S "87e09880561dbe22.pdf"), year 2012]
-  rbrtsn2012Label
-  where
-    rbrtsn2012Label :: Label
-    rbrtsn2012Label = mkLabelRA' "rbrtsn2012Label" "rbrtsn2012" Cite
+  (mkLabelRA' "rbrtsn2012Label" "rbrtsn2012" Cite)
 
 astm2009 = cMisc "astm2009" [author [mononym "ASTM"],
   title (S "Standard Practice for Determining Load Resistance of Glass in Buildings"),
   publisher (S "ASTM International"),
   bookTitle (S "Standard E1300-09a"),
   year (2009), howPublishedU (S "www.astm.org")]
-  astm2009Label
-  where
-    astm2009Label = mkLabelRA' "astm2009Label" "astm2009" Cite
+  (mkLabelRA' "astm2009Label" "astm2009" Cite)
 
 astm2016 = cMisc "astm2016"
   [ author [mononym "ASTM"],
@@ -35,9 +30,7 @@ astm2016 = cMisc "astm2016"
   publisher (S "ASTM International"),  
   address (S "West Conshohocken, PA"),
   year 2016, howPublishedU (S "https://doi.org/10.1520/C1036-16")]
-  astm2016Label
-  where
-    astm2016Label = mkLabelRA' "astm2016Label" "astm2016" Cite
+  (mkLabelRA' "astm2016Label" "astm2016" Cite)
 
 astm2012 = cMisc "astm2012"
   [ author [mononym "ASTM"],
@@ -46,9 +39,7 @@ astm2012 = cMisc "astm2012"
   publisher (S "ASTM International"),
   address (S "West Conshohocken, PA"),
   year 2012, howPublishedU (S "https://doi.org/10.1520/C1048-12E01")]
-  astm2012Label
-  where
-    astm2012Label = mkLabelRA' "astm2012Label" "astm2012" Cite
+  (mkLabelRA' "astm2012Label" "astm2012" Cite)
 
 beasonEtAl1998 = cMisc "beasonEtAl1998"
   [ author [wlBeason, tlKohutek, jmBracci],
@@ -56,6 +47,4 @@ beasonEtAl1998 = cMisc "beasonEtAl1998"
   bookTitle (S "ASCE Library"),
   month Feb, year 1998,
   howPublishedU (S "doi.org/10.1061/(ASCE)0733-9445(1998)124:2(215)")]
-  beasonEtAl1998Label
-  where
-    beasonEtAl1998Label = mkLabelRA' "beasonEtAl1998Label" "beasonEtAl1998" Cite
+  (mkLabelRA' "beasonEtAl1998Label" "beasonEtAl1998" Cite)

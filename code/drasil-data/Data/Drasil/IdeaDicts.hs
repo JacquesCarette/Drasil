e820720e0f4014279dd4d6fea9045b522e50d557
zhaoy148 <zhaoy148@mcmaster.ca>
2018-11-09 08:46:51 -0500

    Modify term description for Glass.

--- code/drasil-data/Data/Drasil/IdeaDicts.hs
+++ code/drasil-data/Data/Drasil/IdeaDicts.hs
@@ -12,11 +12,11 @@ compScience  = mkIdea  "compScience"    (cn' "Computer Science")      (Just "CS"
 softEng      = mkIdea  "softEng"        (cn' "Software Engineering")  (Just "SE")
 mathematics  = mkIdea  "mathematics"    (cn' "Mathematics")           Nothing
 progLanguage = mkIdea  "progLanguage"   (cn' "Programming Language")  Nothing
-idglass      = mkIdea  "glass"          (cn' "GlassBR")               (Just "GlassBR")
-physics      = mkIdea  "physics"        (cn' "physics")               Nothing
-civilEng     = mkIdea  "civilEng"       (cn' "civil engineering")     Nothing
-materialEng  = mkIdea  "materialEng"    (cn' "material engineering")  Nothing
-documentc    = mkIdea  "documentc"      (cn' "document")              (Just "Doc")
+idglass      = mkIdea  "glass"          (cn' "Glass")                 Nothing
+physics      = mkIdea  "physics"        (cn' "Physics")               Nothing
+civilEng     = mkIdea  "civilEng"       (cn' "Civil Engineering")     Nothing
+materialEng  = mkIdea  "materialEng"    (cn' "Material Engineering")  Nothing
+documentc    = mkIdea  "documentc"      (cn' "Document")              (Just "Doc")
         
 
 

bec3a8f41b95834dfcab80c6c09031f09737947c
zhaoy148 <zhaoy148@mcmaster.ca>
2018-12-02 16:07:11 -0500

    Fix some naming errors.

--- code/drasil-docLang/Drasil/DocumentLanguage/Labels.hs
+++ code/drasil-docLang/Drasil/DocumentLanguage/Labels.hs
@@ -9,10 +9,10 @@ goalStmt_label :: Label
 goalStmt_label = mkLabelRALst "goalStmt" "goalStmt"
 
 solution_label :: Label
-solution_label = mkLabelRALst "solutionGM" "solutionGM"
+solution_label = mkLabelRALst "solution" "solution"
 
 characteristics_label :: Label
-characteristics_label = mkLabelRALst "characteristicsBR" "characteristicsBR"
+characteristics_label = mkLabelRALst "characteristics" "characteristics"
 
 physSystDescription_label :: Label
-physSystDescription_label = mkLabelRALst "physSystDescriptionBR" "physSystDescriptionBR"
+physSystDescription_label = mkLabelRALst "physSystDescription" "physSystDescription"

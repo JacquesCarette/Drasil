ba6e6a3ced74af74fee3f17b755111c43c942dc4
Jacques Carette <carette@mcmaster.ca>
2018-12-24 16:56:09 -0500

    Further simplify ShortName down to what is actually used.

--- code/drasil-lang/Language/Drasil/ShortName.hs
+++ code/drasil-lang/Language/Drasil/ShortName.hs
@@ -1,21 +1,9 @@
-module Language.Drasil.ShortName(
-  ShortName, getStringSN, shortname', concatSN) where
+module Language.Drasil.ShortName(ShortName, getStringSN, shortname') where
 
-data ShortName =
-    ShortNm String
-  | Concat ShortName ShortName
-    deriving Eq  --FIXME: HACK FOR Document/Extract to work. We don't necessarily want this.
-
-instance Monoid ShortName where
-  mempty = shortname' ""
-  mappend = Concat
+data ShortName = ShortNm String
 
 getStringSN :: ShortName -> String
 getStringSN (ShortNm s) = s
-getStringSN (Concat a b) = (getStringSN a) ++ getStringSN b
 
 shortname' :: String -> ShortName
 shortname' = ShortNm
-
-concatSN :: ShortName -> ShortName -> ShortName
-concatSN = Concat

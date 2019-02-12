4bc4914563e2ffc93f78928eb28b778bab869c21
Brooks MacLachlan <brooksel_14@hotmail.com>
2019-02-12 14:20:56 -0500

    Removed old files from imports and cabal

--- code/drasil-code/Language/Drasil/Code.hs
+++ code/drasil-code/Language/Drasil/Code.hs
@@ -1,31 +1,6 @@
 -- | re-export smart constructors for external code writing
 module Language.Drasil.Code (
-  Body, Class, StateVar, Value, Parameter, Module, FunctionDecl,
-  Label, StateType, Library, Statement,
-  bool, int, float, char, string, infile, outfile, listT, obj,
-  arg, self,
-  methodType, methodTypeVoid, block, defaultValue, true, false,
-  pubClass, privClass, privMVar, pubMVar, pubGVar, privMethod, pubMethod, constructor,
-  mainMethod,
-  (?!), (?<), (?<=), (?>), (?>=), (?==), (?!=), (?&&), (?||),
-  (#~), (#/^), (#|), (#+), (#-), (#*), (#/), (#%), (#^),
-  (&=), (&.=), (&=.), (&+=), (&-=), (&++), (&~-), (&.+=), (&.-=), (&.++), (&.~-),
-  ($->), ($.), ($:), log, exp,
-  alwaysDel, neverDel, assign, at, binExpr, break, cast, constDecDef, extends, for,
-  forEach, ifCond, ifExists, listDec, listDec', listDecValues, listOf, litBool, litChar,
-  litFloat, litInt, litObj, litObj', litString, noElse, noParent, objDecDef, oneLiner, param,
-  params, paramToVar, print, printLn, printStr, printStrLn, printFile, printFileLn, printFileStr,
-  openFileR, openFileW, closeFile, getInput, getFileInput, getFileInputAll, getFileInputLine,
-  printFileStrLn, return, returnVar, switch, throw, tryCatch, typ, varDec, varDecDef,
-  while, zipBlockWith, zipBlockWith4, addComments, comment, commentDelimit,
-  endCommentDelimit, prefixFirstBlock, getterName, setterName, convertToClass,
-  convertToMethod, bodyReplace, funcReplace, valListReplace, objDecNew,
-  objDecNewVoid, objDecNew', objDecNewVoid',
-  listSize, listAccess, listAppend, listSlice, stringSplit,
-  var, svToVar, objMethodCall, objMethodCallVoid, valStmt, funcApp, funcApp', func, continue,
-  cSharpLabel, cppLabel, goolLabel, javaLabel, objectiveCLabel, pythonLabel, luaLabel,
-  makeCode, createCodeFiles, toAbsCode, getClassName, buildModule, moduleName,
-  Options(..),
+  makeCode, createCodeFiles, 
   generator, generateCode,
   ($:=), Choices(..), CodeSpec, Comments(CommentNone), ConstraintBehaviour(..), Func, 
   FuncStmt(..), ImplementationType(..), Lang(..), Logging(LogNone), Mod(Mod), Structure(..),
@@ -35,38 +10,8 @@ module Language.Drasil.Code (
 ) where
 
 import Prelude hiding (break, print, return, log, exp)
-import Language.Drasil.Code.Imperative.AST (alwaysDel, neverDel, assign, at, binExpr, 
-  break, cast, constDecDef, extends, for,
-  forEach, ifCond, ifExists, listDec, listDec', listDecValues, listOf, litBool, litChar,
-  litFloat, litInt, litObj, litObj', litString, noElse, noParent, objDecDef, oneLiner, param,
-  params, paramToVar, print, printLn, printStr, printStrLn, printFile, printFileLn, printFileStr,
-  openFileR, openFileW, closeFile, getInput, getFileInput, getFileInputAll, getFileInputLine,
-  printFileStrLn, return, returnVar, switch, throw, tryCatch, typ, varDec, varDecDef,
-  while, zipBlockWith, zipBlockWith4, addComments, comment, commentDelimit,
-  endCommentDelimit, prefixFirstBlock, getterName, setterName, convertToClass,
-  convertToMethod, bodyReplace, funcReplace, valListReplace, objDecNew,
-  objDecNewVoid, objDecNew', objDecNewVoid',
-  listSize, listAccess, listAppend, listSlice, stringSplit,
-  var, svToVar, objMethodCall, objMethodCallVoid, valStmt, funcApp, funcApp', func, continue,
-  toAbsCode, getClassName, buildModule, moduleName, 
-  (?!), (?<), (?<=), (?>), (?>=), (?==), (?!=), (?&&), (?||),
-  (#~), (#/^), (#|), (#+), (#-), (#*), (#/), (#%), (#^),
-  (&=), (&.=), (&=.), (&+=), (&-=), (&++), (&~-), (&.+=), (&.-=), (&.++), (&.~-),
-  ($->), ($.), ($:), log, exp,
-  Body, Class, StateVar, Value, Parameter, Module, FunctionDecl,
-  Label, StateType, Library, Statement,
-  bool, int, float, char, string, infile, outfile, listT, obj,
-  arg, self,
-  methodType, methodTypeVoid, block, defaultValue, true, false,
-  pubClass, privClass, privMVar, pubMVar, pubGVar, privMethod, pubMethod, constructor,
-  mainMethod)
 
 import Language.Drasil.Code.Imperative.Import (generator, generateCode)
-
-import Language.Drasil.Code.Imperative.LanguageRenderer (Options(..))
-
-import Language.Drasil.Code.Imperative.Parsers.ConfigParser (cSharpLabel, cppLabel, 
-  goolLabel, javaLabel, objectiveCLabel, pythonLabel, luaLabel)
 
 import Language.Drasil.Code.CodeGeneration (makeCode, createCodeFiles)
 

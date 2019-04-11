bb9a2fec0bfb1316ec36f016d507347c6e167aad
bmaclach <brooksel_14@hotmail.com>
2019-04-11 14:19:05 -0400

    Fixing GlassBR codegen by making constants doubles (#1194)
    * Made LDF and sFlawParamK doubles to fix codegen
    
    * Fixed a warning

--- code/drasil-example/Drasil/GlassBR/Unitals.hs
+++ code/drasil-example/Drasil/GlassBR/Unitals.hs
@@ -392,9 +392,9 @@ gbConstants = [constant_M, constant_K, constant_ModElas, constant_LoadDur, const
 
 constant_M, constant_K, constant_ModElas, constant_LoadDur, constant_LoadSF :: QDefinition
 constant_K       = mkQuantDef sflawParamK  $ dbl 2.86e-53
-constant_M       = mkQuantDef sflawParamM  $ 7
+constant_M       = mkQuantDef sflawParamM  $ dbl 7
 constant_ModElas = mkQuantDef mod_elas     $ dbl 7.17e10
-constant_LoadDur = mkQuantDef load_dur     $ 3
+constant_LoadDur = mkQuantDef load_dur     $ dbl 3
 constant_LoadSF  = mkQuantDef loadSF       $ 1
 --Equations--
 

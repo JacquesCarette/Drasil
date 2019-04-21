e346733ea4eddd013f872ad5d9c0052eee8a7c3f
Gabriel D'Alimonte <gabriel.dalimonte@gmail.com>
2019-04-20 22:52:45 -0400

    Create smart constructors for Rules and Commands.

--- code/drasil-build/Build/Drasil/Make/AST.hs
+++ code/drasil-build/Build/Drasil/Make/AST.hs
@@ -15,3 +15,19 @@ data Type = Abstract
 
 type Target = String
 type Dependencies = Target
+
+-- | Creates a Rule which results in a file being created
+mkFile :: Target -> [Dependencies] -> [Command] -> Rule
+mkFile t d c = R t d File c
+
+-- | Creates an abstract Rule not associated to a specific file
+mkRule :: Target -> [Dependencies] -> [Command] -> Rule
+mkRule t d c = R t d Abstract c
+
+-- | Creates a Command which fails the make process if it does not return zero
+mkCheckedCommand :: String -> Command
+mkCheckedCommand = flip C []
+
+-- | Creates a command which executes and ignores the return code
+mkCommand :: String -> Command
+mkCommand = flip C [IgnoreReturnCode]

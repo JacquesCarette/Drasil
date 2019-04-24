75863882b2b06ab5438230de11e4a80ca7ed28cc
Gabriel D'Alimonte <gabriel.dalimonte@gmail.com>
2019-04-24 16:22:42 -0400

    Expose Runnable in language configs.

--- code/drasil-code/Language/Drasil/Code/Imperative/LanguageRenderer/CSharpRenderer.hs
+++ code/drasil-code/Language/Drasil/Code/Imperative/LanguageRenderer/CSharpRenderer.hs
@@ -6,6 +6,7 @@ module Language.Drasil.Code.Imperative.LanguageRenderer.CSharpRenderer (
 
 import Language.Drasil.Code.Code (Code(..))
 import Language.Drasil.Code.Imperative.AST hiding (body,comment,bool,int,float,char)
+import Language.Drasil.Code.Imperative.Build.AST (nativeBinary)
 import Language.Drasil.Code.Imperative.LanguageRenderer (Config(Config), FileType(Source),
   DecDef(Dec, Def), getEnv, complexDoc, inputDoc, ioDoc, functionListDoc, functionDoc, unOpDoc,
   valueDoc, methodTypeDoc, methodDoc, methodListDoc, statementDoc, stateDoc, stateListDoc,
@@ -23,7 +24,7 @@ import Language.Drasil.Code.Imperative.LanguageRenderer (Config(Config), FileTyp
   doubleSlash, retDocD, patternDocD, clsDecListDocD, clsDecDocD, funcAppDocD, enumElementsDocD,
   litDocD, callFuncParamListD, bodyDocD, blockDocD, binOpDocD,
   classDec, namespaceD, includeD, fileNameD, functionDocD, new, printDocD, objVarDocD,
-  classDocD, exceptionDocD, exprDocD'', declarationDocD, conditionalDocD)
+  classDocD, exceptionDocD, exprDocD'', declarationDocD, conditionalDocD, runnable)
 import Language.Drasil.Code.Imperative.Helpers (oneTab, vibmap)
 
 import Prelude hiding (print,(<>))
@@ -42,6 +43,7 @@ csharpConfig _ c =
         enumsEqualInts   = False,
         ext              = ".cs",
         dir              = "csharp",
+        runnable         = nativeBinary,
         fileName         = fileNameD c,
         include          = includeD "using",
         includeScope     = scopeDoc c,

d454d4ee08af87bfc9512946b8aa98fd718a945f
bmaclach <brooksel_14@hotmail.com>
2019-04-24 22:16:33 -0400

    SSP Inputs (#1221)
    * Updated typical values to be in Pa
    
    * Added missing input unitals
    
    * slope and water table coordinates should be inputs
    
    * ConstF is also an input
    
    * Updated stable
    
    * Added constraint information to notes field of an IM

--- code/drasil-example/Drasil/SSP/DataCons.hs
+++ code/drasil-example/Drasil/SSP/DataCons.hs
@@ -4,9 +4,9 @@ import Language.Drasil (LabelledContent)
 
 import Drasil.DocLang (inDataConstTbl, outDataConstTbl)
 
-import Drasil.SSP.Unitals (sspInputs, sspOutputs)
+import Drasil.SSP.Unitals (sspInputsWUncrtn, sspOutputs)
 
 {-input and output tables-}
 data_constraint_Table2, data_constraint_Table3 :: LabelledContent
-data_constraint_Table2 = inDataConstTbl sspInputs --FIXME: issue #295
+data_constraint_Table2 = inDataConstTbl sspInputsWUncrtn --FIXME: issue #295
 data_constraint_Table3 = outDataConstTbl sspOutputs

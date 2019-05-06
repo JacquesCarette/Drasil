61af6f74ffedb7e4d5b925337e64bad499f4147e
Daniel Scime <danscime@gmail.com>
2019-05-06 10:37:15 -0400

    Fixing addition of section'' in Document.hs

--- code/drasil-lang/Language/Drasil/Document.hs
+++ code/drasil-lang/Language/Drasil/Document.hs
@@ -73,8 +73,8 @@ mkRawLC x lb = llcc lb x
 section :: Sentence -> [Contents] -> [Section] -> Reference -> Section
 section title intro secs = Section title (map Con intro ++ map Sub secs)
 
-section :: Sentence -> [Contents] -> [Section] -> Reference -> Section
-section title intro secs lbe = section title intro secs lbe
+section'' :: Sentence -> [Contents] -> [Section] -> Reference -> Section
+section'' title intro secs lbe = section'' title intro secs lbe
 
 extractSection :: Document -> [Section]
 extractSection (Document _ _ sec) = concatMap getSec sec

88defde82a7578c09cdfb677c8402da895c12003
Daniel Scime <danscime@gmail.com>
2019-05-08 12:30:33 -0400

    Refactoring z_vector, y_matrix, and x_matrix in GlassBR ModuleDefs

--- code/drasil-example/Drasil/GlassBR/ModuleDefs.hs
+++ code/drasil-example/Drasil/GlassBR/ModuleDefs.hs
@@ -19,7 +19,7 @@ allMods = [readTableMod, inputMod, interpMod]
 -- It's a bit odd that this has to be explicitly built here...
 implVars :: [QuantityDict]
 implVars = [v, x_z_1, y_z_1, x_z_2, y_z_2, mat, col,
-  i, j, k, z, z_vector, y_matrix, x_matrix, y, arr, filename,
+  i, j, k, z, zVector, yMatrix, xMatrix, y, arr, filename,
   y_2, y_1, x_2, x_1, x]
 
 --from TSD.txt:
@@ -29,9 +29,9 @@ readTableMod = packmod "ReadTable" [readTable]
 
 readTable :: Func
 readTable = funcData "read_table" $
-  [ singleLine (repeated [junk, listEntry [WithPattern] z_vector]) ',',
-    multiLine (repeated [listEntry [WithLine, WithPattern] x_matrix,
-                         listEntry [WithLine, WithPattern] y_matrix]) ','
+  [ singleLine (repeated [junk, listEntry [WithPattern] zVector]) ',',
+    multiLine (repeated [listEntry [WithLine, WithPattern] xMatrix,
+                         listEntry [WithLine, WithPattern] yMatrix]) ','
   ]
 
 -----
@@ -75,16 +75,16 @@ x_2  = var "x2"          (sub lX two) Real
 x    = var "x"            lX          Real -- = params.wtnt from mainFun.py
 
 v, x_z_1, y_z_1, x_z_2, y_z_2, mat, col,
-  i, j, k, z, z_vector, y_matrix, x_matrix, y, arr, filename :: QuantityDict
+  i, j, k, z, zVector, yMatrix, xMatrix, y, arr, filename :: QuantityDict
 v       = var "v"         lV                          Real
 i       = var "i"         lI                          Natural
 j       = var "j"         lJ                          Natural
 k       = var "k"         (sub lK two)                Natural
 y       = var "y"         lY                          Real
 z       = var "z"         lZ                          Real
-z_vector = var "z_vector" (sub lZ (Atomic "vector")) (Vect Real)
-y_matrix = var "y_matrix" (sub lY (Atomic "matrix")) (Vect $ Vect Real)
-x_matrix = var "x_matrix" (sub lX (Atomic "matrix")) (Vect $ Vect Real)
+zVector = var "zVector" (sub lZ (Atomic "vector")) (Vect Real)
+yMatrix = var "yMatrix" (sub lY (Atomic "matrix")) (Vect $ Vect Real)
+xMatrix = var "xMatrix" (sub lX (Atomic "matrix")) (Vect $ Vect Real)
 arr     = var "arr"       (Atomic "arr")             (Vect Real)--FIXME: temporary variable for findCT?
 x_z_1   = var "x_z_1"     (sub lX (sub lZ one))      (Vect Real)
 y_z_1   = var "y_z_1"     (sub lY (sub lZ one))      (Vect Real)
@@ -170,42 +170,42 @@ interpY :: Func
 interpY = funcDef "interpY" [filename, x, z] Real
   [
   -- hack
-  fdec x_matrix,
-  fdec y_matrix,
-  fdec z_vector,
+  fdec xMatrix,
+  fdec yMatrix,
+  fdec zVector,
   --
-  call readTable [filename, z_vector, x_matrix, y_matrix],
+  call readTable [filename, zVector, xMatrix, yMatrix],
   -- endhack
-    i     $:= find z_vector z,
-    x_z_1 $:= getCol x_matrix i 0,
-    y_z_1 $:= getCol y_matrix i 0,
-    x_z_2 $:= getCol x_matrix i 1,
-    y_z_2 $:= getCol y_matrix i 1,
+    i     $:= find zVector z,
+    x_z_1 $:= getCol xMatrix i 0,
+    y_z_1 $:= getCol yMatrix i 0,
+    x_z_2 $:= getCol xMatrix i 1,
+    y_z_2 $:= getCol yMatrix i 1,
     FTry
       [ j $:= find x_z_1 x,
         k $:= find x_z_2 x ]
       [ FThrow "Interpolation of y failed" ],
     y_1 $:= (linInterp $ interpOver x_z_1 y_z_1 j x),
     y_2 $:= (linInterp $ interpOver x_z_2 y_z_2 k x),
-    FRet $ linInterp [ vLook z_vector i 0, sy y_1, vLook z_vector i 1, sy y_2, sy z ]
+    FRet $ linInterp [ vLook zVector i 0, sy y_1, vLook zVector i 1, sy y_2, sy z ]
   ]
 
 interpZ :: Func
 interpZ = funcDef "interpZ" [filename, x, y] Real
   [
     -- hack
-  fdec x_matrix,
-  fdec y_matrix,
-  fdec z_vector,
+  fdec xMatrix,
+  fdec yMatrix,
+  fdec zVector,
   --
-  call readTable [filename, z_vector, x_matrix, y_matrix],
+  call readTable [filename, zVector, xMatrix, yMatrix],
   -- endhack
-    ffor i (sy i $< (dim (sy z_vector) - 1))
+    ffor i (sy i $< (dim (sy zVector) - 1))
       [
-        x_z_1 $:= getCol x_matrix i 0,
-        y_z_1 $:= getCol y_matrix i 0,
-        x_z_2 $:= getCol x_matrix i 1,
-        y_z_2 $:= getCol y_matrix i 1,
+        x_z_1 $:= getCol xMatrix i 0,
+        y_z_1 $:= getCol yMatrix i 0,
+        x_z_2 $:= getCol xMatrix i 1,
+        y_z_2 $:= getCol yMatrix i 1,
         FTry
           [ j $:= find x_z_1 x,
             k $:= find x_z_2 x ]
@@ -213,7 +213,7 @@ interpZ = funcDef "interpZ" [filename, x, y] Real
         y_1 $:= (linInterp $ interpOver x_z_1 y_z_1 j x),
         y_2 $:= (linInterp $ interpOver x_z_2 y_z_2 k x),
         FCond ((sy y_1 $<= sy y) $&& (sy y $<= sy y_2))
-          [ FRet $ linInterp [ sy y_1, vLook z_vector i 0, sy y_2, vLook z_vector i 1, sy y ]
+          [ FRet $ linInterp [ sy y_1, vLook zVector i 0, sy y_2, vLook zVector i 1, sy y ]
           ] []
       ],
     FThrow "Interpolation of z failed"

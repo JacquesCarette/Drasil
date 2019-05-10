0a2437419dfbb3c34ebe44c30570deeaacc3249b
bmaclach <brooksel_14@hotmail.com>
2019-05-10 17:58:35 -0400

    GlassBR Instance Models Intro (#1315)
    * Made goals Referable (ConceptInstances)
    
    * Added the instance model intro sentence
    
    * Updated stable

---
+++ code/drasil-example/Drasil/GlassBR/Goals.hs
@@ -0,0 +1,18 @@
+module Drasil.GlassBR.Goals (goals, willBreakGS) where
+
+import Language.Drasil
+
+import Data.Drasil.Concepts.Documentation (goalStmtDom, userInput)
+import Data.Drasil.Concepts.Thermodynamics (degree_')
+import Data.Drasil.SentenceStructures (foldlSent, sAnd, sOf)
+
+import Drasil.GlassBR.Concepts (glaSlab)
+import Drasil.GlassBR.Unitals (explosion)
+
+goals :: [ConceptInstance]
+goals = [willBreakGS]
+
+willBreakGS :: ConceptInstance
+willBreakGS = cic "willBreakGS" (foldlSent [S "Analyze" `sAnd` 
+  S "predict whether the", phrase glaSlab, S "under consideration will be able",S "to withstand the", phrase explosion `sOf` S "a certain", phrase degree_',
+  S "which is calculated based on", phrase userInput]) "Predict-Glass-Withstands-Explosion" goalStmtDom

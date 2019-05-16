0bb5ca54d5406b6523424e2b7a6d8166d5eeafb3
Gabriel D'Alimonte <gabriel.dalimonte@gmail.com>
2019-05-16 00:28:24 -0400

    Make indentation consistent between shell scripts.

--- code/scripts/tex_build.sh
+++ code/scripts/tex_build.sh
@@ -1,21 +1,21 @@
 if [ -z "$EDIR" ]; then
-	echo "Missing EDIR."
-	exit 1
+  echo "Missing EDIR."
+  exit 1
 fi
 if [ -z "$MAKE" ]; then
-	echo "Missing MAKE."
-	exit 1
+  echo "Missing MAKE."
+  exit 1
 fi
 if [ -z "$SUMMARIZE_TEX" ]; then
-	echo "Missing SUMMARIZE_TEX."
-	exit 1
+  echo "Missing SUMMARIZE_TEX."
+  exit 1
 fi
 
 if [ "$SUMMARIZE_TEX" = "yes" ]; then
-	IMODE=batchmode
-	BIFLAGS=-terse
+  IMODE=batchmode
+  BIFLAGS=-terse
 else
-	IMODE=nonstopmode
+  IMODE=nonstopmode
 fi
 
 GEN_NAME_SUFFIX=_SRS
@@ -25,22 +25,22 @@ $MAKE TEXFLAGS=--interaction="$IMODE" BIBTEXFLAGS="$BIFLAGS"
 RET=$?
 
 if [ "$SUMMARIZE_TEX" = "yes" ]; then
-	echo "\n\n\033[0;33m$EDIR TeX Summary\033[0m:"
-	if [ "$RET" -eq 0 ]; then
-		# Approximate error gathering from TeX logs.
-		cat "$EDIR$GEN_NAME_SUFFIX".log | grep -E "erfull|Warning"
-		cat "$EDIR$GEN_NAME_SUFFIX".blg | grep -B3 -E "Error"
-		BIBERRS=$(cat "$EDIR$GEN_NAME_SUFFIX".blg | grep -E "Error" | wc -l)
-		if [ "$BIBERRS" -gt 0 ]; then
-			# This conditional is due to the current way TeX makefiles are generated.
-			# BibTeX return value is ignored (specifically with Tiny having no
-			# references). 
-			RET=1
-		fi
-	else
-		# Most "useful" output is the last run of lualatex. Only print that.
-		cat "$EDIR$GEN_NAME_SUFFIX".log
-	fi
-	echo ""
+  echo "\n\n\033[0;33m$EDIR TeX Summary\033[0m:"
+  if [ "$RET" -eq 0 ]; then
+    # Approximate error gathering from TeX logs.
+    cat "$EDIR$GEN_NAME_SUFFIX".log | grep -E "erfull|Warning"
+    cat "$EDIR$GEN_NAME_SUFFIX".blg | grep -B3 -E "Error"
+    BIBERRS=$(cat "$EDIR$GEN_NAME_SUFFIX".blg | grep -E "Error" | wc -l)
+    if [ "$BIBERRS" -gt 0 ]; then
+      # This conditional is due to the current way TeX makefiles are generated.
+      # BibTeX return value is ignored (specifically with Tiny having no
+      # references). 
+      RET=1
+    fi
+  else
+    # Most "useful" output is the last run of lualatex. Only print that.
+    cat "$EDIR$GEN_NAME_SUFFIX".log
+  fi
+  echo ""
 fi
 exit $RET

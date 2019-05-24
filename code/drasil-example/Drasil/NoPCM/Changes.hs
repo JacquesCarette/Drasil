61bd0b3a7dae1990ac60b3e0343f95ed014ff0ec
Sam Crawford <sam.crawford82@gmail.com>
2019-05-24 18:06:23 -0400

    Linting and consistency improvements

--- code/drasil-example/Drasil/NoPCM/Changes.hs
+++ code/drasil-example/Drasil/NoPCM/Changes.hs
@@ -21,10 +21,10 @@ likelyChgs = [likeChgDT]
 
 likeChgDT :: ConceptInstance
 likeChgDT = cic "likeChgDT" (
-  chgsStart assumpCTNTD $ S "The" +:+ phrase model +:+
-  S "currently only accounts for charging of the tank. That is, increasing the" +:+ phrase temp +:+
-  S "of the water to match the" +:+ phrase temp +:+ S "of the coil. A more complete"  
-  +:+ phrase model +:+. S "would also account for discharging of the tank") 
+  foldlSent [chgsStart assumpCTNTD (S "The"), phrase model,
+  S "currently only accounts for charging of the tank. That is, increasing the",
+  phrase temp, S "of the water to match the", phrase temp +:+. S "of the coil",
+  S "A more complete", phrase model,S "would also account for discharging of the tank"]) 
   "Discharging-Tank" likeChgDom
 
 
@@ -33,11 +33,11 @@ unlikelyChgs = [unlikeChgWFS, unlikeChgNIHG]
 
 unlikeChgWFS :: ConceptInstance
 unlikeChgWFS = cic "unlikeChgWFS" (
-  foldlSent [chgsStart assumpWAL $ S "It is unlikely for the change of",
+  foldlSent [chgsStart assumpWAL (S "It is unlikely for the change of"),
   phrase water, S "from liquid to a solid, or from liquid to gas to be considered"])
   "Water-Fixed-States" unlikeChgDom
 
 unlikeChgNIHG :: ConceptInstance
 unlikeChgNIHG = cic "unlikeChgNIHG" (
-  foldlSent [chgsStart assumpNIHGBW $ S "Is used for the derivations of",
+  foldlSent [chgsStart assumpNIHGBW (S "Is used for the derivations of"),
   makeRef2S eBalanceOnWtr] ) "No-Internal-Heat-Generation" unlikeChgDom

07ab333a4136a0647c202cdd1c6841994e5784fd
Daniel Scime <danscime@gmail.com>
2019-05-24 10:43:20 -0400

    Refactoring glassChoices to choices in GlassBR

--- code/drasil-example/Drasil/GlassBR/Main.hs
+++ code/drasil-example/Drasil/GlassBR/Main.hs
@@ -7,8 +7,8 @@ import Language.Drasil.Printers (DocSpec(DocSpec), DocType(SRS, Website))
 
 import Drasil.GlassBR.Body (glassBRCode, srs, printSetting)
 
-glassChoices :: Choices
-glassChoices = Choices {
+choices :: Choices
+choices = Choices {
   lang = [Python, Cpp, CSharp, Java],
   impType = Program,
   logFile = "log.txt",
@@ -23,4 +23,4 @@ main :: IO()
 main = do
   gen (DocSpec SRS "GlassBR_SRS")     srs printSetting
   gen (DocSpec Website "GlassBR_SRS") srs printSetting
-  genCode glassChoices glassBRCode
+  genCode choices glassBRCode

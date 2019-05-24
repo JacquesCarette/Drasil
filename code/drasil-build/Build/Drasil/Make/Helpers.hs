db2934659e04465377a647a94f0a6155809bee20
Gabriel D'Alimonte <gabriel.dalimonte@gmail.com>
2019-05-24 01:14:24 -0400

    Refactor osDefinitions, use nest instead of tabs.

--- code/drasil-build/Build/Drasil/Make/Helpers.hs
+++ code/drasil-build/Build/Drasil/Make/Helpers.hs
@@ -1,24 +1,25 @@
 module Build.Drasil.Make.Helpers where
 
-import Text.PrettyPrint (Doc, text, ($+$))
+import Text.PrettyPrint (Doc, nest, text, vcat, ($+$))
 
--- | Helper for rendering OS-specific extensions
+-- | Helper for rendering OS-specific variables
 osDefinitions :: Doc
-osDefinitions = text "ifeq \"$(OS)\" \"Windows_NT\"" $+$
-                text "\tTARGET_EXTENSION=.exe" $+$
-                text "\tRM=del" $+$
-                text "else" $+$
-                text "\tUNAME_S := $(shell uname -s)" $+$
-                text "\tifeq ($(UNAME_S), Linux)" $+$
-                text "\t\tTARGET_EXTENSION=" $+$
-                text "\t\tRM=rm" $+$
-                text "\tendif" $+$
-                text "\tifeq ($(UNAME_S), Darwin)" $+$
-                text "\t\tTARGET_EXTENSION=" $+$
-                text "\t\tRM=rm" $+$
-                text "\tendif" $+$
-                text "endif" $+$
-                text ""
+osDefinitions =
+  text "ifeq \"$(OS)\" \"Windows_NT\"" $+$
+  msIndent (text "TARGET_EXTENSION=.exe") $+$
+  msIndent (text "RM=del") $+$
+  text "else" $+$
+  msIndent (vcat [text "UNAME_S := $(shell uname -s)",
+    text "ifeq ($(UNAME_S), Linux)",
+    msIndent $ text "TARGET_EXTENSION=",
+    msIndent $ text "RM=rm",
+    text "endif",
+    text "ifeq ($(UNAME_S), Darwin)",
+    msIndent $ text "TARGET_EXTENSION=",
+    msIndent $ text "RM=rm",
+    text "endif"]) $+$
+  text "endif" $+$
+  text ""
 
 -- | Helper for prepending common features to a Makefile
 addCommonFeatures :: Doc -> Doc
@@ -26,3 +27,7 @@ addCommonFeatures m = osDefinitions $+$ m
 
 tab :: Doc
 tab = text "\t"
+
+-- | Makefile Syntax Indent (i.e. non recipes)
+msIndent :: Doc -> Doc
+msIndent = nest 4

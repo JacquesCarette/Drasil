d0c14d30984131606315361b2b84e9125ceb20d5
Sam Crawford <35857611+samm82@users.noreply.github.com>
2019-05-24 09:30:57 -0400

    Began Work on Projectile (#1397)
    * Added Projectile folder and picture of current work
    
    * Moved Requirements to txt
    
    * Added empty Projectile SRS
    
    * Removed Projectile pdf
    
    * Added code from GlassBR to Projectile to modify
    
    * Updated input files for Projectile
    
    * Updated Projectile calculations
    
    * Added/modified projectile input constraints
    
    * Misc fixed and modified projectile Output Format
    
    * Modified projectile Control
    
    * Added projectile Makefile
    
    * Fixed else typo and added typical input file
    
    * Added more input files
    
    * Added projectile exception input files
    
    * Only define symbDescription in preamble if will be used; closes #1394
    
    * Removed projectile output.txt and added it to .gitignore

---
+++ code/drasil-example/Drasil/Projectile/src/Calculations.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import sys
+import math
+import InputParameters
+
+
+def func_d(inParams):
+    return (2 * (inParams.v ** 2) * math.sin(math.radians(inParams.theta)) * math.cos(math.radians(inParams.theta))) / 9.8
+
+def func_short(inParams, d):
+    return (inParams.d_aim > d)
+
+def func_offset(inParams, d):
+    return abs(inParams.d_aim - d)
+
+def func_hit(inParams, offset):
+    return (offset < 0.02 * inParams.d_aim)
+
+

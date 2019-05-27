519c4d7607983d43bfc6407f4b673174f294d31a
Sam Crawford <sam.crawford82@gmail.com>
2019-05-27 12:57:02 -0400

    Added empty assumptions

--- code/drasil-example/Drasil/Projectile/Assumptions.hs
+++ code/drasil-example/Drasil/Projectile/Assumptions.hs
@@ -5,10 +5,36 @@ import Language.Drasil
 import Data.Drasil.Concepts.Documentation as Doc (assumpDom)
 
 assumptions :: [ConceptInstance]
-assumptions = [twoDMotion]
+assumptions = [twoDMotion, pointMass, equalHeights, accelZeroX, accelGravityY,
+  ignoreCurvature, freeFlight]
 
-twoDMotion :: ConceptInstance
-twoDMotion = cic "twoDMotion" twoDMotionDesc "twoDMotion" assumpDom
+twoDMotion, pointMass, equalHeights, accelZeroX, accelGravityY,
+  ignoreCurvature, freeFlight :: ConceptInstance
+twoDMotion      = cic "twoDMotion"      twoDMotionDesc      "twoDMotion"      assumpDom
+pointMass       = cic "pointMass"       pointMassDesc       "pointMass"       assumpDom
+equalHeights    = cic "equalHeights"    equalHeightsDesc    "equalHeights"    assumpDom
+accelZeroX      = cic "accelZeroX"      accelZeroXDesc      "accelZeroX"      assumpDom
+accelGravityY   = cic "accelGravityY"   accelGravityYDesc   "accelGravityY"   assumpDom
+ignoreCurvature = cic "ignoreCurvature" ignoreCurvatureDesc "ignoreCurvature" assumpDom
+freeFlight      = cic "freeFlight"      freeFlightDesc      "freeFlight"      assumpDom
 
 twoDMotionDesc :: Sentence
 twoDMotionDesc = EmptyS
+
+pointMassDesc :: Sentence
+pointMassDesc = EmptyS
+
+equalHeightsDesc :: Sentence
+equalHeightsDesc = EmptyS
+
+accelZeroXDesc :: Sentence
+accelZeroXDesc = EmptyS
+
+accelGravityYDesc :: Sentence
+accelGravityYDesc = EmptyS
+
+ignoreCurvatureDesc :: Sentence
+ignoreCurvatureDesc = EmptyS
+
+freeFlightDesc :: Sentence
+freeFlightDesc = EmptyS

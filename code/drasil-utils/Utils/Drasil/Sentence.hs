962a540275cbae24ca58f6ea62861320f720d285
Sam Crawford <sam.crawford82@gmail.com>
2019-05-28 16:00:06 -0400

    Refactored with sentHelper

--- code/drasil-utils/Utils/Drasil/Sentence.hs
+++ code/drasil-utils/Utils/Drasil/Sentence.hs
@@ -3,23 +3,26 @@ module Utils.Drasil.Sentence (andIts, andThe, isExpctdToHv, isThe, ofGiv,
 
 import Language.Drasil
 
+sentHelper :: String -> Sentence -> Sentence -> Sentence
+sentHelper inStr a b = a +:+ S inStr +:+ b
+
 andIts, andThe, isExpctdToHv, isThe, ofGiv, ofGiv', ofThe, ofThe', sOf, sOr,
   sVersus, sAnd, sAre, sIn, sIs, toThe :: Sentence -> Sentence -> Sentence
 
-andIts p1 p2 = p1 +:+ S "and its" +:+ p2
-andThe p1 p2 = p1 +:+ S "and the" +:+ p2
-isThe p1 p2 = p1 +:+ S "is the" +:+ p2
-sAnd p1 p2 = p1 +:+ S "and" +:+ p2
-sAre p1 p2 = p1 +:+ S "are" +:+ p2
-sIn p1 p2 = p1 +:+ S "in" +:+ p2
-sIs p1 p2 = p1 +:+ S "is" +:+ p2
-sOf p1 p2 = p1 +:+ S "of" +:+ p2
-sOr p1 p2 = p1 +:+ S "or" +:+ p2
-sVersus p1 p2 = p1 +:+ S "versus" +:+ p2
-toThe p1 p2 = p1 +:+ S "to the" +:+ p2
+andIts  = sentHelper "and its"
+andThe  = sentHelper "and the"
+isThe   = sentHelper "is the"
+sAnd    = sentHelper "and"
+sAre    = sentHelper "are"
+sIn     = sentHelper "in"
+sIs     = sentHelper "is"
+sOf     = sentHelper "of"
+sOr     = sentHelper "or"
+sVersus = sentHelper "versus"
+toThe   = sentHelper "to the"
 
-isExpctdToHv a b = S "The" +:+ a +:+ S "is expected to have" +:+ b
-ofGiv  p1 p2 = S "the" +:+ p1 +:+ S "of a given" +:+ p2
-ofGiv' p1 p2 = S "The" +:+ p1 +:+ S "of a given" +:+ p2
-ofThe  p1 p2 = S "the" +:+ p1 +:+ S "of the" +:+ p2
-ofThe' p1 p2 = S "The" +:+ p1 +:+ S "of the" +:+ p2
+isExpctdToHv a b = S "The" +:+ (sentHelper "is expected to have" a b)
+ofGiv        a b = S "the" +:+ (sentHelper "of a given"          a b)
+ofGiv'       a b = S "The" +:+ (sentHelper "of a given"          a b)
+ofThe        a b = S "the" +:+ (sentHelper "of the"              a b)
+ofThe'       a b = S "The" +:+ (sentHelper "of the"              a b)

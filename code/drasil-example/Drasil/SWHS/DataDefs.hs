1e0d467341e6bdd7983879954a54807494754b1d
Daniel Scime <danscime@gmail.com>
2019-05-30 08:41:35 -0400

    Refactoring melt_frac_eqn to meltFracEqn

--- code/drasil-example/Drasil/SWHS/DataDefs.hs
+++ code/drasil-example/Drasil/SWHS/DataDefs.hs
@@ -98,14 +98,14 @@ dd3HtFusion = dd dd3HtFusionQD [makeCiteInfo bueche1986 $ Page [282]]
 dd4MeltFracQD :: QDefinition
 dd4MeltFracQD = fromEqn' (melt_frac ^. uid) -- FIXME Should (^. id) be used
   (melt_frac ^. term) (S "fraction of the PCM that is liquid")
-  (eqSymb melt_frac) melt_frac_eqn 
+  (eqSymb melt_frac) meltFracEqn 
 
 --FIXME: "Phi is the melt fraction" is produced; 
   --"Phi is the fraction of the PCM that is liquid" is what is supposed to be
   -- produced according to CaseStudies' original
 
-melt_frac_eqn :: Expr
-melt_frac_eqn = (sy latentE_P) / ((sy htFusion) * (sy pcm_mass))
+meltFracEqn :: Expr
+meltFracEqn = (sy latentE_P) / ((sy htFusion) * (sy pcm_mass))
 
 dd4MeltFrac :: DataDefinition
 dd4MeltFrac = dd dd4MeltFracQD [makeCite koothoor2013] [] "melt_frac"

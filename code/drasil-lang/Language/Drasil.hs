fe541f11847fbf9fd7a3c34bb4a369ae2d7c9f5c
Daniel Scime <danscime@gmail.com>
2019-05-31 15:38:38 -0400

    Refactoring plural's to pluralPoss

--- code/drasil-lang/Language/Drasil.hs
+++ code/drasil-lang/Language/Drasil.hs
@@ -142,7 +142,7 @@ module Language.Drasil (
   , getAdd, prepend
   , LblType(RP, Citation, URI), IRefProg(..)
   -- Development.Sentence
-  , introduceAbb, phrase, plural, phrasePoss, plural's, atStart, atStart'
+  , introduceAbb, phrase, plural, phrasePoss, pluralPoss, atStart, atStart'
   , titleize, titleize'
   -- Uncertainty.Core
   , Uncertainty, uncty

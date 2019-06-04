42c29f9a911c7277e8734da3fb6d6bd637cb6925
Sam Crawford <sam.crawford82@gmail.com>
2019-06-04 12:13:01 -0400

    Minor fixes to SWHS introduction

--- code/drasil-example/Drasil/SWHS/Body.hs
+++ code/drasil-example/Drasil/SWHS/Body.hs
@@ -704,11 +704,11 @@ trace3LC6 = ["A15"]
 introP1 :: (NamedIdea en, Definition en) => ConceptChunk -> UnitalChunk -> en -> CI -> CI ->
   ConceptChunk -> UnitalChunk -> ConceptChunk -> Sentence
 introP1 es en sp pcmat pro te lh un = foldlSent [
-  S "Due to", foldlList Comma List (map S ["increasing cost", "diminishing availability",
+  S "Due to the", foldlList Comma List (map S ["increasing cost", "diminishing availability",
     "negative environmental impact of fossil fuels"]) `sC`
   S "there is a higher demand for renewable", plural es `sAnd` phrase en +:+.
-  S "storage technology", sp ^. defn, sParen (short pcmat), S "use renewable",
-  plural es `sAnd` S "provide a novel way of storing" +:+. phrase en,
+  S "storage technology", sp ^. defn, sParen (short pcmat), S "use a renewable",
+  phrase es `sAnd` S "provide a novel way of storing" +:+. phrase en,
   at_start sp, S "improve over the traditional",
   plural pro, S "because of their smaller size. The",
   S "smaller size is possible because of the ability of",

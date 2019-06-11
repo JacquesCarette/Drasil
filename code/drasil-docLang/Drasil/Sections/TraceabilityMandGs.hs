dfaccf5b4ada8f1ef2322513f60c289b5ec69a03
Gabriel D'Alimonte <gabriel.dalimonte@gmail.com>
2019-06-11 01:50:09 -0400

    Add common traceability matrices and expose all the new DocLang infrastructure.

--- code/drasil-docLang/Drasil/Sections/TraceabilityMandGs.hs
+++ code/drasil-docLang/Drasil/Sections/TraceabilityMandGs.hs
@@ -1,6 +1,7 @@
 module Drasil.Sections.TraceabilityMandGs (traceMGF, traceGIntro, generateTraceTable,
   generateTraceTableView, tvAssumps, tvDataDefns, tvGenDefns, tvTheoryModels,
-  tvInsModels, tvGoals, tvReqs, tvChanges) where
+  tvInsModels, tvGoals, tvReqs, tvChanges, traceMatAssumpOther, traceMatRefinement,
+  traceMatOtherReq, traceMatStandard) where
 
 import Language.Drasil
 import Database.Drasil(ChunkDB, SystemInformation, refbyTable, conceptinsTable,
@@ -8,9 +9,10 @@ import Database.Drasil(ChunkDB, SystemInformation, refbyTable, conceptinsTable,
   asOrderedList, dataDefnTable, insmodelTable, theoryModelTable, UMap)
 import Utils.Drasil
 
-import Data.Drasil.Concepts.Documentation (assumpDom, chgProbDom,
-  goalStmtDom, reqDom, purpose, component, dependency,
-  item, reference, section_, traceyGraph, traceyMatrix)
+import qualified Data.Drasil.IdeaDicts as Doc (genDefn, dataDefn, inModel, thModel)
+import Data.Drasil.Concepts.Documentation (assumption, assumpDom, chgProbDom,
+  goalStmt, goalStmtDom, requirement, reqDom, purpose, component, dependency,
+  item, reference, section_, traceyGraph, traceyMatrix, likelyChg, unlikelyChg)
 import Data.Drasil.Concepts.Math (graph)
 
 import Drasil.DocumentLanguage.Definitions (helpToRefField)
@@ -136,3 +138,32 @@ tvReqs = traceViewCC reqDom
 
 tvChanges :: TraceViewCat
 tvChanges = traceViewCC chgProbDom
+
+traceMatAssumpOther :: SystemInformation -> (LabelledContent, [Sentence])
+traceMatAssumpOther si = (generateTraceTableView "TraceMatAvsAll"
+  (titleize' assumption +:+ S "and Other" +:+ titleize' item) [tvAssumps]
+  [tvDataDefns, tvTheoryModels, tvGenDefns, tvInsModels, tvReqs, tvChanges] si,
+  [plural Doc.dataDefn, plural Doc.thModel, plural Doc.genDefn, plural Doc.inModel,
+  plural requirement, plural likelyChg, plural unlikelyChg +:+ S "on the" +:+. plural assumption])
+
+traceMatRefinement :: SystemInformation -> (LabelledContent, [Sentence])
+traceMatRefinement si = (generateTraceTableView "TraceMatRefvsRef"
+  (titleize' item +:+ S "and Other" +:+ titleize' section_)
+  [tvDataDefns, tvTheoryModels, tvGenDefns, tvInsModels]
+  [tvDataDefns, tvTheoryModels, tvGenDefns, tvInsModels] si,
+  [plural Doc.dataDefn, plural Doc.thModel, plural Doc.genDefn, plural Doc.inModel +:+. S "with each other"])
+
+traceMatOtherReq :: SystemInformation -> (LabelledContent, [Sentence])
+traceMatOtherReq si = (generateTraceTableView "TraceMatAllvsR"
+  (x titleize' +:+ S "and Other" +:+ titleize' item)
+  [tvDataDefns, tvTheoryModels, tvGenDefns, tvInsModels, tvReqs] [tvGoals, tvReqs] si,
+  [x plural +:+ S "on the" +:+ plural Doc.dataDefn, plural Doc.thModel, plural Doc.genDefn, plural Doc.inModel]) where
+    x g = foldl (\a (f,t) -> a `sC'` case traceMReferrers (flip f $ _sysinfodb si) $ _sysinfodb si of
+      [] -> mempty
+      _ -> g t) mempty [(tvReqs, requirement), (tvGoals, goalStmt)]
+    sC' EmptyS b = b
+    sC' a EmptyS = a
+    sC' a b = sC a b
+
+traceMatStandard :: SystemInformation -> [(LabelledContent, [Sentence])]
+traceMatStandard s = map ($ s) [traceMatAssumpOther, traceMatRefinement, traceMatOtherReq]

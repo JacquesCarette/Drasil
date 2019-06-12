9416d01682db797fa3d70726b18cb9c98a422c5f
Daniel Scime <danscime@gmail.com>
2019-06-12 11:36:34 -0400

    Refactoring dynamic to dynamic_

--- code/drasil-code/Language/Drasil/Code/Imperative/New.hs
+++ code/drasil-code/Language/Drasil/Code/Imperative/New.hs
@@ -56,7 +56,7 @@ class (ValueSym repr, PermanenceSym repr) => KeywordSym repr where
 class PermanenceSym repr where
   type Permanence repr
   static_  :: repr (Permanence repr)
-  dynamic :: repr (Permanence repr)
+  dynamic_ :: repr (Permanence repr)
 
 class (BlockSym repr) => BodySym repr where
   type Body repr

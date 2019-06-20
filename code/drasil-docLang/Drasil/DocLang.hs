5849b57917410699d5b1056b4aa041719c20e8c6
Sam Crawford <sam.crawford82@gmail.com>
2019-06-20 17:49:11 -0400

    Cleaned up DocLang exports

--- code/drasil-docLang/Drasil/DocLang.hs
+++ code/drasil-docLang/Drasil/DocLang.hs
@@ -1,79 +1,69 @@
 module Drasil.DocLang (
-    -- DocumentLanguage
-    AppndxSec(..), AuxConstntSec(..), DerivationDisplay(..), DocDesc, DocSection(..),
-    Emphasis(..), ExistingSolnSec(..), GSDSec(GSDProg2), GSDSub(UsrChars, SystCons, SysCntxt),
-    IntroSec(..), IntroSub(..), LCsSec(..), LFunc(..), Literature(Doc', Lit, Manual),
-    ProblemDescription(..), RefSec(..), RefTab(..), ReqrmntSec(..), ReqsSub(..),
-    SCSSub(..), SolChSpec(..), SSDSec(..), SSDSub(..), StkhldrSec(StkhldrProg2),
-    StkhldrSub(Client, Cstmr), TConvention(..), TraceabilitySec(TraceabilityProg),
-    TSIntro(..), UCsSec(..), mkDoc, mkEnumSimple, mkEnumSimpleD, tsymb, tsymb'', 
-    -- DocumentLanguage.Definitions
-    Field(..), Fields, InclUnits(IncludeUnits), Verbosity(Verbose), ddefn,
-    -- DocumentLanguage.RefHelpers 
-    ModelDB, ddRefDB, mdb,
-    -- DocumentLanguage.TraceabilityMatrix
-    -- Sections.AuxiliaryConstants
-    valsOfAuxConstantsF,
-    -- Sections.GeneralSystDesc
-    genSysF, 
-    -- Sections.Introduction
-    -- Sections.ReferenceMaterial
-    intro,
-    -- Sections.Requirements
-    reqF, mkInputPropsTable, mkQRTuple, mkQRTupleRef, mkValsSourceTable, 
-    -- Sections.SpecificSystemDescription
-    assumpF, dataConstraintUncertainty, dataDefnF, goalStmtF, inDataConstTbl, 
-    inModelF, outDataConstTbl, physSystDesc, probDescF, termDefnF, specSysDescr,
-    -- Sections.Stakeholders
-    -- Sections.TableOfAbbAndAcronyms
-    -- Sections.TableOfSymbols
-    -- Sections.TableOfUnits
-    -- Sections.TraceabilityMandGs
-    traceGIntro, traceMGF, generateTraceTable, generateTraceTableView,
-    traceMatStandard, tvAssumps, tvDataDefns, tvGenDefns, tvTheoryModels,
-    tvInsModels, tvGoals, tvReqs, tvChanges, traceMatAssumpOther,
-    traceMatRefinement, traceMatOtherReq,
-    -- ExtractDocDesc
-    getDocDesc, egetDocDesc, ciGetDocDesc, generateTraceMap,
-    -- Tracetable
-    getTraceMapFromTM, getTraceMapFromGD,
-    getTraceMapFromDD, getTraceMapFromIM, getSCSSub, generateTraceMap',
-   -- Labels
-    goalStmtLabel, solutionLabel, characteristicsLabel,
-    physSystDescriptionLabel
-    ) where 
+  -- DocumentLanguage
+  AppndxSec(..), AuxConstntSec(..), DerivationDisplay(..), DocDesc, DocSection(..),
+  Emphasis(..), ExistingSolnSec(..), GSDSec(GSDProg2), GSDSub(UsrChars, SystCons, SysCntxt),
+  IntroSec(..), IntroSub(..), LCsSec(..), LFunc(..), Literature(Doc', Lit, Manual),
+  ProblemDescription(..), RefSec(..), RefTab(..), ReqrmntSec(..), ReqsSub(..),
+  SCSSub(..), SolChSpec(..), SSDSec(..), SSDSub(..), StkhldrSec(StkhldrProg2),
+  StkhldrSub(Client, Cstmr), TConvention(..), TraceabilitySec(TraceabilityProg),
+  TSIntro(..), UCsSec(..), mkDoc, mkEnumSimpleD, tsymb, tsymb'', 
+  -- DocumentLanguage.Definitions
+  Field(..), Fields, InclUnits(IncludeUnits), Verbosity(Verbose), ddefn,
+  -- DocumentLanguage.RefHelpers 
+  ModelDB, ddRefDB, mdb,
+  -- DocumentLanguage.TraceabilityMatrix
+  -- Sections.AuxiliaryConstants
+  -- Sections.GeneralSystDesc
+  -- Sections.Introduction
+  -- Sections.ReferenceMaterial
+  intro,
+  -- Sections.Requirements
+  mkInputPropsTable, mkQRTuple, mkQRTupleRef, mkValsSourceTable, 
+  -- Sections.SpecificSystemDescription
+  dataConstraintUncertainty, goalStmtF, inDataConstTbl, outDataConstTbl, physSystDesc, termDefnF,
+  -- Sections.Stakeholders
+  -- Sections.TableOfAbbAndAcronyms
+  -- Sections.TableOfSymbols
+  -- Sections.TableOfUnits
+  -- Sections.TraceabilityMandGs
+  traceMatStandard,
+  -- ExtractDocDesc
+  getDocDesc, egetDocDesc, ciGetDocDesc, generateTraceMap,
+  -- Tracetable
+  getTraceMapFromTM, getTraceMapFromGD, getTraceMapFromDD,
+  getTraceMapFromIM, getSCSSub, generateTraceMap',
+ -- Labels
+  solutionLabel, characteristicsLabel, physSystDescriptionLabel
+) where 
 
-import Drasil.DocumentLanguage (AppndxSec(..), AuxConstntSec(..), DerivationDisplay(..), DocDesc, DocSection(..),
-    Emphasis(..), ExistingSolnSec(..), GSDSec(GSDProg2), GSDSub(UsrChars, SystCons, SysCntxt),
-    IntroSec(..), IntroSub(..), LCsSec(..), LFunc(..), Literature(Doc', Lit, Manual),
-    ProblemDescription(..), RefSec(..), RefTab(..), ReqrmntSec(..), ReqsSub(..),
-    SCSSub(..), SolChSpec(..), SSDSec(..), SSDSub(..), StkhldrSec(StkhldrProg2),
-    StkhldrSub(Client, Cstmr), TConvention(..), TraceabilitySec(TraceabilityProg),
-    TSIntro(..), UCsSec(..), mkDoc, mkEnumSimple, mkEnumSimpleD, tsymb, tsymb'')
-import Drasil.DocumentLanguage.Definitions (Field(..), Fields, 
-    InclUnits(IncludeUnits), Verbosity(Verbose), ddefn)
+import Drasil.DocumentLanguage (AppndxSec(..), AuxConstntSec(..), DerivationDisplay(..),
+  DocDesc, DocSection(..), Emphasis(..), ExistingSolnSec(..), GSDSec(GSDProg2),
+  GSDSub(UsrChars, SystCons, SysCntxt), IntroSec(..), IntroSub(..), LCsSec(..),
+  LFunc(..), Literature(Doc', Lit, Manual), ProblemDescription(..), RefSec(..),
+  RefTab(..), ReqrmntSec(..), ReqsSub(..), SCSSub(..), SolChSpec(..), SSDSec(..),
+  SSDSub(..), StkhldrSec(StkhldrProg2), StkhldrSub(Client, Cstmr), TConvention(..),
+  TraceabilitySec(TraceabilityProg), TSIntro(..), UCsSec(..), mkDoc, mkEnumSimpleD,
+  tsymb, tsymb'')
+import Drasil.DocumentLanguage.Definitions (Field(..), Fields, InclUnits(IncludeUnits),
+  Verbosity(Verbose), ddefn)
 import Drasil.DocumentLanguage.RefHelpers (ModelDB, ddRefDB, mdb)
 --import Drasil.DocumentLanguage.TraceabilityMatrix
-import Drasil.Sections.AuxiliaryConstants (valsOfAuxConstantsF)
-import Drasil.Sections.GeneralSystDesc (genSysF)
+--import Drasil.Sections.AuxiliaryConstants
+--import Drasil.Sections.GeneralSystDesc
 --import Drasil.Sections.Introduction
 import Drasil.Sections.ReferenceMaterial (intro)
-import Drasil.Sections.Requirements (reqF, mkInputPropsTable, mkQRTuple, mkQRTupleRef,
+import Drasil.Sections.Requirements (mkInputPropsTable, mkQRTuple, mkQRTupleRef,
     mkValsSourceTable)
-import Drasil.Sections.SpecificSystemDescription (assumpF, 
-    dataConstraintUncertainty, dataDefnF, goalStmtF, inDataConstTbl, inModelF, 
-    outDataConstTbl, physSystDesc, probDescF, termDefnF, specSysDescr)
+import Drasil.Sections.SpecificSystemDescription (dataConstraintUncertainty,
+    goalStmtF, inDataConstTbl, outDataConstTbl, physSystDesc, termDefnF)
 --import Drasil.Sections.Stakeholders
 --import Drasil.Sections.TableOfAbbAndAcronyms
 --import Drasil.Sections.TableOfSymbols
 --import Drasil.Sections.TableOfUnits
-import Drasil.Sections.TraceabilityMandGs (traceGIntro, traceMGF, generateTraceTable,
-    generateTraceTableView, traceMatStandard, tvAssumps, tvDataDefns, tvGenDefns,
-    tvTheoryModels, tvInsModels, tvGoals, tvReqs, tvChanges, traceMatAssumpOther,
-    traceMatRefinement, traceMatOtherReq)
+import Drasil.Sections.TraceabilityMandGs (traceMatStandard)
 import Drasil.ExtractDocDesc (getDocDesc, egetDocDesc, ciGetDocDesc)
 import Drasil.TraceTable (generateTraceMap, getTraceMapFromTM, getTraceMapFromGD,
     getTraceMapFromDD, getTraceMapFromIM, getSCSSub, generateTraceMap')
 -- Commented out modules aren't used - uncomment if this changes
-import Drasil.DocumentLanguage.Labels (goalStmtLabel, solutionLabel, characteristicsLabel,
-    physSystDescriptionLabel)
+import Drasil.DocumentLanguage.Labels (solutionLabel, characteristicsLabel,
+  physSystDescriptionLabel)

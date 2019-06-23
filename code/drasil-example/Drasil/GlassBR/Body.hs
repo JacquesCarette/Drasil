571d7367f7e5094a689609d42df988a547612b73
Sam Crawford <35857611+samm82@users.noreply.github.com>
2019-06-23 10:39:27 -0400

    Automate Physical System Description (#1604)
    * Added PhySysDecs constructor and fixed examples accordingly
    
    * Used PhySysDesc in NoPCM
    
    * Used PhySysDesc in SWHS
    
    * Used PhySysDesc in GlassBR
    
    * Used PhySysDesc in SSP
    
    * Removed unused import from DocLang
    
    * Made PSD parts list unlabeled

--- code/drasil-example/Drasil/GlassBR/Body.hs
+++ code/drasil-example/Drasil/GlassBR/Body.hs
@@ -16,28 +16,25 @@ import Utils.Drasil
 import Drasil.DocLang (AppndxSec(..), AuxConstntSec(..), DerivationDisplay(..), 
   DocDesc, DocSection(..), Field(..), Fields, GSDSec(GSDProg2), GSDSub(..), 
   InclUnits(IncludeUnits), IntroSec(IntroProg), IntroSub(IChar, IOrgSec, IPurpose, IScope), 
-  LCsSec'(..), ProblemDescription(..), PDSub(Goals), RefSec(RefProg),
+  LCsSec'(..), ProblemDescription(..), PDSub(..), RefSec(RefProg),
   RefTab(TAandA, TUnits), ReqrmntSec(..), ReqsSub(..), SCSSub(..),
   SSDSec(..), SSDSub(..), SolChSpec(..), StkhldrSec(StkhldrProg2), 
   StkhldrSub(Client, Cstmr), TraceabilitySec(TraceabilityProg), 
   TSIntro(SymbOrder, TSPurpose), UCsSec(..), Verbosity(Verbose),
-  dataConstraintUncertainty, inDataConstTbl, intro, mkDoc, 
-  outDataConstTbl, physSystDesc, termDefnF, tsymb, generateTraceMap,
-  getTraceMapFromTM, getTraceMapFromGD, getTraceMapFromDD, getTraceMapFromIM,
-  getSCSSub, traceMatStandard, characteristicsLabel, physSystDescriptionLabel,
-  generateTraceMap')
+  dataConstraintUncertainty, inDataConstTbl, intro, mkDoc, outDataConstTbl,
+  termDefnF, tsymb, generateTraceMap, getTraceMapFromTM, getTraceMapFromGD,
+  getTraceMapFromDD, getTraceMapFromIM, getSCSSub, traceMatStandard,
+  characteristicsLabel, generateTraceMap')
 
-import qualified Drasil.DocLang.SRS as SRS (reference, valsOfAuxCons,
-  assumpt, inModel)
+import qualified Drasil.DocLang.SRS as SRS (reference, valsOfAuxCons, assumpt, inModel)
 
 import Data.Drasil.Concepts.Computation (computerApp, inDatum, inParam, compcon, algorithm)
 import Data.Drasil.Concepts.Documentation as Doc (analysis, appendix, aspect,
   assumption, characteristic, company, condition, content, dataConst, datum,
   definition, doccon, doccon', document, emphasis, environment, goal,
-  information, input_, interface, model, organization, physical, physSyst,
-  problem, product_, purpose, reference, software, softwareConstraint,
-  softwareSys, srsDomains, standard, sysCont, system, template, term_, user,
-  value, variable)
+  information, input_, interface, model, organization, physical, problem,
+  product_, purpose, reference, software, softwareConstraint, softwareSys,
+  srsDomains, standard, sysCont, system, template, term_, user, value, variable)
 import qualified Data.Drasil.Concepts.Documentation as Doc (srs, code)
 import Data.Drasil.IdeaDicts as Doc (inModel, thModel)
 import qualified Data.Drasil.IdeaDicts as Doc (dataDefn)
@@ -154,7 +151,9 @@ mkSRS = [RefSec $ RefProg intro [TUnits, tsymb [TSPurpose, SymbOrder], TAandA],
     UsrChars [userCharacteristicsIntro], SystCons [] [] ],
   SSDSec $
     SSDProg
-      [SSDProblem $ PDProg prob [termsAndDesc, physSystDescription] [Goals goalInputs goals],
+      [SSDProblem $ PDProg prob [termsAndDesc]
+        [ PhySysDesc glassBR physSystParts physSystFig []
+        , Goals goalInputs goals],
        SSDSolChSpec $ SCSProg
         [ Assumptions
         , TMs [] (Label : stdFields) tMods
@@ -206,10 +205,6 @@ systInfo = SI {
    refdb       = refDB
 }
   --FIXME: All named ideas, not just acronyms.
-
-termsAndDesc, physSystDescription :: Section
-
-physSystDescriptionList, appdxIntro :: Contents
 
 inputDataConstraints, outputDataConstraints :: LabelledContent
 
@@ -384,31 +379,18 @@ prob = foldlSent_ [S "efficiently" `sAnd` S "correctly predict whether a",
 
 {--Terminology and Definitions--}
 
+termsAndDesc :: Section
 termsAndDesc = termDefnF (Just (S "All" `sOf` S "the" +:+ plural term_ +:+
   S "are extracted from" +:+ makeCiteS astm2009 `sIn`
   makeRef2S (SRS.reference ([]::[Contents]) ([]::[Section])))) [termsAndDescBullets]
 
 {--Physical System Description--}
 
-physSystDescription = physSystDesc (short glassBR) physSystFig 
-  [physSystDescriptionList, LlC physSystFig]
-
-physSystDescriptionList = LlC $ enumSimple physSystDescriptionLabel 1 (short physSyst) physSystDescriptionListPhysys
-
---"Dead" knowledge?
-physSystDescriptionListPhysys :: [Sentence]
-physSystDescriptionListPhysys1 :: Sentence
-physSystDescriptionListPhysys2 :: NamedIdea n => n -> Sentence
-
-physSystDescriptionListPhysys = [physSystDescriptionListPhysys1, physSystDescriptionListPhysys2 ptOfExplsn]
-
-physSystDescriptionListPhysys1 = S "The" +:+. phrase glaSlab
-
-physSystDescriptionListPhysys2 imprtntElem = foldlSent [S "The"
-  +:+. phrase imprtntElem, S "Where the", phrase bomb `sC`
-  S "or", (blast ^. defn) `sC` S "is located. The", phrase sD
-  `isThe` phrase distance, S "between the", phrase imprtntElem `sAnd`
-  S "the glass"]
+physSystParts :: [Sentence]
+physSystParts = [S "The" +:+. phrase glaSlab,
+  foldlSent [S "The" +:+. phrase ptOfExplsn, S "Where the", phrase bomb `sC`
+  S "or", (blast ^. defn) `sC` S "is located. The", phrase sD `isThe`
+  phrase distance, S "between the", phrase ptOfExplsn `sAnd` S "the glass"]]
 
 {--Goal Statements--}
 
@@ -454,6 +436,7 @@ traceabilityMatrices = traceMatStandard systInfo
 
 {--APPENDIX--}
 
+appdxIntro :: Contents
 appdxIntro = foldlSP [
   S "This", phrase appendix, S "holds the", plural graph,
   sParen (makeRef2S demandVsSDFig `sAnd` makeRef2S dimlessloadVsARFig),

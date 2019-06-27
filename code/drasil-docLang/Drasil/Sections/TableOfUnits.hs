c62c268db809b8de78287f01064d13c706f0edc5
Sam Crawford <35857611+samm82@users.noreply.github.com>
2019-06-27 14:45:22 -0400

    Formatting from Projectile (#1634)
    * Added column for SI name in Table of Units
    
    * Updated definition of pi
    
    * Capitalized 1D, 2D, and 3D in table of A and A
    
    * Linted

--- code/drasil-docLang/Drasil/Sections/TableOfUnits.hs
+++ code/drasil-docLang/Drasil/Sections/TableOfUnits.hs
@@ -1,6 +1,6 @@
 -- Standard code to make a table of units
 -- First true example of a (small!) recipe.
-module Drasil.Sections.TableOfUnits(tableOfUnits, unit_table) where
+module Drasil.Sections.TableOfUnits (tableOfUnits) where
 
 import Control.Lens ((^.))
 import Prelude hiding (id)
@@ -9,14 +9,12 @@ import Data.Drasil.Concepts.Documentation (symbol_, description)
 
 -- | Table of units section builder. Takes a list of units and an introduction
 tableOfUnits :: IsUnit s => [s] -> Contents -> Section
-tableOfUnits u intro = Section (S "Table of Units") [Con intro, Con $ LlC (unit_table u)]
+tableOfUnits u intro = Section (S "Table of Units") [Con intro, Con $ LlC (unitTable u)]
   (makeSecRef "ToU" "ToU")
 
 -- | Creates the actual table of units from a list of units
-unit_table :: IsUnit s => [s] -> LabelledContent
-unit_table u = llcc (makeTabRef "ToU") $ Table
-  (map atStart [symbol_, description])  (mkTable
-  [Sy . usymb,
-   \x -> (x ^. defn) +:+ sParen (phrase x)
-  ] u)
+unitTable :: IsUnit s => [s] -> LabelledContent
+unitTable u = llcc (makeTabRef "ToU") $ Table
+  [atStart symbol_, atStart description, S "SI Name"]
+  (mkTable [Sy . usymb, (^. defn), phrase] u)
   (S "Table of Units") False

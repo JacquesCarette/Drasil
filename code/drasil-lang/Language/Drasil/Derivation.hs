026acd0cab1a76bbdad9ad73b092224e22c76dc3
Sam Crawford <sam.crawford82@gmail.com>
2019-07-02 14:08:58 -0400

    Changed Derivation to have a header

--- code/drasil-lang/Language/Drasil/Derivation.hs
+++ code/drasil-lang/Language/Drasil/Derivation.hs
@@ -1,8 +1,18 @@
+{-# Language TemplateHaskell #-}
 module Language.Drasil.Derivation where
 
-import Language.Drasil.Sentence (Sentence)
+import Control.Lens (makeLenses)
+
+import Language.Drasil.Sentence (Sentence(S), (+:))
 
 -- Derivations are an ordered list of sentences and expressions.
 -- They are rendered in order as paragraphs and equation blocks to display
 -- the derivation.
-type Derivation = [Sentence]
+data Derivation = Deriv { _title :: Sentence, _deriv :: [Sentence] }
+makeLenses ''Derivation
+
+mkDeriv :: Sentence -> [Sentence] -> Derivation
+mkDeriv = Deriv
+
+mkDerivName :: Sentence -> [Sentence] -> Derivation
+mkDerivName s = Deriv (S "Detailed derivation of" +: s)

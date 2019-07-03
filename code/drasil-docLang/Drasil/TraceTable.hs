a0b1cc8dc7752f12cd535a23d4468a1c831ed2bf
Gabriel D'Alimonte <gabriel.dalimonte@gmail.com>
2019-07-03 18:28:05 -0400

    Remove old DocLang passes.

--- code/drasil-docLang/Drasil/TraceTable.hs
+++ code/drasil-docLang/Drasil/TraceTable.hs
@@ -6,12 +6,11 @@ import Drasil.DocumentLanguage.Core
 import Language.Drasil
 import Language.Drasil.Development (lnames')
 import Database.Drasil (TraceMap, traceMap)
-import Theory.Drasil (DataDefinition, GenDefn, InstanceModel, TheoryModel, Theory(..))
+import Theory.Drasil (Theory(..))
 
 import Control.Lens ((^.))
 import Data.Functor.Constant (Constant(Constant))
 import Data.Generics.Multiplate (foldFor, preorderFold, purePlate)
-import Data.Maybe (mapMaybe)
 
 dependencyPlate :: DLPlate (Constant [(UID, [UID])])
 dependencyPlate = preorderFold $ purePlate {
@@ -41,55 +40,5 @@ dependencyPlate = preorderFold $ purePlate {
   notes :: HasAdditionalNotes a => a -> [Sentence]
   notes = (^. getNotes)
 
-getTraceMapFromDocSec :: [DocSection] -> SSDSec
-getTraceMapFromDocSec (SSDSec ssd:_) = ssd
-getTraceMapFromDocSec (_:tl)         = getTraceMapFromDocSec tl
-getTraceMapFromDocSec []             = error "No SSDSec found."
-
-getTraceMapFromSSDSec :: SSDSec -> [SSDSub]
-getTraceMapFromSSDSec (SSDProg s) = s
-
-getTraceMapFromSSDSub :: [SSDSub] -> SolChSpec
-getTraceMapFromSSDSub (SSDSolChSpec s:_) = s
-getTraceMapFromSSDSub (_:tl)             = getTraceMapFromSSDSub tl
-getTraceMapFromSSDSub []                 = error "No SolChSpec found."
-
-getTraceMapFromSolCh :: SolChSpec -> [SCSSub]
-getTraceMapFromSolCh (SCSProg s) = s
-
-getTraceMapFromTM :: [SCSSub] -> [TheoryModel]
-getTraceMapFromTM (TMs _ _ t:_) = t
-getTraceMapFromTM (_:tl)        = getTraceMapFromTM tl
-getTraceMapFromTM []            = error "No TM found."
-
-getTraceMapFromGD :: [SCSSub] -> [GenDefn]
-getTraceMapFromGD (GDs _ _ gd _:_) = gd
-getTraceMapFromGD (_:tl)           = getTraceMapFromGD tl
-getTraceMapFromGD []               = []
-
-getTraceMapFromDD :: [SCSSub] -> [DataDefinition]
-getTraceMapFromDD l = concat $ mapMaybe getDD l
-  where getDD (DDs _ _ d _) = Just d
-        getDD _           = Nothing
-
-getTraceMapFromIM :: [SCSSub] -> [InstanceModel]
-getTraceMapFromIM (IMs _ _ imod _:_) = imod
-getTraceMapFromIM (_:tl)             = getTraceMapFromIM tl
-getTraceMapFromIM []                 = []
-
-extractSFromNotes :: HasAdditionalNotes l => l -> [Sentence]
-extractSFromNotes c = c ^. getNotes
-
-extractSFromDeriv :: HasDerivation l => l -> [Sentence]
-extractSFromDeriv c = c ^. derivations
-
-getSCSSub :: [DocSection] -> [SCSSub]
-getSCSSub a = getTraceMapFromSolCh $ getTraceMapFromSSDSub $ getTraceMapFromSSDSec
- $ getTraceMapFromDocSec a
-
 generateTraceMap :: [DocSection] -> TraceMap
 generateTraceMap = traceMap . concatMap (foldFor docSec dependencyPlate)
-
--- This is a hack as ConceptInstance cannot be collected yet.
-generateTraceMap' :: [ConceptInstance] -> TraceMap
-generateTraceMap' = traceMap . map (\x -> (x ^. uid, lnames' [x ^. defn]))

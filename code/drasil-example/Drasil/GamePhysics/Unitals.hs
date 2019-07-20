37bee2a663f0c137fe426eb89e0fd24e47c28461
Olu <owojaiyo@mcmaster.ca>
2019-07-20 11:45:11 -0400

    replaced nonNegativeConstraint with gtZeroConstr

--- code/drasil-example/Drasil/GamePhysics/Unitals.hs
+++ code/drasil-example/Drasil/GamePhysics/Unitals.hs
@@ -19,6 +19,7 @@ import Data.Drasil.Units.Physics (accelU, angVelU, impulseU, momtInertU,
   torqueU, velU)
 
 import Control.Lens((^.))
+import Data.Drasil.Constraints (gtZeroConstr)
 
 defSymbols :: [DefinedQuantityDict]
 defSymbols = map dqdWr unitSymbs ++ map dqdWr inputConstraints ++
@@ -303,9 +304,9 @@ outputConstraints = map (`uq` defaultUncrt)
 nonNegativeConstraint :: Constraint -- should be pulled out and put somewhere for generic constraints
 nonNegativeConstraint = physc $ UpFrom (Inc,0)
 
-lengthCons     = constrained' QPP.len               [nonNegativeConstraint] (dbl 44.2)
-massCons       = constrained' QPP.mass              [nonNegativeConstraint] (dbl 56.2)
-mmntOfInCons   = constrained' QP.momentOfInertia    [nonNegativeConstraint] (dbl 74.5)
+lengthCons     = constrained' QPP.len               [gtZeroConstr] (dbl 44.2)
+massCons       = constrained' QPP.mass              [gtZeroConstr] (dbl 56.2)
+mmntOfInCons   = constrained' QP.momentOfInertia    [gtZeroConstr] (dbl 74.5)
 gravAccelCons  = constrained' QP.gravitationalConst [] (QP.gravitationalConstValue ^. defnExpr)
 posCons        = constrained' QP.position           [] (dbl 0.412) --FIXME: should be (0.412, 0.502) vector
 veloCons       = constrained' QP.velocity           [] (dbl 2.51)

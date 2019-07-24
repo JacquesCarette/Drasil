c7fbb0449517a04a6738f6181053b675b89340ba
danscime <danscime@gmail.com>
2019-07-24 12:25:08 -0400

    Adding second traceability matrix to projectile jupyter

--- People/DanScime/Jupyter-Tests/TraceyScript.hs
+++ People/DanScime/Jupyter-Tests/TraceyScript.hs
@@ -4,7 +4,7 @@ import Data.List.Split
 
 toLink :: String -> String
 toLink l = "<a href=#" ++ l1 ++ ">" ++ l ++ "</a>"
-  where l1 = last (splitOn ": " l)
+  where l1 = (\y -> head y ++ ":" ++ last y) (splitOn ": " l)
 
 leftAlign :: String -> String
 leftAlign = (":" ++)
@@ -15,7 +15,7 @@ main = do
   let s1 = lines s
   let s2 = map (splitOn "|") s1
   let s3 = (map toLink $ head s2) : (map leftAlign $ head $ tail s2) :
-         (map (\x -> toLink (head (tail x)) : tail (tail x))) (tail $ tail s2)
+         (map (\x -> ("|" ++) (toLink (head (tail x))) : tail (tail x))) (tail $ tail s2)
 
 
   writeFile "output.txt" (intercalate "\n" $ map (intercalate "|") s3)

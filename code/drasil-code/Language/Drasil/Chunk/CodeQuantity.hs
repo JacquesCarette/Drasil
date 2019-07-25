dfe68400a87263b7a153c5fa607ee04bc95cc0cf
Brooks MacLachlan <brooksel_14@hotmail.com>
2019-07-25 16:53:32 -0400

    Added more constructors for CodeQuantityDict

--- code/drasil-code/Language/Drasil/Chunk/CodeQuantity.hs
+++ code/drasil-code/Language/Drasil/Chunk/CodeQuantity.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Language.Drasil.Chunk.CodeQuantity 
-  (HasCodeType(ctyp), CodeQuantityDict, cqw) where
+  (HasCodeType(ctyp), CodeQuantityDict, cqd, implCQD, cqw) where
 
 import Control.Lens (Lens', (^.), makeLenses, view)
 
@@ -26,6 +26,18 @@ instance HasSymbol   CodeQuantityDict where symbol = view symb'
 instance HasCodeType CodeQuantityDict where ctyp = typ'
 instance Eq          CodeQuantityDict where a == b = (a ^. uid) == (b ^. uid)
 instance MayHaveUnit CodeQuantityDict where getUnit = view unit'
+
+cqd :: String -> NP -> Maybe String -> CodeType -> (Stage -> Symbol) -> 
+  Maybe UnitDefn -> CodeQuantityDict
+cqd s np a = CQD (mkIdea s np a)
+
+-- For CodeQuantityDict with implementation-only symbol
+implCQD :: String -> NP -> Maybe String -> CodeType -> Symbol -> 
+  Maybe UnitDefn -> CodeQuantityDict
+implCQD s np a t sym = cqd s np a t f
+  where f :: Stage -> Symbol
+        f Implementation = sym
+        f Equational = Empty
 
 cqw :: (Quantity q, MayHaveUnit q) => q -> CodeQuantityDict
 cqw q = CQD (nw q) (spaceToCodeType $ q^.typ) (symbol q) (getUnit q)

1b03f901560a6f653aacbb505a349a29aa419ff3
Sam Crawford <sam.crawford82@gmail.com>
2019-07-29 16:30:05 -0400

    Tidied up code

--- code/drasil-docLang/Drasil/DocLang/SRS.hs
+++ code/drasil-docLang/Drasil/DocLang/SRS.hs
@@ -1,13 +1,12 @@
-module Drasil.DocLang.SRS
- (doc, doc', intro, prpsOfDoc, scpOfReq, charOfIR, orgOfDoc, stakeholder, theCustomer, theClient,
-  genSysDes, sysCont, userChar, sysCon, scpOfTheProj, prodUCTable, indPRCase, specSysDes,
-  probDesc, termAndDefn, termogy, physSyst, goalStmt, solCharSpec, assumpt, thModel,
-  genDefn, inModel, dataDefn, datCon, require, nonfuncReq, funcReq, likeChg, unlikeChg, 
-  traceyMandG, appendix, reference, propCorSol, offShelfSol, valsOfAuxCons,
-  physSystLabel, datConLabel, genDefnLabel, thModelLabel, dataDefnLabel, 
-  inModelLabel, likeChgLabel, tOfSymbLabel, valsOfAuxConsLabel, referenceLabel,
-  indPRCaseLabel, unlikeChgLabel, assumptLabel, funcReqLabel,
-  tOfSymb, tOfUnit, solCharSpecLabel) where
+module Drasil.DocLang.SRS (appendix, assumpt, assumptLabel, charOfIR, datCon,
+  datConLabel, dataDefn, dataDefnLabel, doc, doc', funcReq, funcReqLabel, genDefn,
+  genDefnLabel, genSysDes, goalStmt, inModel, inModelLabel, indPRCase,
+  indPRCaseLabel, intro, likeChg, likeChgLabel, nonfuncReq, offShelfSol, orgOfDoc,
+  physSyst, physSystLabel, probDesc, prodUCTable, propCorSol, prpsOfDoc, reference,
+  referenceLabel, require, scpOfReq, scpOfTheProj, solCharSpec, solCharSpecLabel,
+  specSysDes, stakeholder, sysCon, sysCont, tOfSymb, tOfSymbLabel, tOfUnit,
+  termAndDefn, termogy, thModel, thModelLabel, theClient, theCustomer, traceyMandG,
+  unlikeChg, unlikeChgLabel, userChar, valsOfAuxCons, valsOfAuxConsLabel) where
 --Temporary file for keeping the "srs" document constructor until I figure out
 -- a better place for it. Maybe Data.Drasil or Language.Drasil.Template?
 
@@ -44,56 +43,57 @@ doc' sys = Document (Doc.srs `forTT'` sys)
 intro, prpsOfDoc, scpOfReq, charOfIR, orgOfDoc, stakeholder, theCustomer, theClient, 
   genSysDes, sysCont, userChar, sysCon, scpOfTheProj, prodUCTable, indPRCase, specSysDes,
   probDesc, termAndDefn, termogy, physSyst, goalStmt, solCharSpec, assumpt, thModel,
-  genDefn, inModel, dataDefn, datCon, propCorSol, require, nonfuncReq, funcReq, likeChg, traceyMandG, tOfSymb, tOfUnit,
-  appendix, reference, offShelfSol, valsOfAuxCons, unlikeChg :: [Contents] -> [Section] -> Section
+  genDefn, inModel, dataDefn, datCon, propCorSol, require, nonfuncReq, funcReq, likeChg,
+  unlikeChg, traceyMandG, valsOfAuxCons, appendix, reference, offShelfSol, tOfSymb,
+  tOfUnit :: [Contents] -> [Section] -> Section
 
-intro       cs ss = section' (titleize Doc.introduction) cs ss "Intro"
-prpsOfDoc   cs ss = section' (titleize Doc.prpsOfDoc) cs ss "DocPurpose"
-scpOfReq    cs ss = section' (titleize Doc.scpOfReq)  cs ss "ReqsScope"
-charOfIR    cs ss = section' (titleize' Doc.charOfIR) cs ss "ReaderChars"
-orgOfDoc    cs ss = section' (titleize Doc.orgOfDoc)  cs ss "DocOrg"
+intro     cs ss = section' (titleize Doc.introduction) cs ss "Intro"
+prpsOfDoc cs ss = section' (titleize Doc.prpsOfDoc)    cs ss "DocPurpose"
+scpOfReq  cs ss = section' (titleize Doc.scpOfReq)     cs ss "ReqsScope"
+charOfIR  cs ss = section' (titleize' Doc.charOfIR)    cs ss "ReaderChars"
+orgOfDoc  cs ss = section' (titleize Doc.orgOfDoc)     cs ss "DocOrg"
 
-stakeholder cs ss = section' (titleize' Doc.stakeholder) cs ss "Stakeholder"
+stakeholder cs ss = section' (titleize' Doc.stakeholder)      cs ss "Stakeholder"
 theCustomer cs ss = section' (titleizeNP $ the' Doc.customer) cs ss "Customer"
-theClient   cs ss = section' (titleizeNP $ the' Doc.client) cs ss "Client"
+theClient   cs ss = section' (titleizeNP $ the' Doc.client)   cs ss "Client"
 
-genSysDes   cs ss = section' (titleize Doc.generalSystemDescription) cs ss "GenSysDesc"
-sysCont     cs ss = section' (titleize Doc.sysCont)              cs ss  "SysContext"
-userChar    cs ss = section' (titleize' Doc.userCharacteristic)  cs ss  "UserChars"
-sysCon      cs ss = section' (titleize' Doc.systemConstraint)    cs ss  "SysConstraints"
+genSysDes cs ss = section' (titleize Doc.generalSystemDescription) cs ss "GenSysDesc"
+sysCont   cs ss = section' (titleize Doc.sysCont)                  cs ss "SysContext"
+userChar  cs ss = section' (titleize' Doc.userCharacteristic)      cs ss "UserChars"
+sysCon    cs ss = section' (titleize' Doc.systemConstraint)        cs ss "SysConstraints"
 
 scpOfTheProj cs ss = section' (atStart (Doc.scpOfTheProj titleize)) cs ss "ProjScope"
-prodUCTable cs ss  = section' (titleize Doc.prodUCTable)      cs ss      "UseCaseTable"
-indPRCase   cs ss  = section (titleize' Doc.indPRCase)       cs ss      indPRCaseLabel
+prodUCTable  cs ss = section' (titleize Doc.prodUCTable)            cs ss "UseCaseTable"
+indPRCase    cs ss = section  (titleize' Doc.indPRCase)             cs ss indPRCaseLabel
 
 specSysDes  cs ss = section' (titleize Doc.specificsystemdescription) cs ss "SpecSystDesc"
-probDesc    cs ss = section' (titleize Doc.problemDescription) cs ss "ProbDesc"
-termAndDefn cs ss = section' (titleize' Doc.termAndDef)        cs ss "TermDefs"
-termogy     cs ss = section' (titleize Doc.terminology)        cs ss "Terminology"
-physSyst    cs ss = section (titleize Doc.physSyst)           cs ss physSystLabel
-goalStmt    cs ss = section' (titleize' Doc.goalStmt)          cs ss "GoalStmt"
-solCharSpec cs ss = section (titleize Doc.solutionCharSpec)   cs ss solCharSpecLabel
-assumpt     cs ss = section (titleize' Doc.assumption)        cs ss assumptLabel
-thModel     cs ss = section (titleize' Doc.thModel)           cs ss thModelLabel
-genDefn     cs ss = section (titleize' Doc.genDefn)           cs ss genDefnLabel
-inModel     cs ss = section (titleize' Doc.inModel)           cs ss inModelLabel
-dataDefn    cs ss = section (titleize' Doc.dataDefn)          cs ss dataDefnLabel
-datCon      cs ss = section (titleize' Doc.datumConstraint)   cs ss datConLabel
+probDesc    cs ss = section' (titleize Doc.problemDescription)        cs ss "ProbDesc"
+termAndDefn cs ss = section' (titleize' Doc.termAndDef)               cs ss "TermDefs"
+termogy     cs ss = section' (titleize Doc.terminology)               cs ss "Terminology"
+physSyst    cs ss = section  (titleize Doc.physSyst)                  cs ss physSystLabel
+goalStmt    cs ss = section' (titleize' Doc.goalStmt)                 cs ss "GoalStmt"
+solCharSpec cs ss = section  (titleize Doc.solutionCharSpec)          cs ss solCharSpecLabel
+assumpt     cs ss = section  (titleize' Doc.assumption)               cs ss assumptLabel
+thModel     cs ss = section  (titleize' Doc.thModel)                  cs ss thModelLabel
+genDefn     cs ss = section  (titleize' Doc.genDefn)                  cs ss genDefnLabel
+inModel     cs ss = section  (titleize' Doc.inModel)                  cs ss inModelLabel
+dataDefn    cs ss = section  (titleize' Doc.dataDefn)                 cs ss dataDefnLabel
+datCon      cs ss = section  (titleize' Doc.datumConstraint)          cs ss datConLabel
 
-propCorSol  cs ss = section' (titleize' Doc.propOfCorSol)      cs ss "CorSolProps"
+propCorSol cs ss = section' (titleize' Doc.propOfCorSol) cs ss "CorSolProps"
 
-require     cs ss = section' (titleize' Doc.requirement)      cs ss "Requirements"
-nonfuncReq  cs ss = section (titleize' Doc.nonfunctionalRequirement) cs ss nonfuncReqLabel
-funcReq     cs ss = section (titleize' Doc.functionalRequirement) cs ss funcReqLabel
+require    cs ss = section' (titleize' Doc.requirement)              cs ss "Requirements"
+nonfuncReq cs ss = section  (titleize' Doc.nonfunctionalRequirement) cs ss nonfuncReqLabel
+funcReq    cs ss = section  (titleize' Doc.functionalRequirement)    cs ss funcReqLabel
 
-likeChg     cs ss = section (titleize' Doc.likelyChg)        cs ss likeChgLabel
-unlikeChg   cs ss = section (titleize' Doc.unlikelyChg)      cs ss unlikeChgLabel
+likeChg   cs ss = section (titleize' Doc.likelyChg)   cs ss likeChgLabel
+unlikeChg cs ss = section (titleize' Doc.unlikelyChg) cs ss unlikeChgLabel
 
-traceyMandG cs ss   = section' (titleize' Doc.traceyMandG)      cs ss "TraceMatrices"
-valsOfAuxCons cs ss = section (titleize Doc.consVals)        cs ss valsOfAuxConsLabel
-appendix    cs ss   = section' (titleize Doc.appendix)          cs ss "Appendix"
+traceyMandG   cs ss = section' (titleize' Doc.traceyMandG) cs ss "TraceMatrices"
+valsOfAuxCons cs ss = section  (titleize Doc.consVals)     cs ss valsOfAuxConsLabel
+appendix      cs ss = section' (titleize Doc.appendix)     cs ss "Appendix"
 
-reference   cs ss = section (titleize' Doc.reference)        cs ss referenceLabel
+reference   cs ss = section  (titleize' Doc.reference)        cs ss referenceLabel
 offShelfSol cs ss = section' (titleize' Doc.offShelfSolution) cs ss "offShelfSolns"
 
 tOfSymb cs ss = section  (titleize Doc.tOfSymb) cs ss tOfSymbLabel

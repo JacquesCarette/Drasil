5be55b3f1daf4a229aa082a5a3dfb3ebaee65d14
Brooks MacLachlan <brooksel_14@hotmail.com>
2019-08-07 11:06:33 -0400

    Updated stable

--- code/stable/projectile/src/cpp/InputConstraints.cpp
+++ code/stable/projectile/src/cpp/InputConstraints.cpp
@@ -19,13 +19,34 @@ using std::ofstream;
 
 void input_constraints(InputParameters &inParams) {
     if (!(inParams.v_launch > 0)) {
-        std::cout << "Warning: constraint violated" << std::endl;
+        std::cout << "Warning: ";
+        std::cout << "v_launch has value ";
+        std::cout << inParams.v_launch;
+        std::cout << " but suggested to be ";
+        std::cout << "above ";
+        std::cout << 0;
+        std::cout << "." << std::endl;
     }
     if (!(0 < inParams.theta && inParams.theta < 3.14159265 / 2)) {
-        std::cout << "Warning: constraint violated" << std::endl;
+        std::cout << "Warning: ";
+        std::cout << "theta has value ";
+        std::cout << inParams.theta;
+        std::cout << " but suggested to be ";
+        std::cout << "between ";
+        std::cout << 0;
+        std::cout << " and ";
+        std::cout << (3.14159265 / 2);
+        std::cout << " ((pi)/(2))";
+        std::cout << "." << std::endl;
     }
     if (!(inParams.p_target > 0)) {
-        std::cout << "Warning: constraint violated" << std::endl;
+        std::cout << "Warning: ";
+        std::cout << "p_target has value ";
+        std::cout << inParams.p_target;
+        std::cout << " but suggested to be ";
+        std::cout << "above ";
+        std::cout << 0;
+        std::cout << "." << std::endl;
     }
 }
 
--- code/stable/projectile/src/python/InputConstraints.py
+++ code/stable/projectile/src/python/InputConstraints.py
@@ -10,10 +10,31 @@ import InputParameters
 # \param inParams structure holding the input values
 def input_constraints(inParams):
     if (not(inParams.v_launch > 0)) :
-        print("Warning: constraint violated")
+        print("Warning: ", end='')
+        print("v_launch has value ", end='')
+        print(inParams.v_launch, end='')
+        print(" but suggested to be ", end='')
+        print("above ", end='')
+        print(0, end='')
+        print(".")
     if (not(0 < inParams.theta and inParams.theta < 3.14159265 / 2)) :
-        print("Warning: constraint violated")
+        print("Warning: ", end='')
+        print("theta has value ", end='')
+        print(inParams.theta, end='')
+        print(" but suggested to be ", end='')
+        print("between ", end='')
+        print(0, end='')
+        print(" and ", end='')
+        print(3.14159265 / 2, end='')
+        print(" ((pi)/(2))", end='')
+        print(".")
     if (not(inParams.p_target > 0)) :
-        print("Warning: constraint violated")
+        print("Warning: ", end='')
+        print("p_target has value ", end='')
+        print(inParams.p_target, end='')
+        print(" but suggested to be ", end='')
+        print("above ", end='')
+        print(0, end='')
+        print(".")
 
 

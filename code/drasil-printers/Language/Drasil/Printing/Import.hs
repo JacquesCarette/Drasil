3340a32ba47797981a9e4db0fd212106572476ac
Brooks MacLachlan <brooksel_14@hotmail.com>
2019-08-09 16:53:22 -0400

    Treat strings as labels, not identifiers

--- code/drasil-printers/Language/Drasil/Printing/Import.hs
+++ code/drasil-printers/Language/Drasil/Printing/Import.hs
@@ -31,7 +31,7 @@ space Radians = error "Radians not translated"
 space (Vect _) = error "Vector space not translated"
 space (DiscreteI _) = error "DiscreteI" --ex. let A = {1, 2, 4, 7}
 space (DiscreteD _) = error "DiscreteD" -- [Double]
-space (DiscreteS l) = P.Fenced P.Curly P.Curly $ P.Row $ intersperse (P.MO P.Comma) $ map P.Ident l --ex. let Meal = {"breakfast", "lunch", "dinner"}
+space (DiscreteS l) = P.Fenced P.Curly P.Curly $ P.Row $ intersperse (P.MO P.Comma) $ map P.Label l --ex. let Meal = {"breakfast", "lunch", "dinner"}
 
 {-
 p_space :: Space -> String

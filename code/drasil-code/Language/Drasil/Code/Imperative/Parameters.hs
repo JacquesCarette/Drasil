46587f5f8c48d6959844d241ef6b5a5b36a2b32a
Brooks MacLachlan <brooksel_14@hotmail.com>
2019-08-12 12:30:31 -0400

    Moved functions for getting parameters to Parameters

---
+++ code/drasil-code/Language/Drasil/Code/Imperative/Parameters.hs
@@ -0,0 +1,70 @@
+module Language.Drasil.Code.Imperative.Parameters(
+  getInputFormatIns, getInputFormatOuts, getDerivedIns, getDerivedOuts,
+  getConstraintParams, getCalcParams, getOutputParams
+) where
+
+getInputFormatIns :: Reader State [CodeChunk]
+getInputFormatIns = do
+  g <- ask
+  let getIns :: Structure -> [CodeChunk]
+      getIns Unbundled = []
+      getIns Bundled = [codevar inParams]
+  getParams $ codevar inFileName : getIns (inStruct g)
+
+getInputFormatOuts :: Reader State [CodeChunk]
+getInputFormatOuts = do
+  g <- ask
+  let getOuts :: Structure -> [CodeChunk]
+      getOuts Unbundled = extInputs $ csi $ codeSpec g
+      getOuts Bundled = []
+  getParams $ getOuts (inStruct g)
+
+getDerivedIns :: Reader State [CodeChunk]
+getDerivedIns = do
+  g <- ask
+  let s = csi $ codeSpec g
+      dvals = derivedInputs s
+      reqdVals = concatMap (flip codevars (sysinfodb s) . codeEquat) dvals
+  getParams reqdVals
+
+getDerivedOuts :: Reader State [CodeChunk]
+getDerivedOuts = do
+  g <- ask
+  let getOuts :: Structure -> [CodeChunk]
+      getOuts Unbundled = map codeChunk $ derivedInputs $ csi $ codeSpec g
+      getOuts Bundled = []
+  getParams $ getOuts (inStruct g)
+
+getConstraintParams :: Reader State [CodeChunk]
+getConstraintParams = do 
+  g <- ask
+  let cm = cMap $ csi $ codeSpec g
+      mem = eMap $ codeSpec g
+      db = sysinfodb $ csi $ codeSpec g
+      varsList = filter (\i -> member (i ^. uid) cm) (inputs $ csi $ codeSpec g)
+      reqdVals = nub $ varsList ++ concatMap (\v -> constraintvarsandfuncs v db 
+        mem) (getConstraints cm varsList)
+  getParams reqdVals
+
+getCalcParams :: CodeDefinition -> Reader State [CodeChunk]
+getCalcParams c = do
+  g <- ask
+  getParams $ codevars' (codeEquat c) $ sysinfodb $ csi $ codeSpec g
+
+getOutputParams :: Reader State [CodeChunk]
+getOutputParams = do
+  g <- ask
+  getParams $ outputs $ csi $ codeSpec g
+
+getParams :: (CodeIdea c) => [c] -> Reader State [CodeChunk]
+getParams cs' = do
+  g <- ask
+  let cs = map codeChunk cs'
+      ins = inputs $ csi $ codeSpec g
+      consts = map codeChunk $ constants $ csi $ codeSpec g
+      inpVars = filter (`elem` ins) cs
+      conVars = filter (`elem` consts) cs
+      csSubIns = cs \\ (ins ++ consts)
+      inVs = getInputVars (inStruct g) inpVars
+      conVs = getConstVars conVars
+  return $ inVs ++ conVs ++ csSubIns

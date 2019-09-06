d360e351570607fb9ffbaf1cc13b78dd58382203
Brooks MacLachlan <brooksel_14@hotmail.com>
2019-09-06 18:28:29 -0400

    Updated stable

--- code/stable/projectile/src/cpp/InputFormat.cpp
+++ code/stable/projectile/src/cpp/InputFormat.cpp
@@ -17,7 +17,7 @@ using std::ofstream;
 
 #include "InputParameters.hpp"
 
-void get_input(string filename, InputParameters &inParams) {
+void get_input(InputParameters &inParams, string filename) {
     ifstream infile;
     infile.open(filename, std::fstream::in);
     infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');

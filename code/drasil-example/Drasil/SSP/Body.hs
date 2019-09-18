919608d32977ad3e3a3a6f951c1ac6eb6038ee7e
Brooks MacLachlan <brooksel_14@hotmail.com>
2019-09-18 12:29:50 -0400

    Added commas

--- code/drasil-example/Drasil/SSP/Body.hs
+++ code/drasil-example/Drasil/SSP/Body.hs
@@ -182,7 +182,7 @@ refDB = rdb citations concIns
 tableOfSymbIntro :: [TSIntro]
 tableOfSymbIntro = [TSPurpose, TypogConvention [Verb $ foldlSent_
   [S "a subscript", ch index, S "indicates that the", phrase value, 
-  S "will be taken at and analyzed at a", phrase slice `sOr` phrase slice, 
+  S "will be taken at, and analyzed at, a", phrase slice `sOr` phrase slice, 
   S "interface composing the total slip", phrase mass]], VectorUnits]
 
 -- SECTION 1.3 --

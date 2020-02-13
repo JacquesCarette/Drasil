6271e591b723904378b6513626e03663cc555879
Brooks MacLachlan <brooksel_14@hotmail.com>
2020-02-13 11:47:06 -0500

    Update projectile to build many program family members

--- code/drasil-example/Drasil/Projectile/Main.hs
+++ code/drasil-example/Drasil/Projectile/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
-import Language.Drasil.Code (Choices(..), CodeSpec, Comments(..), 
+import Language.Drasil (getAccStr)
+import Language.Drasil.Code (Choices(..), Comments(..), 
   Verbosity(..), ConstraintBehaviour(..), ImplementationType(..), Lang(..), 
   Logging(..), Modularity(..), Structure(..), ConstantStructure(..), 
   ConstantRepr(..), InputModule(..), CodeConcept(..), matchConcepts, 
@@ -11,31 +12,65 @@ import Language.Drasil.Printers (DocSpec(DocSpec), DocType(SRS, Website))
 import Data.Drasil.Quantities.Math (piConst)
 
 import Drasil.Projectile.Body (printSetting, si, srs)
+import Drasil.Projectile.Concepts (projectileTitle)
 
-code :: CodeSpec
-code = codeSpec si choices []
-
-choices :: Choices
-choices = Choices {
-  lang = [Python, Cpp, CSharp, Java],
-  modularity = Modular Combined,
-  impType = Program,
-  logFile = "log.txt",
-  logging = LogNone,
-  comments = [CommentFunc, CommentClass, CommentMod],
-  doxVerbosity = Quiet,
-  dates = Hide,
-  onSfwrConstraint = Warning,
-  onPhysConstraint = Warning,
-  inputStructure = Bundled,
-  constStructure = Store Unbundled,
-  constRepr = Var,
-  conceptMatch = matchConcepts [piConst] [[Pi]],
-  auxFiles = [SampleInput]
-}
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory, 
+  setCurrentDirectory)
 
 main :: IO()
 main = do
   gen (DocSpec SRS     "Projectile_SRS") srs printSetting
   gen (DocSpec Website "Projectile_SRS") srs printSetting
-  genCode choices code
+  genCodeWithChoices (zip choiceCombos [1..])
+
+genCodeWithChoices :: [(Choices, Int)] -> IO ()
+genCodeWithChoices [] = return ()
+genCodeWithChoices ((c,n):cns) = let dir = getAccStr projectileTitle ++ show n 
+  in do
+    workingDir <- getCurrentDirectory
+    createDirectoryIfMissing False dir
+    setCurrentDirectory dir
+    genCode c (codeSpec si c [])
+    setCurrentDirectory workingDir
+    genCodeWithChoices cns
+
+modularities :: [Modularity]
+modularities = [Unmodular, Modular Combined, Modular Separated]
+
+loggings :: [Logging]
+loggings = [LogNone, LogAll]
+
+-- comms :: [[Comments]]
+-- comms = [[], [CommentFunc, CommentClass, CommentMod]]
+
+inputStructs :: [Structure]
+inputStructs = [Bundled, Unbundled]
+
+constStructs :: [ConstantStructure]
+constStructs = [Inline, Store Bundled, Store Unbundled, WithInputs]
+
+constReprs :: [ConstantRepr]
+constReprs = [Var, Const]
+
+choiceCombos :: [Choices]
+choiceCombos = [buildChoices m l i cs cr | m <- modularities, l <- loggings, i <- inputStructs, cs <- constStructs, cr <- constReprs]
+
+buildChoices :: Modularity -> Logging -> Structure -> 
+  ConstantStructure -> ConstantRepr -> Choices
+buildChoices m l i cs cr = Choices {
+  lang = [Python, Cpp, CSharp, Java],
+  modularity = m,
+  impType = Program,
+  logFile = "log.txt",
+  logging = l,
+  comments = [CommentFunc, CommentClass, CommentMod],
+  doxVerbosity = Quiet,
+  dates = Hide,
+  onSfwrConstraint = Warning,
+  onPhysConstraint = Warning,
+  inputStructure = i,
+  constStructure = cs,
+  constRepr = cr,
+  conceptMatch = matchConcepts [piConst] [[Pi]],
+  auxFiles = [SampleInput]
+}
--- code/scripts/code_build.sh
+++ code/scripts/code_build.sh
@@ -9,6 +9,25 @@ fi
 
 RET=0
 
+if [ "$EDIR" = "Projectile" ]; then
+  cd "$BUILD_FOLDER$EDIR"
+  E_DIR=$(pwd)
+  for d in */; do
+    cd "$d"
+    if [ -d "$EXAMPLE_CODE_SUBFOLDER" ]; then
+      cd "$EXAMPLE_CODE_SUBFOLDER"
+      OLD_DIR=$(pwd)
+      for dr in */; do
+        cd "$dr"
+        "$MAKE" $TARGET
+        RET=$(( $RET || $? ))
+        cd "$OLD_DIR"
+      done
+    fi
+    cd "$E_DIR"
+  done
+fi
+
 if [ -d "$BUILD_FOLDER$EDIR/$EXAMPLE_CODE_SUBFOLDER" ]; then
   cd "$BUILD_FOLDER$EDIR/$EXAMPLE_CODE_SUBFOLDER"
   OLD_DIR=$(pwd)

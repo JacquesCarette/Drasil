cb35024c50e1e5ab7652c2da702f12fa9d942db2
Brooks MacLachlan <brooksel_14@hotmail.com>
2020-03-03 13:29:35 -0500

    Add check that CodeChunk passed to new is actually a function

--- code/drasil-code/Language/Drasil/CodeExpr.hs
+++ code/drasil-code/Language/Drasil/CodeExpr.hs
@@ -2,9 +2,12 @@ module Language.Drasil.CodeExpr (new) where
 
 import Language.Drasil
 
-import Language.Drasil.Chunk.Code (CodeChunk)
+import Language.Drasil.Chunk.Code (CodeChunk(..), VarOrFunc(..))
 
 import Control.Lens ((^.))
 
 new :: CodeChunk -> [Expr] -> Expr
-new c = New (c ^. uid)
+new c ps = checkFunc (kind c)
+  where checkFunc Var = error $ "Attempt to create new actor but passed " ++
+          "CodeChunk is not callable"
+        checkFunc Func = New (c ^. uid) ps

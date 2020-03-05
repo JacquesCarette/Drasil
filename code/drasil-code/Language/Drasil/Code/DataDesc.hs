31ff78120cfb6198bd5101b496cf1f2ab2b67a21
Brooks MacLachlan <brooksel_14@hotmail.com>
2020-03-05 12:58:06 -0500

    New AST could not handle combination of intramixing within lists and intermixing between lists -- added a new level to the AST to distinguish these cases

--- code/drasil-code/Language/Drasil/Code/DataDesc.hs
+++ code/drasil-code/Language/Drasil/Code/DataDesc.hs
@@ -10,7 +10,38 @@ type DataItem = CodeVarChunk
 
 data DataDesc' = DD Data' Delimiter DataDesc' | End Data'
 
-data Data' = Data Integer [DataItem] [Delimiter] | Junk 
+data Data' = Datum DataItem' 
+  | Data -- To be used in cases where multiple list-type data have their 
+         -- elements intermixed, and thus need to be described together
+    [DataItem'] -- The DataItems being simultaneously described. 
+                -- The intra-list delimiters for any shared dimensions should 
+                -- be the same between the DataItems. For example, if mixing 2 
+                -- lists of same dimension, the intra-list delimiters must be 
+                -- the same between the two lists. If mixing a 1-D list with a 
+                -- 2-D list, the first delimiter must be the same, but the 2nd 
+                -- delimiter for the 2-D list is not constrained because there 
+                -- is no corresponding delimiter for the 1-D list.
+    Integer -- Degree of intermixing
+            -- 0 <= degree of intermixing <= minimum dimension of the DataItems
+            -- Ex. 2 2-D lists with 1 degree of intermixing (and 0 intramixing):
+              -- x11 x12, y11 y12; x21 x22, y21 y22
+            -- Ex. 2 2-D lists with 2 degrees of intermixing (and 0 intramixing):
+              -- x11, y11 x12, y12; x21, y21 x22, y22
+    Delimiter -- Delimiter between elements from different lists
+  | Junk -- Data that can be ignored/skipped over
+
+data DataItem' = DI 
+  CodeChunk -- The datum being described
+  Integer -- Degree of intramixing
+          -- 0 <= degree of intramixing < dimension of datum
+          -- Ex. 2D list with 0 degree of intramixing:
+            -- x11, x12; x21, x22
+          -- Ex. 2D list with 1 degree of intramixing:
+            -- x11, x21; x12, x22
+  [Delimiter] -- Delimiters between list elements. 
+              -- Size of list should equal dimension of datum 
+              -- Ex. a 1-D list needs 1 delimiter, a 2-D list needs 2 delimiters
+              -- Outermost delimiter first, innermost last
 
 type Delimiter = String
 
@@ -21,14 +52,17 @@ dataDesc [d] _ = End d
 dataDesc (d:ds) dlm = DD d dlm (dataDesc ds dlm) 
 dataDesc [] _ = error "DataDesc must have at least one data item"
 
-singleton' :: DataItem -> Data'
-singleton' d = Data 0 [d] []
+singleton' :: CodeChunk -> Data'
+singleton' d = Datum $ DI d 0 []
 
-list :: DataItem -> Delimiter -> Data
-list d dlm = Data 0 [d] [dlm]
+list :: CodeChunk -> Delimiter -> Data'
+list d dlm = Datum $ DI d 0 [dlm]
 
-interwovenLists :: [DataItem] -> [Delimiter] -> Data'
-interwovenLists = Data (length d)
+interwovenLists :: [DataItem'] -> Integer -> Delimiter -> Data'
+interwovenLists = Data
+
+junk :: Data'
+junk = Junk
 
 -- Old DataDesc
 

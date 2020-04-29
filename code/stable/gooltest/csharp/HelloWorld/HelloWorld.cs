d8165e3961c6c175e9edd07e9b34244294ad3be1
Brooks MacLachlan <brooksel_14@hotmail.com>
2020-04-29 11:39:33 -0400

    Add generated test files to stable

---
+++ code/stable/gooltest/csharp/HelloWorld/HelloWorld.cs
@@ -0,0 +1,171 @@
+/** \file HelloWorld.cs
+    \author Brooks MacLachlan
+    \brief Tests various GOOL functions. It should run without errors.
+*/
+using System;
+using System.Collections.Generic;
+
+public class HelloWorld {
+    
+    public static void Main(string[] args) {
+        // Initializing variables
+        int a;
+        int b = 5;
+        List<double> myOtherList = new List<double> {1.0, 1.5};
+        int oneIndex = myOtherList.IndexOf(1.0);
+        Console.WriteLine(oneIndex);
+        a = myOtherList.Count;
+        myOtherList.Insert(2, 2.0);
+        myOtherList.Add(2.5);
+        double e;
+        e = myOtherList[1];
+        myOtherList[1] = 17.4;
+        List<string> myName = new List<string>(7);
+        myName = new List<string>("Brooks Mac".Split(' '));
+        Console.Write("[");
+        for (int list_i1 = 0; list_i1 < myName.Count - 1; list_i1++) {
+            Console.Write(myName[list_i1]);
+            Console.Write(", ");
+        }
+        if (myName.Count > 0) {
+            Console.Write(myName[myName.Count - 1]);
+        }
+        Console.WriteLine("]");
+        List<Boolean> boringList = new List<Boolean> {false, false, false, false, false};
+        Console.Write("[");
+        for (int list_i1 = 0; list_i1 < boringList.Count - 1; list_i1++) {
+            Console.Write(boringList[list_i1]);
+            Console.Write(", ");
+        }
+        if (boringList.Count > 0) {
+            Console.Write(boringList[boringList.Count - 1]);
+        }
+        Console.WriteLine("]");
+        List<double> mySlicedList = new List<double>(2);
+        
+        List<double> temp = new List<double>(0);
+        for (int i_temp = 1; i_temp < 3; i_temp++) {
+            temp.Add(myOtherList[i_temp]);
+        }
+        mySlicedList = temp;
+        
+        if (b >= 6) {
+            string dummy = "dummy";
+        }
+        else if (b == 5) {
+            // If body ----------------------------------------------------------------
+            int c;
+            int d;
+            a = 5;
+            b = a + 2;
+            c = b + 3;
+            d = b;
+            d = d - a;
+            c = c - d;
+            b += 17;
+            c += 17;
+            a++;
+            d++;
+            c = c - 1;
+            b = b - 1;
+            List<int> myList = new List<int>(5);
+            char myObj = 'o';
+            const string myConst = "Imconstant";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.Write("[");
+            for (int list_i1 = 0; list_i1 < myOtherList.Count - 1; list_i1++) {
+                Console.Write(myOtherList[list_i1]);
+                Console.Write(", ");
+            }
+            if (myOtherList.Count > 0) {
+                Console.Write(myOtherList[myOtherList.Count - 1]);
+            }
+            Console.WriteLine("]");
+            Console.Write("[");
+            for (int list_i1 = 0; list_i1 < mySlicedList.Count - 1; list_i1++) {
+                Console.Write(mySlicedList[list_i1]);
+                Console.Write(", ");
+            }
+            if (mySlicedList.Count > 0) {
+                Console.Write(mySlicedList[mySlicedList.Count - 1]);
+            }
+            Console.WriteLine("]");
+            Console.WriteLine("Type an int");
+            d = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Type another");
+            Console.ReadLine();
+            
+            Console.WriteLine(" too");
+            Console.Write("boo");
+            Console.Write(true);
+            Console.Write(0);
+            Console.Write('c');
+            Console.WriteLine(!(true));
+            Console.WriteLine(-1);
+            Console.WriteLine(Math.Sqrt(4.0));
+            Console.WriteLine(Math.Abs(-4));
+            Console.WriteLine(Math.Log10(2.0));
+            Console.WriteLine(Math.Log(2.0));
+            Console.WriteLine(Math.Exp(-2.0));
+            Console.WriteLine(Math.Sin(2.0));
+            Console.WriteLine(Math.Cos(2.0));
+            Console.WriteLine(Math.Tan(2.0));
+            Console.WriteLine(Math.Tan(2.0));
+            Console.WriteLine(true && false);
+            Console.WriteLine(true || false);
+            Console.WriteLine(true && !(false));
+            Console.WriteLine(!(true && true));
+            Console.WriteLine(6 + 2);
+            Console.WriteLine(6 - 2);
+            Console.WriteLine(6 * 2);
+            Console.WriteLine(6 / 2);
+            Console.WriteLine(6 % 4);
+            Console.WriteLine(Math.Pow(6, 2));
+            Console.WriteLine(6 + 2 * 3);
+            Console.WriteLine(1.0 / Math.Sin(1.0));
+            Console.WriteLine(1.0 / Math.Cos(1.0));
+            Console.WriteLine(a);
+            Console.WriteLine(true ? 5 : 0);
+            Console.WriteLine(1.0 / Math.Tan(1.0));
+            // End If body ------------------------------------------------------------
+        }
+        else {
+            Console.WriteLine(args[5]);
+        }
+        if (boringList != null) {
+            Console.WriteLine("Ew, boring list!");
+        }
+        else {
+            Console.WriteLine("Great, no bores!");
+        }
+        switch(a) {
+            case 5:
+                b = 10;
+                break;
+            case 0:
+                b = 5;
+                break;
+            default:
+                b = 0;
+                break;
+        };
+        for (int i = 0; i < 9; i += 1) {
+            Console.WriteLine(i);
+        }
+        while (a < 13) {
+            Console.WriteLine("Hello");
+            a++;
+        }
+        foreach (double num in myOtherList) {
+            Console.WriteLine(Helper.doubleAndAdd(num, 1.0));
+        }
+        try {
+            throw new Exception("Good-bye!");
+        } catch {
+            Console.WriteLine("Caught intentional error");
+        }
+    }
+}
---
+++ code/stable/gooltest/java/HelloWorld/HelloWorld/HelloWorld.java
@@ -0,0 +1,142 @@
+package HelloWorld;
+
+/** \file HelloWorld.java
+    \author Brooks MacLachlan
+    \brief Tests various GOOL functions. It should run without errors.
+*/
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        // Initializing variables
+        int a;
+        int b = 5;
+        ArrayList<Double> myOtherList = new ArrayList<Double>(Arrays.asList(1.0, 1.5));
+        int oneIndex = myOtherList.indexOf(1.0);
+        System.out.println(oneIndex);
+        a = myOtherList.size();
+        myOtherList.add(2, 2.0);
+        myOtherList.add(2.5);
+        double e;
+        e = myOtherList.get(1);
+        myOtherList.set(1, 17.4);
+        ArrayList<String> myName = new ArrayList<String>(7);
+        myName = new ArrayList<String>(Arrays.asList("Brooks Mac".split(" ")));
+        System.out.println(myName);
+        ArrayList<Boolean> boringList = new ArrayList<Boolean>(Arrays.asList(false, false, false, false, false));
+        System.out.println(boringList);
+        ArrayList<Double> mySlicedList = new ArrayList<Double>(2);
+        
+        ArrayList<Double> temp = new ArrayList<Double>(0);
+        for (int i_temp = 1; i_temp < 3; i_temp++) {
+            temp.add(myOtherList.get(i_temp));
+        }
+        mySlicedList = temp;
+        
+        if (b >= 6) {
+            String dummy = "dummy";
+        }
+        else if (b == 5) {
+            // If body ----------------------------------------------------------------
+            int c;
+            int d;
+            a = 5;
+            b = a + 2;
+            c = b + 3;
+            d = b;
+            d = d - a;
+            c = c - d;
+            b += 17;
+            c += 17;
+            a++;
+            d++;
+            c = c - 1;
+            b = b - 1;
+            ArrayList<Integer> myList = new ArrayList<Integer>(5);
+            char myObj = 'o';
+            final String myConst = "Imconstant";
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(myOtherList);
+            System.out.println(mySlicedList);
+            System.out.println("Type an int");
+            d = Integer.parseInt((new Scanner(System.in)).nextLine());
+            System.out.println("Type another");
+            (new Scanner(System.in)).next();
+            
+            System.out.println(" too");
+            System.out.print("boo");
+            System.out.print(true);
+            System.out.print(0);
+            System.out.print('c');
+            System.out.println(!(true));
+            System.out.println(-1);
+            System.out.println(Math.sqrt(4.0));
+            System.out.println(Math.abs(-4));
+            System.out.println(Math.log10(2.0));
+            System.out.println(Math.log(2.0));
+            System.out.println(Math.exp(-2.0));
+            System.out.println(Math.sin(2.0));
+            System.out.println(Math.cos(2.0));
+            System.out.println(Math.tan(2.0));
+            System.out.println(Math.tan(2.0));
+            System.out.println(true && false);
+            System.out.println(true || false);
+            System.out.println(true && !(false));
+            System.out.println(!(true && true));
+            System.out.println(6 + 2);
+            System.out.println(6 - 2);
+            System.out.println(6 * 2);
+            System.out.println(6 / 2);
+            System.out.println(6 % 4);
+            System.out.println(Math.pow(6, 2));
+            System.out.println(6 + 2 * 3);
+            System.out.println(1.0 / Math.sin(1.0));
+            System.out.println(1.0 / Math.cos(1.0));
+            System.out.println(a);
+            System.out.println(true ? 5 : 0);
+            System.out.println(1.0 / Math.tan(1.0));
+            // End If body ------------------------------------------------------------
+        }
+        else {
+            System.out.println(args[5]);
+        }
+        if (boringList != null) {
+            System.out.println("Ew, boring list!");
+        }
+        else {
+            System.out.println("Great, no bores!");
+        }
+        switch(a) {
+            case 5:
+                b = 10;
+                break;
+            case 0:
+                b = 5;
+                break;
+            default:
+                b = 0;
+                break;
+        };
+        for (int i = 0; i < 9; i += 1) {
+            System.out.println(i);
+        }
+        while (a < 13) {
+            System.out.println("Hello");
+            a++;
+        }
+        for (double num : myOtherList) {
+            System.out.println(Helper.doubleAndAdd(num, 1.0));
+        }
+        try {
+            throw new Exception("Good-bye!");
+        } catch (Exception exc) {
+            System.out.println("Caught intentional error");
+        }
+    }
+}

0050f292aebeca40c521bca1aa2e60681b59d85f
Nathaniel-Hu <hun4@mcmaster.ca>
2020-05-26 15:55:20 -0400

    Skeleton Sample Code to address Issue #801
    unstable

---
+++ code/drasil-printers/Language/Drasil/DOT/TraceabilityGraphGenerator.hs
@@ -0,0 +1,25 @@
+module Language.Drasil.DOT.TraceabilityGraphGenerator (genTraceGraph) where
+
+-- import needed 
+
+import Language.Drasil.DOT.TraceabilityGraphGeneratorInfo as TGGI
+import System.IO
+
+type CaseStudy = String
+type Chunk = String
+
+-- input chunks to be included in generated traceability graph, outputs .dot file
+-- e.g. sample usage: genTraceGraph SWHS [TMods, IMods, GenDefs]
+genTraceGraph :: CaseStudy -> [Chunk] -> IO ()
+genTraceGraph caseStudy [chunk] = do
+	nodeDefs <- TGGI.getInfo ["all"]
+	dotFile <- openFile (caseStudy ++ "_TG.dot") WriteMode
+
+	hPutStrLn dotFile ("digraph " ++ caseStudy ++ "_TG {")
+	hPutStrLn dotFile ("\tnewrank=true; rankdir=TB;\n")
+
+	sequence_ (map (hPutStrLn dotFile) nodeDefs)
+
+	hPutStrLn dotFile ("}")
+
+	hClose dotFile

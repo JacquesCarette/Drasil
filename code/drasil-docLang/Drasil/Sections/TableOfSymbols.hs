df31d4e6612e667c98c37b83238911e87d22143a
muhammad ali <alim102@mcmaster.ca>
2020-06-05 13:24:52 -0400

    Preventing duplicate symbols hlint

--- code/drasil-docLang/Drasil/Sections/TableOfSymbols.hs
+++ code/drasil-docLang/Drasil/Sections/TableOfSymbols.hs
@@ -2,22 +2,24 @@
 module Drasil.Sections.TableOfSymbols (table, symbTableRef) where
 
 import Language.Drasil
+
 import Data.List (nub, (\\))
+import Control.Lens (view)
+import Data.Bifunctor (first)
+import Text.PrettyPrint.HughesPJ (render)
+
 import Drasil.DocumentLanguage.Units (toSentence)
 import Data.Drasil.Concepts.Documentation (symbol_, description, tOfSymb)
 import Data.Drasil.Concepts.Math (unit_)
-import Control.Lens (view)
 import Language.Drasil.Printers (symbolDoc)
-import Text.PrettyPrint.HughesPJ (render)
 
---Removed SymbolForm Constraint and filtered non-symbol'd chunks 
+--Removed SymbolForm Constraint, filtered non-symbol'd chunks and checks for duplicate symbol error
 -- | table of symbols creation function
 table :: (Quantity s, MayHaveUnit s) => Stage -> [s] -> (s -> Sentence) -> LabelledContent
 table st ls f
     |noDuplicate = llcc symbTableRef $
       Table [atStart symbol_, atStart description, atStart' unit_]
-      (mkTable [P . (`symbol` st), f, toSentence]
-      $ filteredChunks)
+      (mkTable [P . (`symbol` st), f, toSentence] filteredChunks)
       (titleize tOfSymb) True
     | otherwise = error $ "Same symbols for different quantities found "  ++ show symUidPairDuplicates
     where 
@@ -27,7 +29,7 @@ table st ls f
         symUidPair     = zip symbolsCol uidCol
         symDuplicates  = symbolsCol \\ nub symbolsCol
         noDuplicate    = null symDuplicates
-        symUidPairDuplicates = [(render . symbolDoc $ fst x, snd x) | x <- symUidPair, 
+        symUidPairDuplicates = [(first (render . symbolDoc) x, snd x) | x <- symUidPair, 
           fst x `elem` symDuplicates]
 
 symbTableRef :: Reference

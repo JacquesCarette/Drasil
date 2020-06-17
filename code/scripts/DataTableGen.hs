10e1270db53fbe611f8fec7715942735ff19dccd
Nathaniel-Hu <hun4@mcmaster.ca>
2020-06-17 17:00:12 -0400

    FilePath Changes + HLint Edits
    DataTableGen.hs
    - added new variables to temporarily store full updated file paths (for testing)
    - made HLint suggested edits to code
    (i.e. removed redundant brackets, where statements, uses of map; used guards, more function composition)
    
    SourceCodeReader.hs
    - made HLint suggested edits to code (i.e. removed redundant brackets, uses of map, used more function composition)

--- code/scripts/DataTableGen.hs
+++ code/scripts/DataTableGen.hs
@@ -31,11 +31,16 @@ main = do
   -- prints all drasil- package subdirectories
   DC.iterator filtered
 
+  let curDir1 = "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil"
+      curDir2 = "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-gool/GOOL/Drasil"
+      curDir3 = "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil/Document"
+      curDir4 = "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil/Data"
+
   -- easy + hard test for classes, medium test for data/newtypes, easy test for data types
-  entry1 <- createEntry "Classes.hs" "/Users/Nathaniel_Hu/Desktop/Co-op/Issue#703" classInstances
-  entry2 <- createEntry "RendererClasses.hs" "/Users/Nathaniel_Hu/Desktop/Co-op/Issue#703" classInstances
-  entry3 <- createEntry "Core.hs" "/Users/Nathaniel_Hu/Desktop/Co-op/Issue#703" classInstances
-  entry4 <- createEntry "Date.hs" "/Users/Nathaniel_Hu/Desktop/Co-op/Issue#703" classInstances
+  entry1 <- createEntry "Classes.hs" curDir1 classInstances
+  entry2 <- createEntry "RendererClasses.hs" curDir2 classInstances
+  entry3 <- createEntry "Core.hs" curDir3 classInstances
+  entry4 <- createEntry "Date.hs" curDir4 classInstances
 
   -- entryData contains joined string with each file entries' data (intercalate)
   let entryData = intercalate "\n" [entry1,entry2,entry3,entry4]
@@ -57,9 +62,9 @@ output :: EntryData -> [String] -> IO ()
 output entryData classInstances = do
   setCurrentDirectory "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/scripts"
   dataTable <- openFile "DataTable.csv" WriteMode
-  hPutStrLn dataTable ("Package,\t,\t,\t,\t,\t,ClassInstances")
+  hPutStrLn dataTable "Package,\t,\t,\t,\t,\t,ClassInstances"
   hPutStrLn dataTable ("drasil-,File Path,File Name,Data Type,Newtype Type,Class Definitions," ++ intercalate "," classInstances)
-  hPutStrLn dataTable (entryData)
+  hPutStrLn dataTable entryData
   hClose dataTable
 
 -- creates entries for each file FIXME pass classInsances to this function
@@ -68,43 +73,41 @@ createEntry name path classInstances = do
   [dataNames,newtypeNames,classNames,stripInstances] <- SCR.extractEntryData name path
 
   -- f data newtype class
-  let f d n c = intercalate "," [drasilPack,filePath,fileName,d,n,c] where 
+  let f d n c = intercalate "," [drasilPack,filePath,fileName,d,n,c] 
       drasilPack = takeWhile (/= '/') . (\\ "-") $ dropWhile (/='-') path 
-      filePath = path \\ "/Users/Nathaniel_Hu/"
+      filePath = (\\ "/") . dropWhile (/= '/') $ (\\ "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-") path
       fileName = name
 
-  let entry = if not (null dataNames) 
-                then f (dataNames !! 0) "\t" "\t"
-              else if not (null newtypeNames) 
-                then f "\t" (newtypeNames !! 0) "\t"
-              else if not (null classNames)
-                then f "\t" "\t" (classNames !! 0)
-              else f "\t" "\t" "\t"
+  let entry 
+        | not (null dataNames) = f (head dataNames) "\t" "\t"
+        | not (null newtypeNames) = f "\t" (head newtypeNames) "\t"
+        | not (null classNames) = f "\t" "\t" (head classNames)
+        | otherwise = f "\t" "\t" "\t"
   
-  let dataEntries = map ("\t,\t,\t,"++) $ map (++",\t,\t") (dataNames)
-      newtypeEntries = map ("\t,\t,\t,\t,"++) $ map (++",\t") (newtypeNames)
-      classEntries = map ("\t,\t,\t,\t,\t,"++) (classNames)
+  let dataEntries = map (("\t,\t,\t,"++) . (++",\t,\t")) dataNames
+      newtypeEntries = map (("\t,\t,\t,\t,"++) . (++",\t")) newtypeNames
+      classEntries = map ("\t,\t,\t,\t,\t,"++) classNames
 
   -- creating "Y" references to class instances for data types
-  let dataInstanceRefNames = map (f stripInstances) dataNames where 
+  let dataInstanceRefNames = map (f stripInstances) dataNames
       f b a = map (\\ (a ++ " ")) $ filter (isPrefixOf a) b
-      dataInstanceRefs = zipWith (\a b -> map (isInstanceOf a) b) dataInstanceRefNames instanceSkeleton where 
+      dataInstanceRefs = zipWith (map . isInstanceOf) dataInstanceRefNames instanceSkeleton where 
           instanceSkeleton = replicate (length dataInstanceRefNames) classInstances
       dataRefLines = map (intercalate ",") dataInstanceRefs
       dataEntries2 = zipWith (\a b -> a ++ "," ++ b) dataEntries dataRefLines
 
   -- creating "Y" references to class instances for newtype types
-  let newtypeInstanceRefNames = map (f stripInstances) newtypeNames where 
+  let newtypeInstanceRefNames = map (f stripInstances) newtypeNames
       f b a = map (\\ (a ++ " ")) $ filter (isPrefixOf a) b
-      newtypeInstanceRefs = zipWith (\a b -> map (isInstanceOf a) b) newtypeInstanceRefNames instanceSkeleton where 
+      newtypeInstanceRefs = zipWith (map . isInstanceOf) newtypeInstanceRefNames instanceSkeleton where 
           instanceSkeleton = replicate (length newtypeInstanceRefNames) classInstances
       newtypeRefLines = map (intercalate ",") newtypeInstanceRefs
       newtypeEntries2 = zipWith (\a b -> a ++ "," ++ b) newtypeEntries newtypeRefLines
 
   let entryData = dataEntries2 ++ newtypeEntries2 ++ classEntries
       
-  let output = if (length entryData) > 1
-                 then entry ++ "\n" ++ subEntries else f entry (dataRefLines !! 0) where
+  let output = if length entryData > 1
+                 then entry ++ "\n" ++ subEntries else f entry (head dataRefLines) where
                    subEntries = intercalate "\n" (drop 1 entryData)
                    f a b = a ++ "," ++ b
   -- mapM_ print (lines output)
--- code/scripts/SourceCodeReader.hs
+++ code/scripts/SourceCodeReader.hs
@@ -18,20 +18,20 @@ extractEntryData fileName filePath = do
   setCurrentDirectory filePath
   scriptFile <- readFile fileName
   -- removes comment lines
-  let scriptFileLines = (lines scriptFile) \\ filter (isInfixOf "-- ") (lines scriptFile)
+  let scriptFileLines = lines scriptFile \\ filter (isInfixOf "-- ") (lines scriptFile)
       dataTypes = filter (isPrefixOf "data ") scriptFileLines
       newtypeTypes = filter (isPrefixOf "newtype ") scriptFileLines
   -- derived classes (that use '=>') vs. defined classes
       (derivClass,definClass) = partition (isInfixOf "=>") (filter (isPrefixOf "class ") scriptFileLines)
       definInstances = filter (isPrefixOf "instance ") scriptFileLines
 
-  let dataNames = map (takeWhile (/=' ')) $ map (\\ "data ") dataTypes
-      newtypeNames = map (takeWhile (/=' ')) $ map (\\ "newtype ") newtypeTypes
-      stripDeriv = map (takeWhile (/=' ')) . map (\\ "> ") $ map (dropWhile (/='>')) derivClass
-      stripDefin = map (takeWhile (/=' ')) $ map (\\ "class ") definClass
+  let dataNames = map (takeWhile (/=' ') . (\\ "data ")) dataTypes
+      newtypeNames = map (takeWhile (/=' ') . (\\ "newtype ")) newtypeTypes
+      stripDeriv = map (takeWhile (/=' ') . (\\ "> ") . dropWhile (/='>')) derivClass
+      stripDefin = map (takeWhile (/=' ') . (\\ "class ")) definClass
       classNames = stripDeriv ++ stripDefin
       stripInstances = zipWith (\a b -> b ++ " " ++ a) (map (!! 0) d) (map (!! 1) d) where
-                       d = map words $ map (\\ "instance ") definInstances
+                       d = map (words . (\\ "instance ")) definInstances
   
   -- print stripInstances
   -- instanceNames = 

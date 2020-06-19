5e27b01d932e4ec7bb61a8c5319106aa8e4baf6f
Nathaniel-Hu <hun4@mcmaster.ca>
2020-06-19 09:22:26 -0400

    Update Directory Control/File Searching Functions
    DataTableGen.hs
    - added new references to iterator2, finder functions from DirectoryController.hs
    - added two new tests for iterator2 function (for later optimization)
    - created new createEntry2 function for optimized file processing (to replace createEntry function)
    
    DirectoryController.hs
    - added new type for FolderName (FilePath)
    - updated iterator function to print out results
    - added new iterator2 function to do same work as iterator function; optimized to work on lists of filenames + filepaths

--- code/scripts/DataTableGen.hs
+++ code/scripts/DataTableGen.hs
@@ -3,7 +3,7 @@ module DataTableGen (main) where
 import Data.List
 import System.IO
 import System.Directory
-import DirectoryController as DC (iterator)
+import DirectoryController as DC (iterator, iterator2, finder)
 import SourceCodeReader as SCR (extractEntryData)
 
 type EntryData = String
@@ -19,17 +19,26 @@ main :: IO ()
 main = do
   all <- getDirectoryContents "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code"
   
+  -- filters all drasil- packages
   let filtered = filter (isPrefixOf "drasil-") all
-  mapM_ print filtered
   
   setCurrentDirectory "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code"
-  cd <- getCurrentDirectory
-  print cd
 
   -- imports configuration settings
   [packageNames,classInstanceGroups,classInstances] <- config
   -- prints all drasil- package subdirectories
   DC.iterator filtered
+
+  test1 <- DC.iterator2 "Language /Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang"
+  print test1
+
+  test2 <- mapM DC.iterator2 ["Language /Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang",
+                              "GOOL /Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-gool"]
+  print test2
+
+  -- list of all files (names + filepaths)
+  fpsCode <- DC.finder "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code" (head filtered) 
+  mapM_ print fpsCode
 
   let curDir1 = "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil"
       curDir2 = "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-gool/GOOL/Drasil"
@@ -37,14 +46,24 @@ main = do
       curDir4 = "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil/Data"
 
   -- easy + hard test for classes, medium test for data/newtypes, easy test for data types
-  entry1 <- createEntry "Classes.hs" curDir1 classInstances
-  entry2 <- createEntry "RendererClasses.hs" curDir2 classInstances
-  entry3 <- createEntry "Core.hs" curDir3 classInstances
-  entry4 <- createEntry "Date.hs" curDir4 classInstances
+  entry1 <- createEntry classInstances "Classes.hs" curDir1
+  entry2 <- createEntry classInstances "RendererClasses.hs" curDir2
+  entry3 <- createEntry classInstances "Core.hs" curDir3
+  entry4 <- createEntry classInstances "Date.hs" curDir4
 
   -- entryData contains joined string with each file entries' data (intercalate)
   let entryData = intercalate "\n" [entry1,entry2,entry3,entry4]
   -- mapM_ print (lines entryData)
+
+  let rawFileData = ["Classes.hs /Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil",
+                     "RendererClasses.hs /Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-gool/GOOL/Drasil",
+                     "Core.hs /Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil/Document",
+                     "Date.hs /Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-lang/Language/Drasil/Data"]
+  rawEntryData <- mapM (createEntry2 classInstances . words) rawFileData
+
+  -- print [entry1,entry2,entry3,entry4]
+  -- print rawEntryData
+
   -- creates and writes to output data file
   output entryData classInstances
 
@@ -60,6 +79,7 @@ config = do
 -- function creates and writes output data file DataTable.csv
 output :: EntryData -> [String] -> IO ()
 output entryData classInstances = do
+  -- adjust this later to be "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/analysis"
   setCurrentDirectory "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/scripts"
   dataTable <- openFile "DataTable.csv" WriteMode
   hPutStrLn dataTable "Package,\t,\t,\t,\t,\t,ClassInstances"
@@ -67,9 +87,9 @@ output entryData classInstances = do
   hPutStrLn dataTable entryData
   hClose dataTable
 
--- creates entries for each file FIXME pass classInsances to this function
-createEntry :: FileName -> FilePath -> [ClassInstance] -> IO String
-createEntry name path classInstances = do
+-- creates entries for each file (original input format)
+createEntry :: [ClassInstance] -> FileName -> FilePath -> IO String
+createEntry classInstances name path = do
   [dataNames,newtypeNames,classNames,stripInstances] <- SCR.extractEntryData name path
 
   -- f data newtype class
@@ -106,10 +126,64 @@ createEntry name path classInstances = do
 
   let entryData = dataEntries2 ++ newtypeEntries2 ++ classEntries
       
-  let output = if length entryData > 1
-                 then entry ++ "\n" ++ subEntries else f entry (head dataRefLines) where
-                   subEntries = intercalate "\n" (drop 1 entryData)
-                   f a b = a ++ "," ++ b
+  let output 
+        | length entryData > 1 = entry ++ "\n" ++ subEntries
+        | length entryData == 1 && not (null dataRefLines) = f entry (head dataRefLines)
+        | length entryData == 1 && not (null newtypeRefLines) = f entry (head newtypeRefLines)
+        | otherwise = entry
+      subEntries = intercalate "\n" (drop 1 entryData)
+      f a b = a ++ "," ++ b
+
+  -- mapM_ print (lines output)
+  return output
+
+  -- creates entries for each file (different input format)
+createEntry2 :: [ClassInstance] -> [FilePath] -> IO String
+createEntry2 classInstances [name,path] = do
+  [dataNames,newtypeNames,classNames,stripInstances] <- SCR.extractEntryData name path
+
+  -- f data newtype class
+  let f d n c = intercalate "," [drasilPack,filePath,fileName,d,n,c] 
+      drasilPack = takeWhile (/= '/') . (\\ "-") $ dropWhile (/='-') path 
+      filePath = (\\ "/") . dropWhile (/= '/') $ (\\ "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code/drasil-") path
+      fileName = name
+
+  let entry 
+        | not (null dataNames) = f (head dataNames) "\t" "\t"
+        | not (null newtypeNames) = f "\t" (head newtypeNames) "\t"
+        | not (null classNames) = f "\t" "\t" (head classNames)
+        | otherwise = f "\t" "\t" "\t"
+  
+  let dataEntries = map (("\t,\t,\t,"++) . (++",\t,\t")) dataNames
+      newtypeEntries = map (("\t,\t,\t,\t,"++) . (++",\t")) newtypeNames
+      classEntries = map ("\t,\t,\t,\t,\t,"++) classNames
+
+  -- creating "Y" references to class instances for data types
+  let dataInstanceRefNames = map (f stripInstances) dataNames
+      f b a = map (\\ (a ++ " ")) $ filter (isPrefixOf a) b
+      dataInstanceRefs = zipWith (map . isInstanceOf) dataInstanceRefNames instanceSkeleton where 
+          instanceSkeleton = replicate (length dataInstanceRefNames) classInstances
+      dataRefLines = map (intercalate ",") dataInstanceRefs
+      dataEntries2 = zipWith (\a b -> a ++ "," ++ b) dataEntries dataRefLines
+
+  -- creating "Y" references to class instances for newtype types
+  let newtypeInstanceRefNames = map (f stripInstances) newtypeNames
+      f b a = map (\\ (a ++ " ")) $ filter (isPrefixOf a) b
+      newtypeInstanceRefs = zipWith (map . isInstanceOf) newtypeInstanceRefNames instanceSkeleton where 
+          instanceSkeleton = replicate (length newtypeInstanceRefNames) classInstances
+      newtypeRefLines = map (intercalate ",") newtypeInstanceRefs
+      newtypeEntries2 = zipWith (\a b -> a ++ "," ++ b) newtypeEntries newtypeRefLines
+
+  let entryData = dataEntries2 ++ newtypeEntries2 ++ classEntries
+      
+  let output 
+        | length entryData > 1 = entry ++ "\n" ++ subEntries
+        | length entryData == 1 && not (null dataRefLines) = f entry (head dataRefLines)
+        | length entryData == 1 && not (null newtypeRefLines) = f entry (head newtypeRefLines)
+        | otherwise = entry
+      subEntries = intercalate "\n" (drop 1 entryData)
+      f a b = a ++ "," ++ b
+
   -- mapM_ print (lines output)
   return output
 
--- code/scripts/DirectoryController.hs
+++ code/scripts/DirectoryController.hs
@@ -1,4 +1,4 @@
-module DirectoryController (iterator) where
+module DirectoryController (iterator, iterator2, finder) where
 
 import Data.List
 import System.IO
@@ -6,19 +6,53 @@ import System.Directory
 
 type EntryData = String
 type FileName = FilePath
+type FolderName = FilePath
 
 type ClassInstance = String
 
 type FileInstance = String
 
--- iterates through each drasil- package and outputs subdirectories
+-- iterates through each drasil- package and outputs subdirectories and haskell files
 iterator :: [FilePath] -> IO [a0]
 iterator (x:xs) = do 
   setCurrentDirectory "/Users/Nathaniel_Hu/Documents/GitHub/Drasil/code"
-  some <- getDirectoryContents x
-  print x
+  some <- listDirectory x
+  wd <- getCurrentDirectory
+  
   let l = filter (isInfixOf ".") some
-      d = some \\ l
-  print d
+      d = map ((++ x) . (++"/") . (++ wd) . (++" ")) $ (some \\ l)
+      h = filter (isSuffixOf ".hs") some
+  
+  print x -- << drasil- package
+  print d -- << sub-folders
+  print h -- << haskell files
+
   if xs /= [] then iterator xs
   else return []
+
+-- iterates through each drasil- package and outputs subdirectories and haskell files
+iterator2 :: FilePath -> IO [[FileName]]
+iterator2 filePath = do
+  let [fn,wd] = words filePath
+  setCurrentDirectory wd
+  fc <- listDirectory fn
+
+  let fcs = [a,b]
+      a = map ((++fn) . (++"/") . (++wd) . (++" ")) $ fc \\ (filter (isInfixOf ".") fc)
+      b = map ((++fn) . (++"/") . (++wd) . (++" ")) $ filter (isSuffixOf ".hs") fc
+  return fcs
+
+finder :: FilePath -> FolderName -> IO [FileName]
+finder filePath x = do
+  setCurrentDirectory filePath
+  all <- listDirectory x
+
+  let l = filter (isInfixOf ".") all
+      d = map ((++ x) . (++"/") . (++ filePath) . (++" ")) $ (all \\ l)
+      h = filter (isSuffixOf ".hs") all
+
+  print x -- << drasil- package
+  print d -- << sub-folders
+  print h -- << haskell files
+
+  return h

45793b53c4fc67ec03849386827977d655935fcb
muhammad ali <alim102@mcmaster.ca>
2020-07-08 16:01:12 -0400

    Matching UID to CodeConcept

--- code/drasil-code/Language/Drasil/Code/Imperative/ConceptMatch.hs
+++ code/drasil-code/Language/Drasil/Code/Imperative/ConceptMatch.hs
@@ -5,13 +5,13 @@ module Language.Drasil.Code.Imperative.ConceptMatch (
 
 import Language.Drasil (UID)
 
-import Language.Drasil.Choices (Choices(..), CodeConcept(..), ConceptMatchMap, 
+import Language.Drasil.Choices (Choices(..), CodeConcept(..), 
     MatchedConceptMap, showChs)
 
 import GOOL.Drasil (SValue, OOProg, MathConstant(..))
 
 import Prelude hiding (pi)
-import qualified Data.Map as Map (map, keys, lookup)
+import qualified Data.Map as Map (map, mapWithKey)
 import Control.Monad.State (State, modify)
 import Text.PrettyPrint.HughesPJ (Doc, text, ($$))
 
@@ -20,20 +20,19 @@ import Text.PrettyPrint.HughesPJ (Doc, text, ($$))
 -- being mapped, so we just take the head of the list of CodeConcepts
 -- The conceptMatchMap from choices is passed to chooseConept' internally, this way
 -- any codeconcept list can be matched to its appropiate UID
-chooseConcept :: Choices -> State Doc MatchedConceptMap
-chooseConcept chs = sequence $ Map.map (chooseConcept' chs $ conceptMatch chs) (conceptMatch chs)
-  where chooseConcept' :: Choices -> ConceptMatchMap-> [CodeConcept] -> State Doc CodeConcept
-        chooseConcept' _ _ [] = error $ "Empty list of CodeConcepts in the " ++ 
-          "ConceptMatchMap"
-        chooseConcept' _ cmm cs = do 
-            let uid = findUid (Map.keys cmm) cmm cs
-            modify ($$ (text $ "Code Concept "++ uid ++" selected as " ++ showChs (head cs)))
-            return $ head cs
 
--- | Maps CodeConcepts to corresponding GOOL values
+chooseConcept :: Choices -> State Doc MatchedConceptMap
+chooseConcept chs = sequence . addMatchLog $ Map.map (chooseConcept' chs) (conceptMatch chs)
+  where chooseConcept' _ [] = error $ "Empty list of CodeConcepts in the " ++ 
+          "ConceptMatchMap"
+        chooseConcept' _ cs = return $ head cs
+        addMatchLog = Map.mapWithKey addMessage
+        
+addMessage :: UID -> State Doc CodeConcept -> State Doc CodeConcept
+addMessage uid s' = do 
+    s <- s'
+    modify ($$ (text $ "Code Concept "++ uid ++" selected as " ++ showChs s))
+    return s
+
 conceptToGOOL :: (OOProg r) => CodeConcept -> SValue r
 conceptToGOOL Pi = pi
-
-findUid :: [UID] -> ConceptMatchMap -> [CodeConcept] -> UID 
-findUid [] _ _ = error "CodeConcept not in Concept Match Map"
-findUid (x:xs) mapp cs = if Map.lookup x mapp == (Just cs) then x else findUid xs mapp cs 

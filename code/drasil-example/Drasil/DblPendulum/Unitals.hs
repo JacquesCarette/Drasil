970b9bc08df97ee8579e2190ebaf5e4c90f7aed6
Olu <owojaiyo@mcmaster.ca>
2020-09-22 06:41:55 -0400

    small chane in unitals to remove extra bracket

--- code/drasil-example/Drasil/DblPendulum/Unitals.hs
+++ code/drasil-example/Drasil/DblPendulum/Unitals.hs
@@ -15,20 +15,6 @@ import Data.Drasil.SI_Units (metre, degree)
 import qualified Data.Drasil.Quantities.Physics as QP (position, ixPos, force)
 import Data.Drasil.Concepts.Physics (pendulum)
 import Data.Drasil.Concepts.Math (angle)
-  --acceleration, constAccel,
---   gravitationalAccelConst, iPos, iSpeed, iVel, ixPos, iyPos, ixVel, iyVel,
---   position, scalarPos, speed, time, velocity, xAccel, xConstAccel, xPos,
---   xVel, yAccel, yConstAccel, yPos, yVel)
--- import Data.Drasil.Quantities.Math (pi_, piConst)
-
--- import Data.Drasil.Constraints (gtZeroConstr)
--- import Data.Drasil.SI_Units (radian, metre, second)
--- import Data.Drasil.Units.Physics (velU)
-
--- import Drasil.Projectile.Concepts (landingPos, launcher, launchAngle,
---   launchSpeed, projectile, targetPos, target)
--- import qualified Drasil.Projectile.Concepts as C (flightDur, offset)
-
 
 symbols:: [QuantityDict]
 symbols = map qw unitalChunks
@@ -40,52 +26,12 @@ symbols = map qw unitalChunks
 -- acronyms = [oneD, twoD, assumption, dataDefn, genDefn, goalStmt, inModel,
 --   physSyst, requirement, srs, thModel, typUnc]
 
--- constants :: [QDefinition]
--- constants = [gravitationalAccelConst, piConst, tol]
 inputs :: [QuantityDict]
 inputs = map qw [lenRod, QP.force] 
 --mass, length, angle, g, t
 
 outputs :: [QuantityDict]
 outputs = map qw [QP.position]
--- unitalQuants :: [QuantityDict]
--- unitalQuants = message : map qw constrained
-
--- unitalIdeas :: [IdeaDict]
--- unitalIdeas = nw message : map nw constrained
-
--- inConstraints :: [UncertQ]
--- inConstraints = [launAngleUnc, launSpeedUnc, targPosUnc]
-
--- outConstraints :: [UncertQ]
--- outConstraints = [landPosUnc, offsetUnc]
-
--- constrained :: [ConstrConcept]
--- constrained = [flightDur, landPos, launAngle, launSpeed, offset, targPos]
-
--- ---
--- landPosUnc, launAngleUnc, launSpeedUnc, offsetUnc, targPosUnc :: UncertQ
--- landPosUnc   = uq landPos   defaultUncrt
--- launAngleUnc = uq launAngle defaultUncrt
--- launSpeedUnc = uq launSpeed defaultUncrt
--- offsetUnc    = uq offset    defaultUncrt
--- targPosUnc   = uq targPos   defaultUncrt
-
--- flightDur, landPos, launAngle, launSpeed, offset, targPos :: ConstrConcept
--- flightDur = constrainedNRV' (dqd  flightDurConcept (unitHelper lT "flight") Real second)  [gtZeroConstr]
--- landPos   = constrainedNRV' (dqd  landPosConcept   (unitHelper lP "land"  ) Real metre)   [gtZeroConstr]
--- launAngle = constrained'    (dqd' launAngleConcept (autoStage lTheta) Real (Just radian)) [physc $ Bounded (Exc, 0) (Exc, sy pi_ / 2)] (sy pi_ / 4)
--- launSpeed = constrained'    (dqd  launSpeedConcept (unitHelper lV "launch") Real velU)    [gtZeroConstr] (int 100)
--- offset    = constrainedNRV' (dqd  offsetConcept    (unitHelper lD "offset") Real metre)   [physc $ UpFrom (Exc, negate $ sy landPos) ]
--- targPos   = constrained'    (dqd  targPosConcept   (unitHelper lP "target") Real metre)   [gtZeroConstr] (int 1000)
-
--- unitHelper :: Symbol -> String -> Symbol
--- unitHelper sym substr = sub sym $ Label substr
-
-{-lenRodConcept :: ConceptChunk
-lenRodConcept = cc' lenRod
-     (foldlSent_ [S "the", phrase len `ofThe` (S "rod")])
---            -}
 
 units :: [UnitaryConceptDict]
 units = map ucw unitalChunks ++ map ucw [lenRod, pendAngle]
@@ -97,38 +43,11 @@ lenRod, pendAngle :: UnitalChunk
 
 lenRod = makeUCWDS "l_rod" (cn "length of rod")
         (S "the" +:+ phrase len `ofThe` S "rod")
-        (sub (cL) lRod) metre
+        (sub cL lRod) metre
 
 pendAngle = makeUCWDS "pendAngle" (cn "angle of pendulum")
         (S "the" +:+ phrase angle `ofThe` phrase pendulum)
-        (lTheta) degree     
--- launAngleConcept :: ConceptChunk
--- launAngleConcept = cc' launchAngle
---   (foldlSent_ [S "the", phrase angle, S "between the", phrase launcher `sAnd` S "a straight line",
---              S "from the", phrase launcher `toThe` phrase target])
-
--- launSpeedConcept :: ConceptChunk
--- launSpeedConcept = cc' launchSpeed (phrase iSpeed `ofThe` phrase projectile +:+ S "when launched")
-
--- offsetConcept :: ConceptChunk
--- offsetConcept = cc' C.offset (S "the offset between the" +:+ phrase targetPos `andThe` phrase landingPos)
-
--- targPosConcept :: ConceptChunk
--- targPosConcept = cc' targetPos
---   (foldlSent_ [S "the", phrase distance, S "from the", phrase launcher `toThe` phrase target])
-
--- flightDurConcept :: ConceptChunk
--- flightDurConcept = cc' C.flightDur
---   (foldlSent_ [S "the", phrase time, S "when the", phrase projectile, S "lands"])
-
--- ---
--- message :: QuantityDict
--- message = vc "message" (nounPhraseSent (S "output message as a string")) lS String
-
--- ---
--- tol :: QDefinition
--- tol = mkQuantDef (vcSt "tol" (nounPhraseSP "hit tolerance") (autoStage vEpsilon)
---   Rational) (Perc 2 2)
+        (lTheta) degree
 
 
 --Labels

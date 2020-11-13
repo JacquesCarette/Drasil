274f6e3252ac7f87d46208b2adf1fb2ede18db06
Olu <owojaiyo@mcmaster.ca>
2020-11-13 12:26:43 -0500

    emoved unused comments

--- code/drasil-example/Drasil/DblPendulum/References.hs
+++ code/drasil-example/Drasil/DblPendulum/References.hs
@@ -8,18 +8,3 @@ import Drasil.Projectile.References (accelerationWiki, velocityWiki, hibbeler200
 citations :: BibRef
 citations = [accelerationWiki, velocityWiki, hibbeler2004, cartesianWiki]
 
--- accelerationWiki, velocityWiki, hibbeler2004 :: Citation
-
--- accelerationWiki = cMisc [author [mononym "Wikipedia Contributors"],
---   title "Acceleration", howPublishedU "https://en.wikipedia.org/wiki/Acceleration",
---   month Jun, year 2019]
---   "accelerationWiki"
-
--- velocityWiki = cMisc [author [mononym "Wikipedia Contributors"],
---   title "Velocity", howPublishedU "https://en.wikipedia.org/wiki/Velocity",
---   month Jun, year 2019]
---   "velocityWiki"
-
--- hibbeler2004 = cBookA [rcHibbeler] "Engineering Mechanics: Dynamics"
---   "Pearson Prentice Hall" 2004 []
---   "hibbeler2004"

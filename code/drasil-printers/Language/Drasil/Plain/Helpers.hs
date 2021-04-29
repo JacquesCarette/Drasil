d02b9bb04d13955241d3447ab647f2763988ecd9
Jason Balaci <jasonbalaci@gmail.com>
2021-04-29 17:51:06 -0400

    Remove MissingH dependency from drasil-printers

--- code/drasil-printers/Language/Drasil/Plain/Helpers.hs
+++ code/drasil-printers/Language/Drasil/Plain/Helpers.hs
@@ -1,12 +1,6 @@
 module Language.Drasil.Plain.Helpers where
 
-import Data.String.Utils (replace)
-
 toPlainName :: String -> String
-toPlainName s =
-    let illegalChars = [
-            ",","~","`","-","=","!","@","#","$","%","^","&","*","+",
-            "[","]","\\",";","'","/","|","\"","<",">","?"," "]
-    in foldl varNameReplace s illegalChars
-    where  varNameReplace :: String -> String -> String
-           varNameReplace l old = replace old "_" l
+toPlainName (c:cs) | c `elem` ",~`-=!@#$%^&*+[]\\;'/|\"<>? " = '_' : toPlainName cs
+                   | otherwise                               = c : toPlainName cs
+toPlainName cs = cs

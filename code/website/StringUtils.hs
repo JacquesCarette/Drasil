30a2ae44456df8112b7644ce323a2adb49eb4370
Jason Balaci <jasonbalaci@gmail.com>
2021-04-30 12:06:09 -0400

    2322 : add credit to MissingH

--- code/website/StringUtils.hs
+++ code/website/StringUtils.hs
@@ -1,5 +1,9 @@
 module StringUtils where
 
+{- The below functions are based on that of MissingH's Data.String.Utils
+https://hackage.haskell.org/package/MissingH-1.4.3.0/docs/Data-String-Utils.html
+-}
+
 strip :: String -> String
 strip = rstrip . lstrip
 

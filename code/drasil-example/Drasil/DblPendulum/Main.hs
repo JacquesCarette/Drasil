f6ba39a48a4394d8552a4d9d7647a478e203844c
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2021-05-04 11:39:26 -0400

    removed extra lines and updated stable

--- code/drasil-example/Drasil/DblPendulum/Main.hs
+++ code/drasil-example/Drasil/DblPendulum/Main.hs
@@ -3,9 +3,7 @@ module Drasil.DblPendulum.Main (main) where
 import GHC.IO.Encoding
 
 import Language.Drasil.Generate (gen, DocSpec(DocSpec), DocType(SRS, Website))
-
 import Drasil.DblPendulum.Body (srs, printSetting)
-
 
 
 main :: IO()

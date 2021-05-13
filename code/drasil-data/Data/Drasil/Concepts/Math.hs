67ae4cf950e6cff1bb55435f62284c6558e4d0f8
Awurama Nyarko <aknyarko@yahoo.co.uk>
2021-05-13 07:31:35 -0400

    fixing spaces

--- code/drasil-data/Data/Drasil/Concepts/Math.hs
+++ code/drasil-data/Data/Drasil/Concepts/Math.hs
@@ -55,19 +55,19 @@ number      = dcc "number"       (cn' "number")                  "a mathematical
 orient      = dcc "orientation"  (cn' "orientation")             "the relative physical position or direction of something"
 parameter   = dcc "parameter"    (cn' "parameter")               "a quantity whose value is selected depending on particular circumstances"
 --FIXME: Should "parameter" be in math?
-perp         = dcc "perp"         (cn' "perpendicular")           "At right angles"
+perp         = dcc "perp"         (cn' "perpendicular")          "At right angles"
 pi_          = dcc "pi"           (cn' "ratio of circumference to diameter for any circle") "The ratio of a circle's circumference to its diameter"
-qdPosInf     = dcc "qdPosInf"     (cn' "Positive Infinity")     "the limit of a sequence or function that eventually exceeds any prescribed bound"
-qdNegInf     = dcc "qdNegInf"     (cn' "Negative Infinity")     "Opposite of positive infinity"
+qdPosInf     = dcc "qdPosInf"     (cn' "Positive Infinity")      "the limit of a sequence or function that eventually exceeds any prescribed bound"
+qdNegInf     = dcc "qdNegInf"     (cn' "Negative Infinity")      "Opposite of positive infinity"
 point        = dccWDS "point"     (pn' "point")                   $ S "An exact location, it has no size, only position" +:+
                                                                   sParen (S "from" +:+ makeRef2S pointSource)
-probability  = dcc "probability"  (cnIES "probability")           "The likelihood of an event to occur"
-rate         = dcc "rate"         (cn' "rate")                    "Ratio that compares two quantities having different units of measure"
+probability  = dcc "probability"  (cnIES "probability")          "The likelihood of an event to occur"
+rate         = dcc "rate"         (cn' "rate")                   "Ratio that compares two quantities having different units of measure"
 rightHand    = dcc "rightHand"    (cn' "right-handed coordinate system")  "A coordinate system where the positive z-axis comes out of the screen."
-shape        = dcc "shape"        (cn' "shape")                   "The outline of an area or figure"
-surface      = dcc "surface"      (cn' "surface")                 "The outer or topmost boundary of an object"
-unit_        = dcc "unit"         (cn' "unit")                    "Identity element"
-vector       = dcc "vector"       (cn' "vector")                  "Object with magnitude and direction"
+shape        = dcc "shape"        (cn' "shape")                  "The outline of an area or figure"
+surface      = dcc "surface"      (cn' "surface")                "The outer or topmost boundary of an object"
+unit_        = dcc "unit"         (cn' "unit")                   "Identity element"
+vector       = dcc "vector"       (cn' "vector")                 "Object with magnitude and direction"
 
 xAxis = dcc "xAxis" (nounPhraseSent $ P lX :+: S "-axis") "the primary axis of a system of coordinates"
 yAxis = dcc "yAxis" (nounPhraseSent $ P lY :+: S "-axis") "the secondary axis of a system of coordinates"

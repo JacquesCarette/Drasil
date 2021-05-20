43c976090741514ccb7a0c1368a91b24d4054cd0
Jason Balaci <jasonbalaci@gmail.com>
2021-05-20 16:04:31 -0400

    Fix typing of physical constraints

--- code/drasil-data/Data/Drasil/Constraints.hs
+++ code/drasil-data/Data/Drasil/Constraints.hs
@@ -3,5 +3,5 @@ module Data.Drasil.Constraints where
 import Language.Drasil
 
 gtZeroConstr, probConstr :: Constraint
-gtZeroConstr = physc $ UpFrom  (Exc, Int 0)
-probConstr   = physc $ Bounded (Inc, Int 0) (Inc, Int 1)
+gtZeroConstr = physc $ UpFrom  (Exc, dbl 0)
+probConstr   = physc $ Bounded (Inc, dbl 0) (Inc, dbl 1)

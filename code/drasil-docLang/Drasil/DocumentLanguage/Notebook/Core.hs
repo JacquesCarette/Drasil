c350237c062b023d47f6fb1b4101cf0e63807bf2
tingyu528 <wu30724@gmail.com>
2021-05-25 03:54:01 -0400

    update nb core lang

--- code/drasil-docLang/Drasil/DocumentLanguage/Notebook/Core.hs
+++ code/drasil-docLang/Drasil/DocumentLanguage/Notebook/Core.hs
@@ -1,5 +1,7 @@
 module Drasil.DocumentLanguage.Notebook.Core where
 
+import Data.Generics.Multiplate (Multiplate(multiplate, mkPlate))
+
 type DocDesc = [DocSection]
 
 
@@ -31,6 +33,39 @@ data BodySub where
 
 {--}
 
+newtype SmmrySec = SmmryProg [Contents]
+
+{--}
+
 newtype AppndxSec = AppndxProg [Contents]
 
 {--}
+
+data DLPlate f = DLPlate {
+  docSec :: DocSection -> f DocSection,
+  introSec :: IntroSec -> f IntroSec,
+  introSub :: IntroSub -> f IntroSub,
+  bodySec :: BodySec -> f BodySec,
+  bodySub :: BodySub -> f BodySub,
+  smmrySec ::SmmrySec -> f SmmrySec,
+  appendSec :: AppndxSec -> f AppndxSec
+}
+
+instance Multiplate DLPlate where
+  multiplate p = DLPlate ds intro intro' body body' smry aps where
+    ds (IntroSec x) = IntroSec <$> introSec p x
+    ds (BodySec x) = BodySec <$> bodySec p x
+    ds (SmmrySec x) = SmmrySec <$> smmrySec p x
+    ds (AppndxSec x) = AppndxSec <$> appendSec p x
+    ds Bibliography = pure Bibliography
+
+    intro (IntroProg s1 s2 progs) = IntroProg s1 s2 <$>
+      traverse (introSub p) progs
+    intro' (IPurpose s) = pure $ IPurpose s
+    intro' (IScope s) = pure $ IScope s
+    body' (Review c s) = pure $ Review c s
+    body' (MethsAndAnls c s) = pure $ MethsAndAnls c s
+    smry (SmmryProg con) = pure $ SmmryProg con 
+    aps (AppndxProg con) = pure $ AppndxProg con
+  mkPlate b = DLPlate (b docSec) (b introSec) (b introSub) (b bodySec)
+    (b bodySub) (b smmrySec) (b appendSec)

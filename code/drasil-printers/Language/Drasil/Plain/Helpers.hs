df26ba8b1d9973bae108b3ae7038df9144f0221b
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2021-06-04 15:38:00 -0400

    added documentation to Plain file

--- code/drasil-printers/Language/Drasil/Plain/Helpers.hs
+++ code/drasil-printers/Language/Drasil/Plain/Helpers.hs
@@ -1,5 +1,6 @@
 module Language.Drasil.Plain.Helpers where
 
+-- | Replace occurences of special characters (",~`-=!@#$%^&*+[]\\;'/|\"<>? ") with underscores ("_").
 toPlainName :: String -> String
 toPlainName (c:cs) | c `elem` ",~`-=!@#$%^&*+[]\\;'/|\"<>? " = '_' : toPlainName cs
                    | otherwise                               = c : toPlainName cs

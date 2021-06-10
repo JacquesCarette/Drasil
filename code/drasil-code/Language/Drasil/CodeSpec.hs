4adae33ecf4f2a3fbc64b014de3ad15d5172ff27
Awurama Nyarko <aknyarko@yahoo.co.uk>
2021-06-10 15:20:48 -0400

    deleting relToQD & convertRel

--- code/drasil-code/Language/Drasil/CodeSpec.hs
+++ code/drasil-code/Language/Drasil/CodeSpec.hs
@@ -109,16 +109,6 @@ codeSpec SI {_sys = sys
 
 -- medium hacks ---
 
--- Converts a chunk with a defining relation to a QDefinition
---relToQD :: ExprRelat c => ChunkDB -> c -> QDefinition
---relToQD sm r = convertRel sm (relat r)
-
--- Converts an Expr representing a definition (i.e. an equality where the left 
--- side is just a variable) to a QDefinition.
---convertRel :: ChunkDB -> Expr -> QDefinition
---convertRel sm (EqBinaryOp Eq (C x) r) = ec (symbResolve sm x) r
---convertRel _ _ = error "Conversion failed"
-
 -- | Convert a Func to an implementation-stage QuantityDict representing the 
 -- function.
 asVC :: Func -> QuantityDict

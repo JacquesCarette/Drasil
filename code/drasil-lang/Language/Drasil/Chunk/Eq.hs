44fadfd925294214bac816e243fe74699cc5b254
Jason Balaci <jasonbalaci@gmail.com>
2021-06-15 15:16:20 -0400

    Remove 1 redundant import

--- code/drasil-lang/Language/Drasil/Chunk/Eq.hs
+++ code/drasil-lang/Language/Drasil/Chunk/Eq.hs
@@ -12,7 +12,7 @@ import Language.Drasil.Classes (NamedIdea(term), Idea(getA),
 import Language.Drasil.Chunk.Quantity (QuantityDict, mkQuant, mkQuant', qw)
 
 import Language.Drasil.Expr.Display (defines)
-import Language.Drasil.Expr (Expr, ($=))
+import Language.Drasil.Expr (Expr)
 import Language.Drasil.Expr.Math (sy)
 import Language.Drasil.NounPhrase (NP)
 import Language.Drasil.Space (Space)

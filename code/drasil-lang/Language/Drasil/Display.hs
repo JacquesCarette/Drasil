3536b2d20b51591475dc1ff5ae29a35e312c5bf2
Jason Balaci <jasonbalaci@gmail.com>
2021-06-18 10:43:13 -0400

    Follow same commenting scheme as Language.Drasil.Development

--- code/drasil-lang/Language/Drasil/Display.hs
+++ code/drasil-lang/Language/Drasil/Display.hs
@@ -1,3 +1,7 @@
-module Language.Drasil.Display (Decoration(..), Symbol(..), compsy) where
+{- re-export many things to simplify external use -}
+module Language.Drasil.Display (
+    -- Symbol
+    Decoration(..), Symbol(..), compsy
+    ) where
 
 import Language.Drasil.Symbol (Decoration(..), Symbol(..), compsy)

f61d1789587cc07e31f54c83c42f9d6ec9da7f11
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2021-06-29 11:31:50 -0400

    adding comments to Language.Drasil.Code

--- code/drasil-code/Language/Drasil/Data/ODELibPckg.hs
+++ code/drasil-code/Language/Drasil/Data/ODELibPckg.hs
@@ -9,11 +9,17 @@ import Language.Drasil.Code.Lang (Lang)
 import Language.Drasil.Data.ODEInfo (ODEInfo)
 import Language.Drasil.Mod (Name, Version)
 
+-- | Holds an ODE library package.
 data ODELibPckg = ODELib {
+  -- | Library name.
   libName :: Name,
+  -- | Version.
   libVers :: Version,
+  -- | Library specifications.
   libSpec :: ExternalLibrary,
+  -- | Library call.
   libCall :: ODEInfo -> ExternalLibraryCall,
+  -- | Library path.
   libPath :: Maybe FilePath,
   -- It has been said that language information should be in language-related 
   -- file, but this needs more thought. The language would need to declare 
@@ -24,18 +30,19 @@ data ODELibPckg = ODELib {
   -- to update an internal Drasil file to add their library to a language's 
   -- compatible libraries. So maybe declaring compatible languages at library 
   -- definition time really is the right way to do this.
+  -- | Compatible OO languages.
   compatibleLangs :: [Lang]
 }
 
--- | Makes an ODELibPckg with the given name, ExternalLibrary specification, 
--- ExternalLibraryCall specification parameterized by an ODEInfo, local file 
+-- | Makes an 'ODELibPckg' with the given name, 'ExternalLibrary' specification, 
+-- 'ExternalLibraryCall' specification parameterized by an 'ODEInfo', local file 
 -- path to the library, and list of compatible languages.
 mkODELib :: Name -> Version -> ExternalLibrary -> (ODEInfo -> 
   ExternalLibraryCall) -> FilePath -> [Lang] -> ODELibPckg
 mkODELib n v e c f = ODELib n v e c (Just f)
 
--- | Makes an ODELibPckg with the given name, ExternalLibrary specification, 
--- ExternalLibraryCall specification parameterized by an ODEInfo, and list of 
+-- | Makes an 'ODELibPckg' with the given name, 'ExternalLibrary' specification, 
+-- 'ExternalLibraryCall' specification parameterized by an 'ODEInfo', and list of 
 -- compatible languages.
 mkODELibNoPath :: Name -> Version -> ExternalLibrary -> (ODEInfo -> 
   ExternalLibraryCall) -> [Lang] -> ODELibPckg

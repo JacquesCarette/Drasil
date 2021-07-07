8b4b24b77ada3f8c093706b9b283198c797c9c16
Jason Balaci <jasonbalaci@gmail.com>
2021-07-07 18:13:15 -0400

    Remove a _bit_ of redundant wording

--- code/drasil-example/Drasil/GamePhysics/IMods.hs
+++ code/drasil-example/Drasil/GamePhysics/IMods.hs
@@ -112,7 +112,7 @@ rotMotDeriv = mkDerivName (phrase rotMot)
 
 rotMotDerivStmts :: [Sentence]
 rotMotDerivStmts = [
-    foldlSent [S "We know that we may calculate the total angular acceleration of rigid body", 
+    foldlSent [S "We may calculate the total angular acceleration of rigid body", 
       P lJ, S "by calculating the derivative of it's angular velocity with respect to time", fromSource angAccelDD],
     S "Performing the derivative, we obtain:"
   ]

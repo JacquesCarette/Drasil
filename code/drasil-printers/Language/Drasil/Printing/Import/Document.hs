e9e5dfaa2825bbb71d138611b4fac386641e187a
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2021-07-14 13:57:19 -0400

    fix build fail

--- code/drasil-printers/Language/Drasil/Printing/Import/Document.hs
+++ code/drasil-printers/Language/Drasil/Printing/Import/Document.hs
@@ -16,7 +16,7 @@ import Data.Bifunctor (bimap, second)
 
 -- | Translates from 'Document' to a printable representation of 'T.Document'.
 makeDocument :: PrintingInformation -> Document -> T.Document
-makeDocument sm (Document titleLb authorName toC sections) =
+makeDocument sm (Document titleLb authorName _ sections) =
   T.Document (spec sm titleLb) (spec sm authorName) (createLayout sm sections)
 
 -- | Helper for translating sections into a printable representation of layout objects ('T.LayoutObj').

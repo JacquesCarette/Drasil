f6dd803ba30a0d40e9833805ec31991c01ff69f5
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2021-07-15 14:55:20 -0400

    brief documentation for drasil-gen

--- code/drasil-gen/Language/Drasil/Output/Formats.hs
+++ code/drasil-gen/Language/Drasil/Output/Formats.hs
@@ -1,4 +1,4 @@
--- | Defines output formats for the different documents we can generate
+-- | Defines output formats for the different documents we can generate.
 module Language.Drasil.Output.Formats where
 
 import Data.Char (toLower)
@@ -6,13 +6,17 @@ import Build.Drasil ((+:+), Command, makeS, mkCheckedCommand, mkCommand, mkFreeV
   mkFile, mkRule, RuleTransformer(makeRule))
 
 -- | When choosing your document, you must specify the filename for
--- the generated output (specified /without/ a file extension)
+-- the generated output (specified /without/ a file extension).
 type Filename = String
 
+-- | Document types include Software Requirements Specification, MG, MIS, and Website.
+-- The first three will generate as TeX files while the Website generates as HTML.
 data DocType = SRS | MG | MIS | Website
 
+-- | Document specifications. Holds the type of document ('DocType') and its name ('Filename').
 data DocSpec = DocSpec DocType Filename
 
+-- | Allows the creation of Makefiles for documents that use LaTeX.
 instance RuleTransformer DocSpec where
   makeRule (DocSpec Website _) = []
   makeRule (DocSpec dt fn) = [
@@ -24,15 +28,16 @@ instance RuleTransformer DocSpec where
         bibtex = mkCommand . (+:+) (makeS "bibtex" +:+ mkFreeVar "BIBTEXFLAGS") . makeS
         pdfName = makeS $ fn ++ ".pdf"
 
+-- | Shows the different types of documents.
 instance Show DocType where
   show SRS      = "SRS"
   show MG       = "MG"
   show MIS      = "MIS"
   show Website  = "Website"
              
--- | LaTeX helper
+-- | LaTeX helper.
 data DocClass = DocClass (Maybe String) String
--- | LaTeX helper for adding packages
+-- | LaTeX helper for adding packages. Wraps a list of package names.
 newtype UsePackages = UsePackages [String] -- Package name list
--- | LaTeX helper
+-- | LaTeX helper.
 data ExDoc = ExDoc (Maybe String) String

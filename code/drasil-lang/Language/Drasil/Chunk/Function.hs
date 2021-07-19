d78a008c298462db99314360512ec651979acf29
Jason Balaci <jasonbalaci@gmail.com>
2021-07-19 15:14:12 -0400

    Annotate function definitions

--- code/drasil-lang/Language/Drasil/Chunk/Function.hs
+++ code/drasil-lang/Language/Drasil/Chunk/Function.hs
@@ -7,7 +7,7 @@ module Language.Drasil.Chunk.Function (
 
 import Control.Lens ((^.), view, makeLenses)
 
-import Language.Drasil.Chunk.UnitDefn (unitWrapper, IsUnit, MayHaveUnit(..))
+import Language.Drasil.Chunk.UnitDefn (unitWrapper, IsUnit, MayHaveUnit(..), UnitDefn)
 import Language.Drasil.Classes.Core (HasSymbol(..), HasUID(..))
 import Language.Drasil.Classes (ConceptDomain(..), Display(..), DefiningExpr(..),
   Quantity, Definition(..), HasSpace(..), Idea(..), NamedIdea(..))
@@ -24,9 +24,9 @@ data FuncDefn = FD {
   _qua    :: QuantityDict,
   _inputs :: [UID],
   _defn'  :: Sentence,
-  _equat  :: Expr,
+  _ex     :: Expr,
   _spc    :: Space,
-  cd      :: [UID] -- TODO: make use of this later
+  cd      :: [UID] -- FIXME: unused!
 }
 makeLenses ''FuncDefn
 
@@ -37,21 +37,39 @@ instance HasSpace      FuncDefn where typ = spc
 instance HasSymbol     FuncDefn where symbol = symbol . (^. qua)  -- TODO: what should the symbol be? "F(X,Y,Z)" or just "F"? Leaving as "F" for now since we match stable
 instance Definition    FuncDefn where defn = defn'
 instance Quantity      FuncDefn where
-instance DefiningExpr  FuncDefn where defnExpr = equat
+instance DefiningExpr  FuncDefn where defnExpr = ex
 instance Eq            FuncDefn where a == b = (a ^. uid) == (b ^. uid)
 instance MayHaveUnit   FuncDefn where getUnit = getUnit . view qua
-instance Display       FuncDefn where toDispExpr q = defines (FCall (q ^. uid) (map C $ q ^. inputs) []) (q ^. defnExpr)
 instance ConceptDomain FuncDefn where cdom = cd
+instance Display       FuncDefn where
+  toDispExpr q = defines (FCall (q ^. uid) (map C $ q ^. inputs) []) (q ^. defnExpr)
 
-mkFuncDefn :: (HasUID f, HasSymbol f, HasSpace f, HasUID i, HasSymbol i, HasSpace i, IsUnit u) =>
+-- | Factored version of 'mkFuncDefn' functions
+mkFuncDefn0 :: (HasUID f, HasSymbol f, HasSpace f,
+                HasUID i, HasSymbol i, HasSpace i) =>
+  f -> NP -> Sentence -> Maybe UnitDefn -> [i] -> Expr -> FuncDefn
+mkFuncDefn0 f n s u is e = FD
+  (mkQuant' (f ^. uid) n Nothing (f ^. typ) (symbol f) u)
+  (map (^. uid) is) s e
+  (mkPrimitiveMapping (map (^. typ) is) (f ^. typ)) []
+
+-- | Create 'FuncDefn' with a symbol, name, term, list of inputs, resultant units, and a defining Expr
+mkFuncDefn :: (HasUID f, HasSymbol f, HasSpace f,
+               HasUID i, HasSymbol i, HasSpace i,
+               IsUnit u) =>
   f -> NP -> Sentence -> u -> [i] -> Expr -> FuncDefn
-mkFuncDefn f n s u is e = FD (mkQuant' (f ^. uid) n Nothing (f ^. typ) (symbol f) (Just $ unitWrapper u)) (map (^. uid) is) s e (mkPrimitiveMapping (map (^. typ) is) (f ^. typ)) []
+mkFuncDefn f n s u = mkFuncDefn0 f n s (Just $ unitWrapper u)
 
-mkFuncDefn' :: (HasUID f, HasSymbol f, HasSpace f, HasUID i, HasSymbol i, HasSpace i) =>
+-- | Create 'FuncDefn' with a symbol, name, term, list of inputs, and a defining Expr
+mkFuncDefn' :: (HasUID f, HasSymbol f, HasSpace f,
+                HasUID i, HasSymbol i, HasSpace i) =>
   f -> NP -> Sentence -> [i] -> Expr -> FuncDefn
-mkFuncDefn' f n s is e = FD (mkQuant' (f ^. uid) n Nothing (f ^. typ) (symbol f) Nothing) (map (^. uid) is) s e (mkPrimitiveMapping (map (^. typ) is) (f ^. typ)) []
+mkFuncDefn' f n s = mkFuncDefn0 f n s Nothing
 
-mkFuncDefnByQ :: (Quantity c, MayHaveUnit c, HasSpace c, Quantity i, HasSpace i) => c -> [i] -> Expr -> FuncDefn
-mkFuncDefnByQ f is e = case getUnit f of
-  Just u  -> mkFuncDefn  f (f ^. term) EmptyS u is e
-  Nothing -> mkFuncDefn' f (f ^. term) EmptyS   is e
+-- | Create 'FuncDefn's using a symbol, list of inputs, and a defining Expr
+mkFuncDefnByQ :: (Quantity c, MayHaveUnit c, HasSpace c,
+                  Quantity i, HasSpace i) =>
+  c -> [i] -> Expr -> FuncDefn
+mkFuncDefnByQ f = case getUnit f of
+  Just u  -> mkFuncDefn  f (f ^. term) EmptyS u
+  Nothing -> mkFuncDefn' f (f ^. term) EmptyS

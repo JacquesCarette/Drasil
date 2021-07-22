d0cd2e155e32dbee8ac0167737de091a8fda81d6
Jason Balaci <jasonbalaci@gmail.com>
2021-07-22 12:26:08 -0400

    Remove original packages

--- code/drasil-example/Drasil/Projectile/References.hs
+++
@@ -1,7 +0,0 @@
-module Drasil.Projectile.References where
-
-import Language.Drasil
-import Data.Drasil.Citations (cartesianWiki, accelerationWiki, velocityWiki, hibbeler2004)
-
-citations :: BibRef
-citations = [accelerationWiki, velocityWiki, hibbeler2004, cartesianWiki]

6b4548990b9fe81547a97e77664cd6c97d594bd3
Jason Balaci <jasonbalaci@gmail.com>
2021-07-22 12:18:46 -0400

    SWHS compiling

---
+++ code/drasil-example/swhs/lib/Drasil/SWHS/Concepts.hs
@@ -0,0 +1,92 @@
+module Drasil.SWHS.Concepts where --all of this file is exported
+
+import Control.Lens ((^.))
+
+import Language.Drasil
+import Utils.Drasil.Concepts
+
+import Data.Drasil.Concepts.Documentation (assumption, goalStmt,
+  likelyChg, physSyst, requirement, srs, typUnc, unlikelyChg)
+import Data.Drasil.Concepts.Math (ode, parameter, rightSide)
+import Data.Drasil.Domains (materialEng)
+import Data.Drasil.TheoryConcepts (dataDefn, genDefn, inModel, thModel)
+
+con :: [ConceptChunk]
+con = [charging, coil, discharging, gaussDiv,
+  perfectInsul, phaseChangeMaterial, tank,
+  tankPCM, transient, water, sWHT, tankParam]
+
+---Acronyms---
+acronyms :: [CI]
+acronyms = [assumption, dataDefn, genDefn, goalStmt, inModel, likelyChg, ode,
+  progName, physSyst, requirement, srs, thModel, typUnc, unlikelyChg]
+
+acronymsFull :: [CI]
+acronymsFull = acronyms ++ [phsChgMtrl, rightSide]
+
+phsChgMtrl, progName :: CI
+
+phsChgMtrl  = commonIdeaWithDict "phsChgMtrl" (nounPhrase "phase change material"
+  "phase change materials") "PCM" [materialEng]
+
+progName    = commonIdeaWithDict "swhsName"   (nounPhrase "solar water heating system"
+  "solar water heating systems") "SWHS" [materialEng]
+
+full :: NamedChunk
+full = nc "full" (progName `with` phsChgMtrl)
+-- I want to include SI as an acronym, but I can't find a way for the
+-- description to have accents when using dcc.
+
+---ConceptChunks---
+
+charging, coil, discharging, gaussDiv,
+  perfectInsul, phaseChangeMaterial, tank,
+  tankPCM, transient, water, sWHT, tankParam :: ConceptChunk
+
+charging = dcc "charging" (nounPhraseSP "charging") "charging of the tank"
+
+coil = dcc "coil" (cn' "heating coil")
+  "coil in tank that heats by absorbing solar energy"
+
+discharging = dcc "discharging" (nounPhraseSP "discharging")
+  "discharging of the tank"
+
+transient = dcc "transient" (nounPhraseSP "transient") "changing with time"
+
+gaussDiv = dcc "gaussDiv" (nounPhraseSP "gauss's divergence theorem")
+  ("a result that relates the flow of a vector field through a surface" ++
+  "to the behavior of the vector field inside the surface")
+--TODO: Physical property.
+
+perfectInsul = dcc "perfectInsul" (nounPhraseSP "perfectly insulated")
+  ("describes the property of a material not allowing" ++
+  "heat transfer through its boundaries")
+
+phaseChangeMaterial = dcc "pcm" (phsChgMtrl ^. term)
+  ("a substance that uses phase changes (such as melting) to absorb or " ++
+  "release large amounts of heat at a constant temperature")
+  
+tankParam = dcc "tankParam" (compoundPhrase' (tank ^. term)
+  (parameter ^. term))
+  "values associated with the tank"
+
+tank  = dcc "tank"  (cn' "tank") "enclosure containing some kind of substance"
+sWHT  = dcc "sWHT"  (cn' "solar water heating tank") "solar water heating tank"
+water = dcc "water" (cn' "water") "the liquid with which the tank is filled"
+
+tankPCM = dcc "tankPCM" (nounPhrase''
+  (phrase sWHT +:+ S "incorporating" +:+ short phsChgMtrl)
+  (phrase sWHT +:+ S "incorporating" +:+ short phsChgMtrl)
+  CapFirst CapWords)
+  "solar water heating tank incorporating phase change material"
+
+swhsPCM :: CommonConcept
+-- Nounphrase'' hack to get nounPhraseSP words to accept
+-- nounPhrases instead of strings
+-- Another capitalization hack.
+swhsPCM = dcc' "swhsPCM" (nounPhrase''
+  (S "solar water heating systems" +:+ S "incorporating" +:+ short phsChgMtrl)
+  (S "solar water heating systems" +:+ S "incorporating" +:+ short phsChgMtrl)
+  CapFirst CapWords)
+  "solar water heating systems incorporating phase change material"
+  "SWHS"
---
+++ code/drasil-example/swhs/lib/Drasil/SWHS/README.md
@@ -0,0 +1,61 @@
+--------------------------------------------------
+### Summary of Folder Structure and File Contents
+Last updated: July 19, 2018
+--------------------------------------------------
+  
+Assumptions.hs
+  - A list of assumptions for the SWHS example
+
+ATrace.png
+  - Figure of the Assumption traceability graph for the documentation
+
+Body.hs
+  - The main document body representation for the SWHS example
+  
+Changes.hs
+  - A list of likely and unlikely changes for the SWHS example
+  
+Concepts.hs
+  - SWHS's example-specific concepts 
+  
+DataDefs.hs
+  - SWHS's data definitions
+
+DataDesc.hs
+  - The input data description for the SWHS input file
+
+GenDefs.hs
+  - SWHS's general definitions
+  
+Generate.hs
+  - A list of what should be generated
+
+IMods.hs
+  - SWHS's instance models
+  
+Main.hs 
+  - Contains the main function which runs the generator
+ 
+README.md
+  - This file
+
+References.hs
+  - SWHS's references
+
+Requirements.hs
+  - A list of requirements (specifications) for the SWHS example
+  
+RTrace.png
+  - Requirements traceability graph
+
+SystemContextFigure.png
+  - Figure for the documentation
+
+Tank.png
+  - Figure of a tank for the SWHS documentation
+  
+TMods.hs
+  - SWHS's theoretical models
+  
+Unitals.hs
+  - SWHS's example-specific quantities

84c390e369a307586a5b7561273f2504e53bfcd7
Don <cd155@163.com>
2021-07-30 11:57:54 -0400

    add new rod, mass, anglular vel, and dis angle

--- code/drasil-example/dblpendulum/lib/Drasil/DblPendulum/Unitals.hs
+++ code/drasil-example/dblpendulum/lib/Drasil/DblPendulum/Unitals.hs
@@ -19,6 +19,8 @@ import Data.Drasil.Concepts.Physics (pendulum, twoD)
 import Data.Drasil.Concepts.Math as CM (angle, iAngle)
 import Data.Drasil.Quantities.Math as QM (unitVect, unitVectj, pi_)
 import Drasil.DblPendulum.Concepts (rod, firstRod, secondRod, firstObject, secondObject)
+import Data.Drasil.Units.Physics (angVelU)
+import Data.Drasil.Quantities.Physics (angularVelocity)
 
 
 symbols:: [QuantityDict]
@@ -38,38 +40,66 @@ units :: [UnitaryConceptDict]
 units = map ucw unitalChunks
 
 unitalChunks :: [UnitalChunk]
-unitalChunks = [lenRod, lenRod_1, lenRod_2, massObj_1, massObj_2, QPP.mass, QP.force, QP.ixPos, QP.xPos, QP.yPos,
-   QP.angularVelocity, QP.angularAccel, QP.gravitationalAccel, QP.tension, QP.acceleration,
-   QP.yAccel, QP.xAccel, QP.yVel, QP.xVel, QP.iyPos, QP.time, QP.velocity, QP.position, QP.torque,
-   QP.momentOfInertia, QP.angularDisplacement, QP.angularVelocity, initialPendAngle,
-   QP.angularFrequency, QP.frequency, QP.period, pendDisplacementAngle]
+unitalChunks = [lenRod, lenRod_1, lenRod_2, massObj_1, massObj_2, angularVel_1, angularVel_2,
+  pendDisAngle_1, pendDisAngle_2, 
+  QPP.mass, QP.force, QP.ixPos, QP.xPos, QP.yPos,
+  QP.angularVelocity, QP.angularAccel, QP.gravitationalAccel, QP.tension, QP.acceleration,
+  QP.yAccel, QP.xAccel, QP.yVel, QP.xVel, QP.iyPos, QP.time, QP.velocity, QP.position, QP.torque,
+  QP.momentOfInertia, QP.angularDisplacement, QP.angularVelocity, initialPendAngle,
+  QP.angularFrequency, QP.frequency, QP.period, pendDisplacementAngle]
 
-lenRod, lenRod_1, lenRod_2, massObj_1, massObj_2, pendDisplacementAngle, initialPendAngle :: UnitalChunk
+lenRod, lenRod_1, lenRod_2, massObj_1, massObj_2, angularVel_1, angularVel_2, 
+  pendDisAngle_1, pendDisAngle_2, pendDisplacementAngle, initialPendAngle,
+  initPendAngle_1, initPendAngle_2 :: UnitalChunk
 
--- fix me, replace lenRod with lenOne
-lenRod = makeUCWDS "l_rod" (cn "length of the rod")
+-- Fix me, replace lenRod with lenOne
+lenRod = makeUCWDS "l_rod" (nounPhraseSent $ phraseNP(len `the_ofThe` rod))
         (phraseNP (len `the_ofThe` rod))
         (sub cL lRod) metre
 
-lenRod_1 = makeUCWDS "l_1" (cn "length of the rod")
-        (phraseNP (len `the_ofThe` firstRod))
+lenRod_1 = makeUCWDS "l_1" (nounPhraseSent $ phraseNP(len `the_ofThe` firstRod))
+        (phraseNP (len `the_ofThe` firstRod)) -- Fix me, can have more information 
         (sub cL label1) metre
 
-lenRod_2 = makeUCWDS "l_2" (cn "length of the rod")
+lenRod_2 = makeUCWDS "l_2" (nounPhraseSent $ phraseNP(len `the_ofThe` secondRod))
         (phraseNP (len `the_ofThe` secondRod))
         (sub cL label2) metre
 
-massObj_1 = makeUCWDS "m_1" (cn "mass of the object")
+massObj_1 = makeUCWDS "m_1" (nounPhraseSent $ phraseNP (mass `the_ofThe` firstObject))
         (phraseNP (mass `the_ofThe` firstObject))
         (sub lM label1) kilogram
 
-massObj_2 = makeUCWDS "m_2" (cn "mass of the object")
+massObj_2 = makeUCWDS "m_2" (nounPhraseSent $ phraseNP (mass `the_ofThe` secondObject))
         (phraseNP (mass `the_ofThe` secondObject))
         (sub lM label2) kilogram
 
-pendDisplacementAngle = makeUCWDS "pendDisplacementAngle" (cn "displacement angle of the pendulum")
+angularVel_1 = makeUCWDS "w_1" (nounPhraseSent $ phraseNP (angularVelocity `the_ofThe` firstObject))
+        (phraseNP (angularVelocity `the_ofThe` firstObject))
+        (sub lW label1) angVelU
+
+angularVel_2 = makeUCWDS "w_2" (nounPhraseSent $ phraseNP (angularVelocity `the_ofThe` secondObject))
+        (phraseNP (angularVelocity `the_ofThe` secondObject))
+        (sub lW label2) angVelU
+
+pendDisAngle_1 = makeUCWDS "theta_1" (nounPhraseSent $ phraseNP (angle `the_ofThe` firstRod))
+        (phraseNP (angle `the_ofThe` firstRod))
+        (sub lTheta (Concat [lP, label1])) degree
+
+pendDisAngle_2 = makeUCWDS "theta_2" (nounPhraseSent $ phraseNP (angle `the_ofThe` secondRod))
+        (phraseNP (angle `the_ofThe` secondRod))
+        (sub lTheta (Concat [lP, label2])) degree
+
+pendDisplacementAngle = makeUCWDS "pendDisplacementAngle" (nounPhraseSent $ phraseNP (angle `the_ofThe` pendulum))
         (phraseNP (angle `the_ofThe` pendulum))
         (sub lTheta lP) degree
+
+initPendAngle_1 = makeUCWDS "initialPendAngle" (cn "initial pendulum angle")
+        (phraseNP (NP.the (CM.iAngle `of_` pendulum)))
+        (sub lTheta (Concat [lI, label1])) radian
+
+initPendAngle_2 = makeUCWDS "initialPendAngle" (cn "initial pendulum angle")
+        (phraseNP (NP.the (CM.iAngle `of_` pendulum)))
+        (sub lTheta (Concat [lI, label2])) radian
 
 initialPendAngle = makeUCWDS "initialPendAngle" (cn "initial pendulum angle")
         (phraseNP (NP.the (CM.iAngle `of_` pendulum)))

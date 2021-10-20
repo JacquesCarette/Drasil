1155614870917bcbdbfd4dc34b9506e4ee73c104
cd155 <github@donchen.page>
2021-10-19 21:50:40 -0400

    add displayODE which constructs the Canonical form

--- code/drasil-lang/lib/Language/Drasil/Chunk/DifferentialModel.hs
+++ code/drasil-lang/lib/Language/Drasil/Chunk/DifferentialModel.hs
@@ -6,32 +6,28 @@ module Language.Drasil.Chunk.DifferentialModel (
     makeLinear
     ) where
 
-import Control.Lens (makeLenses, (^.), view)-- set
-
-import Language.Drasil.Chunk.Concept (ConceptChunk, dccWDS) -- cw
+import Control.Lens (makeLenses, (^.), view)
+import Language.Drasil.Chunk.Concept (ConceptChunk, dccWDS)
 import Language.Drasil.Classes.Core (HasUID(uid))
-import Language.Drasil.Classes (Express(..), -- Concept,
+import Language.Drasil.Classes (Express(..),
   ConceptDomain(..), Definition(..), Idea(..), NamedIdea(..))
 import Language.Drasil.ModelExpr.Lang (ModelExpr)
 import Language.Drasil.NounPhrase.Core (NP)
 import Language.Drasil.Sentence (Sentence)
 import Language.Drasil.Expr.Lang (Expr(..))
--- u' = ax0 + bx1 + cx2 + .... + Contant -- 
+import Language.Drasil.Chunk.Unital (UnitalChunk)
+import Language.Drasil.ModelExpr.Class (ModelExprC(nthderiv, equiv))
+import Language.Drasil.Expr.Class (ExprC(mulRe, addRe, exactDbl))
 
--- 1 how to build the coefficients
--- 2 auto constr relation when coeff and const were given
+-- checked 1 how to build the coefficients 
+-- checked 2 auto constr relation when coeff and const were given
 -- 3 integrate ODEInfo, build ODEInfo
 data DifferentialModel = Linear {
-                                  -- _dmuid :: UID,
-                                  -- _namedidea :: NP,
-                                  -- _idea :: Maybe String,
-                                  -- _defintion :: Sentence,
-                                  -- _conceptdomain :: [UID],
-                                  _coefficients :: [Expr], -- how this be built
+                                  _indepVar :: UnitalChunk, -- often time
+                                  _depVar :: ModelExpr, -- qdProcessVariableTD in PDController
+                                  _coefficients :: [Expr],
                                   _constant :: Expr,
-                                  _conc :: ConceptChunk,
-                                  _rel  :: ModelExpr -- const from coeff and const
-                                  -- ODEInfo
+                                  _conc :: ConceptChunk
                                 }
 makeLenses ''DifferentialModel
 
@@ -48,7 +44,20 @@ instance Definition    DifferentialModel where defn = conc . defn
 instance ConceptDomain DifferentialModel where cdom = cdom . view conc
 -- | Finds the domain of the 'ConceptChunk' used to make the 'DifferentialModel'.
 -- | Convert the 'DifferentialModel' into the model expression language.
-instance Express       DifferentialModel where express = (^. rel)
+-- | Set Canonical form of ODE to Zero, e.g. ax0 + bx1 + cx2 + .... + c = 0
+instance Express       DifferentialModel where express d = equiv $ displayODE d : [exactDbl 0]
 
-makeLinear :: Express e => String -> NP -> Sentence -> [Expr] -> Expr -> e -> DifferentialModel
-makeLinear rID rTerm rDefn coeff consta = Linear coeff consta (dccWDS rID rTerm rDefn) . express
+-- | Construct a Canonical form of ODE, e.g. ax0 + bx1 + cx2 + .... + c
+-- | x0 is the highest order, x1 is the second higest order, and so on. The c is the constant.
+displayODE :: DifferentialModel -> ModelExpr
+displayODE d = foldr1 addRe (
+               zipWith mulRe (map express (d ^.coefficients))
+               [nthderiv (toInteger n) (d ^. depVar) (d ^. indepVar)
+               | n <- reverse [1..length (d ^. coefficients)]])
+               `addRe` express (d ^. constant)
+
+-- | Create a 'DifferentialModel' from a given indepVar ('UnitalChunk'), DepVar ('ModelExpr'),
+-- | Coefficients ('[Expr]'), Constant ('Expr'), UID ('String'), term ('NP'), definition ('Sentence').
+makeLinear :: UnitalChunk -> ModelExpr -> [Expr] -> Expr -> String -> NP -> Sentence -> DifferentialModel
+makeLinear dmIndepVar dmDepVar dmCoeff dmConst dmID dmTerm dmDefn = 
+  Linear dmIndepVar dmDepVar dmCoeff dmConst (dccWDS dmID dmTerm dmDefn)

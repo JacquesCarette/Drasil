b26aa4d2e5e6719023483449384f374db7a432d0
cd155 <github@donchen.page>
2022-05-24 16:43:11 -0400

    add gencode in main

--- code/drasil-example/dblpendulum/app/Main.hs
+++ code/drasil-example/dblpendulum/app/Main.hs
@@ -2,13 +2,14 @@ module Main (main) where
 
 import GHC.IO.Encoding
 
-import Language.Drasil.Generate (gen, genDot, genLog, DocSpec(DocSpec), DocType(SRS), Format(..), docChoices)
+import Language.Drasil.Generate (gen, genCode, genDot, genLog, DocSpec(DocSpec), DocType(SRS), Format(..), docChoices)
 import Drasil.DblPendulum.Body (srs, printSetting, fullSI)
-
+import Drasil.DblPendulum.Choices (choices, code)
 
 main :: IO()
 main = do
   setLocaleEncoding utf8
   gen (DocSpec (docChoices SRS [HTML, TeX]) "DblPendulum_SRS") srs printSetting
+  genCode choices code
   genDot fullSI
   genLog fullSI printSetting

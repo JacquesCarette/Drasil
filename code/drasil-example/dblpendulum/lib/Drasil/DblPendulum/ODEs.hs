cafbf20aa9e0e3d68604afbdb30408ce575f1e46
cd155 <github@donchen.page>
2022-05-26 15:57:41 -0400

    correct the order of o1 and o2

--- code/drasil-example/dblpendulum/lib/Drasil/DblPendulum/ODEs.hs
+++ code/drasil-example/dblpendulum/lib/Drasil/DblPendulum/ODEs.hs
@@ -20,39 +20,26 @@ dblPenODEInfo = odeInfo
     quantvar massObj_2, quantvar lenRod_1, quantvar lenRod_2]
   (exactDbl 0)
   (exactDbl 30) -- final time
-  [dbl 0.5, exactDbl 0, dbl 0.25, exactDbl 0]
-  [o1,
+  [dbl 1.3463968515384828, exactDbl 0, dbl 2.356194490192345, exactDbl 0]
+  [ o1,
     neg g `mulRe`
-        (two `mulRe` m1 `addRe` m2) `mulRe` sin t1 $-
-        (m2 `mulRe` g `mulRe`
-        sin (t1 $- (two `mulRe` t2))) $-
-        ((two `mulRe` sin (t1 $- t2 )) `mulRe` m2 `mulRe`
-        (
-            square o2 `mulRe` l2 `addRe`
-            (square o1 `mulRe` l1 `mulRe` cos (t1 $- t2))
-        ))
-        $/
-        l1 `mulRe`
-        (
-            two `mulRe` m1 `addRe` m2 $-
-            (m2 `mulRe`
-            cos (two `mulRe` t1  $- (two `mulRe` t2)))
-        ),
+      (two `mulRe` m1 `addRe` m2) `mulRe` sin t1 $-
+      (m2 `mulRe` g `mulRe` sin (t1 $- (two `mulRe` t2))) $-
+      ((two `mulRe` sin (t1 $- t2 )) `mulRe` m2 `mulRe`
+      (square o2 `mulRe` l2 `addRe`
+      (square o1 `mulRe` l1 `mulRe` cos (t1 $- t2))))
+      $/
+      l1 `mulRe`(two `mulRe` m1 `addRe` m2 $-
+      (m2 `mulRe` cos (two `mulRe` t1  $- (two `mulRe` t2)))),
+    o2,
     two `mulRe` sin (t1 $- t2) `mulRe`
-        (
-            square o1 `mulRe` l1 `mulRe` (m1 `addRe` m2 ) `addRe`
-            (g `mulRe` (m1 `addRe` m2 ) `mulRe` cos t1) `addRe`
-            (square o2 `mulRe` l2 `mulRe` m2 `mulRe`
-            cos (t1 $- t2 ))
-        )
-        $/
-        l2 `mulRe`
-        (
-            two `mulRe` m1 `addRe` m2 $-
-            (m2 `mulRe`
-            cos (two `mulRe` t1  $- (two `mulRe` t2)))
-        ),
-    o2]
+      (square o1 `mulRe` l1 `mulRe` (m1 `addRe` m2 ) `addRe`
+      (g `mulRe` (m1 `addRe` m2 ) `mulRe` cos t1) `addRe`
+      (square o2 `mulRe` l2 `mulRe` m2 `mulRe` cos (t1 $- t2 )))
+      $/
+      l2 `mulRe`(two `mulRe` m1 `addRe` m2 $-
+      (m2 `mulRe` cos (two `mulRe` t1  $- (two `mulRe` t2))))
+    ]
   dblPenODEOpts
     where t1 = idx (sy pendDisAngle) (int 0) -- t1 is theta 1
           o1 = idx (sy pendDisAngle) (int 1) -- o1 is omega 1

07cd0c126537641e2b2629afe6221ba515196bf9
cd155 <github@donchen.page>
2022-06-14 14:01:19 -0400

    change in stable

--- code/stable/nopcm/src/cpp/ODE.cpp
+++ code/stable/nopcm/src/cpp/ODE.cpp
@@ -8,5 +8,5 @@ ODE::ODE(double tau_W, double T_C) : tau_W(tau_W), T_C(T_C) {
 }
 
 void ODE::operator()(vector<double> T_W, vector<double> &dT_W, double t) {
-    dT_W.at(0) = 1.0 / tau_W * (T_C - T_W.at(0));
+    dT_W.at(0) = -(1.0 / tau_W) * T_W.at(0) + 1.0 / tau_W * T_C;
 }

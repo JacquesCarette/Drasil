8f5c10bfcb8b136b215f7cf0806f377e2ef494f9
Jason Balaci <jason@balaci.me>
2022-11-17 15:15:50 -0500

    Revert spacing changes to MultiDefn, but still clean it up a bit

--- code/drasil-theory/lib/Theory/Drasil/MultiDefn.hs
+++ code/drasil-theory/lib/Theory/Drasil/MultiDefn.hs
@@ -1,27 +1,15 @@
-{-# LANGUAGE PostfixOperators #-}
-{-# LANGUAGE Rank2Types #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE PostfixOperators, Rank2Types, ScopedTypeVariables, TemplateHaskell,
+             FlexibleInstances, MultiParamTypeClasses, TupleSections #-}
 
 -- | Defines types and functions for creating mult-definitions.
-module Theory.Drasil.MultiDefn
-  ( -- * Types
-    MultiDefn,
-    DefiningExpr,
-
-    -- * Constructors
-    mkMultiDefn,
-    mkMultiDefnForQuant,
-    mkDefiningExpr,
-
-    -- * Functions
-    multiDefnGenQD,
-    multiDefnGenQDByUID,
-  )
-where
+module Theory.Drasil.MultiDefn(
+  -- * Types
+  MultiDefn, DefiningExpr,
+  -- * Constructors
+  mkMultiDefn, mkMultiDefnForQuant, mkDefiningExpr,
+  -- * Functions
+  multiDefnGenQD, multiDefnGenQDByUID,
+) where
 
 import Control.Lens (makeLenses, view, (^.))
 import Data.List (union)
@@ -31,7 +19,8 @@ import Language.Drasil.Development (showUID)
 
 -- | 'DefiningExpr' are the data that make up a (quantity) definition, namely
 --   the description, the defining (rhs) expression and the context domain(s).
---   These are meant to be 'alternate' but equivalent definitions for a single concept.
+--   These are meant to be 'alternate' but equivalent definitions for a single
+--   concept.
 data DefiningExpr e = DefiningExpr
   { -- | UID
     _deUid :: UID,
@@ -55,53 +44,46 @@ instance Definition (DefiningExpr e) where defn = rvDesc
 
 -- | 'MultiDefn's are QDefinition factories, used for showing one or more ways
 --   we can define a QDefinition.
-data MultiDefn e = MultiDefn
-  { -- | UID
-    _rUid :: UID,
-    -- | Underlying quantity it defines
-    _qd :: QuantityDict,
-    -- | Defining description/statement
-    _rDesc :: Sentence,
-    -- | All possible/omitted ways we can define the related quantity
-    _rvs :: NE.NonEmpty (DefiningExpr e)
-  }
+data MultiDefn e = MultiDefn{
+  -- | UID
+  _rUid :: UID,
+  -- | Underlying quantity it defines.
+  _qd :: QuantityDict,
+  -- | Explanation of the different ways we can define a quantity.
+  _rDesc :: Sentence,
+  -- | All possible ways we can define the related quantity.
+  _rvs :: NE.NonEmpty (DefiningExpr e)
+}
 
 makeLenses ''MultiDefn
 
-instance HasUID (MultiDefn e) where uid = rUid
-
-instance HasSymbol (MultiDefn e) where symbol = symbol . (^. qd)
-
-instance NamedIdea (MultiDefn e) where term = qd . term
-
-instance Idea (MultiDefn e) where getA = getA . (^. qd)
-
-instance HasSpace (MultiDefn e) where typ = qd . typ
-
-instance Quantity (MultiDefn e)
-
-instance MayHaveUnit (MultiDefn e) where getUnit = getUnit . view qd
-
--- | The concept domain of a MultiDefn is the union of the concept domains of the underlying variants.
-instance ConceptDomain (MultiDefn e) where cdom = foldr1 union . NE.toList . NE.map (^. cd) . (^. rvs)
-
-instance Definition (MultiDefn e) where defn = rDesc
-
-instance TypeChecks (MultiDefn Expr) Expr Space where
+instance HasUID        (MultiDefn e) where uid     = rUid
+instance HasSymbol     (MultiDefn e) where symbol  = symbol . (^. qd)
+instance NamedIdea     (MultiDefn e) where term    = qd . term
+instance Idea          (MultiDefn e) where getA    = getA . (^. qd)
+instance HasSpace      (MultiDefn e) where typ     = qd . typ
+instance Definition    (MultiDefn e) where defn    = rDesc
+instance Quantity      (MultiDefn e)
+instance MayHaveUnit   (MultiDefn e) where getUnit = getUnit . view qd
+-- | The concept domain of a MultiDefn is the union of the concept domains of
+-- the underlying variants.
+instance ConceptDomain (MultiDefn e) where
+  cdom = foldr1 union . NE.toList . NE.map (^. cd) . (^. rvs)
+instance TypeChecks    (MultiDefn Expr) Expr Space where
   typeCheckExpr md = map (\x -> (x ^. expr, md ^. typ)) $ NE.toList $ md ^. rvs
 
--- | The complete Relation of a MultiDefn is defined as the quantity and the related expressions being equal
---   e.g., `q $= a $= b $= ... $= z`
+-- | The complete Relation of a MultiDefn is defined as the quantity and the
+--   related expressions being equal (e.g., `q $= a $= b $= ... $= z`)
 instance Express e => Express (MultiDefn e) where
   express q = equiv $ sy q : NE.toList (NE.map (express . (^. expr)) (q ^. rvs))
 
--- | Smart constructor for MultiDefns, does nothing special at the moment. First argument is the 'String' to become a 'UID'.
+-- | Smart constructor for MultiDefns, does nothing special at the moment. First
+-- argument is the 'String' to become a 'UID'.
 mkMultiDefn :: String -> QuantityDict -> Sentence -> NE.NonEmpty (DefiningExpr e) -> MultiDefn e
 mkMultiDefn u q s des
   | length des == dupsRemovedLen = MultiDefn (mkUid u) q s des
-  | otherwise =
-    error $
-      "MultiDefn '" ++ u ++ "' created with non-unique list of expressions"
+  | otherwise                    = error $
+      "MultiDefn `" ++ u ++ "` created with non-unique list of expressions"
   where
     dupsRemovedLen = length $ NE.nub des
 
@@ -131,7 +113,8 @@ multiDefnGenQD md de =
 multiDefnGenQDByUID :: MultiDefn e -> UID -> QDefinition e
 multiDefnGenQDByUID md u
   | length matches == 1 = multiDefnGenQD md matched
-  | otherwise = error $ "Invalid UID for multiDefn QD generation; " ++ show u
+  | otherwise           = error $
+    "Invalid defining expression `" ++ show u ++ "` for QDef creation in MultiDefn `" ++ show u ++ "`"
   where
     matches = NE.filter (\x -> x ^. uid == u) (md ^. rvs)
     matched = head matches

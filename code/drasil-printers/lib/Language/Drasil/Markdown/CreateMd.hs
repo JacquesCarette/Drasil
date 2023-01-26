ba4d31a18e0747146cf7f333c9affa92994b061c
Peter Michalski <michap@mcmaster.ca>
2023-01-26 18:46:09 -0500

    adding blurb before example name

--- code/drasil-printers/lib/Language/Drasil/Markdown/CreateMd.hs
+++ code/drasil-printers/lib/Language/Drasil/Markdown/CreateMd.hs
@@ -19,7 +19,12 @@ makeMd = vcat . punctuate secSep . filtEmp
 
 -- | Example title, authors, and maybe purpose section.
 introInfo :: String -> [String] -> Maybe String -> Doc
-introInfo name auths descr = introSec (text name) (listToDoc auths) (length auths) (maybePurpDoc descr)
+introInfo name auths descr = introSec (text name) (prepName name)
+  (listToDoc auths) (length auths) (maybePurpDoc descr)
+
+-- | Helper for prepending the example name.
+prepName :: String -> Doc
+prepName name = text "> The program documented here is called" <+> text name
 
 -- | Instruction section, contains 3 paragraphs, Running, Building and Config Files.
 -- The Config file section is only displayed if there are configuration files.
@@ -125,9 +130,9 @@ bkQuote3 = text "```"
 -- FIXME as explained in #2224 we still need to add in the purpose section, 
 -- this could be done by adding a third parameter to introSec
 -- | Constructs introduction section from header and message.
-introSec ::  Doc -> Doc -> Int -> Doc -> Doc
-introSec hd ms1 l descr = text "#" <+> hd <+> contSep <> (if l == 1 then text "> Author:" else text "> Authors: ") 
-  <+> ms1 <> descr
+introSec ::  Doc -> Doc -> Doc -> Int -> Doc -> Doc
+introSec hd name ms1 l descr = text "#" <+> hd <+> contSep <> name <> doubleSep <> 
+  (if l == 1 then text "> Author:" else text "> Authors: ") <+> ms1 <> descr
 
 -- | Constructs regular section section from header and message.
 regularSec :: Doc -> Doc -> Doc

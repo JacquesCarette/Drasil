2b4872bd9940f3b3899d0001feb447babbe6c23c
Harmanpreet Singh Sagar <harmanpreet1308@gmail.com>
2023-05-09 13:37:40 -0400

    Revert "Updated stable to add smithEtAl2007 and smithKoothoor2016 references"
    This reverts commit 960baf922e71d28523a13c88714b4b3a8806d425.

--- code/stable/build/glassbr/src/python/InputParameters.py
+++
@@ -1,6 +0,0 @@
-## \file InputParameters.py
-# \author Nikitha Krithnan and W. Spencer Smith
-# \brief Provides the structure for holding input values
-## \brief Structure for holding the input values and derived values
-class InputParameters:
-    None

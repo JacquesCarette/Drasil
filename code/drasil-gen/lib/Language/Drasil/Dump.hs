525a60f4e7d3542165871ee8641d394223c29287
Jason Balaci <jason@balaci.me>
2023-06-09 14:25:18 -0400

    Dump trace and reverse trace too

--- code/drasil-gen/lib/Language/Drasil/Dump.hs
+++ code/drasil-gen/lib/Language/Drasil/Dump.hs
@@ -6,12 +6,14 @@ import SysInfo.Drasil (SystemInformation(_sysinfodb))
 
 import System.Directory
 import System.IO
-import Data.Aeson
-import Data.Aeson.Encode.Pretty
+import Data.Aeson (ToJSON)
+import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy.Char8 as LB
 import qualified Data.Map.Strict as SM
 
 import Utils.Drasil (invert)
+import Database.Drasil (traceTable, refbyTable)
+import Control.Lens ((^.))
 
 type Path = String
 type TargetFile = String
@@ -19,13 +21,18 @@ type TargetFile = String
 dumpEverything :: SystemInformation -> Path -> IO ()
 dumpEverything si targetPath = do
   createDirectoryIfMissing True targetPath
-  let chunkDump = DB.dumpChunkDB $ _sysinfodb si
-  let invertedChunkDump = invert chunkDump
-  let sharedUIDs = SM.filter (\x -> length x > 1) invertedChunkDump
+  let chunkDb = _sysinfodb si
+      chunkDump = DB.dumpChunkDB chunkDb
+      invertedChunkDump = invert chunkDump
+      sharedUIDs = SM.filter (\x -> length x > 1) invertedChunkDump
+      traceDump = chunkDb ^. traceTable
+      refByDump = chunkDb ^. refbyTable
 
   dumpTo chunkDump $ targetPath ++ "seeds.json"
   dumpTo invertedChunkDump $ targetPath ++ "inverted_seeds.json"
   dumpTo sharedUIDs $ targetPath ++ "problematic_seeds.json"
+  dumpTo traceDump $ targetPath ++ "trace.json"
+  dumpTo refByDump $ targetPath ++ "reverse_trace.json"
 
 -- FIXME: This is more of a general utility than it is drasil-database specific
 dumpTo :: ToJSON a => a -> TargetFile -> IO ()

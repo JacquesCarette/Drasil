96b47c5f285506f8625cfc8c6d79b3ece2adfbd0
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2023-07-22 23:23:09 -0400

    Issue #3481 Change version #1
    First version: deleting _instmodels objects in the "SystemInformation.hs" file

--- code/drasil-sysinfo/lib/SysInfo/Drasil/SystemInformation.hs
+++ code/drasil-sysinfo/lib/SysInfo/Drasil/SystemInformation.hs
@@ -10,7 +10,7 @@ module SysInfo.Drasil.SystemInformation (
   -- ** Types
   SystemInformation(..), Block(..),
   -- ** Lenses
-  instModels, datadefs, configFiles, inputs, purpose, background,
+  datadefs, configFiles, inputs, purpose, background,
   defSequence, constraints, constants, sysinfodb, usedinfodb,
   -- ** Lookup Functions
   citeDB, citationsFromBibMap,
@@ -52,7 +52,6 @@ data SystemInformation where
   , _background  :: Background
   , _quants      :: [e]
   , _concepts    :: [f]
-  , _instModels  :: [InstanceModel]
   , _datadefs    :: [DataDefinition]
   , _configFiles :: [String]
   , _inputs      :: [h]

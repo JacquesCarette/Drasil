a14473c3a898fcdc2e066616d1ad3d232a958733
BilalM04 <bilal71612@gmail.com>
2024-05-29 12:21:49 -0400

    created helper and print packages for Markdown

---
+++ code/drasil-printers/lib/Language/Drasil/Markdown/Helpers.hs
@@ -0,0 +1,169 @@
+-- | Defines helper functions for creating jupyter notebooks.
+module Language.Drasil.Markdown.Helpers where
+
+import Prelude hiding ((<>))
+import Text.PrettyPrint (Doc, text, empty, (<>), vcat, hcat)
+import Data.List (intersperse)
+import Data.List.Split (splitOn)
+
+import Language.Drasil (MaxWidthPercent)
+import qualified Language.Drasil.Printing.Helpers as H
+import Language.Drasil.HTML.Helpers (img)
+import Numeric (showHex)
+
+data Variation = Class | Id
+
+tr, td, figure, li, pa, ba :: Doc -> Doc
+-- | Table row tag wrapper
+tr         = wrap "tr" []
+-- | Table cell tag wrapper
+td         = wrap "td" []
+-- | Figure tag wrapper
+figure     = wrap "figure" []
+-- | List tag wrapper
+li         = wrap' "li" []
+-- | Paragraph in list tag wrapper
+pa         = wrap "p" []
+-- | Bring attention to element wrapper.
+ba         = wrap "b" []
+
+ol, ul, table :: [String] -> Doc -> Doc
+-- | Ordered list tag wrapper
+ol       = wrap "ol"
+-- | Unordered list tag wrapper
+ul       = wrap "ul"
+-- | Table tag wrapper
+table    = wrap "table"
+
+-- FIXME: Why are we using a Doc if we use 'show'?
+nbformat :: Doc -> Doc
+nbformat s = text ("    \"" ++ escapeStringForJson (show s) ++ "\\n\",")
+
+-- TODO: This replaces the `json` library import, and is modelled after it:
+-- https://www.stackage.org/haddock/lts-20.20/json-0.10/src/Text.JSON.String.html#encJSString
+-- However, we should ultimately replace this with our own JSON encoding with
+-- more printing options and using `Doc`s appropriately. Alternatively, if we
+-- prefer to use a premade library, Aeson has more features, is actively
+-- developed, and is already compiled by our other dependencies.
+escapeStringForJson :: String -> String
+escapeStringForJson = concatMap (either id ('\\' :) . special)
+  where
+    special :: Char -> Either String String
+    special c
+      | c `elem` "\"\\"       = Right [c]
+      | c `elem` "\b\f\n\r\t" = Right (show [c])
+      -- note: the below double quotes disregard the point of ShowS, but that
+      -- shouldn't be an issue if we switch to using `Doc`s.
+      | c < '\x20'            = Right (showHex (fromEnum c) "")
+      | otherwise             = Left [c]
+
+codeformat :: Doc -> Doc
+codeformat s = text ("    \"" ++ escapeStringForJson (show s) ++ "\\n\"")
+
+wrap :: String -> [String] -> Doc -> Doc
+wrap a = wrapGen' vcat Class a empty
+
+wrap' :: String -> [String] -> Doc -> Doc
+wrap' a = wrapGen' hcat Class a empty
+
+wrapGen' :: ([Doc] -> Doc) -> Variation -> String -> Doc -> [String] -> Doc -> Doc
+wrapGen' sepf _ s _ [] = \x ->
+  let tb c = text $ "<" ++ c ++ ">"
+  --in sepf [quote(tb s), x, quote(tb $ '/':s)]
+  in if s == "li" then sepf [tb s, x, tb $ '/':s] else sepf [tb s, x, tb $ '/':s]
+wrapGen' sepf Class s _ ts = \x ->
+  let tb c = text $ "<" ++ c ++ " class=\\\"" ++ foldr1 (++) (intersperse " " ts) ++ "\\\">"
+  in let te c = text $ "</" ++ c ++ ">"
+  in sepf [tb s, x, te s]
+wrapGen' sepf Id s ti _ = \x ->
+  let tb c = text ("\n<" ++ c ++ " id=\"") <> ti <> text "\">\n"
+      te c = text $ "\n</" ++ c ++ ">\n"
+  in  sepf [tb s, x, te s]
+
+refwrap :: Doc -> Doc -> Doc
+refwrap = flip (wrapGen' vcat Id "div") [""]
+
+refID :: Doc -> Doc
+refID i = text "<div id=\"" <> i <> text "\"></div>\n"
+
+-- | Helper for setting up links to references
+reflink :: String -> Doc -> Doc
+reflink ref txt = text "[" <> txt <> text ("](#" ++ ref ++ ")")
+--reflink ref txt = text ("<a href=#" ++ ref ++ ">") <> txt <> text "</a>"
+
+-- | Helper for setting up links to external URIs
+reflinkURI :: String -> Doc -> Doc
+reflinkURI ref txt = text ("<a href=\\\"" ++ ref ++ "\\\">") <> txt <> text "</a>"
+
+-- | Helper for setting up figures
+image :: Doc -> Doc -> Doc
+image f c =  text "![" <> c <> text "](" <> f <> text ")"
+
+h :: Int -> Doc
+h n       | n < 1 = error "Illegal header (too small)"
+          | n > 4 = error "Illegal header (too large)"
+          | otherwise = text (hash n)
+              where hash 1 = "# "
+                    hash 2 = "## "
+                    hash 3 = "### "
+                    hash 4 = "#### "
+                    hash _ = "Illegal header"
+
+-- | Curly braces.
+br :: Doc -> Doc
+br x = text "{" <> x <> text "}"
+
+mkDiv :: String -> Doc -> Doc -> Doc
+mkDiv s a0 a1 = (H.bslash <> text s) <> br a0 <> br a1
+
+-- Maybe use "lines" instead (Data.List @lines :: String -> [String])
+stripnewLine :: String -> Doc
+stripnewLine s = hcat (map text (splitOn "\n" s))
+
+-- | Helper for building Markdown cells
+markdownB, markdownB', markdownE, markdownE' :: Doc
+markdownB  = text "{\n \"cells\": [\n  {\n   \"cell_type\": \"markdown\",\n   \"metadata\": {},\n   \"source\": [\n" 
+markdownB' = text "  {\n   \"cell_type\": \"markdown\",\n   \"metadata\": {},\n   \"source\": [\n" 
+markdownE  = text "    \"\\n\"\n   ]\n  },"
+markdownE' = text "    \"\\n\"\n   ]\n  }\n ],"
+
+-- | Helper for building code cells
+codeB, codeE :: Doc
+codeB = text "  {\n   \"cell_type\": \"code\",\n   \"execution_count\": null,\n   \"metadata\": {},\n   \"outputs\": [],\n   \"source\": [" 
+codeE  = text "\n   ]\n  },"
+
+-- | Helper for generate a Markdown cell
+markdownCell :: Doc -> Doc
+markdownCell c = markdownB' <> c <> markdownE
+
+-- | Helper for generate a code cell
+codeCell :: Doc -> Doc
+codeCell c = codeB <> c <> codeE
+
+-- | Generate the metadata necessary for a notebook document.
+makeMetadata :: Doc  
+makeMetadata = vcat [
+  text " \"metadata\": {",
+  vcat [
+    text "  \"kernelspec\": {",
+    text "   \"display_name\": \"Python 3\",",
+    text "   \"language\": \"python\",",
+    text "   \"name\": \"python3\"",
+    text "  },"],
+  vcat [
+    text "  \"language_info\": {",
+    text "   \"codemirror_mode\": {",
+    text "    \"name\": \"ipython\",",
+    text "    \"version\": 3",
+    text "   },"],
+  text "   \"file_extension\": \".py\",",
+  text "   \"mimetype\": \"text/x-python\",",
+  text "   \"name\": \"python\",",
+  text "   \"nbconvert_exporter\": \"python\",",
+  text "   \"pygments_lexer\": \"ipython3\",",
+  text "   \"version\": \"3.9.1\"",
+  text "  }",
+  text " },",
+  text " \"nbformat\": 4,", 
+  text " \"nbformat_minor\": 4" 
+ ]
---
+++ code/drasil-printers/lib/Language/Drasil/Markdown/Print.hs
@@ -0,0 +1,279 @@
+-- | Defines .json printers to generate jupyter notebooks. For more information on each of the helper functions, please view the [source files](https://jacquescarette.github.io/Drasil/docs/full/drasil-printers-0.1.10.0/src/Language.Drasil.JSON.Print.html).
+module Language.Drasil.Markdown.Print(genMD) where
+
+import Prelude hiding (print, (<>))
+import Text.PrettyPrint hiding (Str)
+import Numeric (showEFloat)
+
+import qualified Language.Drasil as L
+
+import Language.Drasil.Format (DocType(Jupyter))
+
+import Language.Drasil.Printing.Import (makeDocument)
+import Language.Drasil.Printing.AST (Spec, ItemType(Flat, Nested),  
+  ListType(Ordered, Unordered, Definitions, Desc, Simple), Expr, 
+  Ops(..), Expr(..), Spec(Quote, EmptyS, Ref, HARDNL, Sp, S, E, (:+:)),
+  Fonts(Bold), OverSymb(Hat), Label, LinkType(Internal, Cite2, External))
+import Language.Drasil.Printing.Citation (BibRef)
+import Language.Drasil.Printing.LayoutObj (Document(Document), LayoutObj(..))
+import Language.Drasil.Printing.Helpers (sqbrac, unders, hat)
+import Language.Drasil.Printing.PrintingInformation (PrintingInformation)
+
+import qualified Language.Drasil.TeX.Print as TeX (spec, pExpr)
+import Language.Drasil.TeX.Monad (runPrint, MathContext(Math), D, toMath, PrintLaTeX(PL))
+import Language.Drasil.HTML.Monad (unPH)
+import Language.Drasil.HTML.Helpers (th, bold, reflinkInfo)
+import Language.Drasil.HTML.Print(renderCite, OpenClose(Open, Close), fence)
+
+import Language.Drasil.Markdown.Helpers (makeMetadata, h, stripnewLine, nbformat, codeformat,
+ tr, td, image, li, pa, ba, table, refwrap, refID, reflink, reflinkURI, mkDiv, 
+ markdownB, markdownB', markdownE, markdownE', markdownCell, codeCell)
+
+-- | Generate a python notebook document (using json).
+-- build : build the SRS document in JSON format
+-- build': build the general Jupyter Notbook document
+genMD :: PrintingInformation -> L.Document -> Doc
+genMD sm doc = build (makeDocument sm doc)
+
+-- | Build the JSON Document, called by genJSON
+build :: Document -> Doc
+build (Document t a c) = 
+  text "# " <> pSpec t $$
+  text "## " <> pSpec a $$
+  print c 
+
+-- | Helper for rendering a D from Latex print
+printMath :: D -> Doc
+printMath = (`runPrint` Math)
+
+-- | Helper for rendering LayoutObjects into JSON
+-- printLO is used for generating SRS
+printLO :: LayoutObj -> Doc
+printLO (Header n contents l)            = empty $$ (h (n + 1) <> pSpec contents)
+printLO (Cell layoutObs)                 = markdownCell $ vcat (map printLO layoutObs)
+printLO (HDiv _ layoutObs _)             = vcat (map printLO layoutObs)
+printLO (Paragraph contents)             = empty $$ (stripnewLine (show(pSpec contents)))
+printLO (EqnBlock contents)              = mathEqn
+  where
+    toMathHelper (PL g) = PL (\_ -> g Math)
+    mjDelimDisp d  = text "$$" <> stripnewLine (show d) <> text "$$" 
+    mathEqn = mjDelimDisp $ printMath $ toMathHelper $ TeX.spec contents
+printLO (Table _ rows r _ _)            = empty $$ makeTable rows (pSpec r)
+printLO (Definition dt ssPs l)          = (text "<br>") $$ makeDefn dt ssPs (pSpec l)
+printLO (List t)                        = empty $$ makeList t False
+printLO (Figure r c f wp)               = makeFigure (pSpec r) (pSpec c) (text f) wp
+printLO (Bib bib)                       = makeBib bib
+printLO Graph{}                         = empty 
+printLO CodeBlock {}                    = empty
+
+-- printLO' is used for generating general notebook (lesson plans)
+printLO' :: LayoutObj -> Doc
+printLO' (HDiv ["equation"] layoutObs _)  = markdownCell $ vcat (map printLO' layoutObs)
+printLO' (Header n contents l)            = markdownCell $ (h (n + 1) <> pSpec contents)
+printLO' (Cell layoutObs)                 = vcat (map printLO' layoutObs)
+printLO' HDiv {}                          = empty
+printLO' (Paragraph contents)             = markdownCell $ (stripnewLine (show(pSpec contents)))
+printLO' (EqnBlock contents)              = mathEqn
+  where
+    toMathHelper (PL g) = PL (\_ -> g Math)
+    mjDelimDisp d  = text "$$" <> stripnewLine (show d) <> text "$$" 
+    mathEqn = mjDelimDisp $ printMath $ toMathHelper $ TeX.spec contents
+printLO' (Table _ rows r _ _)            = markdownCell $ makeTable rows (pSpec r)
+printLO' Definition {}                   = empty
+printLO' (List t)                        = markdownCell $ makeList t False
+printLO' (Figure r c f wp)               = markdownCell $ makeFigure (pSpec r) (pSpec c) (text f) wp
+printLO' (Bib bib)                       = markdownCell $ makeBib bib
+printLO' Graph{}                         = empty 
+printLO' (CodeBlock contents)            = codeCell $ codeformat $ cSpec contents
+
+
+-- | Called by build, uses 'printLO' to render the layout
+-- objects in Doc format.
+print :: [LayoutObj] -> Doc
+print = foldr (($$) . printLO) empty
+
+-- | Called by build', uses 'printLO'' to render the layout
+-- objects in Doc format.
+print' :: [LayoutObj] -> Doc
+print' = foldr (($$) . printLO') empty
+
+pSpec :: Spec -> Doc
+pSpec (E e)  = text "$" <> pExpr e <> text "$" -- symbols used
+pSpec (a :+: b) = pSpec a <> pSpec b
+pSpec (S s)     = either error (text . concatMap escapeChars) $ L.checkValidStr s invalid
+  where
+    invalid = ['<', '>']
+    escapeChars '&' = "\\&"
+    escapeChars c = [c]
+pSpec (Sp s)    = text $ unPH $ L.special s
+pSpec HARDNL    = empty
+pSpec (Ref Internal r a)      = reflink     r $ pSpec a
+pSpec (Ref (Cite2 n)   r a)    = reflinkInfo r (pSpec a) (pSpec n)
+pSpec (Ref External r a)      = reflinkURI  r $ pSpec a
+pSpec EmptyS    = text "" -- Expected in the output
+pSpec (Quote q) = doubleQuotes $ pSpec q
+
+cSpec :: Spec -> Doc
+cSpec (E e)  = pExpr e 
+cSpec _      = empty
+
+
+-- | Renders expressions in JSON (called by multiple functions)
+pExpr :: Expr -> Doc
+pExpr (Dbl d)        = text $ showEFloat Nothing d ""
+pExpr (Int i)        = text $ show i
+pExpr (Str s)        = doubleQuotes $ text s
+pExpr (Div n d)      = mkDiv "frac" (pExpr n) (pExpr d)
+pExpr (Row l)        = hcat $ map pExpr l
+pExpr (Ident s)      = text s
+pExpr (Label s)      = text s
+pExpr (Spec s)       = text $ unPH $ L.special s
+pExpr (Sub e)        = unders <> pExpr e
+pExpr (Sup e)        = hat <> pExpr e
+pExpr (Over Hat s)   = pExpr s <> text "&#770;"
+pExpr (MO o)         = text $ pOps o
+pExpr (Fenced l r e) = text (fence Open l) <> pExpr e <> text (fence Close r)
+pExpr (Font Bold e)  = pExpr e
+--pExpr (Font Bold e)  = bold $ pExpr e -- used before
+--pExpr (Font Emph e)  = text "<em>" <> pExpr e <> text "</em>" -- HTML used
+--pExpr (Spc Thin)     = text "&#8239;" -- HTML used
+-- Uses TeX for Mathjax for all other exprs 
+pExpr e              = printMath $ toMath $ TeX.pExpr e
+
+
+
+-- TODO: edit all operations in markdown format
+pOps :: Ops -> String
+pOps IsIn     = "&thinsp;&isin;&thinsp;"
+pOps Integer  = "&#8484;"
+pOps Rational = "&#8474;"
+pOps Real     = "&#8477;"
+pOps Natural  = "&#8469;"
+pOps Boolean  = "&#120121;"
+pOps Comma    = ","
+pOps Prime    = "&prime;"
+pOps Log      = "log"
+pOps Ln       = "ln"
+pOps Sin      = "sin"
+pOps Cos      = "cos"
+pOps Tan      = "tan"
+pOps Sec      = "sec"
+pOps Csc      = "csc"
+pOps Cot      = "cot"
+pOps Arcsin   = "arcsin"
+pOps Arccos   = "arccos"
+pOps Arctan   = "arctan"
+pOps Not      = "&not;"
+pOps Dim      = "dim"
+pOps Exp      = "e"
+pOps Neg      = "-"
+pOps Cross    = "&#10799;"
+pOps VAdd     = " + "
+pOps VSub     = " - "
+pOps Dot      = "&sdot;"
+pOps Scale    = "" -- same as Mul
+pOps Eq       = " = " -- with spaces?
+pOps NEq      = "&ne;"
+pOps Lt       = "&thinsp;&lt;&thinsp;" --thin spaces make these more readable
+pOps Gt       = "&thinsp;&gt;&thinsp;"
+pOps LEq      = "&thinsp;&le;&thinsp;"
+pOps GEq      = "&thinsp;&ge;&thinsp;"
+pOps Impl     = " &rArr; "
+pOps Iff      = " &hArr; "
+pOps Subt     = " - "
+pOps And      = " &and; "
+pOps Or       = " &or; "
+pOps Add      = " + "
+pOps Mul      = ""
+pOps Summ     = "&sum"
+pOps Inte     = "&int;"
+pOps Prod     = "&prod;"
+pOps Point    = "."
+pOps Perc     = "%"
+pOps LArrow   = " &larr; "
+pOps RArrow   = " &rarr; "
+pOps ForAll   = " ForAll "
+pOps Partial  = "&part;"
+
+
+-- | Renders Markdown table, called by 'printLO'
+makeTable :: [[Spec]] -> Doc -> Doc
+makeTable [] _      = error "No table to print"
+makeTable (l:lls) r = refwrap r (empty $$ (makeHeaderCols l $$ makeRows lls) $$ empty)
+
+-- | Helper for creating table rows
+makeRows :: [[Spec]] -> Doc
+makeRows = foldr (($$) . makeColumns) empty
+
+-- | makeHeaderCols: Helper for creating table header row (each of the column header cells)
+-- | makeColumns: Helper for creating table columns
+makeHeaderCols, makeColumns :: [Spec] -> Doc
+makeHeaderCols l = (text header) $$ (text $ genMDtable ++ "|")
+  where header = show(text "|" <> hcat(punctuate (text "|") (map pSpec l)) <> text "|")        
+        c = count '|' header
+        genMDtable = concat (replicate (c-1) "|:--- ")
+
+makeColumns ls = (text "|" <> hcat(punctuate (text "|") (map pSpec ls)) <> text "|")
+
+count :: Char -> String -> Int
+count _ [] = 0
+count c (x:xs) 
+  | c == x = 1 + count c xs
+  | otherwise = count c xs
+
+-- | Renders definition tables (Data, General, Theory, etc.)
+makeDefn :: L.DType -> [(String,[LayoutObj])] -> Doc -> Doc
+makeDefn _ [] _  = error "L.Empty definition"
+makeDefn dt ps l = refID l $$ table [dtag dt]
+  (tr ((th (text "Refname")) $$ td ((bold l))) $$ makeDRows ps)
+  where dtag L.General  = "gdefn"
+        dtag L.Instance = "idefn"
+        dtag L.Theory   = "tdefn"
+        dtag L.Data     = "ddefn"
+
+-- | Helper for making the definition table rows
+makeDRows :: [(String,[LayoutObj])] -> Doc
+makeDRows []         = error "No fields to create defn table"
+makeDRows [(f,d)]    = tr ((th (text f)) $$ td (vcat $ map printLO d))
+makeDRows ((f,d):ps) = tr ((th (text f)) $$ td (vcat $ map printLO d)) $$ makeDRows ps
+
+
+-- | Renders lists
+makeList :: ListType -> Bool -> Doc -- FIXME: ref id's should be folded into the li
+makeList (Simple items) _      = vcat $ 
+  map (\(b,e,l) -> mlref l $ (pSpec b <> text ": " <> sItem e) $$ empty) items
+makeList (Desc items) bl       = vcat $ 
+  map (\(b,e,l) -> pa $ mlref l $ ba $ pSpec b <> text ": " <> pItem e bl) items
+makeList (Ordered items) bl    = vcat $ map (\(i,l) -> mlref l $ pItem i bl) items
+makeList (Unordered items) bl  = vcat $ map (\(i,l) -> mlref l $ pItem i bl) items
+--makeList (Definitions items) _ = ul ["hide-list-style-no-indent"] $ vcat $ 
+  --map (\(b,e,l) -> li $ mlref l $ quote(pSpec b <> text " is the" <+> sItem e)) items
+makeList (Definitions items) _ = vcat $ map (\(b,e,l) -> li $ mlref l $ pSpec b <> text " is the" <+> sItem e) items
+
+-- | Helper for setting up references
+mlref :: Maybe Label -> Doc -> Doc
+mlref = maybe id $ refwrap . pSpec
+
+-- | Helper for rendering list items
+pItem :: ItemType ->  Bool -> Doc
+pItem (Flat s)     b = (if b then text " - " else text "- ") <> pSpec s
+pItem (Nested s l) _ = vcat [text "- " <> pSpec s, makeList l True]
+  --where listIndent = strBreak "\"" (show $ makeList l)
+--indent <> text "\"- " <> pSpec s <> text "\\n\","
+
+sItem :: ItemType -> Doc
+sItem (Flat s)     = pSpec s
+sItem (Nested s l) = vcat [pSpec s, makeList l False]
+
+-- | Renders figures in HTML
+makeFigure :: Doc -> Doc -> Doc -> L.MaxWidthPercent -> Doc
+makeFigure r c f wp = refwrap r (image f c)
+
+-- | Renders assumptions, requirements, likely changes
+makeRefList :: Doc -> Doc -> Doc -> Doc
+makeRefList a l i = refwrap l (i <> text ": " <> a)
+
+makeBib :: BibRef -> Doc
+makeBib = vcat .
+  zipWith (curry (\(x,(y,z)) -> makeRefList z y x))
+  [text $ sqbrac $ show x | x <- [1..] :: [Int]] . map renderCite

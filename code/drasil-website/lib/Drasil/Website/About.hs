01663405f5398e768e3d53716d7b0ca4dc3e6fad
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2024-06-03 00:40:07 -0400

    #3360

--- code/drasil-website/lib/Drasil/Website/About.hs
+++ code/drasil-website/lib/Drasil/Website/About.hs
@@ -29,18 +29,18 @@ aboutParagraph1 repoRef wikiRef = S "Drasil is a framework for generating all of
   \duplication and improve traceability. The artifacts are generated from a common knowledge-base using recipes \
   \written in a Domain-Specific Language (DSL). These recipes allow us to specify which pieces of \
   \knowledge should be used in which artifacts, how to transform them, and more. For more information on the design, documentation, \
-  \useage, and specifics of Drasil, please visit the" +:+ namedRef repoRef (S "GitHub repository") +:+ S "or the" +:+ 
+  \usage, and specifics of Drasil, please visit the" +:+ namedRef repoRef (S "GitHub repository") +:+ S "or the" +:+ 
   (namedRef wikiRef (S "GitHub Wiki") !.)
 
 -- | Paragraph to describe the layout of the rest of the Drasil website.
 aboutParagraph2 :: Reference -> Reference -> Reference -> Sentence
-aboutParagraph2 caseStudySecRef docsRef analysisSecRef = S "This webpage is designed to contain the most up to date" +:+
+aboutParagraph2 caseStudySecRef docsRef analysisSecRef = S "This webpage is designed to contain the most up-to-date" +:+
   foldlList Comma List (zipWith (\x y -> namedRef x (S y)) [caseStudySecRef, docsRef, analysisSecRef] ["case study artifacts", 
   "Haddock documentation", "Drasil analysis"]) +:+ S "from the Drasil repository. \
   \The case study artifacts include the Software Requirements Specification (SRS) for each case study, \
   \which specifies what the program sets out to achieve. \
   \The Haddock Documentation section contains the current documentation for the Drasil framework. \
-  \The package dependency graphs shows the hierarchy of modules within each package."
+  \The package dependency graphs show the hierarchy of modules within each package."
   -- \The footer of this page contains the continuous integration build of the project, \
   -- \as well as the commit number that the build and artifacts are based off of.
 
@@ -92,7 +92,7 @@ aboutParagraph6 chunksWiki = S "As described in the" +:+ namedRef chunksWiki (S 
 
 -- | Paragraph describing recipes
 aboutParagraph7 :: Reference -> Sentence
-aboutParagraph7 recipesWiki= S "As described in the" +:+ namedRef recipesWiki (S "Recipes") +:+ 
+aboutParagraph7 recipesWiki = S "As described in the" +:+ namedRef recipesWiki (S "Recipes") +:+ 
   sC wikiSentence (S "recipes are instructions that") +:+
   S "unpackage necessary information from chunks and send \
   \that information to Drasil generators/printers to build complete artifacts. When an artifact needs to be changed, the recipe is modified to \

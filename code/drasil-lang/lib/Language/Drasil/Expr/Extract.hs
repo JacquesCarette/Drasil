9b2b1b055bcf38f5a3c6f543d964798e915a6b87
Noah <cardoson@mcmaster.ca>
2024-06-17 10:59:34 -0400

    Changed eNames and getUIDs to Sets

--- code/drasil-lang/lib/Language/Drasil/Expr/Extract.hs
+++ code/drasil-lang/lib/Language/Drasil/Expr/Extract.hs
@@ -1,73 +1,73 @@
 -- | Extract UIDs from an expression so that they can be looked up in the chunk database and rendered.
 module Language.Drasil.Expr.Extract where
 
-import Data.Set (Set, fromList)
+import Data.Set (Set, union, unions, insert, singleton, empty)
 
 import Language.Drasil.Expr.Lang (Expr(..))
 import Language.Drasil.Space (RealInterval(..))
 import Language.Drasil.UID (UID)
 
 -- | Generic traverse of all expressions that could lead to names.
-eNames :: Expr -> [UID]
-eNames (AssocA _ l)          = concatMap eNames l
-eNames (AssocB _ l)          = concatMap eNames l
-eNames (C c)                 = [c]
-eNames Lit{}                 = []
-eNames (FCall f x)           = f : concatMap eNames x
-eNames (Case _ ls)           = concatMap (eNames . fst) ls ++
-                               concatMap (eNames . snd) ls
+eNames :: Expr -> Set UID
+eNames (AssocA _ l)          = unions (map eNames l)
+eNames (AssocB _ l)          = unions (map eNames l)
+eNames (C c)                 = singleton c
+eNames Lit{}                 = empty
+eNames (FCall f x)           = insert f (unions (map eNames x))
+eNames (Case _ ls)           = unions (map (eNames . fst) ls) `union`
+                               unions (map (eNames . snd) ls)
 eNames (UnaryOp _ u)         = eNames u
 eNames (UnaryOpB _ u)        = eNames u
 eNames (UnaryOpVV _ u)       = eNames u
 eNames (UnaryOpVN _ u)       = eNames u
-eNames (ArithBinaryOp _ a b) = eNames a ++ eNames b
-eNames (BoolBinaryOp _ a b)  = eNames a ++ eNames b
-eNames (EqBinaryOp _ a b)    = eNames a ++ eNames b
-eNames (LABinaryOp _ a b)    = eNames a ++ eNames b
-eNames (OrdBinaryOp _ a b)   = eNames a ++ eNames b
-eNames (VVVBinaryOp _ a b)   = eNames a ++ eNames b
-eNames (VVNBinaryOp _ a b)   = eNames a ++ eNames b
-eNames (NVVBinaryOp _ a b)   = eNames a ++ eNames b
+eNames (ArithBinaryOp _ a b) = eNames a `union` eNames b
+eNames (BoolBinaryOp _ a b)  = eNames a `union` eNames b
+eNames (EqBinaryOp _ a b)    = eNames a `union` eNames b
+eNames (LABinaryOp _ a b)    = eNames a `union` eNames b
+eNames (OrdBinaryOp _ a b)   = eNames a `union` eNames b
+eNames (VVVBinaryOp _ a b)   = eNames a `union` eNames b
+eNames (VVNBinaryOp _ a b)   = eNames a `union` eNames b
+eNames (NVVBinaryOp _ a b)   = eNames a `union` eNames b
 eNames (Operator _ _ e)      = eNames e
-eNames (Matrix a)            = concatMap (concatMap eNames) a
-eNames (RealI c b)           = c : eNamesRI b
+eNames (Matrix a)            = unions (map (unions . map eNames) a)
+eNames (RealI c b)           = insert c (eNamesRI b)
 
 -- | Generic traversal of everything that could come from an interval to names (similar to 'eNames').
-eNamesRI :: RealInterval Expr Expr -> [UID]
-eNamesRI (Bounded (_, il) (_, iu)) = eNames il ++ eNames iu
+eNamesRI :: RealInterval Expr Expr -> Set UID
+eNamesRI (Bounded (_, il) (_, iu)) = eNames il `union` eNames iu
 eNamesRI (UpTo (_, iu))            = eNames iu
 eNamesRI (UpFrom (_, il))          = eNames il
 
 -- | Generic traverse of all positions that could lead to 'eNames' without
 -- functions.  FIXME : this should really be done via post-facto filtering, but
 -- right now the information needed to do this is not available!
-eNames' :: Expr -> [UID]
-eNames' (AssocA _ l)          = concatMap eNames' l
-eNames' (AssocB _ l)          = concatMap eNames' l
-eNames' (C c)                 = [c]
-eNames' Lit{}                 = []
-eNames' (FCall _ x)           = concatMap eNames' x
-eNames' (Case _ ls)           = concatMap (eNames' . fst) ls ++ 
-                                concatMap (eNames' . snd) ls
+eNames' :: Expr -> Set UID
+eNames' (AssocA _ l)          = unions (map eNames' l)
+eNames' (AssocB _ l)          = unions (map eNames' l)
+eNames' (C c)                 = singleton c
+eNames' Lit{}                 = empty
+eNames' (FCall _ x)           = unions (map eNames' x)
+eNames' (Case _ ls)           = unions (map (eNames' . fst) ls) `union`
+                                unions (map (eNames' . snd) ls)
 eNames' (UnaryOp _ u)         = eNames' u
 eNames' (UnaryOpB _ u)        = eNames' u
 eNames' (UnaryOpVV _ u)       = eNames' u
 eNames' (UnaryOpVN _ u)       = eNames' u
-eNames' (ArithBinaryOp _ a b) = eNames' a ++ eNames' b
-eNames' (BoolBinaryOp _ a b)  = eNames' a ++ eNames' b
-eNames' (EqBinaryOp _ a b)    = eNames' a ++ eNames' b
-eNames' (LABinaryOp _ a b)    = eNames' a ++ eNames' b
-eNames' (OrdBinaryOp _ a b)   = eNames' a ++ eNames' b
-eNames' (VVVBinaryOp _ a b)   = eNames' a ++ eNames' b
-eNames' (VVNBinaryOp _ a b)   = eNames' a ++ eNames' b
-eNames' (NVVBinaryOp _ a b)   = eNames' a ++ eNames' b
+eNames' (ArithBinaryOp _ a b) = eNames' a `union` eNames' b
+eNames' (BoolBinaryOp _ a b)  = eNames' a `union` eNames' b
+eNames' (EqBinaryOp _ a b)    = eNames' a `union` eNames' b
+eNames' (LABinaryOp _ a b)    = eNames' a `union` eNames' b
+eNames' (OrdBinaryOp _ a b)   = eNames' a `union` eNames' b
+eNames' (VVVBinaryOp _ a b)   = eNames' a `union` eNames' b
+eNames' (VVNBinaryOp _ a b)   = eNames' a `union` eNames' b
+eNames' (NVVBinaryOp _ a b)   = eNames' a `union` eNames' b
 eNames' (Operator _ _ e)      = eNames' e
-eNames' (Matrix a)            = concatMap (concatMap eNames') a
-eNames' (RealI c b)           = c : eNamesRI' b
+eNames' (Matrix a)            = unions (map (unions . map eNames') a)
+eNames' (RealI c b)           = insert c (eNamesRI' b)
 
 -- | Generic traversal of everything that could come from an interval to names without functions (similar to 'eNames'').
-eNamesRI' :: RealInterval Expr Expr -> [UID]
-eNamesRI' (Bounded il iu) = eNames' (snd il) ++ eNames' (snd iu)
+eNamesRI' :: RealInterval Expr Expr -> Set UID
+eNamesRI' (Bounded il iu) = eNames' (snd il) `union` eNames' (snd iu)
 eNamesRI' (UpTo iu)       = eNames' (snd iu)
 eNamesRI' (UpFrom il)     = eNames' (snd il)
 
@@ -76,4 +76,4 @@ eNamesRI' (UpFrom il)     = eNames' (snd il)
 
 -- | Get dependencies from an equation.  
 eDep :: Expr -> Set UID
-eDep = fromList . eNames
+eDep = eNames
--- code/drasil-lang/lib/Language/Drasil/Sentence/Extract.hs
+++ code/drasil-lang/lib/Language/Drasil/Sentence/Extract.hs
@@ -2,7 +2,7 @@
 -- chunk database in order to render terms, symbols, and references properly.
 module Language.Drasil.Sentence.Extract(sdep, shortdep, lnames, lnames') where
 
-import Data.Set (fromList, Set)
+import Data.Set (fromList, Set, empty, singleton, union)
 
 import Language.Drasil.UID (UID)
 import Language.Drasil.Sentence(Sentence(..), SentenceStyle(..))
@@ -10,47 +10,47 @@ import Language.Drasil.ModelExpr.Extract (meNames)
 
 
 -- | Generic traverse of all positions that could lead to /symbolic/ 'UID's from 'Sentence's.
-getUIDs :: Sentence -> [UID]
-getUIDs (Ch ShortStyle _ _) = []
-getUIDs (Ch TermStyle _ _)  = []
-getUIDs (Ch PluralTerm _ _) = []
-getUIDs (SyCh a)            = [a]
-getUIDs Sy {}               = []
-getUIDs S {}                = []
-getUIDs P {}                = []
-getUIDs Ref {}              = []
-getUIDs Percent             = []
-getUIDs ((:+:) a b)         = getUIDs a ++ getUIDs b
+getUIDs :: Sentence -> Set UID
+getUIDs (Ch ShortStyle _ _) = empty
+getUIDs (Ch TermStyle _ _)  = empty
+getUIDs (Ch PluralTerm _ _) = empty
+getUIDs (SyCh a)            = singleton a
+getUIDs Sy {}               = empty
+getUIDs S {}                = empty
+getUIDs P {}                = empty
+getUIDs Ref {}              = empty
+getUIDs Percent             = empty
+getUIDs ((:+:) a b)         = getUIDs a `union` getUIDs b
 getUIDs (Quote a)           = getUIDs a
-getUIDs (E a)               = meNames a
-getUIDs EmptyS              = []
+getUIDs (E a)               = fromList $ meNames a
+getUIDs EmptyS              = empty
 
 -- | Generic traverse of all positions that could lead to /symbolic/ and /abbreviated/ 'UID's from 'Sentence's
 -- but doesn't go into expressions.
-getUIDshort :: Sentence -> [UID]
-getUIDshort (Ch ShortStyle _ a) = [a]
-getUIDshort (Ch TermStyle _ _)  = []
-getUIDshort (Ch PluralTerm _ _) = []
-getUIDshort SyCh {}             = []
-getUIDshort Sy {}               = []
-getUIDshort S {}                = []
-getUIDshort Percent             = []
-getUIDshort P {}                = []
-getUIDshort Ref {}              = []
-getUIDshort ((:+:) a b)         = getUIDshort a ++ getUIDshort b
+getUIDshort :: Sentence -> Set UID
+getUIDshort (Ch ShortStyle _ a) = singleton a
+getUIDshort (Ch TermStyle _ _)  = empty
+getUIDshort (Ch PluralTerm _ _) = empty
+getUIDshort SyCh {}             = empty
+getUIDshort Sy {}               = empty
+getUIDshort S {}                = empty
+getUIDshort Percent             = empty
+getUIDshort P {}                = empty
+getUIDshort Ref {}              = empty
+getUIDshort ((:+:) a b)         = getUIDshort a `union` getUIDshort b
 getUIDshort (Quote a)           = getUIDshort a
-getUIDshort E {}                = []
-getUIDshort EmptyS              = []
+getUIDshort E {}                = empty
+getUIDshort EmptyS              = empty
 
 -----------------------------------------------------------------------------
 -- And now implement the exported traversals all in terms of the above
 -- | This is to collect /symbolic/ 'UID's that are printed out as a 'Symbol'.
 sdep :: Sentence -> Set UID
-sdep = fromList . getUIDs
+sdep = getUIDs
 
 -- This is to collect symbolic 'UID's that are printed out as an /abbreviation/.
 shortdep :: Sentence -> Set UID
-shortdep = fromList . getUIDshort
+shortdep = getUIDshort
 
 -- | Generic traverse of all positions that could lead to /reference/ 'UID's from 'Sentence's.
 lnames :: Sentence -> [UID]

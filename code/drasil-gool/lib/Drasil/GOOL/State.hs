7f5e492ba6f45b063fd054e947158c698193a3e6
Noah <cardoson@mcmaster.ca>
2024-06-27 01:59:17 -0400

    rm nub from callMapTransClosure

--- code/drasil-gool/lib/Drasil/GOOL/State.hs
+++ code/drasil-gool/lib/Drasil/GOOL/State.hs
@@ -470,7 +470,7 @@ callMapTransClosure = over callMap tClosure
         traceCalls :: Map QualifiedName [QualifiedName] -> [QualifiedName] -> 
           [QualifiedName]
         traceCalls _ [] = []
-        traceCalls cm (c:cs) = toList . fromList $ c : traceCalls cm ( cs ++ 
+        traceCalls cm (c:cs) = c : traceCalls cm ( cs ++ 
           Map.findWithDefault [] c cm)
 
 updateMEMWithCalls :: GOOLState -> GOOLState

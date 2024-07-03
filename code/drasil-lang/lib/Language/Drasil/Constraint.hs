cb36a02c8cb2e52da44364c73a7930b51bd9895a
Noah <cardoson@mcmaster.ca>
2024-07-03 10:41:47 -0400

    Constraint.hs

--- code/drasil-lang/lib/Language/Drasil/Constraint.hs
+++ code/drasil-lang/lib/Language/Drasil/Constraint.hs
@@ -9,6 +9,7 @@ module Language.Drasil.Constraint (
 
 import Language.Drasil.Expr.Lang
 import Language.Drasil.Space (RealInterval(..))
+import Data.Set (Set)
 
 -- | The reason behind the constraint's existence.
 data ConstraintReason = Physical | Software
@@ -21,9 +22,14 @@ data Constraint a where
   -- | By default, physical and software constraints are ranges.
   Range          :: ConstraintReason -> RealInterval a a -> Constraint a
 
+  Elem :: ConstraintReason -> Set a -> Constraint a
+
 -- | Smart constructor for range of 'Physical' constraints between two given expressions.
 physc :: RealInterval Expr Expr -> ConstraintE
 physc = Range Physical
+
+enumc :: Set Expr -> ConstraintE
+enumc = Elem Physical
 
 -- | Smart constructor for range of 'Software' constraints between two given expressions.
 sfwrc :: RealInterval Expr Expr -> ConstraintE
@@ -33,8 +39,10 @@ isPhysC, isSfwrC :: Constraint e -> Bool
 
 -- | Helpful for filtering for Physical constraints. True if constraint is 'Physical'.
 isPhysC (Range Physical _) = True
+isPhysC (Elem Physical _) = True
 isPhysC _ = False
 
 -- | Helpful for filtering for Software constraints. True if constraint is 'Software'.
 isSfwrC (Range Software _) = True
+isPhysC (Elem Software _) = True
 isSfwrC _ = False

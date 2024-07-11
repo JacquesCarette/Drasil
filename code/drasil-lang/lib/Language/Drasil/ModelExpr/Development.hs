de0c4980aa136a8ff022030114c077f58274b42c
Noah <cardoson@mcmaster.ca>
2024-07-11 16:14:33 -0400

    added nomThick back to constrained

--- code/drasil-lang/lib/Language/Drasil/ModelExpr/Development.hs
+++ code/drasil-lang/lib/Language/Drasil/ModelExpr/Development.hs
@@ -15,7 +15,7 @@ module Language.Drasil.ModelExpr.Development (
   -- ModelExpr.Extract
   , meDep
   -- ModelExpr.Precedence
-  , mePrec, precB, precA
+  , mePrec, precB, precA, precC
 ) where
 
 import Language.Drasil.Expr.Lang (Completeness(..))

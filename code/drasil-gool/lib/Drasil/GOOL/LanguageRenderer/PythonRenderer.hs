52acea40510fd09b530505f9e70702bde80e3d30
Noah <cardoson@mcmaster.ca>
2024-07-22 12:45:09 -0400

    added back 'in' in Python

--- code/drasil-gool/lib/Drasil/GOOL/LanguageRenderer/PythonRenderer.hs
+++ code/drasil-gool/lib/Drasil/GOOL/LanguageRenderer/PythonRenderer.hs
@@ -459,7 +459,7 @@ instance List PythonCode where
 
 instance Set PythonCode where
   --fromList =
-  contains = CP.contains pyIn
+  contains a b = isin b a
 
 instance InternalList PythonCode where
   listSlice' b e s vn vo = pyListSlice vn vo (getVal b) (getVal e) (getVal s)
@@ -852,7 +852,7 @@ pySplit = "split"
 pyRange = "range"
 pyRstrip = "rstrip"
 pyMath = "math"
-pyIn = "__contains__"
+pyIn = "in"
 pySetAdd = "Add"
 
 pyDef, pyLambdaDec, pyElseIf, pyRaise, pyExcept :: Doc

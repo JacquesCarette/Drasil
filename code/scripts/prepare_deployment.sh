c91c610c96b2e261bf8c78e823b9462d96f370bc
BilalM04 <bilal71612@gmail.com>
2024-07-22 17:49:50 -0400

    update deployment script

--- code/scripts/prepare_deployment.sh
+++ code/scripts/prepare_deployment.sh
@@ -54,7 +54,7 @@ if [ -z "$MAKE" ]; then
 fi
 
 DOC_DEST=docs/
-SRS_DEST=SRS/
+SRS_DEST=SRS
 DOX_DEST=doxygen/
 EXAMPLE_DEST=examples/
 CUR_DIR="$PWD/"
@@ -90,13 +90,13 @@ copy_examples() {
     # Only copy actual examples
     if [[ "$EXAMPLE_DIRS" == *"$example_name"* ]]; then
       target_srs_dir="./$EXAMPLE_DEST$example_name/$SRS_DEST"
-      mkdir -p "$target_srs_dir"PDF
-      mkdir -p "$target_srs_dir"HTML
+      mkdir -p "$target_srs_dir/PDF"
+      mkdir -p "$target_srs_dir/HTML"
       if [ -d "$example/SRS/PDF" ]; then
-        cp "$example/SRS/PDF/"*.pdf "$target_srs_dir"PDF
+        cp "$example/SRS/PDF/"*.pdf "$target_srs_dir/PDF"
       fi
       if [ -d "$example/SRS/HTML" ]; then
-        cp -r "$example/SRS/HTML/." "$target_srs_dir"HTML
+        cp -r "$example/SRS/HTML/." "$target_srs_dir/HTML"
       fi
       if [ -d "$example/src" ]; then
         mkdir -p "$EXAMPLE_DEST$example_name/$DOX_DEST"

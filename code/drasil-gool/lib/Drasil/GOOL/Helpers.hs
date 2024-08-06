fc1e8e48e80c40a4990f47930a2499cbe6342aea
Noah <cardoson@mcmaster.ca>
2024-08-06 12:42:58 -0400

    update error msg

--- code/drasil-gool/lib/Drasil/GOOL/Helpers.hs
+++ code/drasil-gool/lib/Drasil/GOOL/Helpers.hs
@@ -90,7 +90,7 @@ getInnerType :: C.CodeType -> C.CodeType
 getInnerType (C.List innerT) = innerT
 getInnerType (C.Array innerT) = innerT
 getInnerType (C.Set innerT) = innerT
-getInnerType _ = error "Attempt to extract inner type of list from a non-list type" 
+getInnerType _ = error "Attempt to extract inner type from a non-nested type"
 
 getNestDegree :: Integer -> C.CodeType -> Integer
 getNestDegree n (C.List t) = getNestDegree (n+1) t

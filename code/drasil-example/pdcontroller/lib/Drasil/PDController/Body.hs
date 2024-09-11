a35b065546a0e827e5ac619114c503ad2fb412b2
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2024-09-10 23:45:04 -0400

    #3252 Sentence Reworking (for the PdController Example, Part 1)
    Re-work some existing sentences by inserting defined concepts inside.

--- code/drasil-example/pdcontroller/lib/Drasil/PDController/Body.hs
+++ code/drasil-example/pdcontroller/lib/Drasil/PDController/Body.hs
@@ -118,9 +118,9 @@ si = SI {
    refdb = refDB}
 
 purp :: Sentence
-purp = foldlSent_ [S "provide a model of a", phrase pidC,
-         S "that can be used for the tuning of the gain constants before",
-         S "the deployment of the controller"]
+purp = foldlSent_ [S "provide a model" `S.ofA` phrase pidC,
+         S "that can be used for the tuning" `S.ofThe` S "gain constants before",
+         S "the deployment" `S.ofThe` S "controller"]
 
 symbolsAll :: [QuantityDict]
 symbolsAll = symbols ++ map qw pidDqdConstants ++ map qw pidConstants

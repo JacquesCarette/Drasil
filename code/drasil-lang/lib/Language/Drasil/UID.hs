67690b6bc5c15656c0643eb33cd77eea3f8a0499
Jason Balaci <jason@balaci.me>
2025-02-28 01:52:11 -0500

    #2885 : Clean up UIDs a little bit, reorder how the 'full display' is created

--- code/drasil-lang/lib/Language/Drasil/UID.hs
+++ code/drasil-lang/lib/Language/Drasil/UID.hs
@@ -23,51 +23,42 @@ class HasUID c where
   -- | Provides a /unique/ id for internal Drasil use.
   uid :: Getter c UID
 
--- | A @UID@ is a 'unique identifier' for things that we will put into our database
--- of information. We use a newtype wrapper to make sure we are only using
--- 'UID's where desired.
+-- | A @UID@ is a 'unique identifier' for things that we will put into our
+-- database of information. We use a newtype wrapper to make sure we are only
+-- using 'UID's where desired.
 data UID = UID { _namespace :: [String], _baseName :: String }
-  deriving (Eq, Ord, Generic) --, ToJSONKey)
+  deriving (Eq, Ord, Generic)
 
 makeLenses ''UID
 
-fullName :: UID -> [String]
-fullName u = u ^. namespace ++ [u ^. baseName]
-
 instance ToJSON UID where
-  toJSON = toJSON . fullName
+  toJSON = toJSON . show
 
 instance ToJSONKey UID where
   toJSONKey = toJSONKeyText (pack . show)
 
 instance Show UID where
-  show = intercalate ":" . fullName
+  show u = intercalate ":" $ u ^. baseName : u ^. namespace
 
--- | Smart constructor for making a 'UID' from a 'String'.
+-- | Smart constructor for 'UID's from raw 'String's.
 mkUid :: String -> UID
 mkUid s = UID { _namespace = [], _baseName = s }
-  -- '►' `elem` s = error $ "► not allowed in UID " ++ show s -- FIXME: Need to implement other constructors before we can use this.
-  -- null s       = error "UID must be non-zero length" -- FIXME: See Drasil.DocumentLanguage.TraceabilityGraph (uses an empty UID)
-  -- otherwise    = UID s
 
--- | Nest UID under a namespace
+-- | Nest a 'UID' under a namespace.
 nsUid :: String -> UID -> UID
 nsUid ns = over namespace (ns:)
 
--- | For when we need to modify a UID. We first take the base chunk's UID and then append a suffix to it.
+-- | Append a suffix to a thing with a 'UID' and get the resulting 'UID'.
 (+++) :: HasUID a => a -> String -> UID
-a +++ suff
-  | null suff       = error "Suffix must be non-zero length"
-  | otherwise       = (a ^. uid) +++. suff
-  -- otherwise       = UID $ s ++ '►':suff --FIXME: Implement this properly.
-  --   where UID s = a ^. uid
+(+++) a = (+++.) (a ^. uid)
 
--- | For when we need to append something to a UID.
+-- | Append a suffix to a 'UID'.
 (+++.) :: UID -> String -> UID
 a +++. suff
   | null suff       = error $ "Suffix must be non-zero length for UID " ++ show a
   | otherwise       = over baseName (++ suff) a
 
+-- | Merge the 'UID's of two chunks through simple concatenation.
 (+++!) :: (HasUID a, HasUID b) => a -> b -> UID
 a +++! b
   | s ^. namespace /= t ^. namespace = error $ show s ++ " and " ++ show t ++ " are not in the same namespace"
@@ -77,6 +68,6 @@ a +++! b
     s = a ^. uid
     t = b ^. uid
 
--- | Grabs the UID from something that has a UID and displays it as a String.
+-- | Get something's 'UID' as a 'String'.
 showUID :: HasUID a => a -> String
 showUID = show . view uid

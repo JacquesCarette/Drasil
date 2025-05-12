c9604f0ba7c62dad1a188de3dd3a754910d45735
Jack Wyand <wyandj@mcmaster.ca>
2025-05-12 20:59:41 +0100

    Remove requirement UID duplicate

--- code/drasil-example/ssp/lib/Drasil/SSP/Body.hs
+++ code/drasil-example/ssp/lib/Drasil/SSP/Body.hs
@@ -157,7 +157,7 @@ symbMap = cdb (map qw symbols) (map nw symbols
   ++ map nw defs ++ map nw defs' ++ map nw softwarecon ++ map nw physicCon 
   ++ map nw physicsTMs
   ++ map nw mathcon ++ map nw mathcon' ++ map nw solidcon ++ map nw physicalcon
-  ++ map nw doccon' ++ map nw derived ++ map nw fundamentals ++ map nw educon
+  ++ map nw derived ++ map nw fundamentals ++ map nw educon
   ++ map nw compcon ++ [nw algorithm, nw ssp] ++ map nw units)
   (map cw SSP.iMods ++ map cw symbols ++ srsDomains) units SSP.dataDefs SSP.iMods
   generalDefinitions tMods concIns labCon allRefs citations

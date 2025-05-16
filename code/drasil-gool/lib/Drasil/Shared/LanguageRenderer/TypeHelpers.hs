40755c43060f70c7a4d9da040b4e1bf9e6bb00fc
Sarra <sarrasoussia2001@gmail.com>
2025-05-16 15:13:43 -0400

    type helpers file

--- code/drasil-gool/lib/Drasil/Shared/LanguageRenderer/TypeHelpers.hs
+++ code/drasil-gool/lib/Drasil/Shared/LanguageRenderer/TypeHelpers.hs
@@ -1,7 +1,45 @@
-int
-double
-float
-string
-string'
-bool
-funcType
+module Drasil.Shared.LanguageRenderer.TypeHelpers (
+  int,
+  double,
+  float,
+  string,
+  string',
+  bool,
+  funcType,
+  arrayType
+) where
+
+import Drasil.Shared.RendererClassesCommon (CommonRenderSym, VSType)
+import Text.PrettyPrint.HughesPJ (text)
+
+-- | Integer type
+int :: (CommonRenderSym r) => VSType r
+int = typeFromData Integer "int" (text "int")
+
+-- | Double/float type
+double :: (CommonRenderSym r) => VSType r
+double = typeFromData Double "double" (text "double")
+
+-- | Float type
+float :: (CommonRenderSym r) => VSType r
+float = typeFromData Float "float" (text "float")
+
+-- | String type
+string :: (CommonRenderSym r) => VSType r
+string = typeFromData String "string" (text "string")
+
+-- | Alternate string type (if needed)
+string' :: (CommonRenderSym r) => VSType r
+string' = typeFromData String "String" (text "String")
+
+-- | Boolean type
+bool :: (CommonRenderSym r) => VSType r
+bool = typeFromData Boolean "bool" (text "bool")
+
+-- | Function type
+funcType :: (CommonRenderSym r) => [VSType r] -> VSType r -> VSType r
+funcType args ret = typeFromData (Function args ret) "func" (text "func")
+
+-- | Array type
+arrayType :: (CommonRenderSym r) => VSType r -> VSType r
+arrayType t = typeFromData (Array t) (show t ++ "[]") (text (show t ++ "[]"))

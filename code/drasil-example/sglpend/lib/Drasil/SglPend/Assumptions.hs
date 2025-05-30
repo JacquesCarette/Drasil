b837f80b078d415988f194d8f48dbf46abc3e8c4
Jason Balaci <jason@balaci.me>
2025-05-30 14:17:52 -0400

    #4036 : sglpend: Refine imports, add type signature to startOriginSingle.

--- code/drasil-example/sglpend/lib/Drasil/SglPend/Assumptions.hs
+++ code/drasil-example/sglpend/lib/Drasil/SglPend/Assumptions.hs
@@ -1,21 +1,21 @@
 {-# LANGUAGE PostfixOperators #-}
 module Drasil.SglPend.Assumptions (assumpSingle) where
     
-import Language.Drasil
-import Language.Drasil.Chunk.Concept.NamedCombinators
+import Language.Drasil (cic, phrase, atStartNP, (!.), ConceptInstance, Sentence(S))
+import Language.Drasil.Chunk.Concept.NamedCombinators (the)
 import qualified Language.Drasil.Sentence.Combinators as S
 
 import Data.Drasil.Concepts.Documentation (assumpDom) 
-import Data.Drasil.Concepts.Math (cartesian, xAxis, yAxis, direction, origin, positive)
-import Data.Drasil.Concepts.Physics (gravity, twoD, pendulum)
+import Data.Drasil.Concepts.Math (origin)
+import Data.Drasil.Concepts.Physics (pendulum)
 
 import Drasil.DblPend.Assumptions (assumpBasic)
-import Drasil.DblPend.Concepts (pendMotion, firstRod, secondRod, firstObject, secondObject)
 
 assumpSingle :: [ConceptInstance]
 assumpSingle = startOriginSingle : assumpBasic
 
-startOriginSingle = cic "startOrigin1x" startOriginDescSingle   "startOrigin"   assumpDom
+startOriginSingle :: ConceptInstance
+startOriginSingle = cic "startOrigin1x" startOriginDescSingle "startOrigin" assumpDom
 
 startOriginDescSingle :: Sentence
 startOriginDescSingle = atStartNP (the pendulum) `S.is` S "attached" `S.toThe` (phrase origin !.)

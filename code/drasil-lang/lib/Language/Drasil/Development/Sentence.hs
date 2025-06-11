2b5afa1cc8178eda84d5e89c7046407ce02c6b99
Jack Wyand <wyandj@mcmaster.ca>
2025-06-11 14:47:54 +0100

    Documentation for getA, short, and abrv

--- code/drasil-lang/lib/Language/Drasil/Development/Sentence.hs
+++ code/drasil-lang/lib/Language/Drasil/Development/Sentence.hs
@@ -20,6 +20,10 @@ import qualified Language.Drasil.NounPhrase as NP
 import Drasil.Database.UID (HasUID(..))
 
 -- | Get short form (if it exists), else get term of an 'Idea'.
+-- Uses the UID of the 'Idea' in a 'Ch' Sentence constructor to get the short
+-- form using getA. getA may return Nothing, in which case lookupS uses the
+-- term, where lookupS is the main helper for getting the short form of a
+-- 'Ch' Sentence.
 short :: Idea c => c -> Sentence
 short c = sentenceShort (c ^. uid)
 

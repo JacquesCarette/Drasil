38c819eef47e3ab6a0c750d6b4a58ee9c0521361
Jack Wyand <wyandj@mcmaster.ca>
2025-06-17 20:24:03 +0100

    Remove firstPend, secondPend, and startOrigin assumptions

--- code/drasil-example/dblpend/lib/Drasil/DblPend/Assumptions.hs
+++ code/drasil-example/dblpend/lib/Drasil/DblPend/Assumptions.hs
@@ -1,32 +1,28 @@
 {-# LANGUAGE PostfixOperators #-}
 module Drasil.DblPend.Assumptions (twoDMotion, cartSys, cartSysR,
-  yAxisDir, startOriginDouble, firstPend, secondPend, assumpBasic, assumpDouble) where
+  yAxisDir, assumpBasic, assumpDouble) where
     
 import Language.Drasil
 import Language.Drasil.Chunk.Concept.NamedCombinators
 import qualified Language.Drasil.Sentence.Combinators as S
 
 import Data.Drasil.Concepts.Documentation (assumpDom) 
-import Data.Drasil.Concepts.Math (cartesian, xAxis, yAxis, direction, origin, positive)
+import Data.Drasil.Concepts.Math (cartesian, xAxis, yAxis, direction, positive)
 import Data.Drasil.Concepts.Physics (gravity, twoD)
-import Drasil.DblPend.Concepts (pendMotion, firstRod, secondRod, firstObject, secondObject)
+import Drasil.DblPend.Concepts (pendMotion)
 
 assumpBasic :: [ConceptInstance]
 assumpBasic = [twoDMotion, cartSys, cartSysR, yAxisDir]
 
 assumpDouble :: [ConceptInstance]
-assumpDouble = assumpBasic ++ [startOriginDouble, firstPend, secondPend]
+assumpDouble = assumpBasic
 
-twoDMotion, cartSys, cartSysR, yAxisDir, startOriginDouble,
-  firstPend, secondPend:: ConceptInstance 
+twoDMotion, cartSys, cartSysR, yAxisDir :: ConceptInstance 
 
 twoDMotion        = cic "twoDMotion"    twoDMotionDesc          "twoDMotion"    assumpDom
 cartSys           = cic "cartSys"       cartSysDesc             "cartSys"       assumpDom
 cartSysR          = cic "cartSysR"      cartSysRDesc            "cartSysR"      assumpDom
 yAxisDir          = cic "yAxisDir"      yAxisDirDesc            "yAxisDir"      assumpDom
-startOriginDouble = cic "startOrigin2x" startOriginDescDouble   "startOrigin"   assumpDom
-firstPend         = cic "firstPend"     firstPendDesc           "firstPend"     assumpDom
-secondPend        = cic "secondPend"    secondPendDesc          "secondPend"    assumpDom
 
 twoDMotionDesc :: Sentence
 twoDMotionDesc = atStartNP (the pendMotion) `S.is` phrase twoD +:+. sParen (short twoD)
@@ -40,18 +36,3 @@ cartSysRDesc = atStartNP (the cartesian) `S.is` S "right-handed where" +:+
 
 yAxisDirDesc :: Sentence
 yAxisDirDesc = atStartNP (direction `the_ofThe` yAxis) `S.is` S "directed opposite to" +:+. phrase gravity
-
-startOriginDescDouble :: Sentence
-startOriginDescDouble = atStartNP (the firstRod) `S.is` S "attached" `S.toThe` (phrase origin !.)
-
-firstPendDesc :: Sentence
-firstPendDesc = foldlSent[
-  atStartNP (the firstRod) +:+. S "has two sides", 
-  S "One side attaches" `S.toThe` (phrase origin !.), 
-  S "Another side attaches" `S.toThe` phrase firstObject]
-
-secondPendDesc :: Sentence
-secondPendDesc = foldlSent[
-  atStartNP (the secondRod) +:+. S "has two sides", 
-  S "One side attaches" `S.toThe` (phrase firstObject !.),
-  S "Another side attaches" `S.toThe` phrase secondObject]

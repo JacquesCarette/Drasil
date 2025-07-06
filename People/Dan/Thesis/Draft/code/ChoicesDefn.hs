2b99fe0a9152d741d6b9e0eca877583d13f1437e
Daniel Szymczak <szymczdm@mcmaster.ca>
2025-07-06 17:14:36 -0400

    Dan Thesis: Lots of minor updates, nearly finished section 4.3.2

---
+++ People/Dan/Thesis/Draft/code/ChoicesDefn.hs
@@ -0,0 +1,365 @@
+-- | Defines the design language for SCS.
+module Language.Drasil.Choices (
+  Choices(..), Architecture (..), makeArchit, DataInfo(..), makeData, Maps(..), 
+  makeMaps, spaceToCodeType, Constraints(..), makeConstraints, ODE(..), makeODE, 
+  DocConfig(..), makeDocConfig, LogConfig(..), makeLogConfig, OptionalFeatures(..), 
+  makeOptFeats, ExtLib(..), Modularity(..), InputModule(..), inputModule, Structure(..),
+  ConstantStructure(..), ConstantRepr(..), ConceptMatchMap, MatchedConceptMap,
+  CodeConcept(..), matchConcepts, SpaceMatch, matchSpaces, ImplementationType(..),
+  ConstraintBehaviour(..), Comments(..), Verbosity(..), Visibility(..),
+  Logging(..), AuxFile(..), getSampleData, hasSampleInput, defaultChoices,
+  choicesSent, showChs) where
+
+import Language.Drasil hiding (None)
+import Language.Drasil.Code.Code (spaceToCodeType)
+import Language.Drasil.Code.Lang (Lang(..))
+import Language.Drasil.Data.ODEInfo (ODEInfo)
+import Language.Drasil.Data.ODELibPckg (ODELibPckg)
+
+import GOOL.Drasil (CodeType)
+
+import Control.Lens ((^.))
+import Data.Map (Map, fromList)
+
+-- | The instruction indicates how the generated program should be written down.
+-- Full details of Choices documentation https://github.com/JacquesCarette/Drasil/wiki/The-Code-Generator
+data Choices = Choices {
+  -- | Target languages.
+  -- Choosing multiple means program will be generated in multiple languages.
+  lang :: [Lang],
+  -- | Architecture of the program, include modularity and implementation type
+  architecture :: Architecture,
+  -- | Data structure and represent
+  dataInfo :: DataInfo,
+  -- | Maps for 'Drasil concepts' to 'code concepts' or 'Space' to a 'CodeType
+  maps :: Maps,
+  -- | Setting for Softifacts that can be added to the program or left it out
+  optFeats :: OptionalFeatures,
+  -- | Constraint violation behaviour. Exception or Warning.
+  srsConstraints :: Constraints,
+  -- | List of external libraries what to utilize 
+  extLibs :: [ExtLib]
+}
+
+-- | Renders program choices as a 'Sentence'.
+class RenderChoices a where
+    showChs :: a -> Sentence
+    showChsList :: [a] -> Sentence
+    showChsList lst = foldlSent_ (map showChs lst)
+
+-- | Architecture of a program
+data Architecture = Archt {
+  -- | How the program should be modularized.
+  modularity :: Modularity,
+  -- | Implementation type, program or library.
+  impType :: ImplementationType
+}
+-- | Constructor to create a Architecture
+makeArchit :: Modularity -> ImplementationType -> Architecture
+makeArchit = Archt
+
+-- | Modularity of a program.
+data Modularity = Modular InputModule -- ^ Different modules. For controller, 
+                                      -- input, calculations, output.
+                | Unmodular -- ^ All generated code is in one module/file.
+
+-- | Renders the modularity of a program.
+instance RenderChoices Modularity where
+  showChs Unmodular = S "Unmodular"
+  showChs (Modular Combined) = S "Modular Combined"
+  showChs (Modular Separated)= S "Modular Separated"
+
+-- | Options for input modules.
+data InputModule = Combined -- ^ Input-related functions combined in one module.
+                 | Separated -- ^ Input-related functions each in own module.
+
+-- | Determines whether there is a 'Combined' input module or many 'Separated' input 
+-- modules, based on a 'Choices' structure. An 'Unmodular' design implicitly means 
+-- that input modules are 'Combined'.
+inputModule :: Choices -> InputModule
+inputModule c = inputModule' $ modularity $ architecture c
+  where inputModule' Unmodular = Combined
+        inputModule' (Modular im) = im
+
+-- | Program implementation options.
+data ImplementationType = Library -- ^ Generated code does not include Controller.
+                        | Program -- ^ Generated code includes Controller.
+
+-- | Renders options for program implementation.
+instance RenderChoices ImplementationType where
+  showChs Library = S "Library"
+  showChs Program = S "Program"
+
+-- | Data of a program - how information should be encoded.
+data DataInfo = DataInfo {
+  -- | Structure of inputs (bundled or not).
+  inputStructure :: Structure,
+  -- | Structure of constants (inlined or bundled or not, or stored with inputs).
+  constStructure :: ConstantStructure,
+  -- | Representation of constants (as variables or as constants).
+  constRepr :: ConstantRepr
+}
+-- | Constructor to create a DataInfo
+makeData :: Structure -> ConstantStructure -> ConstantRepr -> DataInfo
+makeData = DataInfo
+
+-- | Variable structure options.
+data Structure = Unbundled -- ^ Individual variables
+               | Bundled -- ^ Variables bundled in a class
+
+-- | Renders the structure of variables in a program.
+instance RenderChoices Structure where
+  showChs Unbundled = S "Unbundled"
+  showChs Bundled = S "Bundled"
+
+-- | Constants options.
+data ConstantStructure = Inline -- ^ Inline values for constants.
+                       | WithInputs -- ^ Store constants with inputs.
+                       | Store Structure -- ^ Store constants separately from 
+                                         -- inputs, whether bundled or unbundled.
+
+-- | Renders the structure of constants in a program.
+instance RenderChoices ConstantStructure where
+  showChs Inline = S "Inline"
+  showChs WithInputs = S "WithInputs"
+  showChs (Store Unbundled) = S "Store Unbundled"
+  showChs (Store Bundled) = S "Store Bundled"
+
+-- | Options for representing constants in a program.
+data ConstantRepr = Var -- ^ Constants represented as regular variables.
+                  | Const -- ^ Use target language's mechanism for defining constants.
+
+-- | Renders the representation of constants in a program.
+instance RenderChoices ConstantRepr where
+  showChs Var = S "Var"
+  showChs Const = S "Const"
+
+-- | Maps for Concepts and Space 
+data Maps = Maps {
+  -- | Map of 'UID's for Drasil concepts to code concepts.
+  -- Matching a 'UID' to a code concept means the code concept should be used
+  -- instead of the chunk associated with the 'UID'.
+  conceptMatch :: ConceptMatchMap,
+  -- | Map of 'Space's to 'CodeType's
+  -- Matching a 'Space' to a 'CodeType' means values of the 'Space' should have that
+  -- 'CodeType' in the generated code.
+  spaceMatch :: SpaceMatch
+}
+-- | Constructor to create a Maps
+makeMaps :: ConceptMatchMap -> SpaceMatch -> Maps
+makeMaps = Maps
+
+-- | Specifies matches between chunks and 'CodeConcept's, meaning the target 
+-- language's pre-existing definition of the concept should be used instead of 
+-- defining a new variable for the concept in the generated code. 
+-- ['CodeConcept'] is preferentially-ordered, generator concretizes a 
+-- 'ConceptMatchMap' to a 'MatchedConceptMap' by checking user's other choices.
+type ConceptMatchMap = Map UID [CodeConcept]
+-- | Concrete version of ConceptMatchMap dependent on user choices.
+type MatchedConceptMap = Map UID CodeConcept
+
+-- Currently we only support one code concept, more will be added later
+-- | Code concepts. For now, just pi.
+data CodeConcept = Pi deriving Eq
+
+-- | Renders 'CodeConcept's.
+instance RenderChoices CodeConcept where
+  showChs Pi = S "Pi"
+
+-- | Builds a 'ConceptMatchMap' from an association list of chunks and 'CodeConcepts'.
+matchConcepts :: (HasUID c) => [(c, [CodeConcept])] -> ConceptMatchMap
+matchConcepts = fromList . map (\(cnc,cdc) -> (cnc ^. uid, cdc))
+
+-- | Specifies which 'CodeType' should be used to represent each mathematical 
+-- 'Space'. ['CodeType'] is preferentially-ordered, first 'CodeType' that does not 
+-- conflict with other choices will be selected.
+type SpaceMatch = Space -> [CodeType]
+
+-- | Updates a 'SpaceMatch' by matching the given 'Space' with the given ['CodeType'].
+matchSpace :: Space -> [CodeType] -> SpaceMatch -> SpaceMatch
+matchSpace _ [] _ = error "Must match each Space to at least one CodeType"
+matchSpace s ts sm = \sp -> if sp == s then ts else sm sp
+
+-- | Builds a 'SpaceMatch' from an association list of 'Spaces' and 'CodeTypes'.
+matchSpaces :: [(Space, [CodeType])] -> SpaceMatch
+matchSpaces spMtchs = matchSpaces' spMtchs spaceToCodeType
+  where matchSpaces' ((s,ct):sms) sm = matchSpaces' sms $ matchSpace s ct sm
+        matchSpaces' [] sm = sm
+
+-- Optional Features can be added to the program or left it out
+data OptionalFeatures = OptFeats{
+  docConfig :: DocConfig,
+  logConfig :: LogConfig,
+  -- | Turns generation of different auxiliary (non-source-code) files on or off.
+  auxFiles :: [AuxFile]
+}
+-- | Constructor to create a OptionalFeatures
+makeOptFeats :: DocConfig -> LogConfig -> [AuxFile] -> OptionalFeatures
+makeOptFeats = OptFeats
+
+-- | Configuration for Doxygen documentation 
+data DocConfig = DocConfig {
+  -- | Turns Doxygen comments for different code structures on or off.
+  comments :: [Comments],
+  -- | Standard output from running Doxygen: verbose or quiet?
+  doxVerbosity :: Verbosity,
+  -- | Turns date field on or off in the generated module-level Doxygen comments.
+  dates :: Visibility
+}
+-- | Constructor to create a DocConfig
+makeDocConfig :: [Comments] -> Verbosity -> Visibility -> DocConfig
+makeDocConfig = DocConfig
+
+-- | Comment implementation options.
+data Comments = CommentFunc -- ^ Function/method-level comments.
+              | CommentClass -- ^ Class-level comments.
+              | CommentMod -- ^ File/Module-level comments.
+              deriving Eq
+
+-- | Renders options for implementation of comments.
+instance RenderChoices Comments where
+  showChs CommentFunc = S "CommentFunc"
+  showChs CommentClass = S "CommentClass"
+  showChs CommentMod = S "CommentMod"
+
+-- | Doxygen file verbosity options.
+data Verbosity = Verbose | Quiet
+
+-- | Renders options for doxygen verbosity.
+instance RenderChoices Verbosity where
+  showChs Verbose = S "Verbose"
+  showChs Quiet = S "Quiet"
+
+-- | Doxygen date-field visibility options.
+data Visibility = Show
+                | Hide
+
+-- | Renders options for doxygen date-field visibility.
+instance RenderChoices Visibility where
+  showChs Show = S "Show"
+  showChs Hide = S "Hide"
+
+-- | Log Configuration
+data LogConfig = LogConfig {
+  -- | Turns different forms of logging on or off.
+  logging :: [Logging],
+  -- | Name of log file.
+  logFile :: FilePath
+}
+-- | Constructor to create a LogConfig
+makeLogConfig :: [Logging] -> FilePath -> LogConfig
+makeLogConfig = LogConfig
+
+-- | Logging options for function calls and variable assignments.
+-- Eq instances required for Logging and Comments because generator needs to 
+-- check membership of these elements in lists
+data Logging = LogFunc -- ^ Log messages generated for function calls.
+             | LogVar -- ^ Log messages generated for variable assignments.
+             deriving Eq
+
+-- | Renders options for program logging.
+instance RenderChoices Logging where
+  showChs LogFunc = S "LogFunc"
+  showChs LogVar = S "LogVar"
+
+-- | Currently we only support two kind of auxiliary files: sample input file, readme.
+-- To generate a sample input file compatible with the generated program,
+-- 'FilePath' is the path to the user-provided file containing a sample set of input data.
+data AuxFile = SampleInput FilePath
+             | ReadME
+             deriving Eq
+
+-- | Renders options for auxiliary file generation.
+instance RenderChoices AuxFile where
+  showChs (SampleInput fp) = S "SampleInput" +:+ S fp
+  showChs ReadME = S "ReadME"
+
+-- | Gets the file path to a sample input data set from a 'Choices' structure, if 
+-- the user chose to generate a sample input file.
+getSampleData :: Choices -> Maybe FilePath
+getSampleData chs = getSampleData' (auxFiles $ optFeats chs)
+  where getSampleData' [] = Nothing
+        getSampleData' (SampleInput fp:_) = Just fp
+        getSampleData' (_:xs) = getSampleData' xs
+
+-- | Predicate that returns true if the list of 'AuxFile's includes a 'SampleInput'.
+hasSampleInput :: [AuxFile] -> Bool
+hasSampleInput [] = False
+hasSampleInput (SampleInput _:_) = True
+hasSampleInput (_:xs) = hasSampleInput xs
+
+-- | SRS Constraints
+data Constraints = Constraints{
+  onSfwrConstraint :: ConstraintBehaviour,
+  onPhysConstraint :: ConstraintBehaviour
+}
+-- | Constructor to create a Constraints
+makeConstraints :: ConstraintBehaviour -> ConstraintBehaviour -> Constraints
+makeConstraints = Constraints
+
+-- | Constraint behaviour options within program.
+data ConstraintBehaviour = Warning -- ^ Print warning when constraint violated.
+                         | Exception -- ^ Throw exception when constraint violated.
+
+-- | Renders options for program implementation.
+instance RenderChoices ConstraintBehaviour where
+  showChs Warning = S "Warning"
+  showChs Exception = S "Exception"
+
+-- | External Library Options
+newtype ExtLib = Math ODE
+
+-- | All Information needed to solve an ODE 
+data ODE = ODE{
+  -- FIXME: ODEInfos should be automatically built from Instance models when 
+  -- needed, but we can't do that yet so I'm passing it through Choices instead.
+  -- This choice should really just be for an ODEMethod
+  -- | ODE information.
+  odeInfo :: [ODEInfo],
+  -- | Preferentially-ordered list ODE libraries to try.
+  odeLib :: [ODELibPckg]
+}
+-- | Constructor to create an ODE
+makeODE :: [ODEInfo] -> [ODELibPckg] -> ODE
+makeODE = ODE
+
+-- | Default choices to be used as the base from which design specifications 
+-- can be built.
+defaultChoices :: Choices
+defaultChoices = Choices {
+  lang = [Python],
+  architecture = makeArchit (Modular Combined) Program,
+  dataInfo = makeData Bundled Inline Const,
+  maps = makeMaps 
+    (matchConcepts ([] :: [(SimpleQDef, [CodeConcept])])) 
+    spaceToCodeType,
+  optFeats = makeOptFeats 
+    (makeDocConfig [] Verbose Hide) 
+    (makeLogConfig [] "log.txt") 
+    [ReadME],
+  srsConstraints = makeConstraints Exception Warning,
+  extLibs = []
+}
+
+-- | Renders 'Choices' as 'Sentence's.
+choicesSent :: Choices -> [Sentence]
+choicesSent chs = map chsFieldSent [
+    (S "Languages",                     foldlSent_ $ map (S . show) $ lang chs), 
+    (S "Modularity",                    showChs $ modularity $ architecture chs), 
+    (S "Input Structure",               showChs $ inputStructure $ dataInfo chs), 
+    (S "Constant Structure",            showChs $ constStructure $ dataInfo chs), 
+    (S "Constant Representation",       showChs $ constRepr $ dataInfo chs), 
+    (S "Implementation Type",           showChs $ impType $ architecture chs), 
+    (S "Software Constraint Behaviour", showChs $ onSfwrConstraint $ srsConstraints chs), 
+    (S "Physical Constraint Behaviour", showChs $ onPhysConstraint $ srsConstraints chs), 
+    (S "Comments",                      showChsList $ comments $ docConfig $ optFeats chs), 
+    (S "Dox Verbosity",                 showChs $ doxVerbosity $ docConfig $ optFeats chs), 
+    (S "Dates",                         showChs $ dates $ docConfig $ optFeats chs), 
+    (S "Log File Name",                 S $ logFile $ logConfig $ optFeats chs), 
+    (S "Logging",                       showChsList $ logging $ logConfig $ optFeats chs), 
+    (S "Auxiliary Files",               showChsList $ auxFiles $ optFeats chs)
+  ]
+
+-- | Helper to combine pairs of 'Sentence's for rendering 'Choices'.
+chsFieldSent :: (Sentence, Sentence) -> Sentence
+chsFieldSent (rec, chc) = rec +:+ S "selected as" +:+. chc
---
+++ People/Dan/Thesis/Draft/code/ModuleDefs.hs
@@ -0,0 +1,224 @@
+-- Convention used below:
+-- when 'name' and 'nameCT' both appear, 'name' is the Haskell function and
+-- 'nameCT' is the "Code Template" that 'name' builds.
+
+module Drasil.GlassBR.ModuleDefs (allMods, implVars, interpY, interpZ) where
+
+import Language.Drasil (QuantityDict, Space(..), implVar, nounPhraseSP,
+  label, sub, HasSymbol(..), HasUID, Symbol)
+import Language.Drasil.Display (Symbol(..))
+import Language.Drasil.ShortHands
+import Language.Drasil.Code (($:=), Func, FuncStmt(..), Mod, 
+  asVC, funcDef, fDecDef, ffor, funcData, quantvar, 
+  multiLine, packmod, repeated, singleLine)
+import Language.Drasil.CodeExpr
+import qualified Drasil.GlassBR.Unitals as U
+import Language.Drasil.Printers
+
+
+allMods :: [Mod]
+allMods = [readTableMod, interpMod]
+
+-- It's a bit odd that this has to be explicitly built here...
+implVars :: [QuantityDict]
+implVars = [v, x_z_1, y_z_1, x_z_2, y_z_2, mat, col,
+  i, j, k, z, zVector, yMatrix, xMatrix, y, arr, filename,
+  y_2, y_1, x_2, x_1, x]
+
+--from TSD.txt:
+
+readTableMod :: Mod
+readTableMod = packmod "ReadTable"
+  "Provides a function for reading glass ASTM data" [] [readTable]
+
+readTable :: Func
+readTable = funcData "read_table"
+  "Reads glass ASTM data from a file with the given file name"
+  [ singleLine (repeated [quantvar zVector]) ',',
+    multiLine (repeated (map quantvar [xMatrix, yMatrix])) ','
+  ]
+
+-----
+
+one, two :: Symbol
+one = Integ 1
+two = Integ 2
+
+var :: String -> String -> Symbol -> Space -> QuantityDict
+var nam np symb sp = implVar nam (nounPhraseSP np) sp symb
+
+y_2, y_1, x_2, x_1, x :: QuantityDict
+y_1  = var "y1" "lower y-coordinate"             (sub lY one) Real
+y_2  = var "y2" "upper y-coordinate"             (sub lY two) Real
+x_1  = var "x1" "lower x-coordinate"             (sub lX one) Real
+x_2  = var "x2" "upper x-coordinate"             (sub lX two) Real
+x    = var "x"  "x-coordinate to interpolate at" lX           Real -- = params.wtnt from mainFun.py
+
+v, x_z_1, y_z_1, x_z_2, y_z_2, mat, col,
+  i, j, k, z, zVector, yMatrix, xMatrix, y, arr, filename :: QuantityDict
+i = var "i" "index" lI           Natural
+j = var "j" "index" lJ           Natural
+k = var "k" "index" (sub lK two) Natural     
+v = var "v" "value whose index will be found" lV Real
+y = var "y" "y-coordinate to interpolate at"  lY Real
+z = var "z" "z-coordinate to interpolate at"  lZ Real
+
+zVector = var "zVector" "list of z values" 
+  (sub lZ (label "vector")) (Vect Real)               
+yMatrix = var "yMatrix" "lists of y values at different z values" 
+  (sub lY (label "matrix")) (Vect $ Vect Real)        
+xMatrix = var "xMatrix" "lists of x values at different z values" 
+  (sub lX (label "matrix")) (Vect $ Vect Real)        
+arr     = var "arr"     "array in which value should be found" 
+  (label "arr")             (Vect Real)  --FIXME: temporary variable for findCT?
+x_z_1   = var "x_z_1"   "list of x values at a specific z value"    
+  (sub lX (sub lZ one))      (Vect Real)
+y_z_1   = var "y_z_1"   "list of y values at a specific z value"    
+  (sub lY (sub lZ one))      (Vect Real)   
+x_z_2   = var "x_z_2"   "list of x values at a specific z value"    
+  (sub lX (sub lZ two))      (Vect Real)
+y_z_2   = var "y_z_2"   "list of y values at a specific z value"   
+  (sub lY (sub lZ two))      (Vect Real)
+mat     = var "mat"     "matrix from which column will be extracted"     
+  (label "mat")             (Vect $ Vect Real)
+col     = var "col"     "extracted column"    
+  (label "col")             (Vect Real)               
+filename = var "filename" "name of file with x y and z data" 
+  (label "filename")        String
+
+------------------------------------------------------------------------------------------
+--
+-- Some semantic functions
+
+-- Given two points (x1,y1) and (x2,y2), return the slope of the line going through them
+slope :: (CodeExpr, CodeExpr) -> (CodeExpr, CodeExpr) -> CodeExpr
+slope (x1, y1) (x2, y2) = (y2 $- y1) $/ (x2 $- x1)
+
+-- Given two points (x1,y1) and (x2,y2), and an x ordinate, return
+-- extrapoled y on the straight line in between
+onLine :: (CodeExpr, CodeExpr) -> (CodeExpr, CodeExpr) -> CodeExpr -> CodeExpr
+onLine p1@(x1, y1) p2 x_ = (m `mulRe` (x_ $- x1)) `addRe` y1
+                 where m = slope p1 p2
+
+------------------------------------------------------------------------------------------
+-- Code Template helper functions
+
+vLook :: (HasSymbol a, HasSymbol i, HasUID a, HasUID i) => a -> i -> CodeExpr -> CodeExpr
+vLook a i_ p = idx (sy a) (sy i_ `addI` p)
+
+aLook :: (HasSymbol a, HasSymbol i, HasSymbol j, HasUID a, HasUID i, HasUID j) =>
+  a -> i -> j -> CodeExpr
+aLook a i_ j_ = idx (idx (sy a) (sy i_)) (sy j_)
+
+getCol :: (HasSymbol a, HasSymbol i, HasUID a, HasUID i) => a -> i -> CodeExpr -> CodeExpr
+getCol a_ i_ p = apply (asVC extractColumnCT) [sy a_, sy i_ `addI` p]
+
+call :: Func -> [QuantityDict] -> FuncStmt
+call f l = FVal $ apply (asVC f) $ map sy l
+
+find :: (HasUID zv, HasUID z, HasSymbol zv, HasSymbol z) => zv -> z -> CodeExpr
+find zv z_ = apply (asVC findCT) [sy zv, sy z_]
+
+linInterp :: [CodeExpr] -> CodeExpr
+linInterp = apply (asVC linInterpCT)
+
+interpOver :: (HasUID ptx, HasUID pty, HasUID ind, HasUID vv,
+  HasSymbol ptx, HasSymbol pty, HasSymbol ind, HasSymbol vv) =>
+  ptx -> pty -> ind -> vv -> [CodeExpr]
+interpOver ptx pty ind vv =
+  [ vLook ptx ind (int 0), vLook pty ind (int 0)
+  , vLook ptx ind (int 1), vLook pty ind (int 1)
+  , sy vv ]
+------------------------------------------------------------------------------------------
+-- Code Templates
+
+-- Note how this one uses a semantic function in its body
+-- But it is also 'wrong' in the sense that it assumes x_1 <= x <= x_2
+linInterpCT :: Func
+linInterpCT = funcDef "lin_interp" "Performs linear interpolation" 
+  [x_1, y_1, x_2, y_2, x] Real (Just "y value interpolated at given x value")
+  [ FRet $ onLine (sy x_1, sy y_1) (sy x_2, sy y_2) (sy x) ]
+
+findCT :: Func
+findCT = funcDef "find" 
+  "Finds the array index for a value closest to the given value" 
+  [arr, v] Natural (Just "index of given value in given array")
+  [
+    ffor i (dim (sy arr) $- int 1)
+      [ FCond ((vLook arr i (int 0) $<= sy v) $&& (sy v $<= vLook arr i (int 1)))
+        [ FRet $ sy i ] [] ],
+    FThrow "Bound error"
+  ]
+
+extractColumnCT :: Func
+extractColumnCT = funcDef "extractColumn" "Extracts a column from a 2D matrix" 
+  [mat, j] (Vect Real) (Just "column of the given matrix at the given index")
+  [
+    fDecDef col (matrix [[]]),
+    --
+    ffor i (dim (sy mat))
+      [ FAppend (sy col) (aLook mat i j) ],
+    FRet (sy col)
+  ]
+
+interpY :: Func
+interpY = funcDef (showHasSymbImpl U.interpY)
+  "Linearly interpolates a y value at given x and z values" 
+  [filename, x, z] Real (Just "y value interpolated at given x and z values")
+  [
+  -- hack
+  fDecDef xMatrix (matrix [[]]),
+  fDecDef yMatrix (matrix [[]]),
+  fDecDef zVector (matrix [[]]),
+  --
+  call readTable [filename, zVector, xMatrix, yMatrix],
+  -- endhack
+    i     $:= find zVector z,
+    x_z_1 $:= getCol xMatrix i (int 0),
+    y_z_1 $:= getCol yMatrix i (int 0),
+    x_z_2 $:= getCol xMatrix i (int 1),
+    y_z_2 $:= getCol yMatrix i (int 1),
+    FTry
+      [ j $:= find x_z_1 x,
+        k $:= find x_z_2 x ]
+      [ FThrow "Interpolation of y failed" ],
+    y_1 $:= linInterp (interpOver x_z_1 y_z_1 j x),
+    y_2 $:= linInterp (interpOver x_z_2 y_z_2 k x),
+    FRet $ linInterp [ vLook zVector i (int 0), sy y_1, vLook zVector i (int 1), sy y_2, sy z ]
+  ]
+
+interpZ :: Func
+interpZ = funcDef (showHasSymbImpl U.interpZ)
+  "Linearly interpolates a z value at given x and y values" 
+  [filename, x, y] Real (Just "z value interpolated at given x and y values")
+  [
+    -- hack
+  fDecDef xMatrix (matrix [[]]),
+  fDecDef yMatrix (matrix [[]]),
+  fDecDef zVector (matrix [[]]),
+  --
+  call readTable [filename, zVector, xMatrix, yMatrix],
+  -- endhack
+    ffor i (dim (sy zVector) $- int 1)
+      [
+        x_z_1 $:= getCol xMatrix i (int 0),
+        y_z_1 $:= getCol yMatrix i (int 0),
+        x_z_2 $:= getCol xMatrix i (int 1),
+        y_z_2 $:= getCol yMatrix i (int 1),
+        FTry
+          [ j $:= find x_z_1 x,
+            k $:= find x_z_2 x ]
+          [ FContinue ],
+        y_1 $:= linInterp (interpOver x_z_1 y_z_1 j x),
+        y_2 $:= linInterp (interpOver x_z_2 y_z_2 k x),
+        FCond ((sy y_1 $<= sy y) $&& (sy y $<= sy y_2))
+          [ FRet $ linInterp [ sy y_1, vLook zVector i (int 0), sy y_2, vLook zVector i (int 1), sy y ]
+          ] []
+      ],
+    FThrow "Interpolation of z failed"
+  ]
+
+interpMod :: Mod
+interpMod = packmod "Interpolation" 
+  "Provides functions for linear interpolation on three-dimensional data" []
+  [linInterpCT, findCT, extractColumnCT, interpY, interpZ]

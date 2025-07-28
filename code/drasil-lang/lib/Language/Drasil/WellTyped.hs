35b54ca6be1bd460a8b04bfc04a6d322986b248c
Jason Balaci <jason@balaci.me>
2025-07-28 19:59:07 -0400

    TypeChecking: Practice conventional `Either` for 'Error's

--- code/drasil-lang/lib/Language/Drasil/WellTyped.hs
+++ code/drasil-lang/lib/Language/Drasil/WellTyped.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Language.Drasil.WellTyped where
 
@@ -19,23 +20,23 @@ type TypeError = String
 type TypingContext t = M.Map UID t
 
 -- | Look for a known type of a specific 'UID'.
-inferFromContext :: TypingContext t -> UID -> Either t TypeError
-inferFromContext cxt u = maybe
-    (Right $ "`" ++ show u ++ "` lacks type binding in context")
-    Left
-    (M.lookup u cxt)
+inferFromContext :: TypingContext t -> UID -> Either TypeError t
+inferFromContext cxt u =
+  case M.lookup u cxt of
+    Just t  -> pure t
+    Nothing -> Left $ "`" ++ show u ++ "` lacks type binding in context"
 
 -- | Build a bidirectional type checker for your expression language, e, with
 --   respect to a specific type universe, t.
 class (Eq t, Show t) => Typed e t where
-  
+
   -- | Given a typing context and an expression, infer a unique type or explain
   --   what went awry.
-  infer :: TypingContext t -> e -> Either t TypeError
+  infer :: TypingContext t -> e -> Either TypeError t
 
   -- | Given a typing context, an expression, and an expected type, check if the
   --   expression can satisfy the expectation.
-  check :: TypingContext t -> e -> t -> Either t TypeError
+  check :: TypingContext t -> e -> t -> Either TypeError t
 
 -- | For all containers, c, which contain typed expressions, e, against a
 --   specific type universe, t, expose all expressions and relations that need
@@ -44,28 +45,43 @@ class Typed e t => RequiresChecking c e t where
   -- | All things that need type checking.
   requiredChecks :: c -> [(e, t)]
 
+assertEq :: Eq t => t -> t -> TypeError -> Either TypeError t
+assertEq lt rt te
+  | lt == rt  = pure lt
+  | otherwise = Left te
+
+infix 4 ~==
+(~==) :: Eq t => t -> t -> TypeError -> Either TypeError t
+(~==) = assertEq
+
+infix 0 ~?!
+(~?!) :: forall t. (TypeError -> Either TypeError t) -> TypeError -> Either TypeError t
+(~?!) = ($)
+
 -- | ``Check'' an expressions type based by an inference.
-typeCheckByInfer :: Typed e t => TypingContext t -> e -> t -> Either t TypeError
-typeCheckByInfer cxt e t = either
-    (\infT -> if t == infT
-      then Left t
-      else Right $ "Inferred type `" ++ show t ++ "` does not match expected type `" ++ show infT ++ "`")
-    Right
-    (infer cxt e)
+typeCheckByInfer :: Typed e t => TypingContext t -> e -> t -> Either TypeError t
+typeCheckByInfer cxt e t = do
+  et <- infer cxt e
+  et ~== t 
+    ~?! "Inferred type `" ++ show et ++ "` does not match expected type `" ++ show t ++ "`"
 
 {- FIXME: temporary hacks below (they're aware of the "printing" code!), pending
           replacement when TypeError is upgraded -}
 
-allOfType :: Typed e t => TypingContext t -> [e] -> t -> t -> TypeError -> Either t TypeError
-allOfType cxt es expect ret s
-  | allTsAreSp = Left ret
-  | otherwise  = Right $ temporaryIndent "  " (s ++ "\nReceived:\n" ++ dumpAllTs)
+assertAllEq :: Typed e t => TypingContext t -> [e] -> t -> t -> TypeError -> Either TypeError t
+assertAllEq cxt es expect ret s
+  | allTsAreSp = pure ret
+  | otherwise  = Left $ temporaryIndent "  " (s ++ "\nReceived:\n" ++ dumpAllTs)
   where
     allTs = map (infer cxt) es
     allTsAreSp = all (\case
-      Left  t -> t == expect
-      Right _ -> False) allTs
-    dumpAllTs = intercalate "\n" $ map (("- " ++) . either show ("ERROR: " ++)) allTs
+      Right  t -> t == expect
+      Left _   -> False) allTs
+    dumpAllTs = intercalate "\n" $ map (("- " ++) . either ("ERROR: " ++) show) allTs
+
+allOfType :: Typed e t => TypingContext t -> [e] -> t -> t -> TypeError -> Either TypeError t
+allOfType = assertAllEq
+{-# DEPRECATED allOfType "Use assertAllEq instead." #-}
 
 -- | A temporary, hacky, indentation function. It should be removed when we
 -- switch to using something else for error messages, which can be later

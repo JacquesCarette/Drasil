c9f2dec99843129f35b6b050239d4f1ad7ded1fc
Daniel Szymczak <szymczdm@mcmaster.ca>
2025-08-04 22:02:14 -0400

    Dan Thesis: Rewrite generation pipeline section -- had too much redundancy + minor edits

---
+++ People/Dan/Thesis/Draft/code/Generate.hs
@@ -0,0 +1,132 @@
+-- | Defines Drasil generator functions.
+module Language.Drasil.Generate (
+  -- * Generator Functions
+  gen, genDot, genCode, genLog,
+  -- * Types (Printing Options)
+  DocType(..), DocSpec(DocSpec), Format(TeX, HTML), DocChoices(DC),
+  -- * Constructor
+  docChoices) where
+
+import System.IO (hClose, hPutStrLn, openFile, IOMode(WriteMode))
+import Text.PrettyPrint.HughesPJ (Doc, render)
+import Prelude hiding (id)
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory,
+  setCurrentDirectory)
+import Data.Time.Clock (getCurrentTime, utctDay)
+import Data.Time.Calendar (showGregorian)
+
+import Build.Drasil (genMake)
+import Language.Drasil
+import Drasil.DocLang (mkGraphInfo)
+import SysInfo.Drasil (SystemInformation(SI, _sys))
+import Language.Drasil.Printers (Format(TeX, HTML, JSON), 
+ makeCSS, genHTML, genTeX, genJSON, PrintingInformation, outputDot, printAllDebugInfo)
+import Language.Drasil.Code (generator, generateCode, Choices(..), CodeSpec(..),
+  Lang(..), getSampleData, readWithDataDesc, sampleInputDD, 
+  unPP, unJP, unCSP, unCPPP, unSP)
+import Language.Drasil.Output.Formats(DocType(SRS, Website, Jupyter), Filename, DocSpec(DocSpec), DocChoices(DC))
+
+import GOOL.Drasil (unJC, unPC, unCSC, unCPPC, unSC)
+import Data.Char (isSpace)
+
+-- | Generate a number of artifacts based on a list of recipes.
+gen :: DocSpec -> Document -> PrintingInformation -> IO ()
+gen ds fn sm = prnt sm ds fn -- FIXME: 'prnt' is just 'gen' with the arguments reordered
+
+-- TODO: Include Jupyter into the SRS setup.
+-- | Generate the output artifacts (TeX+Makefile or HTML).
+prnt :: PrintingInformation -> DocSpec -> Document -> IO ()
+prnt sm (DocSpec (DC Jupyter _) fn) body =
+  do prntDoc body sm fn Jupyter JSON
+prnt sm (DocSpec (DC dtype fmts) fn) body =
+  do mapM_ (prntDoc body sm fn dtype) fmts
+
+-- | Helper for writing the documents (TeX / HTML) to file.
+prntDoc :: Document -> PrintingInformation -> String -> DocType -> Format -> IO ()
+prntDoc d pinfo fn Jupyter _ = prntDoc' "Jupyter" fn JSON d pinfo
+prntDoc d pinfo fn dtype fmt =
+  case fmt of
+    HTML -> do prntDoc' (show dtype ++ "/HTML") fn HTML d pinfo
+               prntCSS dtype fn d
+    TeX -> do prntDoc' (show dtype ++ "/PDF") fn TeX d pinfo
+              prntMake $ DocSpec (DC dtype []) fn
+    _ -> mempty
+
+-- | Helper that takes the directory name, document name, format of documents,
+-- document information and printing information. Then generates the document file.
+prntDoc' :: String -> String -> Format -> Document -> PrintingInformation -> IO ()
+prntDoc' dt' fn format body' sm = do
+  createDirectoryIfMissing True dt'
+  outh <- openFile (dt' ++ "/" ++ fn ++ getExt format) WriteMode
+  hPutStrLn outh $ render $ writeDoc sm format fn body'
+  hClose outh
+  where getExt TeX  = ".tex"
+        getExt HTML = ".html"
+        getExt JSON = ".ipynb"
+        getExt _    = error "We can only write in TeX, HTML and in Python Notebooks (for now)."
+
+-- | Helper for writing the Makefile(s).
+prntMake :: DocSpec -> IO ()
+prntMake ds@(DocSpec (DC dt _) _) =
+  do outh <- openFile (show dt ++ "/PDF/Makefile") WriteMode
+     hPutStrLn outh $ render $ genMake [ds]
+     hClose outh
+
+-- | Helper that creates a CSS file to accompany an HTML file.
+-- Takes in the folder name, generated file name, and the document.
+prntCSS :: DocType -> String -> Document -> IO ()
+prntCSS docType fn body = do
+  outh2 <- openFile (getFD docType ++ fn ++ ".css") WriteMode
+  hPutStrLn outh2 $ render (makeCSS body)
+  hClose outh2
+  where
+    getFD dtype = show dtype ++ "/HTML/"
+
+-- | Renders the documents.
+writeDoc :: PrintingInformation -> Format -> Filename -> Document -> Doc
+writeDoc s TeX  _  doc = genTeX doc s
+writeDoc s HTML fn doc = genHTML s fn doc
+writeDoc s JSON _ doc  = genJSON s doc
+writeDoc _    _  _   _ = error "we can only write TeX/HTML (for now)"
+
+-- | Generates traceability graphs as .dot files.
+genDot :: SystemInformation -> IO ()
+genDot si = do
+    workingDir <- getCurrentDirectory
+    let gi = mkGraphInfo si
+    outputDot "TraceyGraph" gi
+    setCurrentDirectory workingDir
+
+-- | Generates debugging logs to show all of the 'UID's used in an example.
+genLog :: SystemInformation -> PrintingInformation -> IO ()
+genLog SI{_sys = sysName} pinfo = do
+  workingDir <- getCurrentDirectory
+  createDirectoryIfMissing True $ "../../debug/" ++ filter (not.isSpace) (abrv sysName) ++ "/SRSlogs"
+  setCurrentDirectory $ "../../debug/" ++ filter (not.isSpace) (abrv sysName) ++ "/SRSlogs"
+  handle <- openFile (filter (not.isSpace) (abrv sysName) ++ "_SRS.log") WriteMode
+  mapM_ (hPutStrLn handle . render) $ printAllDebugInfo pinfo
+  hClose handle
+  setCurrentDirectory workingDir
+
+-- | Calls the code generator.
+genCode :: Choices -> CodeSpec -> IO ()
+genCode chs spec = do 
+  workingDir <- getCurrentDirectory
+  time <- getCurrentTime
+  sampData <- maybe (return []) (\sd -> readWithDataDesc sd $ sampleInputDD 
+    (extInputs spec)) (getSampleData chs)
+  createDirectoryIfMissing False "src"
+  setCurrentDirectory "src"
+  let genLangCode Java = genCall Java unJC unJP
+      genLangCode Python = genCall Python unPC unPP
+      genLangCode CSharp = genCall CSharp unCSC unCSP
+      genLangCode Cpp = genCall Cpp unCPPC unCPPP
+      genLangCode Swift = genCall Swift unSC unSP
+      genCall lng unProgRepr unPackRepr = generateCode lng unProgRepr 
+        unPackRepr $ generator lng (showGregorian $ utctDay time) sampData chs spec
+  mapM_ genLangCode (lang chs)
+  setCurrentDirectory workingDir
+
+-- | Constructor for users to choose their document options
+docChoices :: DocType -> [Format] -> DocChoices
+docChoices = DC
---
+++ People/Dan/Thesis/Draft/code/ImperativeGenerator.hs
@@ -0,0 +1,197 @@
+-- | Defines generation functions for SCS code packages.
+module Language.Drasil.Code.Imperative.Generator (
+  generator, generateCode
+) where
+
+import Language.Drasil
+import Language.Drasil.Code.Imperative.ConceptMatch (chooseConcept)
+import Language.Drasil.Code.Imperative.Descriptions (unmodularDesc)
+import Language.Drasil.Code.Imperative.SpaceMatch (chooseSpace)
+import Language.Drasil.Code.Imperative.GenerateGOOL (ClassType(..),
+  genDoxConfig, genReadMe, genModuleWithImports)
+import Language.Drasil.Code.Imperative.GenODE (chooseODELib)
+import Language.Drasil.Code.Imperative.Helpers (liftS)
+import Language.Drasil.Code.Imperative.Import (genModDef, genModFuncs,
+  genModClasses)
+import Language.Drasil.Code.Imperative.Modules (chooseInModule, genConstClass,
+  genConstMod, genInputClass, genInputConstraints, genInputDerived,
+  genInputFormat, genMain, genMainFunc, genCalcMod, genCalcFunc,
+  genOutputFormat, genOutputMod, genSampleInput)
+import Language.Drasil.Code.Imperative.DrasilState (GenState, DrasilState(..),
+  designLog, inMod, modExportMap, clsDefMap)
+import Language.Drasil.Code.Imperative.GOOL.ClassInterface (ReadMeInfo(..),
+  PackageSym(..), AuxiliarySym(..))
+import Language.Drasil.Code.Imperative.GOOL.Data (PackData(..), ad)
+import Language.Drasil.Code.CodeGeneration (createCodeFiles, makeCode)
+import Language.Drasil.Code.ExtLibImport (auxMods, imports, modExports)
+import Language.Drasil.Code.Lang (Lang(..))
+import Language.Drasil.Choices (Choices(..), Modularity(..), Architecture(..),
+  Visibility(..), DataInfo(..), Constraints(..), choicesSent, DocConfig(..),
+  LogConfig(..), OptionalFeatures(..))
+import Language.Drasil.CodeSpec (CodeSpec(..), getODE)
+import Language.Drasil.Printers (Linearity(Linear), sentenceDoc)
+
+import GOOL.Drasil (GSProgram, SFile, OOProg, ProgramSym(..), ScopeTag(..),
+  ProgData(..), initialState, unCI)
+
+import System.Directory (setCurrentDirectory, createDirectoryIfMissing,
+  getCurrentDirectory)
+import Control.Lens ((^.))
+import Control.Monad.State (get, evalState, runState)
+import Data.List (nub)
+import Data.Map (fromList, member, keys, elems)
+import Data.Maybe (maybeToList, catMaybes)
+import Text.PrettyPrint.HughesPJ (isEmpty, vcat)
+
+-- | Initializes the generator's 'DrasilState'.
+-- 'String' parameter is a string representing the date.
+-- \['Expr'\] parameter is the sample input values provided by the user.
+generator :: Lang -> String -> [Expr] -> Choices -> CodeSpec -> DrasilState
+generator l dt sd chs spec = DrasilState {
+  -- constants
+  codeSpec = spec,
+  modular = modularity $ architecture chs,
+  inStruct = inputStructure $ dataInfo chs,
+  conStruct = constStructure $ dataInfo chs,
+  conRepr = constRepr $ dataInfo chs,
+  concMatches = mcm,
+  spaceMatches = chooseSpace l chs,
+  implType = impType $ architecture chs,
+  onSfwrC = onSfwrConstraint $ srsConstraints chs,
+  onPhysC = onPhysConstraint $ srsConstraints chs,
+  commented = comments $ docConfig $ optFeats chs,
+  doxOutput = doxVerbosity $ docConfig $ optFeats chs,
+  date = showDate $ dates $ docConfig $ optFeats chs,
+  logKind  = logging $ logConfig $ optFeats chs,
+  logName = logFile $ logConfig $ optFeats chs,
+  auxiliaries = auxFiles $ optFeats chs,
+  sampleData = sd,
+  modules = modules',
+  extLibNames = nms,
+  extLibMap = fromList elmap,
+  libPaths = maybeToList pth,
+  eMap = mem,
+  libEMap = lem,
+  clsMap = cdm,
+  defList = nub $ keys mem ++ keys cdm,
+
+  -- stateful
+  currentModule = "",
+  currentClass = "",
+  _designLog = des,
+  _loggedSpaces = [] -- Used to prevent duplicate logs added to design log
+}
+  where (mcm, concLog) = runState (chooseConcept chs) []
+        showDate Show = dt
+        showDate Hide = ""
+        ((pth, elmap, lname), libLog) = runState (chooseODELib l $ getODE $ extLibs chs) []
+        els = map snd elmap
+        nms = [lname]
+        mem = modExportMap spec chs modules'
+        lem = fromList (concatMap (^. modExports) els)
+        cdm = clsDefMap spec chs modules'
+        modules' = mods spec ++ concatMap (^. auxMods) els
+        nonPrefChs = choicesSent chs
+        des = vcat . map (sentenceDoc (sysinfodb spec) Implementation Linear) $
+          (nonPrefChs ++ concLog ++ libLog)
+
+-- | Generates a package with the given 'DrasilState'. The passed
+-- un-representation functions determine which target language the package will 
+-- be generated in.
+generateCode :: (OOProg progRepr, PackageSym packRepr) => Lang ->
+  (progRepr (Program progRepr) -> ProgData) -> (packRepr (Package packRepr) ->
+  PackData) -> DrasilState -> IO ()
+generateCode l unReprProg unReprPack g = do
+  workingDir <- getCurrentDirectory
+  createDirectoryIfMissing False (getDir l)
+  setCurrentDirectory (getDir l)
+  let (pckg, ds) = runState (genPackage unReprProg) g
+      code = makeCode (progMods $ packProg $ unReprPack pckg)
+        ([ad "designLog.txt" (ds ^. designLog) | not $ isEmpty $
+          ds ^. designLog] ++ packAux (unReprPack pckg))
+  createCodeFiles code
+  setCurrentDirectory workingDir
+
+-- | Generates a package, including a Makefile, sample input file, and Doxygen 
+-- configuration file (all subject to the user's choices). 
+-- The passed un-representation function determines which target language the 
+-- package will be generated in.
+-- GOOL's static code analysis interpreter is called to initialize the state 
+-- used by the language renderer.
+genPackage :: (OOProg progRepr, PackageSym packRepr) =>
+  (progRepr (Program progRepr) -> ProgData) ->
+  GenState (packRepr (Package packRepr))
+genPackage unRepr = do
+  g <- get
+  ci <- genProgram
+  p <- genProgram
+  let info = unCI $ evalState ci initialState
+      (reprPD, s) = runState p info
+      pd = unRepr reprPD
+      m = makefile (libPaths g) (implType g) (commented g) s pd
+      as = case codeSpec g of CodeSpec {authors = a} -> map name a
+      cfp = configFiles $ codeSpec g
+  i <- genSampleInput
+  d <- genDoxConfig s
+  rm <- genReadMe ReadMeInfo {
+        langName = "",
+        langVersion = "",
+        invalidOS = Nothing,
+        implementType = implType g,
+        extLibNV = extLibNames g,
+        extLibFP = libPaths g,
+        contributors = as,
+        configFP = cfp,
+        caseName = ""}
+  return $ package pd (m:catMaybes [i,rm,d])
+
+-- | Generates an SCS program based on the problem and the user's design choices.
+genProgram :: (OOProg r) => GenState (GSProgram r)
+genProgram = do
+  g <- get
+  ms <- chooseModules $ modular g
+  let n = pName $ codeSpec g
+  return $ prog n ms
+
+-- | Generates either a single module or many modules, based on the users choice 
+-- of modularity.
+chooseModules :: (OOProg r) => Modularity -> GenState [SFile r]
+chooseModules Unmodular = liftS genUnmodular
+chooseModules (Modular _) = genModules
+
+-- | Generates an entire SCS program as a single module.
+genUnmodular :: (OOProg r) => GenState (SFile r)
+genUnmodular = do
+  g <- get
+  umDesc <- unmodularDesc
+  let n = pName $ codeSpec g
+      cls = any (`member` clsMap g)
+        ["get_input", "derived_values", "input_constraints"]
+  genModuleWithImports n umDesc (concatMap (^. imports) (elems $ extLibMap g))
+    (genMainFunc
+      : map (fmap Just) (map genCalcFunc (execOrder $ codeSpec g)
+        ++ concatMap genModFuncs (modules g))
+      ++ ((if cls then [] else [genInputFormat Pub, genInputDerived Pub,
+        genInputConstraints Pub]) ++ [genOutputFormat]))
+    ([genInputClass Auxiliary, genConstClass Auxiliary]
+      ++ map (fmap Just) (concatMap genModClasses $ modules g))
+
+-- | Generates all modules for an SCS program.
+genModules :: (OOProg r) => GenState [SFile r]
+genModules = do
+  g <- get
+  mn     <- genMain
+  inp    <- chooseInModule $ inMod g
+  con    <- genConstMod
+  cal    <- genCalcMod
+  out    <- genOutputMod
+  moddef <- traverse genModDef (modules g) -- hack ?
+  return $ mn : inp ++ con ++ cal : out ++ moddef
+
+-- | Private utilities used in 'generateCode'.
+getDir :: Lang -> String
+getDir Cpp = "cpp"
+getDir CSharp = "csharp"
+getDir Java = "java"
+getDir Python = "python"
+getDir Swift = "swift"

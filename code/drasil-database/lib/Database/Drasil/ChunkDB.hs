fcc04158f998312d17ff2c3a3ff3b02b2e08e326
Jason Balaci <jason@balaci.me>
2025-08-12 17:54:19 -0400

    Add comments to ChunkDB.hs.

--- code/drasil-database/lib/Database/Drasil/ChunkDB.hs
+++ code/drasil-database/lib/Database/Drasil/ChunkDB.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Database.Drasil.ChunkDB (
+  -- * Core database types and functions.
   ChunkDB,
   empty, fromList,
   registered, typesRegistered, size,
@@ -10,7 +11,7 @@ module Database.Drasil.ChunkDB (
   findUnused,
   find, findOrErr,
   findAll, findAll',
-  whatRefs, whatRefsOrErr,
+  dependants, dependantsOrErr,
   findTypeOf,
   insert, insertAll, union,
   -- * Temporary functions for working with non-chunk tables
@@ -23,8 +24,8 @@ module Database.Drasil.ChunkDB (
 
 import Control.Lens ((^.))
 import Data.Foldable (foldl')
-import Data.List (nub, (\\))
-import Data.Maybe (fromMaybe, isJust, mapMaybe)
+import Data.List (nub)
+import Data.Maybe (fromMaybe, mapMaybe)
 import Data.Typeable (Proxy (Proxy), TypeRep, Typeable, typeOf, typeRep, cast)
 
 -- NOTE: Debug.Trace should only be used for warnings and errors, not for
@@ -38,68 +39,95 @@ import Debug.Trace (trace)
 -- other databases.
 import qualified Data.Map.Strict as M
 
-import Drasil.Database.Chunk (Chunk, HasChunkRefs (chunkRefs), IsChunk, mkChunk, unChunk, chunkType)
+import Drasil.Database.Chunk (Chunk, HasChunkRefs(chunkRefs), IsChunk,
+  mkChunk, unChunk, chunkType)
 import Language.Drasil (HasUID(..), UID, LabelledContent, Reference)
 import Utils.Drasil (errMsg, warnMsg)
 
+-- | A chunk that depends on another.
 type Dependant = UID
 
-type ChunkByUID = M.Map UID (Chunk, [Dependant])
+-- | Mapping of 'UID's to 'Chunk's and their dependants.
+type ChunkByUID = M.Map UID (Chunk, [Dependant]) -- FIXME: the list of dependants should be a Set.
 
+-- | Mapping of chunk types to lists of instances of them (chunks).
 type ChunksByTypeRep = M.Map TypeRep [Chunk]
 
+-- | Drasil's knowledge database.
 data ChunkDB = ChunkDB {
     chunkTable     :: ChunkByUID
   , chunkTypeTable :: ChunksByTypeRep
 
-  -- FIXME: All things below need to be rebuilt.
-  , labelledcontentTable :: UMap LabelledContent -- TODO: LabelledContent needs to be rebuilt. See JacquesCarette/Drasil#4023.
-  , refTable             :: UMap Reference -- TODO: References need to be rebuilt. See JacquesCarette/Drasil#4022.
+  -- FIXME: All things below need to be rebuilt!!
+
+  -- FIXME: All code in this file contains hacks specifically for the old
+  -- LabelledContent and Reference chunks. Once rebuilt, these chunks should not
+  -- have a unique 'UID' and should be registered in the 'ChunkDB' like any
+  -- other chunk.
+
+  -- TODO: LabelledContent needs to be rebuilt. See JacquesCarette/Drasil#4023.
+  , labelledcontentTable :: UMap LabelledContent
+  -- TODO: References need to be rebuilt. See JacquesCarette/Drasil#4022.
+  , refTable             :: UMap Reference
   , traceTable           :: M.Map UID [UID]
   , refbyTable           :: M.Map UID [UID]
 }
 
+-- | An empty 'ChunkDB'.
 empty :: ChunkDB
 empty = ChunkDB M.empty M.empty M.empty M.empty M.empty M.empty
 
+-- | Create a 'ChunkDB' from a list of chunks. This will insert all chunks into
+-- the database from the list, from left to right.
 fromList :: IsChunk a => [a] -> ChunkDB
 fromList = flip insertAll empty
 
+-- | Query the 'ChunkDB' for all registered chunks (by their 'UID's).
 registered :: ChunkDB -> [UID]
 registered cdb =
      M.keys (chunkTable cdb)
   ++ M.keys (labelledcontentTable cdb)
   ++ M.keys (refTable cdb)
 
+-- | Check if a 'UID' is registered in the 'ChunkDB'.
 isRegistered :: UID -> ChunkDB -> Bool
 isRegistered u cdb =
      M.member u (chunkTable cdb)
   || M.member u (labelledcontentTable cdb)
   || M.member u (refTable cdb)
 
+-- | Enumerate all types registered in the 'ChunkDB'.
 typesRegistered :: ChunkDB -> [TypeRep]
 typesRegistered cdb =
     typeRep (Proxy @LabelledContent)
   : typeRep (Proxy @Reference)
   : M.keys (chunkTypeTable cdb)
 
+-- | Get the number of chunks registered in the 'ChunkDB'.
 size :: ChunkDB -> Int
 size cdb =
     M.size (chunkTable cdb)
   + M.size (labelledcontentTable cdb)
   + M.size (refTable cdb)
 
+-- | Filter the 'ChunkDB' for chunks that are not needed by any other chunks.
+-- These are the only chunks that can safely be removed from the database,
+-- though we do not include this functionality.
 findUnused :: ChunkDB -> [UID]
 findUnused = M.keys . M.filter (\(_, refs) -> null refs) . chunkTable
 
+-- | Find a chunk by its 'UID' in the 'ChunkDB'.
 find :: Typeable a => UID -> ChunkDB -> Maybe a
 find u cdb = do
   (c', _) <- M.lookup u (chunkTable cdb)
   unChunk c'
 
+-- | Find a chunk by its 'UID' in the 'ChunkDB', throwing a hard error if it is
+-- not found.
 findOrErr :: Typeable a => UID -> ChunkDB -> a
 findOrErr u = fromMaybe (error $ "Failed to find chunk " ++ show u) . find u
 
+-- | Find all chunks of a specific type in the 'ChunkDB'.
 findAll :: forall a. IsChunk a => ChunkDB -> [a]
 findAll cdb
   | tr == typeRep (Proxy @LabelledContent) =
@@ -111,6 +139,8 @@ findAll cdb
   where
     tr = typeRep (Proxy :: Proxy a)
 
+-- | Find all chunks of a specific type in the 'ChunkDB', returning their 'UID's
+-- rather than the chunks themselves.
 findAll' :: TypeRep -> ChunkDB -> [UID]
 findAll' tr cdb
   | tr == typeRep (Proxy @LabelledContent) =
@@ -120,49 +150,64 @@ findAll' tr cdb
   | otherwise =
       maybe [] (map (^. uid)) $ M.lookup tr (chunkTypeTable cdb)
 
-whatRefs :: UID -> ChunkDB -> Maybe [UID]
-whatRefs u cdb = do
+-- | Find all chunks that depend on a specific one.
+dependants :: UID -> ChunkDB -> Maybe [UID]
+dependants u cdb = do
   (_, refs) <- M.lookup u (chunkTable cdb)
   Just refs
 
-whatRefsOrErr :: UID -> ChunkDB -> [UID]
-whatRefsOrErr u = fromMaybe (error $ "Failed to find references for unknown chunk " ++ show u) . find u
+-- | Find all chunks that depend on a specific one, throwing a hard error if the
+-- dependency chunk is not found.
+dependantsOrErr :: UID -> ChunkDB -> [UID]
+dependantsOrErr u = fromMaybe (error $ "Failed to find references for unknown chunk " ++ show u) . find u
 
+-- | Find the type of a chunk by its 'UID'.
 findTypeOf :: UID -> ChunkDB -> Maybe TypeRep
 findTypeOf u cdb = chunkType . fst <$> M.lookup u (chunkTable cdb)
 
+-- | Internal function for inserting a dependancy of a chunk into the
+-- dependancy's respective dependants list.
+insertRefExpectingExistence :: UID -> UID -> ChunkByUID -> ChunkByUID
+insertRefExpectingExistence depdnt depdncy cbu =
+  case M.insertLookupWithKey (\_ _ (c, depdnts) -> (c, depdnt : depdnts)) depdncy (undefined, []) cbu of
+    (Just _, cbu') -> cbu' -- If the chunk is already registered, we just updated its dependants, and everything is fine.
+    (Nothing, _) -> -- But if no data was found, then we have a problem: the chunk we are inserting depends on a chunk that does not exist.
+      error $ "Chunk dependancy is missing for `" ++ show depdnt ++ "`. Missing: `" ++ show depdncy ++ "`."
+
+-- | Internal function to insert a chunk into the 'ChunkDB'. This function
+-- assumes that the chunk is not already registered in the database, and quietly
+-- break table synchronicity if it is.
 insert0 :: IsChunk a => ChunkDB -> a -> ChunkDB
 insert0 cdb c = cdb'
   where
-    c' :: Chunk
+    -- Box our chunk.
     c' = mkChunk c
 
-    cu' :: ChunkByUID
-    cu' = M.insert (c ^. uid) (c', []) (chunkTable cdb) -- insert our chunk, it is not currently referred by anything.
+    -- Insert our chunk, it is not currently depended on by anything.
+    chunkTable' = M.insert (c ^. uid) (c', []) (chunkTable cdb)
 
-    insertRefExpectingExistence :: UID -> ChunkByUID -> ChunkByUID
-    insertRefExpectingExistence u cbu =
-      if isJust prev
-        then cbu'
-        else error $ "Referred knowledge is missing for `" ++ show (c ^. uid) ++ "`; needs `" ++ show u ++ "`"
-      where
-        (prev, cbu') = M.insertLookupWithKey (\_ _ (rcc, rcref) -> (rcc, u : rcref)) u (undefined, []) cbu
+    -- Capture all dependencies of this chunk.
+    chunkTable'' = foldr (insertRefExpectingExistence $ c' ^. uid) chunkTable'
+      $ nub (chunkRefs c)
 
-    finalCu :: ChunkByUID
-    finalCu = foldr insertRefExpectingExistence cu' $ nub (chunkRefs c) \\ [c ^. uid]
+    -- Add our chunk to its corresponding 'chunks by type' list.
+    chunkTypeTable' = M.alter (Just . maybe [c'] (++ [c'])) (typeOf c) (chunkTypeTable cdb)
 
-    ctr' :: ChunksByTypeRep
-    ctr' = M.alter (Just . maybe [c'] (++ [c'])) (typeOf c) (chunkTypeTable cdb)
+    -- Finally, build the updated database.
+    cdb' = cdb { chunkTable = chunkTable'', chunkTypeTable = chunkTypeTable' }
 
-    cdb' :: ChunkDB
-    cdb' = cdb {chunkTable = finalCu, chunkTypeTable = ctr'}
-
+-- | Insert a chunk into the 'ChunkDB' if it is sensibly to do so (i.e., does
+-- not depend on itself and is not a 'ChunkDB'). We temporarily allow chunks to
+-- overwrite other ones, but we warn when this happens.
 insert :: IsChunk a => a -> ChunkDB -> ChunkDB
 insert c cdb
+  | c ^. uid `elem` chunkRefs c =
+      error $ "Chunk `" ++ show (c ^. uid) ++ "` cannot reference itself as a dependancy."
   | typeOf c == typeRep (Proxy @ChunkDB) =
       error "Insertion of ChunkDBs in ChunkDBs is disallowed; please perform unions with them instead."
   | (Just x) <- findTypeOf (c ^. uid) cdb =
-      -- Overwrite: remove previous chunk from chunk refs and chunksByType tables before inserting new one.
+      -- Overwrite: remove previous chunk from chunk refs and chunksByType
+      -- tables before inserting new one.
       let prevChunk = fst $ fromMaybe (error "Internal error: chunk missing after findTypeOf succeeded") (M.lookup (c ^. uid) (chunkTable cdb))
           prevType  = chunkType prevChunk
           cu' = M.delete (c ^. uid) (chunkTable cdb)
@@ -174,9 +219,11 @@ insert c cdb
             else trace (errMsg $ "SUPER-MEGA-ULTRA-DELUXE-WARNING! Overwriting a chunk (`" ++ show (c ^. uid) ++ "` :: `" ++ show x ++ "`) with a chunk of a different type: `" ++ show (typeOf c) ++ "`") cdb''
   | otherwise = insert0 cdb c
 
+-- | Insert a list of chunks into a 'ChunkDB'.
 insertAll :: IsChunk a => [a] -> ChunkDB -> ChunkDB
 insertAll as cdb = foldl' (flip insert) cdb as
 
+-- | Union two 'ChunkDB's together, throwing an error for any 'UID' collisions.
 union :: ChunkDB -> ChunkDB -> ChunkDB
 union cdb1 cdb2 = ChunkDB um' trm' lc' ref' trc' refb'
   where
@@ -195,8 +242,12 @@ union cdb1 cdb2 = ChunkDB um' trm' lc' ref' trc' refb'
 -- built properly (i.e., using the `HasChunkRefs` typeclass).
 --------------------------------------------------------------------------------
 
+-- | An ordered map based on 'Data.Map.Strict' for looking up chunks by their
+-- 'UID's.
 type UMap a = M.Map UID (a, Int)
 
+-- | Create a 'UMap' from a list of chunks. Assumes that the leftmost chunk in
+-- the list has index 0, increasing by 1 each step to the right.
 idMap :: HasUID a => [a] -> UMap a
 idMap vals = M.fromList $ zipWith (\v i -> (v ^. uid, (v, i))) vals [0..]
 
@@ -205,15 +256,19 @@ uMapLookup :: String -> String -> UID -> UMap a -> a
 uMapLookup tys ms u t = getFM $ M.lookup u t
   where getFM = maybe (error $ tys ++ ": " ++ show u ++ " not found in " ++ ms) fst
 
+-- | Find a 'LabelledContent' by its 'UID', throwing an error if it is not
+-- found.
 labelledcontentFind :: UID -> ChunkDB -> LabelledContent
 labelledcontentFind u cdb = uMapLookup "LabelledContent" "labelledcontentTable" u (labelledcontentTable cdb)
 
+-- | Find a 'Reference' by its 'UID', throwing an error if it is not found.
 refFind :: UID -> ChunkDB -> Reference
 refFind u cdb = uMapLookup "Reference" "refTable" u (refTable cdb)
 
+-- | Find what chunks reference a given 'UID'.
 refbyLookup :: UID -> M.Map UID [UID] -> [UID]
 refbyLookup c = fromMaybe [] . M.lookup c
 
--- | Trace a 'UID' to related 'UID's.
+-- | Query a chunk for to what chunks it refers to.
 traceLookup :: UID -> M.Map UID [UID] -> [UID]
-traceLookup c = fromMaybe [] . M.lookup c
+traceLookup = refbyLookup -- Same implementation, just different name for code clarity.

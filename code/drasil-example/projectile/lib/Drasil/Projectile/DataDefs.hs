25f3375b42548ccac8fecabc09d531523b6afaf4
Sarra Soussia <sarrasoussia@MacBook-Pro-de-Sarra.local>
2025-08-14 14:38:24 +0100

    TAandA migration

--- code/drasil-example/projectile/lib/Drasil/Projectile/DataDefs.hs
+++ code/drasil-example/projectile/lib/Drasil/Projectile/DataDefs.hs
@@ -1,21 +1,29 @@
-module Drasil.Projectile.DataDefs (dataDefs, speedIX, speedIY) where
+module Drasil.Projectile.DataDefs (dataDefs, speedIX, speedIY, speedDD) where
 
-import Prelude hiding (sin, cos)
+import Prelude hiding (sin, cos, sqrt)
 import Language.Drasil
 import qualified Language.Drasil.Sentence.Combinators as S
 
 import Theory.Drasil (DataDefinition, ddENoRefs)
 
-import Data.Drasil.Quantities.Physics (iSpeed, ixVel, iyVel)
-import Data.Drasil.Theories.Physics (vecMag)
+import Data.Drasil.Quantities.Physics (iSpeed, ixVel, iyVel, speed)
 
 import Drasil.Projectile.Figures (figLaunch)
 import Drasil.Projectile.Unitals (launAngle)
 
 dataDefs :: [DataDefinition]
-dataDefs = [vecMag, speedIX, speedIY]
+dataDefs = [speedDD, speedIX, speedIY]
 
 ----------
+speedDD :: DataDefinition 
+speedDD = ddENoRefs speedQD Nothing "vecMag" [speedNote]
+
+speedQD :: SimpleQDef
+speedQD = mkQuantDef speed $ sqrt (square (sy ixVel) $+ square (sy iyVel))
+
+speedNote :: Sentence
+speedNote = S "The magnitude of velocity computed from its components"
+
 speedIX, speedIY :: DataDefinition
 speedIX = ddENoRefs speedIXQD Nothing "speedIX" [speedRef, figRef]
 speedIY = ddENoRefs speedIYQD Nothing "speedIY" [speedRef, figRef]
@@ -26,7 +34,7 @@ speedIYQD = mkQuantDef iyVel $ sy iSpeed $* sin (sy launAngle)
 
 ----------
 speedRef :: Sentence
-speedRef = ch iSpeed `S.is` S "from" +:+. refS vecMag
+speedRef = ch iSpeed `S.is` S "from" +:+. refS speedDD
 
 figRef :: Sentence
 figRef = ch launAngle `S.is` S "shown in" +:+. refS figLaunch

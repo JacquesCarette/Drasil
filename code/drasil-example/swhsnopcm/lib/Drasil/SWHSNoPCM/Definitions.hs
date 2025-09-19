c5dd8e4a702b76dd01f2d22267719f07ecd71d22
Jason Balaci <jason@balaci.me>
2025-09-19 16:45:02 -0400

    SWHSNoPCM: Remove duplicate (incorrect?) copy of SWHSNoPCM CI.

--- code/drasil-example/swhsnopcm/lib/Drasil/SWHSNoPCM/Definitions.hs
+++ code/drasil-example/swhsnopcm/lib/Drasil/SWHSNoPCM/Definitions.hs
@@ -6,6 +6,3 @@ import Language.Drasil
 htTrans :: IdeaDict
 htTrans = nc "heat transfer" (cn "heat transfer") --Not really a nounphase,
                                                    --just a hack to get RefSec to work
-
-srsSWHS :: CI -- Used to make the title of the paper
-srsSWHS = commonIdea "srsSWHS" (nounPhraseSP "Solar Water Heating Systems") "SWHSNoPCM" []

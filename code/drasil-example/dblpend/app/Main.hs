ab9d3db21479303ee534ab993cca32eabaecbe53
sarrasoussia <sarrasoussia2001@gmail.com>
2025-10-11 23:14:18 +0100

    work on code generation

--- code/drasil-example/dblpend/app/Main.hs
+++ code/drasil-example/dblpend/app/Main.hs
@@ -2,9 +2,10 @@ module Main (main) where
 
 import GHC.IO.Encoding
 
-import Drasil.Generator (gen, typeCheckSI, genDot,
+import Drasil.Generator (gen, typeCheckSI, genCode, genDot,
   DocSpec(DocSpec), DocType(SRS), Format(..), docChoices, dumpEverything)
 import Drasil.DblPend.Body (srs, printSetting, fullSI)
+import Drasil.DblPend.Choices (choices, code)
 
 main :: IO()
 main = do
@@ -12,5 +13,5 @@ main = do
   dumpEverything fullSI printSetting ".drasil/"
   typeCheckSI fullSI
   gen (DocSpec (docChoices SRS [HTML, TeX, Jupyter, MDBook]) "DblPend_SRS") srs printSetting
-  -- genCode choices code
+  genCode choices code
   genDot fullSI

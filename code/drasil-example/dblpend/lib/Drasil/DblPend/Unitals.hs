1c7e70899c4ff4fe91791e690a538277e2e123ce
sarrasoussia <sarrasoussia2001@gmail.com>
2025-10-13 14:20:47 +0100

    removed extra code

--- code/drasil-example/dblpend/lib/Drasil/DblPend/Unitals.hs
+++ code/drasil-example/dblpend/lib/Drasil/DblPend/Unitals.hs
@@ -105,13 +105,13 @@ pendDisAngle_2 = uc' "theta_2" (CM.angle `ofThe` secondRod)
 posVec_1, posVec_2 :: UnitalChunk
 posVec_1 = uc' "p_1" (QP.position `ofThe` firstObject)
   (phraseNP (QP.position `the_ofThe` firstObject))
-  (vec lP `sub` label1) (realVect vecDim) metre
+  (vec lP `sub` label1) realVect metre
 
 posVec_2 = uc' "p_2" (QP.position `ofThe` secondObject)
   (phraseNP (QP.position `the_ofThe` secondObject))
-  (vec lP `sub` label2) (realVect vecDim) metre
+  (vec lP `sub` label2) realVect metre
 
--- Clifford velocities (placeholders for code generation)
+-- Clifford velocities
 mvVel_1, mvVel_2 :: UnitalChunk
 mvVel_1 = uc' "v_mv1" (QP.velocity `ofThe` firstObject)
   (phraseNP (QP.velocity `the_ofThe` firstObject))
@@ -121,7 +121,7 @@ mvVel_2 = uc' "v_mv2" (QP.velocity `ofThe` secondObject)
   (phraseNP (QP.velocity `the_ofThe` secondObject))
   (vec lV `sub` label2) (realVect vecDim) velU
 
--- Clifford accelerations (placeholders)
+-- Clifford accelerations
 mvAccel_1, mvAccel_2 :: UnitalChunk
 mvAccel_1 = uc' "a_mv1" (QP.acceleration `ofThe` firstObject)
   (phraseNP (QP.acceleration `the_ofThe` firstObject))
@@ -131,7 +131,7 @@ mvAccel_2 = uc' "a_mv2" (QP.acceleration `ofThe` secondObject)
   (phraseNP (QP.acceleration `the_ofThe` secondObject))
   (vec lA `sub` label2) (realVect vecDim) accelU
 
--- Clifford forces (placeholders)
+-- Clifford forces
 mvForce_1, mvForce_2 :: UnitalChunk
 mvForce_1 = uc' "F_mv1" (QP.force `ofThe` firstObject)
   (phraseNP (QP.force `the_ofThe` firstObject))

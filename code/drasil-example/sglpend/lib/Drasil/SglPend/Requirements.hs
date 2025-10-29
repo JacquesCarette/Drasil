765981a5ab9717f1e99d6ade5aeabf64d967cb4f
Jacques Carette <carette@mcmaster.ca>
2025-10-29 14:48:12 -0400

    get rid of some duplication

--- code/drasil-example/sglpend/lib/Drasil/SglPend/Requirements.hs
+++ code/drasil-example/sglpend/lib/Drasil/SglPend/Requirements.hs
@@ -1,4 +1,4 @@
-module Drasil.SglPend.Requirements where
+module Drasil.SglPend.Requirements (funcReqs) where
 
 import Language.Drasil
 import qualified Language.Drasil.Sentence.Combinators as S
@@ -6,7 +6,7 @@ import Data.Drasil.Concepts.Documentation (funcReqDom, output_, value)
 import Drasil.SglPend.IMods (angularDisplacementIM)
 import Drasil.SglPend.Unitals (lenRod, pendDisplacementAngle)
 import Data.Drasil.Quantities.Physics (angularDisplacement)
-import Drasil.DblPend.Requirements(verifyInptVals, verifyInptValsDesc)
+import Drasil.DblPend.Requirements(verifyInptVals)
 
 --Functional Requirements--
 funcReqs :: [ConceptInstance]

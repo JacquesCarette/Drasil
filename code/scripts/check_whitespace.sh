4a1e924fd86fa2703c7ece8645acc3edf8adba17
Jason Balaci <jason@balaci.me>
2025-11-15 21:13:32 -0500

    check_whitespace.sh: Avoid using find and a for-loop...

--- code/scripts/check_whitespace.sh
+++ code/scripts/check_whitespace.sh
@@ -2,23 +2,21 @@
 
 exit_code=0
 
-for f in $(find . -type f -name "*.hs")
+while IFS= read -r -d '' f
 do
-    if [ ! -z "$(tail -c 1 <"$f")" ] ; then
+    if [ -n "$(tail -c 1 <"$f")" ] ; then
         echo "MISSING TRAILING NEWLINE CHARACTER: $f"
+
         exit_code=1
     fi
-done
 
-for f in $(find . -type f -name "*.hs")
-do
     bad_lines=$(grep -nE "\s+$" "$f")
-    if [ ! -z "$bad_lines" ] ; then
+    if [ -n "$bad_lines" ] ; then
         echo "$f CONTAINS PER-LINE TRAILING WHITESPACE:"
         echo "$bad_lines"
 
         exit_code=1
     fi
-done
+done < <(find . -type f -name "*.hs" -print0)
 
 exit $exit_code

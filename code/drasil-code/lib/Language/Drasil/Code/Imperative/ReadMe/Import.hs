1c14a3589d105195f7f7c9a6c5d45ee2da048c8a
Jason Balaci <jason@balaci.me>
2025-12-03 21:38:39 -0500

    Move `makeReadMe` closer to definition of `ReadMeInfo`.

--- code/drasil-code/lib/Language/Drasil/Code/Imperative/ReadMe/Import.hs
+++ code/drasil-code/lib/Language/Drasil/Code/Imperative/ReadMe/Import.hs
@@ -1,11 +1,19 @@
 -- | This module defines structures and functions for handling README files,
 -- which are used as auxiliary documentation files for the system.
 module Language.Drasil.Code.Imperative.ReadMe.Import (
-  ReadMeInfo(..),
+  ReadMeInfo(..), makeReadMe
 ) where
 
+import Prelude hiding ((<>))
+
+import Data.List.NonEmpty (nonEmpty, toList)
+import Text.PrettyPrint.HughesPJ (Doc, empty)
+
+import Language.Drasil.Printers (makeMd, introInfo, verInfo, unsupOS,
+    extLibSec, instDoc, endNote, whatInfo)
+
+import Language.Drasil.Choices (ImplementationType(..))
 import Language.Drasil.Mod (Name, Version)
-import Language.Drasil.Choices (ImplementationType)
 
 -- | Language name.
 type LangAbbrev = String
@@ -46,3 +54,40 @@ data ReadMeInfo = ReadMeInfo {
   folderNum :: Int,
   inputOutput :: (InFile, OutFile)
 }
+
+-- | Generates a README file.
+makeReadMe :: ReadMeInfo -> Doc
+makeReadMe ReadMeInfo {
+        langName = progLang,
+        langVersion = progLangVers,
+        invalidOS = unsupportedOSs,
+        implementType = imptype,
+        extLibNV = extLibns,
+        extLibFP = extLibfp,
+        contributors = auths,
+        configFP = configFPs,
+        caseName = name,
+        examplePurpose = purp,
+        exampleDescr = descr,
+        exampleMotivation = motiv,
+        exampleScope = sc,
+        folderNum = number,
+        inputOutput = inoutf} =
+    makeMd [introInfo name auths (fieldEmptySTR motiv)
+      (fieldEmptySTR purp),
+    whatInfo (fieldEmptySTR descr) (fieldEmptySTR sc),
+    makeInstr imptype configFPs name inoutf,
+    verInfo progLang progLangVers,
+    unsupOS unsupportedOSs,
+    extLibSec extLibns extLibfp,
+    endNote number auths] -- add date information to end note for license
+
+-- | Helper for encoding the type of program (either library or controller-based) in a README file.
+makeInstr :: ImplementationType -> [FilePath] -> String -> (String, String) -> Doc
+makeInstr Library _ _ _ = empty
+makeInstr Program cfp n inOutf = instDoc cfp n inOutf
+
+-- |Helper that checks if the field is empty; allowing optional content
+-- rendering in a README file
+fieldEmptySTR :: String -> Maybe String
+fieldEmptySTR = fmap toList . nonEmpty

49927264376359ff6051834e5b339b0f8ccf3f29
Jason Balaci <jason@balaci.me>
2025-12-13 14:23:18 -0500

    `drasil-database`: Create an untyped version of `TypedUIDRef`.

--- code/drasil-database/lib/Drasil/Database/UIDRef.hs
+++ code/drasil-database/lib/Drasil/Database/UIDRef.hs
@@ -1,6 +1,9 @@
 module Drasil.Database.UIDRef (
-  TypedUIDRef, mkRef,
-  typedFind, typedFindOrErr,
+  -- * 'UID' References
+  TypedUIDRef, mkTypedRef,
+  typedDeref, typedDerefOrErr,
+  UIDRef, mkRef,
+  deref, derefOrErr
 ) where
 
 import Control.Lens ((^.))
@@ -11,8 +14,9 @@ import Drasil.Database.ChunkDB (ChunkDB, find)
 import Drasil.Database.UID (HasUID(..), UID)
 import qualified Data.Set as S (singleton)
 
--- | 'UID' references that contain information about the type of data the 'UID'
--- refers to, useful for type-safe dereferencing.
+-- | A reference to another chunk through its 'UID', with a type variable to
+-- retain information about the original chunk's type, useful for type-safe
+-- dereferencing.
 newtype TypedUIDRef typ = TypedUIDRef UID
 
 instance HasChunkRefs (TypedUIDRef t) where
@@ -21,13 +25,34 @@ instance HasChunkRefs (TypedUIDRef t) where
   {-# INLINABLE chunkRefs #-}
 
 -- | Create a 'TypedUIDRef' to a chunk.
-mkRef :: IsChunk t => t -> TypedUIDRef t
-mkRef t = TypedUIDRef $ t ^. uid
+mkTypedRef :: IsChunk t => t -> TypedUIDRef t
+mkTypedRef = TypedUIDRef . (^. uid)
 
--- | Find a chunk by its typed UID reference.
-typedFind :: IsChunk t => TypedUIDRef t -> ChunkDB -> Maybe t
-typedFind (TypedUIDRef u) = find u
+-- | Find a chunk by a 'TypedUIDRef'.
+typedDeref :: IsChunk t => TypedUIDRef t -> ChunkDB -> Maybe t
+typedDeref (TypedUIDRef u) = find u
 
--- | Find a chunk by its typed UID reference, erroring if not found.
-typedFindOrErr :: IsChunk t => TypedUIDRef t -> ChunkDB -> t
-typedFindOrErr tu cdb = fromMaybe (error "Typed UID dereference failed.") (typedFind tu cdb)
+-- | Find a chunk by a 'TypedUIDRef', erroring if not found.
+typedDerefOrErr :: IsChunk t => TypedUIDRef t -> ChunkDB -> t
+typedDerefOrErr tu cdb = fromMaybe (error "Typed UID dereference failed.") (typedDeref tu cdb)
+
+-- | A variant of 'TypedUIDRef' without type information about the chunk being
+-- referred to.
+newtype UIDRef = UIDRef UID
+
+instance HasChunkRefs UIDRef where
+  -- | A 'UIDRef t' carries a 'UID' referring to another chunk.
+  chunkRefs (UIDRef u) = S.singleton u
+  {-# INLINABLE chunkRefs #-}
+
+-- | Create a 'UIDRef' to a chunk.
+mkRef :: IsChunk t => t -> UIDRef
+mkRef = UIDRef . (^. uid)
+
+-- | Find a chunk by its 'UIDRef'.
+deref :: IsChunk t => UIDRef -> ChunkDB -> Maybe t
+deref (UIDRef u) = find u
+
+-- | Find a chunk by its 'UIDRef', erroring if not found.
+derefOrErr :: IsChunk t => UIDRef -> ChunkDB -> t
+derefOrErr tu cdb = fromMaybe (error "Untyped UID dereference failed.") (deref tu cdb)

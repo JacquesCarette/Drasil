6db5b620f88ff6d55ee58b173702be611f3fd2c6
Xinlu <yanxinlu@outlook.com>
2026-01-19 22:50:34 -0500

    short-circuit HasChunkRefs for NP

--- code/drasil-lang/lib/Language/Drasil/NounPhrase/Core.hs
+++ code/drasil-lang/lib/Language/Drasil/NounPhrase/Core.hs
@@ -4,46 +4,12 @@ module Language.Drasil.NounPhrase.Core (
   module Language.Drasil.NounPhrase.Types
 ) where
 
-import Drasil.Database (HasChunkRefs(..), UID)
+import Drasil.Database (HasChunkRefs(..))
 
 import Language.Drasil.NounPhrase.Types
-import qualified Language.Drasil.Sentence as Sent
-import Language.Drasil.Sentence (Sentence, sentenceRefs)
-
-import qualified Data.Set as Set
 
 -- | Gather the chunk references mentioned within an 'NP'.
 instance HasChunkRefs NP where
-    chunkRefs = npRefs
+    -- NPStruct only contains Strings and Symbols, so it cannot embed UID refs.
+    chunkRefs _ = mempty
     {-# INLINABLE chunkRefs #-}
-
--- | Collect references contained in a noun phrase.
-npRefs :: NP -> Set.Set UID
-npRefs (ProperNoun _ _)         = Set.empty
-npRefs (CommonNoun _ _ cRule)   = capRuleRefs cRule
-npRefs (Phrase sing plural c1 c2) =
-  npStructRefs sing `Set.union`
-  npStructRefs plural `Set.union`
-  capRuleRefs c1 `Set.union`
-  capRuleRefs c2
-{-# INLINABLE npRefs #-}
-
--- | Extract references embedded in capitalization rules (only 'Replace').
-capRuleRefs :: CapitalizationRule -> Set.Set UID
-capRuleRefs CapFirst     = Set.empty
-capRuleRefs CapWords     = Set.empty
-capRuleRefs CapNothing   = Set.empty
-capRuleRefs (Replace s)  = npStructRefs s
-{-# INLINABLE capRuleRefs #-}
-
--- | Convert a noun-phrase structure into a sentence and reuse 'sentenceRefs'.
-npStructRefs :: NPStruct -> Set.Set UID
-npStructRefs = sentenceRefs . npStructToSentence
-{-# INLINABLE npStructRefs #-}
-
--- | Translate 'NPStruct' into a 'Sentence' so that sentence helpers can traverse it.
-npStructToSentence :: NPStruct -> Sentence
-npStructToSentence (S s)       = Sent.S s
-npStructToSentence (a :-: b)   = npStructToSentence a Sent.:+: npStructToSentence b
-npStructToSentence (a :+: b)   = npStructToSentence a Sent.+:+ npStructToSentence b
-npStructToSentence (P sym)     = Sent.P sym

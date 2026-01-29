5e486c499074e54ec2e0c446f3f380714e02626e
Jason Balaci <jason@balaci.me>
2026-01-29 11:18:30 -0500

    Remove unused `hibbeler2004` reference from DblPend & SglPend.
    Reason why "both" at the same time: SglPend inherits its references from
    DblPend.

--- code/drasil-example/dblpend/lib/Drasil/DblPend/References.hs
+++ code/drasil-example/dblpend/lib/Drasil/DblPend/References.hs
@@ -1,12 +1,11 @@
 module Drasil.DblPend.References (citations, koothoor2013,
   smithEtAl2007, smithLai2005, smithKoothoor2016) where
 
-import Language.Drasil
+import Language.Drasil (BibRef)
 import Data.Drasil.Citations (cartesianWiki, accelerationWiki, velocityWiki,
- parnasClements1986, hibbeler2004, koothoor2013, smithEtAl2007, smithLai2005,
- smithKoothoor2016)
+ parnasClements1986, koothoor2013, smithEtAl2007, smithLai2005, smithKoothoor2016)
 
 citations :: BibRef
-citations = [accelerationWiki, velocityWiki, hibbeler2004, cartesianWiki,
+citations = [accelerationWiki, velocityWiki, cartesianWiki,
              parnasClements1986, koothoor2013, smithEtAl2007, smithLai2005,
              smithKoothoor2016]

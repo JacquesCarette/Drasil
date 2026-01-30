fb5576d1f7b72056d36f2e1c04e1a3af6bdfa450
Brandon Bosman <brandobosman@gmail.com>
2026-01-30 12:30:30 -0500

    Removed PackageFiles

--- code/drasil-code/lib/Language/Drasil/Code/PackageFiles.hs
+++ code/drasil-code/lib/Language/Drasil/Code/PackageFiles.hs
@@ -1,40 +1,35 @@
--- | Contains the high-level functionality to create 'PackageFiles' and then produce the
+-- | Contains the high-level functionality to consolidate and then produce the
 -- actual generated package files.
 module Language.Drasil.Code.PackageFiles (
     consolidatePackageFiles,
     createPackageFiles,
 ) where
 
-import Text.PrettyPrint.HughesPJ ( Doc, Doc, render )
+import Text.PrettyPrint.HughesPJ (render)
 
-import Drasil.GOOL (FileData(..), ModData(modDoc))
+import qualified Drasil.GOOL as G (FileData(..), ModData(modDoc))
 import Utils.Drasil (createDirIfMissing)
 
-import Language.Drasil.Code.FileData (FileAndContents(fileDoc))
-import qualified Language.Drasil.Code.FileData as D (
-  filePath)
+import Language.Drasil.Code.FileData (FileAndContents(filePath, fileDoc), fileAndContents)
 import System.FilePath.Posix (takeDirectory)
 import System.IO (hPutStrLn, hClose, openFile, IOMode(WriteMode))
 
--- | Represents the generated files of a package as a unified
---   list of pairs of file names and rendered contents.
-newtype PackageFiles = PackageFiles [(FilePath, Doc)]
-
 -- | Converts 'FileData' ('FilePath's with module data) and 'FileAndContents'
--- ('FilePath's with auxiliary document information) into 'PackageFiles'
--- (a unified format for all file types)
-consolidatePackageFiles :: [FileData] -> [FileAndContents] -> PackageFiles
-consolidatePackageFiles files aux = PackageFiles $ zip (map filePath files ++ map D.filePath aux)
-  (map (modDoc . fileMod) files ++ map fileDoc aux)
+-- ('FilePath's with auxiliary document information) into a big list of
+-- 'FileAndContents'
+consolidatePackageFiles :: [G.FileData] -> [FileAndContents] -> [FileAndContents]
+consolidatePackageFiles files aux = aux ++ map (\file -> fileAndContents (G.filePath file) ((G.modDoc . G.fileMod) file)) files
 
--- | Outputs the requested 'Package Files' into system files.
-createPackageFiles :: PackageFiles -> IO ()
-createPackageFiles (PackageFiles cs) = mapM_ createPackageFile cs
+-- | Outputs the requested 'FileAndContents's into system files.
+createPackageFiles :: [FileAndContents] -> IO ()
+createPackageFiles = mapM_ createPackageFile
 
--- | Helper that uses pairs of 'PackageFiles' to create a file written with the given
--- document at the given 'FilePath'.
-createPackageFile :: (FilePath, Doc) -> IO ()
-createPackageFile (path, contents) = do
+-- | Helper to convert a FileAndContents into a real file with the given document
+-- at the given FilePath
+createPackageFile :: FileAndContents -> IO ()
+createPackageFile file = do
+  let path     = filePath file
+      contents = fileDoc file
   createDirIfMissing True (takeDirectory path)
   h <- openFile path WriteMode
   hPutStrLn h (render contents)

60543843394d2b94423a9ef6fe45b1cf3317c03d
Brandon Bosman <97637834+B-rando1@users.noreply.github.com>
2026-02-02 10:09:34 -0500

    Apply suggestion from @balacij
    Co-authored-by: balacij <1627302+balacij@users.noreply.github.com>

--- code/drasil-utils/lib/Utils/Drasil/FileIO.hs
+++ code/drasil-utils/lib/Utils/Drasil/FileIO.hs
@@ -2,7 +2,7 @@ module Utils.Drasil.FileIO (createFile) where
 
 import Utils.Drasil.Directory (createDirIfMissing)
 import Text.PrettyPrint.HughesPJ (Doc, render)
-import System.FilePath.Posix (takeDirectory)
+import System.FilePath (takeDirectory)
 import System.IO (hPutStrLn, hClose, openFile, IOMode(WriteMode))
 
 -- | Helper to convert a FileAndContents into a real file with the given document

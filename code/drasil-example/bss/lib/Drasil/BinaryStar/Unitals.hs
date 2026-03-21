5b72d667d7cd645a725c8146f7435c22885062da
Xinlu <yanxinlu@outlook.com>
2026-03-21 11:40:39 -0400

    add complete data constraints with spec parameters and UncertQ
    (cherry picked from commit 63e6445ad260036d07f17da25d0b9a25db9fa5e0)

--- code/drasil-example/bss/lib/Drasil/BinaryStar/Unitals.hs
+++ code/drasil-example/bss/lib/Drasil/BinaryStar/Unitals.hs
@@ -32,7 +32,8 @@ symbols = map dqdWr [mass_1, mass_2, xPos_1, yPos_1, xPos_2, yPos_2,
   xVel_1, yVel_1, xVel_2, yVel_2, xAccel_1, yAccel_1, xAccel_2, yAccel_2,
   xPos_1_0, yPos_1_0, xPos_2_0, yPos_2_0,
   xVel_1_0, yVel_1_0, xVel_2_0, yVel_2_0,
-  tFinal, sepDist]
+  tFinal, sepDist,
+  massMin, massMax, rMax, vMax, tMin, tMax]
   ++ map dqdWr [QP.velocity, QP.position, QP.acceleration, QP.force, QP.time,
      gravitationalConst, QPP.mass]
   ++ map dqdWr constants
@@ -53,7 +54,7 @@ outputs = map dqdWr [xPos_1, yPos_1, xPos_2, yPos_2]
 
 -- | Constants
 constants :: [ConstQDef]
-constants = [gravitationalConstValue]
+constants = gravitationalConstValue : specParamValues
 
 ---------------------------------------------------------
 -- Unital chunks (physical quantities with symbols & units)
@@ -228,19 +229,121 @@ labelx = label "x"
 labely = label "y"
 
 ---------------------------------------------------------
+-- Specification Parameters (Table: Specification Parameter Values)
+-- These define the software constraint bounds.
+---------------------------------------------------------
+
+massMin, massMax, rMax, vMax, tMin, tMax :: UnitalChunk
+
+massMin = uc' "m_min" (nounPhraseSP "minimum stellar mass")
+  (S "lower bound for stellar mass (approximately 0.05 solar masses)")
+  (sub lM (label "min")) Real kilogram
+
+massMax = uc' "m_max" (nounPhraseSP "maximum stellar mass")
+  (S "upper bound for stellar mass (approximately 50 solar masses)")
+  (sub lM (label "max")) Real kilogram
+
+rMax = uc' "r_max" (nounPhraseSP "maximum initial distance")
+  (S "upper bound for initial position magnitude (covers wide binary separations)")
+  (sub lR (label "max")) Real metre
+
+vMax = uc' "v_max" (nounPhraseSP "maximum initial speed")
+  (S "upper bound for initial velocity magnitude (below relativistic threshold)")
+  (sub lV (label "max")) Real velU
+
+tMin = uc' "t_min" (nounPhraseSP "minimum simulation time")
+  (S "lower bound for simulation duration")
+  (sub lT (label "min")) Real second
+
+tMax = uc' "t_max" (nounPhraseSP "maximum simulation time")
+  (S "upper bound for simulation duration")
+  (sub lT (label "max")) Real second
+
+-- | Spec parameter values
+specParamValues :: [ConstQDef]
+specParamValues =
+  [ mkQuantDef massMin (dbl 1.0e29)   -- ~0.05 M_sun
+  , mkQuantDef massMax (dbl 1.0e32)   -- ~50 M_sun
+  , mkQuantDef rMax    (dbl 1.0e13)   -- ~67 AU
+  , mkQuantDef vMax    (dbl 1.0e6)    -- ~0.3% c
+  , mkQuantDef tMin    (dbl 1.0e3)    -- ~17 min
+  , mkQuantDef tMax    (dbl 1.0e10)   -- ~317 yr
+  ]
+
+---------------------------------------------------------
 -- Constraints (input validation)
 ---------------------------------------------------------
 
+-- | Mass constraints: physical (> 0) + software (m_min <= m <= m_max)
 massCon_1, massCon_2 :: ConstrConcept
-massCon_1 = constrained' mass_1 [gtZeroConstr] (dbl 1.0e30)
-massCon_2 = constrained' mass_2 [gtZeroConstr] (dbl 1.0e30)
+massCon_1 = constrained' mass_1
+  [gtZeroConstr,
+   sfwrRange $ Bounded (Inc, sy massMin) (Inc, sy massMax)]
+  (dbl 2.0e30)
 
+massCon_2 = constrained' mass_2
+  [gtZeroConstr,
+   sfwrRange $ Bounded (Inc, sy massMin) (Inc, sy massMax)]
+  (dbl 1.6e30)
+
+-- | Initial position constraints: software (|x| <= r_max)
+xPosCon_1_0, yPosCon_1_0, xPosCon_2_0, yPosCon_2_0 :: ConstrConcept
+xPosCon_1_0 = constrained' xPos_1_0
+  [sfwrRange $ Bounded (Inc, neg (sy rMax)) (Inc, sy rMax)]
+  (dbl 7.5e10)
+
+yPosCon_1_0 = constrained' yPos_1_0
+  [sfwrRange $ Bounded (Inc, neg (sy rMax)) (Inc, sy rMax)]
+  (dbl 0)
+
+xPosCon_2_0 = constrained' xPos_2_0
+  [sfwrRange $ Bounded (Inc, neg (sy rMax)) (Inc, sy rMax)]
+  (dbl (-7.5e10))
+
+yPosCon_2_0 = constrained' yPos_2_0
+  [sfwrRange $ Bounded (Inc, neg (sy rMax)) (Inc, sy rMax)]
+  (dbl 0)
+
+-- | Initial velocity constraints: software (|v| <= v_max)
+xVelCon_1_0, yVelCon_1_0, xVelCon_2_0, yVelCon_2_0 :: ConstrConcept
+xVelCon_1_0 = constrained' xVel_1_0
+  [sfwrRange $ Bounded (Inc, neg (sy vMax)) (Inc, sy vMax)]
+  (dbl 0)
+
+yVelCon_1_0 = constrained' yVel_1_0
+  [sfwrRange $ Bounded (Inc, neg (sy vMax)) (Inc, sy vMax)]
+  (dbl 1.0e4)
+
+xVelCon_2_0 = constrained' xVel_2_0
+  [sfwrRange $ Bounded (Inc, neg (sy vMax)) (Inc, sy vMax)]
+  (dbl 0)
+
+yVelCon_2_0 = constrained' yVel_2_0
+  [sfwrRange $ Bounded (Inc, neg (sy vMax)) (Inc, sy vMax)]
+  (dbl (-1.0e4))
+
+-- | Time constraint: physical (>= 0) + software (t_min <= t <= t_max)
 tFinalCon :: ConstrConcept
-tFinalCon = constrained' tFinal [gtZeroConstr] (dbl 1.0e8)
+tFinalCon = constrained' tFinal
+  [physRange $ UpFrom (Inc, exactDbl 0),
+   sfwrRange $ Bounded (Inc, sy tMin) (Inc, sy tMax)]
+  (dbl 3.15e7)
 
 -- | Input constraints with uncertainty
 inConstraints :: [UncertQ]
-inConstraints = map (`uq` defaultUncrt) [massCon_1, massCon_2, tFinalCon]
+inConstraints =
+  [ uq massCon_1   (uncty 0.05 Nothing)   -- 5%
+  , uq massCon_2   (uncty 0.05 Nothing)   -- 5%
+  , uq xPosCon_1_0 (uncty 0.01 Nothing)   -- 1%
+  , uq yPosCon_1_0 (uncty 0.01 Nothing)   -- 1%
+  , uq xPosCon_2_0 (uncty 0.01 Nothing)   -- 1%
+  , uq yPosCon_2_0 (uncty 0.01 Nothing)   -- 1%
+  , uq xVelCon_1_0 (uncty 0.05 Nothing)   -- 5%
+  , uq yVelCon_1_0 (uncty 0.05 Nothing)   -- 5%
+  , uq xVelCon_2_0 (uncty 0.05 Nothing)   -- 5%
+  , uq yVelCon_2_0 (uncty 0.05 Nothing)   -- 5%
+  , uq tFinalCon   exact                   -- no uncertainty
+  ]
 
 -- | Output constraints (positions have no specific bounds)
 outConstraints :: [UncertQ]

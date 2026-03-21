4887891918196fb4aedac312a7849fc29e415590
Xinlu <yanxinlu@outlook.com>
2026-03-21 14:50:02 -0400

    reuse velocityTM and accelTM
    (cherry picked from commit 77aee2581994acd2ff202d0799d2364c63e1c94b)

--- code/drasil-example/bss/lib/Drasil/BinaryStar/References.hs
+++ code/drasil-example/bss/lib/Drasil/BinaryStar/References.hs
@@ -3,8 +3,9 @@ module Drasil.BinaryStar.References (citations, koothoor2013,
 
 import Language.Drasil (BibRef)
 import Data.Drasil.Citations (parnasClements1986, koothoor2013,
-  smithEtAl2007, smithLai2005, smithKoothoor2016, hibbeler2004)
+  smithEtAl2007, smithLai2005, smithKoothoor2016, hibbeler2004,
+  velocityWiki, accelerationWiki)
 
 citations :: BibRef
 citations = [parnasClements1986, koothoor2013, smithEtAl2007, smithLai2005,
-             smithKoothoor2016, hibbeler2004]
+             smithKoothoor2016, hibbeler2004, velocityWiki, accelerationWiki]
--- code/drasil-example/bss/lib/Drasil/BinaryStar/TMods.hs
+++ code/drasil-example/bss/lib/Drasil/BinaryStar/TMods.hs
@@ -1,4 +1,5 @@
-module Drasil.BinaryStar.TMods (tMods) where
+module Drasil.BinaryStar.TMods (tMods, centerOfMassTM, velocityTM, accelTM,
+  gravLawTM, relPosTM) where
 
 import Prelude hiding (sin, cos, sqrt)
 import Language.Drasil
@@ -6,27 +7,26 @@ import Theory.Drasil
 import qualified Language.Drasil.Sentence.Combinators as S
 import qualified Data.List.NonEmpty as NE
 
-import Data.Drasil.Quantities.Physics (velocity, position, acceleration,
-  force, time, gravitationalConst)
+import Data.Drasil.Quantities.Physics (position, force, gravitationalConst)
 import Data.Drasil.Quantities.PhysicalProperties (mass)
-import Data.Drasil.Theories.Physics (newtonSL)
+import Data.Drasil.Theories.Physics (newtonSL, accelerationTM, velocityTM)
 
 import Drasil.BinaryStar.Unitals (mass_1, mass_2, sepDist,
   xPos_1, yPos_1, xPos_2, yPos_2,
   xPos_1_0, yPos_1_0, xPos_2_0, yPos_2_0)
+import Drasil.BinaryStar.Assumptions (inertialFrame, newtonianGravity,
+  nonzeroSeparation, planar)
+
+-- | TM2 (velocity), TM3 (acceleration), TM4 (Newton's second law)
+-- are reused from drasil-data, following the DblPend pattern.
+accelTM :: TheoryModel
+accelTM = accelerationTM
 
 tMods :: [TheoryModel]
 tMods = [centerOfMassTM, newtonSL, velocityTM, accelTM, gravLawTM, relPosTM]
--- TM4 (Newton's Second Law) is reused from drasil-data as newtonSL.
 
 ---------------------------------------------------------
 -- TM1: Center-of-Mass Constraint (n=2 specialization)
--- In the COM reference frame (Assumption A7), the initial
--- positions must satisfy:
---   m₁·x₁₀ + m₂·x₂₀ = 0
---   m₁·y₁₀ + m₂·y₂₀ = 0
--- This uses ConstraintSet (like SSP's equilibrium TM)
--- to express relational invariants across multiple variables.
 ---------------------------------------------------------
 centerOfMassTM :: TheoryModel
 centerOfMassTM = tmNoRefs (equationalConstraints' centerOfMassCS)
@@ -47,51 +47,13 @@ centerOfMassCS = mkConstraintSet
 
 centerOfMassNote :: Sentence
 centerOfMassNote = foldlSent
-  [S "In the center-of-mass reference frame, the initial",
+  [S "In the center-of-mass reference frame",
+   sParen (refS inertialFrame) `sC`
+   S "the initial",
    plural position, S "of the two stars must satisfy",
    S "the constraint that the weighted sum of", plural position,
    S "is zero, where the weights are the", plural mass,
    S "of each star"]
-
----------------------------------------------------------
--- TM2: Velocity  v(t) = dr(t)/dt
----------------------------------------------------------
-velocityTM :: TheoryModel
-velocityTM = tmNoRefs (equationalModel' velocityQD)
-  "velocity" [velocityNote]
-
-velocityQD :: ModelQDef
-velocityQD = mkQuantDef' velocity
-  (nounPhraseSP "velocity") velocityExpr
-
-velocityExpr :: ModelExpr
-velocityExpr = deriv (sy position) time
-
-velocityNote :: Sentence
-velocityNote = foldlSent
-  [S "Velocity", ch velocity `S.is`
-   S "defined as the time derivative of the",
-   phrase position, S "vector"]
-
----------------------------------------------------------
--- TM3: Acceleration  a(t) = dv(t)/dt
----------------------------------------------------------
-accelTM :: TheoryModel
-accelTM = tmNoRefs (equationalModel' accelQD)
-  "acceleration" [accelNote]
-
-accelQD :: ModelQDef
-accelQD = mkQuantDef' acceleration
-  (nounPhraseSP "acceleration") accelExpr
-
-accelExpr :: ModelExpr
-accelExpr = deriv (sy velocity) time
-
-accelNote :: Sentence
-accelNote = foldlSent
-  [S "Acceleration", ch acceleration `S.is`
-   S "defined as the time derivative of",
-   phrase velocity]
 
 ---------------------------------------------------------
 -- TM5: Newton's Law of Universal Gravitation
@@ -114,7 +76,11 @@ gravLawNote = foldlSent
   [S "The gravitational", phrase force,
    S "between two bodies is proportional to the product of their",
    plural mass `S.and_`
-   S "inversely proportional to the square of the distance between them"]
+   S "inversely proportional to the square of the distance between them.",
+   S "This assumes Newtonian gravitation",
+   sParen (refS newtonianGravity) `S.and_`
+   S "requires the separation distance to be positive",
+   sParen (refS nonzeroSeparation)]
 
 ---------------------------------------------------------
 -- TM6: Relative Position and Separation
@@ -140,4 +106,7 @@ relPosNote = foldlSent
    S "corresponding separation distance (the magnitude of the relative",
    phrase position, S "vector), computed from the",
    phrase position, S "coordinates",
-   ch xPos_1 `sC` ch yPos_1 `sC` ch xPos_2 `sC` ch yPos_2]
+   ch xPos_1 `sC` ch yPos_1 `sC` ch xPos_2 `sC` ch yPos_2 +:+.
+   S "",
+   S "The motion is confined to a 2D plane",
+   sParen (refS planar)]

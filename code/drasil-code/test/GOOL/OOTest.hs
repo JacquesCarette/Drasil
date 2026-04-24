8fff13102b5fed740e897eb06a2d6ca413e9c424
Brandon Bosman <brandobosman@gmail.com>
2026-04-24 14:12:33 -0400

    Minimal working program

--- code/drasil-code/test/GOOL/OOTest.hs
+++ code/drasil-code/test/GOOL/OOTest.hs
@@ -1,8 +1,15 @@
 -- | GOOL OOTest module. Various tests of general object functionality.
 module GOOL.OOTest (ooTest) where
 
-import Drasil.GOOL (OOProg, GSProgram, ProgramSym(..))
+import Drasil.GOOL (OOProg, GSProgram, SMethod, ProgramSym(..), FileSym (..),
+  ModuleSym (..), MethodSym (..), BodySym (..), IOStatement (..), Literal (..))
+import Drasil.Metadata (watermark)
+import Drasil.GProc (BlockSym(..))
 
 ooTest :: (OOProg r) => GSProgram r
 ooTest = prog "OOTest" "Tests various aspects of general object functionality"
-    []
+  [docMod "Tests various aspects of general object functionality" watermark
+  ["Brandon Bosman"] "Apr. 24, 2026" (fileDoc (buildModule "OOTest" [] [ooTestMain] []))]
+
+ooTestMain :: (OOProg r) => SMethod r
+ooTestMain = mainFunction (body [block [printLn $ litString "Hello World"]])

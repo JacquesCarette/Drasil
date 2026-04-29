988f18ec85c229d46a610091ae8169599abd7609
Jason Balaci <jason@balaci.me>
2026-04-28 23:00:34 -0400

    feat(rendering): pretty- & prettyprinter-based `Artifact` rendering

--- code/drasil-build-artifacts/lib/Drasil/Build/Artifacts/Render.hs
+++ code/drasil-build-artifacts/lib/Drasil/Build/Artifacts/Render.hs
@@ -2,11 +2,21 @@ module Drasil.Build.Artifacts.Render (
   Renderable(..)
 ) where
 
+import Text.PrettyPrint qualified as PLegacy
+import Prettyprinter qualified as PNew
+import Prettyprinter.Render.Text (renderIO)
+import System.IO (withFile, IOMode(WriteMode))
+
+-- | Render an abstract document representation efficiently.
 class Renderable doc where
   renderToFile :: FilePath -> doc -> IO ()
 
--- instance Renderable Pretty.Doc where
--- ...
+instance Renderable PLegacy.Doc where
+  -- Does conversion to `String` and then does plain `String -> IO ()` writing.
+  renderToFile fp = writeFile fp . PLegacy.render
 
--- instance Renderable Prettyprinter.Doc where
--- ...
+instance Renderable (PNew.Doc ann) where
+  -- `renderIO` skips intermediate representation:
+  -- <https://hackage-content.haskell.org/package/prettyprinter-1.7.2/docs/Prettyprinter-Render-Text.html#v:renderIO>
+  renderToFile fp d = withFile fp WriteMode $ \h ->
+    renderIO h (PNew.layoutPretty PNew.defaultLayoutOptions d)

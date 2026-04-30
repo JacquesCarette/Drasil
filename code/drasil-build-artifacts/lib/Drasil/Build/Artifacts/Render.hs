60a9cb0e473c1f52480163d1ce224c6ae6860b40
Jason Balaci <jason@balaci.me>
2026-04-30 14:44:02 -0400

    fix(artifact-renderer): match previous trailing newspace policy: expect Doc does not contain one, add one when file-writing

--- code/drasil-build-artifacts/lib/Drasil/Build/Artifacts/Render.hs
+++ code/drasil-build-artifacts/lib/Drasil/Build/Artifacts/Render.hs
@@ -8,17 +8,17 @@ import Prettyprinter.Render.Text (renderIO)
 import System.IO (IOMode (WriteMode), withFile)
 import Text.PrettyPrint qualified as PLegacy
 
--- | Render a document and write it to a file. Useful for rendering and writing
--- immediately.
+-- | Render a document and write it to a file (with a trailing newline always
+-- added).
 class Renderable doc where
   renderToFile :: FilePath -> doc -> IO ()
 
 instance Renderable PLegacy.Doc where
   -- Does conversion to `String` and then does plain `String -> IO ()` writing.
-  renderToFile fp = writeFile fp . PLegacy.render
+  renderToFile fp = writeFile fp . PLegacy.render . (PLegacy.$+$ PLegacy.text "")
 
 instance Renderable (PNew.Doc ann) where
   -- `renderIO` skips intermediate representations before writing to disk:
   -- <https://hackage-content.haskell.org/package/prettyprinter-1.7.2/docs/Prettyprinter-Render-Text.html#v:renderIO>
   renderToFile fp d = withFile fp WriteMode $ \h ->
-    renderIO h (PNew.layoutPretty PNew.defaultLayoutOptions d)
+    renderIO h (PNew.layoutPretty PNew.defaultLayoutOptions $ d PNew.<> PNew.line)

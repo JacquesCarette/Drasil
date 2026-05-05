ce0ab5645d627299742b1d8f3efe10729f86c815
Jason Balaci <jason@balaci.me>
2026-05-05 12:02:21 -0400

    refactor(build-artifacts): move `Document.hs` to `Legacy/` sub-folder

--- code/drasil-build-artifacts/lib/Drasil/Build/Artifacts.hs
+++ code/drasil-build-artifacts/lib/Drasil/Build/Artifacts.hs
@@ -1,16 +1,16 @@
 module Drasil.Build.Artifacts (
-  module Drasil.Build.Artifacts.Document,
   module Drasil.Build.Artifacts.FileLayout,
   module Drasil.Build.Artifacts.Legacy.Classes,
+  module Drasil.Build.Artifacts.Legacy.Document,
   module Drasil.Build.Artifacts.Legacy.Directory,
   module Drasil.Build.Artifacts.Legacy.FileData,
   module Drasil.Build.Artifacts.Legacy.FileIO,
   module Drasil.Build.Artifacts.Legacy.FilePath
 ) where
 
-import Drasil.Build.Artifacts.Document
 import Drasil.Build.Artifacts.FileLayout
 import Drasil.Build.Artifacts.Legacy.Classes
+import Drasil.Build.Artifacts.Legacy.Document
 import Drasil.Build.Artifacts.Legacy.Directory
 import Drasil.Build.Artifacts.Legacy.FileData
 import Drasil.Build.Artifacts.Legacy.FileIO

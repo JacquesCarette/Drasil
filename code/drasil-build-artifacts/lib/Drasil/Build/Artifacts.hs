5a30e163447873330cf9e6905be6ce998969791a
Jason Balaci <jason@balaci.me>
2026-05-08 14:51:11 -0400

    feat(filepaths): add a common `OsPath`: local path

--- code/drasil-build-artifacts/lib/Drasil/Build/Artifacts.hs
+++ code/drasil-build-artifacts/lib/Drasil/Build/Artifacts.hs
@@ -1,8 +1,10 @@
 module Drasil.Build.Artifacts
-  ( module Drasil.Build.Artifacts.FileLayout,
+  ( module Drasil.Build.Artifacts.CommonPaths,
+    module Drasil.Build.Artifacts.FileLayout,
     module Drasil.Build.Artifacts.FilePath
   )
 where
 
+import Drasil.Build.Artifacts.CommonPaths
 import Drasil.Build.Artifacts.FileLayout
 import Drasil.Build.Artifacts.FilePath

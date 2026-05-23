c52c19836c008cf952058db6e77fdb43ec571d2f
Jason Balaci <jason@balaci.me>
2026-05-23 17:41:20 -0400

    refactor: unify `Main` program entries of all case studies

--- code/drasil-example/sglpend/app/Main.hs
+++ code/drasil-example/sglpend/app/Main.hs
@@ -1,12 +1,8 @@
 module Main (main) where
 
-import GHC.IO.Encoding
-
-import Drasil.Generator (exportSmithEtAlSrs)
+import Drasil.Generator (caseStudyMainSRS)
 
 import Drasil.SglPend.Body (mkSRS, si)
 
 main :: IO ()
-main = do
-  setLocaleEncoding utf8
-  exportSmithEtAlSrs si mkSRS "SglPend_SRS"
+main = caseStudyMainSRS si mkSRS "SglPend_SRS"

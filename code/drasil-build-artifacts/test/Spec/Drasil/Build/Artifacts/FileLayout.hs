07dad6e007d1ee02dbe87155538f999aee886896
Jason Balaci <jason@balaci.me>
2026-05-28 13:38:28 -0400

    tests(build-artifacts): implement basic testing suite for all `Renderable` instances to ensure they write files with trailing newlines

--- code/drasil-build-artifacts/test/Spec/Drasil/Build/Artifacts/FileLayout.hs
+++ code/drasil-build-artifacts/test/Spec/Drasil/Build/Artifacts/FileLayout.hs
@@ -2,16 +2,22 @@
 
 module Spec.Drasil.Build.Artifacts.FileLayout (fileLayoutTests) where
 
-import Drasil.Build.Artifacts (FileLayout, directory, file, goldenTest, goldenTestingGroup, ps)
+import Data.ByteString.Lazy.Char8 qualified as LB
+import Data.Text qualified as T
 import Prettyprinter (Pretty (..))
 import System.OsPath (osp)
+import Text.PrettyPrint qualified as PLegacy
 import Test.Tasty (TestTree, testGroup)
+
+import Drasil.Build.Artifacts (FileLayout, directory, file, goldenTest,
+  goldenTestingGroup, ps)
 
 fileLayoutTests :: TestTree
 fileLayoutTests =
   testGroup
     "Drasil.Build.Artifacts.FileLayout"
-    [ writeFilesTests
+    [ writeFilesTests,
+      renderToFileTests
     ]
 
 writeFilesTests :: TestTree
@@ -26,6 +32,37 @@ writeFilesTests =
           goldenTest "Single file" helloWorldFile
         ]
     ]
+
+renderToFileTests :: TestTree
+renderToFileTests =
+  testGroup
+    "renderToFile"
+    [ goldenTestingGroup
+        [osp|test/build/renderToFile|]
+        [osp|test/golden/renderToFile|]
+        "Golden Tests"
+        [ goldenTest "PLegacy.Doc ends with newline" plegacyDocFile,
+          goldenTest "PNew.Doc ends with newline" pnewDocFile,
+          goldenTest "String ends with newline" stringFile,
+          goldenTest "T.Text ends with newline" textFile,
+          goldenTest "LB.ByteString ends with newline" byteStringFile
+        ]
+    ]
+
+plegacyDocFile :: FileLayout
+plegacyDocFile = file [ps|plegacy-doc.txt|] (PLegacy.text "plegacy-doc")
+
+pnewDocFile :: FileLayout
+pnewDocFile = file [ps|pnew-doc.txt|] (pretty "pnew-doc")
+
+stringFile :: FileLayout
+stringFile = file [ps|string.txt|] ("string" :: String)
+
+textFile :: FileLayout
+textFile = file [ps|text.txt|] (T.pack "text")
+
+byteStringFile :: FileLayout
+byteStringFile = file [ps|bytestring.txt|] (LB.pack "bytestring")
 
 nestedFiles :: FileLayout
 nestedFiles =

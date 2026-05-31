f7b257c8c4052866e1b8020a21d820254105f396
Jason Balaci <jason@balaci.me>
2026-05-31 18:27:09 -0400

    refactor: break `Drasil.Assets` module in two

--- code/drasil-assets/lib/Drasil/Assets.hs
+++ code/drasil-assets/lib/Drasil/Assets.hs
@@ -1,65 +1,9 @@
-{-# LANGUAGE DeriveLift #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TemplateHaskellQuotes #-}
-
 -- | External file 'Asset's.
 module Drasil.Assets
-  ( -- * Assets
-    Asset,
-
-    -- ** Constructors
-    readAsset,
-
-    -- ** Accessors
-    content,
-    description,
-
-    -- ** File Conversion
-    toFile,
+  ( module Drasil.Assets.Core,
+    module Drasil.Assets.FromLocalPath,
   )
 where
 
-import Data.Text (Text)
-import Data.ByteString qualified as B (ByteString)
-import Drasil.Build.Artifacts (FileLayout, PathSegment, exactFile)
-import Language.Haskell.TH (Code (examineCode), Q, liftCode, runIO)
-import Language.Haskell.TH.Syntax (Lift, addDependentFile, makeRelativeToProject)
-import System.Directory.OsPath (doesPathExist)
-import System.File.OsPath (readFile')
-import System.OsPath (OsPath, decodeUtf, encodeUtf)
-import Prelude hiding (readFile)
-
--- | An arbitrary file asset; a 'B.ByteString' with a description.
-data Asset = Asset Text B.ByteString
-  deriving (Lift)
-
--- | Get the description of an 'Asset'.
-description :: Asset -> Text
-description (Asset desc _) = desc
-
--- | Get the raw 'ByteString' content of an 'Asset'.
-content :: Asset -> B.ByteString
-content (Asset _ c) = c
-
--- | Convert an 'Asset' to a 'FileLayout' node given a 'PathSegment' (file name)
--- it is to be written to.
-toFile :: Asset -> PathSegment -> FileLayout
-toFile a = flip exactFile (content a)
-
--- | Read a file from disk, providing a description of the asset.
-readAsset :: OsPath -> Text -> Code Q Asset
-readAsset filePath desc = liftCode $ do
-  strPath <- runIO (decodeUtf filePath)
-  -- FIXME: HACK: This relies on decoding the path, making it relative, and then
-  -- re-encoding. It works, but, when OsPath and OsString is propagated through
-  -- GHC core libs more, this will break.
-  absPathStr <- makeRelativeToProject strPath
-  absPath <- runIO (encodeUtf absPathStr)
-  exists <- runIO (doesPathExist absPath)
-  if exists
-    then do
-      fc <- runIO (readFile' absPath)
-      let a = Asset desc fc
-      addDependentFile absPathStr
-      examineCode [||a||]
-    else error $ "Asset file lookup failed. File does not exist: " ++ absPathStr
+import Drasil.Assets.Core
+import Drasil.Assets.FromLocalPath

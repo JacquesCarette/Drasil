d700f1fa08019b4f40b25b76b03e1418c83a4c55
D. Jarabek <102425078+djarabek@users.noreply.github.com>
2026-06-09 11:48:14 -0400

    refactor(data-formats-CSV): flip parameters to `renderCSV`
    This order makes more sense for the soon to be added `renderJSON`
    (as it is recursive), and the APIs should be consistent.

--- code/drasil-data-formats/test/Spec/Drasil/Data/Formats/CSV.hs
+++ code/drasil-data-formats/test/Spec/Drasil/Data/Formats/CSV.hs
@@ -112,16 +112,16 @@ renderCSVTests =
         "Golden Tests"
         [
           goldenTest "simpleCSV minimal" $
-            file [ps|simple_m.csv|] $ renderCSV simpleCSV minimal,
+            file [ps|simple_m.csv|] $ renderCSV minimal simpleCSV,
           goldenTest "simpleCSVnoH minimal" $
-            file [ps|simple_m_nh.csv|] $ renderCSV simpleCSVnoH minimal,
+            file [ps|simple_m_nh.csv|] $ renderCSV minimal simpleCSVnoH,
           goldenTest "simpleCSV everywhere" $
-            file [ps|simple_e.csv|] $ renderCSV simpleCSV everywhere,
+            file [ps|simple_e.csv|] $ renderCSV everywhere simpleCSV,
           goldenTest "simpleCSVnoH everywhere" $
-            file [ps|simple_e_nh.csv|] $ renderCSV simpleCSVnoH everywhere,
+            file [ps|simple_e_nh.csv|] $ renderCSV everywhere simpleCSVnoH,
           goldenTest "complexCSV minimal" $
-            file [ps|complex_m.csv|] $ renderCSV complexCSV minimal,
+            file [ps|complex_m.csv|] $ renderCSV minimal complexCSV,
           goldenTest "complexCSV everywhere" $
-            file [ps|complex_e.csv|] $ renderCSV complexCSV everywhere
+            file [ps|complex_e.csv|] $ renderCSV everywhere complexCSV
         ]
     ]

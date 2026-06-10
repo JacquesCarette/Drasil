de611d9b17b1372169ab9627b65c3abbc6e4e480
Jason Balaci <jason@balaci.me>
2026-06-10 13:52:33 -0400

    refactor(`-lesson-plan`): simplify code related to `Section` rendering

--- code/drasil-lesson-plan/lib/Drasil/LessonPlan/Document.hs
+++ code/drasil-lesson-plan/lib/Drasil/LessonPlan/Document.hs
@@ -25,34 +25,25 @@ data LsnChapter = Intro [Contents]
                 | BibSec
                 | Apndx [Contents]
 
--- TODO: Work on detail structure of Lesson Plan
+-- * 'LsnDesc' 'Section' eliminators
 
--- * Section Constructors
-
--- | Section constructors for the lesson plan documents/jupyter notebooks.
--- should be fixed once we have a more concrete idea of the notebook structure
--- TODO: Remove [Section] since the structure of lesson plans should not be nested
--- maybe add a new type Chapter for lesson plans?
+-- | Section constructors for the 'LsnDesc' sections.
 intro, learnObj, review, caseProb, summary, appendix, reference, example :: [Contents] -> [Section] -> Section
-intro     cs ss = section (titleize Doc.introduction) cs ss introLabel
-learnObj  cs ss = section (titleize' Doc.learnObj)    cs ss learnObjLabel
-review    cs ss = section (titleize Doc.review)       cs ss reviewLabel
-caseProb  cs ss = section (titleize Doc.caseProb)     cs ss caseProbLabel
-example   cs ss = section (titleize Doc.example)      cs ss exampleLabel
-summary   cs ss = section (titleize Doc.summary)      cs ss summaryLabel
-appendix  cs ss = section (titleize Doc.appendix)     cs ss appendixLabel
-reference cs ss = section (titleize' Doc.reference)   cs ss referenceLabel
+intro     = mkLsnSec  "Intro"      Doc.introduction
+learnObj  = mkLsnSec' "LearnObj"   Doc.learnObj
+review    = mkLsnSec  "Review"     Doc.review
+caseProb  = mkLsnSec  "CaseProb"   Doc.caseProb
+example   = mkLsnSec  "Example"    Doc.example
+summary   = mkLsnSec  "Summary"    Doc.summary
+appendix  = mkLsnSec  "Appendix"   Doc.appendix
+reference = mkLsnSec' "References" Doc.reference
 
--- * Section References
+-- | Internal: Create a section of the lesson plan. Title is singular.
+mkLsnSec :: Idea c => String -> c -> [Contents] -> [Section] -> Section
+mkLsnSec r c cs ss = section t cs ss (makeSecRef r t)
+  where t = titleize c
 
--- | Individual section reference labels. Used in creating example sections for the notebook.
-introLabel, learnObjLabel, referenceLabel,
-  reviewLabel, caseProbLabel, appendixLabel, summaryLabel, exampleLabel :: Reference
-introLabel          = makeSecRef "Intro"            $ titleize Doc.introduction
-learnObjLabel       = makeSecRef "LearnObj"         $ titleize' Doc.learnObj
-referenceLabel      = makeSecRef "References"       $ titleize' Doc.reference
-reviewLabel         = makeSecRef "Review"           $ titleize Doc.review
-caseProbLabel       = makeSecRef "CaseProb"         $ titleize Doc.caseProb
-appendixLabel       = makeSecRef "Appendix"         $ titleize Doc.appendix
-summaryLabel        = makeSecRef "Summary"          $ titleize Doc.summary
-exampleLabel        = makeSecRef "Example"          $ titleize Doc.example
+-- | Internal: Create a section of the lesson plan. Title is made plural.
+mkLsnSec' :: Idea c => String -> c -> [Contents] -> [Section] -> Section
+mkLsnSec' r c cs ss = section t cs ss (makeSecRef r t)
+  where t = titleize' c

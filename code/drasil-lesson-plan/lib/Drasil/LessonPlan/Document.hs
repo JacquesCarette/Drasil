b1bb4e3cc9c34869b297482a53506dc3e4cb37b5
Jason Balaci <jason@balaci.me>
2026-06-10 13:54:42 -0400

    refactor(`-lesson-plan`): move more code into `Renderer`

--- code/drasil-lesson-plan/lib/Drasil/LessonPlan/Document.hs
+++ code/drasil-lesson-plan/lib/Drasil/LessonPlan/Document.hs
@@ -2,15 +2,9 @@ module Drasil.LessonPlan.Document (
   -- * Lesson Plan
   LsnDesc, LsnChapter(..),
   -- ** Chapter Types
-  -- ** Constructors
-  intro, learnObj, caseProb, example, appendix, review, reference, summary
 ) where
 
-import Language.Drasil
 import Language.Drasil.Document
-
-import qualified Drasil.Metadata.Documentation as Doc (caseProb, introduction,
-  learnObj, review, summary, example, appendix, reference)
 
 -- * Lesson Chapter Types
 
@@ -24,26 +18,3 @@ data LsnChapter = Intro [Contents]
                 | Smmry [Contents]
                 | BibSec
                 | Apndx [Contents]
-
--- * 'LsnDesc' 'Section' eliminators
-
--- | Section constructors for the 'LsnDesc' sections.
-intro, learnObj, review, caseProb, summary, appendix, reference, example :: [Contents] -> [Section] -> Section
-intro     = mkLsnSec  "Intro"      Doc.introduction
-learnObj  = mkLsnSec' "LearnObj"   Doc.learnObj
-review    = mkLsnSec  "Review"     Doc.review
-caseProb  = mkLsnSec  "CaseProb"   Doc.caseProb
-example   = mkLsnSec  "Example"    Doc.example
-summary   = mkLsnSec  "Summary"    Doc.summary
-appendix  = mkLsnSec  "Appendix"   Doc.appendix
-reference = mkLsnSec' "References" Doc.reference
-
--- | Internal: Create a section of the lesson plan. Title is singular.
-mkLsnSec :: Idea c => String -> c -> [Contents] -> [Section] -> Section
-mkLsnSec r c cs ss = section t cs ss (makeSecRef r t)
-  where t = titleize c
-
--- | Internal: Create a section of the lesson plan. Title is made plural.
-mkLsnSec' :: Idea c => String -> c -> [Contents] -> [Section] -> Section
-mkLsnSec' r c cs ss = section t cs ss (makeSecRef r t)
-  where t = titleize' c
--- code/drasil-lesson-plan/lib/Drasil/LessonPlan/Renderer.hs
+++ code/drasil-lesson-plan/lib/Drasil/LessonPlan/Renderer.hs
@@ -4,14 +4,16 @@ module Drasil.LessonPlan.Renderer (mkNb) where
 import Control.Lens ((^.))
 
 import Drasil.Database (ChunkDB)
-import Language.Drasil (Sentence(S), CI, BibRef,
-  foldlList, SepType(Comma), FoldType(List), fullName)
-import Language.Drasil.Document (Section, Document(Notebook), Contents(UlC), ulcc, RawContent(Bib))
+import Language.Drasil (Sentence(S), CI, BibRef, foldlList, SepType(Comma),
+  FoldType(List), fullName, Idea, titleize, titleize')
+import Language.Drasil.Document (Section, Document(Notebook), Contents(UlC),
+  ulcc, RawContent(Bib), section, makeSecRef)
 import Drasil.System (LessonPlan, HasSystemMeta(..))
 import Drasil.Metadata.Documentation (notebook)
+import qualified Drasil.Metadata.Documentation as Doc (caseProb, introduction,
+  learnObj, review, summary, example, appendix, reference)
 
-import Drasil.LessonPlan.Document (LsnDesc, LsnChapter(..),
-  intro, learnObj, caseProb, example, appendix, review, reference, summary)
+import Drasil.LessonPlan.Document (LsnDesc, LsnChapter(..))
 import Drasil.LessonPlan.ExtractBib (extractBib)
 
 -- | Creates a notebook from a lesson description and system information.
@@ -38,3 +40,26 @@ mkSections si dd = map doit dd
 -- | Helper for making the 'Bibliography' section.
 mkBib :: BibRef -> Section
 mkBib bib = reference [UlC $ ulcc (Bib bib)] []
+
+-- * 'LsnDesc' 'Section' eliminators
+
+-- | Section constructors for the 'LsnDesc' sections.
+intro, learnObj, review, caseProb, summary, appendix, reference, example :: [Contents] -> [Section] -> Section
+intro     = mkLsnSec  "Intro"      Doc.introduction
+learnObj  = mkLsnSec' "LearnObj"   Doc.learnObj
+review    = mkLsnSec  "Review"     Doc.review
+caseProb  = mkLsnSec  "CaseProb"   Doc.caseProb
+example   = mkLsnSec  "Example"    Doc.example
+summary   = mkLsnSec  "Summary"    Doc.summary
+appendix  = mkLsnSec  "Appendix"   Doc.appendix
+reference = mkLsnSec' "References" Doc.reference
+
+-- | Internal: Create a section of the lesson plan. Title is singular.
+mkLsnSec :: Idea c => String -> c -> [Contents] -> [Section] -> Section
+mkLsnSec r c cs ss = section t cs ss (makeSecRef r t)
+  where t = titleize c
+
+-- | Internal: Create a section of the lesson plan. Title is made plural.
+mkLsnSec' :: Idea c => String -> c -> [Contents] -> [Section] -> Section
+mkLsnSec' r c cs ss = section t cs ss (makeSecRef r t)
+  where t = titleize' c

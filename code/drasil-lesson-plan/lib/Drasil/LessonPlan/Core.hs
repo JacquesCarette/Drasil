addc34e6cce1d4484398999f37eb05dd7b1624bc
Jason Balaci <jason@balaci.me>
2026-06-10 12:32:28 -0400

    chore(`drasil-lesson-plan`): remove unused code

--- code/drasil-lesson-plan/lib/Drasil/LessonPlan/Core.hs
+++ code/drasil-lesson-plan/lib/Drasil/LessonPlan/Core.hs
@@ -5,7 +5,6 @@ module Drasil.LessonPlan.Core (
   Example(..), Smmry(..), Apndx(..)
 ) where
 
-import Data.Generics.Multiplate (Multiplate(multiplate, mkPlate))
 import Language.Drasil.Document (Contents, Section)
 
 -- * Lesson Chapter Types
@@ -43,37 +42,3 @@ newtype Smmry = SmmryProg [Contents]
 
 -- ** Appendix
 newtype Apndx = ApndxProg [Contents]
-
--- * Multiplate Definition and Type
-
-data DLPlate f = DLPlate {
-  lsnChap :: LsnChapter -> f LsnChapter,
-  intro :: Intro -> f Intro,
-  learnObj :: LearnObj -> f LearnObj,
-  review :: Review -> f Review,
-  caseProb :: CaseProb -> f CaseProb,
-  example :: Example -> f Example,
-  smmry :: Smmry -> f Smmry,
-  apndx :: Apndx -> f Apndx
-}
-
-instance Multiplate DLPlate where
-  multiplate p = DLPlate lc introd lrnObj rvw csProb exmp smry aps where
-    lc (Intro x) = Intro <$> intro p x
-    lc (LearnObj x) = LearnObj <$> learnObj p x
-    lc (Review x) = Review <$> review p x
-    lc (CaseProb x) = CaseProb <$> caseProb p x
-    lc (Example x) = Example <$> example p x
-    lc (Smmry x) = Smmry <$> smmry p x
-    lc (Apndx x) = Apndx <$> apndx p x
-    lc BibSec = pure BibSec
-
-    introd (IntrodProg con) = pure $ IntrodProg con
-    lrnObj (LrnObjProg con) = pure $ LrnObjProg con
-    rvw (ReviewProg con secs) = pure $ ReviewProg con secs
-    csProb (CaseProbProg con secs) = pure $ CaseProbProg con secs
-    exmp (ExampleProg con) = pure $ ExampleProg con
-    smry (SmmryProg con) = pure $ SmmryProg con
-    aps (ApndxProg con) = pure $ ApndxProg con
-  mkPlate b = DLPlate (b lsnChap) (b intro) (b learnObj) (b review)
-    (b caseProb) (b example) (b smmry) (b apndx)

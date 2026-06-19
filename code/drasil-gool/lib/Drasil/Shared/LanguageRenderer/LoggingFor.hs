fe3de4db646c1a4f9fe43340c89a1a4d8f14b9d3
Brandon Bosman <brandobosman@gmail.com>
2026-06-19 12:36:57 -0400

    WIP roughing-in logging architecture

--- code/drasil-gool/lib/Drasil/Shared/LanguageRenderer/LoggingFor.hs
+++ code/drasil-gool/lib/Drasil/Shared/LanguageRenderer/LoggingFor.hs
@@ -1,15 +1,22 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 -- | MVP renderer for logging statements.
 
 module Drasil.Shared.LanguageRenderer.LoggingFor (LoggingFor(..)) where
 
-import Drasil.Shared.InterfaceCommon (VSType, TypeSym(..), VariableSym(..),
-  VariableValue(..), ValueSym(..), Literal(..), IndexTranslator(..), Array(..),
-  VariableElim(..))
+import Prelude hiding (print)
+
+import Control.Lens (zoom)
+import Control.Monad.Reader
+
+import Drasil.Shared.InterfaceCommon
+import Drasil.Shared.State
 import Drasil.GOOL.InterfaceGOOL (OOTypeSym(..), OOVariableSym(..), SelfSym(..),
   InstanceVarSelfSym(..))
 import Drasil.Shared.AST (TypeData(..), td)
@@ -27,151 +34,175 @@ import Text.PrettyPrint.HughesPJ (Doc, text, empty, comma, space, brackets,
 import qualified Text.PrettyPrint.HughesPJ as P (char, integer, float, double)
 import Data.Kind (Type)
 
-newtype LoggingFor (lang :: Type -> Type) a = LC {unLC :: a} deriving Functor
+newtype LoggingFor (lang :: Type -> Type) a = LC {unLC :: lang a}
+  deriving newtype (Functor, Applicative, Monad)
 
-instance Applicative (LoggingFor lang) where
-  pure = LC
-  (LC f) <*> (LC x) = LC (f x)
+instance (VariableSym lang) => VariableSym (LoggingFor lang) where
+  type Variable (LoggingFor lang) = Variable lang
 
-instance Monad (LoggingFor lang) where
-  LC x >>= f = f x
+instance (ValueSym lang) => ValueSym (LoggingFor lang) where
+  type Value (LoggingFor lang) = Value lang
 
-instance VariableSym (LoggingFor lang) where
-  type Variable (LoggingFor lang) = Doc
-  var n _ = return $ return $ text n
-  constant n _ = return $ return $ text n
-  extVar l n _ = return $ return $ text l <> dot <> text n
+instance StatementSym lang => StatementSym (LoggingFor lang) where
+  type Statement (LoggingFor lang) = Block lang
 
-instance OOVariableSym (LoggingFor lang) where
-  classVar = var
-  classConst = constant
-  classVarAccess cls vr = do
-    cls' <- cls
-    vr' <- vr
-    let clsDoc = (typeDoc . unLC) cls'
-        vrDoc = unLC vr'
-    return $ return $ clsDoc <> dot <> vrDoc
-  extClassVarAccess = classVarAccess
-  instanceVarAccess ob vr = do
-    ob' <- ob
-    vr' <- vr
-    return $ return $ unLC ob' <> dot <> unLC vr'
+instance (BlockSym lang, AssignStatement lang, IOStatement lang, Literal lang, VariableElim lang) => AssignStatement (LoggingFor lang) where
+  (&-=) = _
+  (&+=) = _
+  (&++) = _
+  (&--) = _
+  assign x e = do
+    modName <- zoom lensMStoFS getModuleName
+    -- Total vibes here: not sure if the state
+    -- needs to work like this, but this *feels* less dubious...
+    LC <$> block
+      [ assign (unLC <$> x) (unLC <$> e)
+      , do
+          x' <- variableName . unLC <$> zoom lensMStoVS x
+          print $ litString $ "var '" <> x' <> "' assigned"
+      , print (unLC <$> e)
+      , printLn $ litString $ " in module " <> modName
+      ]
 
-instance SelfSym (LoggingFor CodeInfoOO) where
-  self = return $ return empty
+-- instance Applicative (LoggingFor lang) where
+--   pure = LC
+--   (LC f) <*> (LC x) = LC (f x)
 
-instance InstanceVarSelfSym (LoggingFor CodeInfoOO) where
-  instanceVarSelf _ = return $ return empty
+-- instance Monad (LoggingFor lang) where
+--   LC x >>= f = f x
 
-instance SelfSym (LoggingFor JavaCode) where
-  self = return $ return $ text "this"
+-- instance VariableSym (LoggingFor lang) where
+--   type Variable (LoggingFor lang) = Doc
+--   var n _ = return $ return $ text n
+--   constant n _ = return $ return $ text n
+--   extVar l n _ = return $ return $ text l <> dot <> text n
 
-instance InstanceVarSelfSym (LoggingFor JavaCode) where
-  instanceVarSelf vr = do
-    vr' <- vr
-    self' <- self @(LoggingFor JavaCode)
-    return $ return $ unLC self' <> dot <> unLC vr'
+-- instance OOVariableSym (LoggingFor lang) where
+--   classVar = var
+--   classConst = constant
+--   classVarAccess cls vr = do
+--     cls' <- cls
+--     vr' <- vr
+--     let clsDoc = (typeDoc . unLC) cls'
+--         vrDoc = unLC vr'
+--     return $ return $ clsDoc <> dot <> vrDoc
+--   extClassVarAccess = classVarAccess
+--   instanceVarAccess ob vr = do
+--     ob' <- ob
+--     vr' <- vr
+--     return $ return $ unLC ob' <> dot <> unLC vr'
 
-instance SelfSym (LoggingFor CSharpCode) where
-  self = return $ return $ text "this"
+-- instance SelfSym (LoggingFor CodeInfoOO) where
+--   self = return $ return empty
 
-instance InstanceVarSelfSym (LoggingFor CSharpCode) where
-  instanceVarSelf vr = do
-    vr' <- vr
-    self' <- self @(LoggingFor CSharpCode)
-    return $ return $ unLC self' <> dot <> unLC vr'
+-- instance InstanceVarSelfSym (LoggingFor CodeInfoOO) where
+--   instanceVarSelf _ = return $ return empty
 
-instance SelfSym (LoggingFor (CppCode CppSrcCode CppHdrCode)) where
-  self = return $ return $ text "self"
+-- instance SelfSym (LoggingFor JavaCode) where
+--   self = return $ return $ text "this"
 
-instance InstanceVarSelfSym (LoggingFor (CppCode CppSrcCode CppHdrCode)) where
-  instanceVarSelf vr = do
-    vr' <- vr
-    self' <- self @(LoggingFor (CppCode CppSrcCode CppHdrCode))
-    return $ return $ unLC self' <> text "->" <> unLC vr'
+-- instance InstanceVarSelfSym (LoggingFor JavaCode) where
+--   instanceVarSelf vr = do
+--     vr' <- vr
+--     self' <- self @(LoggingFor JavaCode)
+--     return $ return $ unLC self' <> dot <> unLC vr'
 
-instance SelfSym (LoggingFor PythonCode) where
-  self = return $ return $ text "self"
+-- instance SelfSym (LoggingFor CSharpCode) where
+--   self = return $ return $ text "this"
 
-instance InstanceVarSelfSym (LoggingFor PythonCode) where
-  instanceVarSelf vr = do
-    vr' <- vr
-    self' <- self @(LoggingFor PythonCode)
-    return $ return $ unLC self' <> dot <> unLC vr'
+-- instance InstanceVarSelfSym (LoggingFor CSharpCode) where
+--   instanceVarSelf vr = do
+--     vr' <- vr
+--     self' <- self @(LoggingFor CSharpCode)
+--     return $ return $ unLC self' <> dot <> unLC vr'
 
-instance SelfSym (LoggingFor SwiftCode) where
-  self = return $ return $ text "self"
+-- instance SelfSym (LoggingFor (CppCode CppSrcCode CppHdrCode)) where
+--   self = return $ return $ text "self"
 
-instance InstanceVarSelfSym (LoggingFor SwiftCode) where
-  instanceVarSelf vr = do
-    vr' <- vr
-    self' <- self @(LoggingFor SwiftCode)
-    return $ return $ unLC self' <> dot <> unLC vr'
+-- instance InstanceVarSelfSym (LoggingFor (CppCode CppSrcCode CppHdrCode)) where
+--   instanceVarSelf vr = do
+--     vr' <- vr
+--     self' <- self @(LoggingFor (CppCode CppSrcCode CppHdrCode))
+--     return $ return $ unLC self' <> text "->" <> unLC vr'
 
-instance ValueSym (LoggingFor lang) where
-  type Value (LoggingFor lang) = Doc
-  valueType = error "Not implemented"
+-- instance SelfSym (LoggingFor PythonCode) where
+--   self = return $ return $ text "self"
 
-instance TypeSym (LoggingFor lang) where
-  bool = bool
-  int = int
-  float = float
-  double = double
-  char = char
-  string = string
-  infile = infile
-  outfile = outfile
-  referenceType = id -- TODO [Brandon Bosman, 06/18/2026]: This should be
-                     -- in a separate typeclass and implemented on a
-                     -- per-language basis, (similar to `self`),
-                     -- but I don't want to do that right now
-  listType = listType
-  setType = setType
-  arrayType = arrayType
-  innerType = innerType
-  funcType = funcType
-  void = void
+-- instance InstanceVarSelfSym (LoggingFor PythonCode) where
+--   instanceVarSelf vr = do
+--     vr' <- vr
+--     self' <- self @(LoggingFor PythonCode)
+--     return $ return $ unLC self' <> dot <> unLC vr'
 
-instance OOTypeSym (LoggingFor lang) where
-  obj nm = typeFromData (Object nm) ("Object<" ++ nm ++ ">")
+-- instance SelfSym (LoggingFor SwiftCode) where
+--   self = return $ return $ text "self"
 
-typeFromData :: CodeType -> String -> VSType (LoggingFor lang)
-typeFromData tp str = return $ return $ td tp str (text str)
+-- instance InstanceVarSelfSym (LoggingFor SwiftCode) where
+--   instanceVarSelf vr = do
+--     vr' <- vr
+--     self' <- self @(LoggingFor SwiftCode)
+--     return $ return $ unLC self' <> dot <> unLC vr'
 
-instance Literal (LoggingFor lang) where
-  litTrue = litString "True"
-  litFalse = litString "False"
-  litChar = return . return . P.char
-  litDouble = return . return . P.double
-  litFloat = return . return . P.float
-  litInt = return . return . P.integer
-  litString = return . return . text
-  litArray _ vs = do
-    vs' <- sequence vs
-    let docs = map unLC vs'
-    return $ return $ brackets $ hcat $ punctuate (comma <> space) docs
-  litList = litArray
-  litSet _ vs = do
-    vs' <- sequence vs
-    let docs = map unLC vs'
-    return $ return $ braces $ hcat $ punctuate (comma <> space) docs
+-- instance ValueSym (LoggingFor lang) where
+--   type Value (LoggingFor lang) = Doc
+--   valueType = error "Not implemented"
 
-instance IndexTranslator (LoggingFor lang) where
-  intToIndex = id
-  indexToInt = id
+-- instance TypeSym (LoggingFor lang) where
+--   bool = bool
+--   int = int
+--   float = float
+--   double = double
+--   char = char
+--   string = string
+--   infile = infile
+--   outfile = outfile
+--   listType = listType
+--   setType = setType
+--   arrayType = arrayType
+--   listInnerType = listInnerType
+--   funcType = funcType
+--   void = void
 
-instance Array (LoggingFor lang) where
-  arrayElem idx' vr' = do
-    idx <- idx'
-    vr <- vr'
-    return $ return $ unLC idx <> brackets (unLC vr)
-  arrayLength = undefined
-  arrayCopy = undefined
+-- instance OOTypeSym (LoggingFor lang) where
+--   obj nm = typeFromData (Object nm) ("Object<" ++ nm ++ ">")
 
--- Not Implemented
-instance VariableElim (LoggingFor lang) where
-  variableName = undefined
-  variableType = undefined
+-- typeFromData :: CodeType -> String -> VSType (LoggingFor lang)
+-- typeFromData tp str = return $ return $ td tp str (text str)
 
-instance VariableValue (LoggingFor lang) where
-  valueOf = id
+-- instance Literal (LoggingFor lang) where
+--   litTrue = litString "True"
+--   litFalse = litString "False"
+--   litChar = return . return . P.char
+--   litDouble = return . return . P.double
+--   litFloat = return . return . P.float
+--   litInt = return . return . P.integer
+--   litString = return . return . text
+--   litArray _ vs = do
+--     vs' <- sequence vs
+--     let docs = map unLC vs'
+--     return $ return $ brackets $ hcat $ punctuate (comma <> space) docs
+--   litList = litArray
+--   litSet _ vs = do
+--     vs' <- sequence vs
+--     let docs = map unLC vs'
+--     return $ return $ braces $ hcat $ punctuate (comma <> space) docs
+
+-- instance IndexTranslator (LoggingFor lang) where
+--   intToIndex = id
+--   indexToInt = id
+
+-- instance Array (LoggingFor lang) where
+--   arrayElem idx' vr' = do
+--     idx <- idx'
+--     vr <- vr'
+--     return $ return $ unLC idx <> brackets (unLC vr)
+--   arrayLength = undefined
+--   arrayCopy = undefined
+
+-- -- Not Implemented
+-- instance VariableElim (LoggingFor lang) where
+--   variableName = undefined
+--   variableType = undefined
+
+-- instance VariableValue (LoggingFor lang) where
+--   valueOf = id

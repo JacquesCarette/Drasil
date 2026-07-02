c470ceb55bd310e6a5d60ac4d7c0a35b609407dd
Jason Balaci <jason@balaci.me>
2026-07-02 17:19:51 -0400

    refactor: move around "rendering"-related code in `drasil-system`

--- code/drasil-system/lib/Drasil/System.hs
+++ code/drasil-system/lib/Drasil/System.hs
@@ -1,11 +1,11 @@
 module Drasil.System (
   module Drasil.System.Core,
   module Drasil.System.DrasilWebsite,
-  module Drasil.System.Render,
-  module Drasil.System.SmithEtAlSRS
+  module Drasil.System.SmithEtAlSRS,
+  module Drasil.System.Transformations
 ) where
 
 import Drasil.System.Core
 import Drasil.System.DrasilWebsite
-import Drasil.System.Render
 import Drasil.System.SmithEtAlSRS
+import Drasil.System.Transformations

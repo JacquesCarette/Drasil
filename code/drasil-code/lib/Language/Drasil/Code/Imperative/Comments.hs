6d7e7ec659ac035e9b297a1b10d572c335426f2e
Jason Balaci <jason@balaci.me>
2026-07-04 11:42:49 -0400

    refactor: create `oneLine` renderers for various 'chunk atoms'

--- code/drasil-code/lib/Language/Drasil/Code/Imperative/Comments.hs
+++ code/drasil-code/lib/Language/Drasil/Code/Imperative/Comments.hs
@@ -13,14 +13,13 @@ import Drasil.Database.SearchTools (DomDefn (definition), defResolve')
 import Language.Drasil
 import Language.Drasil.Code.Imperative.DrasilState (GenState, DrasilState(..))
 import Language.Drasil.CodeSpec (HasOldCodeSpec(..))
-import Language.Drasil.Printers (SingleLine(OneLine), sentenceDoc, unitDoc)
-import Language.Drasil.Printing.Import (spec)
+import Language.Drasil.Printers (oneLineSentenceDoc, oneLineUnitDoc)
 
 -- | Gets a plain renderering of the term for a chunk.
 getTermDoc :: (CodeIdea c) => c -> GenState Doc
 getTermDoc c = do
   g <- get
-  return $ sentenceDoc OneLine $ spec (printfo g) $ phrase $ codeChunk c
+  return $ oneLineSentenceDoc (printfo g) $ phrase $ codeChunk c
 
 -- | Gets a plain rendering of the definition of a chunk, preceded by a colon
 -- as it is intended to follow the term for the chunk. Returns empty if the
@@ -29,13 +28,13 @@ getDefnDoc :: (CodeIdea c) => c -> GenState Doc
 getDefnDoc c = do
   g <- get
   let db = codeSpec g ^. systemdbO
-  return $ ((<+>) colon . sentenceDoc OneLine . spec (printfo g))
+  return $ ((<+>) colon . oneLineSentenceDoc (printfo g))
     (definition $ defResolve' db (codeChunk c ^. uid))
 
 -- | Gets a plain rendering of the unit of a chunk in parentheses,
 -- or empty if it has no unit.
 getUnitsDoc :: (CodeIdea c) => c -> Doc
-getUnitsDoc c = maybe empty (parens . unitDoc OneLine . usymb)
+getUnitsDoc c = maybe empty (parens . oneLineUnitDoc . usymb)
   (getUnit $ codeChunk c)
 
 -- | Generates a comment string for a chunk, including the term,

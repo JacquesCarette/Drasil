0e8214e910cc93d99751436b36d7363a6513f0e8
Jason Balaci <jason@balaci.me>
2026-07-09 20:56:14 -0400

    debugging: inspect `ChunkDB` rather than `SmithEtAlSRS` for chunk dependencies, making debugging tool more generally applicable to anything satisfying `SystemMeta`

--- code/drasil-gen/lib/Drasil/Generator/ChunkDump.hs
+++ code/drasil-gen/lib/Drasil/Generator/ChunkDump.hs
@@ -10,14 +10,13 @@ import Data.Aeson (ToJSON)
 import Data.Aeson.Encode.Pretty (encodePretty)
 import System.Environment (lookupEnv)
 
-import Drasil.Database (dumpChunkDB)
+import Drasil.Database (dumpChunkDB, dumpChunkDeps)
 import Drasil.FileHandling (FileLayout, PathSegment, directory, file, ps)
-import Drasil.System (systemdb)
-import Drasil.SRS (SmithEtAlSRS, traceTable, refbyTable)
+import Drasil.System (systemdb, HasSystemMeta)
 
 -- | Builds the `.drasil` chunk dump directory if the `DEBUG_ENV` environment
 -- variable is non-empty.
-buildDebugData :: SmithEtAlSRS -> IO (Maybe FileLayout)
+buildDebugData :: HasSystemMeta sys => sys -> IO (Maybe FileLayout)
 buildDebugData si = do
   maybeDebugging <- lookupEnv "DEBUG_ENV"
   case maybeDebugging of
@@ -26,13 +25,16 @@ buildDebugData si = do
 
 -- | Internal: For debugging purposes, constructs a `FileLayout` with a dump of
 -- the chunk maps.
-dumpEverything :: SmithEtAlSRS -> FileLayout
+dumpEverything :: HasSystemMeta sys => sys -> FileLayout
 dumpEverything si =
   directory [ps|.drasil|]
-  [ dumpTo [ps|seeds.json|] $ dumpChunkDB (si ^. systemdb),
-    dumpTo [ps|trace.json|] $ si ^. traceTable,
-    dumpTo [ps|reverse_trace.json|] $ si ^. refbyTable
+  [ dumpTo [ps|initial_chunks.json|] $ dumpChunkDB db
+  , dumpTo [ps|initial_chunk_dependants.json|] dependants
+  , dumpTo [ps|initial_chunk_dependencies.json|] dependencies
   ]
+  where
+    db = si ^. systemdb
+    (dependants, dependencies) = dumpChunkDeps db
 
 -- | Internal: Build a JSON file from arbitrary data.
 dumpTo :: ToJSON a => PathSegment -> a -> FileLayout

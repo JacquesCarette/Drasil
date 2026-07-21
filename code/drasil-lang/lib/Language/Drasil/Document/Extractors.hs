395339c42b6ab701cb2a554e822a36200773bb8a
Jason Balaci <jason@balaci.me>
2026-07-21 16:08:38 -0400

    chore: refine import of `Language.Drasil.Development` in `Language.Drasil.Document.Extractors` to `Language.Drasil.Sentence`

--- code/drasil-lang/lib/Language/Drasil/Document/Extractors.hs
+++ code/drasil-lang/lib/Language/Drasil/Document/Extractors.hs
@@ -15,11 +15,11 @@ import qualified Data.Set as S
 import Drasil.Database (UID, ChunkDB, find)
 import Language.Drasil.Chunk.Citation (Citation, BibRef)
 import Language.Drasil.Data.Citation (compareAuthYearTitle)
-import Language.Drasil.Development (lnames)
-import Language.Drasil.Document.Core
-import Language.Drasil.Document.Sections
+import Language.Drasil.Document.Core (RawContent(..), ListTuple, ItemType(..),
+  ListType(..), HasContents(..))
+import Language.Drasil.Document.Sections (Section(Section), SecCons(..))
 import Language.Drasil.ModelExpr.Lang (ModelExpr)
-import Language.Drasil.Sentence (Sentence(..), eS, eS')
+import Language.Drasil.Sentence (lnames, Sentence(..), eS, eS')
 
 -- | Extracts all referenced 'UID's from things that have 'RawContent's.
 extractChRefs :: HasContents a => [a] -> S.Set UID

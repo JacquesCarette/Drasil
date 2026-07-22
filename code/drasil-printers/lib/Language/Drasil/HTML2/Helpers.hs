8227a24d7ee550637ad7697013adc5b83a849dd4
Camila <camiyshibata@gmail.com>
2026-07-22 10:29:12 -0400

    Remove `foldRaw`, split `.helpers` and adjustments to the names

--- code/drasil-printers/lib/Language/Drasil/HTML2/Helpers.hs
+++ code/drasil-printers/lib/Language/Drasil/HTML2/Helpers.hs
@@ -2,35 +2,21 @@
 
 -- | Helper functions for creating HTML printers (specifically, HTML tag wrappers).
 module Language.Drasil.HTML2.Helpers (
-  printSpec, HTMLRenderOptions (..), BibFormatter (..), htmlBibFormatter,
-  specToHTML, exprToHTML, articleTitle, author, foldRaw, stylesheet, renderMath,
-  specialToString, OpenClose (..), fence, pOps
+  HTMLRenderOptions (..), BibFormatter (..),
+  articleTitle, author, stylesheet,
+  specialToString, OpenClose (..), fence, pOps,
+  colon, period, comma, vol, pg, pp, no, ed, editedBy
   ) where
 
 import Data.Text (Text)
-import qualified Data.Text as T (pack)
 import Drasil.Data.Formats.HTML (
-  Attr (..),
-    Format (Emphasis, Span, Subscript, Superscript),
-    HLevel (..),
-    HTMLBody (..), HTMLHead (..), customTag
+  Attr (..), HLevel (..), HTMLBody (..), HTMLHead (..)
   )
-import qualified Drasil.Data.Formats.HTML as HTML (Format (Bold), HTMLBody (Div))
+import qualified Drasil.Data.Formats.HTML as HTML (HTMLBody (Div))
 import Language.Drasil (Special (..))
-import Language.Drasil.Printing.AST
-  ( Expr (..),
-    Fence (Abs, Curly, Norm, Paren),
-    Fonts (Bold, Emph),
-    LinkType (Cite2, External, Internal),
-    Ops (..),
-    OverSymb (Hat),
-    Spacing (Thin),
-    Spec (E, EmptyS, HARDNL, Quote, Ref, S, Sp, Tooltip, (:+:)),
+import Language.Drasil.Printing.AST (
+  Fence (Abs, Curly, Norm, Paren), Ops (..), Spec (..),
   )
-import Language.Drasil.TeX.Monad (D, MathContext (Math), runPrint, toMath)
-import qualified Language.Drasil.TeX.Print as TeX (pExpr)
-import Numeric (showEFloat)
-import Text.PrettyPrint as PLegacy (Doc, text)
 
 -- | Options for converting layout objects ('LayoutObj's) into HTML AST
 data HTMLRenderOptions = HTMLRO
@@ -48,74 +34,6 @@ data BibFormatter = BibFormatter
     -- | Spec rendering
     spec :: Spec -> [HTMLBody]
   }
-
--- | HTML specific bib rendering functions
-htmlBibFormatter :: BibFormatter
-htmlBibFormatter =
-  BibFormatter
-    { emph = \x -> [TextFormat Emphasis [] x],
-      spec = specToHTML
-    }
-
-printSpec :: Spec -> Text
-printSpec (S s) = T.pack s
-printSpec (E e) = T.pack $ show $ renderMath $ TeX.pExpr e
-printSpec (a :+: b) = printSpec a <> printSpec b
-printSpec HARDNL = " "
-printSpec (Sp s) = T.pack $ specialToString s
-printSpec (Ref (Cite2 n) _ a) = printSpec a <> " " <> printSpec n
-printSpec (Ref _ _ a) = printSpec a
-printSpec EmptyS = ""
-printSpec (Quote q) = "\"" <> printSpec q <> "\""
-printSpec (Tooltip _ s) = printSpec s
-
--- | Transforms the Sentences ('Spec's) into HTML (called by 'loToHTML').
-specToHTML :: Spec -> [HTMLBody]
--- Non-mathjax
-specToHTML (E e) = [TextFormat Emphasis [] (foldRaw (exprToHTML e))]
-specToHTML (a :+: b) = foldRaw (specToHTML a ++ specToHTML b)
-specToHTML (S s) = [RawText (T.pack s)]
-specToHTML (Tooltip t s) = [TextFormat Span [Attr "title" (printSpec t)] (specToHTML s)]
-specToHTML (Sp s) = [RawText (T.pack $ specialToString s)]
-specToHTML HARDNL = [Custom (customTag "br") [] []]
-specToHTML (Ref Internal r a) = [Anchor (T.pack $ "#" ++ r) [] (specToHTML a)]
-specToHTML (Ref (Cite2 EmptyS) r a) = [Anchor (T.pack $ "#" ++ r) [] (specToHTML a)]
-specToHTML (Ref (Cite2 n) r a) = Anchor (T.pack $ "#" ++ r) [] (specToHTML a) : specToHTML n
-specToHTML (Ref External r a) = [Anchor (T.pack r) [] (specToHTML a)]
-specToHTML EmptyS = []
-specToHTML (Quote q) = foldRaw $ [RawText "\""] ++ specToHTML q ++ [RawText "\""]
-
--- | Renders expressions in the HTML document (called by multiple functions).
-exprToHTML :: Expr -> [HTMLBody]
-exprToHTML (Dbl d) = [RawText (T.pack $ showEFloat Nothing d "")]
-exprToHTML (Int i) = [RawText (T.pack $ show i)]
-exprToHTML (Str s) = [RawText $ "\"" <> T.pack s <> "\""]
-exprToHTML (Row l) = concatMap exprToHTML l
-exprToHTML (Ident s) = [RawText (T.pack s)]
-exprToHTML (Label s) = [RawText (T.pack s)]
-exprToHTML (Spec s) = [RawText (T.pack $ specialToString s)]
-exprToHTML (Sub e) = [TextFormat Subscript [] (exprToHTML e)]
-exprToHTML (Sup e) = [TextFormat Superscript [] (exprToHTML e)]
-exprToHTML (Over Hat s) = foldRaw (exprToHTML s ++ [RawText "̂"])
-exprToHTML (MO o) = [RawText (pOps o)]
-exprToHTML (Fenced l r e) =
-  foldRaw $ [RawText (fence Open l)] ++ exprToHTML e ++ [RawText (fence Close r)]
-exprToHTML (Font Bold e) = [TextFormat HTML.Bold [] (exprToHTML e)]
-exprToHTML (Font Emph e) = [TextFormat Emphasis [] (exprToHTML e)]
-exprToHTML (Spc Thin) = [RawText " "]
--- Uses TeX for Mathjax for all other exprs
-exprToHTML e =
-  [RawText $ T.pack $ show $ mjDelimDisp $ renderMath $ TeX.pExpr e]
-  where
-    mjDelimDisp d = PLegacy.text "\\(" <> d <> PLegacy.text "\\)"
-
--- | Helper for rendering a 'D' from Latex print.
-printMath :: D -> PLegacy.Doc
-printMath = (`runPrint` Math)
-
--- | Helper for converting and rendering math expressions to Latex print.
-renderMath :: D -> PLegacy.Doc
-renderMath = printMath . toMath
 
 specialToString :: Special -> String
 specialToString Circle = "°"
@@ -189,14 +107,20 @@ pOps SRemove = " - "
 pOps SContains = " in "
 pOps SUnion = " and "
 
+colon, period, comma, vol, pg, pp, no, ed, editedBy :: HTMLBody
+colon = RawText ": "
+period = RawText ". "
+comma = RawText ", "
+vol = RawText "vol. "
+pg = RawText "pg. "
+pp = RawText "pp. "
+no = RawText "no. "
+ed = RawText " ed., "
+editedBy = RawText "Edited by "
+
 articleTitle, author :: [HTMLBody] -> HTMLBody
 articleTitle t = HTML.Div [Attr "class" "title"] [Heading H1 [] t]
 author a       = HTML.Div [Attr "class" "author"] [Heading H2 [] a]
-
-foldRaw :: [HTMLBody] -> [HTMLBody]
-foldRaw [] = []
-foldRaw (RawText a : RawText b : rest) = foldRaw (RawText (a <> b) : rest)
-foldRaw (x : xs) = x : foldRaw xs
 
 -- | Create the link to the CSS file
 stylesheet :: Text -> HTMLHead
---
+++ code/drasil-printers/lib/Language/Drasil/HTML2/Spec.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Language.Drasil.HTML2.Spec (
+  printSpec, specToHTML, exprToHTML, printMath, renderMath
+) where
+
+import Text.PrettyPrint as PLegacy (Doc, text)
+import Data.Text (Text)
+import qualified Data.Text as T (pack)
+import Numeric (showEFloat)
+
+import Language.Drasil.Printing.AST (
+    Expr (..), Fonts (Bold, Emph), LinkType (Cite2, External, Internal),
+    OverSymb (Hat), Spacing (Thin), Spec
+    (E, EmptyS, HARDNL, Quote, Ref, S, Sp, Tooltip, (:+:))
+  )
+import Drasil.Data.Formats.HTML (
+  Attr (..), Format (Emphasis, Span, Subscript, Superscript), HTMLBody (..), customTag
+  )
+import qualified Drasil.Data.Formats.HTML as HTML (Format (Bold))
+import Language.Drasil.TeX.Monad (D, MathContext (Math), runPrint, toMath)
+import qualified Language.Drasil.TeX.Print as TeX (pExpr)
+import Language.Drasil.HTML2.Helpers (specialToString, fence, OpenClose(..), pOps)
+
+printSpec :: Spec -> Text
+printSpec (S s) = T.pack s
+printSpec (E e) = T.pack $ show $ renderMath $ TeX.pExpr e
+printSpec (a :+: b) = printSpec a <> printSpec b
+printSpec HARDNL = " "
+printSpec (Sp s) = T.pack $ specialToString s
+printSpec (Ref (Cite2 n) _ a) = printSpec a <> " " <> printSpec n
+printSpec (Ref _ _ a) = printSpec a
+printSpec EmptyS = ""
+printSpec (Quote q) = "\"" <> printSpec q <> "\""
+printSpec (Tooltip _ s) = printSpec s
+
+-- | Transforms the Sentences ('Spec's) into HTML (called by 'loToHTML').
+specToHTML :: Spec -> [HTMLBody]
+-- Non-mathjax
+specToHTML (E e) = [TextFormat Emphasis [] (exprToHTML e)]
+specToHTML (a :+: b) = specToHTML a ++ specToHTML b
+specToHTML (S s) = [RawText (T.pack s)]
+specToHTML (Tooltip t s) = [TextFormat Span [Attr "title" (printSpec t)] (specToHTML s)]
+specToHTML (Sp s) = [RawText (T.pack $ specialToString s)]
+specToHTML HARDNL = [Custom (customTag "br") [] []]
+specToHTML (Ref Internal r a) = [Anchor (T.pack $ "#" ++ r) [] (specToHTML a)]
+specToHTML (Ref (Cite2 EmptyS) r a) = [Anchor (T.pack $ "#" ++ r) [] (specToHTML a)]
+specToHTML (Ref (Cite2 n) r a) = Anchor (T.pack $ "#" ++ r) [] (specToHTML a) : specToHTML n
+specToHTML (Ref External r a) = [Anchor (T.pack r) [] (specToHTML a)]
+specToHTML EmptyS = []
+specToHTML (Quote q) = [RawText "\""] ++ specToHTML q ++ [RawText "\""]
+
+-- | Renders expressions in the HTML document (called by multiple functions).
+exprToHTML :: Expr -> [HTMLBody]
+exprToHTML (Dbl d) = [RawText (T.pack $ showEFloat Nothing d "")]
+exprToHTML (Int i) = [RawText (T.pack $ show i)]
+exprToHTML (Str s) = [RawText $ "\"" <> T.pack s <> "\""]
+exprToHTML (Row l) = concatMap exprToHTML l
+exprToHTML (Ident s) = [RawText (T.pack s)]
+exprToHTML (Label s) = [RawText (T.pack s)]
+exprToHTML (Spec s) = [RawText (T.pack $ specialToString s)]
+exprToHTML (Sub e) = [TextFormat Subscript [] (exprToHTML e)]
+exprToHTML (Sup e) = [TextFormat Superscript [] (exprToHTML e)]
+exprToHTML (Over Hat s) = exprToHTML s ++ [RawText "̂"]
+exprToHTML (MO o) = [RawText (pOps o)]
+exprToHTML (Fenced l r e) =
+  [RawText (fence Open l)] ++ exprToHTML e ++ [RawText (fence Close r)]
+exprToHTML (Font Bold e) = [TextFormat HTML.Bold [] (exprToHTML e)]
+exprToHTML (Font Emph e) = [TextFormat Emphasis [] (exprToHTML e)]
+exprToHTML (Spc Thin) = [RawText " "]
+-- Uses TeX for Mathjax for all other exprs
+exprToHTML e =
+  [RawText $ T.pack $ show $ mjDelimDisp $ renderMath $ TeX.pExpr e]
+  where
+    mjDelimDisp d = PLegacy.text "\\(" <> d <> PLegacy.text "\\)"
+
+-- | Helper for rendering a 'D' from Latex print.
+printMath :: D -> PLegacy.Doc
+printMath = (`runPrint` Math)
+
+-- | Helper for converting and rendering math expressions to Latex print.
+renderMath :: D -> PLegacy.Doc
+renderMath = printMath . toMath

71c80238eeef944fc25f6082e1fd839e94fae032
Purav Kanda <kandap1@mcmaster.ca>
2026-09-24 16:10:20 -0400

    Finish unit constructor rename in examples and data

--- code/drasil-data/lib/Data/Drasil/Units/Thermodynamics.hs
+++ code/drasil-data/lib/Data/Drasil/Units/Thermodynamics.hs
@@ -8,20 +8,20 @@ import Language.Drasil (cnIES, cn, cn', cn'', Sentence(S),
 import Data.Drasil.SI_Units (centigrade, joule, kilogram, watt, m_2, m_3)
 
 heatCapacity :: UnitDefn
-heatCapacity = compoundUnit' (cncpt''' (mkUid "heatCapacity") (cnIES "heat capacity")
+heatCapacity = compoundUnit (cncpt''' (mkUid "heatCapacity") (cnIES "heat capacity")
   (S "heat capacity (constant pressure)")) (joule /: centigrade)
 
 heatCapSpec :: UnitDefn --Specific heat capacity
-heatCapSpec = compoundUnit' (cncpt''' (mkUid "heatCapSpec") (cn' "specific heat")
+heatCapSpec = compoundUnit (cncpt''' (mkUid "heatCapSpec") (cn' "specific heat")
   (S "heat capacity per unit mass")) (joule /$ (kilogram *: centigrade))
 
 thermalFlux :: UnitDefn
-thermalFlux = compoundUnit' (cncpt''' (mkUid "thermalFlux") (cn'' "heat flux")
+thermalFlux = compoundUnit (cncpt''' (mkUid "thermalFlux") (cn'' "heat flux")
   (S "the rate of heat energy transfer per unit area")) (watt /: m_2)
 
 heatTransferCoef :: UnitDefn
 heatTransferCoef = compoundUnit' "heat transfer coefficient" (watt /$ (m_2 *: centigrade))
 
 volHtGenU :: UnitDefn
-volHtGenU = compoundUnit' (cncpt''' (mkUid "volHtGenU") (cn "volumetric heat generation")
+volHtGenU = compoundUnit (cncpt''' (mkUid "volHtGenU") (cn "volumetric heat generation")
   (S "the rate of heat energy generation per unit volume")) (watt /: m_3)
